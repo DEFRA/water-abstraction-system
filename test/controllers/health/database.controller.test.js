daac2103256b20cb212dcad54db5a34936b3b9df
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-10-06 17:46:49 +0100

    Fix database health check test (#1)
    We copied the existing `test/services/database_health_check.service.test.js` directly from [sroc-charging-module-api](https://github.com/DEFRA/sroc-charging-module-api). In that project we expect 5 key tables to exist. Or, we did at the time we wrote the test. We would now expect more!
    
    When we copied the test across it all worked fine because we were again pointing at an existing DB with lots of tables. However, when this project got pushed to GitHub the CI started failing because we were connecting to a DB that has nothing!
    
    We've yet to add any migrations so nothing exists but the DB. We've confirmed that as long as the DB exists the connection works and that is sufficient as a health check.
    
    Rather than force a result from PostgreSQL we're happy the fact we don't error is confirmation that the DB is 'healthy'.

--- test/controllers/health/database.controller.test.js
+++ test/controllers/health/database.controller.test.js
@@ -31,13 +31,7 @@ describe('Database controller', () => {
     it('returns stats about each table', async () => {
       const response = await server.inject(options)
 
-      const payload = JSON.parse(response.payload)
-
       expect(response.statusCode).to.equal(200)
-
-      // We expect at least 5 tables to exist and be returned in the results
-      expect(payload.length).to.be.at.least(5)
-      expect(payload[0].relname).to.exist()
     })
   })
 })
