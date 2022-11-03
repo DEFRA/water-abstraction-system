ac47c4122d89245a388bb0e3430c81fc2d4cc003
Rebecca Ransome <beckyrose200@aol.com>
2022-11-03 15:03:42 +0000

    Returning all charge versions id and licence ref

--- app/services/test/supplementary.service.js
+++ app/services/test/supplementary.service.js
@@ -4,20 +4,30 @@
  * @module TestSupplementaryService
  */
 
+const { db } = require('../../../db/db')
+
 /**
  * Returns charge versions selected for supplementary billing
  * At present this returns a set response until further development
 */
+
+// Format into the response and return the data
 class TestSupplementaryService {
   static async go () {
+    const chargeVersions = await this._fetchChargeVersions()
     const response = {
-      chargeVersions: [
-        { id: '986d5b14-8686-429e-9ae7-1164c1300f8d', licenceRef: 'AT/SROC/SUPB/01' },
-        { id: 'ca0e4a77-bb13-4eef-a1a1-2ccf9e302cc4', licenceRef: 'AT/SROC/SUPB/03' }
-      ]
+      chargeVersions
     }
 
     return response
+  }
+
+  static async _fetchChargeVersions () {
+    const chargeVersions = db.table('water.charge_versions')
+      .select('chargeVersionId')
+      .select('licenceRef')
+
+    return chargeVersions
   }
 }
 
