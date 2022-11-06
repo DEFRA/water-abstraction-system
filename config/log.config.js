894abfb42da021d9cd3dbb17fe825c98b220489c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-11-06 23:37:16 +0000

    Update .env.example and put in alpha order

--- config/log.config.js
+++ config/log.config.js
@@ -3,8 +3,8 @@
 const config = {
   // Credit to https://stackoverflow.com/a/323546/6117745 for how to handle
   // converting the env var to a boolean
-  logInTest: (String(process.env.LOG_IN_TEST) === 'true') || false,
-  logAssetRequests: (String(process.env.LOG_ASSET_REQUESTS) === 'true') || false
+  logAssetRequests: (String(process.env.LOG_ASSET_REQUESTS) === 'true') || false,
+  logInTest: (String(process.env.LOG_IN_TEST) === 'true') || false
 }
 
 module.exports = config
