7e6c16946ac1085aaee8f08a57293c6efb322af2
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-11-08 11:20:08 +0000

    Standardise naming of request object (#17)
    In a few places in the code 'req' is the name given to the request object. The naming convention of the request object in Hapi is 'request'. Where 'req' has been used these will be replaced with 'request'.

--- app/controllers/health/database.controller.js
+++ app/controllers/health/database.controller.js
@@ -3,7 +3,7 @@
 const DatabaseHealthCheckService = require('../../services/database_health_check.service.js')
 
 class DatabaseController {
-  static async index (_req, h) {
+  static async index (_request, h) {
     const result = await DatabaseHealthCheckService.go()
 
     return h.response(result).code(200)
