86adb40c10c0776f67169493a091c4524955cb4c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-11-14 00:59:40 +0000

    Add a default value for the timeout
    We'll be setting all our environments to this so it makes sense to add it as a default. That way we don't have to specify it in our GitHub CI.

--- config/request.config.js
+++ config/request.config.js
@@ -1,7 +1,7 @@
 'use strict'
 
 const config = {
-  requestTimeout: parseInt(process.env.REQUEST_TIMEOUT),
+  requestTimeout: parseInt(process.env.REQUEST_TIMEOUT) || 5000,
   httpProxy: process.env.http_proxy,
   httpsProxy: process.env.https_proxy,
   noProxy: process.env.no_proxy
