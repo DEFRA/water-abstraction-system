d8796598e8fe3bb8c6e97196676fb768cc0f3651
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-11-22 12:53:38 +0000

    Update presenter to respond with unique licences

--- app/presenters/supplementary.presenter.js
+++ app/presenters/supplementary.presenter.js
@@ -13,17 +13,52 @@ class SupplementaryPresenter {
     return this._presentation(this._data)
   }
 
+  /**
+   * Creates an array of unique licence objects from the combined data received
+   *
+   * We know the main query in `SupplementaryService` is based on charge versions. As a licence may be linked to
+   * multiple charge versions, it is possible that the licence ID and reference will feature multiple times in the
+   * results.
+   *
+   * To make it easier to confirm we are including the right licences in the Supplementary bill run process we want to
+   * extract a unique list of licence ID's and references.
+   */
+  _licences (data) {
+    // Use map to generate an array of just licence IDs
+    const justLicenceIds = data.chargeVersions.map((chargeVersion) => chargeVersion.licenceId)
+    // Set is used to store unique values. So, if you what you pass in contains duplicates Set will return just the
+    // unique ones
+    const uniqueLicenceIds = new Set(justLicenceIds)
+
+    const licences = []
+    for (const id of uniqueLicenceIds) {
+      // Iterate through our unique Licence Id's and use them to find the index for the first matching entry in the
+      // charge versions data
+      const index = data.chargeVersions.findIndex((chargeVersion) => chargeVersion.licenceId === id)
+      // Destructure the licenceId and licenceRef from the matching object
+      const { licenceId, licenceRef } = data.chargeVersions[index]
+
+      // Push a new object into our array of licences
+      licences.push({ licenceId, licenceRef })
+    }
+
+    return licences
+  }
+
   _presentation (data) {
+    const licences = this._licences(data)
     const chargeVersions = data.chargeVersions.map((chargeVersion) => {
       return {
         chargeVersionId: chargeVersion.chargeVersionId,
         licenceRef: chargeVersion.licenceRef,
+        licenceId: chargeVersion.licenceId,
         scheme: chargeVersion.scheme,
         endDate: chargeVersion.endDate
       }
     })
 
     return {
+      licences,
       chargeVersions
     }
   }
--- test/presenters/supplementary.presenter.test.js
+++ test/presenters/supplementary.presenter.test.js
@@ -19,15 +19,17 @@ describe('Supplementary presenter', () => {
         chargeVersions: [
           {
             chargeVersionId: '4b5cbe04-a0e2-468c-909e-1e2d93810ba8',
-            licenceRef: 'AT/SROC/SUPB/01',
             scheme: 'sroc',
-            endDate: null
+            endDate: null,
+            licenceRef: 'AT/SROC/SUPB/01',
+            licenceId: '0000579f-0f8f-4e21-b63a-063384ad32c8'
           },
           {
             chargeVersionId: '732fde85-fd3b-44e8-811f-8e6f4eb8cf6f',
-            licenceRef: 'AT/SROC/SUPB/01',
             scheme: 'sroc',
-            endDate: null
+            endDate: null,
+            licenceRef: 'AT/SROC/SUPB/01',
+            licenceId: '0000579f-0f8f-4e21-b63a-063384ad32c8'
           }
         ]
       }
@@ -36,6 +38,12 @@ describe('Supplementary presenter', () => {
     it('correctly presents the data', () => {
       const presenter = new SupplementaryPresenter(data)
       const result = presenter.go()
+
+      expect(result.licences).to.have.length(1)
+      expect(result.licences[0]).to.equal({
+        licenceId: data.chargeVersions[0].licenceId,
+        licenceRef: data.chargeVersions[0].licenceRef
+      })
 
       expect(result.chargeVersions).to.have.length(2)
       expect(result.chargeVersions[0]).to.equal(data.chargeVersions[0])
@@ -52,6 +60,8 @@ describe('Supplementary presenter', () => {
     it('correctly presents the data', () => {
       const presenter = new SupplementaryPresenter(data)
       const result = presenter.go()
+
+      expect(result.licences).to.be.empty()
 
       expect(result.chargeVersions).to.be.empty()
     })
