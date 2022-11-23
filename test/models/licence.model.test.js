36676eb8a37295395a1d6b7671b582f797798c9b
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2022-11-23 13:59:47 +0000

    created simple licence model test

---
+++ test/models/licence.model.test.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Thing under test
+const Licence = require('../../app/models/licence.model.js')
+
+describe.only('Licence model', () => {
+  it('returns data', async () => {
+    const query = await Licence.query()
+
+    expect(query).to.exist()
+  })
+})
