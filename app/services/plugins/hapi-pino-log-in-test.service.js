e22d85ee43ffb85f3cb174f858d19b147a1bc892
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-09 16:44:48 +0000

    Refactor Hapi-pino logger to clean things up
    First tweak is around config. We were pulling it into our main `app/server.js` and `HapiPinoIgnoreRequestService`. This meant we also needed to stub the config to get tests working for the `HapiPinoIgnoreRequestService`. By moving the config into the plugin we can use it where its needed, and ensure it gets passed through as an arg. This means `LogConfig` is now only referenced in one place and we can remove the stubbing in our `HapiPinoIgnoreRequestService` tests because we're passing the critical option through.
    
    The second was a mix of conventions. We were using a separate service to contain the logic of whether a request should be logged but a function in the plugin to covere logging in tests. By moving the logging in tests logic to a service we get consistency _and_ we can write some unit tests to cover it.

---
+++ app/services/plugins/hapi-pino-log-in-test.service.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/**
+ * Used by HapiPinoPlugin to determine which requests to log
+ * @module HapiPinoLogInTestService
+ */
+
+/**
+ * Returns test configuration options for the hapi-pino logger
+ *
+ * When we run our unit tests we don't want the output polluted by noise from the logger. So as a default we set the
+ * configuration to tell hapi-pino to ignore all events.
+ *
+ * But there will be times when trying to diagnose an issue that we will want log output. So using an env var we can
+ * override the default and tell hapi-pino to log everything as normal.
+ *
+ * @returns {Object} an empty object or one containing Hapi-pino config to tell it not to log events
+ */
+function go (logInTest) {
+  if (process.env.NODE_ENV !== 'test' || logInTest) {
+    return {}
+  }
+
+  return {
+    // Don't log requests etc
+    logEvents: false,
+    // Don't log anything tagged with DEBUG or info, for example, req.log(['INFO'], 'User is an admin')
+    ignoredEventTags: { log: ['DEBUG', 'INFO'], request: ['DEBUG', 'INFO'] }
+  }
+}
+
+module.exports = {
+  go
+}
--- test/services/plugins/hapi-pino-ignore-request.service.test.js
+++ test/services/plugins/hapi-pino-ignore-request.service.test.js
@@ -3,27 +3,17 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
-const Sinon = require('sinon')
 
-const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { describe, it } = exports.lab = Lab.script()
 const { expect } = Code
-
-// Things we need to stub
-const LogConfig = require('../../../config/log.config.js')
 
 // Thing under test
 const HapiPinoIgnoreRequestService = require('../../../app/services/plugins/hapi-pino-ignore-request.service.js')
 
 describe('Hapi Pino Ignore Request service', () => {
-  const _options = {}
-
-  afterEach(() => {
-    Sinon.restore()
-  })
-
   describe("when the request is for the root '/'", () => {
     it('returns true', () => {
-      const result = HapiPinoIgnoreRequestService.go(_options, { path: '/' })
+      const result = HapiPinoIgnoreRequestService.go({ logAssetRequests: false }, { path: '/' })
 
       expect(result).to.be.true()
     })
@@ -31,7 +21,7 @@ describe('Hapi Pino Ignore Request service', () => {
 
   describe("when the request is for '/status'", () => {
     it('returns true', () => {
-      const result = HapiPinoIgnoreRequestService.go(_options, { path: '/status' })
+      const result = HapiPinoIgnoreRequestService.go({ logAssetRequests: false }, { path: '/status' })
 
       expect(result).to.be.true()
     })
@@ -39,24 +29,16 @@ describe('Hapi Pino Ignore Request service', () => {
 
   describe('when the request is for an asset', () => {
     describe('and LOG_ASSET_REQUESTS is false', () => {
-      beforeEach(() => {
-        Sinon.replace(LogConfig, 'logAssetRequests', false)
-      })
-
       it('returns true', () => {
-        const result = HapiPinoIgnoreRequestService.go(_options, { path: '/assets/stylesheets/application.css' })
+        const result = HapiPinoIgnoreRequestService.go({ logAssetRequests: false }, { path: '/assets/stylesheets/application.css' })
 
         expect(result).to.be.true()
       })
     })
 
     describe('and LOG_ASSET_REQUESTS is true', () => {
-      beforeEach(() => {
-        Sinon.replace(LogConfig, 'logAssetRequests', true)
-      })
-
       it('returns false', () => {
-        const result = HapiPinoIgnoreRequestService.go(_options, { path: '/assets/stylesheets/application.css' })
+        const result = HapiPinoIgnoreRequestService.go({ logAssetRequests: true }, { path: '/assets/stylesheets/application.css' })
 
         expect(result).to.be.false()
       })
@@ -65,7 +47,7 @@ describe('Hapi Pino Ignore Request service', () => {
 
   describe("when the request is not for '/status' or an asset", () => {
     it('returns false', () => {
-      const result = HapiPinoIgnoreRequestService.go(_options, { path: '/bill-run/stuff' })
+      const result = HapiPinoIgnoreRequestService.go({ logAssetRequests: false }, { path: '/bill-run/stuff' })
 
       expect(result).to.be.false()
     })
