405092077ea4c90db49132c5f06539045642bcbf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-09 09:04:00 +0000

    Update services
    Note - this forced us to review how we'd handled 'current date' in the BillingPeriodService. We found Sinon's [useFakeTimers()](https://sinonjs.org/releases/latest/fake-timers/) which is the more appropriate mechanism for controlling what `Date` returns during our tests.

--- app/services/supplementary-billing/fetch-licences.service.js
+++ app/services/supplementary-billing/fetch-licences.service.js
@@ -7,6 +7,7 @@
 
 const LicenceModel = require('../../models/licence.model.js')
 
+<<<<<<< HEAD
 class FetchLicencesService {
   /**
    * Fetches licences flagged for supplementary billing that are linked to the selected region
@@ -20,19 +21,34 @@ class FetchLicencesService {
    */
   static async go (region) {
     const licences = await this._fetch(region)
+=======
+/**
+ * Fetches licences flagged for supplementary billing that are linked to the selected region
+ *
+ * This is a temporary service to help us confirm we are selecting the correct data to use when creating a
+ * supplementary bill run. Its primary aim is to meet the acceptance criteria defined in WATER-3787.
+ *
+ * @param {Objecy} region Instance of `RegionModel` for the selected region
+ *
+ * @returns {Object[]} Array of matching `LicenceModel`
+ */
+async function go (region) {
+  const licences = await _fetch(region)
+>>>>>>> 901b57f (Update services)
 
-    return licences
-  }
-
-  static async _fetch (region) {
-    const result = await LicenceModel.query()
-      .innerJoinRelated('chargeVersions')
-      .where('region_id', region.regionId)
-      .where('include_in_supplementary_billing', 'yes')
-      .where('chargeVersions.scheme', 'sroc')
-
-    return result
-  }
+  return licences
 }
 
-module.exports = FetchLicencesService
+async function _fetch (region) {
+  const result = await LicenceModel.query()
+    .innerJoinRelated('chargeVersions')
+    .where('region_id', region.regionId)
+    .where('include_in_supplementary_billing', 'yes')
+    .where('chargeVersions.scheme', 'sroc')
+
+  return result
+}
+
+module.exports = {
+  go
+}
--- app/services/supplementary-billing/fetch-region.service.js
+++ app/services/supplementary-billing/fetch-region.service.js
@@ -7,31 +7,31 @@
 
 const RegionModel = require('../../models/region.model.js')
 
-class FetchRegionService {
-  /**
-   * Fetches the region with the matching NALD Region ID
-   *
-   * This is a temporary service to help us confirm we are selecting the correct data to use when creating a
-   * supplementary bill run. Its primary aim is to meet the acceptance criteria defined in WATER-3787.
-   *
-   * @param {string} naldRegionId The NALD region ID (a number between 1 to 9, 9 being the test region) for the region
-   * to find
-   *
-   * @returns {Object} Instance of `RegionModel` with the matching NALD Region ID
-   */
-  static async go (naldRegionId) {
-    const region = await this._fetch(naldRegionId)
+/**
+ * Fetches the region with the matching NALD Region ID
+ *
+ * This is a temporary service to help us confirm we are selecting the correct data to use when creating a
+ * supplementary bill run. Its primary aim is to meet the acceptance criteria defined in WATER-3787.
+ *
+ * @param {string} naldRegionId The NALD region ID (a number between 1 to 9, 9 being the test region) for the region
+ * to find
+ *
+ * @returns {Object} Instance of `RegionModel` with the matching NALD Region ID
+ */
+async function go (naldRegionId) {
+  const region = await _fetch(naldRegionId)
 
-    return region
-  }
-
-  static async _fetch (naldRegionId) {
-    const result = await RegionModel.query()
-      .where('nald_region_id', naldRegionId)
-      .first()
-
-    return result
-  }
+  return region
 }
 
-module.exports = FetchRegionService
+async function _fetch (naldRegionId) {
+  const result = await RegionModel.query()
+    .where('nald_region_id', naldRegionId)
+    .first()
+
+  return result
+}
+
+module.exports = {
+  go
+}
