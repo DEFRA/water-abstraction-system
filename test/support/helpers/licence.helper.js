b3b8f78c47902e87005aa942973a345cf153376f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-09 09:12:03 +0000

    Refactor test helpers

--- test/support/helpers/licence.helper.js
+++ test/support/helpers/licence.helper.js
@@ -6,48 +6,49 @@
 
 const { db } = require('../../../db/db.js')
 
-class LicenceHelper {
-  /**
-   * Add a new licence
-   *
-   * If no `data` is provided, default values will be used. These are
-   *
-   * - `licence_ref` - 01/123
-   * - `region_id` - bd114474-790f-4470-8ba4-7b0cc9c225d7
-   *
-   * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
-   *
-   * @returns {string} The ID of the newly created record
-   */
-  static async add (data = {}) {
-    const insertData = this.defaults(data)
+/**
+ * Add a new licence
+ *
+ * If no `data` is provided, default values will be used. These are
+ *
+ * - `licence_ref` - 01/123
+ * - `region_id` - bd114474-790f-4470-8ba4-7b0cc9c225d7
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ *
+ * @returns {string} The ID of the newly created record
+ */
+async function add (data = {}) {
+  const insertData = defaults(data)
 
-    const result = await db.table('water.licences')
-      .insert(insertData)
-      .returning('licence_id')
+  const result = await db.table('water.licences')
+    .insert(insertData)
+    .returning('licence_id')
 
-    return result
+  return result
+}
+
+/**
+ * Returns the defaults used when creating a new licence
+ *
+ * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
+ * for use in tests to avoid having to duplicate values.
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ */
+function defaults (data = {}) {
+  const defaults = {
+    licence_ref: '01/123',
+    region_id: 'bd114474-790f-4470-8ba4-7b0cc9c225d7'
   }
 
-  /**
-   * Returns the defaults used when creating a new licence
-   *
-   * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
-   * for use in tests to avoid having to duplicate values.
-   *
-   * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
-   */
-  static defaults (data = {}) {
-    const defaults = {
-      licence_ref: '01/123',
-      region_id: 'bd114474-790f-4470-8ba4-7b0cc9c225d7'
-    }
-
-    return {
-      ...defaults,
-      ...data
-    }
+  return {
+    ...defaults,
+    ...data
   }
 }
 
-module.exports = LicenceHelper
+module.exports = {
+  add,
+  defaults
+}
--- test/support/helpers/region.helper.js
+++ test/support/helpers/region.helper.js
@@ -6,48 +6,49 @@
 
 const { db } = require('../../../db/db.js')
 
-class RegionHelper {
-  /**
-   * Add a new region
-   *
-   * If no `data` is provided, default values will be used. These are
-   *
-   * - `charge_region_id` - S
-   * - `nald_region_id` - 9
-   *
-   * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
-   *
-   * @returns {string} The ID of the newly created record
-   */
-  static async add (data = {}) {
-    const insertData = this.defaults(data)
+/**
+ * Add a new region
+ *
+ * If no `data` is provided, default values will be used. These are
+ *
+ * - `charge_region_id` - S
+ * - `nald_region_id` - 9
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ *
+ * @returns {string} The ID of the newly created record
+ */
+async function add (data = {}) {
+  const insertData = defaults(data)
 
-    const result = await db.table('water.regions')
-      .insert(insertData)
-      .returning('region_id')
+  const result = await db.table('water.regions')
+    .insert(insertData)
+    .returning('region_id')
 
-    return result
+  return result
+}
+
+/**
+ * Returns the defaults used when creating a new region
+ *
+ * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
+ * for use in tests to avoid having to duplicate values.
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ */
+function defaults (data = {}) {
+  const defaults = {
+    charge_region_id: 'S',
+    nald_region_id: 9
   }
 
-  /**
-   * Returns the defaults used when creating a new region
-   *
-   * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
-   * for use in tests to avoid having to duplicate values.
-   *
-   * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
-   */
-  static defaults (data = {}) {
-    const defaults = {
-      charge_region_id: 'S',
-      nald_region_id: 9
-    }
-
-    return {
-      ...defaults,
-      ...data
-    }
+  return {
+    ...defaults,
+    ...data
   }
 }
 
-module.exports = RegionHelper
+module.exports = {
+  add,
+  defaults
+}
