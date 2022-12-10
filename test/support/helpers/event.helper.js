e471685897e40e74c5fa18c5106c0a334ed7a4bd
Jason Claxton <jason.claxton@defra.gov.uk>
2022-12-10 18:20:31 +0000

    created event helper

---
+++ test/support/helpers/event.helper.js
@@ -0,0 +1,61 @@
+'use strict'
+
+/**
+ * @module EventHelper
+ */
+
+const { db } = require('../../../db/db.js')
+
+/**
+ * Add a new event
+ *
+ * If no `data` is provided, default values will be used. These are
+ *
+ * - `type` - billing-batch
+ * - `subtype` - supplementary
+ * - `issuer` - test.user@defra.gov.uk
+ * - `licences` - []
+ * - `metadata` - {}
+ * - `status` - start
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ *
+ * @returns {string} The ID of the newly created record
+ */
+async function add (data = {}) {
+  const insertData = defaults(data)
+
+  const result = await db.table('water.events')
+    .insert(insertData)
+    .returning('event_id')
+
+  return result
+}
+
+/**
+ * Returns the defaults used when creating a new event
+ *
+ * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
+ * for use in tests to avoid having to duplicate values.
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ */
+function defaults (data = {}) {
+  const defaults = {
+    type: 'billing-batch',
+    subtype: 'supplementary',
+    issuer: 'test.user@defra.gov.uk',
+    licences: [],
+    metadata: {},
+    status: 'start'
+  }
+
+  return {
+    ...defaults,
+    ...data
+  }
+}
+
+module.exports = {
+  add
+}
