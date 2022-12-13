2b26f45034d8cd934307680fda1e18fc5322889c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-13 17:26:54 +0000

    All debugging removed

--- app/services/http-request.service.js
+++ app/services/http-request.service.js
@@ -5,12 +5,11 @@
  * @module HttpRequestService
  */
 
-const { HttpProxyAgent, HttpsProxyAgent } = require('hpagent')
+const { HttpsProxyAgent } = require('hpagent')
 
 const requestConfig = require('../../config/request.config.js')
 
 async function go (url) {
-  console.log('🚀 ~ file: http-request.service.js:13 ~ go ~ url', url)
   // As of v12, the got dependency no longer supports CJS modules. This causes us a problem as we are locked into
   // using these for the time being. Some workarounds are provided here:
   // https://github.com/sindresorhus/got/issues/1789 We have gone the route of using await import('got'). We cannot do
@@ -23,63 +22,17 @@ async function go (url) {
 
   try {
     const options = _requestOptions()
-    console.log('🚀 ~ file: http-request.service.js:25 ~ go ~ options', options)
 
     result.response = await got.get(url, options)
     // If the result is not 2xx or 3xx Got will mark the result as unsuccesful using the response object's `ok:`
     // property
     result.succeeded = result.response.ok
   } catch (error) {
-    console.log('🚀 ~ file: http-request.service.js:32 ~ go ~ error', error)
     result.response = error
   }
 
   return result
 }
-
-// function _requestAgent () {
-//   const baseProxyOptions = {
-//     maxFreeSockets: 256,
-//     maxSockets: 256,
-//     keepAlive: false
-//   }
-
-//   const httpsProxyUrl = new URL(requestConfig.httpsProxy)
-//   const https = new HttpsProxyAgent({
-//     ...baseProxyOptions,
-//     proxy: {
-//       protocol: httpsProxyUrl.protocol,
-//       hostname: httpsProxyUrl.hostname,
-//       port: httpsProxyUrl.port,
-//       username: null,
-//       password: null,
-//     }
-//   })
-//   https.defaultPort = 3128
-
-//   const httpProxyUrl = new URL(requestConfig.httpProxy)
-//   const http = new HttpProxyAgent({
-//     ...baseProxyOptions,
-//     proxy: {
-//       protocol: httpProxyUrl.protocol,
-//       hostname: httpProxyUrl.hostname,
-//       port: httpProxyUrl.port,
-//       username: null,
-//       password: null,
-//     }
-//   })
-//   http.defaultPort = 3128
-
-//   console.log('🚀 ~ file: http-request.service.js:70 ~ _requestAgents ~ https', https)
-//   console.log('🚀 ~ file: http-request.service.js:71 ~ _requestAgents ~ https.proxy', https.proxy)
-//   console.log('🚀 ~ file: http-request.service.js:72 ~ _requestAgents ~ http', http)
-//   console.log('🚀 ~ file: http-request.service.js:73 ~ _requestAgents ~ http.proxy', http.proxy)
-
-//   return {
-//     http,
-//     https
-//   }
-// }
 
 function _requestOptions () {
   const options = {
@@ -94,15 +47,14 @@ function _requestOptions () {
     },
     timeout: {
       request: requestConfig.requestTimeout
-    },
-    agent: {
-      https: new HttpsProxyAgent({ proxy: requestConfig.httpsProxy })
     }
   }
 
-  // if (requestConfig.httpProxy) {
-  //   options.agent = _requestAgent()
-  // }
+  if (requestConfig.httpProxy) {
+    options.agent = {
+      https: new HttpsProxyAgent({ proxy: requestConfig.httpProxy })
+    }
+  }
 
   return options
 }
