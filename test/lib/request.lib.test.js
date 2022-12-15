5c3dcaf81f0169fa61a34aad51438f555255ddc3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-15 15:42:12 +0000

    Make tests clearer they are for get() method

--- test/lib/request.lib.test.js
+++ test/lib/request.lib.test.js
@@ -23,88 +23,55 @@ describe('RequestLib', () => {
     Nock.cleanAll()
   })
 
-  describe('When a request succeeds', () => {
-    beforeEach(() => {
-      Nock(testDomain).get('/').reply(200, { data: 'hello world' })
-    })
-
-    describe('the result it returns', () => {
-      it("has a 'succeeded' property marked as true", async () => {
-        const result = await RequestLib.get(testDomain)
-
-        expect(result.succeeded).to.be.true()
-      })
-
-      it("has a 'response' property containg the web response", async () => {
-        const result = await RequestLib.get(testDomain)
-
-        expect(result.response).to.exist()
-        expect(result.response.statusCode).to.equal(200)
-        expect(result.response.body).to.equal('{"data":"hello world"}')
-      })
-    })
-  })
-
-  describe('When a request fails', () => {
-    describe('because the response was a 500', () => {
+  describe('#get()', () => {
+    describe('When a request succeeds', () => {
       beforeEach(() => {
-        Nock(testDomain).get('/').reply(500, { data: 'hello world' })
+        Nock(testDomain).get('/').reply(200, { data: 'hello world' })
       })
 
       describe('the result it returns', () => {
-        it("has a 'succeeded' property marked as false", async () => {
+        it("has a 'succeeded' property marked as true", async () => {
           const result = await RequestLib.get(testDomain)
 
-          expect(result.succeeded).to.be.false()
+          expect(result.succeeded).to.be.true()
         })
 
         it("has a 'response' property containg the web response", async () => {
           const result = await RequestLib.get(testDomain)
 
           expect(result.response).to.exist()
-          expect(result.response.statusCode).to.equal(500)
+          expect(result.response.statusCode).to.equal(200)
           expect(result.response.body).to.equal('{"data":"hello world"}')
         })
       })
     })
 
-    describe('because there was a network issue', () => {
-      beforeEach(() => {
-        Nock(testDomain).get('/').replyWithError({ code: 'ECONNRESET' })
-      })
-
-      describe('the result it returns', () => {
-        it("has a 'succeeded' property marked as false", async () => {
-          const result = await RequestLib.get(testDomain)
-
-          expect(result.succeeded).to.be.false()
+    describe('When a request fails', () => {
+      describe('because the response was a 500', () => {
+        beforeEach(() => {
+          Nock(testDomain).get('/').reply(500, { data: 'hello world' })
         })
 
-        it("has a 'response' property containg the error", async () => {
-          const result = await RequestLib.get(testDomain)
+        describe('the result it returns', () => {
+          it("has a 'succeeded' property marked as false", async () => {
+            const result = await RequestLib.get(testDomain)
 
-          expect(result.response).to.exist()
-          expect(result.response).to.be.an.error()
-          expect(result.response.code).to.equal('ECONNRESET')
+            expect(result.succeeded).to.be.false()
+          })
+
+          it("has a 'response' property containg the web response", async () => {
+            const result = await RequestLib.get(testDomain)
+
+            expect(result.response).to.exist()
+            expect(result.response.statusCode).to.equal(500)
+            expect(result.response.body).to.equal('{"data":"hello world"}')
+          })
         })
       })
-    })
 
-    describe('because request timed out', () => {
-      beforeEach(async () => {
-        // Set the timeout value to 50ms for these tests
-        Sinon.replace(requestConfig, 'timeout', 50)
-      })
-
-      // Because of the fake delay in this test, Lab will timeout (by default tests have 2 seconds to finish). So, we
-      // have to override the timeout for this specific test to all it to complete
-      describe('and all retries fail', { timeout: 5000 }, () => {
-        beforeEach(async () => {
-          Nock(testDomain)
-            .get(() => true)
-            .delay(100)
-            .reply(200, { data: 'hello world' })
-            .persist()
+      describe('because there was a network issue', () => {
+        beforeEach(() => {
+          Nock(testDomain).get('/').replyWithError({ code: 'ECONNRESET' })
         })
 
         describe('the result it returns', () => {
@@ -119,70 +86,105 @@ describe('RequestLib', () => {
 
             expect(result.response).to.exist()
             expect(result.response).to.be.an.error()
-            expect(result.response.code).to.equal('ETIMEDOUT')
+            expect(result.response.code).to.equal('ECONNRESET')
           })
         })
       })
 
-      describe('and a retry succeeds', () => {
+      describe('because request timed out', () => {
         beforeEach(async () => {
-          // The first response will time out, the second response will return OK
-          Nock(testDomain)
-            .get(() => true)
-            .delay(100)
-            .reply(200, { data: 'hello world' })
-            .get(() => true)
-            .reply(200, { data: 'hello world' })
+          // Set the timeout value to 50ms for these tests
+          Sinon.replace(requestConfig, 'timeout', 50)
         })
 
-        describe('the result it returns', () => {
-          it("has a 'succeeded' property marked as true", async () => {
-            const result = await RequestLib.get(testDomain)
-
-            expect(result.succeeded).to.be.true()
+        // Because of the fake delay in this test, Lab will timeout (by default tests have 2 seconds to finish). So, we
+        // have to override the timeout for this specific test to all it to complete
+        describe('and all retries fail', { timeout: 5000 }, () => {
+          beforeEach(async () => {
+            Nock(testDomain)
+              .get(() => true)
+              .delay(100)
+              .reply(200, { data: 'hello world' })
+              .persist()
           })
 
-          it("has a 'response' property containg the web response", async () => {
-            const result = await RequestLib.get(testDomain)
+          describe('the result it returns', () => {
+            it("has a 'succeeded' property marked as false", async () => {
+              const result = await RequestLib.get(testDomain)
 
-            expect(result.response).to.exist()
-            expect(result.response.statusCode).to.equal(200)
-            expect(result.response.body).to.equal('{"data":"hello world"}')
+              expect(result.succeeded).to.be.false()
+            })
+
+            it("has a 'response' property containg the error", async () => {
+              const result = await RequestLib.get(testDomain)
+
+              expect(result.response).to.exist()
+              expect(result.response).to.be.an.error()
+              expect(result.response.code).to.equal('ETIMEDOUT')
+            })
+          })
+        })
+
+        describe('and a retry succeeds', () => {
+          beforeEach(async () => {
+            // The first response will time out, the second response will return OK
+            Nock(testDomain)
+              .get(() => true)
+              .delay(100)
+              .reply(200, { data: 'hello world' })
+              .get(() => true)
+              .reply(200, { data: 'hello world' })
+          })
+
+          describe('the result it returns', () => {
+            it("has a 'succeeded' property marked as true", async () => {
+              const result = await RequestLib.get(testDomain)
+
+              expect(result.succeeded).to.be.true()
+            })
+
+            it("has a 'response' property containg the web response", async () => {
+              const result = await RequestLib.get(testDomain)
+
+              expect(result.response).to.exist()
+              expect(result.response.statusCode).to.equal(200)
+              expect(result.response.body).to.equal('{"data":"hello world"}')
+            })
           })
         })
       })
     })
-  })
 
-  describe('When I provide additional options', () => {
-    describe('and they expand the default options', () => {
-      beforeEach(() => {
-        Nock(testDomain).get('/').reply(200, { data: 'hello world' })
+    describe('When I provide additional options', () => {
+      describe('and they expand the default options', () => {
+        beforeEach(() => {
+          Nock(testDomain).get('/').reply(200, { data: 'hello world' })
+        })
+
+        it('adds them to the options used when making the request', async () => {
+          // We tell Got to return the response as json rather than text. We can confirm the option has been applied by
+          // checking the result.reponse.body below.
+          const options = { responseType: 'json' }
+          const result = await RequestLib.get(testDomain, options)
+
+          expect(result.succeeded).to.be.true()
+          expect(result.response.body).to.equal({ data: 'hello world' })
+        })
       })
 
-      it('adds them to the options used when making the request', async () => {
-        // We tell Got to return the response as json rather than text. We can confirm the option has been applied by
-        // checking the result.reponse.body below.
-        const options = { responseType: 'json' }
-        const result = await RequestLib.get(testDomain, options)
+      describe('and they replace a default option', () => {
+        beforeEach(() => {
+          Nock(testDomain).get('/').reply(500)
+        })
 
-        expect(result.succeeded).to.be.true()
-        expect(result.response.body).to.equal({ data: 'hello world' })
-      })
-    })
+        it('uses them in the options used when making the request', async () => {
+          // We tell Got throw an error if the response is not 2xx or 3xx
+          const options = { throwHttpErrors: true }
+          const result = await RequestLib.get(testDomain, options)
 
-    describe('and they replace a default option', () => {
-      beforeEach(() => {
-        Nock(testDomain).get('/').reply(500)
-      })
-
-      it('uses them in the options used when making the request', async () => {
-        // We tell Got throw an error if the response is not 2xx or 3xx
-        const options = { throwHttpErrors: true }
-        const result = await RequestLib.get(testDomain, options)
-
-        expect(result.succeeded).to.be.false()
-        expect(result.response).to.be.an.error()
+          expect(result.succeeded).to.be.false()
+          expect(result.response).to.be.an.error()
+        })
       })
     })
   })
