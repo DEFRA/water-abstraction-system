6bcbbd79b679229b594d607b177fc6366c370293
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-15 16:49:52 +0000

    Simplify including agent in options
    Co-authored-by: Stuart Adair <43574728+StuAA78@users.noreply.github.com>

--- app/lib/request.lib.js
+++ app/lib/request.lib.js
@@ -71,14 +71,15 @@ async function _importGot () {
  */
 function _requestOptions (additionalOptions) {
   const defaultOptions = {
-    // This uses the spread operator and a logical AND short circuit evaluation to allow us to determine whether the
-    // following columns are added to the options or not. Thanks to https://stackoverflow.com/a/40560953/6117745 for
-    // this
-    ...(requestConfig.httpProxy && {
-      agent: {
-        https: new HttpsProxyAgent({ proxy: requestConfig.httpProxy })
-      }
-    }),
+    // This uses the ternary operator to give either an `agent` object or an empty object, and the spread operator to
+    // bring the result back into the top level of the `defaultOptions` object.
+    ...(requestConfig.httpProxy
+      ? {
+          agent: {
+            https: new HttpsProxyAgent({ proxy: requestConfig.httpProxy })
+          }
+        }
+      : {}),
     // If we don't have this setting Got will throw its own HTTPError unless the result is 2xx or 3xx. That makes it
     // impossible to see what the status code was because it doesn't get set on the response object Got provides when
     // an error is thrown. With this set Got will treat a 404 in the same way it treats a 204.
