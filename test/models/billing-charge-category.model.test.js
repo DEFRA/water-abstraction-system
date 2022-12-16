45cabaa475598599b4279e07df712d5abafda3a6
Jason Claxton <jason.claxton@defra.gov.uk>
2022-12-16 12:14:47 +0000

    created billing-charge-category model test

---
+++ test/models/billing-charge-category.model.test.js
@@ -0,0 +1,30 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Thing under test
+const BillingChargeCategory = require('../../app/models/billing-charge-category.model.js')
+
+describe.only('Billing Charge Category model', () => {
+  it('can successfully run a query', async () => {
+    const query = await BillingChargeCategory.query()
+
+    expect(query).to.exist()
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to charge element', () => {
+      it('can successfully run a query', async () => {
+        const query = await BillingChargeCategory.query()
+          .innerJoinRelated('chargeElement')
+
+        expect(query).to.exist()
+      })
+    })
+  })
+})
