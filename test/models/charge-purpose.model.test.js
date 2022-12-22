e73b32917b2d2938b89282b29f51ef062419f8fb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-22 18:39:02 +0000

    Update Charge Purpose model tests

--- test/models/charge-purpose.model.test.js
+++ test/models/charge-purpose.model.test.js
@@ -4,26 +4,64 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
+// Test helpers
+const ChargeElementHelper = require('../support/helpers/charge-element.helper.js')
+const ChargeElementModel = require('../../app/models/charge-element.model.js')
+const ChargePurposeHelper = require('../support/helpers/charge-purpose.helper.js')
+const DatabaseHelper = require('../support/helpers/database.helper.js')
+
 // Thing under test
-const ChargePurpose = require('../../app/models/charge-purpose.model.js')
+const ChargePurposeModel = require('../../app/models/charge-purpose.model.js')
 
 describe('Charge Purpose model', () => {
-  it('can successfully run a query', async () => {
-    const query = await ChargePurpose.query()
+  let testRecord
 
-    expect(query).to.exist()
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    testRecord = await ChargePurposeHelper.add()
+  })
+
+  describe('Basic query', () => {
+    it('can successfully run a basic query', async () => {
+      const result = await ChargePurposeModel.query().findById(testRecord.chargePurposeId)
+
+      expect(result).to.be.an.instanceOf(ChargePurposeModel)
+      expect(result.chargePurposeId).to.equal(testRecord.chargePurposeId)
+    })
   })
 
   describe('Relationships', () => {
     describe('when linking to charge element', () => {
-      it('can successfully run a query', async () => {
-        const query = await ChargePurpose.query()
+      let testChargeElement
+
+      beforeEach(async () => {
+        testChargeElement = await ChargeElementHelper.add()
+
+        const { chargeElementId } = testChargeElement
+        testRecord = await ChargePurposeHelper.add({ chargeElementId })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ChargePurposeModel.query()
           .innerJoinRelated('chargeElement')
 
         expect(query).to.exist()
+      })
+
+      it('can eager load the charge element', async () => {
+        const result = await ChargePurposeModel.query()
+          .findById(testRecord.chargePurposeId)
+          .withGraphFetched('chargeElement')
+
+        expect(result).to.be.instanceOf(ChargePurposeModel)
+        expect(result.chargePurposeId).to.equal(testRecord.chargePurposeId)
+
+        expect(result.chargeElement).to.be.an.instanceOf(ChargeElementModel)
+        expect(result.chargeElement).to.equal(testChargeElement)
       })
     })
   })
