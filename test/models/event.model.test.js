9e9f6528f349a65466a2bfee9d4f18f67bb857a7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-23 12:03:03 +0000

    Update event model tests

--- test/models/event.model.test.js
+++ test/models/event.model.test.js
@@ -4,16 +4,31 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
+// Test helpers
+const DatabaseHelper = require('../support/helpers/database.helper.js')
+const EventHelper = require('../support/helpers/event.helper.js')
+
 // Thing under test
-const Event = require('../../app/models/event.model.js')
+const EventModel = require('../../app/models/event.model.js')
 
 describe('Event model', () => {
-  it('can successfully run a query', async () => {
-    const query = await Event.query()
+  let testRecord
 
-    expect(query).to.exist()
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    testRecord = await EventHelper.add()
+  })
+
+  describe('Basic query', () => {
+    it('can successfully run a basic query', async () => {
+      const result = await EventModel.query().findById(testRecord.eventId)
+
+      expect(result).to.be.an.instanceOf(EventModel)
+      expect(result.eventId).to.equal(testRecord.eventId)
+    })
   })
 })
