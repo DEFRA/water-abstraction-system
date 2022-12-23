f30a5a1c5ce20dc3aa9407ea71140fdef5ddea50
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2022-12-23 12:13:42 +0000

    Update Licence model tests

--- test/models/licence.model.test.js
+++ test/models/licence.model.test.js
@@ -4,35 +4,101 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
+// Test helpers
+const ChargeVersionHelper = require('../support/helpers/charge-version.helper.js')
+const ChargeVersionModel = require('../../app/models/charge-version.model.js')
+const DatabaseHelper = require('../support/helpers/database.helper.js')
+const LicenceHelper = require('../support/helpers/licence.helper.js')
+const RegionHelper = require('../support/helpers/region.helper.js')
+const RegionModel = require('../../app/models/region.model.js')
+
 // Thing under test
-const Licence = require('../../app/models/licence.model.js')
+const LicenceModel = require('../../app/models/licence.model.js')
 
 describe('Licence model', () => {
-  it('can successfully run a query', async () => {
-    const query = await Licence.query()
+  let testRecord
 
-    expect(query).to.exist()
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    testRecord = await LicenceHelper.add()
+  })
+
+  describe('Basic query', () => {
+    it('can successfully run a basic query', async () => {
+      const result = await LicenceModel.query().findById(testRecord.licenceId)
+
+      expect(result).to.be.an.instanceOf(LicenceModel)
+      expect(result.licenceId).to.equal(testRecord.licenceId)
+    })
   })
 
   describe('Relationships', () => {
-    describe('when linking to charge versions', () => {
-      it('can successfully run a query', async () => {
-        const query = await Licence.query()
+    describe('when linking to versions', () => {
+      let testChargeVersions
+
+      beforeEach(async () => {
+        const { licenceId, licenceRef } = testRecord
+
+        testChargeVersions = []
+        for (let i = 0; i < 2; i++) {
+          const chargeVersion = await ChargeVersionHelper.add({ licenceRef }, { licenceId })
+          testChargeVersions.push(chargeVersion)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await LicenceModel.query()
           .innerJoinRelated('chargeVersions')
 
         expect(query).to.exist()
       })
+
+      it('can eager load the charge versions', async () => {
+        const result = await LicenceModel.query()
+          .findById(testRecord.licenceId)
+          .withGraphFetched('chargeVersions')
+
+        expect(result).to.be.instanceOf(LicenceModel)
+        expect(result.licenceId).to.equal(testRecord.licenceId)
+
+        expect(result.chargeVersions).to.be.an.array()
+        expect(result.chargeVersions[0]).to.be.an.instanceOf(ChargeVersionModel)
+        expect(result.chargeVersions).to.include(testChargeVersions[0])
+        expect(result.chargeVersions).to.include(testChargeVersions[1])
+      })
     })
 
     describe('when linking to region', () => {
-      it('can successfully run a query', async () => {
-        const query = await Licence.query()
+      let testRegion
+
+      beforeEach(async () => {
+        testRegion = await RegionHelper.add()
+
+        const { regionId } = testRegion
+        testRecord = await LicenceHelper.add({ regionId })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await LicenceModel.query()
           .innerJoinRelated('region')
 
         expect(query).to.exist()
+      })
+
+      it('can eager load the region', async () => {
+        const result = await LicenceModel.query()
+          .findById(testRecord.licenceId)
+          .withGraphFetched('region')
+
+        expect(result).to.be.instanceOf(LicenceModel)
+        expect(result.licenceId).to.equal(testRecord.licenceId)
+
+        expect(result.region).to.be.an.instanceOf(RegionModel)
+        expect(result.region).to.equal(testRegion)
       })
     })
   })
