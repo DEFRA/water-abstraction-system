4b10ae6ceb4dce8d12d8972408c1861de984accf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-01-02 23:30:58 +0000

    Add new initialise Billing Batch service
    At the start of a new bill run we need to
    
    - create the initial billing batch record
    - create the associated event record
    - send a request to the SROC Charging Module API and link it to our billing batch
    
    With those 3 things done we can return control back to the requester (which will be the UI) whilst we get on with the rest of the bill run process.
    
    This adds the service that will handle those 3 steps. We're waiting on additional functionality before we can do the last one. But for now steps 1 & 2 are done and we'll have a home for step 3 when it's ready!

---
+++ app/services/supplementary-billing/initiate-billing-batch.service.js
@@ -0,0 +1,42 @@
+'use strict'
+
+/**
+ * Handles initiating a new billing batch
+ * @module InitiateBillingBatchService
+ */
+
+const BillingPeriodService = require('./billing-period.service.js')
+const CreateBillingBatchPresenter = require('../../presenters/supplementary-billing/create-billing-batch.presenter.js')
+const CreateBillingBatchService = require('./create-billing-batch.service.js')
+const CreateBillingBatchEventService = require('./create-billing-batch-event.service.js')
+
+/**
+ * Initiate a new billing batch
+ *
+ * Initiating a new billing batch means creating both the `billing_batch` and `event` record with the appropriate data.
+ * In the future it will also encompass creating the bill run record in the SROC Charging Module API.
+ *
+ * @param {Object} billRunRequestData Validated version of the data sent in the request to create the new billing batch
+ *
+ * @returns {Object} Details of the newly created billing batch record
+ */
+async function go (billRunRequestData) {
+  // NOTE: It will be required in the future that we cater for a range of billing periods, as changes can be back dated
+  // up to 5 years. For now though, our delivery scope is only for the current billing period hence billingPeriods[0]
+  const billingPeriod = BillingPeriodService.go()[0]
+
+  const { region, scheme, type, user } = billRunRequestData
+  const billingBatch = await CreateBillingBatchService.go(region, billingPeriod, type, scheme)
+
+  await CreateBillingBatchEventService.go(billingBatch, user)
+
+  return _response(billingBatch)
+}
+
+function _response (billingBatch) {
+  return CreateBillingBatchPresenter.go(billingBatch)
+}
+
+module.exports = {
+  go
+}
