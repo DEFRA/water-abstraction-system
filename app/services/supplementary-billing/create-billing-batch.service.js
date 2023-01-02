5c90a5dca6d251f9a02e9185690a31b9dc1b9b78
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-01-02 22:06:52 +0000

    Refactor CreateBillingBatchService (#74)
    https://eaflood.atlassian.net/browse/WATER-3854
    
    To support our work on adding an endpoint that will allow us to create a new SROC bill run we need to refactor the existing `CreateBillingBatchService` a little bit!
    
    Some of the stuff it's currently defaulting we're now going to pass in. We also need it to bring back the instance with `.region` populated. This is to support creating the 'new' billing batch event record.

--- app/services/supplementary-billing/create-billing-batch.service.js
+++ app/services/supplementary-billing/create-billing-batch.service.js
@@ -10,22 +10,27 @@ const BillingBatchModel = require('../../models/billing-batch.model.js')
 /**
  * Create a new billing batch
  *
- * @param {Object} [regionId] The region_id for the selected region
- * @param {Object} [billingPeriod] The billing period in the format { startDate: 2022-04-01, endDate: 2023-03-31 }
+ * @param {Object} regionId The regionId for the selected region
+ * @param {Object} billingPeriod The billing period in the format { startDate: 2022-04-01, endDate: 2023-03-31 }
+ * @param {string} [batchType=supplementary] The type of billing batch to create. Defaults to 'supplementary'
+ * @param {string} [scheme=sroc] The applicable charging scheme. Defaults to 'sroc'
+ * @param {string} [source=wrls] Where the billing batch originated from. Records imported from NALD have the source 'nald'. Those created in the service use 'wrls'. Defaults to 'wrls'
  *
- * @returns {Object} The newly created billing batch record
+ * @returns {module:BillingBatchModel} The newly created billing batch instance with the `.region` property populated
  */
-async function go (regionId, billingPeriod) {
+async function go (regionId, billingPeriod, batchType = 'supplementary', scheme = 'sroc', source = 'wrls') {
   const billingBatch = await BillingBatchModel.query()
     .insert({
       regionId,
-      batchType: 'supplementary',
+      batchType,
       fromFinancialYearEnding: billingPeriod.endDate.getFullYear(),
       toFinancialYearEnding: billingPeriod.endDate.getFullYear(),
       status: 'processing',
-      scheme: 'sroc'
+      scheme,
+      source
     })
     .returning('*')
+    .withGraphFetched('region')
 
   return billingBatch
 }
