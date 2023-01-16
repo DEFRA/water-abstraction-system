d24b7218ab2b5498e1fca400ee8bd8fb67c81feb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-01-16 10:51:33 +0000

    Add require statement to all config files

--- config/airbrake.config.js
+++ config/airbrake.config.js
@@ -5,6 +5,10 @@
  * @module AirbrakeConfig
  */
 
+// We require dotenv directly in each config file to support unit tests that depend on this this subset of config.
+// Requiring dotenv in multiple places has no effect on the app when running for real.
+require('dotenv').config()
+
 const config = {
   host: process.env.AIRBRAKE_HOST,
   projectKey: process.env.AIRBRAKE_KEY,
--- config/log.config.js
+++ config/log.config.js
@@ -5,6 +5,10 @@
  * @module LogConfig
  */
 
+// We require dotenv directly in each config file to support unit tests that depend on this this subset of config.
+// Requiring dotenv in multiple places has no effect on the app when running for real.
+require('dotenv').config()
+
 const config = {
   // Credit to https://stackoverflow.com/a/323546/6117745 for how to handle
   // converting the env var to a boolean
