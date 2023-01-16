f8e31a246d0da18a6c487a9213c8e1d9498ec2f4
Jason Claxton <jason.claxton@defra.gov.uk>
2023-01-16 22:25:46 +0000

    added flag to licence to indicate if included in billing

--- app/presenters/check/supplementary-data.presenter.js
+++ app/presenters/check/supplementary-data.presenter.js
@@ -7,9 +7,12 @@
 
 function go (data) {
   const licences = data.licences.map((licence) => {
+    const licenceExistsInBilling = _licenceExistsInBilling(licence.billingInvoiceLicences)
+
     return {
       licenceId: licence.licenceId,
-      licenceRef: licence.licenceRef
+      licenceRef: licence.licenceRef,
+      licenceExistsInBilling
     }
   })
   const chargeVersions = data.chargeVersions
@@ -19,6 +22,17 @@ function go (data) {
     licences,
     chargeVersions
   }
+}
+
+function _licenceExistsInBilling (billingInvoiceLicences) {
+  let i
+  for (i = 0; i < billingInvoiceLicences.length; i++) {
+    if (billingInvoiceLicences[i].billingInvoice) {
+      return true
+    }
+  }
+
+  return false
 }
 
 module.exports = {
