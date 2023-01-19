26cb1b5d12e34139d8944eebc3bf474e20acaf96
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-01-19 12:55:12 +0000

    Override expiry time
    Currently our requested tokens expire in an hour. We don't expect this to change, but we don't want to make the assumption just in case! We therefore change the cache expiry time to default to 1 day, but add an override in the main method to manually set it to 10 seconds

--- app/plugins/charging-module-token-cache.plugin.js
+++ app/plugins/charging-module-token-cache.plugin.js
@@ -13,13 +13,17 @@ const Hoek = require('@hapi/hoek')
 const ChargingModuleTokenCachePlugin = {
   name: 'Cache',
   register: (server, _options) => {
+    // `flags` is passed in automatically by hapi. We use it to set the ttl on a per-request basis
     server.method('getChargingModuleToken', async () => {
       await Hoek.wait(1000) // Simulate some slow I/O
+      // Manually set the ttl to 10 seconds
+      flags.ttl = 10 * 1000
 
       return Date.now()
     }, {
       cache: {
-        expiresIn: 10 * 1000,
+        // Hapi requires an `expiresIn` time but we will always override it so we set the default to 1 day
+        expiresIn: 24 * 60 * 60 * 1000,
         generateTimeout: 2000
       }
     })
