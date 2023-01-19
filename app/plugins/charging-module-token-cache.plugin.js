af632d66d740da0c0b5b9d033b0461a6fc4721b1
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-01-19 14:39:38 +0000

    Set cache expiry based on token `expiresIn`
    We check the response from `ChargingModuleTokenService` and either set the cache expiry time based on the `expiresIn` value, or set the expiry time to 0 to avoid caching an unsuccessful response.
    
    Note that we set the expiry time to be 1 minute less than the returned `expiresIn` value to avoid cases where the token is retrieved from the cache but expires before the subsequent request is made

--- app/plugins/charging-module-token-cache.plugin.js
+++ app/plugins/charging-module-token-cache.plugin.js
@@ -13,22 +13,30 @@ const ChargingModuleTokenService = require('../services/charging-module/token.se
 const ChargingModuleTokenCachePlugin = {
   name: 'Cache',
   register: (server, _options) => {
-    // `flags` is passed in automatically by hapi. We use it to set the ttl on a per-request basis
+    // `flags` is passed to our server method automatically by hapi. We use it to set the ttl on a per-request basis
     server.method('getChargingModuleToken', async (flags) => {
       const token = await ChargingModuleTokenService.go()
 
-      // Manually set the ttl to 10 seconds
-      flags.ttl = 10 * 1000
+      // If the response has an expiry time then use this to set the cache expiry
+      // Otherwise, set the expiry time to 0 to avoid caching the unsuccessful attempt
+      flags.ttl = token.expiresIn ? _setExpiryTime(token.expiresIn) : 0
 
-      return token
+      return { ...token, ttl: flags.ttl }
     }, {
       cache: {
-        // Hapi requires an `expiresIn` time but we will always override it so we set the default to 1 day
-        expiresIn: 24 * 60 * 60 * 1000,
+        // We need to set an expiry time here but we will always override it
+        expiresIn: 5000,
         generateTimeout: 2000
       }
     })
   }
+}
+
+function _setExpiryTime (expiresIn) {
+  // The expiry time comes to us in seconds so we need to convert it to milliseconds. We also set it to expire 1 minute
+  // before the reported expiry time, to avoid cases where the token is retrieved from the cache but expires before the
+  // request can be made
+  return (expiresIn - 60) * 1000
 }
 
 module.exports = ChargingModuleTokenCachePlugin
