67aa0367ad25710b07cc8d879e0e08e43422835e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-01-29 09:12:34 +0000

    Add our plugin
    With our plugin we make an instance of GlobalNotifierLib available for use anywhere in the app.
    
    Because we're adding a global variable we have to let labrc. It has checks to help stop global variables being inadvertently created.

---
+++ test/plugins/global-notifier.plugin.test.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const GlobalNotifierLib = require('../../app/lib/global-notifier.lib.js')
+
+// For running our service
+const { init } = require('../../app/server.js')
+
+describe('Global Notifier plugin', () => {
+  beforeEach(async () => {
+    // Create server before each test
+    await init()
+  })
+
+  describe('Global Notifier Plugin', () => {
+    describe('when the server is initialised', () => {
+      it('makes an instance of GlobalNotifierLib available globally', async () => {
+        const result = global.GlobalNotifier
+
+        expect(result).to.be.an.instanceOf(GlobalNotifierLib)
+      })
+    })
+  })
+})
