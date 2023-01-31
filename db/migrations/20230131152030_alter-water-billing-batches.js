6787a508bd174cfa30b64995866495ac4ab35bcb
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-01-31 15:26:25 +0000

    Create migration to add `error_code` column to test db
    We have an additional requirement to record the error code for a bill run created with status `error`. For our unit tests we therefore need to create a migration that adds the `error_code` column to the `billing_batches` table

---
+++ db/migrations/20230131152030_alter-water-billing-batches.js
@@ -0,0 +1,21 @@
+'use strict'
+
+const tableName = 'billing_batches'
+
+exports.up = async function (knex) {
+  await knex
+    .schema
+    .withSchema('water')
+    .alterTable(tableName, table => {
+      table.integer('error_code')
+    })
+}
+
+exports.down = async function (knex) {
+  await knex
+    .schema
+    .withSchema('water')
+    .alterTable(tableName, table => {
+      table.dropColumn('error_code')
+    })
+}
