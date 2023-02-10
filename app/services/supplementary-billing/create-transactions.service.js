c7e9f81e94a4b58c4379b2985d7a57b70c16f952
Jason Claxton <jason.claxton@defra.gov.uk>
2023-02-10 14:30:34 +0000

    initial build of transactions service

---
+++ app/services/supplementary-billing/create-transactions.service.js
@@ -0,0 +1,45 @@
+'use strict'
+
+/**
+ * Creates transaction lines based on the charge referece and abstraction period
+ * @module CreateTransactionsService
+ */
+
+const AbstractionBillingPeriodService = require('./abstraction-billing-period.service.js')
+
+/**
+ * Check whether a "live" bill run exists for the specified region, scheme, type and financial year
+ *
+ * We define "live" as having the status `processing`, `ready`, `review` or `queued`
+ *
+ * @param {Object} billingPeriod Object that has a `startDate` and `endDate` that defines the billing period
+ * @param {Object[]} chargeElements An array of `chargeElements` containing an array of `chargePurposes` which define
+ * the abstraction periods
+ *
+ * @returns {Object} Whether a "live" bill run exists
+ */
+function go (billingPeriod, chargeElements) {
+  const transactionLines = []
+  for (const chargeElement of chargeElements) {
+    const transactionLine = {}
+    transactionLine.reference = chargeElement.billingChargeCategory.reference
+    transactionLine.billableDays = _calculateBillableDays(billingPeriod, chargeElement.chargePurposes)
+    transactionLines.push(transactionLine)
+  }
+  return transactionLines
+}
+
+function _calculateBillableDays (billingPeriod, chargePurposes) {
+  let billableDays = 0
+  for (const chargePurpose of chargePurposes) {
+    const abstractionPeriods = AbstractionBillingPeriodService.go(billingPeriod, chargePurpose)
+    for (const abstractionPeriod of abstractionPeriods) {
+      billableDays = billableDays + abstractionPeriod.billableDays
+    }
+  }
+  return billableDays
+}
+
+module.exports = {
+  go
+}
