da5edfe37811e19b09173ffb1092d1424f15cc53
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2023-02-13 12:14:00 +0000

    Update comment as per Alan's review
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/supplementary-billing/create-transactions.service.js
+++ app/services/supplementary-billing/create-transactions.service.js
@@ -8,6 +8,14 @@
 const AbstractionBillingPeriodService = require('./abstraction-billing-period.service.js')
 
 /**
+ * Creates a transaction line and calculates the billable days for each charge element
+ * 
+ * Each SROC charge version linked to a licence will have one or more charge elements that are linked to a charge reference.
+ *
+ * A transaction line is needed for each of these charge references. Linked to the charge element are one or more abstraction periods.
+ *
+ * We need to calculate the sum of the billable days for these periods and apply it to the transaction line.
+ *
  * @param {Object} billingPeriod Object that has a `startDate` and `endDate` that defines the billing period
  * @param {Object[]} chargeElements An array of `chargeElements` containing an array of `chargePurposes` which define
  * the abstraction periods
