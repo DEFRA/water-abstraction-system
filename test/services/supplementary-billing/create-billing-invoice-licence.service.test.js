0242b26e3c3b61d6c6b4f064b17a2dcbd865992b
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-02-17 10:59:01 +0000

    Initial service and unit test

---
+++ test/services/supplementary-billing/create-billing-invoice-licence.service.test.js
@@ -0,0 +1,43 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const BillingBatchHelper = require('../../support/helpers/water/billing-invoice.helper.js')
+const BillingInvoiceLicenceModel = require('../../../app/models/water/billing-invoice-licence.model.js')
+const LicenceHelper = require('../../support/helpers/water/licence.helper.js')
+const DatabaseHelper = require('../../support/helpers/database.helper.js')
+
+// Thing under test
+const CreateBillingInvoiceLicenceService = require('../../../app/services/supplementary-billing/create-billing-invoice-licence.service.js')
+
+describe.only('Create Billing Invoice Licence service', () => {
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+  })
+
+  describe('when a BillingBatchModel instance is provided', () => {
+    let billingInvoice
+    let licence
+
+    beforeEach(async () => {
+      billingInvoice = await BillingBatchHelper.add()
+      licence = await LicenceHelper.add()
+    })
+
+    it('creates an event record', async () => {
+      const result = await CreateBillingInvoiceLicenceService.go(billingInvoice, licence)
+
+      expect(result).to.be.an.instanceOf(BillingInvoiceLicenceModel)
+
+      expect(result.billingInvoiceId).to.equal(billingInvoice.billingInvoiceId)
+      expect(result.licenceRef).to.equal(licence.licenceRef)
+      expect(result.licenceId).to.equal(licence.licenceId)
+    })
+  })
+})
