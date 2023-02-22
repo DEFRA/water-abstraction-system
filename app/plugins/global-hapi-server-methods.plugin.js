7c9d5adbeb1ef6f23910410d3ad2d1a2a3a9dcb2
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-02-22 12:20:39 +0000

    Amend `GlobalHapiServerMethods` comments

--- app/plugins/global-hapi-server-methods.plugin.js
+++ app/plugins/global-hapi-server-methods.plugin.js
@@ -2,6 +2,11 @@
 
 /**
  * Plugin to add Hapi server methods to the global object
+ *
+ * The advantage of server methods is that their output can be cached. From our tests we have seen that accessing them
+ * via `server.methods` and `global.HapiServerMethods` makes use of the same cache. Nevertheless, for consistency we
+ * should only access them via `global.HapiServerMethods` even when the server object is available to us.
+ *
  * @module GlobalHapiServerMethods
  */
 
