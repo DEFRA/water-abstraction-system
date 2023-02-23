ba291585fa74f4f70fd3c372a08b1bfd21532888
Jason Claxton <jason.claxton@defra.gov.uk>
2023-02-23 21:07:34 +0000

    Remove incorrect relationship
    the invoiceAccountId whilst it exists in the billingInvoices table it is not a foreign key

--- app/models/crm-v2/invoice-account.model.js
+++ app/models/crm-v2/invoice-account.model.js
@@ -5,8 +5,6 @@
  * @module InvoiceAccountModel
  */
 
-const { Model } = require('objection')
-
 const CrmV2BaseModel = require('./crm-v2-base.model.js')
 
 class InvoiceAccountModel extends CrmV2BaseModel {
@@ -23,19 +21,6 @@ class InvoiceAccountModel extends CrmV2BaseModel {
       { database: 'dateCreated', model: 'createdAt' },
       { database: 'dateUpdated', model: 'updatedAt' }
     ]
-  }
-
-  static get relationMappings () {
-    return {
-      billingInvoices: {
-        relation: Model.HasManyRelation,
-        modelClass: 'billing-invoice.model',
-        join: {
-          from: 'invoiceAccounts.invoiceAccountId',
-          to: 'billingInvoices.invoiceAccountId'
-        }
-      }
-    }
   }
 }
 
