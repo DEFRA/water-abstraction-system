a24d80d8c2b91089f4556b1d0029e3e52ad430d4
Jason Claxton <jason.claxton@defra.gov.uk>
2023-02-24 11:38:08 +0000

    Added comment to crm-v2 base model

--- app/models/crm-v2/crm-v2-base.model.js
+++ app/models/crm-v2/crm-v2-base.model.js
@@ -7,6 +7,15 @@
 
 const LegacyBaseModel = require('../legacy-base.model.js')
 
+/**
+ * An issue with the way the standard implementation of the SnakeCase conversion was incorrectly handling the schema
+ * name of `crmV2` by converting it to `crm_v_2` has meant that a custom SnakeCase mapper has been created so that
+ * formatting is not applied to the `crm_v2` schema name.
+ *
+ * See `app/lib/legacy-db-snake-case-mappers.lib.js` or https://github.com/DEFRA/water-abstraction-system/pull/131 for
+ * more details.
+ */
+
 class CrmV2BaseModel extends LegacyBaseModel {
   static get schema () {
     return 'crm_v2'
