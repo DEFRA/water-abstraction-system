b3b3dd177a1b2372c713ef22c25360582ecfd58b
Jason Claxton <jason.claxton@defra.gov.uk>
2023-02-24 11:16:24 +0000

    Update create-billing-service

--- app/services/supplementary-billing/create-billing-invoice.service.js
+++ app/services/supplementary-billing/create-billing-invoice.service.js
@@ -7,28 +7,36 @@
  */
 
 const BillingInvoiceModel = require('../../models/water/billing-invoice.model.js')
+const FetchInvoiceAccountService = require('./fetch-invoice-account.service.js')
 
 /**
- * Create a billing invoice for the provided invoice account and billing batch
+ * Create the initial billing invoice for the provided charge version, billing period and billing batch prior to
+ * transactions being sent to the charging module
  *
- * @param {module:InvoiceAccountModel} invoiceAccount An instance of `InvoiceAccountModel`
- * @param {Object} cmTransaction Data returned from the Charging Module relating to the transaction
+ * @param {Object[]} chargeVersion An Object containing the relevant charge version
+ * @param {Object[]} billingPeriod Object that has a `startDate` and `endDate` that defines the billing period
+ * @param {string} billingBatchId GUID of the billing batch
  *
  * @returns {Object} The newly-created billing invoice record
  */
-async function go (invoiceAccount, cmTransaction, billingBatchId) {
+async function go (chargeVersion, billingPeriod, billingBatchId) {
   const billingInvoice = await BillingInvoiceModel.query()
     .insert({
-      invoiceAccountId: invoiceAccount.invoiceAccountId,
-      address: {},
-      invoiceAccountNumber: invoiceAccount.invoiceAccountNumber,
-      netAmount: cmTransaction.netAmount,
-      isCredit: cmTransaction.netAmount === null ? null : cmTransaction.netAmount < 0,
-      billingBatchId
+      invoiceAccountId: chargeVersion.invoiceAccountId,
+      address: {}, // Have left empty as doesn't appear to be used for SROC
+      invoiceAccountNumber: await _getInvoiceAccountNumber(chargeVersion.invoiceAccountId),
+      billingBatchId,
+      financialYearEnding: billingPeriod.endDate.getFullYear()
     })
     .returning('*')
 
   return billingInvoice
+}
+
+async function _getInvoiceAccountNumber (invoiceAccountId) {
+  const invoiceAccount = await FetchInvoiceAccountService.go(invoiceAccountId)
+
+  return invoiceAccount.invoiceAccountNumber
 }
 
 module.exports = {
