7034c34c7e82012f3f8727541043b501745af4e7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-04 10:07:54 +0000

    Add some sad path tests

--- test/services/charging-module/create-transaction.service.test.js
+++ test/services/charging-module/create-transaction.service.test.js
@@ -15,14 +15,14 @@ const ChargingModuleRequestLib = require('../../../app/lib/charging-module-reque
 const ChargingModuleCreateTransactionService = require('../../../app/services/charging-module/create-transaction.service.js')
 
 describe('Charge module create transaction service', () => {
+  const billingBatchId = '2bbbe459-966e-4026-b5d2-2f10867bdddd'
+  const transactionData = { billingTransactionId: '2395429b-e703-43bc-8522-ce3f67507ffa' }
+
   afterEach(() => {
     Sinon.restore()
   })
 
   describe('when the service can create a transaction', () => {
-    const billingBatchId = '2bbbe459-966e-4026-b5d2-2f10867bdddd'
-    const transactionData = { billingTransactionId: '2395429b-e703-43bc-8522-ce3f67507ffa' }
-
     beforeEach(async () => {
       Sinon.stub(ChargingModuleRequestLib, 'post').resolves({
         succeeded: true,
@@ -53,6 +53,66 @@ describe('Charge module create transaction service', () => {
 
       expect(result.response.body.transaction.id).to.equal('fd88e6c5-8da8-4e4f-b22f-c66554cd5bf3')
       expect(result.response.body.transaction.clientId).to.equal(transactionData.billingTransactionId)
+    })
+  })
+
+  describe('when the service cannot create a transaction', () => {
+    describe('because the request did not return a 2xx/3xx response', () => {
+      beforeEach(async () => {
+        Sinon.stub(ChargingModuleRequestLib, 'post').resolves({
+          succeeded: false,
+          response: {
+            info: {
+              gitCommit: '273604040a47e0977b0579a0fef0f09726d95e39',
+              dockerTag: 'ghcr.io/defra/sroc-charging-module-api:v0.19.0'
+            },
+            statusCode: 401,
+            body: {
+              statusCode: 401,
+              error: 'Unauthorized',
+              message: 'Invalid JWT: Token format not valid',
+              attributes: { error: 'Invalid JWT: Token format not valid' }
+            }
+          }
+        })
+      })
+
+      it('returns a `false` success status', async () => {
+        const result = await ChargingModuleCreateTransactionService.go(billingBatchId, transactionData)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error in the `response`', async () => {
+        const result = await ChargingModuleCreateTransactionService.go(billingBatchId, transactionData)
+
+        expect(result.response.body.statusCode).to.equal(401)
+        expect(result.response.body.error).to.equal('Unauthorized')
+        expect(result.response.body.message).to.equal('Invalid JWT: Token format not valid')
+      })
+    })
+
+    describe('because the request attempt returned an error, for example, TimeoutError', () => {
+      beforeEach(async () => {
+        Sinon.stub(ChargingModuleRequestLib, 'post').resolves({
+          succeeded: false,
+          response: new Error("Timeout awaiting 'request' for 5000ms")
+        })
+      })
+
+      it('returns a `false` success status', async () => {
+        const result = await ChargingModuleCreateTransactionService.go(billingBatchId, transactionData)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error in the `response`', async () => {
+        const result = await ChargingModuleCreateTransactionService.go(billingBatchId, transactionData)
+
+        expect(result.response.statusCode).not.to.exist()
+        expect(result.response.body).not.to.exist()
+        expect(result.response.message).to.equal("Timeout awaiting 'request' for 5000ms")
+      })
     })
   })
 })
