3d48d433243147fb6bbbdd6146c075de2c2e618c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-04 10:49:42 +0000

    Implement patch support in ChargingModuleRequestLib

--- test/lib/charging-module-request.lib.test.js
+++ test/lib/charging-module-request.lib.test.js
@@ -121,6 +121,86 @@ describe('ChargingModuleRequestLib', () => {
     })
   })
 
+  describe('#patch', () => {
+    let result
+
+    describe('when the request succeeds', () => {
+      beforeEach(async () => {
+        Sinon.stub(RequestLib, 'patch').resolves({
+          succeeded: true,
+          response: {
+            headers,
+            statusCode: 204,
+            body: {}
+          }
+        })
+
+        result = await ChargingModuleRequestLib.patch(testRoute)
+      })
+
+      it('calls the Charging Module with the required options', async () => {
+        const requestArgs = RequestLib.patch.firstCall.args
+
+        expect(requestArgs[0]).to.endWith('TEST_ROUTE')
+        expect(requestArgs[1].headers).to.include({ authorization: 'Bearer ACCESS_TOKEN' })
+      })
+
+      it('returns a `true` success status', async () => {
+        expect(result.succeeded).to.be.true()
+      })
+
+      it('returns the response body as an object', async () => {
+        expect(result.response.body).to.equal({})
+      })
+
+      it('returns the status code', async () => {
+        expect(result.response.statusCode).to.equal(204)
+      })
+
+      it('returns the information about the running Charging Module API', async () => {
+        expect(result.response.info).to.equal({
+          gitCommit: '273604040a47e0977b0579a0fef0f09726d95e39',
+          dockerTag: 'ghcr.io/defra/sroc-charging-module-api:v0.19.0'
+        })
+      })
+    })
+
+    describe('when the request fails', () => {
+      beforeEach(async () => {
+        Sinon.stub(RequestLib, 'patch').resolves({
+          succeeded: false,
+          response: {
+            headers,
+            statusCode: 404,
+            statusMessage: 'Not Found',
+            body: { statusCode: 404, error: 'Not Found', message: 'Not Found' }
+          }
+        })
+
+        result = await ChargingModuleRequestLib.patch(testRoute)
+      })
+
+      it('returns a `false` success status', async () => {
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error response', async () => {
+        expect(result.response.body.message).to.equal('Not Found')
+      })
+
+      it('returns the status code', async () => {
+        expect(result.response.statusCode).to.equal(404)
+      })
+
+      it('returns the information about the running Charging Module API', async () => {
+        expect(result.response.info).to.equal({
+          gitCommit: '273604040a47e0977b0579a0fef0f09726d95e39',
+          dockerTag: 'ghcr.io/defra/sroc-charging-module-api:v0.19.0'
+        })
+      })
+    })
+  })
+
   describe('#post', () => {
     let result
 
