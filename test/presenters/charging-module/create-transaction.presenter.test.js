b11b13ab534e19291aadcbb62e1ac9adefd2d94a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-05 22:54:44 +0000

    Add Charging Module Create Transaction Presenter (#145)
    https://eaflood.atlassian.net/browse/WATER-3906
    https://eaflood.atlassian.net/browse/WATER-3926
    
    It is not a one-to-one mapping of transaction properties between our service and the [Charging Module API](https://github.com/DEFRA/sroc-charging-module-api). When we have data that we need to 'present' in a certain way we use the concept of presenters.
    
    As part of the [spike work](https://github.com/DEFRA/water-abstraction-system/pull/133) we created one that 'presents' a WRLS transaction in the format needed to send as a request to the Charging Module API.
    
    This adds it to the project properly along with unit tests.

---
+++ test/presenters/charging-module/create-transaction.presenter.test.js
@@ -0,0 +1,108 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const BillingTransactionHelper = require('../../support/helpers/water/billing-transaction.helper.js')
+const DatabaseHelper = require('../../support/helpers/database.helper.js')
+const LicenceHelper = require('../../support/helpers/water/licence.helper.js')
+const LicenceModel = require('../../../app/models/water/licence.model.js')
+const { ref } = require('objection')
+const RegionHelper = require('../../support/helpers/water/region.helper.js')
+
+// Thing under test
+const CreateTransactionPresenter = require('../../../app/presenters/charging-module/create-transaction.presenter.js')
+
+describe('Charging Module Create Transaction presenter', () => {
+  const billingPeriod = {
+    startDate: new Date('2022-04-01'),
+    endDate: new Date('2023-03-31')
+  }
+  const invoiceAccountNumber = 'A51542397A'
+
+  let billingTransaction
+  let licence
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+  })
+
+  describe('when provided with a Billing Transaction and Licence instance', () => {
+    beforeEach(async () => {
+      const region = await RegionHelper.add()
+
+      // NOTE: In the context the presenter is used it is from a Licence instance returned by
+      // FetchChargeVersionsService. We recreate how that instance is formed here, including extracting some of the
+      // values as distinct properties from the licence's `regions` JSONb field.
+      const tempLicence = await LicenceHelper.add({
+        regionId: region.regionId,
+        regions: { historicalAreaCode: 'SAAR', regionalChargeArea: 'Southern' }
+      })
+      licence = await LicenceModel.query()
+        .findById(tempLicence.licenceId)
+        .select([
+          'licenceId',
+          'licenceRef',
+          ref('licences.regions:historicalAreaCode').castText().as('historicalAreaCode'),
+          ref('licences.regions:regionalChargeArea').castText().as('regionalChargeArea')
+        ])
+        .withGraphFetched('region')
+        .modifyGraph('licence.region', builder => {
+          builder.select([
+            'regionId',
+            'chargeRegionId'
+          ])
+        })
+
+      // NOTE: The transaction object the presenter expects is what `FormatSrocTransactionLineService` returns rather
+      // than a simple instance of `BillingTransactionModel`. But to test the presenter, we just need to _represent_
+      // what the service returns whilst avoiding over complicating our tests with the additional setup it needs. So,
+      // we create a standard transaction instance and amend some of the properties to match what
+      // FormatSrocTransactionLineService does.
+      billingTransaction = await BillingTransactionHelper.add()
+      billingTransaction.section127Agreement = false
+      billingTransaction.section130Agreement = false
+    })
+
+    it('correctly presents the data', () => {
+      const result = CreateTransactionPresenter.go(billingTransaction, billingPeriod, invoiceAccountNumber, licence)
+
+      expect(result.clientId).to.equal(billingTransaction.billingTransactionId)
+      expect(result.ruleset).to.equal('sroc')
+      expect(result.periodStart).to.equal('01-APR-2022')
+      expect(result.periodEnd).to.equal('31-MAR-2023')
+      expect(result.credit).to.equal(false)
+      expect(result.abatementFactor).to.equal(1)
+      expect(result.adjustmentFactor).to.equal(1)
+      expect(result.actualVolume).to.equal(11)
+      expect(result.aggregateProportion).to.equal(1)
+      expect(result.areaCode).to.equal('SAAR')
+      expect(result.authorisedDays).to.equal(365)
+      expect(result.authorisedVolume).to.equal(11)
+      expect(result.billableDays).to.equal(365)
+      expect(result.chargeCategoryCode).to.equal('4.5.6')
+      expect(result.chargeCategoryDescription).to.equal('Medium loss, non-tidal, restricted water, up to and including 25 ML/yr, Tier 2 model')
+      expect(result.chargePeriod).to.equal('01-APR-2022 - 31-MAR-2023')
+      expect(result.compensationCharge).to.equal(false)
+      expect(result.customerReference).to.equal(invoiceAccountNumber)
+      expect(result.licenceNumber).to.equal('01/123')
+      expect(result.lineDescription).to.equal('Water abstraction charge: Agriculture other than spray irrigation at East Rudham')
+      expect(result.loss).to.equal('medium')
+      expect(result.region).to.equal('S')
+      expect(result.regionalChargingArea).to.equal('Southern')
+      expect(result.section127Agreement).to.equal(false)
+      expect(result.section130Agreement).to.equal(false)
+      expect(result.supportedSource).to.equal(false)
+      expect(result.supportedSourceName).to.equal(null)
+      expect(result.twoPartTariff).to.equal(false)
+      expect(result.waterCompanyCharge).to.equal(false)
+      expect(result.waterUndertaker).to.equal(false)
+      expect(result.winterOnly).to.equal(false)
+    })
+  })
+})
