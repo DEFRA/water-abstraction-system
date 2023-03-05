0cb18af7fcdf3c9073f0466d51012fa2091ae80b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-05 12:35:10 +0000

    Implement the change
    One thing to note is the latest posts on this issue use `pg.types` rather than some magic number from the DB.

--- db/db.js
+++ db/db.js
@@ -4,17 +4,26 @@ const environment = process.env.NODE_ENV || 'development'
 
 const dbConfig = require('../knexfile.application')[environment]
 
-// Some of our db fields are of type BigInt. The 'pg' driver by default will return these as strings because of concerns
-// about precision loss (a pg BigInt has a bigger range than a JavaScript integer). However, a JavaScript integer has
-// a range of -9007199254740991 to 9007199254740991. This is well, well within the bounds of what we need (assuming
-// we won't generate a bill for more than the national debt of the UK!).
-// So to avoid having to deal with converting strings to BigInt in our code we can tell the pg driver to parse BigInt's
-// as integers instead. See the following for more details about both the issue and this config change
+// Where the 'pg' package has concern that parsing a DB value into its JavaScript equivalent will lead to a loss of
+// data it will return the value as a string. For example, a PostgreSQL BigInt has the range -9223372036854775808 to
+// 9223372036854775807. A JavaScript Integer (its closest type) only has the range -9007199254740991 to
+// 9007199254740991.
+//
+// The intent is it's on us, as the developers, to determine how each value should be used. Fortunately, we know we are
+// never using values that are large enough to concern ourselves with any loss of precision.
+//
+// So, to avoid having to deal with converting strings explicitly in our code we can tell the pg driver to parse these
+// problematic values instead. See the following for more details about both the issue and this config change
 // https://github.com/brianc/node-postgres/pull/353
 // https://github.com/knex/knex/issues/387#issuecomment-51554522
+// https://stackoverflow.com/a/39176670/6117745
 const pg = require('pg')
 // The magic number 20 comes from `SELECT oid FROM pg_type WHERE typname = 'int8';` and is unlikely to change.
 pg.types.setTypeParser(20, 'text', parseInt)
+
+pg.types.setTypeParser(pg.types.builtins.NUMERIC, (value) => {
+  return parseFloat(value)
+})
 
 const db = require('knex')(dbConfig)
 
