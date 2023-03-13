256658d8e8e6b0dff5d65b320d0f84c16b337840
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-03-13 15:09:26 +0000

    Test error codes

--- test/services/supplementary-billing/process-billing-batch.service.test.js
+++ test/services/supplementary-billing/process-billing-batch.service.test.js
@@ -10,10 +10,22 @@ const { expect } = Code
 
 // Test helpers
 const BillingBatchHelper = require('../../support/helpers/water/billing-batch.helper.js')
+const BillingBatchModel = require('../../../app/models/water/billing-batch.model.js')
+const BillingChargeCategoryHelper = require('../../support/helpers/water/billing-charge-category.helper.js')
+const ChangeReasonHelper = require('../../support/helpers/water/change-reason.helper.js')
+const ChargeElementHelper = require('../../support/helpers/water/charge-element.helper.js')
+const ChargePurposeHelper = require('../../support/helpers/water/charge-purpose.helper.js')
+const ChargeVersionHelper = require('../../support/helpers/water/charge-version.helper.js')
+const InvoiceAccountHelper = require('../../support/helpers/crm-v2/invoice-account.helper.js')
+const LicenceHelper = require('../../support/helpers/water/licence.helper.js')
 const DatabaseHelper = require('../../support/helpers/database.helper.js')
+const RegionHelper = require('../../support/helpers/water/region.helper.js')
 
 // Things we need to stub
+const ChargingModuleCreateTransactionService = require('../../../app/services/charging-module/create-transaction.service.js')
 const FetchChargeVersionsService = require('../../../app/services/supplementary-billing/fetch-charge-versions.service.js')
+const GenerateBillingTransactionsService = require('../../../app/services/supplementary-billing/generate-billing-transactions.service.js')
+const HandleErroredBillingBatchService = require('../../../app/services/supplementary-billing/handle-errored-billing-batch.service.js')
 
 // Thing under test
 const ProcessBillingBatchService = require('../../../app/services/supplementary-billing/process-billing-batch.service.js')
@@ -25,16 +37,30 @@ describe.only('Process billing batch service', () => {
   }
 
   let billingBatch
+  let handleErroredBillingBatchStub
   let notifierStub
 
   beforeEach(async () => {
     await DatabaseHelper.clean()
+
+    const { regionId } = await RegionHelper.add()
+    const { licenceId } = await LicenceHelper.add({ includeInSupplementaryBilling: 'yes', regionId })
+    const { changeReasonId } = await ChangeReasonHelper.add()
+    const { invoiceAccountId } = await InvoiceAccountHelper.add()
+    const { chargeVersionId } = await ChargeVersionHelper.add({ changeReasonId, invoiceAccountId }, { licenceId })
+    const { billingChargeCategoryId } = await BillingChargeCategoryHelper.add()
+    const { chargeElementId } = await ChargeElementHelper.add({ billingChargeCategoryId, chargeVersionId })
+    await ChargePurposeHelper.add({ chargeElementId })
+
+    billingBatch = await BillingBatchHelper.add({ regionId })
 
     // RequestLib depends on the GlobalNotifier to have been set. This happens in app/plugins/global-notifier.plugin.js
     // when the app starts up and the plugin is registered. As we're not creating an instance of Hapi server in this
     // test we recreate the condition by setting it directly with our own stub
     notifierStub = { omg: Sinon.stub(), omfg: Sinon.stub() }
     global.GlobalNotifier = notifierStub
+
+    handleErroredBillingBatchStub = Sinon.stub(HandleErroredBillingBatchService, 'go')
   })
 
   afterEach(() => {
@@ -44,9 +70,7 @@ describe.only('Process billing batch service', () => {
   describe('when the service errors', () => {
     const expectedError = new Error('ERROR')
 
-    beforeEach(async () => {
-      billingBatch = await BillingBatchHelper.add()
-
+    beforeEach(() => {
       Sinon.stub(FetchChargeVersionsService, 'go').rejects(expectedError)
     })
 
@@ -54,12 +78,12 @@ describe.only('Process billing batch service', () => {
       await expect(ProcessBillingBatchService.go(billingBatch, billingPeriod)).not.to.reject()
     })
 
-    it('sets the billing batch status to `error`', async () => {
+    it('calls HandleErroredBillingBatchService with the billing batch id', async () => {
       await ProcessBillingBatchService.go(billingBatch, billingPeriod)
 
-      const refreshedBillingBatch = await billingBatch.$query()
+      const handlerArgs = handleErroredBillingBatchStub.firstCall.args
 
-      expect(refreshedBillingBatch.status).to.equal('error')
+      expect(handlerArgs[0]).to.equal(billingBatch.billingBatchId)
     })
 
     it('logs the error', async () => {
@@ -70,6 +94,48 @@ describe.only('Process billing batch service', () => {
       expect(notifierStub.omfg.calledWith('Billing Batch process errored')).to.be.true()
       expect(logDataArg.billingBatch).to.equal(billingBatch)
       expect(logDataArg.error).to.equal(expectedError)
+    })
+  })
+
+  describe('when attempting to fetch the charge versions fails', () => {
+    beforeEach(() => {
+      Sinon.stub(FetchChargeVersionsService, 'go').rejects()
+    })
+
+    it('sets the appropriate error code', async () => {
+      await ProcessBillingBatchService.go(billingBatch, billingPeriod)
+
+      const handlerArgs = handleErroredBillingBatchStub.firstCall.args
+
+      expect(handlerArgs[1]).to.equal(BillingBatchModel.errorCodes.failedToProcessChargeVersions)
+    })
+  })
+
+  describe('when attempting to generate the transaction lines fails', () => {
+    beforeEach(() => {
+      Sinon.stub(GenerateBillingTransactionsService, 'go').throws()
+    })
+
+    it('sets the appropriate error code', async () => {
+      await ProcessBillingBatchService.go(billingBatch, billingPeriod)
+
+      const handlerArgs = handleErroredBillingBatchStub.firstCall.args
+
+      expect(handlerArgs[1]).to.equal(BillingBatchModel.errorCodes.failedToPrepareTransactions)
+    })
+  })
+
+  describe('when attempting to create the transaction lines fails', () => {
+    beforeEach(() => {
+      Sinon.stub(ChargingModuleCreateTransactionService, 'go').rejects()
+    })
+
+    it('sets the appropriate error code', async () => {
+      await ProcessBillingBatchService.go(billingBatch, billingPeriod)
+
+      const handlerArgs = handleErroredBillingBatchStub.firstCall.args
+
+      expect(handlerArgs[1]).to.equal(BillingBatchModel.errorCodes.failedToCreateCharge)
     })
   })
 })
