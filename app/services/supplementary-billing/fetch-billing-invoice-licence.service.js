d162fc1bd69cf02b4eef58c2bac722c0e1e37c42
Jason Claxton <jason.claxton@defra.gov.uk>
2023-03-16 12:53:19 +0000

    Tidy up comments

--- app/services/supplementary-billing/fetch-billing-invoice-licence.service.js
+++ app/services/supplementary-billing/fetch-billing-invoice-licence.service.js
@@ -1,30 +1,30 @@
 'use strict'
 
 /**
- * Fetches a billingInvoiceLicenceId for a the last 'sent' billrun if one exists
+ * Fetches the transactions for a the last 'sent' billrun if one exists
  * @module FetchBillingInvoiceLicenceService
  */
 
 const { db } = require('../../../db/db.js')
 
 /**
- * Return either a new billing invoice licence object to use to find the transactions or an existing one if it exists
+ * Return either a new billing transaction object to use or an existing one if it exists
  *
- * This first checks whether a billing invoice licence with the same licence ID & financial year exists in
- * `foundBillingInvoiceLicenceIds`. The calling service is expected to provide and keep track of this variable between
+ * This first checks whether billing transactions for the same licence ID & financial year exists in
+ * `foundBillingTransactions`. The calling service is expected to provide and keep track of this variable between
  * between calls. If it does, it returns that instance along with the original array unchanged.
  *
  * If it doesn't, we generate a new instance and create a new array, based on the one provided plus our new instance.
  * We then return the instance and the new array as the result.
  *
- * For context, this is all to avoid searching for `billing_invoice_licence` records unnecessarily.
+ * For context, this is all to avoid searching for `billing_transaction` records unnecessarily.
  *
- * @param {Object[]} foundBillingInvoiceLicenceIds An array of previously generated billing invoice licence objects
+ * @param {Object[]} foundBillingTransactions An array of previously generated billing transaction objects
  * @param {*} licenceId UUID of the licence the billing invoice licence is linked to
  * @param {*} financialYearEnding The financial year ending of the related billing invoice
  *
- * @returns {Object} A result object containing either the found or generated billing invoice licence object, and an
- * array of generated billing invoices which includes the one being returned
+ * @returns {Object} A result object containing either the found or generated billing transaction object, and an
+ * array of generated billing transactions which includes the one being returned
  */
 async function go (foundBillingTransactions, licenceId, financialYearEnding) {
   let billingTransaction = _existing(foundBillingTransactions, licenceId, financialYearEnding)
