e5b38e59e683a77f3434cc3066223a4d187e82fa
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-27 22:21:11 +0100

    Add migration
    We need the new column in our test db.

---
+++ db/migrations/20230327211801_alter-licences.js
@@ -0,0 +1,23 @@
+'use strict'
+
+const tableName = 'licences'
+
+exports.up = async function (knex) {
+  await knex
+    .schema
+    .withSchema('water')
+    .alterTable(tableName, table => {
+      table.boolean('include_in_sroc_supplementary_billing').notNullable().defaultTo(false)
+    })
+}
+
+exports.down = async function (knex) {
+  return knex
+    .schema
+    .withSchema('water')
+    .alterTable(tableName, table => {
+      table.dropColumns(
+        'include_in_sroc_supplementary_billing'
+      )
+    })
+}
