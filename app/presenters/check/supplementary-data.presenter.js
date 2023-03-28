ea22bf69a24cc22a54f64358afff3dbb58a9c431
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-03-28 12:24:19 +0100

    Remove FetchLicencesService (#178)
    https://eaflood.atlassian.net/browse/WATER-3948
    
    To resolve the issue of dealing with PRESROC and SROC licences flagged for supplementary billing, and how we remove those flags when bill runs get 'sent' we've [made a change to the `FetchChargeVersionsService`](https://github.com/DEFRA/water-abstraction-system/pull/177) to look at a [new field we've added](https://github.com/DEFRA/water-abstraction-service/pull/2077) to the `water.licences` table.
    
    This broke some tests, a number of which were in the `FetchLicencesService`. We were about to embark on fixing them when we remembered that with the direction our work on supplementary billing took `FetchLicencesService` had become redundant.
    
    So, rather than spend time fixing them we're just getting on and removing the service.

--- app/presenters/check/supplementary-data.presenter.js
+++ app/presenters/check/supplementary-data.presenter.js
@@ -6,18 +6,10 @@
  */
 
 function go (data) {
-  const licences = data.licences.map((licence) => {
-    return {
-      licenceId: licence.licenceId,
-      licenceRef: licence.licenceRef,
-      licenceExistsInBilling: licence.numberOfTimesBilled > 0
-    }
-  })
   const chargeVersions = data.chargeVersions
 
   return {
     billingPeriods: data.billingPeriods,
-    licences,
     chargeVersions
   }
 }
--- app/services/check/supplementary-data.service.js
+++ app/services/check/supplementary-data.service.js
@@ -7,15 +7,12 @@
 
 const BillingPeriodService = require('../supplementary-billing/billing-period.service.js')
 const FetchChargeVersionsService = require('../supplementary-billing/fetch-charge-versions.service.js')
-const FetchLicencesService = require('../supplementary-billing/fetch-licences.service.js')
 const FetchRegionService = require('../supplementary-billing/fetch-region.service.js')
 const SupplementaryDataPresenter = require('../../presenters/check/supplementary-data.presenter.js')
 
 async function go (naldRegionId) {
   const region = await FetchRegionService.go(naldRegionId)
   const billingPeriods = BillingPeriodService.go()
-  const billingPeriodFinancialYearEnding = billingPeriods[0].endDate.getFullYear()
-  const licences = await FetchLicencesService.go(region, billingPeriodFinancialYearEnding)
 
   // We know in the future we will be calculating multiple billing periods and so will have to iterate through each,
   // generating bill runs and reviewing if there is anything to bill. For now, whilst our knowledge of the process
@@ -23,7 +20,7 @@ async function go (naldRegionId) {
   // This is why we are only passing through the first billing period; we know there is only one!
   const chargeVersions = await FetchChargeVersionsService.go(region.regionId, billingPeriods[0])
 
-  return _response({ billingPeriods, licences, chargeVersions })
+  return _response({ billingPeriods, chargeVersions })
 }
 
 function _response (data) {
