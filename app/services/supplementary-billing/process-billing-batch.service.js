08d6170826bf1ee46be0bea7ab3b34605555c72f
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2023-04-06 14:25:48 +0100

    Clear the SROC Supp Billing flag when billrun empty (#184)
    https://eaflood.atlassian.net/browse/WATER-3948
    
    The `include_in_sroc_supplementary_billing` flag on the licence was not set to `false` following a successful bill run if the bill run was empty.
    
    This PR ensures that the flag is set to false if the bill run is empty.

--- app/services/supplementary-billing/process-billing-batch.service.js
+++ app/services/supplementary-billing/process-billing-batch.service.js
@@ -20,6 +20,7 @@ const GenerateBillingInvoiceService = require('./generate-billing-invoice.servic
 const GenerateBillingInvoiceLicenceService = require('./generate-billing-invoice-licence.service.js')
 const HandleErroredBillingBatchService = require('./handle-errored-billing-batch.service.js')
 const LegacyRequestLib = require('../../lib/legacy-request.lib.js')
+const LicenceModel = require('../../models/water/licence.model.js')
 const ProcessBillingTransactionsService = require('./process-billing-transactions.service.js')
 
 /**
@@ -76,7 +77,7 @@ async function go (billingBatch, billingPeriod) {
     }
     await _finaliseCurrentInvoiceLicence(currentBillingData, billingPeriod, billingBatch)
 
-    await _finaliseBillingBatch(billingBatch, currentBillingData.isEmpty)
+    await _finaliseBillingBatch(billingBatch, chargeVersions, currentBillingData.isEmpty)
 
     // Log how long the process took
     _calculateAndLogTime(billingBatchId, startTime)
@@ -167,11 +168,12 @@ async function _fetchChargeVersions (billingBatch, billingPeriod) {
   }
 }
 
-async function _finaliseBillingBatch (billingBatch, isEmpty) {
+async function _finaliseBillingBatch (billingBatch, chargeVersions, isEmpty) {
   try {
     // The bill run is considered empty. We just need to set the status to indicate this in the UI
     if (isEmpty) {
       await _updateStatus(billingBatch.billingBatchId, 'empty')
+      await _setSuppBillingFlagsToFalse(chargeVersions)
 
       return
     }
@@ -290,6 +292,16 @@ async function _updateStatus (billingBatchId, status) {
 
     throw error
   }
+}
+
+async function _setSuppBillingFlagsToFalse (chargeVersions) {
+  const licenceIds = chargeVersions.map((chargeVersion) => {
+    return chargeVersion.licence.licenceId
+  })
+
+  await LicenceModel.query()
+    .whereIn('licenceId', licenceIds)
+    .patch({ includeInSrocSupplementaryBilling: false })
 }
 
 module.exports = {
