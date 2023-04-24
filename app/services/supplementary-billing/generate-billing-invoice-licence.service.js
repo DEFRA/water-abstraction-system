0ea689333fc87c8f05372286ca77985bfbd5d278
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-04-24 15:38:52 +0100

    Amend `GenerateBillingInvoiceLicenceService` docs
    The docs for `GenerateBillingInvoiceLicenceService` refer to us passing in and returning an array of billing invoice licence objects. This was true when the service was originally written but we now pass in and return a single billing invoice licence object. We therefore update the docs to reflect this

--- app/services/supplementary-billing/generate-billing-invoice-licence.service.js
+++ app/services/supplementary-billing/generate-billing-invoice-licence.service.js
@@ -10,12 +10,10 @@ const { randomUUID } = require('crypto')
 /**
  * Return either a new billing invoice licence object ready for persisting or an existing one if it exists
  *
- * This first checks whether a billing invoice licence with the same invoice and licence ID exists in
- * `generatedBillingInvoiceLicences`. The calling service is expected to provide and keep track of this variable between
- * calls. If it does, it returns that instance along with the original array unchanged.
+ * This first checks whether the billing invoice id and licence id of `currentBillingInvoiceLicence` match the ones
+ * passed to this service. If they do, we return that instance.
  *
- * If it doesn't, we generate a new instance and create a new array, based on the one provided plus our new instance. We
- * then return the instance and the new array as the result.
+ * If they don't, we generate a new instance and return it.
  *
  * For context, this is all to avoid creating `billing_invoice` and `billing_invoice_licence` records unnecessarily. The
  * legacy service will create them first, then determine if there are any transactions to be billed. If there aren't, it
@@ -27,13 +25,12 @@ const { randomUUID } = require('crypto')
  * licence data in memory along with ID's, and use this service to provide the right record when persisting the
  * transaction.
  *
- * @param {Object[]} generatedBillingInvoiceLicences An array of previously generated billing invoice licence objects
+ * @param {module:billingInvoiceLicence} currentBillingInvoiceLicence A billing invoice licence object
  * @param {String} billingInvoiceId UUID of the billing invoice this billing invoice licence will be linked to if
  *  persisted
  * @param {module:LicenceModel} licence The licence this billing invoice licence will be linked to
  *
- * @returns {Object} A result object containing either the found or generated billing invoice licence object, and an
- *  array of generated billing invoice licences which includes the one being returned
+ * @returns {Object} A result object containing either the found or generated billing invoice licence object
  */
 function go (currentBillingInvoiceLicence, billingInvoiceId, licence) {
   if (
