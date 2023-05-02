9797492a450cfa211b1b74c367a2a02d2992c22d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-05-02 08:38:41 +0100

    Create new generator functions in test
    We need to add new tests to cover checking when the agreements and charges differ between previous and calculated transactions.
    
    It's going to need more transaction data to be passed through to the service, and continuing to use fixed data is going to make the test file massive.
    
    So, we make a change to move the test data logic to be generator methods instead.

--- test/services/supplementary-billing/process-billing-transactions.service.test.js
+++ test/services/supplementary-billing/process-billing-transactions.service.test.js
@@ -14,7 +14,7 @@ const FetchPreviousBillingTransactionsService = require('../../../app/services/s
 // Thing under test
 const ProcessBillingTransactionsService = require('../../../app/services/supplementary-billing/process-billing-transactions.service.js')
 
-describe('Process billing batch service', () => {
+describe.only('Process billing batch service', () => {
   const billingInvoice = { billingInvoiceId: 'a56ef6d9-370a-4224-b6ec-0fca8bfa4d1f' }
   const billingInvoiceLicence = { billingInvoiceLicenceId: '110ab2e2-6076-4d5a-a56f-b17a048eb269' }
 
@@ -23,94 +23,31 @@ describe('Process billing batch service', () => {
     endDate: new Date('2023-03-31')
   }
 
-  const calculatedTransactions = [
-    {
-      billingTransactionId: '61abdc15-7859-4783-9622-6cb8de7f2461',
-      billingInvoiceLicenceId: billingInvoiceLicence.billingInvoiceLicenceId,
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '4.10.1',
-      billableDays: 365,
-      purposes: 'CALCULATED_TRANSACTION_1'
-    },
-    {
-      billingTransactionId: 'a903cdd3-1804-4237-aeb9-70ef9008469d',
-      billingInvoiceLicenceId: billingInvoiceLicence.billingInvoiceLicenceId,
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '5.11.2',
-      billableDays: 265,
-      purposes: 'CALCULATED_TRANSACTION_2'
-    },
-    {
-      billingTransactionId: '34453414-0ecb-49ce-8442-619d22c882f0',
-      billingInvoiceLicenceId: billingInvoiceLicence.billingInvoiceLicenceId,
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '6.12.3',
-      billableDays: 100,
-      purposes: 'CALCULATED_TRANSACTION_3'
-    }
-  ]
-
-  const allPreviousTransactions = [
-    {
-      billingTransactionId: '8d68eb26-d054-47a7-aee8-cd93a24fa860',
-      billingInvoiceLicenceId: '110ab2e2-6076-4d5a-a56f-b17a048eb269',
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '4.10.1',
-      billableDays: 365,
-      purposes: ['I_WILL_BE_REMOVED_1']
-    },
-    {
-      billingTransactionId: '63742bee-cb1b-44f1-86f6-c7d546f59c88',
-      billingInvoiceLicenceId: '110ab2e2-6076-4d5a-a56f-b17a048eb269',
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '5.11.2',
-      billableDays: 265,
-      purposes: ['I_WILL_BE_REMOVED_2']
-    },
-    {
-      billingTransactionId: '4f254a70-d36e-46eb-9e4b-59503c3d5994',
-      billingInvoiceLicenceId: '110ab2e2-6076-4d5a-a56f-b17a048eb269',
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '6.12.3',
-      billableDays: 100,
-      purposes: ['I_WILL_BE_REMOVED_3']
-    },
-    {
-      billingTransactionId: '733bdb55-5386-4fdb-a581-1e98f9a35bed',
-      billingInvoiceLicenceId: '110ab2e2-6076-4d5a-a56f-b17a048eb269',
-      isCredit: false,
-      status: 'candidate',
-      chargeType: 'standard',
-      chargeCategoryCode: '9.9.9',
-      billableDays: 180,
-      purposes: ['I_WILL_NOT_BE_REMOVED']
-    }
-  ]
-
   afterEach(() => {
     Sinon.restore()
   })
 
   describe('when the billing invoice, licence and period', () => {
+    let calculatedTransactions
+
+    beforeEach(() => {
+      calculatedTransactions = [
+        _generateCalculatedTransaction('4.10.1', 365, 'CALCULATED_TRANSACTION_1'),
+        _generateCalculatedTransaction('5.11.2', 265, 'CALCULATED_TRANSACTION_2'),
+        _generateCalculatedTransaction('6.12.3', 100, 'CALCULATED_TRANSACTION_3')
+      ]
+    })
+
     describe('match to transactions on a previous billing batch', () => {
       describe('and the calculated transactions provided', () => {
         let previousTransactions
 
         describe('completely cancel out the previous transactions from the last billing batch', () => {
           beforeEach(() => {
-            previousTransactions = [allPreviousTransactions[0], allPreviousTransactions[1]]
+            previousTransactions = [
+              _generatePreviousTransaction('4.10.1', 365, 'I_WILL_BE_REMOVED_1'),
+              _generatePreviousTransaction('5.11.2', 265, 'I_WILL_BE_REMOVED_2')
+            ]
 
             Sinon.stub(FetchPreviousBillingTransactionsService, 'go').resolves(previousTransactions)
           })
@@ -130,7 +67,11 @@ describe('Process billing batch service', () => {
 
         describe('partially cancel out the previous transactions from the last billing batch', () => {
           beforeEach(() => {
-            previousTransactions = [allPreviousTransactions[0], allPreviousTransactions[1], allPreviousTransactions[3]]
+            previousTransactions = [
+              _generatePreviousTransaction('4.10.1', 365, 'I_WILL_BE_REMOVED_1'),
+              _generatePreviousTransaction('5.11.2', 265, 'I_WILL_BE_REMOVED_2'),
+              _generatePreviousTransaction('9.9.9', 180, 'I_WILL_NOT_BE_REMOVED')
+            ]
 
             Sinon.stub(FetchPreviousBillingTransactionsService, 'go').resolves(previousTransactions)
           })
@@ -151,7 +92,11 @@ describe('Process billing batch service', () => {
 
         describe('are cancelled out by the previous transactions from the last billing batch', () => {
           beforeEach(() => {
-            previousTransactions = [allPreviousTransactions[0], allPreviousTransactions[1], allPreviousTransactions[2]]
+            previousTransactions = [
+              _generatePreviousTransaction('4.10.1', 365, 'I_WILL_BE_REMOVED_1'),
+              _generatePreviousTransaction('5.11.2', 265, 'I_WILL_BE_REMOVED_2'),
+              _generatePreviousTransaction('6.12.3', 100, 'I_WILL_BE_REMOVED_3')
+            ]
 
             Sinon.stub(FetchPreviousBillingTransactionsService, 'go').resolves(previousTransactions)
           })
@@ -170,7 +115,10 @@ describe('Process billing batch service', () => {
 
         describe('are empty', () => {
           beforeEach(() => {
-            previousTransactions = [allPreviousTransactions[0], allPreviousTransactions[1]]
+            previousTransactions = [
+              _generatePreviousTransaction('4.10.1', 365, 'I_WILL_BE_REMOVED_1'),
+              _generatePreviousTransaction('5.11.2', 265, 'I_WILL_BE_REMOVED_2')
+            ]
 
             Sinon.stub(FetchPreviousBillingTransactionsService, 'go').resolves(previousTransactions)
           })
@@ -214,3 +162,48 @@ describe('Process billing batch service', () => {
     })
   })
 })
+
+function _generateCalculatedTransaction (chargeCategoryCode, billableDays, testReference, changes = {}) {
+  const defaultProperties = {
+    billingTransactionId: '61abdc15-7859-4783-9622-6cb8de7f2461',
+    billingInvoiceLicenceId: '110ab2e2-6076-4d5a-a56f-b17a048eb269',
+    isCredit: false,
+    status: 'candidate',
+    chargeType: 'standard',
+    chargeCategoryCode,
+    billableDays,
+    section126Factor: 1,
+    section127Agreement: false,
+    section130Agreement: false,
+    aggregateFactor: 1,
+    adjustmentFactor: 1,
+    isWinterOnly: false,
+    isSupportedSource: false,
+    supportedSourceName: null,
+    isWaterCompanyCharge: false,
+    purposes: testReference
+  }
+
+  return {
+    ...defaultProperties,
+    ...changes
+  }
+}
+
+function _generatePreviousTransaction (chargeCategoryCode, billableDays, testReference, changes = {}) {
+  const defaultProperties = {
+    billingTransactionId: '8d68eb26-d054-47a7-aee8-cd93a24fa860',
+    billingInvoiceLicenceId: 'a76b3ab3-d70d-4fb0-8d72-2e2cdd334729',
+    isCredit: false,
+    status: 'candidate',
+    chargeType: 'standard',
+    chargeCategoryCode,
+    billableDays,
+    purposes: [testReference]
+  }
+
+  return {
+    ...defaultProperties,
+    ...changes
+  }
+}
