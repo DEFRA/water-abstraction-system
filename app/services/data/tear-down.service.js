27891f668264b07213838b74bc65edcc48002f70
Jason Claxton <jason.claxton@defra.gov.uk>
2023-05-03 16:44:49 +0100

    Delete many things from many tables

--- app/services/data/tear-down.service.js
+++ app/services/data/tear-down.service.js
@@ -9,6 +9,17 @@ const { db } = require('../../../db/db.js')
 
 async function go () {
   await _deleteBilling()
+  await _deleteGaugingStations()
+  await _deleteTestData('water.chargeElements')
+  await _deleteChargeVersions()
+  await _deleteTestData('water.licenceAgreements')
+  await _deleteReturnRequirements()
+  await _deleteLicenceAgreements()
+  await _deleteTestData('water.financialAgreementTypes')
+  await _deleteTestData('water.licenceVersionPurposes')
+  await _deleteTestData('water.licenceVersions')
+  await _deleteTestData('water.licences')
+  await _deleteTestData('water.regions')
 
   return 'Data deleted'
 }
@@ -22,7 +33,6 @@ async function _deleteBilling () {
     .innerJoin('water.billingBatches as bb', 'bi.billingBatchId', 'bb.billingBatchId')
     .innerJoin('water.regions as r', 'bb.regionId', 'r.regionId')
     .where('r.isTest', true)
-    // .where('bt.description', 'test')
     .del(['bt.billingInvoiceLicenceId'])
 
   const billingInvoiceLicenceIds = billingInvoiceLicences.map((billingInvoiceLicence) => {
@@ -65,6 +75,83 @@ async function _deleteBilling () {
   await db
     .from('water.billingBatches')
     .whereIn('billingBatchId', billingBatchIds)
+    .del()
+}
+
+async function _deleteGaugingStations () {
+  // Delete licenceGaugingStations
+  await db
+    .from('water.licenceGaugingStations as lgs')
+    .innerJoin('water.gaugingStations as gs', 'lgs.gaugingStationId', 'gs.gaugingStationId')
+    .where('gs.isTest', true)
+    .del()
+
+  // Delete gaugingStations
+  await _deleteTestData('water.gaugingStations')
+}
+
+async function _deleteChargeVersions () {
+  // Delete chargeVersionWorkflows
+  await db
+    .from('water.chargeVersionWorkflows')
+    .del()
+
+  // Delete chargeElements
+  await db
+    .from('water.chargeElements as ce')
+    .innerJoin('water.chargeVersion as cv', 'ce.chargeVersionId', 'cv.chargeVersionId')
+    .innerJoin('water.licences as l', 'cv.licenceId', 'l.licenceId')
+    .where('l.isTest', true)
+    .del()
+
+  // Delete chargeversions
+  await db
+    .from('water.chargeversions as cv')
+    .innerJoin('water.licences as l', 'cv.licenceId', 'l.licenceId')
+    .where('l.isTest', true)
+    .del()
+}
+
+async function _deleteReturnRequirements () {
+  // Delete returnRequirementPurposes
+  await db
+    .from('water.returnRequirementPurposes as rrp')
+    .innerJoin('water.returnRequirements as rr', 'rrp.returnRequirementId', 'rr.returnRequirementId')
+    .innerJoin('water.returnVersions as rv', 'rr.returnVersionId', 'rv.returnVersionId')
+    .innerJoin('water.licences as l', 'rv.licenceId', 'l.licenceId')
+    .where('l.isTest', true)
+    .del()
+
+  // Delete returnRequirements
+  await db
+    .from('water.returnrequirements as rr')
+    .innerJoin('water.returnVersions as rv', 'rr.returnVersionId', 'rv.returnVersionId')
+    .innerJoin('water.licences as l', 'rv.licenceId', 'l.licenceId')
+    .where('l.isTest', true)
+    .del()
+
+  // Delete returnVersions
+  await db
+    .from('water.returnVersions as rv')
+    .innerJoin('water.licences as l', 'rv.licenceId', 'l.licenceId')
+    .where('l.isTest', true)
+    .del()
+}
+
+async function _deleteLicenceAgreements () {
+  await db
+    .from('water.licenceAgreements as la')
+    .innerJoin('water.licences as l', 'la.licenceRef', 'l.licenceRef')
+    .where('l.isTest', true)
+    .del()
+
+  await _deleteTestData('water.licenceAgreements')
+}
+
+async function _deleteTestData (tableName) {
+  await db
+    .from(tableName)
+    .where('isTest', true)
     .del()
 }
 
