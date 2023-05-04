2625b3edc375aedeab018e087658e0c97eef81bc
Jason Claxton <jason.claxton@defra.gov.uk>
2023-05-04 17:19:24 +0100

    Delete more data from even more tables

--- app/services/data/tear-down.service.js
+++ app/services/data/tear-down.service.js
@@ -8,6 +8,7 @@
 const { db } = require('../../../db/db.js')
 
 async function go () {
+  // water schema
   await _deleteBilling()
   await _deleteGaugingStations()
   await _deleteTestData('water.chargeElements')
@@ -20,6 +21,25 @@ async function go () {
   await _deleteTestData('water.licenceVersions')
   await _deleteTestData('water.licences')
   await _deleteTestData('water.regions')
+
+  // crm schemas
+  await _deleteTestData('crm_v2.documentRoles')
+  await _deleteTestData('crm_v2.companyAddresses')
+  await _deleteEntity()
+  await _deleteInvoiceAccounts()
+  await _deleteTestData('crm_v2.companyContacts')
+  await _deleteTestData('crm_v2.companies')
+  await _deleteTestData('crm_v2.addresses')
+  await _deleteTestData('crm_v2.documents')
+  await _deleteTestData('crm_v2.contacts')
+
+  // returns schema
+  await _deleteReturns()
+
+  // purposes data
+  await _deleteTestData('water.purposesPrimary')
+  await _deleteTestData('water.purposesSecondary')
+  await _deleteTestData('water.purposesUses')
 
   return 'Data deleted'
 }
@@ -99,14 +119,14 @@ async function _deleteChargeVersions () {
   // Delete chargeElements
   await db
     .from('water.chargeElements as ce')
-    .innerJoin('water.chargeVersion as cv', 'ce.chargeVersionId', 'cv.chargeVersionId')
+    .innerJoin('water.chargeVersions as cv', 'ce.chargeVersionId', 'cv.chargeVersionId')
     .innerJoin('water.licences as l', 'cv.licenceId', 'l.licenceId')
     .where('l.isTest', true)
     .del()
 
   // Delete chargeversions
   await db
-    .from('water.chargeversions as cv')
+    .from('water.chargeVersions as cv')
     .innerJoin('water.licences as l', 'cv.licenceId', 'l.licenceId')
     .where('l.isTest', true)
     .del()
@@ -124,7 +144,7 @@ async function _deleteReturnRequirements () {
 
   // Delete returnRequirements
   await db
-    .from('water.returnrequirements as rr')
+    .from('water.returnRequirements as rr')
     .innerJoin('water.returnVersions as rv', 'rr.returnVersionId', 'rv.returnVersionId')
     .innerJoin('water.licences as l', 'rv.licenceId', 'l.licenceId')
     .where('l.isTest', true)
@@ -146,6 +166,49 @@ async function _deleteLicenceAgreements () {
     .del()
 
   await _deleteTestData('water.licenceAgreements')
+}
+
+async function _deleteEntity () {
+  await db
+    .from('crm.entity')
+    .whereLike('entityNm', 'acceptance-test.%')
+    .orWhereLike('entityNm', '%@example.com')
+    .orWhereLike('entityNm', 'regression.tests.%')
+    .del()
+}
+
+async function _deleteInvoiceAccounts () {
+  await db
+    .from('crm_v2.invoiceAccountAddresses as iaa')
+    .innerJoin('crm_v2.invoiceAccounts as ia', 'iaa.invoiceAccountId', 'ia.invoiceAccountId')
+    .innerJoin('crm_v2.companies as c', 'ia.companyId', 'c.companyId')
+    .where('c.isTest', true)
+    .del()
+
+  await _deleteTestData('crm_v2.invoiceAccountAddresses')
+
+  await db
+    .from('crm_v2.invoiceAccounts as ia')
+    .innerJoin('crm_v2.companies as c', 'ia.companyId', 'c.companyId')
+    .where('c.isTest', true)
+    .del()
+}
+
+async function _deleteReturns () {
+  await db
+    .from('returns.lines as l')
+    .innerJoin('returns.versions as v', 'l.versionId', 'v.versionId')
+    .innerJoin('returns.returns as r', 'v.returnId', 'r.returnId')
+    .where('r.isTest', true)
+    .del()
+
+  await db
+    .from('returns.versions as v')
+    .innerJoin('returns.returns as r', 'v.returnId', 'r.returnId')
+    .where('r.isTest', true)
+    .del()
+
+  await _deleteTestData('returns.returns')
 }
 
 async function _deleteTestData (tableName) {
