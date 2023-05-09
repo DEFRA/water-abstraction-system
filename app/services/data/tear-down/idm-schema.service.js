4bb87a79b1b88ef08904756effea9ee2adab0daa
Jason Claxton <jason.claxton@defra.gov.uk>
2023-05-09 15:18:27 +0100

    Move idm schema code into its own service

---
+++ app/services/data/tear-down/idm-schema.service.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+ * Removes all data created for acceptance tests from the idm schema
+ * @module TearDownIdmSchemaService
+ */
+
+const { db } = require('../../../../db/db.js')
+
+async function go () {
+  await db
+    .from('idm.users')
+    .where(db.raw("user_data->>'source' = 'acceptance-test-setup'"))
+    .orWhereLike('userName', '%@example.com')
+    .del()
+}
+
+module.exports = {
+  go
+}
--- app/services/data/tear-down/tear-down.service.js
+++ app/services/data/tear-down/tear-down.service.js
@@ -5,28 +5,18 @@
  * @module TearDownService
  */
 
-const { db } = require('../../../../db/db.js')
 const WaterSchemaService = require('./water-schema.service.js')
 const CrmSchemaService = require('./crm-schema.service.js')
 const ReturnsSchemaService = require('./returns-schema.service.js')
 const PermitSchemaService = require('./permit-schema.service.js')
+const IdmSchemaService = require('./idm-schema.service.js')
 
 async function go () {
   await WaterSchemaService.go()
   await CrmSchemaService.go()
   await ReturnsSchemaService.go()
   await PermitSchemaService.go()
-
-  // idm schema
-  await _deleteUsers()
-}
-
-async function _deleteUsers () {
-  await db
-    .from('idm.users')
-    .where(db.raw("user_data->>'source' = 'acceptance-test-setup'"))
-    .orWhereLike('userName', '%@example.com')
-    .del()
+  await IdmSchemaService.go()
 }
 
 module.exports = {
