bf54e8d4d9ffc7307afb20810fc0c4680e4c6560
Jason Claxton <jason.claxton@defra.gov.uk>
2023-05-10 13:10:05 +0100

    Add some error handling

--- app/controllers/data/data.controller.js
+++ app/controllers/data/data.controller.js
@@ -5,12 +5,17 @@
  * @module DataController
  */
 
+const Boom = require('@hapi/boom')
 const TearDownService = require('../../services/data/tear-down/tear-down.service.js')
 
 async function tearDown (_request, h) {
-  await TearDownService.go()
+  try {
+    await TearDownService.go()
 
-  return h.response().code(204)
+    return h.response().code(204)
+  } catch (error) {
+    return Boom.badImplementation(error.message)
+  }
 }
 
 module.exports = {
--- app/services/data/tear-down/tear-down.service.js
+++ app/services/data/tear-down/tear-down.service.js
@@ -12,11 +12,25 @@ const PermitSchemaService = require('./permit-schema.service.js')
 const IdmSchemaService = require('./idm-schema.service.js')
 
 async function go () {
-  await WaterSchemaService.go()
-  await CrmSchemaService.go()
-  await ReturnsSchemaService.go()
-  await PermitSchemaService.go()
-  await IdmSchemaService.go()
+  const startTime = process.hrtime.bigint()
+
+  await Promise.all([
+    WaterSchemaService.go(),
+    CrmSchemaService.go(),
+    ReturnsSchemaService.go(),
+    PermitSchemaService.go(),
+    IdmSchemaService.go()
+  ])
+
+  _calculateAndLogTime(startTime)
+}
+
+function _calculateAndLogTime (startTime) {
+  const endTime = process.hrtime.bigint()
+  const timeTakenNs = endTime - startTime
+  const timeTakenMs = timeTakenNs / 1000000n
+
+  global.GlobalNotifier.omg(`Tear down: Time taken to process ${timeTakenMs}ms`)
 }
 
 module.exports = {
