1f1b0aaba2c6bfe133a4eb079693ca68071ad12d
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-05-11 16:39:45 +0100

    Refactor `ProcessBillingBatchService` - first pass (#214)
    https://eaflood.atlassian.net/browse/WATER-3996
    
    While reviewing `ProcessBillingBatchService` we realised there is scope to make it more performant. This is a first pass which tackles the issue of us fetching invoice accounts one at a time within the main loop. We refactor so that we now fetch all the invoice accounts we will need in a single query prior to entering the main loop and hold them in a keyed object:
    ```js
    {
      '610ca07a-3bd3-4ea2-8cb1-82f6977cdc71': { invoiceAccountId: '610ca07a-3bd3-4ea2-8cb1-82f6977cdc71', ... },
      '03cec07f-9ad3-4c9a-bad8-1958cec0d61f': { invoiceAccountId: '03cec07f-9ad3-4c9a-bad8-1958cec0d61f', ... }
    }
    ```
    This means that we can easily retrieve a given invoice account once we're in the main loop:
    ```js
    const invoiceAccountId = invoiceAccounts['610ca07a-3bd3-4ea2-8cb1-82f6977cdc71']
    ```

--- app/services/supplementary-billing/generate-billing-invoice.service.js
+++ app/services/supplementary-billing/generate-billing-invoice.service.js
@@ -7,44 +7,20 @@
 
 const { randomUUID } = require('crypto')
 
-const InvoiceAccountModel = require('../../models/crm-v2/invoice-account.model.js')
-
 /**
- * Return either a new billing invoice object ready for persisting or an existing one if it exists
+ * Return a billing invoice object ready for persisting
  *
- * This first checks whether the invoice account ID of `currentBillingInvoice` matches the one passed to this service.
- * If it does, we return that instance.
- *
- * If it doesn't, we generate a new instance and return it.
- *
- * For context, this is all to avoid creating `billing_invoice` and `billing_invoice_licence` records unnecessarily.
- * The legacy service will create them first, then determine if there are any transactions to be billed. If there
- * aren't, it then has to go back and delete the records it created.
- *
- * Our intent is to only call the DB when we have records that need persisting. So, we start at the transaction level
- * and only persist `billing_invoice` and `billing_invoice_licence` records that are linked to billable transactions.
- * But to persist the billing transactions we need the foreign keys. So, we generate our billing invoice and billing
- * licence data in memory along with ID's, and use this service to provide the right record when persisting the
- * transaction.
- *
- * @param {module:BillingInvoiceModel} currentBillingInvoice A billing invoice object
- * @param {String} invoiceAccountId UUID of the invoice account this billing invoice will be linked to if persisted
- * @param {String} billingBatchId UUID of the billing batch this billing invoice will be linked to if persisted
+ * @param {module:InvoiceAccountModel} invoiceAccount The invoice account this billing invoice will be linked to
+ * @param {String} billingBatchId UUID of the billing batch this billing invoice will be linked to
  * @param {Number} financialYearEnding A value that must exist in the persisted record
  *
- * @returns {Object} The current or newly-generated billing invoice object
+ * @returns {Object} The billing invoice object ready to be persisted
  */
-async function go (currentBillingInvoice, invoiceAccountId, billingBatchId, financialYearEnding) {
-  if (currentBillingInvoice?.invoiceAccountId === invoiceAccountId) {
-    return currentBillingInvoice
-  }
-
-  const invoiceAccount = await InvoiceAccountModel.query().findById(invoiceAccountId)
-
+function go (invoiceAccount, billingBatchId, financialYearEnding) {
   const billingInvoice = {
     billingBatchId,
     financialYearEnding,
-    invoiceAccountId,
+    invoiceAccountId: invoiceAccount.invoiceAccountId,
     billingInvoiceId: randomUUID({ disableEntropyCache: true }),
     address: {}, // Address is set to an empty object for SROC billing invoices
     invoiceAccountNumber: invoiceAccount.invoiceAccountNumber,
