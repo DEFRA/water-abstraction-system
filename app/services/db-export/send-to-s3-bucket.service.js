ce127d774935cfecc06000c15f7759b031f59431
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2023-05-16 16:32:03 +0100

    Add endpoint for Db export (#223)
    https://eaflood.atlassian.net/browse/WATER-4008
    
    This pull request is just a small part of a larger project that involves exporting all our database schemas, converting them into CSV files, and uploading them to our Amazon S3 bucket. This PR's primary focus is creating the endpoint which can kick off the entire db-export service. To expedite the export process and see the output sooner, we are using a vertical slicing approach, rather than a horizontal one, which means exporting a single table at a time from each schema.

--- app/services/db-export/send-to-s3-bucket.service.js
+++ app/services/db-export/send-to-s3-bucket.service.js
@@ -9,7 +9,7 @@ const fs = require('fs')
 const path = require('path')
 const { PutObjectCommand, S3Client } = require('@aws-sdk/client-s3')
 
-const S3Config = require('../../../config/s3.config')
+const S3Config = require('../../../config/s3.config.js')
 /**
  * Sends a file to our AWS S3 Bucket using the filePath that it receives
  *
