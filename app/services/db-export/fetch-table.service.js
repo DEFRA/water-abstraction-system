bff251dc827e303632f9fdecb931b7f0654eef8c
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2023-05-22 11:12:41 +0100

    Generalising fetch billing charge categories service (#232)
    https://eaflood.atlassian.net/browse/WATER-4015
    
    This pull request is just a small part of a larger project that involves exporting all our database schemas, converting them into CSV files, and uploading them to our Amazon S3 bucket. This PR's primary focus is to generalize the current service we have so that it can take any table as an argument. To expedite the export process and see the output sooner, we are using a vertical slicing approach, rather than a horizontal one, which means exporting a single table at a time from each schema.

---
+++ app/services/db-export/fetch-table.service.js
@@ -0,0 +1,49 @@
+'use strict'
+
+/**
+ * Fetch all records from the table provided
+ * @module FetchTableService
+ */
+
+const { db } = require('../../../db/db.js')
+
+/**
+ * Retrieves headers, rows and the table name from the table in the db, and returns them as an object
+ * @param {String} tableName The name of the table to retrieve
+ * @param {string} schemaName The schema that the table belongs to
+ *
+ * @returns {Object} The headers, rows and table name from the table
+ */
+async function go (tableName, schemaName) {
+  const data = {
+    headers: await _headers(tableName, schemaName),
+    rows: await _rows(tableName, schemaName),
+    tableName
+  }
+
+  return data
+}
+
+async function _rows (tableName, schemaName) {
+  // Retrieves all rows from the table
+  const rows = await db
+    .withSchema(schemaName)
+    .select('*')
+    .from(tableName)
+
+  // We are only interested in the values from the table
+  return rows.map((row) => {
+    return Object.values(row)
+  })
+}
+
+async function _headers (tableName, schemaName) {
+  const columns = await db(tableName).withSchema(schemaName).columnInfo()
+
+  // We are only interested in the column names
+  return Object.keys(columns)
+}
+
+module.exports = {
+  go
+}
