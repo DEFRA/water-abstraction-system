0e635da7604726b9f62f7b46746c6baa2235ad65
Rebecca Ransome <beckyrose200@aol.com>
2023-05-25 12:54:47 +0100

    Remove schemaNames

--- app/services/db-export/db-export.service.js
+++ app/services/db-export/db-export.service.js
@@ -11,12 +11,6 @@ const SchemaExportService = require('../db-export/schema-export.service')
  * Calls TableExportsService giving it a tableName and schemaName
  */
 async function go () {
-  const schemaNames = ['water', 'returns', 'crm', 'crm_v2', 'idm', 'permit']
-
-  // for (const schemaName of schemaNames) {
-  //   await SchemaExportService.go(schemaName)
-  // }
-
   await SchemaExportService.go('water')
 }
 
