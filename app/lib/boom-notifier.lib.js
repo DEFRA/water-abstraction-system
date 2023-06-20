075a983eaa9e171d3d8798db6b35a0099fb81331
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-06-20 11:22:45 +0100

    Update BoomNotifier based on changes

--- app/lib/boom-notifier.lib.js
+++ app/lib/boom-notifier.lib.js
@@ -13,11 +13,11 @@ const RequestNotifierLib = require('./request-notifier.lib.js')
  * A combined logging and Airbrake (Errbit) notification manager which extends RequestNotifierLib to also throw a Boom
  * `400` error if the .omfg method is called.
  *
- * The use case for RequestNotifierLib is to log errors which occur in syncronous "background" tasks -- ie. where a
+ * The use case for RequestNotifierLib is to log errors which occur in synchronous "background" tasks -- ie. where a
  * controller calls a service then immediately returns a response while the service continues to run. Any errors thrown
  * within the background task are caught and logged using the notifier.
  *
- * The use case for BoomNotifierLib is for when we wish to run one of these background tasks asyncronously -- for
+ * The use case for BoomNotifierLib is for when we wish to run one of these background tasks asynchronously -- for
  * example, sending a bill run would normally be done in the background but we want the 'send bill run' admin endpoint
  * to run it in the foreground so any errors which occur are returned straight away so we don't need to dig through the
  * logs.
