f4851a0f22fe6e070a90c2708fd5a0c789f67ecb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-07-10 17:05:32 +0100

    Add new check TwoPartService
    This will serve as a wrapper for calling the services we'll start building to support 2PT bill runs.
    
    We know just like supplementary region is the primary filter for which licences to include in the bill run. So, to get us started with something we build it with the expectation that we need to fetch it.
    
    Side note - looks like we've found our first generic billing service!

---
+++ app/services/check/two-part.service.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+ * Used to test the Two-part tariff matching logic
+ * @module SupplementaryDataService
+ */
+
+const FetchRegionService = require('../billing/supplementary/fetch-region.service.js')
+
+async function go (naldRegionId) {
+  const region = await FetchRegionService.go(naldRegionId)
+
+  return {
+    regionName: region.name
+  }
+}
+
+module.exports = {
+  go
+}
