f92fd98c2c292772f365afd1ac868e1f79dd91ef
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-07-11 12:25:36 +0100

    Make create bill run validator generic
    We update the validator to accept both supplementary and 2PT arguments (based on what the UI will fire at us).
    
    In the [charging-module-api](https://github.com/DEFRA/sroc-charging-module-api) we used the convention of keeping any static list of values in a central place.
    
    This seemed an apt time to bring in that concept to this project and make use of it in the validator.

--- app/validators/bill-runs/create-bill-run.validator.js
+++ app/validators/bill-runs/create-bill-run.validator.js
@@ -6,12 +6,14 @@
 
 const Joi = require('joi')
 
+const StaticLookupsLib = require('../../lib/static-lookups.lib.js')
+
 /**
  * Checks that the payload of a `create bill run` request is valid
 */
 function go (data) {
   const schema = Joi.object({
-    type: Joi.string().valid('supplementary').required(),
+    type: Joi.string().valid(...StaticLookupsLib.billRunTypes).required(),
     scheme: Joi.string().valid('sroc').required(),
     region: Joi.string().guid().required(),
     user: Joi.string().email().required(),
