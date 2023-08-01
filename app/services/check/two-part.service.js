aac510f8c0d8b6b90e9b6c7e36ca0db598fe79dd
Jason Claxton <jason.claxton@defra.gov.uk>
2023-08-01 10:03:19 +0100

    Add code to identify licences

--- app/services/check/two-part.service.js
+++ app/services/check/two-part.service.js
@@ -5,14 +5,26 @@
  * @module SupplementaryDataService
  */
 
+const ChargeVersion = require('../../models/water/charge-version.model.js')
+const DetermineBillingPeriodsService = require('../billing/determine-billing-periods.service.js')
 const FetchRegionService = require('../billing/fetch-region.service.js')
 
 async function go (naldRegionId) {
   const region = await FetchRegionService.go(naldRegionId)
+  const billingPeriods = DetermineBillingPeriodsService.go()
 
-  return {
-    regionName: region.name
-  }
+  const billingPeriod = billingPeriods[1]
+
+  const licenceRefs = await ChargeVersion.query()
+    .select('licenceRef')
+    .innerJoinRelated('chargeElements')
+    .where('regionCode', naldRegionId)
+    .where('chargeVersions.scheme', 'sroc')
+    .where('startDate', '<=', billingPeriod.endDate)
+    .where('isSection127AgreementEnabled', true)
+    .whereNot('status', 'draft')
+
+  return [region.name, billingPeriods[1], licenceRefs]
 }
 
 module.exports = {
