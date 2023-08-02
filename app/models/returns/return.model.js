8c81c6f0e7b69a1bce5b445bc3a2d8f400dcf3c2
Jason Claxton <jason.claxton@defra.gov.uk>
2023-08-02 14:07:41 +0100

    Add jsonAttributes to return model

--- app/models/returns/return.model.js
+++ app/models/returns/return.model.js
@@ -19,6 +19,13 @@ class ReturnModel extends ReturnsBaseModel {
   static get translations () {
     return []
   }
+
+  // Defining which fields contain json allows us to insert an object without needing to stringify it first
+  static get jsonAttributes () {
+    return [
+      'metadata'
+    ]
+  }
 }
 
 module.exports = ReturnModel
