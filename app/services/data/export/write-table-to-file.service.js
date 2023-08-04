47b73d1612d0fbea00a1a383a5904bfaaa5338d4
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2023-08-04 14:47:05 +0100

    Using streams on schema export service (#319)
    https://eaflood.atlassian.net/browse/WATER-4019
    
    This PR's primary focus is to fix the schema export service. The service is currently not working due to the fact it reaches its memory capacity when it exports the returns table. This is due to the fact the full table is brought into memory. This fix introduced streams into the service to allow the table to be written immediately to a file and not be held in full in memory.

---
+++ app/services/data/export/write-table-to-file.service.js
@@ -0,0 +1,74 @@
+'use strict'
+
+/**
+ * Export converted data to a temporary file
+ * @module WriteTableToFileService
+ */
+
+const fs = require('fs')
+const fsPromises = fs.promises
+const { pipeline, Transform } = require('stream')
+const path = require('path')
+const util = require('util')
+
+const ConvertToCSVService = require('./convert-to-csv.service.js')
+
+/**
+ * Converts data into CSV format and writes it to a file
+ *  @param {Object} data The knex query to fetch the table
+ *  @param {String} schemaFolderPath The folder path of the schema
+ *  @param {String} tableName The name of the table
+ */
+async function go (headers, rows, schemaFolderPath, tableName) {
+  const filePath = await _filenameWithPath(tableName, schemaFolderPath)
+  const writeToFileStream = fs.createWriteStream(filePath, { flags: 'a' })
+  const promisifiedPipeline = util.promisify(pipeline)
+
+  const inputStream = await rows
+
+  const transformDataStream = _transformDataStream()
+
+  const convertedHeaders = ConvertToCSVService.go(headers)
+  writeToFileStream.write(convertedHeaders)
+
+  await promisifiedPipeline(inputStream, transformDataStream, writeToFileStream)
+}
+
+/**
+ * Creates and returns a Transform stream that converts incoming objects to CSV rows
+ *
+ * @returns {Transform} A transform stream with objectMode set to true
+ */
+function _transformDataStream () {
+  return new Transform({
+    objectMode: true,
+    transform: function (row, _encoding, callback) {
+      const datRow = ConvertToCSVService.go(Object.values(row))
+      callback(null, datRow)
+    }
+  })
+}
+
+/**
+ * Returns a file path by joining the schema folder path with the file name.
+ * The schema path has already been created with the temporary directory
+ *
+ * @param {String} tableName The name of the table
+ * @param {String} schemaFolderPath The folder path of the schema
+ *
+ * @returns {String} The full file path
+ */
+async function _filenameWithPath (tableName, schemaFolderPath) {
+  await fsPromises.mkdir(schemaFolderPath, { recursive: true })
+
+  return path.normalize(
+    path.format({
+      dir: schemaFolderPath,
+      name: `${tableName}.csv`
+    })
+  )
+}
+
+module.exports = {
+  go
+}
