288dc3e630b2c7c8178070cf8a1ca21c2188d315
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-08-25 14:07:31 +0100

    Add company relationships

--- app/models/crm-v2/company.model.js
+++ app/models/crm-v2/company.model.js
@@ -5,6 +5,8 @@
  * @module CompanyModel
  */
 
+const { Model } = require('objection')
+
 const CrmV2BaseModel = require('./crm-v2-base.model.js')
 
 class CompanyModel extends CrmV2BaseModel {
@@ -21,6 +23,27 @@ class CompanyModel extends CrmV2BaseModel {
       { database: 'dateCreated', model: 'createdAt' },
       { database: 'dateUpdated', model: 'updatedAt' }
     ]
+  }
+
+  static get relationMappings () {
+    return {
+      invoiceAccounts: {
+        relation: Model.HasManyRelation,
+        modelClass: 'invoice-account.model',
+        join: {
+          from: 'companies.companyId',
+          to: 'invoiceAccounts.companyId'
+        }
+      },
+      invoiceAccountAddresses: {
+        relation: Model.HasManyRelation,
+        modelClass: 'invoice-account-address.model',
+        join: {
+          from: 'companies.companyId',
+          to: 'invoiceAccountAddresses.agentCompanyId'
+        }
+      }
+    }
   }
 }
 
