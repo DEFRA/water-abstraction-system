53b472508d6fd47b112f578857f8cdc7782f9179
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2023-08-29 16:01:08 +0100

    Create `group`, `role` and `user` models for authentication plugin (#387)
    https://eaflood.atlassian.net/browse/WATER-4085
    
    In anticipation of developing our [authentication plugin](#351) we create models for the `group`, `role` and `user` tables, along with helpers and unit tests.
    
    The models are all straightforward ones with the exception of `UserModel`, which adds a static method `generateHashedPassword`. This takes a plaintext password and returns a salted and hashed version. The code is taken from `db/seeds/01-users.js`; the intent is that the seed file will at some point be refactored to insert users using `UserModel`, and therefore use the static method in place of the existing function.

---
+++ app/models/idm/user.model.js
@@ -0,0 +1,46 @@
+'use strict'
+
+/**
+ * Model for user
+ * @module UserModel
+ */
+
+const { hashSync } = require('bcryptjs')
+
+const IDMBaseModel = require('./idm-base.model.js')
+
+class UserModel extends IDMBaseModel {
+  static get tableName () {
+    return 'users'
+  }
+
+  static get idColumn () {
+    return 'userId'
+  }
+
+  static get translations () {
+    return [
+      { database: 'dateCreated', model: 'createdAt' },
+      { database: 'dateUpdated', model: 'updatedAt' }
+    ]
+  }
+
+  // Defining which fields contain json allows us to insert an object without needing to stringify it first
+  static get jsonAttributes () {
+    return [
+      'user_data',
+      'role'
+    ]
+  }
+
+  static generateHashedPassword (password) {
+    // 10 is the number of salt rounds to perform to generate the salt. The legacy code uses
+    // const salt = bcrypt.genSaltSync(10) to pre-generate the salt before passing it to hashSync(). But this is
+    // intended for operations where you need to hash a large number of values. If you just pass in a number bcrypt will
+    // autogenerate the salt for you.
+    // https://github.com/kelektiv/node.bcrypt.js#usage
+    return hashSync(password, 10)
+  }
+}
+
+module.exports = UserModel
