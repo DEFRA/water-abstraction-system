f78ba5d8d0640b5026cfc344941eca3fd940bd45
Rebecca Ransome <beckyrose200@aol.com>
2023-09-01 09:50:33 +0100

    Remove multipart upload

--- app/services/data/export/send-to-s3-bucket.service.js
+++ app/services/data/export/send-to-s3-bucket.service.js
@@ -5,14 +5,7 @@
  * @module SendToS3BucketService
  */
 
-const {
-  CreateMultipartUploadCommand,
-  UploadPartCommand,
-  CompleteMultipartUploadCommand,
-  S3Client,
-  AbortMultipartUploadCommand,
-  PutObjectCommand
-} = require('@aws-sdk/client-s3')
+const { PutObjectCommand, S3Client } = require('@aws-sdk/client-s3')
 const fsPromises = require('fs').promises
 const { HttpsProxyAgent, HttpProxyAgent } = require('hpagent')
 const { NodeHttpHandler } = require('@smithy/node-http-handler')
@@ -21,75 +14,31 @@ const path = require('path')
 const requestConfig = require('../../../../config/request.config.js')
 const S3Config = require('../../../../config/s3.config.js')
 
-const FIVE_MB_IN_BYTES = 5242880
-
 /**
- * Sends a file to our AWS S3 Bucket using the filePath that it receives and setting the config
+ * Sends a file to our AWS S3 Bucket using the filePath that it receives
  *
  * @param {String} filePath A string containing the path of the file to send to the S3 bucket
  */
 async function go (filePath) {
-  console.log('Uploading to S3 bucket')
   const bucketName = S3Config.s3.bucket
   const fileName = path.basename(filePath)
-  const bucketPath = `export/${fileName}`
-  const file = await fsPromises.readFile(filePath)
-  const buffer = Buffer.from(file, 'utf8')
+  const fileContent = await fsPromises.readFile(filePath)
+  const params = {
+    Bucket: bucketName,
+    Key: `export/${fileName}`,
+    Body: fileContent
+  }
 
-  const customConfig = _setCustomConfig()
-  await _uploadSingleFile(bucketName, bucketPath, buffer, customConfig)
-
-  // if (_singleUpload(buffer)) {
-  //   await _uploadSingleFile(bucketName, bucketPath, buffer, customConfig)
-  // } else {
-  //   await _uploadMultipartFile(bucketName, bucketPath, buffer, customConfig)
-  // }
+  await _uploadFileToS3Bucket(params)
 }
 
 /**
- * Completes the multipart upload for the Amazon s3 bucket
- */
-async function _completeMultipartUploadCommand (s3Client, bucketName, bucketPath, uploadId, uploadResults) {
-  return s3Client.send(
-    new CompleteMultipartUploadCommand({
-      Bucket: bucketName,
-      Key: bucketPath,
-      UploadId: uploadId,
-      MultipartUpload: {
-        Parts: uploadResults.map(({ ETag }, i) => ({
-          ETag,
-          PartNumber: i + 1
-        }))
-      }
-    })
-  )
-}
-
-/**
- * Creates a multipart upload in our Amazon s3 bucket
- *
- * @returns {String} The uploadId associated with the created multipart upload
- */
-async function _createMultipartUpload (s3Client, bucketName, bucketPath) {
-  const multipartUpload = await s3Client.send(
-    new CreateMultipartUploadCommand({
-      Bucket: bucketName,
-      Key: bucketPath
-    })
-  )
-
-  return multipartUpload.UploadId
-}
-
-/**
- * Sets the configuration settings for the S3 bucket
+ * Sets the configuration settings for the S3 client
  *
  * If the environment has a proxy then we set that here. The default timeout is 6 minutes but we believe that is far too
  * long to wait. So, we set 'connectionTimeout' to be 10 seconds.
- *
- * @returns {} Custom configuration settings
  */
-function _setCustomConfig () {
+function _customConfig () {
   return {
     requestHandler: new NodeHttpHandler({
     // This uses the ternary operator to give either an `http/httpsAgent` object or an empty object, and the spread
@@ -105,108 +54,12 @@ function _setCustomConfig () {
   }
 }
 
-/**
- * Returns the upload type based on its size to the specified S3 bucket
- *
- * AWS S3 bucket have conditions on using multi-part uploads. One of these conditions is the file has to be over 5 MB.
- * Anything under 5MB must be a single upload. By isolating the service in its own file, we can create a stub for
- * testing upload types with the "send-to-S3-bucket" service.
- * {@link https://docs.aws.amazon.com/AmazonS3/latest/userguide/qfacts.html S3 multipart upload limits}
- *
- * @param {Buffer} buffer The file content as a buffer object
- *
- * @returns {Boolean} True if the buffer is smaller than 5 MB else false
- */
-function _singleUpload (buffer) {
-  return buffer.length <= FIVE_MB_IN_BYTES
-}
-
-/**
- * Splits the buffer into upload parts and sends them to be uploaded
- *
- * @returns An array of responses from the upload promises for each part.
- */
-async function _splitIntoPartsAndUpload (s3Client, bucketName, bucketPath, buffer, uploadId) {
-  const uploadPromises = []
-  // Each part size needs to be a minimum of 5MB to use multipart upload
-  // Calculating how many parts there will be depending on the size of the buffer
-  const totalParts = Math.ceil(buffer.length / FIVE_MB_IN_BYTES)
-
-  await _uploadPart(uploadPromises, buffer, totalParts, bucketName, s3Client, bucketPath, uploadId)
-
-  return Promise.all(uploadPromises)
-}
-
-/**
- * Uploads a file in multiple parts to the specified S3 bucket with the provided key
- */
-async function _uploadMultipartFile (bucketName, bucketPath, buffer, customConfig) {
+async function _uploadFileToS3Bucket (params) {
+  const customConfig = _customConfig()
   const s3Client = new S3Client(customConfig)
-  let uploadId
+  const command = new PutObjectCommand(params)
 
-  try {
-    const uploadId = await _createMultipartUpload(s3Client, bucketName, bucketPath)
-
-    const uploadResults = await _splitIntoPartsAndUpload(s3Client, bucketName, bucketPath, buffer, uploadId)
-
-    await _completeMultipartUploadCommand(s3Client, bucketName, bucketPath, uploadId, uploadResults)
-  } catch (error) {
-    global.GlobalNotifier.omfg('Send to S3 errored', error)
-
-    if (uploadId) {
-      const abortCommand = new AbortMultipartUploadCommand({
-        Bucket: bucketName,
-        Key: bucketPath,
-        UploadId: uploadId
-      })
-      await S3Client.send(abortCommand)
-    }
-  }
-}
-/**
- * Upload all the parts to the AWS s3 bucket
- */
-async function _uploadPart (uploadPromises, buffer, totalParts, bucketName, s3Client, bucketPath, uploadId) {
-  for (let i = 0; i < totalParts; i++) {
-    const start = i * FIVE_MB_IN_BYTES
-    const end = Math.min(start + FIVE_MB_IN_BYTES, buffer.length)
-    uploadPromises.push(
-      s3Client
-        .send(
-          new UploadPartCommand({
-            Bucket: bucketName,
-            Key: bucketPath,
-            UploadId: uploadId,
-            Body: buffer.subarray(start, end),
-            PartNumber: i + 1
-          })
-        )
-        .then((response) => {
-          // Once the promise is resolved return the response metadata (this includes the ETag)
-          return response
-        })
-    )
-  }
-}
-
-/**
- * Uploads a single file content to the specified S3 bucket with the provided bucket path
- */
-async function _uploadSingleFile (bucketName, bucketPath, buffer, customConfig) {
-  console.log('Single Upload')
-  const s3Client = new S3Client(customConfig)
-
-  try {
-    return s3Client.send(
-      new PutObjectCommand({
-        Bucket: bucketName,
-        Key: bucketPath,
-        Body: buffer
-      })
-    )
-  } catch (error) {
-    global.GlobalNotifier.omfg('Send to S3 errored', error)
-  }
+  await s3Client.send(command)
 }
 
 module.exports = {
--- test/services/data/export/send-to-s3-bucket.service.test.js
+++ test/services/data/export/send-to-s3-bucket.service.test.js
@@ -8,150 +8,48 @@ const Sinon = require('sinon')
 const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
 
-// Test helpers
-const mockFs = require('mock-fs')
-const path = require('path')
-
 // Things we need to stub
-const {
-  S3Client,
-  PutObjectCommand,
-  CreateMultipartUploadCommand,
-  UploadPartCommand,
-  CompleteMultipartUploadCommand
-} = require('@aws-sdk/client-s3')
+const { S3Client, PutObjectCommand } = require('@aws-sdk/client-s3')
 
 // Thing under test
 const SendToS3BucketService = require('../../../../app/services/data/export/send-to-s3-bucket.service.js')
 
-describe('Send to S3 bucket service', () => {
-  let largeFilePath
-  let folderNameWithPath
+describe.only('Send to S3 bucket service', () => {
   let s3Stub
-  let notifierStub
-  const smallFilePath = 'test/fixtures/compress-files.service.csv'
-  const fakeFileName = 'FakeFolder'
 
   beforeEach(() => {
-    notifierStub = { omg: Sinon.stub(), omfg: Sinon.stub() }
-    global.GlobalNotifier = notifierStub
+    // Stub the S3 Client's send method, which is used to run the 'put object' command
+    s3Stub = Sinon.stub(S3Client.prototype, 'send')
   })
 
   afterEach(() => {
-    mockFs.restore()
     Sinon.restore()
-    delete global.GlobalNotifier
   })
 
-  describe('when the upload size is greater than 5MB', () => {
-    beforeEach(() => {
-      folderNameWithPath = 'testFolder'
-      largeFilePath = path.join(folderNameWithPath, 'testFile')
+  describe('when successful', () => {
+    const filePath = 'test/fixtures/compress-files.service.csv'
 
-      mockFs({
-        testFolder: {
-          testFile: createString(size)
-        }
-      })
-    })
+    it('uploads a file to the S3 bucket', async () => {
+      await SendToS3BucketService.go(filePath)
 
-    const size = 10 * 1024 * 1024
+      // Test that the S3 Client was called once
+      expect(s3Stub.calledOnce).to.be.true()
 
-    describe('when successful', () => {
-      beforeEach(() => {
-        s3Stub = Sinon.stub(S3Client.prototype, 'send')
-        s3Stub.onCall(0).resolves({ UploadId: 'UxLUb6yRZp' })
-        s3Stub.onCall(1).resolves({ ETag: '9c4b515515e26034f1a0353873a2599a' })
-        s3Stub.onCall(2).resolves({ ETag: '52152ae3c571b3552b66138eb19a235f' })
-        s3Stub.onCall(3).resolves()
-      })
-
-      it('uploads the file to the S3 bucket', async () => {
-        await SendToS3BucketService.go(largeFilePath)
-
-        // Test that the S3 Client was called once
-        expect(s3Stub.callCount).to.equal(4)
-
-        expect(s3Stub.getCall(0).firstArg).to.be.an.instanceof(CreateMultipartUploadCommand)
-        expect(s3Stub.getCall(1).firstArg).to.be.an.instanceof(UploadPartCommand)
-        expect(s3Stub.getCall(2).firstArg).to.be.an.instanceof(UploadPartCommand)
-        expect(s3Stub.getCall(3).firstArg).to.be.an.instanceof(CompleteMultipartUploadCommand)
-      })
-    })
-
-    describe('when unsuccessful', () => {
-      describe('because an invalid file name is given', () => {
-        beforeEach(() => {
-          s3Stub = Sinon.stub(S3Client.prototype, 'send')
-        })
-
-        it('does not upload a file to the S3 bucket', async () => {
-          await expect(SendToS3BucketService.go(fakeFileName)).to.reject()
-
-          expect(s3Stub.called).to.be.false()
-        })
-      })
-
-      describe('when we can not connect to the bucket', () => {
-        beforeEach(() => {
-          Sinon.stub(S3Client.prototype, 'send').rejects()
-        })
-
-        it('catches the error', async () => {
-          await SendToS3BucketService.go(largeFilePath)
-
-          expect(notifierStub.omfg.calledWith(('Send to S3 errored'))).to.be.true()
-        })
-      })
+      // Get the first call and test that it was called with PutObjectCommand
+      const calledCommand = s3Stub.getCall(0).firstArg
+      expect(calledCommand).to.be.an.instanceof(PutObjectCommand)
     })
   })
 
-  describe('when the upload size is less than 5MB', () => {
-    describe('when successful', () => {
-      beforeEach(() => {
-        s3Stub = Sinon.stub(S3Client.prototype, 'send')
-      })
+  describe('when unsuccessful', () => {
+    describe('because an invalid file name is given', () => {
+      const fileName = 'FakeFolder'
 
-      it('uploads the file to the S3 bucket', async () => {
-        await SendToS3BucketService.go(smallFilePath)
+      it('does not upload a file to the S3 bucket', async () => {
+        await expect(SendToS3BucketService.go(fileName)).to.reject()
 
-        // Test that the S3 Client was called once
-        expect(s3Stub.calledOnce).to.be.true()
-
-        // Get the first call and test that it was called with PutObjectCommand
-        const calledCommand = s3Stub.getCall(0).firstArg
-
-        expect(calledCommand).to.be.an.instanceof(PutObjectCommand)
-      })
-    })
-    describe('when unsuccessful', () => {
-      describe('because an invalid file name is given', () => {
-        beforeEach(() => {
-          s3Stub = Sinon.stub(S3Client.prototype, 'send')
-        })
-
-        it('does not upload a file to the S3 bucket', async () => {
-          await expect(SendToS3BucketService.go(fakeFileName)).to.reject()
-
-          expect(s3Stub.called).to.be.false()
-        })
-      })
-
-      describe('when we can not connect to the bucket', () => {
-        beforeEach(() => {
-          Sinon.stub(S3Client.prototype, 'send').rejects()
-        })
-
-        it('catches the error', async () => {
-          await SendToS3BucketService.go(smallFilePath)
-
-          expect(notifierStub.omfg.calledWith(('Send to S3 errored'))).to.be.true()
-        })
+        expect(s3Stub.called).to.be.false()
       })
     })
   })
 })
-
-function createString (size) {
-  return 'x'.repeat(size)
-}
