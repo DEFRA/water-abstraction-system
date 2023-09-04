bc562cfe364f76923fbe2893289f4eb3687968c3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-09-04 07:52:09 +0100

    Add new billing accounts send customer change svc (#396)
    https://eaflood.atlassian.net/browse/WATER-4092
    
    We are working on migrating the change billing account address functionality from the legacy code to this project. A key part of this is being able to tell the [Charging Module API](https://github.com/DEFRA/sroc-charging-module-api) about the change!
    
    We've [[Added a] new CHA create customer change presenter](https://github.com/DEFRA/water-abstraction-system/pull/395) and a [[..] new CHA create customer change service](https://github.com/DEFRA/water-abstraction-system/pull/394). We now add a new `SendCustomerChangeService` to our /billing-accounts to manage both including dealing with any errors in the process.

---
+++ test/services/billing-accounts/send-customer-change.service.test.js
@@ -0,0 +1,70 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const ExpandedError = require('../../../app/errors/expanded.error.js')
+
+// Things we need to stub
+const ChargingModuleCreateCustomerChangePresenter = require('../../../app/presenters/charging-module/create-customer-change.presenter.js')
+const ChargingModuleCreateCustomerChangeService = require('../../../app/services/charging-module/create-customer-change.service.js')
+
+// Thing under test
+const SendCustomerChangeService = require('../../../app/services/billing-accounts/send-customer-change.service.js')
+
+describe('Send billing transactions service', () => {
+  const invoiceAccount = { invoiceAccountId: '3b53f101-d256-40f8-a6be-ddefb5f9647c' }
+
+  beforeEach(() => {
+    Sinon.stub(ChargingModuleCreateCustomerChangePresenter, 'go').returns({
+      region: 'B',
+      customerReference: 'B19120000A',
+      customerName: 'Mr W Aston',
+      addressLine1: 'Park Farm',
+      addressLine2: 'Sugar Lane',
+      addressLine3: 'West Waterford',
+      addressLine4: 'Angleton',
+      addressLine5: 'Southampton',
+      addressLine6: 'Hampshire',
+      postcode: 'SO74 3KD'
+    })
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when calling the Charging Module API is successful', () => {
+    beforeEach(() => {
+      Sinon.stub(ChargingModuleCreateCustomerChangeService, 'go').resolves({
+        succeeded: true
+      })
+    })
+
+    it('does not throw an error', async () => {
+      await expect(SendCustomerChangeService.go(invoiceAccount)).not.to.reject()
+    })
+  })
+
+  describe('when calling the Charging Module API is unsuccessful', () => {
+    beforeEach(() => {
+      Sinon.stub(ChargingModuleCreateCustomerChangeService, 'go').resolves({
+        succeeded: false
+      })
+    })
+
+    it('throws an error', async () => {
+      const result = await expect(SendCustomerChangeService.go(invoiceAccount)).to.reject()
+
+      expect(result).to.be.an.instanceOf(ExpandedError)
+      expect(result.message).to.equal('Customer change failed to send')
+      expect(result.invoiceAccountId).to.equal(invoiceAccount.invoiceAccountId)
+    })
+  })
+})
