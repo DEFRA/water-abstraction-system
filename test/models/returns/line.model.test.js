461ef36d20139cbb7c3a793cf2f82ad8b94299bf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-09-06 15:31:31 +0100

    Add return schema lines and versions models (#408)
    https://eaflood.atlassian.net/browse/WATER-4098
    
    The next step in the two-part tariff work is to calculate the abstraction volumes for each return matched to a charge reference. After some investigation, we have determined that the data is held in `returns.lines`. The only problem is that the table is linked to `returns.returns` via `returns.versions`.
    
    So, before we can start calculating volumes we need to add these 2 models to the project.

---
+++ test/models/returns/line.model.test.js
@@ -0,0 +1,68 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const DatabaseHelper = require('../../support/helpers/database.helper.js')
+const LineHelper = require('../../support/helpers/returns/line.helper.js')
+const VersionHelper = require('../../support/helpers/returns/version.helper.js')
+const VersionModel = require('../../../app/models/returns/version.model.js')
+
+// Thing under test
+const LineModel = require('../../../app/models/returns/line.model.js')
+
+describe('Line model', () => {
+  let testRecord
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+  })
+
+  describe('Basic query', () => {
+    beforeEach(async () => {
+      testRecord = await LineHelper.add()
+    })
+
+    it('can successfully run a basic query', async () => {
+      const result = await LineModel.query().findById(testRecord.lineId)
+
+      expect(result).to.be.an.instanceOf(LineModel)
+      expect(result.lineId).to.equal(testRecord.lineId)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to version', () => {
+      let testVersion
+
+      beforeEach(async () => {
+        testVersion = await VersionHelper.add()
+        testRecord = await LineHelper.add({ versionId: testVersion.versionId })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await LineModel.query()
+          .innerJoinRelated('version')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the version', async () => {
+        const result = await LineModel.query()
+          .findById(testRecord.lineId)
+          .withGraphFetched('version')
+
+        expect(result).to.be.instanceOf(LineModel)
+        expect(result.lineId).to.equal(testRecord.lineId)
+
+        expect(result.version).to.be.an.instanceOf(VersionModel)
+        expect(result.version).to.equal(testVersion)
+      })
+    })
+  })
+})
---
+++ test/models/returns/version.model.test.js
@@ -0,0 +1,109 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const DatabaseHelper = require('../../support/helpers/database.helper.js')
+const LineHelper = require('../../support/helpers/returns/line.helper.js')
+const LineModel = require('../../../app/models/returns/line.model.js')
+const ReturnHelper = require('../../support/helpers/returns/return.helper.js')
+const ReturnModel = require('../../../app/models/returns/return.model.js')
+const VersionHelper = require('../../support/helpers/returns/version.helper.js')
+
+// Thing under test
+const VersionModel = require('../../../app/models/returns/version.model.js')
+
+describe('Version model', () => {
+  let testRecord
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+  })
+
+  describe('Basic query', () => {
+    beforeEach(async () => {
+      testRecord = await VersionHelper.add()
+    })
+
+    it('can successfully run a basic query', async () => {
+      const result = await VersionModel.query().findById(testRecord.versionId)
+
+      expect(result).to.be.an.instanceOf(VersionModel)
+      expect(result.versionId).to.equal(testRecord.versionId)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to return', () => {
+      let testReturn
+
+      beforeEach(async () => {
+        testReturn = await ReturnHelper.add()
+        testRecord = await VersionHelper.add({ returnId: testReturn.returnId })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await VersionModel.query()
+          .innerJoinRelated('return')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the return', async () => {
+        const result = await VersionModel.query()
+          .findById(testRecord.versionId)
+          .withGraphFetched('return')
+
+        expect(result).to.be.instanceOf(VersionModel)
+        expect(result.versionId).to.equal(testRecord.versionId)
+
+        expect(result.return).to.be.an.instanceOf(ReturnModel)
+        expect(result.return).to.equal(testReturn)
+      })
+    })
+
+    describe('when linking to lines', () => {
+      let testLines
+
+      beforeEach(async () => {
+        testRecord = await VersionHelper.add()
+        const { versionId } = testRecord
+
+        testLines = []
+        for (let i = 0; i < 2; i++) {
+          // NOTE: A constraint in the lines table means you cannot have 2 records with the same versionId, substance,
+          // startDate and endDate
+          const substance = i === 0 ? 'water' : 'dirt'
+          const line = await LineHelper.add({ versionId, substance })
+          testLines.push(line)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await VersionModel.query()
+          .innerJoinRelated('lines')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the lines', async () => {
+        const result = await VersionModel.query()
+          .findById(testRecord.versionId)
+          .withGraphFetched('lines')
+
+        expect(result).to.be.instanceOf(VersionModel)
+        expect(result.versionId).to.equal(testRecord.versionId)
+
+        expect(result.lines).to.be.an.array()
+        expect(result.lines[0]).to.be.an.instanceOf(LineModel)
+        expect(result.lines).to.include(testLines[0])
+        expect(result.lines).to.include(testLines[1])
+      })
+    })
+  })
+})
