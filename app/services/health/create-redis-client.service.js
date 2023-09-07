9dc8dc0afc3e37d8dc0011f576140f23e03c2c2a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-09-07 09:52:53 +0100

    Handle unable to connect
    Found this post <https://stackoverflow.com/a/61133867> which put me onto some additional properties we can pass in the config that make the client throw an error as expected when a command fails to complete rather than using the under-the-hood Redis error handling.
    
    This means should we fail to connect an error will be thrown which we can trap and display in our health info page.

--- app/services/health/create-redis-client.service.js
+++ app/services/health/create-redis-client.service.js
@@ -18,6 +18,10 @@ async function go () {
     port: redisConfig.port,
     password: redisConfig.password,
     ...(redisConfig.disableTls ? {} : { tls: {} }),
+    // When a new Redis instance is created, it will connect to Redis server automatically. If you want to keep
+    // disconnected until a command is called, you pass the lazyConnect option
+    lazyConnect: true,
+    // Don't attempt to retry a request to Redis if a command fails
     maxRetriesPerRequest: 0
   })
 }
