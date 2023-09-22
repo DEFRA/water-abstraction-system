c2b0d7616e13ce18cefd26951ed9b7ae897eb410
Manoj Bisht <manoj.bisht@vodafone.com>
2023-09-22 14:22:36 +0100

    Add jest configuration

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,4 @@
 // jest.config.js
 module.exports = {
-  testEnvironment: 'node',
-  // The glob patterns Jest uses to detect test files
-  testMatch: ['**/tests/**/*.js', '**/?(*.)+(spec|test).js']// Use 'jsdom' if testing in a browser-like environment
-}
+    testEnvironment: 'node', // Use 'jsdom' if testing in a browser-like environment
+};
