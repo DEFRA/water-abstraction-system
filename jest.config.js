fe91572d13579b03e3e2f9835847b7ce35323494
Manoj Bisht <manoj.bisht@vodafone.com>
2023-09-26 14:04:15 +0100

    unit test company model the  jest way

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,6 @@
 // jest.config.js
 module.exports = {
-    testEnvironment: 'node', // Use 'jsdom' if testing in a browser-like environment
+    testEnvironment: 'node',
+    // The glob patterns Jest uses to detect test files
+    testMatch: ['**/tests/**/*.js', '**/?(*.)+(spec|test).js'],// Use 'jsdom' if testing in a browser-like environment
 };
