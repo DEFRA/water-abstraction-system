ce033d1f4b86e9110d12c9c4f06dadead1df61aa
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-09-28 11:45:40 +0100

    Update the jest config file
    We wanted to be able to run jest without lots of errors caused by the un-migrated tests. So, specifying what tests to match was the first change we made.
    
    From there we solved our DB clean problem by referencing a new global jest setup file. Code coverage is critical for us migrating so that config has been added. What we have at this time ensures we when running tests can see the gaps whilst still providing the lcov file that SonarCloud needs.
    
    Finally, we write our tests with the aim that they read as documentation. The best way to see this is when running the tests. The default output removed that ability but adding `verbose: true` means our test output now reads like documentation again.

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,25 @@
-// jest.config.js
+'use strict'
+
 module.exports = {
+  collectCoverage: true,
+  collectCoverageFrom: [
+    '**/app/**/*.js',
+    '**/config/**/*.js',
+    '**/index.js'
+  ],
+  coverageDirectory: './coverage',
+  coverageReporters: [
+    'lcov',
+    ['text', { skipFull: true }]
+  ],
+  globalSetup: './test/support/jest.setup.js',
   testEnvironment: 'node',
   // The glob patterns Jest uses to detect test files
-  testMatch: ['**/tests/**/*.js', '**/?(*.)+(spec|test).js']// Use 'jsdom' if testing in a browser-like environment
+  testMatch: [
+    '**/test/controllers/bill-runs.controller.test.js',
+    '**/test/controllers/billing-accounts.controller.test.js',
+    '**/test/errors/*.test.js',
+    '**/test/models/**/*.test.js'
+  ],
+  verbose: true
 }
