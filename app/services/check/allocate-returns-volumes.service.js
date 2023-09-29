2f73b5fddb49347ed85e95d0879601828ea6059d
Jason Claxton <jason.claxton@defra.gov.uk>
2023-09-29 12:21:10 +0100

    Do some refactoring

--- app/services/check/allocate-returns-volumes.service.js
+++ app/services/check/allocate-returns-volumes.service.js
@@ -9,31 +9,25 @@ function go (chargeReference) {
   let returnVolumeInMegalitres
   // Loop through each return
   chargeReference.returns.forEach((returnData) => {
-    // The volumes on the Return are in Cubic Metres so we convert to Megalitres to match the Charge Version data
+    // The volumes on the return are in Cubic Metres so we convert to Megalitres to match the charge version data
     returnVolumeInMegalitres = returnData.volumes.total / 1000
-    // Loop through each chargeElement
+    // Loop through each charge element
     chargeReference.chargeElements.forEach((chargeElement) => {
-      // Match the Return's purpose to the chargeElements
-      if (
-        returnData.metadata.purposes[0].tertiary.code === chargeElement.purpose.legacyId &&
-        returnData.metadata.nald.periodStartDay === chargeElement.abstractionPeriodStartDay.toString() &&
-        returnData.metadata.nald.periodStartMonth === chargeElement.abstractionPeriodStartMonth.toString() &&
-        returnData.metadata.nald.periodEndDay === chargeElement.abstractionPeriodEndDay.toString() &&
-        returnData.metadata.nald.periodEndMonth === chargeElement.abstractionPeriodEndMonth.toString()
-      ) {
-        // Check to see if any of the return's volume can be allocated to the chargeElement
+      // Check if the return's purpose and abstraction period match the charge element
+      if (_matchReturnToElement(returnData.metadata, chargeElement)) {
+        // Check to see if any of the return's volume can be allocated to the charge element
         if (chargeElement.billableAnnualQuantity < chargeElement.authorisedAnnualQuantity) {
-          // Allocate all of the Return's volume to the element if possible
+          // Allocate all of the return's volume to the charge element if possible
           if (
             (chargeElement.billableAnnualQuantity + returnVolumeInMegalitres) <= chargeElement.authorisedAnnualQuantity
           ) {
             chargeElement.billableAnnualQuantity = chargeElement.billableAnnualQuantity + returnVolumeInMegalitres
             returnVolumeInMegalitres = 0
           } else {
-            // If not possible to add all the Return's volume add as much as possible up to the authorised amount
+            // If not possible to add all the return's volume add as much as possible up to the authorised amount
             const volumeToDeduct = chargeElement.authorisedAnnualQuantity - chargeElement.billableAnnualQuantity
             chargeElement.billableAnnualQuantity = chargeElement.authorisedAnnualQuantity
-            // Update the volume remaining on the Return
+            // Update the volume remaining on the return
             returnVolumeInMegalitres = returnVolumeInMegalitres - volumeToDeduct
           }
         }
@@ -47,6 +41,20 @@ function go (chargeReference) {
       returnData.volumes.unallocated = 0
     }
   })
+}
+
+function _matchReturnToElement (returnMetadata, chargeElement) {
+  if (
+    returnMetadata.purposes[0].tertiary.code === chargeElement.purpose.legacyId &&
+    returnMetadata.nald.periodStartDay === chargeElement.abstractionPeriodStartDay.toString() &&
+    returnMetadata.nald.periodStartMonth === chargeElement.abstractionPeriodStartMonth.toString() &&
+    returnMetadata.nald.periodEndDay === chargeElement.abstractionPeriodEndDay.toString() &&
+    returnMetadata.nald.periodEndMonth === chargeElement.abstractionPeriodEndMonth.toString()
+  ) {
+    return true
+  }
+
+  return false
 }
 
 module.exports = {
