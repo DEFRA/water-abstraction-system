687f1d1226b48bb9655712eefafc6764c86b1ad2
Manoj Bisht <manoj.bisht@vodafone.com>
2023-10-04 17:25:21 +0100

    Refactor lib tests - First Part

--- test/lib/general.lib.test.js
+++ test/lib/general.lib.test.js
@@ -1,13 +1,5 @@
 'use strict'
 
-// Test framework dependencies
-const Lab = require('@hapi/lab')
-const Code = require('@hapi/code')
-const Sinon = require('sinon')
-
-const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
-const { expect } = Code
-
 // Thing under test
 const GeneralLib = require('../../app/lib/general.lib.js')
 
@@ -21,31 +13,32 @@ describe('RequestLib', () => {
       const uuid2 = GeneralLib.generateUUID()
       const uuid3 = GeneralLib.generateUUID()
 
-      expect(uuid1).not.to.equal(uuid2)
-      expect(uuid1).not.to.equal(uuid3)
-      expect(uuid2).not.to.equal(uuid3)
+      expect(uuid1).not.toEqual(uuid2)
+      expect(uuid1).not.toEqual(uuid3)
+      expect(uuid2).not.toEqual(uuid3)
     })
   })
 
   describe('#timestampForPostgres', () => {
-    let clock
-    let testDate
+    // Store the original Date constructor
+    const originalDate = global.Date
 
-    beforeEach(() => {
-      testDate = new Date(2015, 9, 21, 20, 31, 57)
-
-      clock = Sinon.useFakeTimers(testDate)
-    })
-
-    afterEach(() => {
-      clock.restore()
-      Sinon.restore()
-    })
+    // Mock the Date constructor to always return a fixed date
+    global.Date = class extends Date {
+      constructor () {
+        super('2015-10-21T20:31:57.000Z')
+      }
+    }
 
     it('returns the current date and time as an ISO string', () => {
       const result = GeneralLib.timestampForPostgres()
 
-      expect(result).to.equal('2015-10-21T20:31:57.000Z')
+      expect(result).toEqual('2015-10-21T20:31:57.000Z')
+    })
+
+    // Restore the original Date constructor after the tests
+    afterAll(() => {
+      global.Date = originalDate
     })
   })
 })
