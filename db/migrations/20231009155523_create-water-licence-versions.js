96dcb9d1be546819d41163c01bbf4244ab7bacc0
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2023-10-10 11:20:26 +0100

    Create migration script, model, helper and unit tests for `licence_versions` (#456)
    https://eaflood.atlassian.net/browse/WATER-3486
    
    Whilst working on the unit tests for the enhancement in this PR https://github.com/DEFRA/water-abstraction-system/pull/443 it was found that we would need the migration script, model, helper and unit tests for table `licence_versions`.
    
    These will be created in this PR.

---
+++ db/migrations/20231009155523_create-water-licence-versions.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const tableName = 'licence_versions'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .withSchema('water')
+    .createTable(tableName, (table) => {
+      // Primary Key
+      table.uuid('licence_version_id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+
+      // Data
+      table.uuid('licence_id').notNullable()
+      table.integer('issue').notNullable()
+      table.integer('increment').notNullable()
+      table.string('status').notNullable()
+      table.date('start_date').notNullable()
+      table.date('end_date')
+      table.string('external_id').notNullable()
+      table.boolean('is_test').notNullable().defaultTo(false)
+
+      // Legacy timestamps
+      table.timestamp('date_created', { useTz: false }).notNullable()
+      table.timestamp('date_updated', { useTz: false }).notNullable()
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .withSchema('water')
+    .dropTableIfExists(tableName)
+}
