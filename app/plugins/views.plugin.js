8c411d589c592104fa67a3d24b6621fd93cdf108
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-10-21 11:48:58 +0100

    Add auth & user details to view context by default (#477)
    https://eaflood.atlassian.net/browse/WATER-4155
    
    When working on [Update page header with user links](https://github.com/DEFRA/water-abstraction-system/pull/474) we hit a blocker.
    
    The header needs to display links but only if a user is authenticated (logged in). So, we need to pass this information into the 'context' used when compiling (rendering) a view.
    
    Commonly, what you will see in controllers that return views is something like this
    
    ```javascript
      // NOTE: naming the object which contains the data collated by the controller `pageData` is a convention we use
      return h.view('info.njk', {
        pageTitle: 'Info',
        ...pageData
      })
    ```
    
    The first arg is the path to the Nunjucks template to be rendered. The second is the 'context' containing the data the template will use.
    
    We could have just done the following.
    
    ```javascript
      const pageData = { authenticated: request.auth.isAuthenticated, user: request.auth.credentials?.user }
    
      return h.view('info.njk', {
        pageTitle: 'Info',
        ...pageData
      })
    ```
    
    If we did that though we'd need to do it in _every_ controller that returns a view. A better solution is to include it in the global context which is always passed in via the engine.
    
    ```javascript
      // app/plugins/views.plugin.js
      plugin: require('@hapi/vision'),
      options: {
        engines: {
          // The 'engine' is the file extension this applies to; in this case, .njk
          njk: {
            compile: (src, options) => {
              const template = nunjucks.compile(src, options.environment)
              return context => template.render(context)
            },
      // ...
    ```
    
    Prior to this change, the context is a static object also found in `app/plugins/views.plugin.js`.
    
    ```javascript
      context: {
        appVersion: pkg.version,
        assetPath: '/assets',
        serviceName: SERVICE_NAME,
        pageTitle: `${SERVICE_NAME} - GOV.UK`
      }
    ```
    
    This change replaces that with a function that will generate the global context including auth details found in the request each time a view is rendered. Whilst we're at it we'll update the plugin to match our conventions rather than being a straight copy of the **Vision's** example.

--- app/plugins/views.plugin.js
+++ app/plugins/views.plugin.js
@@ -11,51 +11,127 @@
  */
 
 const path = require('path')
-const nunjucks = require('nunjucks')
-
-const pkg = require('../../package.json')
+const Nunjucks = require('nunjucks')
+const Vision = require('@hapi/vision')
 
 const ServerConfig = require('../../config/server.config.js')
 
-const SERVICE_NAME = 'Manage your water abstraction or impoundment licence'
-
 const ViewsPlugin = {
-  plugin: require('@hapi/vision'),
+  plugin: Vision,
   options: {
     engines: {
       // The 'engine' is the file extension this applies to; in this case, .njk
       njk: {
-        compile: (src, options) => {
-          const template = nunjucks.compile(src, options.environment)
-          return context => template.render(context)
-        },
-        prepare: (options, next) => {
-          options.compileOptions.environment = nunjucks.configure([
-            path.join(options.relativeTo || process.cwd(), options.path),
-            'node_modules/govuk-frontend/'
-          ], {
-            autoescape: true, // Automatically escape dangerous characters
-            watch: false // We reload the template each time a page is accessed so don't need to watch files for changes
-          })
-
-          return next()
-        }
+        compile,
+        prepare
       }
     },
+    context,
+    // the root file path used to resolve and load the templates identified when calling h.view()
     path: '../views',
+    // a base path used as prefix for `path:`
     relativeTo: __dirname,
-    isCached: ServerConfig.environment !== 'development', // Disable caching if we're running in dev
+    // Only enable caching of templates if we are running in production
+    isCached: ServerConfig.environment === 'production'
+  }
+}
 
-    // The context contains anything we want to pass through to our templates, eg. `assetPath` is referred to in
-    // layout.njk as the path to get static assets like client-side javascript. These are added to or overridden in the
-    // h.view() call in a controller.
-    context: {
-      appVersion: pkg.version,
-      assetPath: '/assets',
-      serviceName: SERVICE_NAME,
-      pageTitle: `${SERVICE_NAME} - GOV.UK`
+/**
+ * The rendering function for the view engine
+ *
+ * When we register the Vision plugin we are required to populate `options:` (see
+ * {@link https://hapi.dev/module/vision/api/?v=7.0.1#options options}). For each `engine:` we register (in our case
+ * just Nunjucks) we must set the `compile:` property to a function which in turn returns a function that will be called
+ * when a view is to be rendered, for example, when `h.view()` is called in a controller.
+ *
+ * We know, it's confusing! This is why we've broken it out here rather than follow the
+ * {@link https://hapi.dev/module/vision/api/?v=7.0.1#nunjucks nunjucks example} which does it all inline.
+ *
+ * We believe it's done in this way to take advantage of a
+ * {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Closures closure}. Before rendering a template
+ * Nunjucks needs to compile it. So, we can generate the compiled template using the args passed to this function.
+ *
+ * Using a closure, we can refer to the compiled template we create here in the function we're returning, even though
+ * that function will be called somewhere else entirely!
+ *
+ * Tl;DR; It the object we pass to Vision `compile:` must be a function that returns a function :-)
+ *
+ * @param {string} template The content of the template
+ * @param {Object} options Vision's `config.compileOptions` property which we assign the Nunjucks Environment instance
+ * to in `prepare()` below
+ */
+function compile (template, options) {
+  const compiledTemplate = Nunjucks.compile(template, options.environment)
+
+  return (context) => {
+    return compiledTemplate.render(context)
+  }
+}
+
+/**
+ * Build global context used with _all_ templates
+ *
+ * According to the Vision docs the global context option can be either an object or a function that takes the `request`
+ * as its only argument and returns a context object.
+ *
+ * When rendering views, the global context will be merged with any context object specified on the handler or using
+ * `h.view()`. When multiple context objects are used, values from the global context always have lowest precedence.
+ *
+ * Expanding that last point what it means is when we call `h.view('bills/view.njk', { ...myContext })` in a controller
+ * Vision will combine the 'context' (data) we pass in with the `params`, `payload`, `query` and `pre` values from the
+ * `request` plus the output of this function and pass that through to the template. Nice!
+ *
+ * > Credit to https://www.solarwinter.net/hapi-vision-and-who-am-i/ for highlighting we could do this
+ *
+ * @param {Object} request Instance of a Hapi {@link https://hapi.dev/api/?v=21.3.2#request Request}
+ *
+ * @returns {Object} the global context for all templates
+ */
+function context (request) {
+  return {
+    // `assetPath` is referred to in layout.njk as the path to get static assets like client-side javascript
+    assetPath: '/assets',
+    auth: {
+      authenticated: request.auth.isAuthenticated,
+      authorized: request.auth.isAuthorized,
+      user: request.auth.credentials?.user,
+      scope: request.auth.credentials?.scope
     }
   }
+}
+
+/**
+ * Initialises additional engine state
+ *
+ * That description and the ones for the params is taken directly from the
+ * {@link https://hapi.dev/module/vision/api/?v=7.0.1#options Vision docs}.
+ *
+ * Essentially, Vision is 'engine agnostic'. It is intended to work with lots of view engines. Some of them, like
+ * Nunjucks, require or can be configured. If `prepare:` is in the plugin options Vision will call it as part of
+ * its initialisation so you can configure your chosen view engine.
+ *
+ * @param {*} config The engine configuration object allowing updates to be made. This is useful for engines like
+ * Nunjucks that rely on additional state for rendering
+ * @param {*} next Has the signature `function(err)`
+ *
+ * @returns the result of calling `next()`
+ */
+function prepare (config, next) {
+  // Tell Nunjucks the paths to where your templates live. We _think_ Nunjucks searches in order of the paths provided.
+  // So, search our templates first before searching in the govuk-frontend package for a template.
+  const paths = [
+    path.join(config.relativeTo, config.path),
+    'node_modules/govuk-frontend/'
+  ]
+
+  // configure() returns an instance of Nunjucks Environment class (
+  // see https://mozilla.github.io/nunjucks/api.html#environment) which is the central object for handling templates.
+  // This gets assigned to Vision's compileOptions which is passed into `compile()` above as `options`.
+  const environment = Nunjucks.configure(paths)
+
+  config.compileOptions.environment = environment
+
+  return next()
 }
 
 module.exports = ViewsPlugin
