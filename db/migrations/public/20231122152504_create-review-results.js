a7f435126c08293f3b1b1322163696d7ed4dbcc6
Rebecca Ransome <beckyrose200@aol.com>
2023-11-27 16:53:00 +0000

    Adding migrations

---
+++ db/migrations/public/20231122152504_create-review-results.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const tableName = 'review_results'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .createTable(tableName, (table) => {
+      // Primary Key
+      table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+
+      // Data
+      table.uuid('bill_run_id').notNullable()
+      table.uuid('licence_id').notNullable()
+      table.uuid('charge_version_id').notNullable()
+      table.uuid('charge_reference_id').notNullable()
+      table.date('charge_period_start_date')
+      table.date('charge_period_end_date')
+      table.string('charge_version_change_reason')
+      table.uuid('review_charge_element_result_id')
+      table.uuid('review_return_result_id')
+
+      // Automatic timestamps
+      table.timestamps(false, true)
+    })
+    .then(() => {
+      knex.raw(`
+        CREATE TRIGGER update_timestamp
+        BEFORE UPDATE
+        ON ${tableName}
+        FOR EACH ROW
+        EXECUTE PROCEDURE update_timestamp();
+      `)
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropTableIfExists(tableName)
+}
