d8ad52acf67c0c329801d1818723c41568a67692
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-11-28 17:37:39 +0000

    Create `crm_v2` schema views (#556)
    https://eaflood.atlassian.net/browse/WATER-4057
    
    As part of the work we have been doing on two-part tariff we are going to be creating all our new tables in the default `public` schema.
    
    We have also decided that when there is a legacy table that we are still going to need we will create a [View](https://www.postgresql.org/docs/current/sql-createview.html) of it in the `public` schema. This allows us to correct any issues with naming conventions, strip out unused fields, and join entities that are currently sat in different schemas. The first example of this approach was done in PR #531 .
    
    This change adds the migrations needed to create views for the tables we are currently using from `crm_v2` schema.
    
    > We'll follow this up with new models and test helpers in another change. The final step is then to refactor the existing code to use the new models and delete the old legacy ones.

---
+++ db/migrations/public/20231128143653_create-companies-view.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const viewName = 'companies'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex('companies').withSchema('crm_v2').select([
+        'companies.company_id AS id',
+        'companies.name',
+        'companies.type',
+        'companies.company_number',
+        // 'companies.external_id', // is populated for companies migrated from NALD but not actually used
+        // 'companies.is_test', // we ignore this legacy test column in tables
+        'companies.organisation_type',
+        // 'companies.last_hash', // is populated but is only used by the legacy import process
+        // 'companies.current_hash', // is populated but is only used by the legacy import process
+        'companies.date_created AS created_at',
+        'companies.date_updated AS updated_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+}
