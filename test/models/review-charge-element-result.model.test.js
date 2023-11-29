2d253e84efab6834912eaa85d8a635730006e640
Rebecca Ransome <beckyrose200@aol.com>
2023-11-29 09:32:40 +0000

    Add tests for review returns and charge elements

--- test/models/review-charge-element-result.model.test.js
+++ test/models/review-charge-element-result.model.test.js
@@ -10,6 +10,8 @@ const { expect } = Code
 // Test helpers
 const DatabaseHelper = require('../support/helpers/database.helper.js')
 const ReviewChargeElementResultHelper = require('../support/helpers/review-charge-element-result.helper.js')
+const ReviewResultHelper = require('../support/helpers/review-result.helper.js')
+const ReviewResultModel = require('../../app/models/review-result.model.js')
 
 // Thing under test
 const ReviewChargeElementResultModel = require('../../app/models/review-charge-element-result.model.js')
@@ -31,6 +33,44 @@ describe('Review Charge Element Result model', () => {
 
       expect(result).to.be.an.instanceOf(ReviewChargeElementResultModel)
       expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to review results', () => {
+      let testReviewResults
+
+      beforeEach(async () => {
+        testRecord = await ReviewChargeElementResultHelper.add()
+        const { id: reviewChargeElementResultId } = testRecord
+
+        testReviewResults = []
+        for (let i = 0; i < 2; i++) {
+          const testReviewResult = await ReviewResultHelper.add({ reviewChargeElementResultId })
+          testReviewResults.push(testReviewResult)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ReviewChargeElementResultModel.query()
+          .innerJoinRelated('reviewResults')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the review results', async () => {
+        const result = await ReviewChargeElementResultModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('reviewResults')
+
+        expect(result).to.be.instanceOf(ReviewChargeElementResultModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.reviewResults).to.be.an.array()
+        expect(result.reviewResults[0]).to.be.an.instanceOf(ReviewResultModel)
+        expect(result.reviewResults).to.include(testReviewResults[0])
+        expect(result.reviewResults).to.include(testReviewResults[1])
+      })
     })
   })
 })
--- test/models/review-return-result.model.test.js
+++ test/models/review-return-result.model.test.js
@@ -9,6 +9,8 @@ const { expect } = Code
 
 // Test helpers
 const DatabaseHelper = require('../support/helpers/database.helper.js')
+const ReviewResultHelper = require('../support/helpers/review-result.helper.js')
+const ReviewResultModel = require('../../app/models/review-result.model.js')
 const ReviewReturnResultHelper = require('../support/helpers/review-return-result.helper.js')
 
 // Thing under test
@@ -31,6 +33,44 @@ describe('Review Return Result model', () => {
 
       expect(result).to.be.an.instanceOf(ReviewReturnResultModel)
       expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to review results', () => {
+      let testReviewResults
+
+      beforeEach(async () => {
+        testRecord = await ReviewReturnResultHelper.add()
+        const { id: reviewReturnResultId } = testRecord
+
+        testReviewResults = []
+        for (let i = 0; i < 2; i++) {
+          const testReviewResult = await ReviewResultHelper.add({ reviewReturnResultId })
+          testReviewResults.push(testReviewResult)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ReviewReturnResultModel.query()
+          .innerJoinRelated('reviewResults')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the review results', async () => {
+        const result = await ReviewReturnResultModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('reviewResults')
+
+        expect(result).to.be.instanceOf(ReviewReturnResultModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.reviewResults).to.be.an.array()
+        expect(result.reviewResults[0]).to.be.an.instanceOf(ReviewResultModel)
+        expect(result.reviewResults).to.include(testReviewResults[0])
+        expect(result.reviewResults).to.include(testReviewResults[1])
+      })
     })
   })
 })
