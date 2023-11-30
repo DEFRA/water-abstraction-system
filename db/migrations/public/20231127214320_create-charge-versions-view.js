87e06e72889f36aa88907cf9aa15227830ab1300
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2023-11-30 14:42:46 +0000

    Create `water` schema views (#551)
    https://eaflood.atlassian.net/browse/WATER-4188
    
    As part of the work we have been doing on two-part tariff we are going to be creating all our new tables in a single schema `public`.
    
    We have also decided that when there is a legacy table that we are still going to need for SROC. We are going to create a View of that table in the `public` schema and correct any issues with naming conventions, unused fields etc in the view. The first of these views that represents the old `returns` tables have been created in https://github.com/DEFRA/water-abstraction-system/pull/531
    
    In this PR we will build the migration scripts to create the Views we need for SROC in the `water` schema.

---
+++ db/migrations/public/20231127214320_create-charge-versions-view.js
@@ -0,0 +1,42 @@
+'use strict'
+
+const viewName = 'charge_versions'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex('charge_versions').withSchema('water').select([
+        'charge_version_id AS id',
+        'licence_ref',
+        'scheme',
+        'external_id',
+        'version_number',
+        'start_date',
+        'status',
+        'apportionment',
+        'error',
+        'end_date',
+        'billed_upto_date AS billed_up_to_date',
+        'region_code',
+        'source',
+        // 'is_test',
+        'company_id',
+        'invoice_account_id AS billing_account_id',
+        'change_reason_id',
+        'created_by',
+        'approved_by',
+        'licence_id',
+        'note_id',
+        'date_created AS created_at',
+        'date_updated AS updated_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+}
---
+++ db/migrations/public/20231127222742_create-purposes-view.js
@@ -0,0 +1,27 @@
+'use strict'
+
+const viewName = 'purposes'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex('purposes_uses').withSchema('water').select([
+        'purpose_use_id AS id',
+        'legacy_id',
+        'description',
+        'loss_factor',
+        'is_two_part_tariff AS two_part_tariff',
+        // 'is_test',
+        'date_created AS created_at',
+        'date_updated AS updated_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+}
