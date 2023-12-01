be0a462279d96bc2121731c76075c0e96344438f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-12-01 17:26:08 +0000

    Create the new models

---
+++ app/models/billing-account-address.model.js
@@ -0,0 +1,55 @@
+'use strict'
+
+/**
+ * Model for crm_v2.invoice_account_addresses
+ * @module BillingAccountAddressModel
+ */
+
+const { Model } = require('objection')
+
+const BaseModel = require('./base.model.js')
+
+class BillingAccountAddressModel extends BaseModel {
+  static get tableName () {
+    return 'billingAccountAddresses'
+  }
+
+  static get relationMappings () {
+    return {
+      address: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'address.model',
+        join: {
+          from: 'billingAccountAddresses.addressId',
+          to: 'addresses.id'
+        }
+      },
+      billingAccount: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'billing-account.model',
+        join: {
+          from: 'billingAccountAddresses.billingAccountId',
+          to: 'billingAccounts.id'
+        }
+      },
+      company: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'company.model',
+        join: {
+          from: 'billingAccountAddresses.companyId',
+          to: 'companies.id'
+        }
+      },
+      contact: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'contact.model',
+        join: {
+          from: 'billingAccountAddresses.contactId',
+          to: 'contacts.id'
+        }
+      }
+    }
+  }
+}
+
+module.exports = BillingAccountAddressModel
---
+++ app/models/company.model.js
@@ -0,0 +1,71 @@
+'use strict'
+
+/**
+ * Model for crm_v2.company
+ * @module CompanyModel
+ */
+
+const { Model } = require('objection')
+
+const BaseModel = require('./base.model.js')
+
+/**
+ * Objection model that represents a `company` in the `crm_v2.companies` table
+ *
+ * ### Notes
+ *
+ * There is no `dataSource` field in the table. But anything that has `externalId` populated can be assumed to have been
+ * imported from NALD and everything else directly entered into WRLS.
+ *
+ * For all records `name` and `type` are set
+ *
+ * When the source is 'nald'
+ *
+ * - `companyNumber` is always null
+ * - `organisationType` is always null
+ * - `lastHash` is always set
+ * - `currentHash` is always set
+ * - `externalId` is always set
+ *
+ * When the source is 'wrls'
+ *
+ * - `lastHash` is always null
+ * - `currentHash` is always null
+ * - `externalId` is always null
+ * - if `type` is 'organisation' then `companyNumber` is always set
+ * - if `type` is 'organisation' then `organisationType` can be null or set
+ * - if `type` is 'person' then `companyNumber` is always null
+ * - if `type` is 'person' then `organisationType` is always null
+ * - as of 2023-08-01 there were 28 companies with `type` set to 'organisation'
+ *
+ * We currently do not know how `organisationType` gets populated!
+ *
+ */
+class CompanyModel extends BaseModel {
+  static get tableName () {
+    return 'companies'
+  }
+
+  static get relationMappings () {
+    return {
+      billingAccountAddresses: {
+        relation: Model.HasManyRelation,
+        modelClass: 'billing-account-address.model',
+        join: {
+          from: 'companies.id',
+          to: 'billingAccountAddresses.companyId'
+        }
+      },
+      billingAccounts: {
+        relation: Model.HasManyRelation,
+        modelClass: 'billing-account.model',
+        join: {
+          from: 'companies.id',
+          to: 'billingAccounts.companyId'
+        }
+      }
+    }
+  }
+}
+
+module.exports = CompanyModel
