174a8c33c11735729e678d4ed319a48ad99b2de3
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-12-04 17:02:21 +0000

    Create `crm_v2` schema models and helpers (#561)
    https://eaflood.atlassian.net/browse/WATER-4057
    
    As part of the work we have been doing on two-part tariff we are going to be creating all our new tables in the default `public` schema.
    
    We have also decided that when there is a legacy table that we are still going to need we will create a [View](https://www.postgresql.org/docs/current/sql-createview.html) of it in the `public` schema. This allows us to correct any issues with naming conventions, strip out unused fields, and join entities that are currently sat in different schemas. The first example of this approach was done in PR #531 .
    
    This change adds the models and helpers for the views in the `crm_v2` schema that were created in PR https://github.com/DEFRA/water-abstraction-system/pull/556
    
    > The final step will then be to refactor the existing code to use the new models and delete the old legacy ones.

---
+++ app/models/address.model.js
@@ -0,0 +1,31 @@
+'use strict'
+
+/**
+ * Model for addresses (crm_v2.addresses)
+ * @module AddressModel
+ */
+
+const { Model } = require('objection')
+
+const BaseModel = require('./base.model.js')
+
+class AddressModel extends BaseModel {
+  static get tableName () {
+    return 'addresses'
+  }
+
+  static get relationMappings () {
+    return {
+      billingAccountAddresses: {
+        relation: Model.HasManyRelation,
+        modelClass: 'billing-account-address.model',
+        join: {
+          from: 'addresses.id',
+          to: 'billingAccountAddresses.addressId'
+        }
+      }
+    }
+  }
+}
+
+module.exports = AddressModel
---
+++ test/models/contact.model.test.js
@@ -0,0 +1,274 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const BillingAccountAddressHelper = require('../support/helpers/billing-account-address.helper.js')
+const BillingAccountAddressModel = require('../../app/models/billing-account-address.model.js')
+const ContactHelper = require('../support/helpers/contact.helper.js')
+const DatabaseHelper = require('../support/helpers/database.helper.js')
+
+// Thing under test
+const ContactModel = require('../../app/models/contact.model.js')
+
+describe('Contact model', () => {
+  let testRecord
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+  })
+
+  describe('Basic query', () => {
+    beforeEach(async () => {
+      testRecord = await ContactHelper.add()
+    })
+
+    it('can successfully run a basic query', async () => {
+      const result = await ContactModel.query().findById(testRecord.id)
+
+      expect(result).to.be.an.instanceOf(ContactModel)
+      expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to billing account addresses', () => {
+      let testBillingAccountAddresses
+
+      beforeEach(async () => {
+        testRecord = await ContactHelper.add()
+        const { id: contactId } = testRecord
+
+        testBillingAccountAddresses = []
+        for (let i = 0; i < 2; i++) {
+          // NOTE: A constraint in the invoice_account_addresses table means you cannot have 2 records with the same
+          // invoiceAccountId and start date
+          const startDate = i === 0 ? new Date(2023, 8, 4) : new Date(2023, 8, 3)
+          const billingAccountAddress = await BillingAccountAddressHelper.add({ startDate, contactId })
+          testBillingAccountAddresses.push(billingAccountAddress)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ContactModel.query()
+          .innerJoinRelated('billingAccountAddresses')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the billing account addresses', async () => {
+        const result = await ContactModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('billingAccountAddresses')
+
+        expect(result).to.be.instanceOf(ContactModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.billingAccountAddresses).to.be.an.array()
+        expect(result.billingAccountAddresses[0]).to.be.an.instanceOf(BillingAccountAddressModel)
+        expect(result.billingAccountAddresses).to.include(testBillingAccountAddresses[0])
+        expect(result.billingAccountAddresses).to.include(testBillingAccountAddresses[1])
+      })
+    })
+  })
+
+  // NOTE: The test data we generate in these tests is in accordance with how a contact record could be populated. For
+  // example, if the data source is 'wrls' and the contact type is 'person', then first name and last name is always
+  // populated. We don't test what would happen if last name wasn't because analysis of the data confirms this would
+  // never happen. See the method's documentation for more details.
+  describe('$name', () => {
+    let contactRecord
+
+    beforeEach(() => {
+      contactRecord = {
+        contactType: null,
+        dataSource: null,
+        department: null,
+        firstName: null,
+        initials: null,
+        lastName: null,
+        middleInitials: null,
+        salutation: null,
+        suffix: null
+      }
+    })
+
+    describe('when the contact was imported in NALD', () => {
+      beforeEach(() => {
+        contactRecord.dataSource = 'nald'
+      })
+
+      describe("and has a last name of 'Villar'", () => {
+        beforeEach(() => {
+          contactRecord.lastName = 'Villar'
+        })
+
+        it("returns 'Villar'", () => {
+          testRecord = ContactModel.fromJson(contactRecord)
+
+          expect(testRecord.$name()).to.equal('Villar')
+        })
+
+        describe("and the salutation 'Mrs'", () => {
+          beforeEach(() => {
+            contactRecord.salutation = 'Mrs'
+          })
+
+          it("returns 'Mrs Villar'", () => {
+            testRecord = ContactModel.fromJson(contactRecord)
+
+            expect(testRecord.$name()).to.equal('Mrs Villar')
+          })
+
+          describe("and the initial 'J'", () => {
+            beforeEach(() => {
+              contactRecord.initials = 'J'
+            })
+
+            it("returns 'Mrs J Villar'", () => {
+              testRecord = ContactModel.fromJson(contactRecord)
+
+              expect(testRecord.$name()).to.equal('Mrs J Villar')
+            })
+          })
+        })
+
+        describe("and a first name of 'Margherita'", () => {
+          beforeEach(() => {
+            contactRecord.firstName = 'Margherita'
+          })
+
+          it("returns 'Margherita Villar'", () => {
+            testRecord = ContactModel.fromJson(contactRecord)
+
+            expect(testRecord.$name()).to.equal('Margherita Villar')
+          })
+
+          describe("and the salutation 'Mrs'", () => {
+            beforeEach(() => {
+              contactRecord.salutation = 'Mrs'
+            })
+
+            it("returns 'Mrs Margherita Villar'", () => {
+              testRecord = ContactModel.fromJson(contactRecord)
+
+              expect(testRecord.$name()).to.equal('Mrs Margherita Villar')
+            })
+
+            describe("and the initial 'J'", () => {
+              beforeEach(() => {
+                contactRecord.initials = 'J'
+              })
+
+              it("returns 'Mrs J Villar'", () => {
+                testRecord = ContactModel.fromJson(contactRecord)
+
+                expect(testRecord.$name()).to.equal('Mrs J Villar')
+              })
+            })
+          })
+        })
+      })
+    })
+
+    describe('when the contact was entered into WRLS', () => {
+      beforeEach(() => {
+        contactRecord.dataSource = 'wrls'
+      })
+
+      describe("and it is a department named 'Humanoid Risk Assessment'", () => {
+        beforeEach(() => {
+          contactRecord.contactType = 'department'
+          contactRecord.department = 'Humanoid Risk Assessment'
+        })
+
+        it("returns 'Humanoid Risk Assessment'", () => {
+          testRecord = ContactModel.fromJson(contactRecord)
+
+          expect(testRecord.$name()).to.equal('Humanoid Risk Assessment')
+        })
+      })
+
+      describe("and it is a 'person'", () => {
+        beforeEach(() => {
+          contactRecord.contactType = 'person'
+          contactRecord.firstName = 'Vincent'
+          contactRecord.lastName = 'Anderson'
+        })
+
+        describe("with the name 'Vincent Anderson'", () => {
+          it("returns 'Vincent Anderson'", () => {
+            testRecord = ContactModel.fromJson(contactRecord)
+
+            expect(testRecord.$name()).to.equal('Vincent Anderson')
+          })
+        })
+
+        describe("and the salutation 'Mr'", () => {
+          beforeEach(() => {
+            contactRecord.salutation = 'Mr'
+          })
+
+          it("returns 'Mr Vincent Anderson'", () => {
+            testRecord = ContactModel.fromJson(contactRecord)
+
+            expect(testRecord.$name()).to.equal('Mr Vincent Anderson')
+          })
+
+          describe("and the middle initial 'P'", () => {
+            beforeEach(() => {
+              contactRecord.middleInitials = 'P'
+            })
+
+            it("returns 'Mr V P Anderson'", () => {
+              testRecord = ContactModel.fromJson(contactRecord)
+
+              expect(testRecord.$name()).to.equal('Mr V P Anderson')
+            })
+
+            describe("and the suffix 'MBE'", () => {
+              beforeEach(() => {
+                contactRecord.suffix = 'MBE'
+              })
+
+              it("returns 'Mr V P Anderson MBE'", () => {
+                testRecord = ContactModel.fromJson(contactRecord)
+
+                expect(testRecord.$name()).to.equal('Mr V P Anderson MBE')
+              })
+            })
+          })
+        })
+
+        describe("and the middle initial 'P'", () => {
+          beforeEach(() => {
+            contactRecord.middleInitials = 'P'
+          })
+
+          it("returns 'V P Anderson'", () => {
+            testRecord = ContactModel.fromJson(contactRecord)
+
+            expect(testRecord.$name()).to.equal('V P Anderson')
+          })
+
+          describe("and the suffix 'MBE'", () => {
+            beforeEach(() => {
+              contactRecord.suffix = 'MBE'
+            })
+
+            it("returns 'V P Anderson MBE'", () => {
+              testRecord = ContactModel.fromJson(contactRecord)
+
+              expect(testRecord.$name()).to.equal('V P Anderson MBE')
+            })
+          })
+        })
+      })
+    })
+  })
+})
