f89e918d6365bce43f3363eacec128f531898de8
Jason Claxton <jason.claxton@defra.gov.uk>
2023-12-05 17:11:14 +0000

    Update `fetch-return-logs...` service to use views

--- app/services/bill-runs/two-part-tariff/fetch-return-logs-for-licence.service.js
+++ app/services/bill-runs/two-part-tariff/fetch-return-logs-for-licence.service.js
@@ -1,30 +1,31 @@
 'use strict'
 
 /**
- * Fetches SROC Returns linked to licences flagged for inclusion in next SROC 2PT billing
- * @module FetchReturnsForLicenceService
+ * Fetches SROC return logs linked to licences flagged for inclusion in next SROC 2PT billing
+ * @module FetchReturnLogsForLicenceService
  */
 
 const { ref } = require('objection')
 
-const ReturnModel = require('../../../models/returns/return.model')
+const ReturnLogModel = require('../../../models/return-log.model.js')
 
 /**
- * Fetch all SROC returns to be processed as part of two-part-tariff billing
+ * Fetch all SROC return logs to be processed as part of two-part-tariff billing
  * *
  * @param {String} licenceRef The reference of the licence that the return relates to
  * @param {Object} billingPeriod Object with a `startDate` and `endDate` property representing the period being billed
  *
- * @returns {Object} Contains an array of Returns and the associated current Version, and Lines if they exist
+ * @returns {Object} Contains an array of `returnLogs` and the associated current `returnSubmissions`, and
+ * `returnSubmissionLines` if they exist
  */
 async function go (licenceRef, billingPeriod) {
   return _fetch(licenceRef, billingPeriod)
 }
 
 async function _fetch (licenceRef, billingPeriod) {
-  const returns = await ReturnModel.query()
+  const returnLogs = await ReturnLogModel.query()
     .select([
-      'returnId',
+      'id',
       'returnRequirement',
       ref('metadata:description').castText().as('description'),
       'startDate',
@@ -47,30 +48,30 @@ async function _fetch (licenceRef, billingPeriod) {
     .whereJsonPath('metadata', '$.isTwoPartTariff', '=', true)
     .orderBy('startDate', 'ASC')
     .orderBy('returnRequirement', 'ASC')
-    .withGraphFetched('versions')
-    .modifyGraph('versions', builder => {
+    .withGraphFetched('returnSubmissions')
+    .modifyGraph('returnSubmissions', builder => {
       builder
         .select([
-          'versionId',
+          'id',
           'nilReturn'
         ])
-        .where('versions.current', true)
+        .where('returnSubmissions.current', true)
     })
-    .withGraphFetched('versions.lines')
-    .modifyGraph('versions.lines', builder => {
+    .withGraphFetched('returnSubmissions.returnSubmissionLines')
+    .modifyGraph('returnSubmissions.returnSubmissionLines', builder => {
       builder
         .select([
-          'lineId',
+          'id',
           'startDate',
           'endDate',
           'quantity'
         ])
-        .where('lines.quantity', '>', 0)
-        .where('lines.startDate', '<=', billingPeriod.endDate)
-        .where('lines.endDate', '>=', billingPeriod.startDate)
+        .where('returnSubmissionLines.quantity', '>', 0)
+        .where('returnSubmissionLines.startDate', '<=', billingPeriod.endDate)
+        .where('returnSubmissionLines.endDate', '>=', billingPeriod.startDate)
     })
 
-  return returns
+  return returnLogs
 }
 
 module.exports = {
