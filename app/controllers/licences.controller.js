1bae7dd9a46da181e9a4e2ce6dbf95c89f153f4c
Robert Parkinson <robert.parkinson@defra.gov.uk>
2023-12-11 14:16:01 +0000

    add the route and page for the select start date page

---
+++ app/controllers/licences.controller.js
@@ -0,0 +1,19 @@
+'use strict'
+
+async function noReturnsRequired (_request, h) {
+  return h.response().code(200)
+}
+
+async function selectReturnStartDate (request, h) {
+  const { id } = request.params
+
+  return h.view('return-requirements/select-return-start-date.njk', {
+    activeNavBar: 'search',
+    licenceId: id
+  })
+}
+
+module.exports = {
+  noReturnsRequired,
+  selectReturnStartDate
+}
