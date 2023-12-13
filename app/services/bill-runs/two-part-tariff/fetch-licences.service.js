29a780652a94e3bca489ff2e77d74e551bc7e971
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2023-12-13 11:26:03 +0000

    Create `fetch-licences` service (#577)
    https://eaflood.atlassian.net/browse/WATER-4188
    
    As part of the work we have been doing, we've been hacking away at a branch in a mob to build the two-part-tariff service. As a result of this, we have some refactoring to do to tidy up the work we have done in our mob. We are keeping track of the refactoring to do in this issue https://github.com/DEFRA/water-abstraction-team/issues/105.
    
    This PR will extract the `fetch-licences` service out of our hacky branch https://github.com/DEFRA/water-abstraction-system/pull/458 and into its own PR along with the unit tests.

---
+++ app/services/bill-runs/two-part-tariff/fetch-licences.service.js
@@ -0,0 +1,66 @@
+'use strict'
+
+/**
+ * Fetches 2PT Licences for the matching region and billing period
+ * @module FetchLicencesService
+ */
+
+const FetchChargeVersionsService = require('./fetch-charge-versions.service.js')
+
+/**
+ * Fetches 2PT Licences for the matching region and billing period plus the charge versions associated with them grouped
+ * by licence
+ *
+ * @param {String} regionId UUID of the region being billed that the licences must be linked to
+ * @param {Object} billingPeriod Object with a `startDate` and `endDate` property representing the period being billed
+ *
+ * @returns {Object[]} the licences to be matched, each containing an array of charge versions applicable for two-part tariff
+ */
+async function go (regionId, billingPeriod) {
+  const chargeVersions = await FetchChargeVersionsService.go(regionId, billingPeriod)
+
+  const uniqueLicenceIds = _extractUniqueLicenceIds(chargeVersions)
+
+  return _groupByLicence(chargeVersions, uniqueLicenceIds)
+}
+
+function _extractUniqueLicenceIds (chargeVersions) {
+  const allLicenceIds = chargeVersions.map((chargeVersion) => {
+    return chargeVersion.licence.id
+  })
+
+  return [...new Set(allLicenceIds)]
+}
+
+function _groupByLicence (chargeVersions, uniqueLicenceIds) {
+  // NOTE: We could have initialized licences as an empty array and pushed each new object. But for a big region
+  // the number of licences we might be dealing will be in the hundreds, possibly thousands. In these cases we get a
+  // performance bump if we create the array sized to our needs first, rather than asking Node to resize the array on
+  // each loop. Only applicable here though! Don't go doing this for every new array you declare ;-)
+  const licences = Array(uniqueLicenceIds.length).fill(undefined)
+
+  for (let i = 0; i < uniqueLicenceIds.length; i++) {
+    const licenceId = uniqueLicenceIds[i]
+    const matchedChargeVersions = chargeVersions.filter((chargeVersion) => {
+      return chargeVersion.licence.id === licenceId
+    })
+
+    const { licenceRef, startDate, expiredDate, lapsedDate, revokedDate } = matchedChargeVersions[0].licence
+
+    licences[i] = {
+      id: licenceId,
+      licenceRef,
+      startDate,
+      expiredDate,
+      lapsedDate,
+      revokedDate,
+      chargeVersions: matchedChargeVersions
+    }
+  }
+
+  return licences
+}
+
+module.exports = {
+  go
+}
