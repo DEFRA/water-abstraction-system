c810d7b80f9c9a33597ef01d5ac6873a698df69f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-12-15 15:24:14 +0000

    Update 2PT check endpoint with alternate changes (#582)
    https://eaflood.atlassian.net/browse/WATER-4057
    
    Way, way back we built our very first iteration of a two-part tariff 'check' endpoint. The intent was to have some way for our QA team to begin verifying how we were matching and allocating returns correctly. Along the journey, we realised we needed to make a major shift in how things were working which has since become known as the [alternate engine](https://github.com/DEFRA/water-abstraction-system/pull/458).
    
    This started as a 'spike' (does _this_ look right?), then was deployed on the branch to environments for double checking, was tweaked, tweaked a bit more, tweaked again, new requests catered for etc. _Now_ we're trying to figure out how we migrate from our hack to 'production-ready' unit-tested code, still in the same branch.
    
    Recently, we lost the original alternate engine changes we made to the 'check' endpoint on that branch when we merged in `main`. That became the trigger to move out of the [alternate-2pt-engine](https://github.com/DEFRA/water-abstraction-system/tree/alternate-2pt-engine) branch those changes which are just related to the check endpoint and what the QA team need to keep an eye on things.
    
    This should mean we won't lose those changes again. Also, the team focused on refactoring can differentiate their changes from stuff we've been doing to keep the QA's happy! 😁
    
    So, this change updates the 'check' endpoint to be the version we've been using and testing against. This includes what was needed to get it to work with our recent model changes.
    
    > When two-part tariff is completed, hopefully soon, we will delete all of this! 😈

---
+++ app/services/check/scenario-formatter.service.js
@@ -0,0 +1,245 @@
+'use strict'
+
+/**
+ * Formats the result of TwoPartService to match the scenario spreadsheet as closely as possible
+ * @module AllocateReturnsService
+ */
+
+const { formatAbstractionDate } = require('../../presenters/base.presenter.js')
+
+function go (licence, licenceIndex) {
+  const {
+    id,
+    licenceRef: reference,
+    startDate,
+    expiredDate,
+    lapsedDate,
+    revokedDate,
+    issue,
+    status,
+    chargeVersions,
+    returnLogs
+  } = licence
+
+  const simpleId = `L${licenceIndex + 1}`
+
+  const formattedReturns = _formatReturnLogs(returnLogs)
+
+  return {
+    simpleId,
+    id,
+    reference,
+    starts: startDate.toLocaleDateString('en-GB'),
+    expires: expiredDate ? expiredDate.toLocaleDateString('en-GB') : '',
+    lapses: lapsedDate ? lapsedDate.toLocaleDateString('en-GB') : '',
+    revokes: revokedDate ? revokedDate.toLocaleDateString('en-GB') : '',
+    status,
+    issue,
+    chargeVersions: _formatChargeVersions(chargeVersions, formattedReturns, simpleId),
+    returns: formattedReturns
+  }
+}
+
+function _formatAbstractionPeriods (abstractionPeriods) {
+  if (!abstractionPeriods) {
+    return ''
+  }
+
+  return abstractionPeriods.map((period) => {
+    const { startDate, endDate } = period
+
+    return `${startDate.toLocaleDateString('en-GB')} - ${endDate.toLocaleDateString('en-GB')}`
+  })
+}
+
+function _formatChargeElements (chargeElements, formattedReturns, chargeReferenceSimpleId) {
+  return chargeElements.map((chargeElement, index) => {
+    const {
+      id,
+      purpose,
+      authorisedAnnualQuantity: authorised,
+      abstractionPeriodStartDay,
+      abstractionPeriodStartMonth,
+      abstractionPeriodEndDay,
+      abstractionPeriodEndMonth,
+      abstractionPeriods,
+      allocatedQuantity: allocated,
+      status,
+      issues,
+      returnLogs: matchedReturns
+    } = chargeElement
+
+    const simpleId = `E${index + 1}-${chargeReferenceSimpleId}`
+    const formattedReturnLogs = _formatChargeElementMatchedReturns(matchedReturns, formattedReturns)
+
+    return {
+      simpleId,
+      id,
+      purpose: `${purpose.description} | ${purpose.legacyId}`,
+      authorised,
+      absStart: formatAbstractionDate(abstractionPeriodStartDay, abstractionPeriodStartMonth),
+      absEnd: formatAbstractionDate(abstractionPeriodEndDay, abstractionPeriodEndMonth),
+      absPeriods: _formatAbstractionPeriods(abstractionPeriods),
+      allocated,
+      status,
+      issues: issues.join(', '),
+      returns: formattedReturnLogs
+    }
+  })
+}
+
+function _formatChargeElementMatchedReturns (matchedReturns, formattedReturns) {
+  return matchedReturns.map((matchedReturn) => {
+    const {
+      id,
+      allocatedQuantity,
+      lines
+    } = matchedReturn
+
+    const matchingReturn = formattedReturns.find((formattedReturn) => formattedReturn.id === matchedReturn.id)
+    lines.forEach((line) => {
+      line.matchingLine = matchingReturn.lines.find((matchingReturnLine) => {
+        return matchingReturnLine.id === line.id
+      })
+    })
+
+    const formattedLines = lines.map((line) => {
+      return {
+        simpleId: line.matchingLine.simpleId,
+        id: line.id,
+        allocated: line.allocated
+      }
+    })
+
+    return {
+      simpleId: matchingReturn.simpleId,
+      id,
+      allocatedQuantity,
+      lines: formattedLines
+    }
+  })
+}
+
+function _formatChargeReferences (chargeReferences, formattedReturns, chargeVersionSimpleId) {
+  return chargeReferences.map((chargeReference, index) => {
+    const {
+      id,
+      chargeCategory,
+      volume: authorised,
+      aggregate,
+      chargeElements
+    } = chargeReference
+
+    const simpleId = `R${index + 1}-${chargeVersionSimpleId}`
+
+    return {
+      simpleId,
+      id,
+      category: chargeCategory.reference,
+      subsistence: chargeCategory.subsistenceCharge,
+      authorised,
+      aggregate: aggregate ?? 1,
+      chargeElements: _formatChargeElements(chargeElements, formattedReturns, simpleId)
+    }
+  })
+}
+
+function _formatChargeVersions (chargeVersions, formattedReturns, licenceSimpleId) {
+  return chargeVersions.map((chargeVersion, index) => {
+    const {
+      id,
+      startDate,
+      endDate,
+      chargePeriod,
+      chargeReferences
+    } = chargeVersion
+
+    const simpleId = `V${index + 1}-${licenceSimpleId}`
+    const chargePeriodStartDate = chargePeriod.startDate ? chargePeriod.startDate.toLocaleDateString('en-GB') : ''
+    const chargePeriodEndDate = chargePeriod.endDate ? chargePeriod.endDate.toLocaleDateString('en-GB') : ''
+
+    return {
+      simpleId,
+      id,
+      starts: startDate.toLocaleDateString('en-GB'),
+      ends: endDate ? endDate.toLocaleDateString('en-GB') : '',
+      chargePeriod: `${chargePeriodStartDate} - ${chargePeriodEndDate}`,
+      chargeReferences: _formatChargeReferences(chargeReferences, formattedReturns, simpleId)
+    }
+  })
+}
+
+function _formatReturnLogs (returnLogs) {
+  return returnLogs.map((returnLog, index) => {
+    const {
+      id,
+      returnRequirement: requirement,
+      description,
+      startDate,
+      endDate,
+      dueDate,
+      receivedDate,
+      status,
+      underQuery,
+      nilReturn,
+      purposes,
+      periodStartDay,
+      periodStartMonth,
+      periodEndDay,
+      periodEndMonth,
+      abstractionPeriods,
+      quantity,
+      allocatedQuantity: allocated,
+      abstractionOutsidePeriod,
+      issues,
+      returnSubmissions,
+      chargeElements
+    } = returnLog
+
+    const simpleId = `T${index + 1}`
+
+    const formattedPurposes = purposes.map((purpose) => {
+      return `${purpose.tertiary.description} | ${purpose.tertiary.code}`
+    })
+
+    const formattedLines = returnSubmissions[0]?.returnSubmissionLines?.map((line, index) => {
+      const { id, startDate, endDate, quantity, unallocated } = line
+
+      return {
+        simpleId: `L${index + 1}-${simpleId}`,
+        id,
+        period: `${startDate.toLocaleDateString('en-GB')} - ${endDate.toLocaleDateString('en-GB')}`,
+        quantity,
+        unallocated
+      }
+    })
+
+    return {
+      simpleId,
+      id,
+      requirement,
+      status,
+      description,
+      starts: startDate.toLocaleDateString('en-GB'),
+      ends: endDate.toLocaleDateString('en-GB'),
+      due: dueDate.toLocaleDateString('en-GB'),
+      received: receivedDate ? receivedDate.toLocaleDateString('en-GB') : '',
+      underQuery,
+      nilReturn,
+      purpose: formattedPurposes,
+      absStart: formatAbstractionDate(periodStartDay, periodStartMonth),
+      absEnd: formatAbstractionDate(periodEndDay, periodEndMonth),
+      absPeriods: _formatAbstractionPeriods(abstractionPeriods),
+      quantity,
+      allocated,
+      abstractionOutsidePeriod,
+      issues: issues.join(', '),
+      lines: formattedLines,
+      chargeElements
+    }
+  })
+}
+
+module.exports = {
+  go
+}
