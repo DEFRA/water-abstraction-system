30714758ecafafc52ec88636ac9840c96b9f7988
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2023-12-16 10:05:49 +0000

    Add new view bill run service (#543)
    https://eaflood.atlassian.net/browse/WATER-4223
    
    This service has been added to orchestrate the fetching and formatting of the data needed to view a bill run. It has been added to support a new bill run page that will replace the existing one in the legacy service.

---
+++ app/services/bill-runs/view-bill-run.service.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data needed for the bill run page
+ * @module ViewBillService
+ */
+
+const ViewBillRunPresenter = require('../../presenters/bill-runs/view-bill-run.presenter.js')
+const ViewBillSummariesPresenter = require('../../presenters/bill-runs/view-bill-summaries.presenter.js')
+const FetchBillRunService = require('./fetch-bill-run.service.js')
+
+/**
+ * Orchestrates fetching and presenting the data needed for the bill run page
+ *
+ * @param {string} id The UUID for the bill run to view
+ *
+ * @returns {Object} an object representing the `pageData` needed by the view bill run template. It contains details of
+ * the bill run and the bills linked to it plus the page title.
+ */
+async function go (id) {
+  const result = await FetchBillRunService.go(id)
+
+  const billRun = ViewBillRunPresenter.go(result.billRun)
+  const billGroups = ViewBillSummariesPresenter.go(result.billSummaries)
+
+  return {
+    ...billRun,
+    billGroupsCount: billGroups.length,
+    billGroups
+  }
+}
+
+module.exports = {
+  go
+}
