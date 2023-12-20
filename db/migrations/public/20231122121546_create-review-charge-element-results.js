a7fc071235aeea012d7bd4032b59bcd2ee6bcfed
Jason Claxton <jason.claxton@defra.gov.uk>
2023-12-20 11:56:17 +0000

    Update migration to increase decimal places

--- db/migrations/public/20231122121546_create-review-charge-element-results.js
+++ db/migrations/public/20231122121546_create-review-charge-element-results.js
@@ -11,8 +11,8 @@ exports.up = function (knex) {
 
       // Data
       table.uuid('charge_element_id').notNullable()
-      table.decimal('allocated').defaultTo(0)
-      table.decimal('aggregate').defaultTo(1)
+      table.decimal('allocated', null, null).defaultTo(0)
+      table.decimal('aggregate', null, null).defaultTo(1)
       table.boolean('charge_dates_overlap').defaultTo(false)
 
       // Automatic timestamps
