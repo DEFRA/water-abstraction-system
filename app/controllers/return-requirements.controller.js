08908558aaecf2aa69ebc9b6bd3183eb0b70edc8
Fazal Khan <fazal.khan@defra.gov.uk>
2024-01-03 11:16:54 +0000

    fix: removed duplicate controller

--- app/controllers/return-requirements.controller.js
+++ app/controllers/return-requirements.controller.js
@@ -217,17 +217,6 @@ async function returnsSettings (request, h) {
   })
 }
 
-async function selectPurpose (request, h) {
-  const { sessionId } = request.params
-
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/purpose.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
-}
-
 async function abstractionPeriod (request, h) {
   const { sessionId } = request.params
 
