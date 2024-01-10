9a7fb37ddd235845ca6d7ccb7d016bc1d9df929e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-10 13:01:01 +0000

    Order tests by route alphabetically

--- test/controllers/return-requirements.controller.test.js
+++ test/controllers/return-requirements.controller.test.js
@@ -33,35 +33,90 @@ describe('Return requirements controller', () => {
     Sinon.restore()
   })
 
-  describe('GET /return-requirements/{sessionId}/start-date', () => {
+  describe('GET /return-requirements/{sessionId}/abstraction-period', () => {
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
-        const response = await server.inject(_options('start-date'))
+        const response = await server.inject(_options('abstraction-period'))
 
         expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select the start date for the return requirement')
+        expect(response.payload).to.contain('Enter the abstraction period for the return requirement')
       })
     })
   })
 
-  describe('GET /return-requirements/{sessionId}/reason', () => {
+  describe('GET /return-requirements/{sessionId}/add-note', () => {
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
-        const response = await server.inject(_options('reason'))
+        const response = await server.inject(_options('add-note'))
 
         expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select the reason for the return requirement')
+        expect(response.payload).to.contain('Add a note')
       })
     })
   })
 
-  describe('GET /return-requirements/{sessionId}/setup', () => {
+  describe('GET /return-requirements/{sessionId}/agreements-exceptions', () => {
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
-        const response = await server.inject(_options('setup'))
+        const response = await server.inject(_options('agreements-exceptions'))
 
         expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('How do you want to set up the return requirement?')
+        expect(response.payload).to.contain('Select agreements and exceptions for the return requirement')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{licenceId}/approved', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('approved'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Returns requirements approved')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/check-your-answers', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('check-your-answers'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Check your answers')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/frequency-collected', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('frequency-collected'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Select how often readings or volumes are collected')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/frequency-reported', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('frequency-reported'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Select how often collected readings or volumes are reported')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/no-returns-check-your-answers', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('no-returns-check-your-answers'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Check your answers')
       })
     })
   })
@@ -81,72 +136,6 @@ describe('Return requirements controller', () => {
     })
   })
 
-  describe('GET /return-requirements/{licenceId}/approved', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('approved'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Returns requirements approved')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/no-returns-check-your-answers', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('no-returns-check-your-answers'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Check your answers')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/check-your-answers', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('check-your-answers'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Check your answers')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/add-note', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('add-note'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Add a note')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/frequency-collected', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('frequency-collected'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select how often readings or volumes are collected')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/site-description', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('site-description'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Enter a site description for the return requirement')
-      })
-    })
-  })
-
   describe('GET /return-requirements/{sessionId}/points', () => {
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
@@ -154,39 +143,6 @@ describe('Return requirements controller', () => {
 
         expect(response.statusCode).to.equal(200)
         expect(response.payload).to.contain('Select the points for the return requirement')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/returns-cycle', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('returns-cycle'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select the returns cycle for the return requirement')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/frequency-reported', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('frequency-reported'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select how often collected readings or volumes are reported')
-      })
-    })
-  })
-
-  describe('GET /return-requirements/{sessionId}/agreements-exceptions', () => {
-    describe('when the request succeeds', () => {
-      it('returns the page successfully', async () => {
-        const response = await server.inject(_options('agreements-exceptions'))
-
-        expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select agreements and exceptions for the return requirement')
       })
     })
   })
@@ -202,13 +158,57 @@ describe('Return requirements controller', () => {
     })
   })
 
-  describe('GET /return-requirements/{sessionId}/abstraction-period', () => {
+  describe('GET /return-requirements/{sessionId}/reason', () => {
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
-        const response = await server.inject(_options('abstraction-period'))
+        const response = await server.inject(_options('reason'))
 
         expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Enter the abstraction period for the return requirement')
+        expect(response.payload).to.contain('Select the reason for the return requirement')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/returns-cycle', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('returns-cycle'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Select the returns cycle for the return requirement')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/setup', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('setup'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('How do you want to set up the return requirement?')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/site-description', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('site-description'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Enter a site description for the return requirement')
+      })
+    })
+  })
+
+  describe('GET /return-requirements/{sessionId}/start-date', () => {
+    describe('when the request succeeds', () => {
+      it('returns the page successfully', async () => {
+        const response = await server.inject(_options('start-date'))
+
+        expect(response.statusCode).to.equal(200)
+        expect(response.payload).to.contain('Select the start date for the return requirement')
       })
     })
   })
