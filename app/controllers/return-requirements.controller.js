84134349cc7ed5139a1a893094f5da0d959559b8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-10 13:52:55 +0000

    Update all submit handlers to redirect
    Also, as best as we can now ensure the redirects are going to the right places. We also checked the back links to make sure they were going where expected.
    
    There are still things to be sorted out
    
    - only one start page but 2 journeys
    - 2 check your answer pages when we should be able to work with one (would fix `/add-note` journey!)

--- app/controllers/return-requirements.controller.js
+++ app/controllers/return-requirements.controller.js
@@ -185,63 +185,43 @@ async function startDate (request, h) {
 async function submitAbstractionPeriod (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/abstraction-period.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/returns-cycle`)
 }
 
 async function submitAddNote (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  const { id } = session
-
-  return h.redirect(`/system/return-requirements/${id}/check-your-answers`)
+  return h.redirect(`/system/return-requirements/${sessionId}/check-your-answers`)
 }
 
 async function submitAgreementsExceptions (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/agreements-exceptions.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/check-your-answers`)
 }
 
 async function submitCheckYourAnswers (request, h) {
-  return h.redirect('/system/return-requirements/approved')
+  const { licenceId } = request.params
+
+  return h.redirect(`/system/return-requirements/${licenceId}/approved`)
 }
 
 async function submitFrequencyCollected (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/frequency-collected.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/frequency-reported`)
 }
 
 async function submitFrequencyReported (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/frequency-reported.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/agreements-exceptions`)
 }
 
 async function submitNoReturnsCheckYourAnswers (request, h) {
-  return h.redirect('/system/return-requirements/approved')
+  const { licenceId } = request.params
+
+  return h.redirect(`/system/return-requirements/${licenceId}/approved`)
 }
 
 async function submitNoReturnsRequired (request, h) {
@@ -253,75 +233,49 @@ async function submitNoReturnsRequired (request, h) {
     return h.view('return-requirements/no-returns-required.njk', pageData)
   }
 
-  return h.redirect(`/system/return-requirements/${sessionId}/check-your-answers`)
+  return h.redirect(`/system/return-requirements/${sessionId}/no-returns-check-your-answers`)
 }
 
 async function submitPoints (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/points.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/abstraction-period`)
 }
 
 async function submitPurpose (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/purpose.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/points`)
 }
 
 async function submitReason (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.redirect(`/system/return-requirements/${session.id}/setup`)
+  return h.redirect(`/system/return-requirements/${sessionId}/setup`)
 }
 
 async function submitReturnsCycle (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/returns-cycle.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/site-description`)
 }
 
 async function submitSetup (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.redirect(`/system/return-requirements/${session.id}/check-your-answers`)
+  return h.redirect(`/system/return-requirements/${sessionId}/purpose`)
 }
 
 async function submitSiteDescription (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.view('return-requirements/site-description.njk', {
-    activeNavBar: 'search',
-    ...session
-  })
+  return h.redirect(`/system/return-requirements/${sessionId}/frequency-collected`)
 }
 
 async function submitStartDate (request, h) {
   const { sessionId } = request.params
 
-  const session = await SessionModel.query().findById(sessionId)
-
-  return h.redirect(`/system/return-requirements/${session.id}/reason`)
+  return h.redirect(`/system/return-requirements/${sessionId}/no-returns-required`)
 }
 
 module.exports = {
