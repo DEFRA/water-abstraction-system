c4f9e143d4b17a762ab71a3a60e36ada6b576346
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-11 09:06:45 +0000

    Create LicenceVersion view migration

---
+++ db/migrations/public/20240111085846_create-licence-versions-view.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const viewName = 'licence_versions'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex('licence_versions').withSchema('water').select([
+        'licence_version_id AS id',
+        'licence_id',
+        'issue',
+        'increment',
+        'status',
+        'start_date',
+        'end_date',
+        'external_id',
+        // 'is_test',
+        'date_created AS created_at',
+        'date_updated AS updated_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+}
