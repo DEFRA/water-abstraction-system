18339e8b541598bb669a11dd974c8246a2d9a50c
Rebecca Ransome <beckyrose200@aol.com>
2024-01-12 09:19:23 +0000

    Make comments for descriptive

--- app/services/bill-runs/two-part-tariff/prepare-charge-version.service.js
+++ app/services/bill-runs/two-part-tariff/prepare-charge-version.service.js
@@ -1,16 +1,29 @@
 'use strict'
 
 /**
- * Stuff
+ * Prepares the charge version for matching
  * @module PrepareChargeVersionService
  */
 const DetermineAbstractionPeriodService = require('../determine-abstraction-periods.service.js')
 const DetermineChargePeriodService = require('../determine-charge-period.service.js')
 
 /**
+ * Prepares the charge version for matching with its returns
  *
- * @param {*} licence
- * @param {*} billingPeriod
+ * This function performs the following steps:
+ *
+ * Sorts all charge references belonging to the charge version by subsistence charge in descending order. This ensures
+ * that allocation is performed with the highest subsistence charge first.
+ *
+ * Determines the charge period for the charge version using an external service and sets it as a property on the charge
+ * version object.
+ *
+ * Loops through each charge reference's elements, preparing them for matching. This involves determining the
+ * abstraction period for each element and adding properties such as returnLogs, allocatedQuantity, and
+ * abstractionPeriods.
+ *
+ * @param {module:ChargeVersionModel} chargeVersion - The charge version to prepare
+ * @param {Object} billingPeriod Object with a `startDate` and `endDate` property representing the period being billed
  */
 function go (chargeVersion, billingPeriod) {
   const { chargeReferences } = chargeVersion
