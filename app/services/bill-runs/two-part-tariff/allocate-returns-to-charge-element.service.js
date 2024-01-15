e42f77b4cf2e6ec6a97d5f6e7b40da527c97b3de
Rebecca Ransome <beckyrose200@aol.com>
2024-01-15 16:16:47 +0000

    Hack

---
+++ app/services/bill-runs/two-part-tariff/allocate-returns-to-charge-element.service.js
@@ -0,0 +1,12 @@
+/**
+ * Match and allocate licences to returns for a two-part tariff bill run for the given billing periods
+ * @module AllocateReturnsToChargeElementService
+ */
+
+function go () {
+
+}
+
+module.exports = {
+  go
+}
