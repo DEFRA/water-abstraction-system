48a974b60864a6aa1f6fb948f06dd38a6cce1f69
Jason Claxton <jason.claxton@defra.gov.uk>
2024-01-16 17:06:21 +0000

    Refactor to make `if` less complex

--- app/services/bill-runs/two-part-tariff/allocate-returns-to-charge-element.service.js
+++ app/services/bill-runs/two-part-tariff/allocate-returns-to-charge-element.service.js
@@ -18,55 +18,50 @@ const { periodsOverlap } = require('../../../lib/general.lib.js')
  * @param {module:ChargeReferenceModel} chargeReference - The charge reference the element belongs to
  */
 function go (chargeElement, matchingReturns, chargePeriod, chargeReference) {
-  _allocateReturns(chargeElement, matchingReturns, chargePeriod, chargeReference)
-}
-
-function _allocateReturns (chargeElement, matchingReturns, chargePeriod, chargeReference) {
   matchingReturns.forEach((matchedReturn, i) => {
     if (
       (chargeElement.allocatedQuantity < chargeElement.authorisedAnnualQuantity) &&
-      (chargeReference.allocatedQuantity < chargeReference.volume)
+      (chargeReference.allocatedQuantity < chargeReference.volume) &&
+      (matchedReturn.issues === false)
     ) {
-      if (matchedReturn.issues === true) {
-        return
-      }
-
       const matchedLines = _matchLines(chargeElement, matchedReturn.returnSubmissions[0].returnSubmissionLines)
 
-      if (matchedLines.length === 0) {
-        return
+      if (matchedLines.length > 0) {
+        _allocateReturns(chargeElement, matchedReturn, chargePeriod, chargeReference, i, matchedLines)
+      }
+    }
+  })
+}
+
+function _allocateReturns (chargeElement, matchedReturn, chargePeriod, chargeReference, i, matchedLines) {
+  matchedLines.forEach((matchedLine) => {
+    const remainingAllocation = chargeElement.authorisedAnnualQuantity - chargeElement.allocatedQuantity
+    if (remainingAllocation > 0) {
+      // We default how much to allocate to what is unallocated on the line i.e. remaining >= line.unallocated
+      let qtyToAllocate = matchedLine.unallocated
+
+      // If what remains is actually less than the line we instead set qtyToAllocate to what remains. We check this
+      // on both the chargeReference and the element
+      const chargeReferenceRemainingAllocation = chargeReference.volume - chargeReference.allocatedQuantity
+
+      if (qtyToAllocate > chargeReferenceRemainingAllocation) {
+        qtyToAllocate = chargeReferenceRemainingAllocation
+      } else if (remainingAllocation < matchedLine.unallocated) {
+        qtyToAllocate = remainingAllocation
       }
 
-      matchedLines.forEach((matchedLine) => {
-        const remainingAllocation = chargeElement.authorisedAnnualQuantity - chargeElement.allocatedQuantity
-        if (remainingAllocation > 0) {
-          // We default how much to allocate to what is unallocated on the line i.e. remaining >= line.unallocated
-          let qtyToAllocate = matchedLine.unallocated
+      // We do this check to prevent overwriting the value with false once it's been set to true as it only requires
+      // a single `matchedLine` to overlap the charge period
+      if (!chargeElement.chargeDatesOverlap) {
+        chargeElement.chargeDatesOverlap = _chargeDatesOverlap(matchedLine, chargePeriod)
+      }
 
-          // If what remains is actually less than the line we instead set qtyToAllocate to what remains. We check this
-          // on both the chargeReference and the element
-          const chargeReferenceRemainingAllocation = chargeReference.volume - chargeReference.allocatedQuantity
+      chargeElement.allocatedQuantity += qtyToAllocate
+      chargeElement.returnLogs[i].allocatedQuantity += qtyToAllocate
 
-          if (qtyToAllocate > chargeReferenceRemainingAllocation) {
-            qtyToAllocate = chargeReferenceRemainingAllocation
-          } else if (remainingAllocation < matchedLine.unallocated) {
-            qtyToAllocate = remainingAllocation
-          }
-
-          // We do this check to prevent overwriting the value with false once it's been set to true as it only requires
-          // a single `matchedLine` to overlap the charge period
-          if (!chargeElement.chargeDatesOverlap) {
-            chargeElement.chargeDatesOverlap = _chargeDatesOverlap(matchedLine, chargePeriod)
-          }
-
-          chargeElement.allocatedQuantity += qtyToAllocate
-          chargeElement.returnLogs[i].allocatedQuantity += qtyToAllocate
-
-          matchedLine.unallocated -= qtyToAllocate
-          matchedReturn.allocatedQuantity += qtyToAllocate
-          chargeReference.allocatedQuantity += qtyToAllocate
-        }
-      })
+      matchedLine.unallocated -= qtyToAllocate
+      matchedReturn.allocatedQuantity += qtyToAllocate
+      chargeReference.allocatedQuantity += qtyToAllocate
     }
   })
 }
