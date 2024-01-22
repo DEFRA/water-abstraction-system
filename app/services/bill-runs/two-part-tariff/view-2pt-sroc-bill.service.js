63fbf4b08f6a1d45d5c77a5ba89b7021b3bc3231
Rebecca Ransome <beckyrose200@aol.com>
2024-01-22 12:32:34 +0000

    Issues WIP
    Co-authored-by: Roble <rvsiyad@users.noreply.github.com>
    Co-authored-by: Jason Claxton <Jozzey@users.noreply.github.com>

--- app/services/bill-runs/two-part-tariff/view-2pt-sroc-bill.service.js
+++ app/services/bill-runs/two-part-tariff/view-2pt-sroc-bill.service.js
@@ -8,6 +8,7 @@
 const BillRunModel = require('../../../models/bill-run.model.js')
 const ReviewResultModel = require('../../../models/review-result.model.js')
 const LicenceModel = require('../../../models/licence.model.js')
+const { review } = require('../../../controllers/bill-runs.controller.js')
 
 async function go (id) {
   const result = _fetchTwoPartTariffBillRun(id)
@@ -28,7 +29,8 @@ async function _fetchTwoPartTariffBillRun (id) {
     licenceId.licenceHolder = licenceHolder
     licenceId.licenceRef = licenceRef
 
-    await _issuesOnLicence(licenceId)
+    const issues = await _issuesOnLicence(licenceId)
+    licenceId.issues = issues
   }
 
   // console.log('Licences Id', licenceIds)
@@ -46,7 +48,11 @@ async function _issuesOnLicence (licenceId) {
 
   const reviewResults = await ReviewResultModel.query()
     .where('licenceId', licenceId.licenceId)
-    .select('reviewChargeElementResultId')
+    .select(
+      'reviewChargeElementResultId',
+      'chargeReferenceId',
+      'reviewReturnResultId'
+    )
     .withGraphFetched('reviewChargeElementResults')
     .modifyGraph('reviewChargeElementResults', (builder) => {
       builder.select([
@@ -61,19 +67,23 @@ async function _issuesOnLicence (licenceId) {
         'id',
         'underQuery',
         'quantity',
-        'allocated'
+        'allocated',
+        'abstractionOutsidePeriod',
+        'status',
+        'dueDate',
+        'receivedDate'
       ])
     })
 
-  const hasChargeDatesOverlap = reviewResults.some(reviewResult => reviewResult.reviewChargeElementResults.chargeDatesOverlap)
+  const abstractionOutsidePeriod = reviewResults.some(reviewResult => reviewResult.reviewReturnResults?.abstractionOutsidePeriod)
 
-  if (hasChargeDatesOverlap) {
+  if (abstractionOutsidePeriod) {
     issues.push('Abstraction outside period')
   }
 
   // Issue 2 Aggregate (anything but 1)
   // reviewChargeElementResult: allocated
-  const hasAggregate = reviewResults.some(reviewResult => reviewResult.reviewChargeElementResults.aggregate !== 1)
+  const hasAggregate = reviewResults.some(reviewResult => reviewResult.reviewChargeElementResults?.aggregate !== 1)
 
   if (hasAggregate) {
     issues.push('Aggregate factor')
@@ -82,7 +92,7 @@ async function _issuesOnLicence (licenceId) {
   // Issue 3 checking query
   // reviewReturnResult: underQuery
 
-  const underQuery = reviewResults.some(reviewResult => reviewResult.reviewReturnResults.underQuery)
+  const underQuery = reviewResults.some(reviewResult => reviewResult.reviewReturnResults?.underQuery)
 
   if (underQuery) {
     issues.push('Checking query')
@@ -91,7 +101,7 @@ async function _issuesOnLicence (licenceId) {
   // Issue 4 No returns received status is due or overdue
   // reviewReturnResult: status
 
-  const noReturnsReceived = reviewResults.some(reviewResult => reviewResult.reviewReturnResults.status === 'due' || 'overdue')
+  const noReturnsReceived = reviewResults.some(reviewResult => reviewResult.reviewReturnResults?.status === 'due' || 'overdue')
 
   if (noReturnsReceived) {
     issues.push('No returns received')
@@ -99,38 +109,97 @@ async function _issuesOnLicence (licenceId) {
 
   // Issue 5 Over abstraction
   // reviewReturnResult quantity-allocated
-
-  const overAbstracted = reviewResults.some(reviewResult => reviewResult.reviewReturnResults.quantity > reviewResult.reviewReturnResults.allocated)
+  const overAbstracted = reviewResults.some(reviewResult => reviewResult.reviewReturnResults?.quantity > reviewResult.reviewReturnResults?.allocated)
 
   if (overAbstracted) {
     issues.push('Over abstraction')
   }
 
-  console.log('Issues :', issues)
+
   // Issue 6 Overlap of charge dates
   // reviewChargeElementResult: chargeDatesOverlap
 
+  const hasChargeDatesOverlap = reviewResults.some(reviewResult => reviewResult.reviewChargeElementResults?.chargeDatesOverlap)
+
+  if (hasChargeDatesOverlap) {
+    issues.push('Overlap of charge dates')
+  }
+
   // Issue 7 Returns received but not processed
-  // reviewReturnResult: status one of the returns has a status of received
+  // reviewReturnResult: status on one of the returns has a status of received
+  const notProcessed = reviewResults.some(reviewResult => reviewResult.reviewReturnResults?.status === 'received')
+
+  if (notProcessed) {
+    issues.push('Returns received but not processed')
+  }
 
   // Issue 8 Returns received late
   // reviewReturnResult: receivedDate - some or all the returns were submitted after the due period 24/04 or 28/11
 
+  const receivedLate = reviewResults.some(reviewResult => reviewResult.reviewReturnResults?.receivedDate > reviewResult.reviewReturnResults?.dueDate)
+
+  if (receivedLate) {
+    issues.push('Returns received late')
+  }
+
   // Issue 9 Returns split over charge references
   // reviewResults: reviewReturnResultId has two records with different chargeReferenceIds
+  // ----------------------------------------------------------------------------------------
+  const seenReviewReturnResults = {}
+  let returnsSplitOverChargeReference
+
+  for (const result of reviewResults) {
+    const { chargeReferenceId, reviewReturnResultId } = result
+
+    if (seenReviewReturnResults[reviewReturnResultId]) {
+      if (seenReviewReturnResults[reviewReturnResultId] !== chargeReferenceId) {
+        returnsSplitOverChargeReference = true
+      }
+    } else {
+      seenReviewReturnResults[reviewReturnResultId] = chargeReferenceId
+    }
+  }
+
+  if (returnsSplitOverChargeReference) {
+    issues.push('Returns split over charge references')
+  }
+
+  // ----------------------------------------------------------------------------------------
 
   // Issue 10 Some returns not received
-  // Leave for now
+  const returnsNotReceived = reviewResults.some((reviewResult) => {
+    if (reviewResult.reviewReturnResultId && reviewResult.reviewChargeElementResultId) {
+      if (reviewResult.reviewReturnResults.status === 'due' || reviewResult.reviewReturnResults.status === 'overdue') {
+        return true
+      }
+    }
 
-  // Issue 11 Unable to allocate return
-  //
+    return false
+  })
+
+  if (returnsNotReceived) {
+    issues.push('Some returns not received')
+  }
 
   // Issue 12 - Unable to match returns
   // reviewResult: a chargeElementId is in the table but not reviewReturnResult
+  const matchingReturns = reviewResults.some(reviewResult => !(reviewResult?.reviewChargeElementResultId && reviewResult?.reviewReturnResultId))
 
+  if (matchingReturns) {
+    issues.push('Unable to match returns')
+  }
+
+  console.log('Issues :', issues)
   // Issue 13
   // Multiple issues (if one or more of these issues are on a licence)
 
+  if (issues.length > 1) {
+    return 'Multiple Issues'
+  } else if (issues.length === 1) {
+    return issues[0]
+  } else {
+    return ''
+  }
 }
 
 function _licenceHolder (licenceDocument) {
