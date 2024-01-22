91b1505591e802b4b55223164897e0e19cbc1366
Robert Parkinson <robertparkinson@users.noreply.github.com>
2024-01-22 14:20:16 +0000

    Add warning text to view licence page (#670)
    * Add warning text to view licence page
    
    https://eaflood.atlassian.net/browse/WATER-4325
    
    As part of the work to migrate the summary page from the old UI to the new one we need to display a warning when a licence has been revoke, expired or lapsed.

--- app/presenters/licences/view-licence.presenter.js
+++ app/presenters/licences/view-licence.presenter.js
@@ -15,28 +15,78 @@ const { formatLongDate } = require('../base.presenter.js')
  * @returns {Object} The data formatted for the view template
  */
 function go (licence) {
-  const { expiredDate, id, licenceRef, region, startDate } = licence
+  const { expiredDate, id, lapsedDate, licenceRef, region, revokedDate, startDate } = licence
+  const warning = _generateWarningMessage(expiredDate, lapsedDate, revokedDate)
 
   return {
     id,
     endDate: _endDate(expiredDate),
     licenceRef,
     region: region.displayName,
-    startDate: formatLongDate(startDate)
+    startDate: formatLongDate(startDate),
+    warning
   }
 }
 
-/**
- * Formats the expired date of the licence as the end date for the view
- *
- * @module ViewLicencePresenter
- */
+function _compareEndDates (firstEndDate, secondEndDate) {
+  if (firstEndDate.date.getTime() === secondEndDate.date.getTime()) {
+    if (firstEndDate.name === 'revoked') return firstEndDate
+    if (secondEndDate.name === 'revoked') return secondEndDate
+    if (firstEndDate.name === 'lapsed') return firstEndDate
+  } else if (firstEndDate.date < secondEndDate.date) {
+    return firstEndDate
+  }
+  return secondEndDate
+}
+
 function _endDate (expiredDate) {
   if (!expiredDate || expiredDate < Date.now()) {
     return null
   }
 
   return formatLongDate(expiredDate)
+}
+
+function _generateWarningMessage (expiredDate, lapsedDate, revokedDate) {
+  const endDates = []
+
+  if (lapsedDate) {
+    endDates.push({
+      name: 'lapsed',
+      message: `This licence lapsed on ${formatLongDate(lapsedDate)}`,
+      date: lapsedDate
+    })
+  }
+
+  if (expiredDate) {
+    endDates.push({
+      name: 'expired',
+      message: `This licence expired on ${formatLongDate(expiredDate)}`,
+      date: expiredDate
+    })
+  }
+
+  if (revokedDate) {
+    endDates.push({
+      name: 'revoked',
+      message: `This licence was revoked on ${formatLongDate(revokedDate)}`,
+      date: revokedDate
+    })
+  }
+
+  if (endDates.length === 0) {
+    return null
+  }
+
+  if (endDates.length === 1) {
+    return endDates[0].message
+  }
+
+  const earliestPriorityEndDate = endDates.reduce((result, endDate) => {
+    return _compareEndDates(result, endDate)
+  })
+
+  return earliestPriorityEndDate.message
 }
 
 module.exports = {
