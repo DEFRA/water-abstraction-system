6204a52570d603a9a95215ba35b0d8095a060071
Fazal <Demwunz@users.noreply.github.com>
2024-01-25 16:43:54 +0000

    Update app/services/return-requirements/start-date.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/return-requirements/start-date.service.js
+++ app/services/return-requirements/start-date.service.js
@@ -9,20 +9,21 @@ const SessionModel = require('../../models/session.model.js')
 const StartDatePresenter = require('../../presenters/return-requirements/start-date.presenter.js')
 
 /**
- * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/save-date` page
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/start-date` page
  *
- * Supports generating the data needed for the save date page in the return requirements setup journey. It
- * fetches the current session record and combines it with the radio buttons, date fields and other information needed for the form.
+ * Supports generating the data needed for the save date page in the return requirements setup journey. It fetches the
+ * current session record and combines it with the radio buttons, date fields and other information needed for the form.
  *
  * If a validation issue is found when the form is submitted, it will be called from the POST handler with the Joi
  * validation error passed in. This extra information will be used to ensure the right error message is included in the
  * data needed by the view.
+ *
  * @param {string} sessionId - The id of the current session
- * @param {Error} [error] - An optional error object
- * @param {object} [payload] - Optional payload data
- * @returns {Promise} A promise resolving to the view data for the start date page
+ * @param {Error} [error] - A Joi validation error if an issue was found with the submitted form data
+ * @param {object} [payload] - The submitted form data
+ *
+ * @returns {Promise<Object>} The view data for the start date page
 */
-
 async function go (sessionId, error = null, payload = {}) {
   const session = await SessionModel.query().findById(sessionId)
   const formattedData = StartDatePresenter.go(session, error, payload)
