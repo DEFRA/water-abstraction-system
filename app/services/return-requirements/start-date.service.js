3ede59eb761809436f9eb860ab6828aaf1ba5039
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-26 12:25:39 +0000

    Simplify StartDateService
    Because the service is no longer trying to do two things we can simplify it.

--- app/services/return-requirements/start-date.service.js
+++ app/services/return-requirements/start-date.service.js
@@ -4,29 +4,23 @@
  * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/start-date` page
  * @module StartDateService
  */
- 
-const SessionModel = require('../../models/session.model.js') 
+
+const SessionModel = require('../../models/session.model.js')
 const StartDatePresenter = require('../../presenters/return-requirements/start-date.presenter.js')
 
 /**
  * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/start-date` page
  *
- * Supports generating the data needed for the save date page in the return requirements setup journey. It fetches the
+ * Supports generating the data needed for the start date page in the return requirements setup journey. It fetches the
  * current session record and combines it with the radio buttons, date fields and other information needed for the form.
  *
- * If a validation issue is found when the form is submitted, it will be called from the POST handler with the Joi
- * validation error passed in. This extra information will be used to ensure the right error message is included in the
- * data needed by the view.
- *
  * @param {string} sessionId - The id of the current session
- * @param {Error} [error] - A Joi validation error if an issue was found with the submitted form data
- * @param {object} [payload] - The submitted form data
  *
  * @returns {Promise<Object>} The view data for the start date page
 */
-async function go (sessionId, error = null, payload = {}) {
+async function go (sessionId) {
   const session = await SessionModel.query().findById(sessionId)
-  const formattedData = StartDatePresenter.go(session, error, payload)
+  const formattedData = StartDatePresenter.go(session)
 
   return {
     activeNavBar: 'search',
