10e6238488aa0d099439fc088d07d7c38504a340
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-27 14:01:20 +0000

    Create LicenceEntityRole model

---
+++ app/models/licence-entity-role.model.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Model for licence_entity_roles (crm.entity_roles)
+ * @module LicenceEntityRoleModel
+ */
+
+const BaseModel = require('./base.model.js')
+
+/**
+ * Represents an instance of a licence entity role record
+ *
+ * For reference, the licence entity role record is related to functionality that was added when the service was first built.
+ * It sits in the old `crm` schema and was not migrated to `crm_v2` as part of the previous team's efforts to replace
+ * the old legacy CRM setup.
+ *
+ * Currently, the only reason we need it is to identify if a licence has a 'registered user'. You'll see this
+ * highlighted when you view a licence.
+ *
+ * From it we can identify the 'entity' which has the role of primary user.
+ */
+class LicenceEntityRoleModel extends BaseModel {
+  static get tableName () {
+    return 'licenceEntityRoles'
+  }
+}
+
+module.exports = LicenceEntityRoleModel
