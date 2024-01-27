d634c486d6413f4e1b5aa4fab4264d1fc28a19fb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-27 14:46:11 +0000

    Link LicenceEntity to LicenceEntityRole

--- test/models/licence-entity.model.test.js
+++ test/models/licence-entity.model.test.js
@@ -10,6 +10,8 @@ const { expect } = Code
 // Test helpers
 const DatabaseHelper = require('../support/helpers/database.helper.js')
 const LicenceEntityHelper = require('../support/helpers/licence-entity.helper.js')
+const LicenceEntityRoleHelper = require('../support/helpers/licence-entity-role.helper.js')
+const LicenceEntityRoleModel = require('../../app/models/licence-entity-role.model.js')
 
 // Thing under test
 const LicenceEntityModel = require('../../app/models/licence-entity.model.js')
@@ -31,6 +33,45 @@ describe('Licence Entity model', () => {
 
       expect(result).to.be.an.instanceOf(LicenceEntityModel)
       expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to licence entity roles', () => {
+      let testLicenceEntityRoles
+
+      beforeEach(async () => {
+        testRecord = await LicenceEntityHelper.add()
+
+        const { id: licenceEntityId } = testRecord
+
+        testLicenceEntityRoles = []
+        for (let i = 0; i < 2; i++) {
+          const licenceEntityRole = await LicenceEntityRoleHelper.add({ licenceEntityId })
+          testLicenceEntityRoles.push(licenceEntityRole)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await LicenceEntityModel.query()
+          .innerJoinRelated('licenceEntityRoles')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the licence entity roles', async () => {
+        const result = await LicenceEntityModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('licenceEntityRoles')
+
+        expect(result).to.be.instanceOf(LicenceEntityModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.licenceEntityRoles).to.be.an.array()
+        expect(result.licenceEntityRoles[0]).to.be.an.instanceOf(LicenceEntityRoleModel)
+        expect(result.licenceEntityRoles).to.include(testLicenceEntityRoles[0])
+        expect(result.licenceEntityRoles).to.include(testLicenceEntityRoles[1])
+      })
     })
   })
 })
