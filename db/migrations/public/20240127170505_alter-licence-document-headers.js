af2236d43f096d3cb2171daac15547a12f39c1e4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-27 17:57:06 +0000

    Alter LicenceDocumentHeader view migration
    There were fields which didn't appear to be used when we create the migration. No doubt we didn't check pre-prod properly because if we had we would have seen that a number of these fields are populated.
    
    Specifically, we need `companyEntityId` in order to link through to the new tables we've added to get the primary user. And we need `document_name` as this is where **Licence name** comes from (shown as a field in the external summary and above the licence ref in the internal summary).

---
+++ db/migrations/public/20240127170505_alter-licence-document-headers.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const viewName = 'licence_document_headers'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .dropView(viewName)
+    .createView(viewName, (view) => {
+      view.as(knex('document_header').withSchema('crm').select([
+        'document_id AS id',
+        // This could be ignored as it is always set to the same ID. But that id comes from a single record in the
+        // crm.entity table which has the `entity_type` regime. So, for the purposes of testing we just have to live
+        // with always populating it even though we don't care about it.
+        'regime_entity_id',
+        // 'system_id',
+        'system_internal_id AS nald_id',
+        'system_external_id AS licence_ref',
+        'metadata',
+        'company_entity_id',
+        // 'verification_id',
+        'document_name AS licence_name',
+        'date_created AS created_at',
+        'date_updated AS updated_at',
+        'date_deleted AS deleted_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropView(viewName)
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex('document_header').withSchema('crm').select([
+        'document_id AS id',
+        // This could be ignored as it is always set to the same ID. But that id comes from a single record in the
+        // crm.entity table which has the `entity_type` regime. So, for the purposes of testing we just have to live
+        // with always populating it even though we don't care about it.
+        'regime_entity_id',
+        // 'system_id',
+        'system_internal_id AS nald_id',
+        'system_external_id AS licence_ref',
+        'metadata',
+        // 'company_entity_id',
+        // 'verification_id',
+        // 'document_name',
+        'date_created AS created_at',
+        'date_updated AS updated_at',
+        'date_deleted AS deleted_at'
+      ]))
+    })
+}
