9faaf04082a41ba77a3cea20e1940e72bcf15020
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-01-27 14:14:23 +0000

    Create LicenceEntityRole helper

---
+++ test/support/helpers/licence-entity-role.helper.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/**
+ * @module LicenceEntityRoleHelper
+ */
+
+const { generateUUID } = require('../../../app/lib/general.lib.js')
+const LicenceEntityRoleModel = require('../../../app/models/licence-entity-role.model.js')
+
+/**
+ * Add a new licence entity role
+ *
+ * If no `data` is provided, default values will be used. These are
+ *
+ * - `id` - [random UUID]
+ * - `licenceEntityId` - [random UUID]
+ * - `role` - primary_user
+ * - `regimeEntityId` - [random UUID]
+ * - `companyEntityId` - [random UUID]
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ *
+ * @returns {module:LicenceEntityRoleModel} The instance of the newly created record
+ */
+async function add (data = {}) {
+  const insertData = defaults(data)
+
+  return LicenceEntityRoleModel.query()
+    .insert({ ...insertData })
+    .returning('*')
+}
+
+/**
+ * Returns the defaults used
+ *
+ * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
+ * for use in tests to avoid having to duplicate values.
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ */
+function defaults (data = {}) {
+  const defaults = {
+    id: generateUUID(),
+    licenceEntityId: generateUUID(),
+    role: 'primary_user',
+    regimeEntityId: generateUUID(),
+    companyEntityId: generateUUID()
+  }
+
+  return {
+    ...defaults,
+    ...data
+  }
+}
+
+module.exports = {
+  add,
+  defaults
+}
