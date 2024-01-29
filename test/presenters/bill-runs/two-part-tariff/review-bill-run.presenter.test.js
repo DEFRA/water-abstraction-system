ed09ca61982eeb55297d97f8448c89439da9834e
Jason Claxton <jason.claxton@defra.gov.uk>
2024-01-29 10:45:23 +0000

    Presenter test WIP

--- test/presenters/bill-runs/two-part-tariff/review-bill-run.presenter.test.js
+++ test/presenters/bill-runs/two-part-tariff/review-bill-run.presenter.test.js
@@ -13,34 +13,20 @@ const ReviewBillRunPresenter = require('../../../../app/presenters/bill-runs/two
 describe.skip('Review Bill Run presenter', () => {
   describe('when there is data to be presented for review', () => {
     let billRun
-    let region
 
     beforeEach(async () => {
       // stuff
     })
 
     describe('and there are licences in the bill run', () => {
-      let testLicence
-
       beforeEach(async () => {
         // stuff
       })
 
       it('returns details of the bill run and the licences in it', async () => {
-        const result = await ReviewBillRunPresenter.go(id)
+        const result = await ReviewBillRunPresenter.go()
 
         expect(result.billRun.id).to.equal(billRun.id)
-        expect(result.billRun.createdAt).to.equal(billRun.createdAt)
-        expect(result.billRun.status).to.equal(billRun.status)
-        expect(result.billRun.toFinancialYearEnding).to.equal(billRun.toFinancialYearEnding)
-        expect(result.billRun.scheme).to.equal(billRun.scheme)
-        expect(result.billRun.batchType).to.equal(billRun.batchType)
-        expect(result.billRun.region.displayName).to.equal(region.displayName)
-
-        expect(result.billRunLicences).to.have.length(1)
-        expect(result.billRunLicences[0].licenceId).to.equal(testLicence.id)
-        expect(result.billRunLicences[0].licenceHolder).to.equal('Licence Holder Ltd')
-        expect(result.billRunLicences[0].licenceRef).to.equal(testLicence.licenceRef)
       })
     })
   })
