261a66e18f806ca3f0ff56a03eff75b17680dea7
Rebecca Ransome <beckyrose200@aol.com>
2024-02-02 13:01:12 +0000

    Add comments and unit test for licence-review-bill-run-service

--- app/services/bill-runs/two-part-tariff/licence-review-bill-run.service.js
+++ app/services/bill-runs/two-part-tariff/licence-review-bill-run.service.js
@@ -1,13 +1,24 @@
 'use strict'
 
 /**
+ * Orchestrates fetching and presenting the data needed for the licence review page
  * @module LicenceReviewBillRunService
  */
 
 const FetchLicenceReviewDataService = require('./fetch-licence-review-data.service.js')
 const LicenceReviewBillRunPresenter = require('../../../presenters/bill-runs/two-part-tariff/licence-review-bill-run.presenter.js')
-const PrepareLicenceReturnsService = require('../two-part-tariff/prepare-licence-returns.service.js')
+const PrepareLicenceReturnsService = require('./prepare-licence-returns.service.js')
 
+/**
+ * Orchestrated fetching and presenting the data needed for the licence review page
+ *
+ * @param {*} billRunId The UUID for the bill run
+ * @param {*} licenceId The UUID of the licence that is being reviewed
+ * @param {*} status The current overall status of the licence
+ *
+ * @returns {Object} an object representing the 'pageData' needed to review the individual licence. It contains the
+ * licence matched and unmatched returns and the licence charge data
+ */
 async function go (billRunId, licenceId, status) {
   const { returnLogs, licence, billRun } = await FetchLicenceReviewDataService.go(billRunId, licenceId)
 
@@ -15,7 +26,7 @@ async function go (billRunId, licenceId, status) {
 
   const pageData = await LicenceReviewBillRunPresenter.go(matchedReturns, unmatchedReturns, chargePeriods, licence, billRun, status)
 
-  return { pageData }
+  return pageData
 }
 
 module.exports = {
