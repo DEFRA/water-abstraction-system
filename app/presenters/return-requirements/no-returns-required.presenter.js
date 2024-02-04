009320563a2aeeebd41042458288963e17b64a4b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-04 23:01:04 +0000

    Move layout back into the view
    We're trying to keep a clean separation of concerns. The view templates manage everything to do with the layout and how things are presented. The 'presenters' are responsible for formatting the data the views need, whether it is to directly to display or make decisions on what to display.

--- app/presenters/return-requirements/no-returns-required.presenter.js
+++ app/presenters/return-requirements/no-returns-required.presenter.js
@@ -5,38 +5,13 @@
  * @module NoReturnsRequiredPresenter
  */
 
-const { reasonNewRequirementsFields } = require('../../lib/static-lookups.lib.js')
-
 function go (session) {
   const data = {
     id: session.id,
-    licenceRef: session.data.licence.licenceRef,
-    radioItems: _radioItems(session)
+    licenceRef: session.data.licence.licenceRef
   }
 
   return data
-}
-
-function _radioItems (_session) {
-  const radioItems = [
-    {
-      value: reasonNewRequirementsFields[0],
-      text: 'Abstraction amount below 100 cubic metres per day',
-      checked: false
-    },
-    {
-      value: reasonNewRequirementsFields[1],
-      text: 'Returns exception',
-      checked: false
-    },
-    {
-      value: reasonNewRequirementsFields[2],
-      text: 'Transfer licence',
-      checked: false
-    }
-  ]
-
-  return radioItems
 }
 
 module.exports = {
