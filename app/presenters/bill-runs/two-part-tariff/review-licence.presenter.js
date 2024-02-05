765f829ef899b2fd5bb6525b7d117cf12fd2d3f3
Rebecca Ransome <beckyrose200@aol.com>
2024-02-05 17:27:25 +0000

    Refactor presenter

--- app/presenters/bill-runs/two-part-tariff/review-licence.presenter.js
+++ app/presenters/bill-runs/two-part-tariff/review-licence.presenter.js
@@ -1,120 +1,108 @@
 'use strict'
 
 /**
- * Formats the two part tariff review data ready for presenting in the review page
+ * Formats the review licence data ready for presenting in the review licence page
  * @module ReviewLicencePresenter
  */
 
 const { formatLongDate } = require('../../base.presenter.js')
 
+/**
+ * Prepares the processes the bill run and review licence data for presentation
+ *
+ * @param {module:ReviewReturnResultModel} matchedReturns matched return logs for an individual licence
+ * @param {module:ReviewReturnResultModel} unmatchedReturns unmatched return logs for an individual licence
+ * @param {Object[]} chargePeriods chargePeriods with start and end date properties
+ * @param {module:BillRunModel} billRun the data from the bill run
+ *
+ * @returns {Object} the prepared bill run and licence data to be passed to the review licence page
+ */
 async function go (matchedReturns, unmatchedReturns, chargePeriods, billRun) {
-  const pageData = {
+  return {
     licenceRef: matchedReturns[0].licence.licenceRef,
     billRunId: billRun.id,
     status: 'Review',
-    region: billRun.region.displayName
+    region: billRun.region.displayName,
+    matchedReturns: _prepareMatchedReturns(matchedReturns),
+    unmatchedReturns: _prepareUnmatchedReturns(unmatchedReturns),
+    chargePeriodDates: _prepareLicenceChargePeriods(chargePeriods)
   }
-
-  const formattedChargePeriods = _formatLicenceChargePeriods(chargePeriods)
-  const preparedReturns = _formattingMatchedReturns(matchedReturns)
-  const preparedUnmatchedReturns = _formattingUnmatchedReturns(unmatchedReturns)
-
-  pageData.matchedReturns = preparedReturns
-  pageData.chargePeriodDates = formattedChargePeriods
-  pageData.unmatchedReturns = preparedUnmatchedReturns
-
-  return pageData
 }
 
-function _formatLicenceChargePeriods (chargePeriods) {
-  const formattedChargePeriods = []
+function _prepareLicenceChargePeriods (chargePeriods) {
+  return chargePeriods.map((chargePeriod) => {
+    const { startDate, endDate } = chargePeriod
 
-  for (const chargePeriod of chargePeriods) {
-    if (chargePeriod.startDate !== null) {
-      const formattedDates = _formatChargePeriods(chargePeriod)
-
-      formattedChargePeriods.push(formattedDates)
-    }
-  }
-
-  return formattedChargePeriods
+    return _prepareDate(startDate, endDate)
+  })
 }
 
-function _formattingUnmatchedReturns (unmatchedReturns) {
-  const preparedUnmatchedReturns = []
+function _prepareUnmatchedReturns (unmatchedReturns) {
+  return unmatchedReturns.map((unmatchedReturn) => {
+    const { returnReference, status, description, purposes, allocated, quantity, startDate, endDate } = unmatchedReturn.reviewReturnResults
 
-  for (const unmatchedReturn of unmatchedReturns) {
-    preparedUnmatchedReturns.push({
-      reference: unmatchedReturn.reviewReturnResults.returnReference,
-      dates: _date(unmatchedReturn),
-      status: unmatchedReturn.reviewReturnResults.status,
-      description: unmatchedReturn.reviewReturnResults.description,
-      purpose: unmatchedReturn.reviewReturnResults.purposes[0].tertiary.description,
-      total: `${unmatchedReturn.reviewReturnResults.allocated} ML / ${unmatchedReturn.reviewReturnResults.quantity} ML`
-    })
-  }
-
-  return preparedUnmatchedReturns
-}
-
-function _formattingMatchedReturns (matchedReturns) {
-  const preparedReturns = []
-
-  for (const returnLog of matchedReturns) {
-    const { status, total, allocated } = _checkStatusAndReturnTotal(returnLog)
-
-    preparedReturns.push({
-      reference: returnLog.reviewReturnResults.returnReference,
-      dates: _date(returnLog),
+    return {
+      reference: returnReference,
+      dates: _prepareDate(startDate, endDate),
       status,
-      description: returnLog.reviewReturnResults.description,
-      purpose: returnLog.reviewReturnResults.purposes[0].tertiary.description,
+      description,
+      purpose: purposes[0].tertiary.description,
+      total: `${allocated} ML / ${quantity} ML`
+    }
+  })
+}
+
+function _prepareMatchedReturns (matchedReturns) {
+  return matchedReturns.map((matchedReturn) => {
+    const { returnStatus, total, allocated } = _checkStatusAndReturnTotal(matchedReturn)
+    const { returnReference, description, purposes, startDate, endDate } = matchedReturn.reviewReturnResults
+
+    return {
+      reference: returnReference,
+      dates: _prepareDate(startDate, endDate),
+      status: returnStatus,
+      description,
+      purpose: purposes[0].tertiary.description,
       total,
       allocated
-    })
-  }
-
-  return preparedReturns
+    }
+  })
 }
 
 function _checkStatusAndReturnTotal (returnLog) {
-  const allocated = _allocated(returnLog)
+  const { status, allocated, quantity, underQuery } = returnLog.reviewReturnResults
 
-  if (returnLog.reviewReturnResults.status === 'completed') {
-    return { status: returnLog.reviewReturnResults.status, total: `${returnLog.reviewReturnResults.allocated} ML / ${returnLog.reviewReturnResults.quantity} ML`, allocated }
-  } else if (returnLog.reviewReturnResults.underQuery === true) {
-    return { status: 'query', total: `${returnLog.reviewReturnResults.allocated} ML / ${returnLog.reviewReturnResults.quantity} ML`, allocated }
-  } else if (returnLog.reviewReturnResults.status === 'received') {
-    return { status: returnLog.reviewReturnResults.status, total: '/', allocated: 'Not processed' }
-  } else if (returnLog.reviewReturnResults.status === 'due') {
-    return { status: 'overdue', total: '/', allocated: 'Not processed' }
-  } else {
-    return { status: returnLog.reviewReturnResults.status, total: '/', allocated: 'Not processed' }
+  let returnStatus = underQuery ? 'query' : status
+  let total = underQuery ? '/' : `${allocated} ML / ${quantity} ML`
+  let allocatedStatus = _allocated(quantity, allocated)
+
+  if (status === 'void' || status === 'received') {
+    total = '/'
+    allocatedStatus = 'Not processed'
+  } else if (status === 'due') {
+    returnStatus = 'overdue'
+    total = '/'
+    allocatedStatus = 'Not processed'
   }
+
+  return { returnStatus, total, allocated: allocatedStatus }
 }
 
-function _formatChargePeriods (chargePeriod) {
-  const start = formatLongDate(chargePeriod.startDate)
-  const end = formatLongDate(chargePeriod.endDate)
-
-  return `${start} to ${end}`
-}
-
-function _allocated (returnLog) {
-  if (returnLog.reviewReturnResults.quantity > returnLog.reviewReturnResults.allocated) {
+function _allocated (quantity, allocated) {
+  if (quantity > allocated) {
     return 'Over abstraction'
-  } else if (returnLog.reviewReturnResults.quantity === returnLog.reviewReturnResults.allocated) {
+  } else if (quantity === allocated) {
     return 'Fully allocated'
   } else {
     return ''
   }
 }
 
-function _date (returnLog) {
-  const startDate = formatLongDate(returnLog.reviewReturnResults.startDate)
-  const endDate = formatLongDate(returnLog.reviewReturnResults.endDate)
+function _prepareDate (startDate, endDate) {
+  const preparedStartDate = formatLongDate(startDate)
+  const preparedEndDate = formatLongDate(endDate)
 
-  return `${startDate} to ${endDate}`
+  return `${preparedStartDate} to ${preparedEndDate}`
 }
 
 module.exports = {
