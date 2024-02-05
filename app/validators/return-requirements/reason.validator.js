968f3645ebbe47f65415f0fb41fa2b39109d054c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-05 00:58:48 +0000

    Make the validator more robust
    As our unit test confirmed it was checking for a value but would accept any value. Now it will only accept certain values.

--- app/validators/return-requirements/reason.validator.js
+++ app/validators/return-requirements/reason.validator.js
@@ -7,12 +7,28 @@
 
 const Joi = require('joi')
 
+const VALID_VALUES = [
+  'change_to_special_agreement',
+  'name_or_address_change',
+  'transfer_and_now_chargeable',
+  'extension_of_licence_validity',
+  'major_change',
+  'minor_change',
+  'new_licence_in_part_succession_or_licence_apportionment',
+  'new_licence',
+  'new_special_agreement',
+  'succession_or_transfer_of_licence',
+  'succession_to_remainder_licence_or_licence_apportionment'
+]
+
 function go (data) {
   const schema = Joi.object({
     reason: Joi.string()
       .required()
+      .valid(...VALID_VALUES)
       .messages({
         'any.required': 'Select the reason for the return requirement',
+        'any.only': 'Select the reason for the return requirement',
         'string.empty': 'Select the reason for the return requirement'
       })
   })
--- test/validators/no-returns-required/reason.validator.test.js
+++ test/validators/no-returns-required/reason.validator.test.js
@@ -11,16 +11,12 @@ const { expect } = Code
 const ReasonValidator = require('../../../app/validators/return-requirements/reason.validator.js')
 
 describe('Select Reason validator', () => {
-  const values = ['bajor', 'risa', 'ferenginar']
-
   describe('when valid data is provided', () => {
-    values.forEach(reason => {
-      it(`confirms the data is valid (reason ${reason})`, () => {
-        const result = ReasonValidator.go({ reason })
+    it('confirms the data is valid', () => {
+      const result = ReasonValidator.go({ reason: 'major_change' })
 
-        expect(result.value).to.exist()
-        expect(result.error).not.to.exist()
-      })
+      expect(result.value).to.exist()
+      expect(result.error).not.to.exist()
     })
   })
 
@@ -28,6 +24,16 @@ describe('Select Reason validator', () => {
     describe("because no 'reason' is given", () => {
       it('fails validation', () => {
         const result = ReasonValidator.go({ reason: '' })
+
+        expect(result.value).to.exist()
+        expect(result.error).to.exist()
+        expect(result.error.details[0].message).to.equal('Select the reason for the return requirement')
+      })
+    })
+
+    describe("because an unknown 'reason' is given", () => {
+      it('fails validation', () => {
+        const result = ReasonValidator.go({ reason: 'just-because' })
 
         expect(result.value).to.exist()
         expect(result.error).to.exist()
