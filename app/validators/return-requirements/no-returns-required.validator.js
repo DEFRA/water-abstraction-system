dc34586352bc2ae85396d863312a1c3e0594766a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-05 01:00:06 +0000

    Update no-rtns-req validator
    Now it is consistent with what we just did in the `ReasonValidator`.

--- app/validators/return-requirements/no-returns-required.validator.js
+++ app/validators/return-requirements/no-returns-required.validator.js
@@ -7,11 +7,17 @@
 
 const Joi = require('joi')
 
+const VALID_VALUES = [
+  'abstraction_below_100_cubic_metres_per_day',
+  'returns_exception',
+  'transfer_licence'
+]
+
 function go (data) {
   const schema = Joi.object({
     'no-returns-required': Joi.string()
       .required()
-      .valid('abstraction_below_100_cubic_metres_per_day', 'returns_exception', 'transfer_licence')
+      .valid(...VALID_VALUES)
       .messages({
         'any.required': 'Select the reason for the return requirement',
         'any.only': 'Select the reason for the return requirement',
