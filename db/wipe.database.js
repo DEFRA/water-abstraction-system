269f3cbf3835fd23513feebe70cde13ea461fcc2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-09 16:46:45 +0000

    Add a new wipe.database.js module
    In order to invoke this from `npm run` and our `package.json` we need a module that calls rather than exports the function we want to run. In our case it is an async function so we also need to use some Node wizardry to await a top level async function.
    
    This new module is what enables both these things to happen.

---
+++ db/wipe.database.js
@@ -0,0 +1,40 @@
+'use strict'
+
+/**
+ * Used to wipe the test database of all tables, views and legacy schemas
+ * @module WipeDatabase
+ */
+
+const { dbConfig } = require('./db.js')
+
+const { wipe } = require('../test/support/helpers/database.helper.js')
+
+/**
+ * Wipe the test database of all tables, views and legacy schemas
+ *
+ * Will only run if NODE_ENV=test as it is only intended to be used to wipe the test DB prior to running migrations.
+ *
+ * NOTE: If we don't have a function before the immediately invoked async below Standard JS shouts at us! So, this also
+ * handily solves that problem.
+ *
+ * @returns {Promise} the promise returned by `wipe()` (does not resolve to anything)
+ */
+async function wipeDatabase () {
+  if (process.env.NODE_ENV !== 'test') {
+    throw Error('Will not wipe database unless NODE_ENV is test')
+  }
+
+  return wipe()
+}
+
+(async () => {
+  await wipeDatabase()
+
+  console.log(`${dbConfig.connection.database} database wiped`)
+
+  process.exit()
+})().catch(error => {
+  console.error(`Could not wipe ${dbConfig.connection.database}: ${error.message}`)
+
+  process.exit(1)
+})
