cc5b5f1aac5766163e66f7e6e3d04742f62ab91f
rvsiyad <roblesiyad123@gmail.com>
2024-02-13 13:19:17 +0000

    Created test cases for the landing page controller

---
+++ test/controllers/landing-page.controller.test.js
@@ -0,0 +1,53 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// For running our service
+const { init } = require('../../app/server.js')
+
+describe.only('Landing Page controller', () => {
+  let server
+
+  // Create server before each test
+  beforeEach(async () => {
+    server = await init()
+  })
+
+  describe('when a request is made to correct URL', () => {
+    it('displays the landing page', async () => {
+      const options = {
+        method: 'GET',
+        url: '/landing-page'
+      }
+
+      const response = await server.inject(options)
+      const payload = response.payload
+
+      expect(response.statusCode).to.equal(200)
+
+      expect(payload).to.include('Landing Page')
+      expect(payload).to.include('Lorem ipsum')
+    })
+  })
+
+  describe('when a request is made to an incorrect URL', () => {
+    it('displays page not found', async () => {
+      const OtherOptions = {
+        method: 'GET',
+        url: '/landing-pages'
+      }
+
+      const response = await server.inject(OtherOptions)
+      const payload = response.payload
+
+      expect(response.statusCode).to.equal(404)
+
+      expect(payload).to.include('Page not found')
+    })
+  })
+})
