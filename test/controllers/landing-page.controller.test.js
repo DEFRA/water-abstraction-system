b0c7b640d30c04a5172dabd1ee12513b95754864
rvsiyad <roblesiyad123@gmail.com>
2024-02-13 16:52:39 +0000

    Removed test for incorrect URL, added stubbing for logger and airbrake

--- test/controllers/landing-page.controller.test.js
+++ test/controllers/landing-page.controller.test.js
@@ -3,6 +3,7 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
 const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
@@ -13,41 +14,31 @@ const { init } = require('../../app/server.js')
 describe('Landing Page controller', () => {
   let server
 
-  // Create server before each test
   beforeEach(async () => {
+    // Create server before each test
     server = await init()
+
+    // We silence calls to server.logger.error in the plugin to keep test output as
+    // clean as possible
+    Sinon.stub(server.logger, 'error')
+
+    // We silence sending a notification to our Errbit instance using Airbrake
+    Sinon.stub(server.app.airbrake, 'notify').resolvesThis()
   })
 
-  describe('when a request is made to correct URL', () => {
-    it('displays the landing page', async () => {
-      const options = {
-        method: 'GET',
-        url: '/landing-page'
-      }
+  describe('GET /landing-page', () => {
+    const options = {
+      method: 'GET',
+      url: '/landing-page'
+    }
 
+    it('displays the landing page', async () => {
       const response = await server.inject(options)
-      const payload = response.payload
 
       expect(response.statusCode).to.equal(200)
 
-      expect(payload).to.include('Landing Page')
-      expect(payload).to.include('Lorem ipsum')
-    })
-  })
-
-  describe('when a request is made to an incorrect URL', () => {
-    it('displays page not found', async () => {
-      const OtherOptions = {
-        method: 'GET',
-        url: '/landing-pages'
-      }
-
-      const response = await server.inject(OtherOptions)
-      const payload = response.payload
-
-      expect(response.statusCode).to.equal(404)
-
-      expect(payload).to.include('Page not found')
+      expect(response.payload).to.include('Landing Page')
+      expect(response.payload).to.include('Lorem ipsum')
     })
   })
 })
