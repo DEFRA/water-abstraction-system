3af3ff868ff3a087834acefcc080888da385d5d5
Jason Claxton <jason.claxton@defra.gov.uk>
2024-02-15 17:30:12 +0000

    Add functionality to remove bill run from CM

--- app/services/bill-runs/cancel-bill-run.service.js
+++ app/services/bill-runs/cancel-bill-run.service.js
@@ -1,11 +1,11 @@
 'use strict'
 
 /**
- * Orchestrates fetching and presenting the data needed for the cancel bill run confirmation page
+ * Cancels the bill run by removing all data relating to it from the database
  * @module CancelBillRunConfirmationService
  */
 
-const LegacyRequestLib = require('../../lib/legacy-request.lib.js')
+const ChargingModuleRequestLib = require('../../lib/charging-module-request.lib.js')
 
 /**
  * Orchestrates fetching and presenting the data needed for the cancel bill run confirmation page
@@ -15,10 +15,18 @@ const LegacyRequestLib = require('../../lib/legacy-request.lib.js')
  * @returns {Object} an object representing the `pageData` needed by the cancel bill run template. It contains details
  * of the bill run.
  */
-async function go (id, billRunBatchType) {
-  const result = await LegacyRequestLib.post('water', `billing/batch/${id}/cancel`)
+async function go (id, billRunBatchType, chargingModuleBillRunId) {
+  if (billRunBatchType === 'two_part_tariff') {
+    _deleteChargingModuleBillRun(chargingModuleBillRunId)
+  }
+}
 
-  console.log('🚀 ~ go ~ result:', result)
+async function _deleteChargingModuleBillRun (chargingModuleBillRunId) {
+  const result = await ChargingModuleRequestLib.delete(`v3/wrls/bill-runs/${chargingModuleBillRunId}`)
+
+  if (!result.succeeded) {
+    global.GlobalNotifier.omg(`Failed to delete bill run from Charging Module. ${result.response.body.message}`)
+  }
 }
 
 module.exports = {
