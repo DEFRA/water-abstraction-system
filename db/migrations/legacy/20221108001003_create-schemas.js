2a45ad997ba51a895f6c4934e833d6217bc7e2c8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-19 23:14:04 +0000

    Add permit schema creation
    We know we're gonna need it!

--- db/migrations/legacy/20221108001003_create-schemas.js
+++ db/migrations/legacy/20221108001003_create-schemas.js
@@ -5,6 +5,7 @@ exports.up = function (knex) {
     CREATE SCHEMA IF NOT EXISTS "crm";
     CREATE SCHEMA IF NOT EXISTS "crm_v2";
     CREATE SCHEMA IF NOT EXISTS "idm";
+    CREATE SCHEMA IF NOT EXISTS "permit";
     CREATE SCHEMA IF NOT EXISTS "returns";
     CREATE SCHEMA IF NOT EXISTS "water";
   `)
@@ -14,6 +15,7 @@ exports.down = function (knex) {
   return knex.raw(`
     DROP SCHEMA IF EXISTS "water";
     DROP SCHEMA IF EXISTS "returns";
+    DROP SCHEMA IF EXISTS "permit";
     DROP SCHEMA IF EXISTS "idm";
     DROP SCHEMA IF EXISTS "crm_v2";
     DROP SCHEMA IF EXISTS "crm";
