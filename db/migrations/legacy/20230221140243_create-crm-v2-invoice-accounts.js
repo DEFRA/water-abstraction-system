f3007a97bc46cd73c63363a59c71bb8d6a8790ca
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-19 15:04:16 +0000

    Update crm_v2.invoice_accounts

--- db/migrations/legacy/20230221140243_create-crm-v2-invoice-accounts.js
+++ db/migrations/legacy/20230221140243_create-crm-v2-invoice-accounts.js
@@ -11,16 +11,21 @@ exports.up = function (knex) {
       table.uuid('invoice_account_id').primary().defaultTo(knex.raw('gen_random_uuid()'))
 
       // Data
-      table.uuid('company_id')
-      table.string('invoice_account_number')
+      table.uuid('company_id').notNullable()
+      table.string('invoice_account_number').notNullable()
       table.date('start_date')
       table.date('end_date')
+      table.boolean('is_test').notNullable().defaultTo(false)
       table.string('last_transaction_file_reference')
       table.timestamp('date_last_transaction_file_reference_updated', { useTz: false }).notNullable().defaultTo(knex.fn.now())
 
       // Legacy timestamps
       table.timestamp('date_created', { useTz: false }).notNullable().defaultTo(knex.fn.now())
       table.timestamp('date_updated', { useTz: false }).notNullable().defaultTo(knex.fn.now())
+
+      // Constraints
+      table.unique(['company_id', 'invoice_account_number'], { useConstraint: true })
+      table.unique(['invoice_account_number'], { useConstraint: true })
     })
 }
 
