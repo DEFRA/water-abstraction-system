fb968f3da6b09adf3cd1d4d4c9755091050cdc09
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-02-20 17:52:04 +0000

    Expand unit tests
    Part of the refactoring will be related to firing all the requests rather than waiting for each one to complete.
    
    Therefore to be sure everything is still working as expected we expand the unit tests to deal with multiple transactions rather than just one.

--- test/services/bill-runs/supplementary/send-transactions.service.test.js
+++ test/services/bill-runs/supplementary/send-transactions.service.test.js
@@ -31,8 +31,80 @@ describe('Send Transactions service', () => {
     regionalChargeArea: 'Yorkshire',
     region: { chargeRegionId: 'N' }
   }
-  const transaction = {
-    id: '9b092372-1a26-436a-bf1f-b5eb3f9aca44',
+
+  let chargingModuleCreateTransactionServiceStub
+  let transactions
+
+  beforeEach(() => {
+    chargingModuleCreateTransactionServiceStub = Sinon.stub(ChargingModuleCreateTransactionService, 'go')
+
+    transactions = [
+      _transaction('fca14c7e-c895-4991-9651-9a76f45b971d'),
+      _transaction('e98ad67f-5a26-43d7-bdba-39eba49ab62c')
+    ]
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when calling the Charging Module API is successful', () => {
+    beforeEach(async () => {
+      chargingModuleCreateTransactionServiceStub.onFirstCall().resolves({
+        ..._chargingModuleResponse('7e752fa6-a19c-4779-b28c-6e536f028795')
+      })
+      chargingModuleCreateTransactionServiceStub.onSecondCall().resolves({
+        ..._chargingModuleResponse('a2086da4-e3b6-4b83-afe1-0e2e5255efaf')
+      })
+    })
+
+    it('updates the transactions with the responses from the Charging Module API', async () => {
+      const results = await SendTransactionsService.go(
+        licence,
+        bill,
+        billRunExternalId,
+        transactions,
+        billingPeriod
+      )
+
+      expect(results).length(2)
+      expect(results[0].status).to.equal('charge_created')
+      expect(results[0].externalId).to.equal('7e752fa6-a19c-4779-b28c-6e536f028795')
+      expect(results[1].status).to.equal('charge_created')
+      expect(results[1].externalId).to.equal('a2086da4-e3b6-4b83-afe1-0e2e5255efaf')
+    })
+  })
+
+  describe('when calling the Charging Module API is unsuccessful', () => {
+    beforeEach(async () => {
+      chargingModuleCreateTransactionServiceStub.onFirstCall().resolves({
+        ..._chargingModuleResponse('7e752fa6-a19c-4779-b28c-6e536f028795')
+      })
+      chargingModuleCreateTransactionServiceStub.onSecondCall().rejects()
+    })
+
+    it('throws a BillRunError with the correct code', async () => {
+      const error = await expect(
+        SendTransactionsService.go(
+          licence,
+          bill,
+          billRunExternalId,
+          transactions,
+          billingPeriod
+        )
+      )
+        .to
+        .reject()
+
+      expect(error).to.be.an.instanceOf(BillRunError)
+      expect(error.code).to.equal(BillRunModel.errorCodes.failedToCreateCharge)
+    })
+  })
+})
+
+function _transaction (transactionId) {
+  return {
+    id: transactionId,
     chargeReferenceId: '32058a19-4813-4ee7-808b-a0559deb8469',
     startDate: new Date('2022-04-01'),
     endDate: new Date('2022-10-31'),
@@ -64,62 +136,13 @@ describe('Send Transactions service', () => {
     winterOnly: false,
     waterUndertaker: false
   }
+}
 
-  let transactions
-
-  beforeEach(() => {
-    transactions = [transaction]
-  })
-
-  afterEach(() => {
-    Sinon.restore()
-  })
-
-  describe('when calling the Charging Module API is successful', () => {
-    beforeEach(async () => {
-      Sinon.stub(ChargingModuleCreateTransactionService, 'go').resolves({
-        succeeded: true,
-        response: {
-          body: { transaction: { id: '7e752fa6-a19c-4779-b28c-6e536f028795' } }
-        }
-      })
-    })
-
-    it('updates the transactions with the responses from the Charging Module API', async () => {
-      const results = await SendTransactionsService.go(
-        licence,
-        bill,
-        billRunExternalId,
-        transactions,
-        billingPeriod
-      )
-
-      expect(results).length(1)
-      expect(results[0].status).to.equal('charge_created')
-      expect(results[0].externalId).to.equal('7e752fa6-a19c-4779-b28c-6e536f028795')
-    })
-  })
-
-  describe('when calling the Charging Module API is unsuccessful', () => {
-    beforeEach(async () => {
-      Sinon.stub(ChargingModuleCreateTransactionService, 'go').rejects()
-    })
-
-    it('throws a BillRunError with the correct code', async () => {
-      const error = await expect(
-        SendTransactionsService.go(
-          licence,
-          bill,
-          billRunExternalId,
-          transactions,
-          billingPeriod
-        )
-      )
-        .to
-        .reject()
-
-      expect(error).to.be.an.instanceOf(BillRunError)
-      expect(error.code).to.equal(BillRunModel.errorCodes.failedToCreateCharge)
-    })
-  })
-})
+function _chargingModuleResponse (transactionId) {
+  return {
+    succeeded: true,
+    response: {
+      body: { transaction: { id: transactionId } }
+    }
+  }
+}
