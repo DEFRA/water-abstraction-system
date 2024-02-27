3b90984670e19c84afcd7385420be734022d6467
rvsiyad <roblesiyad123@gmail.com>
2024-02-27 12:46:18 +0000

    Add Submit Site Description service test

---
+++ test/services/return-requirements/submit-site-description.service.test.js
@@ -0,0 +1,173 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const DatabaseHelper = require('../../support/helpers/database.helper.js')
+const SessionHelper = require('../../support/helpers/session.helper.js')
+
+// Things under test
+const SubmitSiteDescriptionService = require('../../../app/services/return-requirements/submit-site-description.service.js')
+
+describe('Submit Site Description service', () => {
+  let payload
+  let session
+
+  beforeEach(async () => {
+    await DatabaseHelper.clean()
+
+    session = await SessionHelper.add({
+      data: {
+        licence: {
+          id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+          currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+          endDate: null,
+          licenceRef: '01/ABC',
+          licenceHolder: 'Turbo Kid',
+          startDate: '2022-04-01T00:00:00.000Z'
+        },
+        journey: 'returns-required'
+      }
+    })
+  })
+
+  describe('when called', () => {
+    describe('with a valid payload', () => {
+      beforeEach(() => {
+        payload = {
+          siteDescription: 'This is a valid return requirement description'
+        }
+      })
+
+      it('fetches the current setup session record', async () => {
+        const result = await SubmitSiteDescriptionService.go(session.id, payload)
+        expect(result.id).to.equal(session.id)
+      })
+
+      it('returns page data for the view', async () => {
+        const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+        expect(result).to.equal({
+          activeNavBar: 'search',
+          error: null,
+          journey: 'returns-required',
+          pageTitle: 'Enter a site description for the return requirement',
+          licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+          licenceRef: '01/ABC',
+          licenceSiteDescription: 'This is a valid return requirement description'
+        }, { skip: ['id'] })
+      })
+    })
+
+    describe('with an invalid payload', () => {
+      describe('because the user has not entered anything', () => {
+        beforeEach(() => {
+          payload = {}
+        })
+
+        it('fetches the current setup session record', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result.id).to.equal(session.id)
+        })
+
+        it('returns page data for the view', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result).to.equal({
+            activeNavBar: 'search',
+            journey: 'returns-required',
+            pageTitle: 'Enter a site description for the return requirement',
+            licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            licenceRef: '01/ABC',
+            licenceSiteDescription: null
+          }, { skip: ['id', 'error'] })
+        })
+
+        it('returns page data with an error for the text input element', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result.error).to.equal({
+            text: 'Enter a description of the site'
+          })
+        })
+      })
+
+      describe('because the user has entered a description less than 10 characters', () => {
+        beforeEach(() => {
+          payload = {
+            siteDescription: 'Too short'
+          }
+        })
+
+        it('fetches the current setup session record', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result.id).to.equal(session.id)
+        })
+
+        it('returns page data for the view', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result).to.equal({
+            activeNavBar: 'search',
+            journey: 'returns-required',
+            pageTitle: 'Enter a site description for the return requirement',
+            licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            licenceRef: '01/ABC',
+            licenceSiteDescription: 'Too short'
+          }, { skip: ['id', 'error'] })
+        })
+
+        it('returns page data with an error for the text input element', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result.error).to.equal({
+            text: 'Site description must be 10 characters or more'
+          })
+        })
+      })
+
+      describe('because the user has entered a description more than 100 characters', () => {
+        const invalidSiteDescription = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis.'
+        beforeEach(() => {
+          payload = {
+            siteDescription: invalidSiteDescription
+          }
+        })
+
+        it('fetches the current setup session record', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result.id).to.equal(session.id)
+        })
+
+        it('returns page data for the view', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result).to.equal({
+            activeNavBar: 'search',
+            journey: 'returns-required',
+            pageTitle: 'Enter a site description for the return requirement',
+            licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            licenceRef: '01/ABC',
+            licenceSiteDescription: invalidSiteDescription
+          }, { skip: ['id', 'error'] })
+        })
+
+        it('returns page data with an error for the text input element', async () => {
+          const result = await SubmitSiteDescriptionService.go(session.id, payload)
+
+          expect(result.error).to.equal({
+            text: 'Site description must be 100 characters or less'
+          })
+        })
+      })
+    })
+  })
+})
