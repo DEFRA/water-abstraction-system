7f75e9910d7cb2d7343c13e9952ddcb1b9d93d21
rvsiyad <roblesiyad123@gmail.com>
2024-02-27 14:16:53 +0000

    Added JSdoc comments for Submit Site Description service

--- app/services/return-requirements/submit-site-description.service.js
+++ app/services/return-requirements/submit-site-description.service.js
@@ -1,9 +1,27 @@
 'use strict'
 
+/**
+ * Orchestrates validating the data for `/return-requirements/{sessionId}/site-description` page
+ * @module SubmitSiteDescriptionService
+ */
 const SessionModel = require('../../models/session.model.js')
 const SiteDescriptionPresenter = require('../../presenters/return-requirements/site-description.presenter.js')
 const SiteDescriptionValidator = require('../../validators/return-requirements/site-description.validator.js')
 
+/**
+ * Orchestrates validating the data for `/return-requirements/{sessionId}/site-description` page
+ *
+ * It first retrieves the session instance for the returns requirements journey in progress.
+ *
+ * The user input is then validated and the result is then combined with the output of the presenter to generate the page data needed by the view.
+ * If there was a validation error the controller will re-render the page so needs this information. If all is well the
+ * controller will redirect to the next page in the journey.
+ *
+ * @param {string} sessionId - The id of the current session
+ * @param {Object} payload - The submitted form data
+ *
+ * @returns {Promise<Object>} The page data for the start date page
+ */
 async function go (sessionId, payload) {
   const session = await SessionModel.query().findById(sessionId)
 
