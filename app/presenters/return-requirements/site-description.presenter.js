7f9a6b3deb26e9a697303dc7f338b674e171fe91
rvsiyad <roblesiyad123@gmail.com>
2024-02-27 14:14:26 +0000

    Added JSDocs comments

--- app/presenters/return-requirements/site-description.presenter.js
+++ app/presenters/return-requirements/site-description.presenter.js
@@ -1,5 +1,16 @@
 'use strict'
 
+/**
+ * Formats data for the `/return-requirements/{sessionId}/site-description` page
+ * @module SiteDescriptionPresenter
+*/
+
+/**
+ * Formats data for the `/return-requirements/{sessionId}/site-description` page.
+ * @param {module:SessionModel} session - The returns requirements session instance.
+ * @param {Object} [payload] - The payload from the request.
+ * @returns {Object} - The data formatted for the view template.
+ */
 function go (session, payload = {}) {
   const data = {
     id: session.id,
@@ -12,6 +23,11 @@ function go (session, payload = {}) {
 }
 
 function _licenceSiteDescription (payload) {
+  // NOTE: 'siteDescription' is the payload value that tells us whether the user inputted a siteDescription
+  // for the return requirement site.
+  // If it is not set then it is because the presenter has been called from 'SiteDescriptionService' and it's the first
+  // load. Else it has been called by the 'SubmitSiteDescriptionService' and the user has not inputted a site description.
+  // Either way, we use it to tell us wether there is anything in the payload worth transforming.
   const siteDescription = payload.siteDescription
 
   if (!siteDescription) {
