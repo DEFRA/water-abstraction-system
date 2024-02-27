c70e8a3b4c61565f18067894a52daf6725431f58
rvsiyad <roblesiyad123@gmail.com>
2024-02-27 14:15:14 +0000

    Added JSDoc comments for Site Description service

--- app/services/return-requirements/site-description.service.js
+++ app/services/return-requirements/site-description.service.js
@@ -1,8 +1,23 @@
 'use strict'
 
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/site-description` page
+ * @module SiteDescriptionService
+ */
+
 const SessionModel = require('../../models/session.model.js')
 const SiteDescriptionPresenter = require('../../presenters/return-requirements/site-description.presenter.js')
 
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/site-description` page
+ *
+ * Supports generating the data needed for the site description page in the return requirements setup journey. It fetches the
+ * current session record and combines it with the date fields and other information needed for the form.
+ *
+ * @param {string} sessionId - The id of the current session
+ *
+ * @returns {Promise<Object>} The view data for the site description page
+*/
 async function go (sessionId) {
   const session = await SessionModel.query().findById(sessionId)
   const formattedData = SiteDescriptionPresenter.go(session)
