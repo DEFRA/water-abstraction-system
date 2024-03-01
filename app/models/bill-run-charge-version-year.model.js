fe30e8e79f97aaf4ee22a47a21674ef654b8ab25
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-01 12:43:20 +0000

    Add Bill Run Charge Version Years model (#777)
    https://eaflood.atlassian.net/browse/WATER-4387
    
    We need to add this to support our work to improve how bill runs are cancelled. Basically, replace the crummy legacy one!
    
    The driving reason is to make it visible to users when a bill run is being cancelled as this impacts performance. If we can make cancelling bill runs visible, users can make more informed decisions about when to start creating a new one.
    
    In support of that, we need to add the `BillRunChargeVersionYearModel` as it's the one table we don't have a model for and which we need to delete stuff from when cancelling a bill run.

---
+++ app/models/bill-run-charge-version-year.model.js
@@ -0,0 +1,52 @@
+'use strict'
+
+/**
+ * Model for bill_run_charge_version_years (water.billing_batch_charge_version_years)
+ * @module BillRunChargeVersionYearModel
+ */
+
+const { Model } = require('objection')
+
+const BaseModel = require('./base.model.js')
+
+/**
+ * Represents an instance of a bill run charge version year record
+ *
+ * For reference, the bill run charge version year record is a 'nothing' record! Certainly as far as we are concerned.
+ *
+ * They use this table when generating pre-sroc bill runs. We think it was mainly to support supplementary bill runs
+ * and the fact they cover a range of years rather than a single period.
+ *
+ * We haven't needed to do anything like this in our engine and nothing else appears to use this table. The only reason
+ * we reference it is to delete stuff when a bill run gets cancelled.
+ *
+ * Welcome to dealing with the legacy database schema! ¯\_(ツ)_/¯
+ */
+class BillRunChargeVersionYearModel extends BaseModel {
+  static get tableName () {
+    return 'billRunChargeVersionYears'
+  }
+
+  static get relationMappings () {
+    return {
+      billRun: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'bill-run.model',
+        join: {
+          from: 'billRunChargeVersionYears.billRunId',
+          to: 'billRuns.id'
+        }
+      },
+      chargeVersion: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'charge-version.model',
+        join: {
+          from: 'billRunChargeVersionYears.chargeVersionId',
+          to: 'chargeVersions.id'
+        }
+      }
+    }
+  }
+}
+
+module.exports = BillRunChargeVersionYearModel
