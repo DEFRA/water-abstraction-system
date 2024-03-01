54d15970e8d8fa3c36fa9c9bafc1c241b7cf66db
Jason Claxton <jason.claxton@defra.gov.uk>
2024-03-01 14:16:19 +0000

    Create `review-licence` helper

---
+++ test/support/helpers/review-licence.helper.js
@@ -0,0 +1,60 @@
+'use strict'
+
+/**
+ * @module ReviewLicenceHelper
+ */
+
+const { generateUUID } = require('../../../app/lib/general.lib.js')
+const { generateLicenceRef } = require('./licence.helper.js')
+const ReviewLicenceModel = require('../../../app/models/review-licence.model.js')
+
+/**
+ * Add a new review licence record for 2pt matching
+ *
+ * If no `data` is provided, default values will be used. These are
+ *
+ * - `billRunId` - [random UUID]
+ * - `licenceId` - [random UUID]
+ * - `licenceRef` - [randomly generated - 01/123]
+ * - `licenceHolder` - A LicenceHolder
+ * - `status` - ready
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ *
+ * @returns {Promise<module:ReviewLicenceModel>} The instance of the newly created record
+ */
+function add (data = {}) {
+  const insertData = defaults(data)
+
+  return ReviewLicenceModel.query()
+    .insert({ ...insertData })
+    .returning('*')
+}
+
+/**
+ * Returns the defaults used
+ *
+ * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
+ * for use in tests to avoid having to duplicate values.
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here in the database
+ */
+function defaults (data = {}) {
+  const defaults = {
+    billRunId: generateUUID(),
+    licenceId: generateUUID(),
+    licenceRef: generateLicenceRef(),
+    licenceHolder: 'A LicenceHolder',
+    status: 'ready'
+  }
+
+  return {
+    ...defaults,
+    ...data
+  }
+}
+
+module.exports = {
+  add,
+  defaults
+}
