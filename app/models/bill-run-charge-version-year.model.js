19ad11263575554b01f811771d6cac43e5e1f994
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-01 10:58:38 +0000

    Add comment to model

--- app/models/bill-run-charge-version-year.model.js
+++ app/models/bill-run-charge-version-year.model.js
@@ -9,6 +9,19 @@ const { Model } = require('objection')
 
 const BaseModel = require('./base.model.js')
 
+/**
+ * Represents an instance of a bill run charge version year record
+ *
+ * For reference, the bill run charge version year record is a 'nothing' record! Certainly as far as we are concerned.
+ *
+ * They use this table when generating pre-sroc bill runs. We think it was mainly to support supplementary bill runs
+ * and the fact they cover a range of years rather than a single period.
+ *
+ * We haven't needed to do anything like this in our engine and nothing else appears to use this table. The only reason
+ * we reference it is to delete stuff when a bill run gets cancelled.
+ *
+ * Welcome to dealing with the legacy database schema! ¯\_(ツ)_/¯
+ */
 class BillRunChargeVersionYearModel extends BaseModel {
   static get tableName () {
     return 'billRunChargeVersionYears'
