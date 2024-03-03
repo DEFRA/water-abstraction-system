21a191a91bd451b521b0835c13ad620328a7dc8b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-03 23:12:17 +0000

    Remove unused frontend macros from cancel view (#786)
    https://eaflood.atlassian.net/browse/WATER-4387
    
    We spotted a little too late that the `app/views/bill-runs/cancel.njk` view is importing macros it is not using. We know this was because of a copy and paste and doesn't break anything.
    
    Still, we don't like to make a habit of importing things and then not using them.

--- test/presenters/bill-runs/cancel-bill-run.presenter.test.js
+++ test/presenters/bill-runs/cancel-bill-run.presenter.test.js
@@ -40,10 +40,24 @@ describe('Cancel Bill Run presenter', () => {
           billRun.status = 'review'
         })
 
-        it('returns a link to the review page', () => {
-          const result = CancelBillRunPresenter.go(billRun)
+        describe('and the scheme is SROC', () => {
+          it('returns a link to the SROC review page', () => {
+            const result = CancelBillRunPresenter.go(billRun)
 
-          expect(result.backLink).to.equal('/system/bill-runs/420e948f-1992-437e-8a47-74c0066cb017/review')
+            expect(result.backLink).to.equal('/system/bill-runs/420e948f-1992-437e-8a47-74c0066cb017/review')
+          })
+        })
+
+        describe('and the scheme is PRESROC', () => {
+          beforeEach(() => {
+            billRun.scheme = 'alcs'
+          })
+
+          it('returns a link to the PRESROC review page', () => {
+            const result = CancelBillRunPresenter.go(billRun)
+
+            expect(result.backLink).to.equal('/billing/batch/420e948f-1992-437e-8a47-74c0066cb017/two-part-tariff-review')
+          })
         })
       })
 
