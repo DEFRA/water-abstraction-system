60832d9e93c01e3f847617d20e08ae234006c71b
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-09 15:24:27 +0000

    Add new LegacyRefreshBillRunRequest

---
+++ app/requests/legacy/refresh-bill-run.request.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/**
+ * Connects with the water-abstraction-service to refresh a bill run
+ * @module RefreshBillRunRequest
+ */
+
+const LegacyRequest = require('../legacy.request.js')
+
+/**
+ * Send a request to the legacy water-abstraction-service to refresh a bill run
+ *
+ * After the Charging Module API has
+ * {@link https://defra.github.io/sroc-charging-module-api-docs/#/bill-run/GenerateBillRun | generated} a bill run we
+ * need to update the data on our side with the calculated bill totals plus any additional transactions the CHA has
+ * deemed necessary.
+ *
+ * This is handled by the legacy service so we need to send a request in order to start the process.
+ *
+ * @param {string} billRunId - UUID of the bill run to refresh
+ *
+ * @returns {Promise<Object>} The result of the request; whether it succeeded and the response or error returned
+ */
+async function send (billRunId) {
+  const path = `billing/batches/${billRunId}/refresh`
+  const result = await LegacyRequest.post('water', path)
+
+  return result
+}
+
+module.exports = {
+  send
+}
---
+++ test/requests/legacy/refresh-bill-run.request.test.js
@@ -0,0 +1,104 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Things we need to stub
+const LegacyRequest = require('../../../app/requests/legacy.request.js')
+
+// Thing under test
+const RefreshBillRunRequest = require('../../../app/requests/legacy/refresh-bill-run.request.js')
+
+describe('Legacy Refresh Bill Run request', () => {
+  const billRunId = '2bbbe459-966e-4026-b5d2-2f10867bdddd'
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when the request can refresh a bill run', () => {
+    beforeEach(async () => {
+      Sinon.stub(LegacyRequest, 'post').resolves({
+        succeeded: true,
+        response: {
+          statusCode: 200,
+          body: null
+        }
+      })
+    })
+
+    it('returns a `true` success status', async () => {
+      const result = await RefreshBillRunRequest.send(billRunId)
+
+      expect(result.succeeded).to.be.true()
+    })
+
+    it('returns a 200 - ok', async () => {
+      const result = await RefreshBillRunRequest.send(billRunId)
+
+      expect(result.response.statusCode).to.equal(200)
+      expect(result.response.body).to.be.null()
+    })
+  })
+
+  describe('when the request cannot refresh a bill run', () => {
+    describe('because the request did not return a 2xx/3xx response', () => {
+      beforeEach(async () => {
+        Sinon.stub(LegacyRequest, 'post').resolves({
+          succeeded: false,
+          response: {
+            statusCode: 401,
+            body: {
+              statusCode: 401,
+              error: 'Unauthorized',
+              message: 'Invalid JWT: Token format not valid',
+              attributes: { error: 'Invalid JWT: Token format not valid' }
+            }
+          }
+        })
+      })
+
+      it('returns a `false` success status', async () => {
+        const result = await RefreshBillRunRequest.send(billRunId)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error in the `response`', async () => {
+        const result = await RefreshBillRunRequest.send(billRunId)
+
+        expect(result.response.body.statusCode).to.equal(401)
+        expect(result.response.body.error).to.equal('Unauthorized')
+        expect(result.response.body.message).to.equal('Invalid JWT: Token format not valid')
+      })
+    })
+
+    describe('because the request attempt returned an error, for example, TimeoutError', () => {
+      beforeEach(async () => {
+        Sinon.stub(LegacyRequest, 'post').resolves({
+          succeeded: false,
+          response: new Error("Timeout awaiting 'request' for 5000ms")
+        })
+      })
+
+      it('returns a `false` success status', async () => {
+        const result = await RefreshBillRunRequest.send(billRunId)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error in the `response`', async () => {
+        const result = await RefreshBillRunRequest.send(billRunId)
+
+        expect(result.response.statusCode).not.to.exist()
+        expect(result.response.body).not.to.exist()
+        expect(result.response.message).to.equal("Timeout awaiting 'request' for 5000ms")
+      })
+    })
+  })
+})
