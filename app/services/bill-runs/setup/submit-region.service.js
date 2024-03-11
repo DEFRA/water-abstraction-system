a0b12734c575ea8db334281af3105ebcac4b1df5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-11 22:53:34 +0000

    Add service to handle region submission

---
+++ app/services/bill-runs/setup/submit-region.service.js
@@ -0,0 +1,78 @@
+'use strict'
+
+/**
+ * Orchestrates validating the data for `/bill-runs/setup/{sessionId}/region` page
+ * @module SubmitRegionService
+ */
+
+const FetchRegionsService = require('./fetch-regions.service.js')
+const RegionPresenter = require('../../../presenters/bill-runs/setup/region.presenter.js')
+const RegionValidator = require('../../../validators/bill-runs/setup/region.validator.js')
+const SessionModel = require('../../../models/session.model.js')
+
+/**
+ * Orchestrates validating the data for `/bill-runs/setup/{sessionId}/region` page
+ *
+ * It first retrieves the session instance for the setup bill run journey in progress. It then validates the payload of
+ * the submitted request.
+ *
+ * If there is no validation error it will save the selected value to the session then return an object with a
+ * `setupComplete:` property. If the bill run type was not two-part tariff this will be true. Else it will be false.
+ * The fact the property exists will tell the controller the submission was successful. Whether the property
+ * `setupComplete:` is true or false will be used to determine which page to direct the user to next.
+ *
+ * If there is a validation error it is combined with the output of the presenter to generate the page data needed to
+ * re-render the view with an error message.
+ *
+ * @param {string} sessionId - The UUID of the current session
+ * @param {Object} payload - The submitted form data
+ *
+ * @returns {Promise<Object>} An object with a `setupComplete:` property if there are no errors else the page data for
+ * the region page including the validation error details
+ */
+async function go (sessionId, payload) {
+  const session = await SessionModel.query().findById(sessionId)
+  const regions = await FetchRegionsService.go()
+
+  const validationResult = _validate(payload, regions)
+
+  if (!validationResult) {
+    await _save(session, payload)
+
+    // The journey is complete (we don't need any details) if the bill run type is not 2PT
+    return { setupComplete: !session.data.type.startsWith('two_part') }
+  }
+
+  const formattedData = RegionPresenter.go(session, regions)
+
+  return {
+    error: validationResult,
+    ...formattedData
+  }
+}
+
+async function _save (session, payload) {
+  const currentData = session.data
+
+  currentData.region = payload.region
+
+  return session.$query().patch({ data: currentData })
+}
+
+function _validate (payload, regions) {
+  const validation = RegionValidator.go(payload, regions)
+
+  if (!validation.error) {
+    return null
+  }
+
+  const { message } = validation.error.details[0]
+
+  return {
+    text: message
+  }
+}
+
+module.exports = {
+  go
+}
---
+++ test/services/bill-runs/setup/submit-region.service.test.js
@@ -0,0 +1,105 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const DatabaseSupport = require('../../../support/database.js')
+const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Things we need to stub
+const FetchRegionsService = require('../../../../app/services/bill-runs/setup/fetch-regions.service.js')
+
+// Thing under test
+const SubmitRegionService = require('../../../../app/services/bill-runs/setup/submit-region.service.js')
+
+describe('Submit No Returns Required service', () => {
+  const regions = [
+    { id: 'e21b987c-7a5f-4eb3-a794-e4aae4a96a28', displayName: 'Riverlands' },
+    { id: '19a027c6-4aad-47d3-80e3-3917a4579a5b', displayName: 'Stormlands' },
+    { id: '3334054e-03b6-4696-9d74-62b8b76a3c64', displayName: 'Westerlands' }
+  ]
+
+  let payload
+  let session
+
+  beforeEach(async () => {
+    await DatabaseSupport.clean()
+
+    session = await SessionHelper.add({ data: {} })
+
+    Sinon.stub(FetchRegionsService, 'go').resolves(regions)
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when called', () => {
+    describe('with a valid payload', () => {
+      beforeEach(() => {
+        payload = {
+          region: '19a027c6-4aad-47d3-80e3-3917a4579a5b'
+        }
+      })
+
+      describe('and the bill run type was not two-part tariff', () => {
+        beforeEach(async () => {
+          session = await SessionHelper.add({ data: { type: 'annual' } })
+        })
+
+        it('saves the submitted value and returns an object confirming setup is complete', async () => {
+          const result = await SubmitRegionService.go(session.id, payload)
+
+          const refreshedSession = await session.$query()
+
+          expect(refreshedSession.data.region).to.equal('19a027c6-4aad-47d3-80e3-3917a4579a5b')
+          expect(result.setupComplete).to.be.true()
+        })
+      })
+
+      describe('and the bill run type was two-part tariff', () => {
+        beforeEach(async () => {
+          session = await SessionHelper.add({ data: { type: 'two_part_tariff' } })
+        })
+
+        it('saves the submitted value and returns an object confirming setup is not complete', async () => {
+          const result = await SubmitRegionService.go(session.id, payload)
+
+          const refreshedSession = await session.$query()
+
+          expect(refreshedSession.data.region).to.equal('19a027c6-4aad-47d3-80e3-3917a4579a5b')
+          expect(result.setupComplete).to.be.false()
+        })
+      })
+    })
+
+    describe('with an invalid payload', () => {
+      describe('because the user has not selected anything', () => {
+        beforeEach(async () => {
+          payload = {}
+
+          session = await SessionHelper.add({ data: { type: 'annual' } })
+        })
+
+        it('returns page data needed to re-render the view including the validation error', async () => {
+          const result = await SubmitRegionService.go(session.id, payload)
+
+          expect(result).to.equal({
+            sessionId: session.id,
+            regions,
+            selectedRegion: null,
+            error: {
+              text: 'Select the region'
+            }
+          })
+        })
+      })
+    })
+  })
+})
