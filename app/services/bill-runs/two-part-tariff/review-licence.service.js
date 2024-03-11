650461ec93d75c67eefb7597ce0f360ac129607d
Jason Claxton <jason.claxton@defra.gov.uk>
2024-03-11 15:36:31 +0000

    Add comment

--- app/services/bill-runs/two-part-tariff/review-licence.service.js
+++ app/services/bill-runs/two-part-tariff/review-licence.service.js
@@ -12,6 +12,7 @@ const ReviewLicencePresenter = require('../../../presenters/bill-runs/two-part-t
  * Orchestrated fetching and presenting the data needed for the licence review page
  *
  * @param {*} billRunId The UUID for the bill run
+ * @param {*} licenceId The UUID of the licence that is being reviewed
  *
  * @returns {Object} an object representing the 'pageData' needed to review the individual licence. It contains the
  * licence matched and unmatched returns and the licence charge data
