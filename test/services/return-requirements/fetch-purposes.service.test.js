728f923416819e3935defce51b2bf7d8482999a9
rvsiyad <roblesiyad123@gmail.com>
2024-03-13 15:39:22 +0000

    Altered FetchPurposes service test

--- test/services/return-requirements/fetch-purposes.service.test.js
+++ test/services/return-requirements/fetch-purposes.service.test.js
@@ -16,21 +16,24 @@ const LicenceVersionHelper = require('../../support/helpers/licence-version.help
 // Thing under test
 const FetchPurposesService = require('../../../app/services/return-requirements/fetch-purposes.service.js')
 
-describe.only('Fetch purposes service', () => {
+describe('Fetch purposes service', () => {
   let purposes
   let licenceVersion
 
   beforeEach(async () => {
     await DatabaseSupport.clean()
 
+    // Create the initial licenceVersion
     licenceVersion = await LicenceVersionHelper.add()
 
+    // Create 3 descriptions for the purposes
     purposes = await Promise.all([
       await PurposeHelper.add({ description: 'Heat Pump' }),
       await PurposeHelper.add({ description: 'Horticultural Watering' }),
       await PurposeHelper.add({ description: 'Large Garden Watering' })
     ])
 
+    // Create the licenceVersionPurposes with the purposes and licenceVersion
     for (const purpose of purposes) {
       await LicenceVersionPurposeHelper.add({
         licenceVersionId: licenceVersion.id,
@@ -39,7 +42,7 @@ describe.only('Fetch purposes service', () => {
     }
   })
 
-  describe('when called', () => {
+  describe('when called with a valid licenceId', () => {
     it('fetches the data', async () => {
       const result = await FetchPurposesService.go(licenceVersion.licenceId)
 
@@ -51,9 +54,11 @@ describe.only('Fetch purposes service', () => {
     })
   })
 
-  describe('when called and there is no licenceVersion', () => {
-    it('returns something', async () => {
-      await expect(FetchPurposesService.go('5505ca34-270a-4dfb-894c-168c8a4d6e23')).to.reject()
+  describe('when called with an invalid licenceId', () => {
+    it('returns empty result', async () => {
+      const result = await FetchPurposesService.go('5505ca34-270a-4dfb-894c-168c8a4d6e23')
+
+      expect(result).to.be.empty()
     })
   })
 })
