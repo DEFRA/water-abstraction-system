3e78a67836b791f1bb5afc607722bdc5d6fa372b
rvsiyad <roblesiyad123@gmail.com>
2024-03-13 15:40:21 +0000

    Added test cases for Purpose validator test

--- test/validators/no-returns-required/purpose.validator.test.js
+++ test/validators/no-returns-required/purpose.validator.test.js
@@ -10,18 +10,43 @@ const { expect } = Code
 // Thing under test
 const PurposeValidator = require('../../../app/validators/return-requirements/purpose.validator.js')
 
-describe.only('Purpose validator', () => {
-  const purposes = [
-    'Heat Pump',
-    'Horticultural Watering',
-    'Hydraulic Rams',
-    'Hydraulic Testing',
-    'Hydroelectric Power Generation'
-  ]
+describe('Purpose validator', () => {
   describe('when valid data is provided', () => {
+    const payload = {
+      purposes: [
+        'Heat Pump',
+        'Horticultural Watering',
+        'Hydraulic Rams',
+        'Hydraulic Testing',
+        'Hydroelectric Power Generation'
+      ]
+    }
+
     it('confirms the data is valid', () => {
-      const result = PurposeValidator.go(purposes)
-      console.log('🚀🚀🚀 ~ result:', result)
+      const result = PurposeValidator.go(payload)
+
+      expect(result.value).to.exist()
+      expect(result.error).not.to.exist()
+    })
+  })
+
+  describe('when invalid data is provided', () => {
+    const payload = {
+      purposes: [
+        'Invalid purpose',
+        'Invalid purpose',
+        'Invalid purpose',
+        'Invalid purpose',
+        'Invalid purpose'
+      ]
+    }
+
+    it('fails validation', () => {
+      const result = PurposeValidator.go(payload)
+
+      expect(result.value).to.exist()
+      expect(result.error).to.exist()
+      expect(result.error.details[0].message).to.equal('Select any uses for the return requirement')
     })
   })
 })
