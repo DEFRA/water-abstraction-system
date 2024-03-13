72f9698099bc9aedb82178d5f902f30459e1fd3f
rvsiyad <roblesiyad123@gmail.com>
2024-03-13 15:38:55 +0000

    Added additional validation check for invalid values and added JSDocs for Purpose validator

--- app/validators/return-requirements/purpose.validator.js
+++ app/validators/return-requirements/purpose.validator.js
@@ -1,9 +1,26 @@
 'use strict'
 
+/**
+ * Validates data submitted for the `/return-requirements/{sessionId}/purpose` page
+ * @module PurposeValidator
+ */
+
 const Joi = require('joi')
 
-function go (data) {
-  let purposes = data.purposes
+/**
+ * Validates data submitted for the `/return-requirements/{sessionId}/purpose` page
+ *
+ * When setting up a requirement users must specify a purpose for the return requirement.
+ * Users must select one or more purposes linked to the licence. If these requirements are not met
+ * the validation will return an error.
+ *
+ * @param {Object} payload - The payload from the request to be validated
+ *
+ * @returns {Object} the result from calling Joi's schema.validate(). If any errors are found the
+ * `error:` property will also exist detailing what the issue is.
+ */
+function go (payload) {
+  let purposes = payload.purposes
 
   if (!Array.isArray(purposes)) {
     purposes = [purposes]
@@ -15,6 +32,7 @@ function go (data) {
       .required()
       .messages({
         'any.required': 'Select any uses for the return requirement',
+        'any.only': 'Select any uses for the return requirement',
         'array.includesOne': 'Select any uses for the return requirement',
         'array.includes': 'Select any uses for the return requirement',
         'array.sparse': 'Select any uses for the return requirement'
