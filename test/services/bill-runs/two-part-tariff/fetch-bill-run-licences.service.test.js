d9b41399691cf14dd6223d502bcb6d0ec13d1194
Jason Claxton <jason.claxton@defra.gov.uk>
2024-03-14 12:41:07 +0000

    Update `fetch-bill-run-licences` tests

--- test/services/bill-runs/two-part-tariff/fetch-bill-run-licences.service.test.js
+++ test/services/bill-runs/two-part-tariff/fetch-bill-run-licences.service.test.js
@@ -32,10 +32,18 @@ describe('Fetch Bill Run Licences service', () => {
 
     describe('and there are licences in the bill run', () => {
       let testLicenceReady
+      let testLicenceReview
 
       beforeEach(async () => {
-        testLicenceReady = await ReviewLicenceHelper.add({ billRunId: billRun.id })
-        await ReviewLicenceHelper.add({ billRunId: billRun.id, status: 'review' })
+        testLicenceReady = await ReviewLicenceHelper.add({
+          billRunId: billRun.id,
+          licenceHolder: 'Ready Licence Holder Ltd'
+        })
+        testLicenceReview = await ReviewLicenceHelper.add({
+          billRunId: billRun.id,
+          licenceHolder: 'Review Licence Holder Ltd',
+          status: 'review'
+        })
       })
 
       it('returns details of the bill run and the licences in it', async () => {
@@ -47,11 +55,17 @@ describe('Fetch Bill Run Licences service', () => {
         expect(result.billRun.toFinancialYearEnding).to.equal(billRun.toFinancialYearEnding)
         expect(result.billRun.batchType).to.equal(billRun.batchType)
         expect(result.billRun.region.displayName).to.equal(region.displayName)
+        expect(result.billRun.reviewLicences[0].totalNumberOfLicences).to.equal(2)
 
         expect(result.licences).to.have.length(2)
+        expect(result.licences[0].licenceId).to.equal(testLicenceReview.licenceId)
+        expect(result.licences[0].licenceHolder).to.equal('Review Licence Holder Ltd')
+        expect(result.licences[0].licenceRef).to.equal(testLicenceReview.licenceRef)
         expect(result.licences[1].licenceId).to.equal(testLicenceReady.licenceId)
-        expect(result.licences[1].licenceHolder).to.equal('Licence Holder Ltd')
+        expect(result.licences[1].licenceHolder).to.equal('Ready Licence Holder Ltd')
         expect(result.licences[1].licenceRef).to.equal(testLicenceReady.licenceRef)
+
+        expect(result.filterLicenceHolder).to.be.undefined()
       })
 
       it("orders the licence by 'review status'", async () => {
@@ -59,6 +73,39 @@ describe('Fetch Bill Run Licences service', () => {
 
         expect(result.licences[0].status).to.equal('review')
         expect(result.licences[1].status).to.equal('ready')
+      })
+
+      describe('and a filter has been applied to the licence holder', () => {
+        let payload
+
+        beforeEach(async () => {
+          payload = { filterLicenceHolder: 'ready licence' }
+        })
+
+        it('returns details of the bill run and the licences in it', async () => {
+          const result = await FetchBillRunLicencesService.go(billRun.id, payload)
+
+          expect(result.billRun.id).to.equal(billRun.id)
+          expect(result.billRun.createdAt).to.equal(billRun.createdAt)
+          expect(result.billRun.status).to.equal(billRun.status)
+          expect(result.billRun.toFinancialYearEnding).to.equal(billRun.toFinancialYearEnding)
+          expect(result.billRun.batchType).to.equal(billRun.batchType)
+          expect(result.billRun.region.displayName).to.equal(region.displayName)
+          expect(result.billRun.reviewLicences[0].totalNumberOfLicences).to.equal(2)
+
+          expect(result.licences).to.have.length(1)
+          expect(result.licences[0].licenceId).to.equal(testLicenceReady.licenceId)
+          expect(result.licences[0].licenceHolder).to.equal('Ready Licence Holder Ltd')
+          expect(result.licences[0].licenceRef).to.equal(testLicenceReady.licenceRef)
+
+          expect(result.filterLicenceHolder).to.equal('ready licence')
+        })
+
+        it("orders the licence by 'review status'", async () => {
+          const result = await FetchBillRunLicencesService.go(billRun.id, payload)
+
+          expect(result.licences[0].status).to.equal('ready')
+        })
       })
     })
   })
