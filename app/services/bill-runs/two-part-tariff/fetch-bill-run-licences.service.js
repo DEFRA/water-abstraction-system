d007dca09409ebd41060cfdfa848d5bf572dd650
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2024-03-15 13:02:00 +0000

    Filter Licences by Licence Holder Name on the 2PT Review Licences Page (#809)
    https://eaflood.atlassian.net/browse/WATER-4190
    
    Add the functionality to be able to filter on the two-part tariff review screen by licence holder name.

--- app/services/bill-runs/two-part-tariff/fetch-bill-run-licences.service.js
+++ app/services/bill-runs/two-part-tariff/fetch-bill-run-licences.service.js
@@ -15,39 +15,46 @@ const ReviewLicenceModel = require('../../../models/review-licence.model.js')
  * ref.
  *
  * @param {String} id The UUID for the bill run
+ * @param {Object} payload The `request.payload` containing the filter data. This only contains data when there is a
+ * POST request, which only occurs when a filter is applied to the results.
  *
- * @returns {Promise<Object>} an object containing the billRun data and an array of licences for the bill run
+ * @returns {Promise<Object>} An object containing the billRun data and an array of licences for the bill run. Also
+ * included is any data that has been used to filter the results
  */
-async function go (id) {
+async function go (id, payload) {
+  const filterLicenceHolder = payload?.filterLicenceHolder
+
   const billRun = await _fetchBillRun(id)
-  const licences = await _fetchBillRunLicences(id)
+  const licences = await _fetchBillRunLicences(id, filterLicenceHolder)
 
-  return { billRun, licences }
-}
-
-async function _fetchBillRunLicences (id) {
-  return ReviewLicenceModel.query()
-    .where('billRunId', id)
-    .orderBy('status', 'desc')
+  return { billRun, licences, filterLicenceHolder }
 }
 
 async function _fetchBillRun (id) {
   return BillRunModel.query()
     .findById(id)
-    .select([
-      'id',
-      'createdAt',
-      'status',
-      'toFinancialYearEnding',
-      'batchType'
-    ])
+    .select('id', 'createdAt', 'status', 'toFinancialYearEnding', 'batchType')
     .withGraphFetched('region')
     .modifyGraph('region', (builder) => {
-      builder.select([
-        'id',
-        'displayName'
-      ])
+      builder.select('id', 'displayName')
     })
+    .withGraphFetched('reviewLicences')
+    .modifyGraph('reviewLicences', (builder) => {
+      builder.count('licenceId as totalNumberOfLicences')
+        .groupBy('billRunId')
+    })
+}
+
+async function _fetchBillRunLicences (id, filterLicenceHolder) {
+  const reviewLicenceQuery = ReviewLicenceModel.query()
+    .where('billRunId', id)
+    .orderBy('status', 'desc')
+
+  if (filterLicenceHolder) {
+    reviewLicenceQuery.whereILike('licenceHolder', `%${filterLicenceHolder}%`)
+  }
+
+  return reviewLicenceQuery
 }
 
 module.exports = {
