fb6b3569c482a057be141b4d4063576381b588f7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-19 22:24:57 +0000

    Add Legacy DeleteBillRequest
    https://eaflood.atlassian.net/browse/WATER-4416
    
    > Part of a series of changes to fix removing a bill not flagging licences for supplementary billing
    
    We're not ready to implement removing a bill from a bill run as it depends on sending requests to the [Charging Module API](https://github.com/DEFRA/sroc-charging-module-api) and running the legacy refresh job to update everything on our side.
    
    But to trigger all that we need to send a DELETE request to [water-abstraction-service](https://github.com/DEFRA/water-abstraction-service). This adds the legacy request that will deal with sending that request.

---
+++ app/requests/legacy/delete-bill.request.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/**
+ * Connects with the water-abstraction-service to delete a bill
+ * @module DeleteBillRequest
+ */
+
+const LegacyRequest = require('../legacy.request.js')
+
+/**
+ * Send a request to the legacy water-abstraction-service to delete a bill
+ *
+ * Users can remove a bill from a bill run. This requires sending a request to the charging module, then after it has
+ * regenerated the bill run refreshing the data on our side.
+ *
+ * Currently, this is all handled by the water-abstraction-service and we're not ready to migrate this until we have
+ * migrated the refresh functionality (needed by a number of operations)
+ *
+ * So, as this is handled by the legacy service we need to send the request to it once a user confirms they wish to
+ * remove the bill.
+ *
+ * @param {string} billRunId - UUID of the bill run the bill is being removed from
+ * @param {string} billId - UUID of the bill to be removed
+ * @param {module:UserModel} user - Instance representing the user that originated the request
+ *
+ * @returns {Promise<Object>} The result of the request; whether it succeeded and the response or error returned
+ */
+async function send (billRunId, billId, user) {
+  const { id: userId } = user
+  const path = `billing/batches/${billRunId}/invoices/${billId}`
+
+  return LegacyRequest.delete('water', path, userId)
+}
+
+module.exports = {
+  send
+}
