c73673aa4449922859eba76726ff8f7138b59808
Fazal <Demwunz@users.noreply.github.com>
2024-03-20 09:30:27 +0000

    Update app/services/return-requirements/check-your-answers.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/return-requirements/check-your-answers.service.js
+++ app/services/return-requirements/check-your-answers.service.js
@@ -3,14 +3,18 @@
 /**
  * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/check-your-answers` page
  * @module CheckYourAnswersService
- *
- * @param {string} id - The UUID for return requirement setup session record
- *
- * @returns {Promise<Object>} page data needed by the view template
  */
+
 const CheckYourAnswersPresenter = require('../../presenters/return-requirements/check-your-answers.presenter.js')
 const SessionModel = require('../../models/session.model.js')
 
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/check-your-answers` page
+ *
+ * @param {string} sessionId - The UUID for return requirement setup session record
+ *
+ * @returns {Promise<Object>} page data needed by the view template
+ */
 async function go (sessionId) {
   const session = await SessionModel.query().findById(sessionId)
   const formattedData = CheckYourAnswersPresenter.go(session)
