4d19dddff622c90f17f3cdf5b78413514514a2df
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-20 09:52:04 +0000

    Migrate confirm remove bill from bill run page (#837)
    https://eaflood.atlassian.net/browse/WATER-4416
    
    We'd recently spotted that the new billing views we'd developed had dropped a 'Remove licence' button. So, we used that opportunity to [Migrate confirm remove licence from bill run page](https://github.com/DEFRA/water-abstraction-system/pull/828) and [added the button back in](https://github.com/DEFRA/water-abstraction-system/pull/824).
    
    But then our conscientious QA team started testing it and found it didn't do what it claimed to do. That was to flag the removed licence for the next supplementary bill run. We display the same message on the confirm remove bill from bill run so now doubtful they tested that as well with the same result.
    
    We've [fixed the remove licence issue](https://github.com/DEFRA/water-abstraction-service/pull/2464). But whilst we fix the legacy code for remove bill run this is a great opportunity to also migrate the confirm remove bill from bill run page. Not only is it one less thing in the legacy code but we can also update the view to be consistent with others we've done like
    
    - [Migrate confirm remove licence from bill run page](https://github.com/DEFRA/water-abstraction-system/pull/828)
    - [Migrate view errored bill run page](https://github.com/DEFRA/water-abstraction-system/pull/785)
    - [Migrate view empty bill run page](https://github.com/DEFRA/water-abstraction-system/pull/783)
    - [Add cancel bill run page](https://github.com/DEFRA/water-abstraction-system/pull/780)

--- app/routes/bills.routes.js
+++ app/routes/bills.routes.js
@@ -15,6 +15,32 @@ const routes = [
         }
       }
     }
+  },
+  {
+    method: 'GET',
+    path: '/bills/{id}/remove',
+    options: {
+      handler: BillsController.remove,
+      description: 'Confirm bill should be removed from bill run page',
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/bills/{id}/remove',
+    options: {
+      handler: BillsController.submitRemove,
+      description: 'Submit confirm bill should be removed from bill run',
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
   }
 ]
 
---
+++ test/services/bills/submit-remove-bill.services.test.js
@@ -0,0 +1,52 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const BillHelper = require('../../support/helpers/bill.helper.js')
+const DatabaseSupport = require('../../support/database.js')
+
+// Things we need to stub
+const LegacyDeleteBillRequest = require('../../../app/requests/legacy/delete-bill.request.js')
+
+// Thing under test
+const SubmitRemoveBillService = require('../../../app/services/bills/submit-remove-bill.service.js')
+
+describe('Submit Remove Bill service', () => {
+  const user = { id: '0aa9dcaa-9a26-4a77-97ab-c17db54d38a1', useremail: 'carol.shaw@atari.com' }
+
+  let bill
+  let legacyDeleteBillRequestStub
+
+  beforeEach(async () => {
+    await DatabaseSupport.clean()
+
+    bill = await BillHelper.add()
+
+    legacyDeleteBillRequestStub = Sinon.stub(LegacyDeleteBillRequest, 'send').resolves()
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when called', () => {
+    it('sends a request to the legacy service to delete the bill', async () => {
+      await SubmitRemoveBillService.go(bill.id, user)
+
+      expect(legacyDeleteBillRequestStub.called).to.be.true()
+    })
+
+    it('returns the path to the legacy bill run processing page', async () => {
+      const result = await SubmitRemoveBillService.go(bill.id, user)
+
+      expect(result).to.equal(`/billing/batch/${bill.billRunId}/processing`)
+    })
+  })
+})
