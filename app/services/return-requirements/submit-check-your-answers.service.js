46e4b8cd79e7f3931d05141176abe7b75e17408f
Fazal <Demwunz@users.noreply.github.com>
2024-03-20 09:30:08 +0000

    Update app/services/return-requirements/submit-check-your-answers.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/return-requirements/submit-check-your-answers.service.js
+++ app/services/return-requirements/submit-check-your-answers.service.js
@@ -1,20 +1,27 @@
 'use strict'
+/**
+ * Manages converting the session data to return requirement records when check your answers is confirmed
+ * @module SubmitCheckYoursAnswersService
+ */
+
+const CheckLicenceEndedService = require('./check-licence-ended.service.js')
+const ExpandedError = require('../../errors/expanded.error.js')
+const SessionModel = require('../../models/session.model.js')
 
 /**
- * Orchestrates validating the data for `/return-requirements/{sessionId}/check-your-answers` page
+ * Manages converting the session data to return requirement records when check your answers is confirmed
+ * 
+ * > This service is work in progress. Some of the functionality described is yet to be implemented
+ * 
+ * After fetching the session instance for the returns requirements journey in progress it validates that what the user
+ * has setup can be persisted for the licence.
+ * 
+ * If valid it converts the session data to return requirements records then deletes the session record.
  *
- * It first retrieves the session instance for the returns requirements journey in progress. It then fetches licence data.
- * Once licence data has been retrieved we use CheckLicenceEndedService to check if it's either lapsed, expired or revoked.
- * We throw an error which will be logged caught by Hapi. The user will see the regular error page.
+ * @param {string} sessionId - The UUID for return requirement setup session record
  *
- * @param {string} sessionId - The id of the current session
- *
- * @returns {string} The licence id
+ * @returns {string} The licence ID
  */
-const CheckLicenceEndedService = require('./check-licence-ended.service.js')
-const SessionModel = require('../../models/session.model.js')
-const ExpandedError = require('../../errors/expanded.error.js')
-
 async function go (sessionId) {
   const session = await SessionModel.query().findById(sessionId)
   const licenceData = await CheckLicenceEndedService.go(session.data.licence.id)
