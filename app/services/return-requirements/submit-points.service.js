c0af0b7f17cd9e7d876190224f6e75b5a7c9cbba
rvsiyad <roblesiyad123@gmail.com>
2024-03-21 10:50:19 +0000

    Added JSDocs to SubmitPoints service

--- app/services/return-requirements/submit-points.service.js
+++ app/services/return-requirements/submit-points.service.js
@@ -10,6 +10,20 @@ const PointValidation = require('../../validators/return-requirements/point.vali
 const SelectPointsPresenter = require('../../presenters/return-requirements/points.presenter.js')
 const SessionModel = require('../../models/session.model.js')
 
+/**
+ * Orchestrates validating the data for `/return-requirements/{sessionId}/points` page
+ *
+ * It first retrieves the session instance for the returns requirements journey in progress.
+ *
+ * The user input is then validated and the result is then combined with the output of the presenter to generate the page data needed by the view.
+ * If there was a validation error the controller will re-render the page so needs this information. If all is well the
+ * controller will redirect to the next page in the journey.
+ *
+ * @param {string} sessionId - The id of the current session
+ * @param {Object} payload - The submitted form data
+ *
+ * @returns {Promise<Object>} The page data for the start date page
+ */
 async function go (sessionId, payload) {
   const session = await SessionModel.query().findById(sessionId)
 
