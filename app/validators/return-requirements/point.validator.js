689a7a8ee0b286711790f1b1442bc14018c41f96
rvsiyad <roblesiyad123@gmail.com>
2024-03-21 10:50:42 +0000

    Added JSDocs to Point validator

--- app/validators/return-requirements/point.validator.js
+++ app/validators/return-requirements/point.validator.js
@@ -7,6 +7,18 @@
 
 const Joi = require('joi')
 
+/**
+ * Validates data submitted for the `/return-requirements/{sessionId}/points` page
+ *
+ * When setting up a requirement users must specify a points for the return requirement.
+ * Users must select one or more points linked to the licence. If these requirements are not met
+ * the validation will return an error.
+ *
+ * @param {Object} payload - The payload from the request to be validated
+ *
+ * @returns {Object} The result from calling Joi's schema.validate(). If any errors are found the
+ * `error:` property will also exist detailing what the issue is.
+ */
 function go (payload) {
   let points = payload.points
 
