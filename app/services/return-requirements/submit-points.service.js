799185380de5828f467317d056f56c499bbe520b
rvsiyad <roblesiyad123@gmail.com>
2024-03-22 14:28:05 +0000

    Changed import from Point to Points and wrapped comments

--- app/services/return-requirements/submit-points.service.js
+++ app/services/return-requirements/submit-points.service.js
@@ -6,7 +6,7 @@
  */
 
 const FetchPointsService = require('../../services/return-requirements/fetch-points.service.js')
-const PointValidation = require('../../validators/return-requirements/point.validator.js')
+const PointsValidation = require('../../validators/return-requirements/points.validator.js')
 const SelectPointsPresenter = require('../../presenters/return-requirements/points.presenter.js')
 const SessionModel = require('../../models/session.model.js')
 
@@ -15,11 +15,11 @@ const SessionModel = require('../../models/session.model.js')
  *
  * It first retrieves the session instance for the returns requirements journey in progress.
  *
- * The user input is then validated and the result is then combined with the output of the presenter to generate the page data needed by the view.
- * If there was a validation error the controller will re-render the page so needs this information. If all is well the
- * controller will redirect to the next page in the journey.
+ * The user input is then validated and the result is then combined with the output of the presenter to generate the
+ * page data needed by the view. If there was a validation error the controller will re-render the page so needs this
+ * information. If all is well the controller will redirect to the next page in the journey.
  *
- * @param {string} sessionId - The id of the current session
+ * @param {string} sessionId - The UUID of the current session
  * @param {Object} payload - The submitted form data
  *
  * @returns {Promise<Object>} The page data for the start date page
@@ -40,7 +40,7 @@ async function go (sessionId, payload) {
 }
 
 function _validate (payload) {
-  const validation = PointValidation.go(payload)
+  const validation = PointsValidation.go(payload)
 
   if (!validation.error) {
     return null
