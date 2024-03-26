cb6b0d605daa5c59f6c82ad9e3318b924f1c6ed9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-26 16:18:09 +0000

    Add missing JSDoc comments for fetch service

--- app/services/bill-runs/supplementary/fetch-billing-accounts.service.js
+++ app/services/bill-runs/supplementary/fetch-billing-accounts.service.js
@@ -11,6 +11,24 @@ const BillingAccountModel = require('../../../models/billing-account.model.js')
 const ChargeVersionModel = require('../../../models/charge-version.model.js')
 const Workflow = require('../../../models/workflow.model.js')
 
+/**
+ * Fetches all billing accounts to be processed as part of supplementary billing for a region and billing period
+ *
+ * To be selected for billing a billing account must
+ *
+ * - be linked to a licence which is linked to the selected region
+ * - be linked to a licence flagged for SROC supplementary billing
+ * - not be linked to a licence in the workflow
+ * - be linked to a charge version which has the scheme 'sroc'
+ * - be linked to a charge version which has a start date before the end of the billing period
+ * - not be linked to a charge version which has a status of 'draft'
+ *
+ * @param {String} regionId - UUID of the region being billed that the licences must be linked to
+ * @param {Object} billingPeriod - Object with a `startDate` and `endDate` property representing the period being billed
+ *
+ * @returns {Promise<[module:BillingAccountModel]>} An array of `BillingAccountModel` to be billed and their relevant
+ * licence, charge version, charge element etc records needed to generate the bill run
+ */
 async function go (regionId, billingPeriod) {
   return _fetch(regionId, billingPeriod)
 }
