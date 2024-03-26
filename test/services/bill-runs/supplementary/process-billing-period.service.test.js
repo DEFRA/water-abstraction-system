764de9ac07c5b2af092fe1a0e89eeedb0887409d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-03-26 22:53:07 +0000

    Add failing scenario for ProcessBillingPeriodService
    When running the refactored engine through our acceptance tests we had one that repeatedly failed. Diving in we found that our refactoring had introduced a flaw.
    
    Because we used the updated process we'd adopted for the annual bill run engine we copied across where determining the charge period was happening.
    
    This was before we generate and process transactions for the charge version. It could be that a billing account was replaced by another prior to the billing period being processed. This would mean it's end date would be earlier than the billing period's start date resulting in an invalid charge period.
    
    For annual that means we can disregard it completely. For supplementary that just means we shouldn't bother to generate a transaction. It doesn't mean we shouldn't process the charge version and look for previous transactions which might result in a credit being raised.
    
    It means adding an involved and complex unit test. But it's a scenario we can't afford to get wrong.

--- test/services/bill-runs/supplementary/process-billing-period.service.test.js
+++ test/services/bill-runs/supplementary/process-billing-period.service.test.js
@@ -9,6 +9,7 @@ const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
+const { generateAccountNumber } = require('../../../support/helpers/billing-account.helper.js')
 const BillModel = require('../../../../app/models/bill.model.js')
 const BillRunError = require('../../../../app/errors/bill-run.error.js')
 const BillRunModel = require('../../../../app/models/bill-run.model.js')
@@ -19,6 +20,7 @@ const { determineCurrentFinancialYear } = require('../../../../app/lib/general.l
 // Things we need to stub
 const ChargingModuleCreateTransactionRequest = require('../../../../app/requests/charging-module/create-transaction.request.js')
 const GenerateTransactionsService = require('../../../../app/services/bill-runs/generate-transactions.service.js')
+const FetchPreviousTransactionsService = require('../../../../app/services/bill-runs/supplementary/fetch-previous-transactions.service.js')
 
 // Thing under test
 const ProcessBillingPeriodService = require('../../../../app/services/bill-runs/supplementary/process-billing-period.service.js')
@@ -31,7 +33,9 @@ describe('Supplementary Process Billing Period service', () => {
   }
 
   let billingAccount
+  let otherBillingAccount
   let chargingModuleCreateTransactionRequestStub
+  let fetchPreviousTransactionsServiceStub
 
   beforeEach(async () => {
     // NOTE: Although we don't rely on the helpers to create the data we pass into the service it does persist the
@@ -54,9 +58,9 @@ describe('Supplementary Process Billing Period service', () => {
       })
     })
 
-    describe('and there are billing accounts to process', () => {
+    describe('and there is a billing account to process', () => {
       beforeEach(async () => {
-        billingAccount = _testBillingAccount()
+        billingAccount = _billingAccount()
 
         chargingModuleCreateTransactionRequestStub.onFirstCall().resolves({
           ..._chargingModuleResponse('7e752fa6-a19c-4779-b28c-6e536f028795')
@@ -73,9 +77,9 @@ describe('Supplementary Process Billing Period service', () => {
           // linked to the same licence.
           // But we also want coverage for charge versions that do not have a status of 'current'. These are processed
           // by the service though only for any previous transactions associated with them.
-          const supersededChargeVersion = _testChargeVersion(billingAccount.id)
+          const supersededChargeVersion = _chargeVersion(billingAccount.id)
           supersededChargeVersion.status = 'superseded'
-          billingAccount.chargeVersions = [supersededChargeVersion, _testChargeVersion(billingAccount.id), _testChargeVersion(billingAccount.id)]
+          billingAccount.chargeVersions = [supersededChargeVersion, _chargeVersion(billingAccount.id), _chargeVersion(billingAccount.id)]
         })
 
         it('returns true (bill run is not empty)', async () => {
@@ -90,15 +94,15 @@ describe('Supplementary Process Billing Period service', () => {
           // Create a charge version with an abstraction period that starts in June but which is linked to a licence
           // that was revoked at the start of May. The engine should calculate 0 billable days and therefore not attempt
           // to create a bill licence for this record.
-          const unbillableChargeVersion = _testChargeVersion(billingAccount.id)
+          const unbillableChargeVersion = _chargeVersion(billingAccount.id)
           unbillableChargeVersion.licence.id = 'c3726e99-935e-4a36-ab2f-eef8bda9293a'
           unbillableChargeVersion.licence.revokedDate = new Date(billingPeriod.startDate.getFullYear(), 4, 1)
           unbillableChargeVersion.chargeReferences[0].chargeElements[0].abstractionPeriodStartDay = 1
           unbillableChargeVersion.chargeReferences[0].chargeElements[0].abstractionPeriodStartMonth = 6
 
           billingAccount.chargeVersions = [
-            _testChargeVersion(billingAccount.id),
-            _testChargeVersion(billingAccount.id),
+            _chargeVersion(billingAccount.id),
+            _chargeVersion(billingAccount.id),
             unbillableChargeVersion
           ]
         })
@@ -121,7 +125,7 @@ describe('Supplementary Process Billing Period service', () => {
 
       describe('but they are not billable', () => {
         beforeEach(async () => {
-          const chargeVersion = _testChargeVersion(billingAccount.id)
+          const chargeVersion = _chargeVersion(billingAccount.id)
 
           // We update the billing account's charge information so that the engine calculates a charge period that
           // starts after the abstraction period i.e. nothing to bill
@@ -138,12 +142,87 @@ describe('Supplementary Process Billing Period service', () => {
         })
       })
     })
+
+    describe('and there are multiple billing accounts to process', () => {
+      const fixedBillingPeriod = { startDate: new Date('2023-04-01'), endDate: new Date('2024-03-31') }
+
+      beforeEach(() => {
+        billingAccount = _billingAccount()
+        otherBillingAccount = _billingAccount()
+      })
+
+      describe('for the same licence', () => {
+        beforeEach(() => {
+          // NOTE: The scenario we are recreating here is the same as change-billing-account-current-year.cy.js in our
+          // acceptance tests. A billing account initially had 1 charge version starting on 2022-04-01 and a supp. bill
+          // is generated. This creates a debit for each billing period from 2022-2023 onwards.
+          //
+          // It then adds a new charge version with the same details, but for a new billing account starting from
+          // 2023-04-01. Another supp. bill is generated. The result should be
+          //
+          // - No bill for 2022-2023. We'll calculate a debit for the same amount which is cancelled out by the previous
+          //   transaction
+          // - A credit bill for Account A for 2023-2024. We won't calculate a debit but will find the previous
+          //   transaction which when reversed will result in a credit
+          // - A debit for Account B for 2023-2024. We calculate a debit and find no previous transactions
+          //
+          // This example is based on doing this in billing period 2023-2024. We've specifically added this example
+          // because it was the one acceptance test that found a flaw after refactoring ProcessBillingPeriodService.
+          // Next time we intend to catch any flaws sooner!
+          const chargeVersionForAccountOne = _chargeVersion(billingAccount.id)
+          chargeVersionForAccountOne.endDate = new Date('2023-03-31')
+          billingAccount.chargeVersions = [chargeVersionForAccountOne]
+
+          const chargeVersionForAccountTwo = _chargeVersion(otherBillingAccount.id)
+          chargeVersionForAccountTwo.startDate = new Date('2023-04-01')
+          otherBillingAccount.chargeVersions = [chargeVersionForAccountTwo]
+
+          fetchPreviousTransactionsServiceStub = Sinon.stub(FetchPreviousTransactionsService, 'go')
+          fetchPreviousTransactionsServiceStub.onFirstCall().resolves([_previousTransaction()])
+          fetchPreviousTransactionsServiceStub.onSecondCall().resolves([])
+
+          chargingModuleCreateTransactionRequestStub.onFirstCall().resolves({
+            ..._chargingModuleResponse('7e752fa6-a19c-4779-b28c-6e536f028795')
+          })
+          chargingModuleCreateTransactionRequestStub.onSecondCall().resolves({
+            ..._chargingModuleResponse('a2086da4-e3b6-4b83-afe1-0e2e5255efaf')
+          })
+        })
+
+        it('creates a credit transaction for the billing account previously billed, and a debit for the new account', async () => {
+          const result = await ProcessBillingPeriodService.go(billRun, fixedBillingPeriod, [billingAccount, otherBillingAccount])
+
+          // Confirm we billed something first!
+          expect(result).to.be.true()
+
+          // Check the billing account that was replaced at a later date by the other billing account has a credit
+          let matchingBills = await _fetchBills(billingAccount.id)
+
+          expect(matchingBills).to.have.length(1)
+          expect(matchingBills[0].billLicences).to.have.length(1)
+          expect(matchingBills[0].billLicences[0].transactions).to.have.length(1)
+          expect(matchingBills[0].billLicences[0].transactions[0].credit).to.be.true()
+          expect(matchingBills[0].billLicences[0].transactions[0].startDate).to.equal(new Date('2023-04-01'))
+          expect(matchingBills[0].billLicences[0].transactions[0].endDate).to.equal(new Date('2024-03-31'))
+
+          // Check the billing account that replaced the original billing account has a debit
+          matchingBills = await _fetchBills(otherBillingAccount.id)
+
+          expect(matchingBills).to.have.length(1)
+          expect(matchingBills[0].billLicences).to.have.length(1)
+          expect(matchingBills[0].billLicences[0].transactions).to.have.length(1)
+          expect(matchingBills[0].billLicences[0].transactions[0].credit).to.be.false()
+          expect(matchingBills[0].billLicences[0].transactions[0].startDate).to.equal(new Date('2023-04-01'))
+          expect(matchingBills[0].billLicences[0].transactions[0].endDate).to.equal(new Date('2024-03-31'))
+        })
+      })
+    })
   })
 
   describe('when the service errors', () => {
     beforeEach(async () => {
-      billingAccount = _testBillingAccount()
-      billingAccount.chargeVersions = [_testChargeVersion(billingAccount.id)]
+      billingAccount = _billingAccount()
+      billingAccount.chargeVersions = [_chargeVersion(billingAccount.id)]
     })
 
     describe('because generating the calculated transactions fails', () => {
@@ -178,6 +257,13 @@ describe('Supplementary Process Billing Period service', () => {
   })
 })
 
+function _billingAccount () {
+  return {
+    id: generateUUID(),
+    accountNumber: generateAccountNumber()
+  }
+}
+
 function _chargingModuleResponse (transactionId) {
   return {
     succeeded: true,
@@ -187,14 +273,7 @@ function _chargingModuleResponse (transactionId) {
   }
 }
 
-function _testBillingAccount () {
-  return {
-    id: '973a5852-9c06-4c14-b1e2-d32b88d3e878',
-    accountNumber: 'T71117364A'
-  }
-}
-
-function _testChargeVersion (billingAccountId) {
+function _chargeVersion (billingAccountId) {
   return {
     id: generateUUID(),
     scheme: 'sroc',
@@ -226,11 +305,11 @@ function _testChargeVersion (billingAccountId) {
         id: generateUUID(),
         source: 'non-tidal',
         loss: 'low',
-        volume: '6.819',
+        volume: '100',
         adjustments: {
-          s126: null, s127: false, s130: false, charge: null, winter: false, aggregate: '0.562114443'
+          s126: null, s127: false, s130: false, charge: null, winter: false, aggregate: '1'
         },
-        additionalCharges: { isSupplyPublicWater: true },
+        additionalCharges: { isSupplyPublicWater: false },
         description: 'Mineral washing',
         chargeCategory: {
           id: 'cc4a66da-efdb-4882-b18a-daf000131349',
@@ -251,5 +330,60 @@ function _testChargeVersion (billingAccountId) {
         ]
       }
     ]
+  }
+}
+
+async function _fetchBills (billingAccountId) {
+  return BillModel.query()
+    .where('billingAccountId', billingAccountId)
+    .withGraphFetched('billLicences')
+    .modifyGraph('billLicences', (builder) => {
+      builder.select([
+        'id'
+      ])
+    })
+    .withGraphFetched('billLicences.transactions')
+    .modifyGraph('billLicences.transactions', (builder) => {
+      builder.select([
+        'id',
+        'startDate',
+        'endDate',
+        'credit'
+      ])
+    })
+}
+
+function _previousTransaction () {
+  return {
+    authorisedDays: 244,
+    billableDays: 244,
+    waterUndertaker: false,
+    chargeReferenceId: '74bf53b8-06ca-4477-a3ce-be1318c9dfbb',
+    startDate: new Date('2023-04-01'),
+    endDate: new Date('2024-03-31'),
+    source: 'non-tidal',
+    season: 'all year',
+    loss: 'low',
+    credit: false,
+    chargeType: 'standard',
+    authorisedQuantity: 100,
+    billableQuantity: 100,
+    description: 'Water abstraction charge: SROC Charge Element 02',
+    volume: 100,
+    section126Factor: 1,
+    section127Agreement: false,
+    secondPartCharge: false,
+    scheme: 'sroc',
+    aggregateFactor: 1,
+    adjustmentFactor: 1,
+    chargeCategoryCode: '4.4.1',
+    chargeCategoryDescription: 'Low loss, non-tidal, up to and including 5,000 ML/yr',
+    supportedSource: false,
+    supportedSourceName: null,
+    newLicence: false,
+    waterCompanyCharge: false,
+    winterOnly: false,
+    purposes: [{}],
+    section130Agreement: false
   }
 }
