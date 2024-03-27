d3a8668d2f56e75e25aefb503ba7be3d84dc0575
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2024-03-27 15:46:49 +0000

    Filter licences by issue on the 2PT Review Licences Page (#842)
    https://eaflood.atlassian.net/browse/WATER-4218
    
    Add a filter to the two-part tariff Review Licences page to allow the user to filter by license issues.

--- app/lib/static-lookups.lib.js
+++ app/lib/static-lookups.lib.js
@@ -28,10 +28,25 @@ const sources = [
   'wrls'
 ]
 
+const twoPartTariffReviewIssues = {
+  'abs-outside-period': 'Abstraction outside period',
+  'aggregate-factor': 'Aggregate',
+  'checking-query': 'Checking query',
+  'no-returns-received': 'No returns received',
+  'over-abstraction': 'Over abstraction',
+  'overlap-of-charge-dates': 'Overlap of charge dates',
+  'returns-received-not-processed': 'Returns received but not processed',
+  'returns-late': 'Returns received late',
+  'return-split-over-refs': 'Return split over charge references',
+  'some-returns-not-received': 'Some returns not received',
+  'unable-to-match-return': 'Unable to match return'
+}
+
 module.exports = {
   billRunTypes,
   companyTypes,
   contactTypes,
   organisationTypes,
-  sources
+  sources,
+  twoPartTariffReviewIssues
 }
