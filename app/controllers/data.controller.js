9df40fac5768bd412e55cd44243a84fdf621dd6c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-04-04 15:02:38 +0100

    Add new endpoints to /data controller

--- app/controllers/data.controller.js
+++ app/controllers/data.controller.js
@@ -6,12 +6,35 @@
  */
 
 const SeedService = require('../services/data/seed/seed.service.js')
+const SubmitDeduplicateService = require('../services/data/deduplicate/submit-deduplicate.service.js')
 const TearDownService = require('../services/data/tear-down/tear-down.service.js')
+
+async function deduplicate (_request, h) {
+  return h.view('data/deduplicate.njk', {
+    pageTitle: 'De-duplicate a licence',
+    activeNavBar: 'search'
+  })
+}
 
 async function seed (_request, h) {
   await SeedService.go()
 
   return h.response().code(204)
+}
+
+async function submitDeduplicate (request, h) {
+  const pageData = await SubmitDeduplicateService.go(request.payload)
+  console.log('🚀 ~ submitDeduplicate ~ pageData:', pageData)
+
+  if (pageData.error) {
+    return h.view('data/deduplicate.njk', {
+      pageTitle: 'De-duplicate a licence',
+      activeNavBar: 'search',
+      ...pageData
+    })
+  }
+
+  return h.redirect(`/licences?query=${pageData.licenceRef}`)
 }
 
 async function tearDown (_request, h) {
@@ -21,6 +44,8 @@ async function tearDown (_request, h) {
 }
 
 module.exports = {
+  deduplicate,
   seed,
+  submitDeduplicate,
   tearDown
 }
