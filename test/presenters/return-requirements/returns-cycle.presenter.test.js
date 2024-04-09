501f1980f74e3f35b72d68879701bb43500bc729
rvsiyad <roblesiyad123@gmail.com>
2024-04-09 13:25:12 +0100

    Added ReturnsCycle presenter test

---
+++ test/presenters/return-requirements/returns-cycle.presenter.test.js
@@ -0,0 +1,70 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Thing under test
+const ReturnsCyclePresenter = require('../../../app/presenters/return-requirements/returns-cycle.presenter.js')
+
+describe('Returns Cycle presenter', () => {
+  describe('when provided with a populated session', () => {
+    describe('and no returns cycle is session data', () => {
+      const session = {
+        id: '61e07498-f309-4829-96a9-72084a54996d',
+        data: {
+          licence: {
+            id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+            endDate: null,
+            licenceRef: '01/ABC',
+            licenceHolder: 'Turbo Kid',
+            startDate: '2022-04-01T00:00:00.000Z'
+          }
+        }
+      }
+
+      it('correctly presents the data', () => {
+        const result = ReturnsCyclePresenter.go(session)
+
+        expect(result).to.equal({
+          id: '61e07498-f309-4829-96a9-72084a54996d',
+          licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+          licenceRef: '01/ABC',
+          returnsCycle: null
+        })
+      })
+    })
+
+    describe('and with returns cycle in session data', () => {
+      const session = {
+        id: '61e07498-f309-4829-96a9-72084a54996d',
+        data: {
+          licence: {
+            id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+            endDate: null,
+            licenceRef: '01/ABC',
+            licenceHolder: 'Turbo Kid',
+            startDate: '2022-04-01T00:00:00.000Z'
+          },
+          returnsCycle: 'summer'
+        }
+      }
+
+      it('correctly presents the data', () => {
+        const result = ReturnsCyclePresenter.go(session)
+
+        expect(result).to.equal({
+          id: '61e07498-f309-4829-96a9-72084a54996d',
+          licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+          licenceRef: '01/ABC',
+          returnsCycle: 'summer'
+        })
+      })
+    })
+  })
+})
