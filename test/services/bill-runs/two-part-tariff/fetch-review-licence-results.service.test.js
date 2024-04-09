295880f8a8cec10019069b4883071323181ee65c
Jason Claxton <jason.claxton@defra.gov.uk>
2024-04-09 12:03:33 +0100

    Fix broken test

--- test/services/bill-runs/two-part-tariff/fetch-review-licence-results.service.test.js
+++ test/services/bill-runs/two-part-tariff/fetch-review-licence-results.service.test.js
@@ -119,6 +119,7 @@ describe('Fetch Review Licence Results Service', () => {
           status: reviewLicence.status,
           createdAt: reviewLicence.createdAt,
           updatedAt: reviewLicence.updatedAt,
+          markProgress: false,
           reviewReturns: [{
             id: reviewReturn.id,
             reviewLicenceId: reviewReturn.reviewLicenceId,
