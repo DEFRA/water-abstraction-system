dcf3150dff08b7c66475936c26867425990de63d
rvsiyad <roblesiyad123@gmail.com>
2024-04-09 13:23:35 +0100

    Altered ReturnCycle presenter to use session.data.returnsCycle

--- app/presenters/return-requirements/returns-cycle.presenter.js
+++ app/presenters/return-requirements/returns-cycle.presenter.js
@@ -13,30 +13,15 @@
  *
  * @returns {Object} - The data formatted for the view template
  */
-function go (session, payload = {}) {
+function go (session) {
   const data = {
     id: session.id,
     licenceId: session.data.licence.id,
     licenceRef: session.data.licence.licenceRef,
-    returnsCycle: _licenceReturnsCycle(payload)
+    returnsCycle: session.data.returnsCycle ? session.data.returnsCycle : null
   }
 
   return data
-}
-
-function _licenceReturnsCycle (payload) {
-  // NOTE: 'returnsCycle' is the payload value that tells us whether the user has selected a returns cycle
-  // for the return requirement.
-  // If it is not set then it is because the presenter has been called from 'ReturnsCycleService' and it's the first
-  // load. Else it has been called by the 'SubmitReturnsCycleService' and the user has not selected a returns cycle.
-  // Either way, we use it to tell us wether there is anything in the payload worth transforming.
-  const returnsCycle = payload.returnsCycle
-
-  if (!returnsCycle) {
-    return null
-  }
-
-  return returnsCycle
 }
 
 module.exports = {
