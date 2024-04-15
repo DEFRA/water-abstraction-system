ac201d278449e2ee338938e3d6167657d6bfb2b9
rvsiyad <roblesiyad123@gmail.com>
2024-04-15 09:02:50 +0100

    Updated ReturnRequirements controller test to handle FrequencyCollected service

--- test/controllers/return-requirements.controller.test.js
+++ test/controllers/return-requirements.controller.test.js
@@ -10,6 +10,7 @@ const { expect } = Code
 
 // Things we need to stub
 const CheckYourAnswersService = require('../../app/services/return-requirements/check-your-answers.service.js')
+const FrequencyCollectedService = require('../../app/services/return-requirements/frequency-collected.service.js')
 const NoReturnsRequiredService = require('../../app/services/return-requirements/no-returns-required.service.js')
 const PointsService = require('../../app/services/return-requirements/points.service.js')
 const SelectPurposeService = require('../../app/services/return-requirements/purpose.service.js')
@@ -113,6 +114,12 @@ describe('Return requirements controller', () => {
   })
 
   describe('GET /return-requirements/{sessionId}/frequency-collected', () => {
+    beforeEach(async () => {
+      Sinon.stub(FrequencyCollectedService, 'go').resolves({
+        id: '8702b98f-ae51-475d-8fcc-e049af8b8d38', pageTitle: 'Select how often readings or volumes are collected'
+      })
+    })
+
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
         const response = await server.inject(_options('frequency-collected'))
