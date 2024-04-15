b6594aeda43df30ba437b2517eb3a3acd8bb1f83
rvsiyad <roblesiyad123@gmail.com>
2024-04-15 12:49:14 +0100

    Updated ReturnRequirement controller test to use FrequencyReported service

--- test/controllers/return-requirements.controller.test.js
+++ test/controllers/return-requirements.controller.test.js
@@ -10,6 +10,7 @@ const { expect } = Code
 
 // Things we need to stub
 const CheckYourAnswersService = require('../../app/services/return-requirements/check-your-answers.service.js')
+const FrequencyReportedService = require('../../app/services/return-requirements/frequency-reported.service.js')
 const NoReturnsRequiredService = require('../../app/services/return-requirements/no-returns-required.service.js')
 const PointsService = require('../../app/services/return-requirements/points.service.js')
 const SelectPurposeService = require('../../app/services/return-requirements/purpose.service.js')
@@ -124,12 +125,18 @@ describe('Return requirements controller', () => {
   })
 
   describe('GET /return-requirements/{sessionId}/frequency-reported', () => {
+    beforeEach(async () => {
+      Sinon.stub(FrequencyReportedService, 'go').resolves({
+        id: '8702b98f-ae51-475d-8fcc-e049af8b8d38', pageTitle: 'Select how often readings or volumes are reported'
+      })
+    })
+
     describe('when the request succeeds', () => {
       it('returns the page successfully', async () => {
         const response = await server.inject(_options('frequency-reported'))
 
         expect(response.statusCode).to.equal(200)
-        expect(response.payload).to.contain('Select how often collected readings or volumes are reported')
+        expect(response.payload).to.contain('Select how often readings or volumes are reported')
       })
     })
   })
