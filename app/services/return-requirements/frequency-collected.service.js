0ff342290b97bf284af0fc9f8388260e377ac015
rvsiyad <roblesiyad123@gmail.com>
2024-04-17 12:17:45 +0100

    Moved imports between comments

--- app/services/return-requirements/frequency-collected.service.js
+++ app/services/return-requirements/frequency-collected.service.js
@@ -5,19 +5,19 @@
  * @module FrequencyCollectedService
  */
 
+const FrequencyCollectedPresenter = require('../../presenters/return-requirements/frequency-collected.presenter.js')
+const SessionModel = require('../../models/session.model.js')
+
 /**
  * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/frequency-collected` page
  *
- * Supports generating the data needed for the frequency collected page in the return requirements setup journey. It fetches
- * the current session record and combines it with the radio buttons and other information needed for the form.
+ * Supports generating the data needed for the frequency collected page in the return requirements setup journey. It
+ * fetches the current session record and combines it with the radio buttons and other information needed for the form.
  *
  * @param {string} sessionId - The UUID of the current session
  *
  * @returns {Promise<Object>} The view data for the frequency collected page
 */
-const FrequencyCollectedPresenter = require('../../presenters/return-requirements/frequency-collected.presenter.js')
-const SessionModel = require('../../models/session.model.js')
-
 async function go (sessionId) {
   const session = await SessionModel.query().findById(sessionId)
 
