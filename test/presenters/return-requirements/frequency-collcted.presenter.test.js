353302ddd345ede539461dc389f87271b59f68a2
rvsiyad <roblesiyad123@gmail.com>
2024-04-17 12:18:53 +0100

    Used beforeEach and let for session variables

--- test/presenters/return-requirements/frequency-collcted.presenter.test.js
+++ test/presenters/return-requirements/frequency-collcted.presenter.test.js
@@ -4,28 +4,32 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Thing under test
 const FrequencyCollectedPresenter = require('../../../app/presenters/return-requirements/frequency-collected.presenter.js')
 
 describe('Frequency Collected presenter', () => {
+  let session
+
   describe('when provided with a populated session', () => {
     describe('and no returns cycle is session data', () => {
-      const session = {
-        id: '61e07498-f309-4829-96a9-72084a54996d',
-        data: {
-          licence: {
-            id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-            currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-            endDate: null,
-            licenceRef: '01/ABC',
-            licenceHolder: 'Turbo Kid',
-            startDate: '2022-04-01T00:00:00.000Z'
+      beforeEach(() => {
+        session = {
+          id: '61e07498-f309-4829-96a9-72084a54996d',
+          data: {
+            licence: {
+              id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+              currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+              endDate: null,
+              licenceRef: '01/ABC',
+              licenceHolder: 'Turbo Kid',
+              startDate: '2022-04-01T00:00:00.000Z'
+            }
           }
         }
-      }
+      })
 
       it('correctly presents the data', () => {
         const result = FrequencyCollectedPresenter.go(session)
@@ -40,20 +44,22 @@ describe('Frequency Collected presenter', () => {
     })
 
     describe('and with returns cycle in session data', () => {
-      const session = {
-        id: '61e07498-f309-4829-96a9-72084a54996d',
-        data: {
-          licence: {
-            id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-            currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-            endDate: null,
-            licenceRef: '01/ABC',
-            licenceHolder: 'Turbo Kid',
-            startDate: '2022-04-01T00:00:00.000Z'
-          },
-          frequencyCollected: 'weekly'
+      beforeEach(() => {
+        session = {
+          id: '61e07498-f309-4829-96a9-72084a54996d',
+          data: {
+            licence: {
+              id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+              currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+              endDate: null,
+              licenceRef: '01/ABC',
+              licenceHolder: 'Turbo Kid',
+              startDate: '2022-04-01T00:00:00.000Z'
+            },
+            frequencyCollected: 'weekly'
+          }
         }
-      }
+      })
 
       it('correctly presents the data', () => {
         const result = FrequencyCollectedPresenter.go(session)
