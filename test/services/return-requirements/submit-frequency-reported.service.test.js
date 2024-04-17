06c76dd71decd53d729a8f456fdc3f105e73b361
rvsiyad <roblesiyad123@gmail.com>
2024-04-17 12:43:16 +0100

    Fixed uncapitalised R

--- test/services/return-requirements/submit-frequency-reported.service.test.js
+++ test/services/return-requirements/submit-frequency-reported.service.test.js
@@ -14,7 +14,7 @@ const SessionHelper = require('../../support/helpers/session.helper.js')
 // Thing under test
 const SubmitFrequencyReportedService = require('../../../app/services/return-requirements/submit-frequency-reported.service.js')
 
-describe('Submit Frequency reported service', () => {
+describe('Submit Frequency Reported service', () => {
   let session
 
   beforeEach(async () => {
