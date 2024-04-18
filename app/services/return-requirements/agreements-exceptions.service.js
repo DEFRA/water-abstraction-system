5a69fa1e90ff34892e8df0c85da729e75420ffd9
rvsiyad <roblesiyad123@gmail.com>
2024-04-18 14:07:22 +0100

    Added AgreementsExceptions service

---
+++ app/services/return-requirements/agreements-exceptions.service.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/agreements-exceptions` page
+ * @module AgreementExceptionService
+ */
+
+const AgreementsExceptionsPresenter = require('../../presenters/return-requirements/agreements-exceptions.presenter.js')
+const SessionModel = require('../../models/session.model.js')
+
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/agreements-exceptions` page
+ *
+ * Supports generating the data needed for the agreements and exceptions page in the return requirements setup journey. It
+ * fetches the current session record and combines it with the date fields and other information needed for the form.
+ *
+ * @param {string} sessionId - The UUID of the current session
+ *
+ * @returns {Promise<Object>} The view data for the agreements and exceptions page
+*/
+async function go (sessionId) {
+  const session = await SessionModel.query().findById(sessionId)
+  const formattedData = AgreementsExceptionsPresenter.go(session)
+
+  return {
+    activeNavBar: 'search',
+    pageTitle: 'Select agreements and exceptions for the requirements for returns',
+    ...formattedData
+  }
+}
+
+module.exports = {
+  go
+}
