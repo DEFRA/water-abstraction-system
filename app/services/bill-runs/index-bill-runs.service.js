0e8966e84c4ce88e72dbfc90ce4a4de192846c95
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-04-18 16:16:10 +0100

    Add orchestration service

---
+++ app/services/bill-runs/index-bill-runs.service.js
@@ -0,0 +1,63 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data needed for the /bill-runs page
+ * @module IndexBillRunsService
+ */
+
+const CheckBusyBillRunsService = require('./check-busy-bill-runs.service.js')
+const FetchBillRunsService = require('./fetch-bill-runs.service.js')
+const IndexBillRunsPresenter = require('../../presenters/bill-runs/index-bill-runs.presenter.js')
+const PaginatorPresenter = require('../../presenters/paginator.presenter.js')
+
+/**
+ * Orchestrates fetching and presenting the data needed for the /bill-runs page
+ *
+ * @param {string} page - the page number of bill runs to be viewed
+ *
+ * @returns {Promise<Object>} an object representing the `pageData` needed by the index bill run template. It contains
+ * summary details for each bill run for the page selected, for the templates pagination control, the title and the
+ * status of busy bill runs
+ */
+async function go (page) {
+  const selectedPageNumber = _selectedPageNumber(page)
+
+  // We expect the FetchBillRunsService to take longer to complete than CheckBusyBillRunsService. But running them
+  // together means we are only waiting as long as it takes rather than the combined time to run both queries
+  const [fetchedBillRunResult, busyResult] = await Promise.all([
+    FetchBillRunsService.go(selectedPageNumber),
+    CheckBusyBillRunsService.go()
+  ])
+
+  const billRuns = IndexBillRunsPresenter.go(fetchedBillRunResult.results)
+  const pagination = PaginatorPresenter.go(fetchedBillRunResult.total, selectedPageNumber)
+
+  const pageTitle = _pageTitle(pagination.numberOfPages, selectedPageNumber)
+
+  return {
+    billRuns,
+    busy: busyResult,
+    pageTitle,
+    pagination
+  }
+}
+
+function _pageTitle (numberOfPages, selectedPageNumber) {
+  if (numberOfPages === 1) {
+    return 'Bill runs'
+  }
+
+  return `Bill runs (page ${selectedPageNumber} of ${numberOfPages})`
+}
+
+function _selectedPageNumber (page) {
+  if (!page) {
+    return 1
+  }
+
+  return Number(page)
+}
+
+module.exports = {
+  go
+}
