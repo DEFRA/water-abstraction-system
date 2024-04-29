67fef6ea669226f898f9d858b8387a8a7967bdb6
Roble <roblesiyad123@gmail.com>
2024-04-29 16:02:51 +0100

    Fixed AbstractionPeriod service to combine existing session data with the submitted payload

--- app/services/return-requirements/submit-abstraction-period.service.js
+++ app/services/return-requirements/submit-abstraction-period.service.js
@@ -34,13 +34,13 @@ async function go (sessionId, payload) {
     return {}
   }
 
-  const formattedData = AbstractionPeriodPresenter.go(session, payload)
+  const submittedSessionData = _submittedSessionData(session, payload)
 
   return {
     activeNavBar: 'search',
     error: validationResult,
     pageTitle: 'Enter the abstraction period for the requirements for returns',
-    ...formattedData
+    ...submittedSessionData
   }
 }
 
@@ -50,6 +50,16 @@ async function _save (session, payload) {
   currentData.abstractionPeriod = payload
 
   return session.$query().patch({ data: currentData })
+}
+
+/**
+ * Combines the existing session data with the submitted payload formatted by the presenter. If nothing is submitted by
+ * the user, payload will be an empty object.
+ */
+function _submittedSessionData (session, payload) {
+  session.data.abstractionPeriod = Object.keys(payload).length > 0 ? payload : null
+
+  return AbstractionPeriodPresenter.go(session)
 }
 
 function _validate (payload) {
