3d0e2644eef8bf520ed20bcd72e0c53d33ecfe54
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-05-01 15:17:17 +0100

    Refactor View License Summary services and presenters (#957)
    * Refactor View License Summary services and presenters
    
    Extract the common view license data (data not in the View License tabs) into it's owm service and presenter.
    
    Use the new view license and view license summary functionality called from the View License Summary Controller.
    
    Optimise the fetch calls to get licence data per service requirement.
    
    * Ignore .webstorm folder for custom run configurations

---
+++ app/services/licences/view-license-summary.service.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data needed for the licence summary page
+ * @module ViewLicenceSummaryService
+ */
+
+const FetchLicenceAbstractionConditionsService = require('./fetch-licence-abstraction-conditions.service.js')
+const FetchLicenceSummaryService = require('./fetch-license-summary.service')
+const ViewLicenceSummaryPresenter = require('../../presenters/licences/view-license-summary.presenter')
+const ViewLicenceService = require('./view-licence.service')
+
+/**
+ * Orchestrates fetching and presenting the data needed for the licence summary page
+ *
+ * @param {string} licenceId - The UUID of the licence
+ *
+ * @returns {Promise<Object>} an object representing the `pageData` needed by the licence summary template.
+ */
+async function go (licenceId, auth) {
+  const commonData = await ViewLicenceService.go(licenceId, auth)
+
+  const summaryLicenceData = await FetchLicenceSummaryService.go(licenceId)
+
+  const currentLicenceVersionId = summaryLicenceData?.licenceVersions[0]?.id
+
+  const licenceAbstractionConditions = await FetchLicenceAbstractionConditionsService.go(currentLicenceVersionId)
+
+  const pageData = ViewLicenceSummaryPresenter.go(summaryLicenceData, licenceAbstractionConditions)
+
+  return {
+    ...pageData,
+    ...commonData
+  }
+}
+
+module.exports = {
+  go
+}
