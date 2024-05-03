467dbf55f3584d51de2271a2f2155e15e6e15397
jonathangoulding <jonathangoulding21@gmail.com>
2024-05-03 17:23:45 +0100

    fix: reorder license presenter functions alpha

--- app/presenters/licences/view-licence-returns.presenter.js
+++ app/presenters/licences/view-licence-returns.presenter.js
@@ -27,24 +27,24 @@ function _formatPurpose (purpose) {
   return firstPurpose.alias ? firstPurpose.alias : firstPurpose.tertiary.description
 }
 
+function _formatReturnToTableRow (returns) {
+  return returns.map((r) => {
+    return {
+      dates: `${formatLongDate(new Date(r.startDate))} to ${formatLongDate(new Date(r.endDate))}`,
+      description: r.metadata.description,
+      dueDate: formatLongDate(new Date(r.dueDate)),
+      id: r.id,
+      purpose: _formatPurpose(r.metadata.purposes),
+      reference: r.returnReference,
+      status: _formatStatus(r.status)
+    }
+  })
+}
+
 function _formatStatus (status) {
   if (status === 'completed') return 'COMPLETE'
   if (status === 'due') return 'OVERDUE'
   return 'NO STATUS'
-}
-
-function _formatReturnToTableRow (returns) {
-  return returns.map((r) => {
-    return {
-      id: r.id,
-      reference: r.returnReference,
-      purpose: _formatPurpose(r.metadata.purposes),
-      description: r.metadata.description,
-      dueDate: formatLongDate(new Date(r.dueDate)),
-      status: _formatStatus(r.status),
-      dates: `${formatLongDate(new Date(r.startDate))} to ${formatLongDate(new Date(r.endDate))}`
-    }
-  })
 }
 
 module.exports = {
