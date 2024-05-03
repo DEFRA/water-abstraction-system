43d3d92fad53c34129a8e87c30bf1e82f8424d95
Rebecca Ransome <beckyrose200@aol.com>
2024-05-03 14:51:06 +0100

    Refactor the charge reference service

--- app/services/bill-runs/two-part-tariff/charge-reference-details.service.js
+++ app/services/bill-runs/two-part-tariff/charge-reference-details.service.js
@@ -6,8 +6,8 @@
  * @module ChargeReferenceDetailsService
  */
 
-const FetchReviewChargeReferenceService = require('./fetch-review-charge-reference.service.js')
 const ChargeReferenceDetailsPresenter = require('../../../presenters/bill-runs/two-part-tariff/charge-reference-details.presenter.js')
+const FetchReviewChargeReferenceService = require('./fetch-review-charge-reference.service.js')
 
 /**
  * Orchestrates fetching and presenting the data needed for the charge reference details page in a two-part tariff bill
@@ -17,11 +17,14 @@ const ChargeReferenceDetailsPresenter = require('../../../presenters/bill-runs/t
  * @param {String} licenceId - The UUID of the licence that is being reviewed
  * @param {String} reviewChargeReferenceId - The UUID of the charge reference being reviewed
  *
- * @returns {Promise<Object>} the 'pageData' needed for the review licence page. It contains the licence, bill run,
- * matched and unmatched returns and the licence charge data
+ * @returns {Promise<Object>} the 'pageData' needed for the review charge reference page. It contains details of the
+ * bill run, charge reference and the charge adjustments
  */
 async function go (billRunId, licenceId, reviewChargeReferenceId) {
-  const { billRun, reviewChargeReference } = await FetchReviewChargeReferenceService.go(billRunId, reviewChargeReferenceId)
+  const {
+    billRun,
+    reviewChargeReference
+  } = await FetchReviewChargeReferenceService.go(billRunId, reviewChargeReferenceId)
 
   const pageData = ChargeReferenceDetailsPresenter.go(billRun, reviewChargeReference, licenceId)
 
