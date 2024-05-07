13e72dfa9c5f3aa1b6556e211b90a89d0d99b5a6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-07 22:22:00 +0100

    Fix and refactor start date presenter

--- app/presenters/return-requirements/start-date.presenter.js
+++ app/presenters/return-requirements/start-date.presenter.js
@@ -15,15 +15,25 @@ const { formatLongDate } = require('../base.presenter.js')
  * @returns {Object} The data formatted for the view template
  */
 function go (session) {
-  const { id: sessionId, licence } = session
+  const {
+    id: sessionId,
+    licence,
+    startDateOptions,
+    startDateDay,
+    startDateMonth,
+    startDateYear
+  } = session
 
   return {
+    anotherStartDateDay: startDateDay ?? null,
+    anotherStartDateMonth: startDateMonth ?? null,
+    anotherStartDateYear: startDateYear ?? null,
     backLink: _backLink(session),
     licenceId: licence.id,
     licenceRef: licence.licenceRef,
-    licenceVersionStartDate: _licenceVersionStartDate(licence.currentVersionStartDate),
+    licenceVersionStartDate: _licenceVersionStartDate(licence),
     sessionId,
-    ..._transformSession(session)
+    startDateOption: startDateOptions ?? null
   }
 }
 
@@ -37,40 +47,14 @@ function _backLink (session) {
   return `/licences/${licence.id}#charge`
 }
 
-function _licenceVersionStartDate (date) {
+function _licenceVersionStartDate (licence) {
+  const { currentVersionStartDate } = licence
+
   // NOTE: because the session data is stored in a JSONB field when we get the instance from the DB the date values are
   // in JS Date format (string). So, we have to convert it to a date before calling `formatLongDate()`
-  const dateObj = new Date(date)
-  const formattedDate = formatLongDate(dateObj)
+  const dateObj = new Date(currentVersionStartDate)
 
-  return formattedDate
-}
-
-function _transformSession (session) {
-  // NOTE: 'startDateOptions' is the session value that tells us whether the user selected the licence version start
-  // date or another date radio button.
-  //
-  // If it is not set then either it's because the presenter has been called from `StartDateService` and it's the first
-  // load. Else its been called by `SubmitStartDateService` but the user hasn't selected a radio button.
-  //
-  // Either way, we use it to tell us whether there is anything in the session worth transforming.
-  const selectedOption = session.startDateOptions
-
-  if (!selectedOption) {
-    return {
-      anotherStartDateDay: null,
-      anotherStartDateMonth: null,
-      anotherStartDateYear: null,
-      selectedOption: null
-    }
-  }
-
-  return {
-    anotherStartDateDay: session.startDateDay,
-    anotherStartDateMonth: session.startDateMonth,
-    anotherStartDateYear: session.startDateYear,
-    selectedOption
-  }
+  return formatLongDate(dateObj)
 }
 
 module.exports = {
--- test/presenters/return-requirements/start-date.presenter.test.js
+++ test/presenters/return-requirements/start-date.presenter.test.js
@@ -11,74 +11,106 @@ const { expect } = Code
 const StartDatePresenter = require('../../../app/presenters/return-requirements/start-date.presenter.js')
 
 describe('Start Date presenter', () => {
-  describe('when provided with an unpopulated session', () => {
-    let session
+  let session
 
-    beforeEach(async () => {
-      session = {
-        id: 'd3fr4-f3ad-4cb6-a058-78abc4w4t3r',
-        licence: {
-          id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-          currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-          endDate: null,
-          licenceRef: '01/ABC',
-          licenceHolder: 'Turbo Kid',
-          startDate: '2023-11-126T00:00:00.000Z'
-        },
-        selectedOption: null
+  beforeEach(async () => {
+    session = {
+      id: '61e07498-f309-4829-96a9-72084a54996d',
+      licence: {
+        id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+        currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+        endDate: null,
+        licenceRef: '01/ABC',
+        licenceHolder: 'Turbo Kid',
+        startDate: '2023-11-126T00:00:00.000Z'
       }
-    })
+    }
+  })
 
+  describe('when provided with a session', () => {
     it('correctly presents the data', () => {
       const result = StartDatePresenter.go(session)
 
       expect(result).to.equal({
-        id: session.id,
-        licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-        licenceRef: '01/ABC',
-        licenceVersionStartDate: '1 January 2023',
         anotherStartDateDay: null,
         anotherStartDateMonth: null,
         anotherStartDateYear: null,
-        selectedOption: null
-      }, { skip: ['id'] })
-    })
-  })
-
-  describe('when provided with a populated session', () => {
-    let session
-
-    beforeEach(async () => {
-      session = {
-        id: 'd3fr4-f3ad-4cb6-a058-78abc4w4t3r',
-        licence: {
-          id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-          currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-          endDate: null,
-          licenceRef: '01/ABC',
-          licenceHolder: 'Turbo Kid',
-          startDate: '2023-11-126T00:00:00.000Z'
-        },
-        startDateDay: '26',
-        startDateMonth: '11',
-        startDateYear: '2023',
-        startDateOptions: 'anotherStartDate'
-      }
-    })
-
-    it('correctly presents the data', () => {
-      const result = StartDatePresenter.go(session)
-
-      expect(result).to.equal({
-        id: session.id,
+        backLink: '/licences/8b7f78ba-f3ad-4cb6-a058-78abc4d1383d#charge',
         licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
         licenceRef: '01/ABC',
         licenceVersionStartDate: '1 January 2023',
-        anotherStartDateDay: '26',
-        anotherStartDateMonth: '11',
-        anotherStartDateYear: '2023',
-        selectedOption: 'anotherStartDate'
-      }, { skip: ['id'] })
+        sessionId: '61e07498-f309-4829-96a9-72084a54996d',
+        startDateOption: null
+      })
+    })
+  })
+
+  describe("the 'backLink' property", () => {
+    describe("when the user has come from 'check your answers'", () => {
+      beforeEach(() => {
+        session.checkYourAnswersVisited = true
+      })
+
+      it("returns a link back to the 'check your answers' page", () => {
+        const result = StartDatePresenter.go(session)
+
+        expect(result.backLink).to.equal('/system/return-requirements/61e07498-f309-4829-96a9-72084a54996d/check-your-answers')
+      })
+    })
+
+    describe('when the user has come from somewhere else', () => {
+      it("returns a link back to the licence page's charge tab", () => {
+        const result = StartDatePresenter.go(session)
+
+        expect(result.backLink).to.equal('/licences/8b7f78ba-f3ad-4cb6-a058-78abc4d1383d#charge')
+      })
+    })
+  })
+
+  describe("the 'licenceVersionStartDate' property", () => {
+    it('returns the licence start date in long date format', () => {
+      const result = StartDatePresenter.go(session)
+
+      expect(result.licenceVersionStartDate).to.equal('1 January 2023')
+    })
+  })
+
+  describe("the 'startDate' properties", () => {
+    describe('when the user has previously selected the licence start date as the start date', () => {
+      beforeEach(() => {
+        session.startDateOptions = 'licenceStartDate'
+      })
+
+      it("returns the 'startDateOption' property populated to re-select the option", () => {
+        const result = StartDatePresenter.go(session)
+
+        const { anotherStartDateDay, anotherStartDateMonth, anotherStartDateYear, startDateOption } = result
+
+        expect(anotherStartDateDay).to.be.null()
+        expect(anotherStartDateMonth).to.be.null()
+        expect(anotherStartDateYear).to.be.null()
+        expect(startDateOption).to.equal('licenceStartDate')
+      })
+    })
+
+    describe('when the user has previously selected another date as the start date', () => {
+      beforeEach(() => {
+        session.startDateDay = '26'
+        session.startDateMonth = '11'
+        session.startDateYear = '2023'
+        session.startDateOptions = 'anotherStartDate'
+      })
+
+      it('returns the properties needed to re-populate the fields', () => {
+        const result = StartDatePresenter.go(session)
+
+        const { anotherStartDateDay, anotherStartDateMonth, anotherStartDateYear, startDateOption } = result
+
+        expect(anotherStartDateDay).to.equal('26')
+        expect(anotherStartDateMonth).to.equal('11')
+        expect(anotherStartDateYear).to.equal('2023')
+        expect(startDateOption).to.equal('anotherStartDate')
+      })
     })
   })
 })
