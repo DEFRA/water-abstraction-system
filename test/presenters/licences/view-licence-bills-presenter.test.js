2ed0e52b0daf43a3464e6f93cee1e9c21d7fe1d6
jonathangoulding <jonathangoulding21@gmail.com>
2024-05-07 10:20:37 +0100

    fix: linting

--- test/presenters/licences/view-licence-bills-presenter.test.js
+++ test/presenters/licences/view-licence-bills-presenter.test.js
@@ -4,19 +4,13 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { describe, it } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Thing under test
 const ViewLicenceBillsPresenter = require('../../../app/presenters/licences/view-licence-bills.presenter')
 
 describe('View Licence Bills presenter', () => {
-  let auth
-
-  beforeEach(() => {
-    auth = undefined
-  })
-
   describe('when provided with a bills data', () => {
     it('correctly presents the data', () => {
       const result = ViewLicenceBillsPresenter.go(_bills())
