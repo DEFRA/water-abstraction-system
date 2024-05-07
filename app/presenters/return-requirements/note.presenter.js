4f8f6308e35c23900610bdce27467e81a61556d9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-07 09:33:34 +0100

    Housekeeping - corrections for the note page

--- app/presenters/return-requirements/note.presenter.js
+++ app/presenters/return-requirements/note.presenter.js
@@ -5,16 +5,22 @@
  * @module NotePresenter
  */
 
+/**
+ * Formats data for the `/return-requirements/{sessionId}/note` page
+ *
+ * @param {module:SessionModel} session - The returns requirements session instance
+ *
+ * @returns {Object} The data formatted for the view template
+ */
 function go (session) {
-  const { id, note } = session
+  const { id: sessionId, licence, note } = session
 
-  const data = {
-    id,
-    licenceRef: session.licence.licenceRef,
-    note: note ? note.content : ''
+  return {
+    backLink: `/system/return-requirements/${sessionId}/check-your-answers`,
+    licenceRef: licence.licenceRef,
+    note: note ? note.content : '',
+    sessionId
   }
-
-  return data
 }
 
 module.exports = {
