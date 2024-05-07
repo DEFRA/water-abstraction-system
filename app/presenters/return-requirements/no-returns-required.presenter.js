286f1de4e5fa78ed903b3bef546407a5f85c2f28
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-07 09:25:08 +0100

    Housekeeping - corrections for no-returns-required

--- app/presenters/return-requirements/no-returns-required.presenter.js
+++ app/presenters/return-requirements/no-returns-required.presenter.js
@@ -5,14 +5,32 @@
  * @module NoReturnsRequiredPresenter
  */
 
+/**
+ * Formats data for the `/return-requirements/{sessionId}/no-returns-required` page
+ *
+ * @param {module:SessionModel} session - The returns requirements session instance
+ *
+ * @returns {Object} - The data formatted for the view template
+ */
 function go (session) {
-  const data = {
-    selectedOption: session.reason || null,
-    id: session.id,
-    licenceRef: session.licence.licenceRef
+  const { id: sessionId, licence, reason } = session
+
+  return {
+    backLink: _backLink(session),
+    licenceRef: licence.licenceRef,
+    selectedOption: reason || null,
+    sessionId
+  }
+}
+
+function _backLink (session) {
+  const { checkYourAnswersVisited, id } = session
+
+  if (checkYourAnswersVisited) {
+    return `/system/return-requirements/${id}/check-your-answers`
   }
 
-  return data
+  return `/system/return-requirements/${id}/start-date`
 }
 
 module.exports = {
