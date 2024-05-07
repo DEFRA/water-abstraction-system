2740706014ffd6d1395fafe872e1fed6fa9f9754
Roble <roblesiyad123@gmail.com>
2024-05-07 15:40:24 +0100

    Updated presenter for CancelReturnRequirements

--- app/presenters/return-requirements/cancel-requirements.presenter.js
+++ app/presenters/return-requirements/cancel-requirements.presenter.js
@@ -5,6 +5,8 @@
  * @module CancelRequirementsPresenter
 */
 
+const { formatLongDate } = require('../base.presenter.js')
+
 /**
  * Formats data for the `/return-requirements/{sessionId}/cancel-requirements` page
  *
@@ -13,14 +15,48 @@
  *
  * @returns {Object} - The data formatted for the view template
  */
-function go (session, payload = {}) {
+function go (session) {
   const data = {
     id: session.id,
     licenceId: session.data.licence.id,
-    licenceRef: session.data.licence.licenceRef
+    licenceRef: session.data.licence.licenceRef,
+    reason: session.data.reason,
+    startDate: _startDate(session.data),
+    returnRequirements: _formattedReturnRequirement(session.data) ? _formattedReturnRequirement(session.data) : null
   }
 
   return data
+}
+
+function _formattedReturnRequirement (sessionData) {
+  if (!sessionData.returnsCycle) {
+    return null
+  }
+
+  let returnsCycle = sessionData.returnsCycle.charAt(0).toUpperCase() + sessionData.returnsCycle.slice(1)
+  returnsCycle = returnsCycle.replaceAll('-', ' ')
+
+  const { frequencyReported } = sessionData
+  const { siteDescription } = sessionData
+
+  return `${returnsCycle} ${frequencyReported} requirements for returns, ${siteDescription}.`
+}
+
+function _startDate (sessionData) {
+  const selectedOption = sessionData.startDateOptions
+  let date
+
+  if (selectedOption === 'licenceStartDate') {
+    date = new Date(sessionData.licence.currentVersionStartDate)
+  } else {
+    const day = sessionData.startDateDay
+    const month = sessionData.startDateMonth
+    const year = sessionData.startDateYear
+
+    date = new Date(`${year}-${month}-${day}`)
+  }
+
+  return formatLongDate(date)
 }
 
 module.exports = {
