3dfa166e686e2a95a1c0fbced0a5d281e381818c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-07 22:42:45 +0100

    Fix - reason presenter tests

--- test/presenters/return-requirements/reason.presenter.test.js
+++ test/presenters/return-requirements/reason.presenter.test.js
@@ -8,30 +8,80 @@ const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Thing under test
-const SelectReasonPresenter = require('../../../app/presenters/return-requirements/reason.presenter.js')
+const ReasonPresenter = require('../../../app/presenters/return-requirements/reason.presenter.js')
 
-describe('Select Reason presenter', () => {
+describe('Reason presenter', () => {
   let session
 
   beforeEach(() => {
     session = {
-      id: 'f1288f6c-8503-4dc1-b114-75c408a14bd0',
+      id: '61e07498-f309-4829-96a9-72084a54996d',
       licence: {
-        id: 'ea53bfc6-740d-46c5-9558-fc8cabfc6c1f',
-        licenceRef: '01/123',
-        licenceHolder: 'Jane Doe'
-      }
+        id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+        currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+        endDate: null,
+        licenceRef: '01/ABC',
+        licenceHolder: 'Turbo Kid',
+        startDate: '2022-04-01T00:00:00.000Z'
+      },
+      requirements: [{}],
+      checkYourAnswersVisited: false
     }
   })
 
-  describe('when provided with a populated session', () => {
+  describe('when provided with a session', () => {
     it('correctly presents the data', () => {
-      const result = SelectReasonPresenter.go(session)
+      const result = ReasonPresenter.go(session)
 
       expect(result).to.equal({
-        id: 'f1288f6c-8503-4dc1-b114-75c408a14bd0',
-        licenceRef: '01/123',
-        reason: null
+        backLink: '/system/return-requirements/61e07498-f309-4829-96a9-72084a54996d/start-date',
+        licenceRef: '01/ABC',
+        reason: null,
+        sessionId: '61e07498-f309-4829-96a9-72084a54996d'
+      })
+    })
+  })
+
+  describe("the 'backLink' property", () => {
+    describe("when the user has come from 'check your answers'", () => {
+      beforeEach(() => {
+        session.checkYourAnswersVisited = true
+      })
+
+      it("returns a link back to the 'check your answers' page", () => {
+        const result = ReasonPresenter.go(session)
+
+        expect(result.backLink).to.equal('/system/return-requirements/61e07498-f309-4829-96a9-72084a54996d/check-your-answers')
+      })
+    })
+
+    describe('when the user has come from somewhere else', () => {
+      it("returns a link back to the 'start-date' page", () => {
+        const result = ReasonPresenter.go(session)
+
+        expect(result.backLink).to.equal('/system/return-requirements/61e07498-f309-4829-96a9-72084a54996d/start-date')
+      })
+    })
+  })
+
+  describe("the 'reason' property", () => {
+    describe('when the user has previously submitted a reason', () => {
+      beforeEach(() => {
+        session.reason = 'major-change'
+      })
+
+      it('returns a populated reason', () => {
+        const result = ReasonPresenter.go(session)
+
+        expect(result.reason).to.equal('major-change')
+      })
+    })
+
+    describe('when the user has not previously submitted a reason', () => {
+      it('returns an empty reason', () => {
+        const result = ReasonPresenter.go(session)
+
+        expect(result.reason).to.be.null()
       })
     })
   })
