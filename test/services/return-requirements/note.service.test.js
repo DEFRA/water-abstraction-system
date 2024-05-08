2799ac9eb64dec853e22e7773f07a13ce4dc37d1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-08 16:47:58 +0100

    Fix - note service tests

--- test/services/return-requirements/note.service.test.js
+++ test/services/return-requirements/note.service.test.js
@@ -19,9 +19,9 @@ describe('Note service', () => {
 
   beforeEach(async () => {
     await DatabaseSupport.clean()
+
     session = await SessionHelper.add({
       data: {
-        checkYourAnswersVisited: true,
         licence: {
           id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
           currentVersionStartDate: '2023-01-01T00:00:00.000Z',
@@ -29,7 +29,9 @@ describe('Note service', () => {
           licenceRef: '01/ABC',
           licenceHolder: 'Turbo Kid',
           startDate: '2022-04-01T00:00:00.000Z'
-        }
+        },
+        requirements: [{}],
+        checkYourAnswersVisited: false
       }
     })
   })
@@ -38,20 +40,19 @@ describe('Note service', () => {
     it('fetches the current setup session record', async () => {
       const result = await NoteService.go(session.id)
 
-      expect(result.id).to.equal(session.id)
+      expect(result.sessionId).to.equal(session.id)
     })
 
     it('returns page data for the view', async () => {
       const result = await NoteService.go(session.id)
 
       expect(result).to.equal({
-        id: session.id,
         activeNavBar: 'search',
-        checkYourAnswersVisited: true,
         pageTitle: 'Add a note',
+        backLink: `/system/return-requirements/${session.id}/check-your-answers`,
         licenceRef: '01/ABC',
-        note: ''
-      })
+        note: null
+      }, { skip: ['sessionId'] })
     })
   })
 })
