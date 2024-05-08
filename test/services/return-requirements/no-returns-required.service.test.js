227f7bc67d5a0f1031bba1ec4b3e73eb1c35aeb7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-08 16:44:11 +0100

    Fix - no returns required service

--- test/services/return-requirements/no-returns-required.service.test.js
+++ test/services/return-requirements/no-returns-required.service.test.js
@@ -14,33 +14,33 @@ const SessionHelper = require('../../support/helpers/session.helper.js')
 // Thing under test
 const NoReturnsRequiredService = require('../../../app/services/return-requirements/no-returns-required.service.js')
 
-const sessionData = {
-  data: {
-    checkYourAnswersVisited: false,
-    licence: {
-      id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-      currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-      endDate: null,
-      licenceRef: '01/ABC',
-      licenceHolder: 'Turbo Kid',
-      startDate: '2022-04-01T00:00:00.000Z'
-    }
-  }
-}
-
 describe('No Returns Required service', () => {
   let session
 
-  describe('when called', () => {
-    beforeEach(async () => {
-      await DatabaseSupport.clean()
-      session = await SessionHelper.add({ ...sessionData })
-    })
+  beforeEach(async () => {
+    await DatabaseSupport.clean()
 
+    session = await SessionHelper.add({
+      data: {
+        licence: {
+          id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+          currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+          endDate: null,
+          licenceRef: '01/ABC',
+          licenceHolder: 'Turbo Kid',
+          startDate: '2022-04-01T00:00:00.000Z'
+        },
+        requirements: [{}],
+        checkYourAnswersVisited: false
+      }
+    })
+  })
+
+  describe('when called', () => {
     it('fetches the current setup session record', async () => {
       const result = await NoReturnsRequiredService.go(session.id)
 
-      expect(result.id).to.equal(session.id)
+      expect(result.sessionId).to.equal(session.id)
     })
 
     it('returns page data for the view', async () => {
@@ -48,25 +48,11 @@ describe('No Returns Required service', () => {
 
       expect(result).to.equal({
         activeNavBar: 'search',
-        checkYourAnswersVisited: false,
         pageTitle: 'Why are no returns required?',
+        backLink: `/system/return-requirements/${session.id}/start-date`,
         licenceRef: '01/ABC',
-        selectedOption: null
-      }, { skip: ['id'] })
-    })
-  })
-
-  describe('when the user has visited check-your-answers', () => {
-    beforeEach(async () => {
-      await DatabaseSupport.clean()
-      sessionData.data.checkYourAnswersVisited = true
-      session = await SessionHelper.add({ ...sessionData })
-    })
-
-    it('redirects back to check-your-answers', async () => {
-      const result = await NoReturnsRequiredService.go(session.id)
-
-      expect(result.checkYourAnswersVisited).to.be.true()
+        reason: null
+      }, { skip: ['sessionId'] })
     })
   })
 })
