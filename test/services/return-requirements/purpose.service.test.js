8b61fa6183b434e60b9793dd8c7f05afe4ea051a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-08 13:08:14 +0100

    Fix - purpose service tests

--- test/services/return-requirements/purpose.service.test.js
+++ test/services/return-requirements/purpose.service.test.js
@@ -19,6 +19,8 @@ const FetchPurposesService = require('../../../app/services/return-requirements/
 const PurposeService = require('../../../app/services/return-requirements/purpose.service.js')
 
 describe('Purpose service', () => {
+  const requirementIndex = 0
+
   let session
 
   beforeEach(async () => {
@@ -33,13 +35,15 @@ describe('Purpose service', () => {
           licenceRef: '01/ABC',
           licenceHolder: 'Turbo Kid',
           startDate: '2022-04-01T00:00:00.000Z'
-        }
+        },
+        requirements: [{}],
+        checkYourAnswersVisited: false
       }
     })
 
     Sinon.stub(FetchPurposesService, 'go').resolves([
-      'Transfer Between Sources (Pre Water Act 2003)',
-      'Potable Water Supply - Direct'
+      { description: 'Transfer Between Sources (Pre Water Act 2003)' },
+      { description: 'Potable Water Supply - Direct' }
     ])
   })
 
@@ -49,25 +53,26 @@ describe('Purpose service', () => {
 
   describe('when called', () => {
     it('fetches the current setup session record', async () => {
-      const result = await PurposeService.go(session.id)
+      const result = await PurposeService.go(session.id, requirementIndex)
 
-      expect(result.id).to.equal(session.id)
+      expect(result.sessionId).to.equal(session.id)
     })
 
     it('returns page data for the view', async () => {
-      const result = await PurposeService.go(session.id)
+      const result = await PurposeService.go(session.id, requirementIndex)
 
       expect(result).to.equal({
         activeNavBar: 'search',
         pageTitle: 'Select the purpose for the requirements for returns',
-        licenceRef: '01/ABC',
+        backLink: `/system/return-requirements/${session.id}/setup`,
         licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
         licencePurposes: [
           'Transfer Between Sources (Pre Water Act 2003)',
           'Potable Water Supply - Direct'
         ],
-        selectedPurposes: ''
-      }, { skip: ['id'] })
+        licenceRef: '01/ABC',
+        purposes: ''
+      }, { skip: ['sessionId'] })
     })
   })
 })
