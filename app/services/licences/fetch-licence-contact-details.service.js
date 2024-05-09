d4cfa8e232bf7e196875c5eb7ad6be982a90ab30
jonathangoulding <jonathangoulding21@gmail.com>
2024-05-09 11:27:53 +0100

    chore: spelling

--- app/services/licences/fetch-licence-contact-details.service.js
+++ app/services/licences/fetch-licence-contact-details.service.js
@@ -9,11 +9,11 @@ const LicenceDocumentRoleModel = require('../../models/licence-document-role.mod
 const LicenceModel = require('../../models/licence.model')
 
 /**
- * Fetches all bills for a licence which is needed for the view '/licences/{id}/contact-details` page
+ * Fetches all contact details for a licence which is needed for the view '/licences/{id}/contact-details` page
  *
  * @param {string} licenceId - The UUID for the licence to fetch
  *
- * @returns {Promise<Object>} the data needed to populate the view licence page's bills tab
+ * @returns {Promise<Object>} the data needed to populate the view licence page's contact details tab
  */
 async function go (licenceId) {
   const { licenceContacts } = await _fetch(licenceId)
