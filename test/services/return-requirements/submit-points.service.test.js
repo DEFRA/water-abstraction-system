aa0bec8e8aeafcdaa89a5d874952f45dc56f6113
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-09 08:47:26 +0100

    Fix - submit points service tests

--- test/services/return-requirements/submit-points.service.test.js
+++ test/services/return-requirements/submit-points.service.test.js
@@ -14,12 +14,13 @@ const SessionHelper = require('../../support/helpers/session.helper.js')
 
 // Things we need to stub
 const FetchPointsService = require('../../../app/services/return-requirements/fetch-points.service.js')
-const PointsValidator = require('../../../app/validators/return-requirements/points.validator.js')
 
 // Thing under test
 const SubmitPointsService = require('../../../app/services/return-requirements/submit-points.service.js')
 
 describe('Submit Points service', () => {
+  const requirementIndex = 0
+
   let payload
   let session
 
@@ -28,7 +29,6 @@ describe('Submit Points service', () => {
 
     session = await SessionHelper.add({
       data: {
-        checkYourAnswersVisited: false,
         licence: {
           id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
           currentVersionStartDate: '2023-01-01T00:00:00.000Z',
@@ -37,7 +37,8 @@ describe('Submit Points service', () => {
           licenceHolder: 'Turbo Kid',
           startDate: '2022-04-01T00:00:00.000Z'
         },
-        journey: 'returns-required'
+        requirements: [{}],
+        checkYourAnswersVisited: false
       }
     })
   })
@@ -55,23 +56,21 @@ describe('Submit Points service', () => {
           ]
         }
 
-        Sinon.stub(FetchPointsService, 'go').resolves(_testFetchPointsService())
-
-        Sinon.stub(PointsValidator, 'go').resolves(null)
+        Sinon.stub(FetchPointsService, 'go').resolves(_points())
       })
 
       it('saves the submitted value', async () => {
-        await SubmitPointsService.go(session.id, payload)
+        await SubmitPointsService.go(session.id, requirementIndex, payload)
 
         const refreshedSession = await session.$query()
 
-        expect(refreshedSession.points).to.equal([
+        expect(refreshedSession.requirements[0].points).to.equal([
           'At National Grid Reference TQ 69212 50394 (RIVER MEDWAY AT YALDING INTAKE)'
         ])
       })
 
       it('returns the checkYourAnswersVisited property (no page data needed for a redirect)', async () => {
-        const result = await SubmitPointsService.go(session.id, payload)
+        const result = await SubmitPointsService.go(session.id, requirementIndex, payload)
 
         expect(result).to.equal({
           checkYourAnswersVisited: false
@@ -81,38 +80,31 @@ describe('Submit Points service', () => {
   })
 
   describe('with a invalid payload', () => {
-    describe('because the user has not selected anything', () => {
-      beforeEach(() => {
-        payload = {}
+    beforeEach(() => {
+      payload = {}
 
-        Sinon.stub(FetchPointsService, 'go').resolves(_testFetchPointsService())
-      })
+      Sinon.stub(FetchPointsService, 'go').resolves(_points())
+    })
 
-      it('fetches the current setup session record', async () => {
-        const result = await SubmitPointsService.go(session.id, payload)
+    it('returns page data for the view', async () => {
+      const result = await SubmitPointsService.go(session.id, requirementIndex, payload)
 
-        expect(result.id).to.equal(session.id)
-      })
+      expect(result).to.equal({
+        activeNavBar: 'search',
+        pageTitle: 'Select the points for the requirements for returns',
+        backLink: `/system/return-requirements/${session.id}/purpose/0`,
+        licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+        licencePoints: [
+          'At National Grid Reference TQ 69212 50394 (RIVER MEDWAY AT YALDING INTAKE)'
+        ],
+        licenceRef: '01/ABC',
+        points: ''
+      }, { skip: ['sessionId', 'error'] })
+    })
 
-      it('returns page data for the view', async () => {
-        const result = await SubmitPointsService.go(session.id, payload)
-
-        expect(result).to.equal({
-          activeNavBar: 'search',
-          checkYourAnswersVisited: false,
-          pageTitle: 'Select the points for the requirements for returns',
-          licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-          licenceRef: '01/ABC',
-          licencePoints: [
-            'At National Grid Reference TQ 69212 50394 (RIVER MEDWAY AT YALDING INTAKE)',
-            'At National Grid Reference TQ 68083 33604 (BEWL WATER RESERVOIR)'
-          ],
-          selectedPoints: ''
-        }, { skip: ['id', 'error'] })
-      })
-
-      it('returns page data with an error', async () => {
-        const result = await SubmitPointsService.go(session.id, payload)
+    describe('because the user has not submitted anything', () => {
+      it('includes an error for the input element', async () => {
+        const result = await SubmitPointsService.go(session.id, requirementIndex, payload)
 
         expect(result.error).to.equal({
           text: 'Select any points for the return requirement'
@@ -122,7 +114,7 @@ describe('Submit Points service', () => {
   })
 })
 
-function _testFetchPointsService () {
+function _points () {
   return [
     {
       NGR1_EAST: '69212',
@@ -131,21 +123,6 @@ function _testFetchPointsService () {
       NGR4_EAST: 'null',
       LOCAL_NAME: 'RIVER MEDWAY AT YALDING INTAKE',
       NGR1_NORTH: '50394',
-      NGR1_SHEET: 'TQ',
-      NGR2_NORTH: 'null',
-      NGR2_SHEET: 'null',
-      NGR3_NORTH: 'null',
-      NGR3_SHEET: 'null',
-      NGR4_NORTH: 'null',
-      NGR4_SHEET: 'null'
-    },
-    {
-      NGR1_EAST: '68083',
-      NGR2_EAST: 'null',
-      NGR3_EAST: 'null',
-      NGR4_EAST: 'null',
-      LOCAL_NAME: 'BEWL WATER RESERVOIR',
-      NGR1_NORTH: '33604',
       NGR1_SHEET: 'TQ',
       NGR2_NORTH: 'null',
       NGR2_SHEET: 'null',
