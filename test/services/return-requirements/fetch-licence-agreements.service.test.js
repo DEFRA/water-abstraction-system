20132c29cb4eecf950a725d53a3c76aaf7e645ca
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-05-09 23:10:14 +0100

    Fixup fetch financial agreements service

--- test/services/return-requirements/fetch-licence-agreements.service.test.js
+++ test/services/return-requirements/fetch-licence-agreements.service.test.js
@@ -16,103 +16,90 @@ const LicenceAgreementHelper = require('../../support/helpers/licence-agreement.
 const FetchLicenceAgreementsService = require('../../../app/services/return-requirements/fetch-licence-agreements.service.js')
 
 describe('Return requirements Fetch Licence Agreements service', () => {
-  describe('when a licence agreement has a start date but no end date', () => {
-    let licenceAgreement
-    let testFinancialAgreements
+  let licenceAgreement
+  let financialAgreementId
 
-    beforeEach(async () => {
-      await DatabaseSupport.clean()
+  beforeEach(async () => {
+    await DatabaseSupport.clean()
 
-      testFinancialAgreements = await FinancialAgreementHelper.add()
-
-      const { id: financialAgreementId } = testFinancialAgreements
-
-      licenceAgreement = await LicenceAgreementHelper.add({ financialAgreementId })
-    })
-
-    it('should return a result with one financial agreement', async () => {
-      const result = await FetchLicenceAgreementsService.go(licenceAgreement.licenceRef)
-
-      expect(result).to.equal([{
-        id: licenceAgreement.id,
-        financialAgreementId: licenceAgreement.financialAgreementId,
-        licenceRef: licenceAgreement.licenceRef,
-        startDate: licenceAgreement.startDate,
-        endDate: null,
-        dateSigned: licenceAgreement.dateSigned,
-        deletedAt: null,
-        source: 'nald',
-        createdAt: licenceAgreement.createdAt,
-        updatedAt: licenceAgreement.updatedAt,
-        financialAgreements: [{
-          id: testFinancialAgreements.id
-        }]
-      }])
-    })
+    const financialAgreement = await FinancialAgreementHelper.add()
+    financialAgreementId = financialAgreement.id
   })
 
-  describe('when a licence agreement has a start date and an end date in the future', () => {
-    let licenceAgreement
-    let testFinancialAgreements
-    const today = new Date()
-    const tomorrowWithTime = new Date(today.getTime() + 86400000)
-    const tomorrow = new Date(tomorrowWithTime.getFullYear(), tomorrowWithTime.getMonth(), tomorrowWithTime.getDate() + 1)
+  describe('when there are agreements for the licence', () => {
+    describe('and the agreement has no end date', () => {
+      beforeEach(async () => {
+        licenceAgreement = await LicenceAgreementHelper.add({ financialAgreementId })
+      })
 
-    beforeEach(async () => {
-      await DatabaseSupport.clean()
+      it('returns the financial agreement', async () => {
+        const result = await FetchLicenceAgreementsService.go(licenceAgreement.licenceRef)
 
-      testFinancialAgreements = await FinancialAgreementHelper.add()
-
-      const { id: financialAgreementId } = testFinancialAgreements
-
-      licenceAgreement = await LicenceAgreementHelper.add({
-        financialAgreementId,
-        endDate: tomorrow
+        expect(result).to.equal([{
+          id: licenceAgreement.id,
+          financialAgreementId,
+          licenceRef: licenceAgreement.licenceRef,
+          startDate: licenceAgreement.startDate,
+          endDate: null,
+          dateSigned: licenceAgreement.dateSigned,
+          deletedAt: null,
+          source: 'nald',
+          createdAt: licenceAgreement.createdAt,
+          updatedAt: licenceAgreement.updatedAt,
+          financialAgreements: [{
+            id: financialAgreementId
+          }]
+        }])
       })
     })
 
-    it('should return a result with one financial agreement', async () => {
-      const result = await FetchLicenceAgreementsService.go(licenceAgreement.licenceRef)
+    describe('and the agreement has an end date in the future', () => {
+      beforeEach(async () => {
+        licenceAgreement = await LicenceAgreementHelper.add({ financialAgreementId, endDate: new Date('2099-01-01') })
+      })
 
-      expect(result).to.equal([{
-        id: licenceAgreement.id,
-        financialAgreementId: licenceAgreement.financialAgreementId,
-        licenceRef: licenceAgreement.licenceRef,
-        startDate: licenceAgreement.startDate,
-        endDate: tomorrow,
-        dateSigned: licenceAgreement.dateSigned,
-        deletedAt: null,
-        source: 'nald',
-        createdAt: licenceAgreement.createdAt,
-        updatedAt: licenceAgreement.updatedAt,
-        financialAgreements: [{
-          id: testFinancialAgreements.id
-        }]
-      }])
-    })
-  })
+      it('returns the financial agreement', async () => {
+        const result = await FetchLicenceAgreementsService.go(licenceAgreement.licenceRef)
 
-  describe('when a licence agreement has a start date and an end date is in the past', () => {
-    let licenceAgreement
-    let testFinancialAgreements
-
-    beforeEach(async () => {
-      await DatabaseSupport.clean()
-
-      testFinancialAgreements = await FinancialAgreementHelper.add()
-
-      const { id: financialAgreementId } = testFinancialAgreements
-
-      licenceAgreement = await LicenceAgreementHelper.add({
-        financialAgreementId,
-        endDate: new Date('2023-12-31')
+        expect(result).to.equal([{
+          id: licenceAgreement.id,
+          financialAgreementId,
+          licenceRef: licenceAgreement.licenceRef,
+          startDate: licenceAgreement.startDate,
+          endDate: new Date('2099-01-01'),
+          dateSigned: licenceAgreement.dateSigned,
+          deletedAt: null,
+          source: 'nald',
+          createdAt: licenceAgreement.createdAt,
+          updatedAt: licenceAgreement.updatedAt,
+          financialAgreements: [{
+            id: financialAgreementId
+          }]
+        }])
       })
     })
 
-    it('should return an empty array', async () => {
-      const result = await FetchLicenceAgreementsService.go(licenceAgreement.licenceRef)
+    describe('but the agreement has an end date in the past', () => {
+      beforeEach(async () => {
+        licenceAgreement = await LicenceAgreementHelper.add({
+          financialAgreementId,
+          endDate: new Date('2023-12-31')
+        })
+      })
 
-      expect(result).to.equal([])
+      it('returns no results', async () => {
+        const result = await FetchLicenceAgreementsService.go(licenceAgreement.licenceRef)
+
+        expect(result).to.be.empty()
+      })
+    })
+  })
+
+  describe('when there are no agreements for the licence', () => {
+    it('returns no results', async () => {
+      const result = await FetchLicenceAgreementsService.go('01/ABC')
+
+      expect(result).to.be.empty()
     })
   })
 })
