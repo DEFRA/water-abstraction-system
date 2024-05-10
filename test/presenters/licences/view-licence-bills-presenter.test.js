fc1a826b8d2c446e8745f3e77f96661b4565b83a
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-05-10 16:18:05 +0100

    View licence bills Fixes (#998)
    Previous work - https://github.com/DEFRA/water-abstraction-system/pull/986/files
    
    Work item - https://eaflood.atlassian.net/browse/WATER-4316
    
    This change will introduce a bills controller, service and presenter to handle the view licence bills page.
    
    This change updates failures from test and additional missing work from the initial diff with the old system.
    
    Additional conditions added for the Bill Number Column label
    Add rebilling notes to the bill number column
    Add the credit not to in credit accounts

--- test/presenters/licences/view-licence-bills-presenter.test.js
+++ test/presenters/licences/view-licence-bills-presenter.test.js
@@ -21,9 +21,11 @@ describe('View Licence Bills presenter', () => {
           account: 'acc123',
           accountId: 'bicc1233',
           billNumber: 'inv123',
+          credit: false,
           dateCreated: '1 January 2020',
           financialYear: '2021',
           id: 'id123',
+          legacyId: null,
           runType: 'annual',
           total: '£1,234,567.89'
         }]
@@ -38,18 +40,77 @@ describe('View Licence Bills presenter', () => {
       const result = ViewLicenceBillsPresenter.go(_bills())
       expect(result.bills[0].total).to.equal('£1,234,567.89')
     })
+
+    it('correctly formats the two part tariff batch type', () => {
+      const result = ViewLicenceBillsPresenter.go(_billsTwoPartTariff())
+      expect(result.bills[0].runType).to.equal('two part tariff')
+    })
+
+    describe('billNumber', () => {
+      it('correctly formats the \'billNumber\' to the invoice number', () => {
+        const result = ViewLicenceBillsPresenter.go(_bills())
+        expect(result.bills[0].billNumber).to.equal('inv123')
+      })
+
+      it('correctly formats the \'billNumber\' to \'De minimis bill\'', () => {
+        const bill = _bill()
+        bill.invoiceNumber = null
+        bill.deminimis = true
+        const result = ViewLicenceBillsPresenter.go([bill])
+        expect(result.bills[0].billNumber).to.equal('De minimis bill')
+      })
+
+      it('correctly formats the \'billNumber\' to \'NALD revised bill\'', () => {
+        const bill = _bill()
+        bill.invoiceNumber = null
+        bill.legacyId = 'lgcy'
+        const result = ViewLicenceBillsPresenter.go([bill])
+        expect(result.bills[0].billNumber).to.equal('NALD revised bill')
+      })
+
+      it('correctly formats the \'billNumber\' to \'Zero value bill\'', () => {
+        const bill = _bill()
+        bill.invoiceNumber = null
+        bill.netAmount = 0
+        const result = ViewLicenceBillsPresenter.go([bill])
+        expect(result.bills[0].billNumber).to.equal('Zero value bill')
+      })
+    })
   })
 })
 
-function _bills () {
-  return [{
+function _bill () {
+  return {
     accountNumber: 'acc123',
     billRun: { batchType: 'annual' },
     billingAccountId: 'bicc1233',
     createdAt: new Date('2020-01-01'),
+    credit: false,
+    deminimis: false,
     financialYearEnding: '2021',
     id: 'id123',
     invoiceNumber: 'inv123',
+    legacyId: null,
+    netAmount: 123456789
+  }
+}
+
+function _bills () {
+  return [_bill()]
+}
+
+function _billsTwoPartTariff () {
+  return [{
+    accountNumber: 'acc123',
+    billRun: { batchType: 'two_part_tariff' },
+    billingAccountId: 'bicc1233',
+    createdAt: new Date('2020-01-01'),
+    credit: false,
+    deminimis: false,
+    financialYearEnding: '2021',
+    id: 'id123',
+    invoiceNumber: 'inv123',
+    legacyId: null,
     netAmount: 123456789
   }]
 }
