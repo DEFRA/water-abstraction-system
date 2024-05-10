d721566f6a12d1dac15c9f5cef9ff49bf459537f
Rebecca Ransome <beckyrose200@aol.com>
2024-05-10 13:29:24 +0100

    Refactor validator

--- app/validators/bill-runs/two-part-tariff/adjustment-factor.validator.js
+++ app/validators/bill-runs/two-part-tariff/adjustment-factor.validator.js
@@ -40,7 +40,7 @@ function go (payload, maxNumberOfDecimals, validationType) {
 function customValidation (quantity, helpers, maxNumberOfDecimals, validationType) {
   const quantityParts = quantity.toString().split('.')
 
-  if (quantityParts.length === 1 || quantityParts[1].length < maxNumberOfDecimals) {
+  if (quantityParts.length === 1 || quantityParts[1].length <= maxNumberOfDecimals) {
     return quantity
   }
 
@@ -68,7 +68,7 @@ function _validate (quantity, maxNumberOfDecimals, validationType) {
   // The first check we are doing is validating that a number has been inputted. If it has then we can move onto our
   // next check for if there are less than the given amount of decimal places
   if (!validation.error) {
-    const decimalSchema = Joi.number().custom((value, helpers) => customValidation(value, helpers, maxNumberOfDecimals, validationType, 'custom validation'))
+    const decimalSchema = Joi.number().custom((value, helpers) => customValidation(value, helpers, maxNumberOfDecimals, validationType))
 
     return decimalSchema.validate(quantity)
   }
