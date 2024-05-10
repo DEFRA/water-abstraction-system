094d5c2caeb7d27db2ac50fbaa036ee9977bee02
Roble <roblesiyad123@gmail.com>
2024-05-10 12:56:10 +0100

    Add submit remove to controller

--- app/controllers/return-requirements.controller.js
+++ app/controllers/return-requirements.controller.js
@@ -34,6 +34,7 @@ const SubmitNoteService = require('../services/return-requirements/submit-note.s
 const SubmitPointsService = require('../services/return-requirements/submit-points.service.js')
 const SubmitPurposeService = require('../services/return-requirements/submit-purpose.service.js')
 const SubmitReasonService = require('../services/return-requirements/submit-reason.service.js')
+const SubmitRemoveService = require('../services/return-requirements/submit-remove.service.js')
 const SubmitReturnsCycleService = require('../services/return-requirements/submit-returns-cycle.service.js')
 const SubmitSetupService = require('../services/return-requirements/submit-setup.service.js')
 const SubmitSiteDescriptionService = require('../services/return-requirements/submit-site-description.service.js')
@@ -178,9 +179,9 @@ async function reason (request, h) {
 }
 
 async function remove (request, h) {
-  const { sessionId } = request.params
+  const { requirementIndex, sessionId } = request.params
 
-  const pageData = await RemoveService.go(sessionId)
+  const pageData = await RemoveService.go(sessionId, requirementIndex)
 
   return h.view('return-requirements/remove.njk', {
     ...pageData
@@ -382,6 +383,14 @@ async function submitReason (request, h) {
   return h.redirect(`/system/return-requirements/${sessionId}/setup`)
 }
 
+async function submitRemove (request, h) {
+  const { requirementIndex, sessionId } = request.params
+
+  await SubmitRemoveService.go(sessionId, requirementIndex)
+
+  return h.redirect(`/system/return-requirements/${sessionId}/check`)
+}
+
 async function submitReturnsCycle (request, h) {
   const { requirementIndex, sessionId } = request.params
 
@@ -478,6 +487,7 @@ module.exports = {
   submitPoints,
   submitPurpose,
   submitReason,
+  submitRemove,
   submitReturnsCycle,
   submitSetup,
   submitSiteDescription,
