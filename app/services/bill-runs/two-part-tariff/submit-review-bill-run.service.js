1c1515915dbe8e9ad78dcc8c8c59270e2580e4b3
Jason Claxton <jason.claxton@defra.gov.uk>
2024-05-16 14:14:52 +0100

    Make the filters specific to the bill run being viewed

--- app/services/bill-runs/two-part-tariff/submit-review-bill-run.service.js
+++ app/services/bill-runs/two-part-tariff/submit-review-bill-run.service.js
@@ -8,19 +8,20 @@
 /**
  * Updates the session cookie with the filter data needed for the two-part tariff review bill run page
  *
+ * @param {String} billRunId - The UUID of the bill run
  * @param {Object} payload The `request.payload` containing the filter data.
  * @param {Object} yar - The Hapi `request.yar` session manager passed on by the controller
  */
-async function go (payload, yar) {
+async function go (billRunId, payload, yar) {
   const clearFilters = payload?.clearFilters
   const filterIssues = payload?.filterIssues
   const filterLicenceHolder = payload?.filterLicenceHolder
   const filterLicenceStatus = payload?.filterLicenceStatus
 
   if (clearFilters) {
-    yar.clear('reviewFilters')
+    yar.clear(`review-${billRunId}`)
   } else {
-    yar.set('reviewFilters', {
+    yar.set(`review-${billRunId}`, {
       filterIssues,
       filterLicenceHolder,
       filterLicenceStatus
