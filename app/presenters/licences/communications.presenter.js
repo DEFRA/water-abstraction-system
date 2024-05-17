ef01a29d1ee96c2b890c04967272490e17527d6f
jonathangoulding <jonathangoulding21@gmail.com>
2024-05-17 09:31:22 +0100

    refactor: use case presenter helper

--- app/presenters/licences/communications.presenter.js
+++ app/presenters/licences/communications.presenter.js
@@ -5,7 +5,7 @@
  * @module CommunicationsPresenter
  */
 
-const { formatLongDate } = require('../base.presenter.js')
+const { formatLongDate, sentenceCase } = require('../base.presenter.js')
 
 /**
  * Formats data for the `/licences/{id}/communications` view licence communications page
@@ -25,18 +25,9 @@ function _communications (communications) {
       type: _type(communication),
       sender: communication.event.issuer,
       sent: formatLongDate(new Date(communication.event.createdAt)),
-      method: _sentenceCase(communication.messageType)
+      method: sentenceCase(communication.messageType)
     }
   })
-}
-
-function _titleCase (text = '') {
-  return text.charAt(0).toUpperCase() + text.slice(1)
-}
-function _sentenceCase (text = '') {
-  const sentence = text.toLowerCase()
-
-  return _titleCase(sentence)
 }
 
 function _type (communication) {
@@ -50,7 +41,7 @@ function _type (communication) {
 
 function _typeAlert (communication) {
   if (communication.event.metadata.name === 'Water abstraction alert') {
-    return `${_sentenceCase(communication.event.metadata.options.sendingAlertType)} - Water abstraction alert`
+    return `${sentenceCase(communication.event.metadata.options.sendingAlertType)} - Water abstraction alert`
   }
 
   return null
