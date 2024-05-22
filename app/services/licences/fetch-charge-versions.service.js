3ed812f2555075a69fb846e4df89af56bed93751
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-05-22 14:08:47 +0100

    Update app/services/licences/fetch-charge-versions.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/licences/fetch-charge-versions.service.js
+++ app/services/licences/fetch-charge-versions.service.js
@@ -8,11 +8,9 @@
 const ChargeVersionModel = require('../../models/charge-version.model.js')
 
 /**
- * Fetch the matching licence and return data needed for the view licence set up page
+ * Fetches charge version data needed for the view '/licences/{id}/set-up` page
  *
- * Was built to provide the data needed for the '/licences/{id}/licence-set-up' page
- *
- * @param {string} licenceId The string for the licence to fetch
+ * @param {string} licenceId - The UUID for the licence to fetch charge versions for
  *
  * @returns {Promise<Object>} the data needed to populate the view licence page's set up tab
  */
