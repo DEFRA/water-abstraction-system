2375838c70dc9c9819cebf8f07db187469c35930
Rebecca Ransome <beckyrose200@aol.com>
2024-05-22 09:56:32 +0100

    GET route for amend authorised volume page

---
+++ app/presenters/bill-runs/two-part-tariff/amend-authorised-volume.presenter.js
@@ -0,0 +1,56 @@
+'use strict'
+
+/**
+ * Formats the two part tariff review data ready for presenting in the amend authorised volume page
+ * @module AmendAuthorisedVolumePresenter
+ */
+
+const { formatLongDate, formatFinancialYear } = require('../../base.presenter.js')
+
+/**
+ * Prepares and processes bill run and review charge reference data for presenting
+ *
+ * @param {module:BillRunModel} billRun - the data from the bill run
+ * @param {module:ReviewChargeReference} reviewChargeReference - the data from the review charge reference
+ * @param {String} licenceId - the UUID of the licence being reviewed
+ *
+ * @returns {Object} the prepared bill run and charge reference data to be passed to the amend authorised volume page
+ */
+function go (billRun, reviewChargeReference, licenceId) {
+  return {
+    billRunId: billRun.id,
+    licenceId,
+    financialYear: formatFinancialYear(billRun.toFinancialYearEnding),
+    chargePeriod: _prepareDate(
+      reviewChargeReference.reviewChargeVersion.chargePeriodStartDate,
+      reviewChargeReference.reviewChargeVersion.chargePeriodEndDate
+    ),
+    chargeReference: {
+      id: reviewChargeReference.id,
+      description: reviewChargeReference.chargeReference.chargeCategory.shortDescription,
+      authorisedVolume: reviewChargeReference.amendedAuthorisedVolume,
+      totalBillableReturns: _totalBillableReturns(reviewChargeReference.reviewChargeElements)
+    }
+  }
+}
+
+function _prepareDate (startDate, endDate) {
+  const preparedStartDate = formatLongDate(startDate)
+  const preparedEndDate = formatLongDate(endDate)
+
+  return `${preparedStartDate} to ${preparedEndDate}`
+}
+
+function _totalBillableReturns (reviewChargeElements) {
+  let totalBillableReturns = 0
+
+  reviewChargeElements.forEach((reviewChargeElement) => {
+    totalBillableReturns += reviewChargeElement.amendedAllocated
+  })
+
+  return totalBillableReturns
+}
+
+module.exports = {
+  go
+}
--- app/services/bill-runs/two-part-tariff/amend-authorised-volume.service.js
+++ app/services/bill-runs/two-part-tariff/amend-authorised-volume.service.js
@@ -5,8 +5,8 @@
  * @module AmendAuthorisedVolumeService
  */
 
-const AmendAuthorisedVolumePresenter = require('../../../presenters/bill-runs/two-part-tariff/amend-adjustment-factor.presenter.js')
-const FetchReviewChargeReferenceService = require('./fetch-review-charge-reference.service.js')
+const AmendAuthorisedVolumePresenter = require('../../../presenters/bill-runs/two-part-tariff/amend-authorised-volume.presenter.js')
+const FetchAuthorisedVolumeService = require('./fetch-authorised-volume.service.js')
 
 /**
  * Orchestrates fetching and presenting the data needed for the amend authorised volume page
@@ -15,13 +15,10 @@ const FetchReviewChargeReferenceService = require('./fetch-review-charge-referen
  * @param {String} licenceId - The UUID of the licence that is being reviewed
  * @param {String} reviewChargeReferenceId - The UUID of the review charge reference being viewed
  *
- * @returns {Promise<Object>} the 'pageData' needed to view the amend adjustment factor page
+ * @returns {Promise<Object>} the 'pageData' needed to view the amend authorised volume page
  */
 async function go (billRunId, licenceId, reviewChargeReferenceId) {
-  const {
-    billRun,
-    reviewChargeReference
-  } = await FetchReviewChargeReferenceService.go(billRunId, reviewChargeReferenceId)
+  const { billRun, reviewChargeReference } = await FetchAuthorisedVolumeService.go(billRunId, reviewChargeReferenceId)
 
   const pageData = AmendAuthorisedVolumePresenter.go(billRun, reviewChargeReference, licenceId)
 
