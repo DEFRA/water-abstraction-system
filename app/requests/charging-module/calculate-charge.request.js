eef3565c215bfc88e52dbc278255a9be59cfcc4d
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2024-05-23 09:47:57 +0100

    Correct link in comment
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/requests/charging-module/calculate-charge.request.js
+++ app/requests/charging-module/calculate-charge.request.js
@@ -10,7 +10,7 @@ const ChargingModuleRequest = require('../charging-module.request.js')
 /**
  * Sends a request to the Charging Module to calculate a standalone charge and returns the result.
  *
- * See {@link https://defra.github.io/sroc-charging-module-api-docs/#/bill-run/AddBillRunTransaction | API docs} for
+ * See {@link https://defra.github.io/sroc-charging-module-api-docs/#/calculate/CalculateCharge | API docs} for
  * more details
  *
  * @param {Object} transactionData - The transaction details to be sent in the body of the request
