a8dfbe260017b1adda6c9a4d5b9865a32e0635b5
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-05-23 13:17:18 +0100

    View Licence set up page (#1034)
    * View Licence set up page
    
    This page replace the existing charge information page.
    
    The page will show charge information for a licence and allow users to create a new charge version to manage licence billing.
    
    Only restricted users can use this page and only some of those users can create a new charge and make the licence non-chargeable.

---
+++ app/services/licences/fetch-charge-versions.service.js
@@ -0,0 +1,44 @@
+'use strict'
+
+/**
+ * Fetches charge version data needed for the view '/licences/{id}/set-up` page
+ * @module FetchChargeVersionsService
+ */
+
+const ChargeVersionModel = require('../../models/charge-version.model.js')
+
+/**
+ * Fetches charge version data needed for the view '/licences/{id}/set-up` page
+ *
+ * @param {string} licenceId - The UUID for the licence to fetch charge versions for
+ *
+ * @returns {Promise<Object>} the data needed to populate the view licence page's set up tab
+ */
+async function go (licenceId) {
+  return _fetch(licenceId)
+}
+
+async function _fetch (licenceId) {
+  return ChargeVersionModel.query()
+    .where('licenceId', licenceId)
+    .select([
+      'id',
+      'startDate',
+      'endDate',
+      'status',
+      'licenceId'
+    ])
+    .withGraphFetched('changeReason')
+    .modifyGraph('changeReason', (builder) => {
+      builder.select([
+        'description'
+      ])
+    })
+    .orderBy([
+      { column: 'startDate', order: 'desc' }
+    ])
+}
+
+module.exports = {
+  go
+}
---
+++ app/services/licences/fetch-workflows.service.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/**
+ * Fetches workflow data needed for the view '/licences/{id}/set-up` page
+ * @module FetchWorkflowsService
+ */
+
+const WorkflowModel = require('../../models/workflow.model.js')
+
+/**
+ * Fetches workflow data needed for the view '/licences/{id}/set-up` page
+ *
+ * @param {string} licenceId - The UUID for the licence to fetch workflow data for
+ *
+ * @returns {Promise<Object>} the data needed to populate the view licence page's set up tab
+ */
+async function go (licenceId) {
+  return _fetch(licenceId)
+}
+
+async function _fetch (licenceId) {
+  return WorkflowModel.query()
+    .where('licenceId', licenceId)
+    .andWhere('deletedAt', null)
+    .select([
+      'id',
+      'createdAt',
+      'status',
+      'licenceId',
+      'data'
+    ])
+    .orderBy([
+      { column: 'createdAt', order: 'desc' }
+    ])
+}
+
+module.exports = {
+  go
+}
