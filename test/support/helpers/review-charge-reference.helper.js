763a7b95647737dbcc54b684c9d67df2f51b3dc1
Jason Claxton <jason.claxton@defra.gov.uk>
2024-05-29 20:41:55 +0100

    Update helper defaults to more realistic values

--- test/support/helpers/review-charge-reference.helper.js
+++ test/support/helpers/review-charge-reference.helper.js
@@ -54,9 +54,9 @@ function defaults (data = {}) {
     canalAndRiverTrustAgreement: false,
     twoPartTariffAgreement: true,
     winterDiscount: false,
-    abatementAgreement: 0,
-    chargeAdjustment: 0,
-    amendedChargeAdjustment: 0,
+    abatementAgreement: 1,
+    chargeAdjustment: 1,
+    amendedChargeAdjustment: 1,
     authorisedVolume: 50,
     amendedAuthorisedVolume: 50
   }
