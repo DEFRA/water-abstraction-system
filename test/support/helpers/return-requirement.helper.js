3e866c224c9d522af9dd337fa33f683ddf95647f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-04 14:41:27 +0100

    Fix other errors in the Rtn Req helper

--- test/support/helpers/return-requirement.helper.js
+++ test/support/helpers/return-requirement.helper.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * @module ReturnVersionHelper
+ * @module ReturnRequirementHelper
  */
 
 const { generateUUID } = require('../../../app/lib/general.lib.js')
@@ -12,11 +12,10 @@ const ReturnRequirementModel = require('../../../app/models/return-requirement.m
  *
  * If no `data` is provided, default values will be used. These are
  *
- * - `id` - [random UUID]
  * - `return_version_id` - [random UUID]
- * - `returns_frequency` - [enum] - defult to approved
- * - `summer` - [boolean] - false
- * - `upload` - [boolean] - false
+ * - `returns_frequency` - day
+ * - `summer` - false
+ * - `upload` - false
  * - `createdAt` - 2022-11-16 09:42:11.000
  *
  * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
@@ -41,17 +40,15 @@ function add (data = {}) {
  */
 function defaults (data = {}) {
   const defaults = {
-    id: generateUUID(),
     returnVersionId: generateUUID(),
-    returnsFrequency: 'approved',
+    returnsFrequency: 'day',
     summer: false,
     upload: false,
-    // INFO: The lines table does not have a default for the date_created column. But it is set as
-    // 'not nullable'! So, we need to ensure we set it when creating a new record, something we'll never actually need
-    // to do because it's a static table. Also, we can't use Date.now() because Javascript returns the time since the
-    // epoch in milliseconds, whereas a PostgreSQL timestamp field can only hold the seconds since the epoch. Pass it
-    // an ISO string though ('2023-01-05T08:37:05.575Z') and PostgreSQL can do the conversion
-    // https://stackoverflow.com/a/61912776/6117745
+    // INFO: The table does not have a default for the date_created column. But it is set as 'not nullable'!  So, we
+    // need to ensure we set it when creating a new record, something we'll never actually need to do because it's a
+    // static table. Also, we can't use Date.now() because Javascript returns the time since the epoch in milliseconds,
+    // whereas a PostgreSQL timestamp field can only hold the seconds since the epoch. Pass it an ISO string though
+    // ('2023-01-05T08:37:05.575Z') and PostgreSQL can do the conversion https://stackoverflow.com/a/61912776/6117745
     createdAt: new Date('2022-11-16 09:42:11.000').toISOString()
   }
 
