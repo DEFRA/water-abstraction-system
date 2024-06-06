fc06e2ea49a5b6a2af06f9e873f25f3c9b19b7ea
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-06 17:37:14 +0100

    Add relationships for return version

--- app/models/return-version.model.js
+++ app/models/return-version.model.js
@@ -5,11 +5,34 @@
  * @module ReturnVersionModel
  */
 
+const { Model } = require('objection')
+
 const BaseModel = require('./base.model.js')
 
 class ReturnVersionModel extends BaseModel {
   static get tableName () {
     return 'returnVersions'
+  }
+
+  static get relationMappings () {
+    return {
+      licence: {
+        relation: Model.BelongsToOneRelation,
+        modelClass: 'licence.model',
+        join: {
+          from: 'returnVersions.licenceId',
+          to: 'licences.id'
+        }
+      },
+      returnRequirements: {
+        relation: Model.HasManyRelation,
+        modelClass: 'return-requirement.model',
+        join: {
+          from: 'returnVersions.id',
+          to: 'returnRequirements.returnVersionId'
+        }
+      }
+    }
   }
 }
 
--- test/models/return-version.model.test.js
+++ test/models/return-version.model.test.js
@@ -9,6 +9,10 @@ const { expect } = Code
 
 // Test helpers
 const DatabaseSupport = require('../support/database.js')
+const LicenceHelper = require('../support/helpers/licence.helper.js')
+const LicenceModel = require('../../app/models/licence.model.js')
+const ReturnRequirementHelper = require('../support/helpers/return-requirement.helper.js')
+const ReturnRequirementModel = require('../../app/models/return-requirement.model.js')
 const ReturnVersionHelper = require('../support/helpers/return-version.helper.js')
 
 // Thing under test
@@ -19,16 +23,87 @@ describe('Return Version model', () => {
 
   beforeEach(async () => {
     await DatabaseSupport.clean()
-
-    testRecord = await ReturnVersionHelper.add()
   })
 
   describe('Basic query', () => {
+    beforeEach(async () => {
+      testRecord = await ReturnVersionHelper.add()
+    })
+
     it('can successfully run a basic query', async () => {
       const result = await ReturnVersionModel.query().findById(testRecord.id)
 
       expect(result).to.be.an.instanceOf(ReturnVersionModel)
       expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to licence', () => {
+      let testLicence
+
+      beforeEach(async () => {
+        testLicence = await LicenceHelper.add()
+
+        const { id: licenceId } = testLicence
+        testRecord = await ReturnVersionHelper.add({ licenceId })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ReturnVersionModel.query()
+          .innerJoinRelated('licence')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the licence', async () => {
+        const result = await ReturnVersionModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('licence')
+
+        expect(result).to.be.instanceOf(ReturnVersionModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.licence).to.be.an.instanceOf(LicenceModel)
+        expect(result.licence).to.equal(testLicence)
+      })
+    })
+
+    describe('when linking to return requirements', () => {
+      let testReturnRequirements
+
+      beforeEach(async () => {
+        testRecord = await ReturnVersionHelper.add()
+
+        testReturnRequirements = []
+        for (let i = 0; i < 2; i++) {
+          const returnRequirement = await ReturnRequirementHelper.add(
+            { siteDescription: `TEST RTN REQ ${i}`, returnVersionId: testRecord.id }
+          )
+          testReturnRequirements.push(returnRequirement)
+        }
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ReturnVersionModel.query()
+          .innerJoinRelated('returnRequirements')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the return requirements', async () => {
+        const result = await ReturnVersionModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('returnRequirements')
+
+        expect(result).to.be.instanceOf(ReturnVersionModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.returnRequirements).to.be.an.array()
+        expect(result.returnRequirements[0]).to.be.an.instanceOf(ReturnRequirementModel)
+        expect(result.returnRequirements).to.include(testReturnRequirements[0])
+        expect(result.returnRequirements).to.include(testReturnRequirements[1])
+      })
     })
   })
 })
