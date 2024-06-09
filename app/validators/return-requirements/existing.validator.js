125472d7bccde5f52688fac589ff7b50dc78779a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-09 22:57:39 +0100

    Add new existing validator
    We have to pass it in our array of return versions to allow us to validate that the one being sent in the post request is one that belongs to the licence in the session.

---
+++ app/validators/return-requirements/existing.validator.js
@@ -0,0 +1,41 @@
+'use strict'
+
+/**
+ * Validates data submitted for the `/return-requirements/{sessionId}/existing` page
+ * @module ExistingValidator
+ */
+
+const Joi = require('joi')
+
+const errorMessage = 'Select an existing requirements for returns'
+
+/**
+ * Validates data submitted for the `/return-requirements/{sessionId}/existing` page
+ *
+ * @param {Object} payload - The payload from the request to be validated
+ *
+ * @returns {Object} the result from calling Joi's schema.validate(). It will be an object with a `value:` property. If
+ * any errors are found the `error:` property will also exist detailing what the issues were
+ */
+function go (data, returnVersions) {
+  const returnVersionIds = returnVersions.map((returnVersion) => {
+    return returnVersion.id
+  })
+
+  const schema = Joi.object({
+    existing: Joi.string()
+      .required()
+      .valid(...returnVersionIds)
+      .messages({
+        'any.required': errorMessage,
+        'any.only': errorMessage,
+        'string.empty': errorMessage
+      })
+  })
+
+  return schema.validate(data, { abortEarly: false })
+}
+
+module.exports = {
+  go
+}
