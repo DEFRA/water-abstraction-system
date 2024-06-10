c953e432df26ac224032423979d2c7b54558ba62
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-10 15:03:06 +0100

    Add new fetch existing requirements service
    This fetches the existing data and transforms it into a format that is can be used by the return requirements setup journey and its pages.
    
    To support this and ensure the service is fully tested, we added a new support seed to create the 'complete' requirements for returns record.

---
+++ app/services/return-requirements/fetch-existing-requirements.service.js
@@ -0,0 +1,151 @@
+'use strict'
+
+/**
+ * Fetches existing return requirements to be copied from
+ * @module FetchExistingRequirementsService
+ */
+
+const ReturnVersionModel = require('../../models/return-version.model.js')
+
+const FREQUENCIES = {
+  day: 'daily',
+  week: 'weekly',
+  fortnight: 'fortnightly',
+  month: 'monthly',
+  quarter: 'quarterly',
+  year: 'yearly'
+}
+
+/**
+ * Fetches existing return requirements to be copied from
+ *
+ * In the returns setup journey we allow users to select the option to create new requirements by copying from them
+ * from an existing return version. This service fetches the selected return version and its requirements
+ *
+ * @param {string} returnVersionId - The UUID of the selected return version to copy requirements from
+ *
+ * @returns {Promise<Object>} The return version and its linked return requirements, plus their points and purposes
+ */
+async function go (returnVersionId) {
+  const returnVersion = await _fetch(returnVersionId)
+
+  return _transformForSetup(returnVersion)
+}
+
+async function _fetch (returnVersionId) {
+  return ReturnVersionModel.query()
+    .findById(returnVersionId)
+    .select([
+      'id'
+    ])
+    .withGraphFetched('returnRequirements')
+    .modifyGraph('returnRequirements', (builder) => {
+      builder.select([
+        'id',
+        'abstractionPeriodEndDay',
+        'abstractionPeriodEndMonth',
+        'abstractionPeriodStartDay',
+        'abstractionPeriodStartMonth',
+        'collectionFrequency',
+        'fiftySixException',
+        'gravityFill',
+        'reabstraction',
+        'reportingFrequency',
+        'siteDescription',
+        'summer',
+        'twoPartTariff'
+      ])
+    })
+    .withGraphFetched('returnRequirements.returnRequirementPoints')
+    .modifyGraph('returnRequirements.returnRequirementPoints', (builder) => {
+      builder.select([
+        'id',
+        'naldPointId'
+      ])
+    })
+    .withGraphFetched('returnRequirements.returnRequirementPurposes')
+    .modifyGraph('returnRequirements.returnRequirementPurposes', (builder) => {
+      builder.select([
+        'id',
+        'purposeId'
+      ])
+    })
+}
+
+function _transformForSetup (returnVersion) {
+  const { returnRequirements } = returnVersion
+
+  return returnRequirements.map((returnRequirement) => {
+    const {
+      abstractionPeriodEndDay,
+      abstractionPeriodEndMonth,
+      abstractionPeriodStartDay,
+      abstractionPeriodStartMonth,
+      collectionFrequency,
+      reportingFrequency,
+      returnRequirementPoints,
+      returnRequirementPurposes,
+      siteDescription,
+      summer
+    } = returnRequirement
+
+    return {
+      points: _points(returnRequirementPoints),
+      purposes: _purposes(returnRequirementPurposes),
+      returnsCycle: summer ? 'summer' : 'winter-and-all-year',
+      siteDescription,
+      abstractionPeriod: {
+        'end-abstraction-period-day': abstractionPeriodEndDay,
+        'end-abstraction-period-month': abstractionPeriodEndMonth,
+        'start-abstraction-period-day': abstractionPeriodStartDay,
+        'start-abstraction-period-month': abstractionPeriodStartMonth
+      },
+      frequencyReported: FREQUENCIES[reportingFrequency],
+      frequencyCollected: FREQUENCIES[collectionFrequency],
+      agreementsExceptions: _agreementExceptions(returnRequirement)
+    }
+  })
+}
+
+function _agreementExceptions (returnRequirement) {
+  const { fiftySixException, gravityFill, reabstraction, twoPartTariff } = returnRequirement
+  const agreementsExceptions = []
+
+  if (fiftySixException) {
+    agreementsExceptions.push('56-returns-exception')
+  }
+
+  if (gravityFill) {
+    agreementsExceptions.push('gravity-fill')
+  }
+
+  if (reabstraction) {
+    agreementsExceptions.push('transfer-re-abstraction-scheme')
+  }
+
+  if (twoPartTariff) {
+    agreementsExceptions.push('two-part-tariff')
+  }
+
+  if (agreementsExceptions.length === 0) {
+    agreementsExceptions.push('none')
+  }
+
+  return agreementsExceptions
+}
+
+function _points (returnRequirementPoints) {
+  return returnRequirementPoints.map((returnRequirementPoint) => {
+    return returnRequirementPoint.naldPointId.toString()
+  })
+}
+
+function _purposes (returnRequirementPurposes) {
+  return returnRequirementPurposes.map((returnRequirementPurpose) => {
+    return returnRequirementPurpose.purposeId
+  })
+}
+
+module.exports = {
+  go
+}
---
+++ test/support/seeders/requirements-for-returns.seeder.js
@@ -0,0 +1,86 @@
+'use strict'
+
+/**
+ * @module RequirementsForReturnsSeeder
+ */
+
+const ReturnRequirementPointHelper = require('../helpers/return-requirement-point.helper.js')
+const ReturnRequirementPurposeHelper = require('../helpers/return-requirement-purpose.helper.js')
+const ReturnRequirementHelper = require('../helpers/return-requirement.helper.js')
+const ReturnVersionHelper = require('../helpers/return-version.helper.js')
+
+/**
+ * Add a complete 'requirements for returns' record, including return version, requirements, points and purposes
+ *
+ * Was built to support the testing of `FetchExistingRequirementsService` but can be used in any scenario where a
+ * complete 'requirements for returns' record is needed.
+ *
+ * It creates the parent return version then two return requirement records. To each it adds a return requirement point
+ * and purpose record.
+ *
+ * The first requirement has a winter returns cycle. reports weekly, and has no agreements. The second has a summer
+ * returns cycle, reports monthly, and has _all_ the agreements!
+ *
+ * Because of this it can be useful to test presenters and services that need to transform the data.
+ *
+ * @param {String} [licenceId] - The UUID of the licence to link the return version to
+ *
+ * @returns a 'complete' `ReturnVersionModel` instance with two return requirements, each containing a point and a
+ * purpose
+ */
+async function seed (licenceId = '1c68cd37-84af-46a4-b3ce-1fc625fcbf37') {
+  // Create a return version to which we'll link multiple return requirements
+  const returnVersion = await ReturnVersionHelper.add({ licenceId })
+
+  // Create the first requirement record
+  let returnRequirement = await _returnRequirement(
+    returnVersion.id,
+    'week',
+    false,
+    1234,
+    '1a1a68cc-b1f5-43db-8d1a-3452425bcc68',
+    false
+  )
+  returnVersion.returnRequirements = [returnRequirement]
+
+  // Create the second requirement record
+  returnRequirement = await _returnRequirement(
+    returnVersion.id,
+    'month',
+    true,
+    4321,
+    '91bac151-1c95-4ae5-b0bb-490980396e24',
+    true
+  )
+  returnVersion.returnRequirements.push(returnRequirement)
+
+  return returnVersion
+}
+
+async function _returnRequirement (returnVersionId, reportingFrequency, summer, naldPointId, purposeId, agreements) {
+  const returnRequirement = await ReturnRequirementHelper.add({
+    collectionFrequency: 'week',
+    fiftySixException: agreements,
+    gravityFill: agreements,
+    reabstraction: agreements,
+    reportingFrequency,
+    returnVersionId,
+    siteDescription: summer ? 'SECOND BOREHOLE AT AVALON' : 'FIRST BOREHOLE AT AVALON',
+    summer,
+    twoPartTariff: agreements
+  })
+
+  const { id: returnRequirementId } = returnRequirement
+
+  const point = await ReturnRequirementPointHelper.add({ naldPointId, returnRequirementId })
+  returnRequirement.returnRequirementPoints = [point]
+
+  const purpose = await ReturnRequirementPurposeHelper.add({ purposeId, returnRequirementId })
+  returnRequirement.returnRequirementPurposes = [purpose]
+
+  return returnRequirement
+}
+
+module.exports = {
+  seed
+}
