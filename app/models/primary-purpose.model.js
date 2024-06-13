0a9d58b27ae94178b7d49348b674005f313945e8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-13 08:21:03 +0100

    Add purpose primary and secondary models (#1088)
    https://eaflood.atlassian.net/browse/WATER-4442
    
    > Part of the work to replace NALD for handling return requirements
    
    We want to be able to offer users the option to generate return requirements from the licence's abstraction data. On the `/setup` page, they can select this option and the service is expected to generate return requirements using the source abstraction data against the licence. The other options are to copy existing return requirements or set them up manually.
    
    When you setup a return requirement you have to specify the frequency (daily, weekly or monthly) meter readings need to be collected and reported.
    
    - collected - visit the meter and record the reading
    - reported - when submitting the readings how they are to be provided
    
    Generally they are the same, but there are scenarios where we may ask for readings to be collected more frequently than they are reported.
    
    In order to generate the requirements automatically, we need to determine what frequencies to use based on the licence's abstraction data. A specific scenario is licences for the purpose of electricity generation; P-ELC-200 and P-ELC-240.
    
    - `P` is the 'purpose primary' code
    - `ELC` is the 'purpose secondary' code
    - `200` is the 'purpose' legacy ID
    
    We record all 3 against a `LicenceVersionPurpose` but currently the only one that exists as a model is 'purpose'. To support this work, we also need to know what the primary and secondary purposes on a `LicenceVersionPurposeModel` are.
    
    So, this change adds the migrations, helpers and models for `PurposePrimary` and `PurposeSecondary` and sets up the relationships between them and `LicenceVersionPurposeModel`.
    
    It then adds a modifier to `LicenceVersionPurpose` to automate fetching them, along with a custom model instance method that can be used to determine if a `LicenceVersionPurposeModel` is for the purpose of electricity generation.

---
+++ app/models/primary-purpose.model.js
@@ -0,0 +1,39 @@
+'use strict'
+
+/**
+ * Model for primary_purposes (water.purposes_primary)
+ * @module PrimaryPurposeModel
+ */
+
+const { Model } = require('objection')
+
+const BaseModel = require('./base.model.js')
+
+class PrimaryPurposeModel extends BaseModel {
+  static get tableName () {
+    return 'primaryPurposes'
+  }
+
+  static get relationMappings () {
+    return {
+      licenceVersionPurposes: {
+        relation: Model.HasManyRelation,
+        modelClass: 'licence-version-purpose.model',
+        join: {
+          from: 'primaryPurposes.id',
+          to: 'licenceVersionPurposes.primaryPurposeId'
+        }
+      },
+      returnRequirementPurposes: {
+        relation: Model.HasManyRelation,
+        modelClass: 'return-requirement-purpose.model',
+        join: {
+          from: 'primaryPurposes.id',
+          to: 'returnRequirementPurposes.primaryPurposeId'
+        }
+      }
+    }
+  }
+}
+
+module.exports = PrimaryPurposeModel
---
+++ db/migrations/public/20240611215502_alter-return-requirement-purposes-view.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const viewName = 'return_requirement_purposes'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+    .createView(viewName, (view) => {
+      view.as(knex('return_requirement_purposes').withSchema('water').select([
+        'return_requirement_purpose_id AS id',
+        'return_requirement_id',
+        'purpose_primary_id AS primary_purpose_id',
+        'purpose_secondary_id AS secondary_purpose_id',
+        'purpose_use_id AS purpose_id',
+        'purpose_alias AS alias',
+        'external_id',
+        'date_created AS created_at',
+        'date_updated AS updated_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+    .createView(viewName, (view) => {
+      view.as(knex('return_requirement_purposes').withSchema('water').select([
+        'return_requirement_purpose_id AS id',
+        'return_requirement_id',
+        'purpose_primary_id',
+        'purpose_secondary_id',
+        'purpose_use_id AS purpose_id',
+        'purpose_alias AS alias',
+        'external_id',
+        'date_created AS created_at',
+        'date_updated AS updated_at'
+      ]))
+    })
+}
