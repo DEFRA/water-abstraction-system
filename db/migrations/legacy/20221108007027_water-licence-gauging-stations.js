961cd88e2cf1faded9a3b1f1932f183970ea6d88
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-06-14 10:09:18 +0100

    Fix legacy migrations
    Columns incorrectly flagged as nullable or not, missed column renames etc.

--- db/migrations/legacy/20221108007027_water-licence-gauging-stations.js
+++ db/migrations/legacy/20221108007027_water-licence-gauging-stations.js
@@ -8,13 +8,13 @@ exports.up = function (knex) {
     .withSchema('water')
     .createTable(tableName, (table) => {
       // Primary Key
-      table.uuid('licence_gauging_station_id').primary()
+      table.uuid('licence_gauging_station_id').primary().defaultTo(knex.raw('gen_random_uuid()'))
 
       // Data
       table.uuid('licence_id').notNullable()
       table.uuid('gauging_station_id').notNullable()
       table.string('source').notNullable()
-      table.uuid('licence_version_purpose_condition_id').notNullable()
+      table.uuid('licence_version_purpose_condition_id')
       table.smallint('abstraction_period_start_day')
       table.smallint('abstraction_period_start_month')
       table.smallint('abstraction_period_end_day')
@@ -22,15 +22,15 @@ exports.up = function (knex) {
       table.string('restriction_type').notNullable()
       table.string('threshold_unit').notNullable()
       table.decimal('threshold_value').notNullable()
-      table.string('status').notNullable()
+      table.string('status').notNullable().defaultTo('resume')
       table.timestamp('date_status_updated', { useTz: false })
       table.timestamp('date_deleted', { useTz: false })
-      table.string('alert_type').notNullable()
-      table.boolean('is_test')
+      table.string('alert_type').notNullable().default('reduce')
+      table.boolean('is_test').defaultTo(false)
 
       // Legacy timestamps
-      table.timestamp('date_created', { useTz: false }).notNullable().defaultTo(knex.fn.now())
-      table.timestamp('date_updated', { useTz: false }).notNullable().defaultTo(knex.fn.now())
+      table.timestamp('date_created', { useTz: false }).notNullable()
+      table.timestamp('date_updated', { useTz: false }).notNullable()
     })
 }
 
