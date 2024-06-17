904ef59e4835172f4ea11b370d2a5b862a977f7a
Jason Claxton <jason.claxton@defra.gov.uk>
2024-06-17 15:23:05 +0100

    Create unit tests for service

--- app/services/jobs/session-cleanup/process-session-storage-cleanup.service.js
+++ app/services/jobs/session-cleanup/process-session-storage-cleanup.service.js
@@ -6,28 +6,24 @@
  */
 
 const { calculateAndLogTimeTaken, currentTimeInNanoseconds } = require('../../../lib/general.lib.js')
-const Session = require('../../../models/session.model.js')
+const SessionModel = require('../../../models/session.model.js')
 
 /**
  * Deletes any temporary session records where the `created_at` date is more than 1 day ago
  */
 async function go () {
-  try {
-    const startTime = currentTimeInNanoseconds()
+  const startTime = currentTimeInNanoseconds()
 
-    const numberOfRowsDeleted = await _deleteSessionRecords()
+  const numberOfRowsDeleted = await _deleteSessionRecords()
 
-    calculateAndLogTimeTaken(startTime, 'Session storage cleanup job complete', { rowsDeleted: numberOfRowsDeleted })
-  } catch (error) {
-    global.GlobalNotifier.omfg('Session storage cleanup job failed', null, error)
-  }
+  calculateAndLogTimeTaken(startTime, 'Session storage cleanup job complete', { rowsDeleted: numberOfRowsDeleted })
 }
 
 async function _deleteSessionRecords () {
   const maxAgeInDays = 1
   const maxSessionAge = new Date(new Date().setDate(new Date().getDate() - maxAgeInDays)).toISOString()
 
-  const numberOfRowsDeleted = await Session.query()
+  const numberOfRowsDeleted = await SessionModel.query()
     .delete()
     .where('created_at', '<', maxSessionAge)
 
