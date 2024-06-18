6c27f4932874db1c4335e54febbedc15ad487801
jonathangoulding <jonathangoulding21@gmail.com>
2024-06-18 14:54:24 +0100

    Add requirements for returns view page
    https://eaflood.atlassian.net/browse/WATER-4422
    
    The requirements for return journey has been built and the check page has been finished. This showed a page with requirements for returns allowing users to make changes to requirements for returns.
    
    This piece of work will replicate the requirements for returns check page but will not allow users to make changes (the card and information is identical, apart from the title and a return reference).
    
    There will be a link "View" on the licence set up page that links to this page. That is added as part of this change. Work for this was done here - https://github.com/DEFRA/water-abstraction-system/pull/1054

---
+++ app/services/return-requirements/view.service.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/view` page
+ * @module ViewService
+ */
+
+const FetchRequirementsForReturnsService = require('./fetch-requirements-for-returns.service.js')
+
+/**
+ * Orchestrates fetching and presenting the data for `/return-requirements/{sessionId}/view` page
+ *
+ * @param {string} returnVersionId - The UUID for return requirement version
+ *
+ * @returns {Promise<Object>} page data needed by the view template
+ */
+async function go (returnVersionId) {
+  // const requirementsForReturns = await FetchRequirementsForReturnsService.go(returnVersionId)
+
+  return {
+    activeNavBar: 'search'
+  }
+}
+
+module.exports = {
+  go
+}
