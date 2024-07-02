24d632de6c83cc27e401a230a1731d4c9634d42f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-07-02 23:57:18 +0100

    Refactor presenter
    We started with the aim of just updating the presenter to use the existing bill run type formatter. But we then spotted that the unit tests didn't follow our standard pattern of setting up test conditions in the `beforeEach()` (it was doing it in the `it`)
    
    It was also testing things that are using existing formatters that have their own tests which means there is no need to repeat them here.
    
    Finally, we felt we could simplify things if we renamed things to just be as they are (which meant some tweaks to the view as well). We also dropped `legacyId` from the presenter result because nothing in the view is using it.

--- test/presenters/licences/view-licence-bills.presenter.test.js
+++ test/presenters/licences/view-licence-bills.presenter.test.js
@@ -4,76 +4,99 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = exports.lab = Lab.script()
+const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Thing under test
 const ViewLicenceBillsPresenter = require('../../../app/presenters/licences/view-licence-bills.presenter.js')
 
 describe('View Licence Bills presenter', () => {
+  let bill
+
   describe('when provided with a bills data', () => {
+    beforeEach(() => {
+      bill = _bill()
+    })
+
     it('correctly presents the data', () => {
-      const result = ViewLicenceBillsPresenter.go(_bills())
+      const result = ViewLicenceBillsPresenter.go([bill])
 
       expect(result).to.equal({
         activeTab: 'bills',
         bills: [{
-          account: 'acc123',
-          accountId: 'bicc1233',
-          billNumber: 'inv123',
+          accountNumber: 'BA1234443S',
+          billingAccountId: '2563bda0-73d8-4055-b3e7-421cf188d4dc',
+          billId: 'dfed8cdd-05c0-4f03-9a95-a7bae74fe7be',
+          billNumber: 'WAC0003872T',
+          billRunType: 'Annual',
           credit: false,
           dateCreated: '1 January 2020',
-          financialYear: '2021',
-          id: 'id123',
-          legacyId: null,
-          runType: 'annual',
+          financialYearEnding: '2021',
           total: '£1,234,567.89'
         }]
       })
     })
-    it('correctly formats the created date to convention', () => {
-      const result = ViewLicenceBillsPresenter.go(_bills())
-      expect(result.bills[0].dateCreated).to.equal('1 January 2020')
-    })
 
-    it('correctly formats the currency to UK standard', () => {
-      const result = ViewLicenceBillsPresenter.go(_bills())
-      expect(result.bills[0].total).to.equal('£1,234,567.89')
-    })
+    describe('the "bills" property', () => {
+      describe('for each bill returned', () => {
+        describe('when the invoice number exists', () => {
+          it('correctly formats the "billNumber" to the invoice number', () => {
+            const result = ViewLicenceBillsPresenter.go([bill])
 
-    it('correctly formats the two part tariff batch type', () => {
-      const result = ViewLicenceBillsPresenter.go(_billsTwoPartTariff())
-      expect(result.bills[0].runType).to.equal('two part tariff')
-    })
+            expect(result.bills[0].billNumber).to.equal('WAC0003872T')
+          })
+        })
 
-    describe('billNumber', () => {
-      it('correctly formats the "billNumber" to the invoice number', () => {
-        const result = ViewLicenceBillsPresenter.go(_bills())
-        expect(result.bills[0].billNumber).to.equal('inv123')
-      })
+        describe('when it is flagged as deminimis', () => {
+          beforeEach(() => {
+            bill.invoiceNumber = null
+            bill.deminimis = true
+          })
 
-      it('correctly formats the "billNumber" to "De minimis bill"', () => {
-        const bill = _bill()
-        bill.invoiceNumber = null
-        bill.deminimis = true
-        const result = ViewLicenceBillsPresenter.go([bill])
-        expect(result.bills[0].billNumber).to.equal('De minimis bill')
-      })
+          it('correctly formats the "billNumber" to "De minimis bill"', () => {
+            const result = ViewLicenceBillsPresenter.go([bill])
 
-      it('correctly formats the "billNumber" to "NALD revised bill"', () => {
-        const bill = _bill()
-        bill.invoiceNumber = null
-        bill.legacyId = 'lgcy'
-        const result = ViewLicenceBillsPresenter.go([bill])
-        expect(result.bills[0].billNumber).to.equal('NALD revised bill')
-      })
+            expect(result.bills[0].billNumber).to.equal('De minimis bill')
+          })
+        })
 
-      it('correctly formats the "billNumber" to "Zero value bill"', () => {
-        const bill = _bill()
-        bill.invoiceNumber = null
-        bill.netAmount = 0
-        const result = ViewLicenceBillsPresenter.go([bill])
-        expect(result.bills[0].billNumber).to.equal('Zero value bill')
+        describe('when it has a legacy ID', () => {
+          beforeEach(() => {
+            bill.invoiceNumber = null
+            bill.legacyId = '100456'
+          })
+
+          it('correctly formats the "billNumber" to "NALD revised bill"', () => {
+            const result = ViewLicenceBillsPresenter.go([bill])
+
+            expect(result.bills[0].billNumber).to.equal('NALD revised bill')
+          })
+        })
+
+        describe('when it has a zero net amount', () => {
+          beforeEach(() => {
+            bill.invoiceNumber = null
+            bill.netAmount = 0
+          })
+
+          it('correctly formats the "billNumber" to "Zero value bill"', () => {
+            const result = ViewLicenceBillsPresenter.go([bill])
+
+            expect(result.bills[0].billNumber).to.equal('Zero value bill')
+          })
+        })
+
+        describe('when it has no invoice number, deminimis flag, legacy ID, and net amount is not zero', () => {
+          beforeEach(() => {
+            bill.invoiceNumber = null
+          })
+
+          it('returns an empty string', () => {
+            const result = ViewLicenceBillsPresenter.go([bill])
+
+            expect(result.bills[0].billNumber).to.equal('')
+          })
+        })
       })
     })
   })
@@ -81,36 +104,16 @@ describe('View Licence Bills presenter', () => {
 
 function _bill () {
   return {
-    accountNumber: 'acc123',
-    billRun: { batchType: 'annual' },
-    billingAccountId: 'bicc1233',
+    accountNumber: 'BA1234443S',
+    billRun: { batchType: 'annual', scheme: 'sroc', summer: false },
+    billingAccountId: '2563bda0-73d8-4055-b3e7-421cf188d4dc',
     createdAt: new Date('2020-01-01'),
     credit: false,
     deminimis: false,
     financialYearEnding: '2021',
-    id: 'id123',
-    invoiceNumber: 'inv123',
+    id: 'dfed8cdd-05c0-4f03-9a95-a7bae74fe7be',
+    invoiceNumber: 'WAC0003872T',
     legacyId: null,
     netAmount: 123456789
   }
-}
-
-function _bills () {
-  return [_bill()]
-}
-
-function _billsTwoPartTariff () {
-  return [{
-    accountNumber: 'acc123',
-    billRun: { batchType: 'two_part_tariff' },
-    billingAccountId: 'bicc1233',
-    createdAt: new Date('2020-01-01'),
-    credit: false,
-    deminimis: false,
-    financialYearEnding: '2021',
-    id: 'id123',
-    invoiceNumber: 'inv123',
-    legacyId: null,
-    netAmount: 123456789
-  }]
 }
