5291606313a8475a1d3a3554118258a586a608a1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-07-02 14:19:49 +0100

    Doh!

--- test/services/licences/view-licence-summary.service.test.js
+++ test/services/licences/view-licence-summary.service.test.js
@@ -18,7 +18,7 @@ const ViewLicenceService = require('../../../app/services/licences/view-licence.
 // Thing under test
 const ViewLicenceSummaryService = require('../../../app/services/licences/view-licence-summary.service.js')
 
-describe.only('View Licence Summary service', () => {
+describe('View Licence Summary service', () => {
   const testId = '2c80bd22-a005-4cf4-a2a2-73812a9861de'
 
   let fetchLicenceResult
