2563cfd034e7c85c3696b621f35fa4d86671ca6c
Rebecca Ransome <beckyrose200@aol.com>
2024-07-10 10:54:34 +0100

    Remove duplicate deletion

--- app/services/data/tear-down/public-schema.service.js
+++ app/services/data/tear-down/public-schema.service.js
@@ -68,22 +68,6 @@ async function _deleteAllTestData () {
 
   DELETE
   FROM
-    "public"."review_charge_elements" AS "rce"
-      USING "public"."review_charge_references" AS "rcr",
-    "public"."review_charge_versions" AS "rcv",
-    "public"."review_licences" AS "rl",
-    "public"."bill_runs" AS "br",
-    "public"."regions" AS "r"
-  WHERE
-    "r"."nald_region_id" = 9
-    AND "rce"."review_charge_reference_id" = "rcr"."id"
-    AND "rcr"."review_charge_version_id" = "rcv"."id"
-    AND "rcv"."review_licence_id" = "rl"."id"
-    AND "rl"."bill_run_id" = "br"."id"
-    AND "br"."region_id" = "r"."id";
-
-  DELETE
-  FROM
     "public"."review_charge_references" AS "rcr"
       USING "public"."review_charge_versions" AS "rcv",
     "public"."review_licences" AS "rl",
