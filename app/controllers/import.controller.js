cb3b39351f2e0191bf90141e9472909c97eb95ee
jonathangoulding <jonathangoulding21@gmail.com>
2024-07-17 08:55:07 +0100

    test: import controller

--- app/controllers/import.controller.js
+++ app/controllers/import.controller.js
@@ -1,17 +1,22 @@
 'use strict'
 
 const LegacyImportLicenceService = require('../services/import/legacy-licence.service.js')
+const Boom = require('@hapi/boom')
 
 /**
  * Controller for /import
  * @module ImportController
  */
 async function licence (request, h) {
-  const { licenceRef } = request.payload
+  try {
+    const { licenceRef } = request.payload
 
-  await LegacyImportLicenceService.go(licenceRef)
+    await LegacyImportLicenceService.go(licenceRef)
 
-  return h.response().code(204)
+    return h.response().code(204)
+  } catch (error) {
+    return Boom.badImplementation(error.message)
+  }
 }
 
 module.exports = {
---
+++ test/controllers/import.controller.test.js
@@ -0,0 +1,76 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Things we need to stub
+const LegacyImportLicenceService = require('../../app/services/import/legacy-licence.service.js')
+
+// For running our service
+const { init } = require('../../app/server.js')
+
+describe('Import controller', () => {
+  let options
+  let server
+
+  beforeEach(async () => {
+    // Create server before each test
+    server = await init()
+
+    // We silence any calls to server.logger.error made in the plugin to try and keep the test output as clean as
+    // possible
+    Sinon.stub(server.logger, 'error')
+
+    // We silence sending a notification to our Errbit instance using Airbrake
+    Sinon.stub(server.app.airbrake, 'notify').resolvesThis()
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('/import/licence', () => {
+    describe('POST /import/licence', () => {
+      beforeEach(async () => {
+        options = {
+          method: 'POST',
+          url: '/import/licence',
+          payload: {
+            licenceRef: 'mock-licence-ref'
+          }
+        }
+      })
+
+      describe('when a request is valid', () => {
+        beforeEach(() => {
+          Sinon.stub(LegacyImportLicenceService, 'go').resolves()
+        })
+
+        it('redirects to select return start date page', async () => {
+          const response = await server.inject(options)
+
+          expect(response.statusCode).to.equal(204)
+        })
+      })
+
+      describe('when a request does include the licence ref', () => {
+        beforeEach(() => {
+          options.payload = {}
+
+          Sinon.stub(LegacyImportLicenceService, 'go').rejects()
+        })
+
+        it('redirects to select return start date page', async () => {
+          const response = await server.inject(options)
+
+          expect(response.statusCode).to.equal(500)
+        })
+      })
+    })
+  })
+})
