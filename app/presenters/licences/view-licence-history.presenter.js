e5e91af4bdc042e73a75b98f49272a927e6f76fd
Roble <roblesiyad123@gmail.com>
2024-07-31 10:44:05 +0100

    Format methods alphabetically in view licence history presenter

--- app/presenters/licences/view-licence-history.presenter.js
+++ app/presenters/licences/view-licence-history.presenter.js
@@ -3,25 +3,48 @@
 const { formatLongDate } = require('../base.presenter.js')
 
 function go (history) {
-  const { entries, licence, testEntries2 } = history
+  const { entries, licence } = history
 
   return {
     licenceId: licence.id,
     licenceRef: licence.licenceRef,
-    entries: _entries(entries),
-    testEntries: _entries2(testEntries2)
+    entries: _entries(entries)
   }
 }
 
 function _entries (entries) {
+  const { licenceVersions, chargeVersions, returnVersions } = entries
+
+  const sortedEntries = _sortEntriesByCreatedAt([...licenceVersions, ...chargeVersions, ...returnVersions])
+
+  const mappedEntries = _mapEntries(sortedEntries)
+
+  return mappedEntries
+}
+
+function _link (entryType, entryId, licenceId) {
+  if (entryType === 'charge-version') {
+    return `/licences/${licenceId}/charge-information/${entryId}/view`
+  }
+
+  if (entryType === 'return-version') {
+    return `/system/return-requirements/${entryId}/view`
+  }
+
+  return null
+}
+
+function _mapEntries (entries) {
   return entries.map((entry) => {
     return {
-      type: _type(entry.entry_type),
+      type: _type(entry.entryType),
       reason: entry.reason,
-      dateCreated: formatLongDate(entry.created_at),
-      createdBy: entry.created_by ? entry.created_by : 'Migrated from NALD',
-      note: entry.note,
-      link: _link(entry.entry_type, entry.entry_id, entry.licence_id)
+      dateCreated: formatLongDate(entry.createdAt),
+      createdBy: entry.createdBy ? entry.createdBy : 'Migrated from NALD',
+      note: entry.note ? entry.note : null,
+      version: entry.versionNumber,
+      source: entry.source ? entry.source : null,
+      link: _link(entry.entryType, entry.entryId, entry.licenceId)
     }
   })
 }
@@ -38,49 +61,12 @@ function _type (entryType) {
   return 'Licence version'
 }
 
-function _link (entryType, entryId, licenceId) {
-  if (entryType === 'charge-version') {
-    return `/licences/${licenceId}/charge-information/${entryId}/view`
-  }
-
-  if (entryType === 'return-version') {
-    return `/system/return-requirements/${entryId}/view`
-  }
-
-  return null
-}
-
-function _entries2 (entries) {
-  const { licenceVersions, chargeVersions, returnVersions } = entries
-
-  entries = _sortEntriesByCreatedAt([...licenceVersions, ...chargeVersions, ...returnVersions])
-
-  const mappedEntries = _mapEntries(entries)
-
-  return mappedEntries
-}
-
 function _sortEntriesByCreatedAt (entries) {
   entries.sort((a, b) => {
     return new Date(b.createdAt) - new Date(a.createdAt)
   })
 
   return entries
-}
-
-function _mapEntries (entries) {
-  return entries.map((entry) => {
-    return {
-      type: _type(entry.entryType),
-      reason: entry.reason,
-      dateCreated: formatLongDate(entry.createdAt),
-      createdBy: entry.createdBy ? entry.createdBy : 'Migrated from Nald',
-      note: entry.note ? entry.note : null,
-      version: entry.versionNumber,
-      source: entry.source ? entry.source : null,
-      link: _link(entry.entryType, entry.entryId, entry.licenceId)
-    }
-  })
 }
 
 module.exports = {
