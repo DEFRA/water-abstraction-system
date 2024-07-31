6421803bb6f59fd99f6290dab28c4f652615bf05
Roble <roblesiyad123@gmail.com>
2024-07-31 11:45:35 +0100

    Update licence controller tests to expect 'Charging agreements' instead of the old title 'Agreements'

--- test/controllers/licences.controller.test.js
+++ test/controllers/licences.controller.test.js
@@ -23,7 +23,7 @@ const ViewLicenceSummaryService = require('../../app/services/licences/view-lice
 // For running our service
 const { init } = require('../../app/server.js')
 
-describe('Licences controller', () => {
+describe.only('Licences controller', () => {
   let options
   let server
 
@@ -339,7 +339,7 @@ describe('Licences controller', () => {
         expect(response.payload).to.contain('Make licence non-chargeable')
 
         // Agreements
-        expect(response.payload).to.contain('Agreements')
+        expect(response.payload).to.contain('Charging agreements')
         // Agreements table headers
         expect(response.payload).to.contain('Agreement')
         expect(response.payload).to.contain('Date signed')
@@ -367,7 +367,7 @@ describe('Licences controller', () => {
         expect(response.payload).to.contain('No requirements for returns have been set up for this licence.')
         expect(response.payload).to.contain('Charge information')
         expect(response.payload).to.contain('No charge information for this licence.')
-        expect(response.payload).to.contain('Agreements')
+        expect(response.payload).to.contain('Charging agreements')
         expect(response.payload).to.contain('No agreements for this licence.')
       })
     })
