8888bbc4be8c51ed38de727f3cd0af96a0503a7c
Roble <roblesiyad123@gmail.com>
2024-07-31 16:27:16 +0100

    Add whereNull check for deleted licence agreements in fetch agreements service

--- app/services/licences/fetch-agreements.service.js
+++ app/services/licences/fetch-agreements.service.js
@@ -21,6 +21,7 @@ async function go (licenceRef) {
 async function _fetch (licenceRef) {
   return LicenceAgreementModel.query()
     .where('licenceRef', licenceRef)
+    .whereNull('deleted_at')
     .select([
       'id',
       'startDate',
