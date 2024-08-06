695139d048461970b1c9d042beb18f8dd6c648af
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-06 12:04:43 +0100

    Add documentation to RegionHelper select()

--- test/support/helpers/region.helper.js
+++ test/support/helpers/region.helper.js
@@ -49,6 +49,20 @@ function defaults (data = {}) {
   }
 }
 
+/**
+ * Select an entry from the reference data entries seeded at the start of testing
+ *
+ * Because this helper is linked to a reference record instead of a transaction, we don't expect these to be created
+ * when using the service.
+ *
+ * So, they are seeded automatically when tests are run. Tests that need to link to a record can use this method to
+ * select a specific entry, or have it it return one at random.
+ *
+ * @param {Number} [index=-1] - The reference entry to select. Defaults to -1 which means an entry will be returned at
+ * random from the reference data
+ *
+ * @returns {Object} The selected reference entry or one picked at random
+ */
 function select (index = -1) {
   if (index > -1) {
     return Regions.data[index]
