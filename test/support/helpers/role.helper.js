c579056258e086671a0cd0dd5eac0da42f4ddfc2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-06 17:49:47 +0100

    Update RoleHelper

--- test/support/helpers/role.helper.js
+++ test/support/helpers/role.helper.js
@@ -4,7 +4,11 @@
  * @module RoleHelper
  */
 
+const { selectRandomEntry } = require('../general.js')
 const RoleModel = require('../../../app/models/role.model.js')
+const Roles = require('../../../db/seeds/data/roles.js')
+
+const DEFAULT_INDEX = 8
 
 /**
  * Add a new role
@@ -46,7 +50,32 @@ function defaults (data = {}) {
   }
 }
 
+/**
+ * Select an entry from the reference data entries seeded at the start of testing
+ *
+ * Because this helper is linked to a reference record instead of a transaction, we don't expect these to be created
+ * when using the service.
+ *
+ * So, they are seeded automatically when tests are run. Tests that need to link to a record can use this method to
+ * select a specific entry, or have it it return one at random.
+ *
+ * @param {Number} [index=-1] - The reference entry to select. Defaults to -1 which means an entry will be returned at
+ * random from the reference data
+ *
+ * @returns {Object} The selected reference entry or one picked at random
+ */
+function select (index = -1) {
+  if (index > -1) {
+    return Roles.data[index]
+  }
+
+  return selectRandomEntry(Roles.data)
+}
+
 module.exports = {
   add,
-  defaults
+  data: Roles.data,
+  DEFAULT_INDEX,
+  defaults,
+  select
 }
