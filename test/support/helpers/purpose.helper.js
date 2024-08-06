5f34de771634256262d9a2e0fa8aa92888ff376a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-06 12:57:58 +0100

    Update PurposeHelper
    We add the select and expose the data just as we did with RegionHelper.

--- test/support/helpers/purpose.helper.js
+++ test/support/helpers/purpose.helper.js
@@ -4,8 +4,11 @@
  * @module PurposeHelper
  */
 
+const { randomInteger, selectRandomEntry } = require('../general.js')
 const PurposeModel = require('../../../app/models/purpose.model.js')
-const { randomInteger } = require('../general.js')
+const Purposes = require('../../../db/seeds/data/purposes.js')
+
+const DEFAULT_INDEX = 39
 
 /**
  * Add a new purpose
@@ -38,17 +41,34 @@ function add (data = {}) {
  * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
  */
 function defaults (data = {}) {
-  const defaults = {
-    legacyId: generatePurposeCode(),
-    description: 'Spray Irrigation - Storage',
-    lossFactor: 'high',
-    twoPartTariff: true
-  }
+  const defaults = select(DEFAULT_INDEX)
 
   return {
     ...defaults,
     ...data
   }
+}
+
+/**
+ * Select an entry from the reference data entries seeded at the start of testing
+ *
+ * Because this helper is linked to a reference record instead of a transaction, we don't expect these to be created
+ * when using the service.
+ *
+ * So, they are seeded automatically when tests are run. Tests that need to link to a record can use this method to
+ * select a specific entry, or have it it return one at random.
+ *
+ * @param {Number} [index=-1] - The reference entry to select. Defaults to -1 which means an entry will be returned at
+ * random from the reference data
+ *
+ * @returns {Object} The selected reference entry or one picked at random
+ */
+function select (index = -1) {
+  if (index > -1) {
+    return Purposes.data[index]
+  }
+
+  return selectRandomEntry(Purposes.data)
 }
 
 function generatePurposeCode () {
@@ -59,6 +79,9 @@ function generatePurposeCode () {
 
 module.exports = {
   add,
+  DEFAULT_INDEX,
+  data: Purposes.data,
   defaults,
-  generatePurposeCode
+  generatePurposeCode,
+  select
 }
