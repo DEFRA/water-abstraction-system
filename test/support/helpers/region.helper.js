2ca95aa86e28f18c3693ab9f7955f9e5a5dc5986
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-06 12:05:39 +0100

    Remove add from RegionHelper
    The final step to switching to reference data; remove the add() so no one is tempted to re-use it by accident in new tests.

--- test/support/helpers/region.helper.js
+++ test/support/helpers/region.helper.js
@@ -5,32 +5,9 @@
  */
 
 const { selectRandomEntry } = require('../general.js')
-const RegionModel = require('../../../app/models/region.model.js')
 const Regions = require('../../../db/seeds/data/regions.js')
 
 const TEST_REGION_INDEX = 8
-
-/**
- * Add a new region
- *
- * If no `data` is provided, default values will be used. These are
- *
- * - `chargeRegionId` - [selected based on randomly generated naldRegionId]
- * - `naldRegionId` - [randomly generated - 8]
- * - `name` - Kingdom of Avalon
- * - `displayName` - Avalon
- *
- * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
- *
- * @returns {Promise<module:RegionModel>} The instance of the newly created record
- */
-function add (data = {}) {
-  const insertData = defaults(data)
-
-  return RegionModel.query()
-    .insert({ ...insertData })
-    .returning('*')
-}
 
 /**
  * Returns the defaults used
@@ -72,7 +49,6 @@ function select (index = -1) {
 }
 
 module.exports = {
-  add,
   data: Regions.data,
   defaults,
   select,
