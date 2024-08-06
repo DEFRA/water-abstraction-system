2f89d0c1dad4c37c87e7508b578548a7d87df083
Rebecca Ransome <beckyrose200@aol.com>
2024-08-06 12:14:59 +0100

    Licence Supplementary Year Helper

---
+++ test/support/helpers/licence-supplementary-year.helper.js
@@ -0,0 +1,53 @@
+'use strict'
+
+/**
+ * @module LicenceSupplementaryYearHelper
+ */
+
+const { generateUUID } = require('../../../app/lib/general.lib.js')
+const LicenceSupplementaryYearModel = require('../../../app/models/licence-supplementary-year.model.js')
+
+/**
+ * Add a new review licence record for 2pt matching
+ *
+ * If no `data` is provided, default values will be used. These are
+ *
+ * - `licenceId` - [random UUID]
+ * - `financialYearEnd` - 2023
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
+ *
+ * @returns {Promise<module:LicenceSupplementaryYearModel>} The instance of the newly created record
+ */
+function add (data = {}) {
+  const insertData = defaults(data)
+
+  return LicenceSupplementaryYearModel.query()
+    .insert({ ...insertData })
+    .returning('*')
+}
+
+/**
+ * Returns the defaults used
+ *
+ * It will override or append to them any data provided. Mainly used by the `add()` method, we make it available
+ * for use in tests to avoid having to duplicate values.
+ *
+ * @param {Object} [data] Any data you want to use instead of the defaults used here in the database
+ */
+function defaults (data = {}) {
+  const defaults = {
+    licenceId: generateUUID(),
+    financialYearEnd: 2023
+  }
+
+  return {
+    ...defaults,
+    ...data
+  }
+}
+
+module.exports = {
+  add,
+  defaults
+}
