e9760a183d7232e81b810f239fe70aa04e6127d7
Roble <roblesiyad123@gmail.com>
2024-08-06 15:36:47 +0100

    Divide _fetchEntries() query into three separate queries for charge, licence and return versions

--- app/services/licences/fetch-licence-history.service.js
+++ app/services/licences/fetch-licence-history.service.js
@@ -3,14 +3,20 @@
 const { db } = require('../../../db/db.js')
 const { ref } = require('objection')
 
+const ChargeVersionModel = require('../../models/charge-version.model.js')
 const LicenceModel = require('../../models/licence.model.js')
+const LicenceVersionModel = require('../../models/licence-version.model.js')
+const ReturnVersionModel = require('../../models/return-version.model.js')
 
 async function go (licenceId) {
   const licence = await _fetchLicence(licenceId)
-  const results = await _fetchEntries(licenceId)
+
+  const chargeVersions = await _fetchChargeVersions(licenceId)
+  const licenceVersions = await _fetchLicenceVersions(licenceId)
+  const returnVersions = await _fetchReturnVersions(licenceId)
 
   return {
-    entries: results,
+    entries: [...chargeVersions, ...licenceVersions, ...returnVersions],
     licence
   }
 }
@@ -25,69 +31,65 @@ async function _fetchLicence (licenceId) {
     ])
 }
 
-async function _fetchEntries (licenceId) {
-  const licenceEntries = LicenceModel.query()
-    .findById(licenceId)
-    .withGraphFetched('licenceVersions')
-    .modifyGraph('licenceVersions', (builder) => {
-      builder
-        .select(
-          'licenceVersions.licenceId as licenceId',
-          db.raw("'licence-version' as entry_type"),
-          'licenceVersions.id as entry_id',
-          db.raw("'' as reason"),
-          'licenceVersions.createdAt as created_at',
-          db.raw("'' as createdBy"),
-          db.raw("'' as note"),
-          'issue as version_number'
-        )
-        .orderBy([
-          { column: 'createdAt', order: 'desc' },
-          { column: 'issue', order: 'desc' }
-        ])
-    })
-    .withGraphFetched('chargeVersions')
-    .modifyGraph('chargeVersions', (builder) => {
-      builder
-        .select(
-          'chargeVersions.licenceId as licenceId',
-          db.raw("'charge-version' as entry_type"),
-          'chargeVersions.id as entry_id',
-          'public.changeReasons.description as reason',
-          'chargeVersions.createdAt as created_at',
-          ref('createdBy:email').castText().as('created_by'),
-          'water.notes.text as note',
-          'versionNumber as version_number',
-          'chargeVersions.source as source'
-        )
-        .leftJoin('water.notes', 'notes.noteId', '=', 'chargeVersions.noteId')
-        .leftJoin('public.changeReasons', 'changeReasons.id', '=', 'chargeVersions.changeReasonId')
-        .orderBy([
-          { column: 'chargeVersions.createdAt', order: 'desc' },
-          { column: 'chargeVersions.versionNumber', order: 'desc' }
-        ])
-    })
-    .withGraphFetched('returnVersions')
-    .modifyGraph('returnVersions', (builder) => {
-      builder
-        .select(
-          'returnVersions.licenceId as licenceId',
-          db.raw("'return-version' as entry_type"),
-          'returnVersions.id as entry_id',
-          'reason',
-          'returnVersions.createdAt as created_at',
-          'users.username as created_by',
-          'notes as note',
-          'version as version_number'
-        )
-        .leftJoin('public.users', 'users.id', '=', 'returnVersions.createdBy')
-        .orderBy([
-          { column: 'createdAt', order: 'desc' },
-          { column: 'version', order: 'desc' }
-        ])
-    })
+async function _fetchChargeVersions (licenceId) {
+  return ChargeVersionModel.query()
+    .select(
+      'chargeVersions.licenceId as licenceId',
+      db.raw("'charge-version' as entry_type"),
+      'chargeVersions.id as entry_id',
+      'public.changeReasons.description as reason',
+      'chargeVersions.createdAt as created_at',
+      ref('createdBy:email').castText().as('created_by'),
+      'water.notes.text as note',
+      'versionNumber as version_number',
+      'chargeVersions.source as source'
+    )
+    .leftJoin('water.notes', 'notes.noteId', '=', 'chargeVersions.noteId')
+    .leftJoin('public.changeReasons', 'changeReasons.id', '=', 'chargeVersions.changeReasonId')
+    .orderBy([
+      { column: 'chargeVersions.createdAt', order: 'desc' },
+      { column: 'chargeVersions.versionNumber', order: 'desc' }
+    ])
+    .where('chargeVersions.licenceId', licenceId)
+}
 
-  return licenceEntries
+async function _fetchLicenceVersions (licenceId) {
+  return LicenceVersionModel.query()
+    .select(
+      'licenceVersions.licenceId as licenceId',
+      db.raw("'licence-version' as entry_type"),
+      'licenceVersions.id as entry_id',
+      db.raw("'' as reason"),
+      'licenceVersions.createdAt as created_at',
+      db.raw("'' as createdBy"),
+      db.raw("'' as note"),
+      'issue as version_number'
+    )
+    .orderBy([
+      { column: 'createdAt', order: 'desc' },
+      { column: 'issue', order: 'desc' }
+    ])
+    .where('licenceVersions.licenceId', licenceId)
+}
+
+async function _fetchReturnVersions (licenceId) {
+  return ReturnVersionModel.query()
+    .select(
+      'returnVersions.licenceId as licenceId',
+      db.raw("'return-version' as entry_type"),
+      'returnVersions.id as entry_id',
+      'reason',
+      'returnVersions.createdAt as created_at',
+      'users.username as created_by',
+      'notes as note',
+      'version as version_number'
+    )
+    .leftJoin('public.users', 'users.id', '=', 'returnVersions.createdBy')
+    .orderBy([
+      { column: 'createdAt', order: 'desc' },
+      { column: 'version', order: 'desc' }
+    ])
+    .where('returnVersions.licenceId', licenceId)
 }
 
 module.exports = {
