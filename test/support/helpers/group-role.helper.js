57d40bf04aac4027ee7fc6ac27f1b79bcf628516
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-07 00:18:25 +0100

    Update GroupRoleHelper

--- test/support/helpers/group-role.helper.js
+++ test/support/helpers/group-role.helper.js
@@ -4,8 +4,11 @@
  * @module GroupRoleHelper
  */
 
-const { generateUUID } = require('../../../app/lib/general.lib.js')
+const { selectRandomEntry } = require('../general.js')
 const GroupRoleModel = require('../../../app/models/group-role.model.js')
+const GroupRoles = require('../../../db/seeds/data/group-roles.js')
+
+const DEFAULT_INDEX = 18
 
 /**
  * Add a new group role
@@ -36,10 +39,7 @@ function add (data = {}) {
  * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
  */
 function defaults (data = {}) {
-  const defaults = {
-    groupId: generateUUID(),
-    roleId: generateUUID()
-  }
+  const defaults = select(DEFAULT_INDEX)
 
   return {
     ...defaults,
@@ -47,7 +47,32 @@ function defaults (data = {}) {
   }
 }
 
+/**
+ * Select an entry from the reference data entries seeded at the start of testing
+ *
+ * Because this helper is linked to a reference record instead of a transaction, we don't expect these to be created
+ * when using the service.
+ *
+ * So, they are seeded automatically when tests are run. Tests that need to link to a record can use this method to
+ * select a specific entry, or have it it return one at random.
+ *
+ * @param {Number} [index=-1] - The reference entry to select. Defaults to -1 which means an entry will be returned at
+ * random from the reference data
+ *
+ * @returns {Object} The selected reference entry or one picked at random
+ */
+function select (index = -1) {
+  if (index > -1) {
+    return GroupRoles.data[index]
+  }
+
+  return selectRandomEntry(GroupRoles.data)
+}
+
 module.exports = {
   add,
-  defaults
+  data: GroupRoles.data,
+  DEFAULT_INDEX,
+  defaults,
+  select
 }
