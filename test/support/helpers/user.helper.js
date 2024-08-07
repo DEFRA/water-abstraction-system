b0e32d283a9945d29dd69a7ead867866ed8fe2b2
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-07 16:13:58 +0100

    Update UserHelper

--- test/support/helpers/user.helper.js
+++ test/support/helpers/user.helper.js
@@ -4,15 +4,18 @@
  * @module UserHelper
  */
 
-const { randomInteger } = require('../general.js')
+const { randomInteger, selectRandomEntry } = require('../general.js')
 const UserModel = require('../../../app/models/user.model.js')
+const Users = require('../../../db/seeds/data/users.js')
+
+const DEFAULT_INDEX = 4
 
 /**
  * Add a new user
  *
  * If no `data` is provided, default values will be used. These are
  *
- * - `username` - user.name@test.com
+ * - `username` - billing.data@wrls.gov.uk
  * - `password` - P@55word (note that this is salted and hashed before being persisted)
  * - `resetRequired` - 0
  * - `badLogins` - 0
@@ -42,13 +45,7 @@ function add (data = {}) {
  * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
  */
 function defaults (data = {}) {
-  const defaults = {
-    username: 'user.name@test.com',
-    password: 'P@55word',
-    resetRequired: 0,
-    badLogins: 0,
-    application: 'water_admin'
-  }
+  const defaults = select(DEFAULT_INDEX)
 
   return {
     ...defaults,
@@ -57,11 +54,37 @@ function defaults (data = {}) {
 }
 
 function generateUserId () {
-  return randomInteger(100001, 199999)
+  // The last ID in the pre-seeded users is 100009
+  return randomInteger(100010, 199999)
+}
+
+/**
+ * Select an entry from the reference data entries seeded at the start of testing
+ *
+ * Because this helper is linked to a reference record instead of a transaction, we don't expect these to be created
+ * when using the service.
+ *
+ * So, they are seeded automatically when tests are run. Tests that need to link to a record can use this method to
+ * select a specific entry, or have it it return one at random.
+ *
+ * @param {Number} [index=-1] - The reference entry to select. Defaults to -1 which means an entry will be returned at
+ * random from the reference data
+ *
+ * @returns {Object} The selected reference entry or one picked at random
+ */
+function select (index = -1) {
+  if (index > -1) {
+    return Users.data[index]
+  }
+
+  return selectRandomEntry(Users.data)
 }
 
 module.exports = {
   add,
+  data: Users.data,
+  DEFAULT_INDEX,
   defaults,
-  generateUserId
+  generateUserId,
+  select
 }
