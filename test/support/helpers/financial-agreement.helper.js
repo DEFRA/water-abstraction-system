50044381c8d24976918b8c0a2b9b8f7c77cc9b3a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-07 22:43:29 +0100

    Update FinancialAgreementHelper

--- test/support/helpers/financial-agreement.helper.js
+++ test/support/helpers/financial-agreement.helper.js
@@ -5,7 +5,10 @@
  */
 
 const FinancialAgreementModel = require('../../../app/models/financial-agreement.model.js')
-const { randomInteger } = require('../general.js')
+const FinancialAgreements = require('../../../db/seeds/data/financial-agreements.js')
+const { randomInteger, selectRandomEntry } = require('../general.js')
+
+const DEFAULT_INDEX = 3
 
 /**
  * Add a new financial agreement
@@ -36,12 +39,7 @@ async function add (data = {}) {
  * @param {Object} [data] Any data you want to use instead of the defaults used here or in the database
  */
 function defaults (data = {}) {
-  const code = data.code ? data.code : generateFinancialAgreementCode()
-
-  const defaults = {
-    code,
-    description: `Section ${code}`
-  }
+  const defaults = select(DEFAULT_INDEX)
 
   return {
     ...defaults,
@@ -53,8 +51,33 @@ function generateFinancialAgreementCode () {
   return `S${randomInteger(100, 999)}`
 }
 
+/**
+ * Select an entry from the reference data entries seeded at the start of testing
+ *
+ * Because this helper is linked to a reference record instead of a transaction, we don't expect these to be created
+ * when using the service.
+ *
+ * So, they are seeded automatically when tests are run. Tests that need to link to a record can use this method to
+ * select a specific entry, or have it it return one at random.
+ *
+ * @param {Number} [index=-1] - The reference entry to select. Defaults to -1 which means an entry will be returned at
+ * random from the reference data
+ *
+ * @returns {Object} The selected reference entry or one picked at random
+ */
+function select (index = -1) {
+  if (index > -1) {
+    return FinancialAgreements.data[index]
+  }
+
+  return selectRandomEntry(FinancialAgreements.data)
+}
+
 module.exports = {
   add,
+  data: FinancialAgreements.data,
+  DEFAULT_INDEX,
   defaults,
-  generateFinancialAgreementCode
+  generateFinancialAgreementCode,
+  select
 }
