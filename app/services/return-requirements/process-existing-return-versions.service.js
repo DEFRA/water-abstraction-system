25326505b13e16fc0cf855b6ca86b4906b763513
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-08 23:01:39 +0100

    Solution 2

--- app/services/return-requirements/process-existing-return-versions.service.js
+++ app/services/return-requirements/process-existing-return-versions.service.js
@@ -5,8 +5,6 @@
  * @module ProcessExistingReturnVersionsService
  */
 
-const { db } = require('../../../db/db.js')
-
 const ReturnVersionModel = require('../../models/return-version.model.js')
 
 /**
@@ -23,28 +21,29 @@ const ReturnVersionModel = require('../../models/return-version.model.js')
  * if there is no `endDate`
  */
 async function go (licenceId, newVersionStartDate) {
+  const previousVersions = await _previousVersions(licenceId)
   const previousVersionEndDate = _previousVersionEndDate(newVersionStartDate)
 
-  let results
+  let result
 
-  results = await _endLatestVersion(licenceId, newVersionStartDate, previousVersionEndDate)
-  if (results.length > 0) {
+  result = await _endLatestVersion(previousVersions, newVersionStartDate, previousVersionEndDate)
+  if (result) {
     return null
   }
 
-  results = await _insertBetweenVersions(licenceId, newVersionStartDate, previousVersionEndDate)
-  if (results.length > 0) {
-    return results[0].endDate
+  result = await _insertBetweenVersions(previousVersions, newVersionStartDate, previousVersionEndDate)
+  if (result) {
+    return result
   }
 
-  results = await _replaceLatestVersion(licenceId, newVersionStartDate)
-  if (results.length > 0) {
+  result = await _replaceLatestVersion(previousVersions, newVersionStartDate)
+  if (result) {
     return null
   }
 
-  results = await _replacePreviousVersion(licenceId, newVersionStartDate)
-  if (results.length > 0) {
-    return results[0].endDate
+  result = await _replacePreviousVersion(previousVersions, newVersionStartDate)
+  if (result) {
+    return result
   }
 
   return null
@@ -73,23 +72,17 @@ async function go (licenceId, newVersionStartDate) {
  * This function finds return version **3** and updates its end date to the start date of the new return version minus
  * 1 day. We don't care about the end date because the new return version doesn't need one.
  */
-async function _endLatestVersion (licenceId, newVersionStartDate, endDate) {
-  return ReturnVersionModel.query()
-    .patch({ endDate })
-    .where('licenceId', licenceId)
-    .where('startDate', '<', newVersionStartDate)
-    .whereNull('endDate')
-    .returning('id')
-}
+async function _endLatestVersion (previousVersions, newVersionStartDate, endDate) {
+  const match = previousVersions.find((previousVersion) => {
+    return previousVersion.startDate < newVersionStartDate &&
+      previousVersion.endDate === null
+  })
 
-function _previousVersionEndDate (newVersionStartDate) {
-  // NOTE: You have to create a new date from newVersionStartDate else when we call setDate we amend the source
-  // newVersionStartDate passed to the service.
-  const previousVersionEndDate = new Date(newVersionStartDate)
+  if (match) {
+    return _update(match.id, { endDate })
+  }
 
-  previousVersionEndDate.setDate(previousVersionEndDate.getDate() - 1)
-
-  return previousVersionEndDate
+  return null
 }
 
 /**
@@ -115,40 +108,38 @@ function _previousVersionEndDate (newVersionStartDate) {
  * This function finds return version **2** and updates its end date to the start date of the new return version minus
  * 1 day. We also return the end date from version **2** as this needs to be applied to the new return version. Hence,
  * we are _inserting between versions_.
- *
- * ## A note about the query
- *
- * PostgreSQL has an _awesome_ feature called
- * {@link https://www.postgresql.org/docs/current/dml-returning.html|returning}. This allows you to retrieve values from
- * the rows which were modified by a INSERT, UPDATE OR DELETE command. However, it returns the values _after_ a change
- * has been made. In this scenario we need to know the end date of the return version _before_ we update it.
- *
- * We could have solved this by firing two queries; first SELECT to extract the date, then the update. However, through
- * some SQL magic (hence the need to drop to Knex with some raw sprinkled in) we can use
- * {@link https://www.postgresql.org/docs/current/sql-update.html|FROM}. It is intended to allow you to update one table
- * with values from another. But by referring to return versions twice, it gives us access to the old end date in our
- * `returning` clause even after we've updated it.
- *
- * All credit goes to {@link https://stackoverflow.com/a/7927957/6117745|Erwin Brandstetter} for this.
- *
- * Within the service, we could have ignored the issue of concurrent write load and used Erwin's simpler solution. But
- * when unit testing the speed is such that you encounter it. Hence, we need to lock the row we want to edit which
- * necessitates using a subquery i.e. Erwin's more complex solution.
  */
-async function _insertBetweenVersions (licenceId, newVersionStartDate, endDate) {
-  const subQuery = db('returnVersions AS rv').withSchema('public')
-    .select('rv.id', 'rv.end_date')
-    .where('rv.licenceId', licenceId)
-    .where('rv.startDate', '<', newVersionStartDate)
-    .where('rv.endDate', '>', newVersionStartDate)
-    .limit(1)
-    .forUpdate() // Lock the selected rows
+async function _insertBetweenVersions (previousVersions, newVersionStartDate, endDate) {
+  const match = previousVersions.find((previousVersion) => {
+    return previousVersion.startDate < newVersionStartDate &&
+      previousVersion.endDate > newVersionStartDate
+  })
 
-  return db('returnVersions AS rvx').withSchema('public')
-    .update({ endDate })
-    .updateFrom(subQuery.as('rvy'))
-    .whereRaw('rvx.id = rvy.id')
-    .returning(['rvx.id', db.raw('?? as new_end_date', 'rvx.end_date'), 'rvy.endDate'])
+  if (match) {
+    await _update(match.id, { endDate })
+
+    return match.endDate
+  }
+
+  return null
+}
+
+function _previousVersionEndDate (newVersionStartDate) {
+  // NOTE: You have to create a new date from newVersionStartDate else when we call setDate we amend the source
+  // newVersionStartDate passed to the service.
+  const previousVersionEndDate = new Date(newVersionStartDate)
+
+  previousVersionEndDate.setDate(previousVersionEndDate.getDate() - 1)
+
+  return previousVersionEndDate
+}
+
+function _previousVersions (licenceId) {
+  return ReturnVersionModel.query()
+    .select(['endDate', 'id', 'startDate'])
+    .where('licenceId', licenceId)
+    .where('status', 'current')
+    .orderBy('startDate', 'desc')
 }
 
 /**
@@ -174,13 +165,21 @@ async function _insertBetweenVersions (licenceId, newVersionStartDate, endDate) 
  * This function finds return version **3** and updates its status to `superseded`. We don't care about the end date
  * because the new return version doesn't need one.
  */
-async function _replaceLatestVersion (licenceId, newVersionStartDate) {
-  return ReturnVersionModel.query()
-    .patch({ status: 'superseded' })
-    .where('licenceId', licenceId)
-    .where('startDate', newVersionStartDate)
-    .whereNull('endDate')
-    .returning('id')
+async function _replaceLatestVersion (previousVersions, newVersionStartDate) {
+  const match = previousVersions.find((previousVersion) => {
+    // NOTE: When you use the equality operator JavaScript will check for reference equality. Dates being objects this
+    // will always return false, even though they refer to the exact same time. This means you need to convert them to
+    // a more primitive value like a string or number first. `getTime()` seems to be the winner according to
+    // stack overflow https://stackoverflow.com/a/4587089/6117745
+    return previousVersion.startDate.getTime() === newVersionStartDate.getTime() &&
+      previousVersion.endDate === null
+  })
+
+  if (match) {
+    return _update(match.id, { status: 'superseded' })
+  }
+
+  return null
 }
 
 /**
@@ -207,13 +206,23 @@ async function _replaceLatestVersion (licenceId, newVersionStartDate) {
  * version **2** as this needs to be applied to the new return version as its end date. Hence, we are _replacing a
  * previous version_.
  */
-async function _replacePreviousVersion (licenceId, newVersionStartDate) {
-  return ReturnVersionModel.query()
-    .patch({ status: 'superseded' })
-    .where('licenceId', licenceId)
-    .where('startDate', newVersionStartDate)
-    .whereNotNull('endDate')
-    .returning('id', 'endDate')
+async function _replacePreviousVersion (previousVersions, newVersionStartDate) {
+  const match = previousVersions.find((previousVersion) => {
+    return previousVersion.startDate.getTime() === newVersionStartDate.getTime() &&
+      previousVersion.endDate > newVersionStartDate
+  })
+
+  if (match) {
+    await _update(match.id, { status: 'superseded' })
+
+    return match.endDate
+  }
+
+  return null
+}
+
+async function _update (id, payload) {
+  return ReturnVersionModel.query().patch(payload).where({ id })
 }
 
 module.exports = {
