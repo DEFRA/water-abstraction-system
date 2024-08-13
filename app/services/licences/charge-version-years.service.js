c1be15913da2f410b0dfd60b32cb6e0814e329f3
Rebecca Ransome <beckyrose200@aol.com>
2024-08-13 09:24:30 +0100

    Refactor charge version service

--- app/services/licences/charge-version-years.service.js
+++ app/services/licences/charge-version-years.service.js
@@ -1,14 +1,17 @@
 'use strict'
 
 /**
- *
+ * Retrieves details about a charge version and determines relevant financial years for supplementary billing
  * @module ChargeVersionYearsService
  */
 
 const ChargeVersionModel = require('../../models/charge-version.model.js')
 
+const MONTHS_UNTIL_FINANCIAL_YEAR_START = 3
+const LAST_PRE_SROC_FINANCIAL_YEAR_END = 2022
+
 /**
- *  Retrieves details about a charge version and determines relevant financial years for supplementary billing
+ * Retrieves details about a charge version and determines relevant financial years for supplementary billing
  *
  * If a `chargeVersionId` is provided, the service fetches the associated charge version details, including charge
  * references and the licence information. It then checks for the presence of two-part tariff indicators within the
@@ -60,38 +63,33 @@ async function _getChargeVersion (chargeVersionId) {
   return chargeVersion
 }
 
+/**
+ * When flagging a licence for the supplementary bill run, we need to consider which financial years have been
+ * impacted by the change on the charge version. We only care about the financial year ends. So if the startDate for
+ * a new chargeVersion is `2022-05-31`, the financial year end is considered to be `2023` since the financial years
+ * run April to March. Same goes for if a charge versions endDate is `2024-04-05`, the financial year end is `2025`.
+ */
+function _getAdjustedFinancialYearEnd (date) {
+  let year = date.getFullYear()
+
+  if (date.getMonth() >= MONTHS_UNTIL_FINANCIAL_YEAR_START) {
+    year++
+  }
+
+  return year
+}
+
 function _getFinancialYears (startDate, endDate) {
-  const lastPreSrocFinancialYearEnd = 2022
   const years = []
 
-  let endYear
-  let startYear = startDate.getFullYear()
-
+  const startYear = _getAdjustedFinancialYearEnd(startDate)
   // As some charge versions don't have an end date we need to take this into consideration
-  if (!endDate) {
-    endDate = new Date()
-  }
-
-  endYear = endDate.getFullYear()
-
-  // When flagging a licence for the supplementary bill run, we need to consider which financial years have been
-  // impacted by the change on the charge version. We only care about the financial year ends. So if the startDate for
-  // a new chargeVersion is `2022-05-31`, the financial year end is considered to be `2023` since the financial years
-  // run April to March. Same goes for if a charge versions endDate is `2024-04-05`, the financial year end is `2025`.
-
-  // If the month is after April or after, the financial year end is the following year
-  if (startDate.getMonth() >= 3) {
-    startYear++
-  }
-
-  if (endDate.getMonth() >= 3) {
-    endYear++
-  }
+  const endYear = _getAdjustedFinancialYearEnd(endDate || new Date())
 
   for (let year = startYear; year <= endYear; year++) {
     // SROC supplementary billing started in the financial year 2022/2023. Anything before this year is not considered
     // to be SROC
-    if (year > lastPreSrocFinancialYearEnd) {
+    if (year > LAST_PRE_SROC_FINANCIAL_YEAR_END) {
       years.push(year)
     }
   }
