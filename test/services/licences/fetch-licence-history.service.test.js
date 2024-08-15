774026af56815b83bdc7bb95859dc403988944d6
Roble <roblesiyad123@gmail.com>
2024-08-15 16:48:07 +0100

    Update fetch licence history service test to retrieve matching licence, charge and return versions for a licence

--- test/services/licences/fetch-licence-history.service.test.js
+++ test/services/licences/fetch-licence-history.service.test.js
@@ -9,6 +9,7 @@ const { expect } = Code
 
 // Test helpers
 const ChargeVersionHelper = require('../../support/helpers/charge-version.helper.js')
+const ChargeVersionNoteHelper = require('../../support/helpers/charge-version-note.helper.js')
 const LicenceHelper = require('../../support/helpers/licence.helper.js')
 const LicenceVersionHelper = require('../../support/helpers/licence-version.helper.js')
 const ReturnVersionHelper = require('../../support/helpers/return-version.helper.js')
@@ -17,26 +18,87 @@ const ReturnVersionHelper = require('../../support/helpers/return-version.helper
 const FetchLicenceHistoryService = require('../../../app/services/licences/fetch-licence-history.service.js')
 
 describe('Fetch Licence History service', () => {
+  let chargeVersion
+  let licence
   let licenceId
+  let licenceVersion
+  let returnVersion
 
-  describe('when the licence has contact details', () => {
+  describe('when the licence has licence versions, charge versions and return versions', () => {
     beforeEach(async () => {
-      const licence = await LicenceHelper.add()
+      licence = await LicenceHelper.add()
 
       licenceId = licence.id
 
       const { licenceRef } = licence
 
-      const chargeVersions = await ChargeVersionHelper.add({ licenceId, licenceRef })
-      const LicenceVersions = await LicenceVersionHelper.add({ licenceId })
-      const ReturnVersions = await ReturnVersionHelper.add({ licenceId })
+      const chargeVersionNote = await ChargeVersionNoteHelper.add()
+
+      chargeVersion = await ChargeVersionHelper.add({ licenceId, licenceRef, noteId: chargeVersionNote.id })
+      licenceVersion = await LicenceVersionHelper.add({ licenceId })
+      returnVersion = await ReturnVersionHelper.add({
+        licenceId,
+        modLog: {
+          code: 'XRET',
+          note: 'Returns requirements changed - Operational Instruction 056_08',
+          createdAt: '2008-11-06',
+          createdBy: 'NALD_OWNER',
+          description: 'Changes to Returns requirements April 2008'
+        }
+      })
     })
 
     it('returns the matching licence versions, charge versions and return versions', async () => {
       const result = await FetchLicenceHistoryService.go(licenceId)
 
       expect(result).to.equal({
-
+        entries: [
+          {
+            createdAt: chargeVersion.createdAt,
+            createdBy: null,
+            entryId: chargeVersion.id,
+            entryType: 'charge-version',
+            licenceId,
+            modLog: '',
+            note: 'This is a test note',
+            reason: null,
+            source: 'wrls',
+            versionNumber: 1
+          },
+          {
+            createdAt: licenceVersion.createdAt,
+            createdBy: '',
+            entryId: licenceVersion.id,
+            entryType: 'licence-version',
+            licenceId,
+            modLog: '',
+            note: '',
+            reason: '',
+            versionNumber: 1
+          },
+          {
+            createdAt: returnVersion.createdAt,
+            createdBy: null,
+            entryId: returnVersion.id,
+            entryType: 'return-version',
+            licenceId,
+            modLog: {
+              code: 'XRET',
+              createdAt: '2008-11-06',
+              createdBy: 'NALD_OWNER',
+              description: 'Changes to Returns requirements April 2008',
+              note: 'Returns requirements changed - Operational Instruction 056_08'
+            },
+            note: null,
+            reason: 'new-licence',
+            versionNumber: 100
+          }
+        ],
+        licence: {
+          createdAt: licence.createdAt,
+          id: licenceId,
+          licenceRef: licence.licenceRef
+        }
       })
     })
   })
