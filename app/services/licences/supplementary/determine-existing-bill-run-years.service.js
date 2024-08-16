373142f31948589843f52cec8f7ad2454a877f84
Rebecca Ransome <beckyrose200@aol.com>
2024-08-16 16:35:26 +0100

    Update comment

--- app/services/licences/supplementary/determine-existing-bill-run-years.service.js
+++ app/services/licences/supplementary/determine-existing-bill-run-years.service.js
@@ -14,13 +14,13 @@ const BillRunModel = require('../../../models/bill-run.model.js')
  * This allows those years to be flagged on the licence for supplementary billing.
  *
  * The service has been passed an array of years that the change on the licence covers. It then verify whether an annual
- *  or two-part tariff bill run has already been sent for those years. If it has then the year is returned. We do not
+ * or two-part tariff bill run has already been sent for those years. If it has then the year is returned. We do not
  * want to flag any years on the licence that hasn't had a bill run already created, as the change on the licence will
  * get picked up when the annual/ two-part tariff bill run is created for that year.
  *
  * @param {String} regionId - The UUID of the region to search for
  * @param {Object[]} years - The years that the licence can be flagged for
- * @param {Boolean} twoPartTariff - The bill run type, two-part tariff or annual
+ * @param {Boolean} twoPartTariff - If there are two-part tariff indicators on the licence
  *
  * @returns {Object[]} - The years that can be flagged for supplementary billing
  */
