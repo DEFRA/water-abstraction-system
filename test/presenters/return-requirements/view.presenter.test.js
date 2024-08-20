56c5bc0c0b0b0c366b03a19e746ad03fdccd7f13
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-20 23:38:39 +0100

    Housekeeping - remove redundant describe
    Made me keep putting the `only()` in the wrong place.

--- test/presenters/return-requirements/view.presenter.test.js
+++ test/presenters/return-requirements/view.presenter.test.js
@@ -22,251 +22,249 @@ describe('Return Requirements - View presenter', () => {
     returnVersion = ReturnVersionModel.fromJson(returnVersionData)
   })
 
-  describe('when provided with a return version', () => {
-    it('correctly presents the data', () => {
+  it('correctly presents the data', () => {
+    const result = ViewPresenter.go(returnVersion)
+
+    expect(result).to.equal({
+      additionalSubmissionOptions: {
+        multipleUpload: 'No'
+      },
+      createdBy: 'carol.shaw@atari.com',
+      createdDate: '5 April 2022',
+      licenceId: '761bc44f-80d5-49ae-ab46-0a90495417b5',
+      licenceRef: '01/123',
+      notes: 'A special note',
+      pageTitle: 'Requirements for returns for Mr Ingles',
+      reason: 'New licence',
+      requirements: [
+        {
+          abstractionPeriod: 'From 1 April to 31 October',
+          agreementsExceptions: 'None',
+          frequencyCollected: 'monthly',
+          frequencyReported: 'monthly',
+          points: ['At National Grid Reference SE 4044 7262 (Borehole in top field)'],
+          purposes: ['Spray Irrigation - Direct'],
+          returnReference: 10012345,
+          returnsCycle: 'Winter and all year',
+          siteDescription: 'Borehole in field',
+          title: 'Borehole in field'
+        }
+      ],
+      startDate: '1 April 2022',
+      status: 'current'
+    })
+  })
+
+  describe('the "additionalSubmissionOptions" property', () => {
+    describe('when multipleUpload is true', () => {
+      beforeEach(() => {
+        returnVersion.multipleUpload = true
+      })
+
+      it('returns "Yes"', () => {
+        const result = ViewPresenter.go(returnVersion)
+
+        expect(result.additionalSubmissionOptions.multipleUpload).to.equal('Yes')
+      })
+    })
+
+    describe('when multipleUpload is false', () => {
+      it('returns "No"', () => {
+        const result = ViewPresenter.go(returnVersion)
+
+        expect(result.additionalSubmissionOptions.multipleUpload).to.equal('No')
+      })
+    })
+  })
+
+  describe('the "createdBy" property', () => {
+    describe('when there is no user linked to the return', () => {
+      beforeEach(() => {
+        returnVersion.user = null
+      })
+
+      it('returns "Migrated from NALD" ', () => {
+        const result = ViewPresenter.go(returnVersion)
+
+        expect(result.createdBy).to.equal('Migrated from NALD')
+      })
+    })
+
+    describe('when there is a user linked to the return', () => {
+      it("returns the user's username", () => {
+        const result = ViewPresenter.go(returnVersion)
+
+        expect(result.createdBy).to.equal('carol.shaw@atari.com')
+      })
+    })
+  })
+
+  describe('the "createdDate" property', () => {
+    it('returns created date', () => {
       const result = ViewPresenter.go(returnVersion)
 
-      expect(result).to.equal({
-        additionalSubmissionOptions: {
-          multipleUpload: 'No'
-        },
-        createdBy: 'carol.shaw@atari.com',
-        createdDate: '5 April 2022',
-        licenceId: '761bc44f-80d5-49ae-ab46-0a90495417b5',
-        licenceRef: '01/123',
-        notes: 'A special note',
-        pageTitle: 'Requirements for returns for Mr Ingles',
-        reason: 'New licence',
-        requirements: [
-          {
-            abstractionPeriod: 'From 1 April to 31 October',
-            agreementsExceptions: 'None',
-            frequencyCollected: 'monthly',
-            frequencyReported: 'monthly',
-            points: ['At National Grid Reference SE 4044 7262 (Borehole in top field)'],
-            purposes: ['Spray Irrigation - Direct'],
-            returnReference: 10012345,
-            returnsCycle: 'Winter and all year',
-            siteDescription: 'Borehole in field',
-            title: 'Borehole in field'
-          }
-        ],
-        startDate: '1 April 2022',
-        status: 'current'
-      })
+      expect(result.createdDate).to.equal('5 April 2022')
     })
+  })
 
-    describe('the "additionalSubmissionOptions" property', () => {
-      describe('when multipleUpload is true', () => {
-        beforeEach(() => {
-          returnVersion.multipleUpload = true
-        })
+  describe('the "pageTitle" property', () => {
+    it("returns the title incorporating the licence holder's name", () => {
+      const result = ViewPresenter.go(returnVersion)
 
-        it('returns "Yes"', () => {
-          const result = ViewPresenter.go(returnVersion)
-
-          expect(result.additionalSubmissionOptions.multipleUpload).to.equal('Yes')
-        })
-      })
-
-      describe('when multipleUpload is false', () => {
-        it('returns "No"', () => {
-          const result = ViewPresenter.go(returnVersion)
-
-          expect(result.additionalSubmissionOptions.multipleUpload).to.equal('No')
-        })
-      })
+      expect(result.pageTitle).to.equal('Requirements for returns for Mr Ingles')
     })
+  })
 
-    describe('the "createdBy" property', () => {
-      describe('when there is no user linked to the return', () => {
-        beforeEach(() => {
-          returnVersion.user = null
-        })
-
-        it('returns "Migrated from NALD" ', () => {
-          const result = ViewPresenter.go(returnVersion)
-
-          expect(result.createdBy).to.equal('Migrated from NALD')
-        })
-      })
-
-      describe('when there is a user linked to the return', () => {
-        it("returns the user's username", () => {
-          const result = ViewPresenter.go(returnVersion)
-
-          expect(result.createdBy).to.equal('carol.shaw@atari.com')
-        })
-      })
-    })
-
-    describe('the "createdDate" property', () => {
-      it('returns created date', () => {
+  describe('the "reason" property', () => {
+    describe('when there is a reason', () => {
+      it('returns the formatted reason', () => {
         const result = ViewPresenter.go(returnVersion)
 
-        expect(result.createdDate).to.equal('5 April 2022')
+        expect(result.reason).to.equal('New licence')
       })
     })
 
-    describe('the "pageTitle" property', () => {
-      it("returns the title incorporating the licence holder's name", () => {
+    describe('when there is no reason', () => {
+      beforeEach(() => {
+        returnVersion.reason = null
+      })
+
+      it('returns an empty string', () => {
         const result = ViewPresenter.go(returnVersion)
 
-        expect(result.pageTitle).to.equal('Requirements for returns for Mr Ingles')
+        expect(result.reason).to.equal('')
+      })
+    })
+  })
+
+  describe('the "requirements" property', () => {
+    describe('the requirements "abstractionPeriod" property', () => {
+      it('formats the abstraction period for display', () => {
+        const result = ViewPresenter.go(returnVersion)
+
+        const { abstractionPeriod } = result.requirements[0]
+
+        expect(abstractionPeriod).to.equal('From 1 April to 31 October')
       })
     })
 
-    describe('the "reason" property', () => {
-      describe('when there is a reason', () => {
-        it('returns the formatted reason', () => {
+    describe('the requirements "agreementsExceptions" property', () => {
+      describe('when no agreements or exceptions have been applied', () => {
+        it('returns "None"', () => {
           const result = ViewPresenter.go(returnVersion)
 
-          expect(result.reason).to.equal('New licence')
+          const { agreementsExceptions } = result.requirements[0]
+
+          expect(agreementsExceptions).to.equal('None')
         })
       })
 
-      describe('when there is no reason', () => {
+      describe('when one agreement or exception was applied', () => {
         beforeEach(() => {
-          returnVersion.reason = null
+          returnVersion.returnRequirements[0].gravityFill = true
         })
 
-        it('returns an empty string', () => {
+        it("returns it's display text (Gravity fill)", () => {
           const result = ViewPresenter.go(returnVersion)
 
-          expect(result.reason).to.equal('')
+          const { agreementsExceptions } = result.requirements[0]
+
+          expect(agreementsExceptions).to.equal('Gravity fill')
+        })
+      })
+
+      describe('when two agreements or exceptions were selected', () => {
+        beforeEach(() => {
+          returnVersion.returnRequirements[0].gravityFill = true
+          returnVersion.returnRequirements[0].reabstraction = true
+        })
+
+        it('returns them joined with an "and" (Gravity fill and Transfer re-abstraction scheme)', () => {
+          const result = ViewPresenter.go(returnVersion)
+
+          const { agreementsExceptions } = result.requirements[0]
+
+          expect(agreementsExceptions).to.equal('Gravity fill and Transfer re-abstraction scheme')
+        })
+      })
+
+      describe('when more than two options were selected', () => {
+        beforeEach(() => {
+          returnVersion.returnRequirements[0].fiftySixException = true
+          returnVersion.returnRequirements[0].gravityFill = true
+          returnVersion.returnRequirements[0].reabstraction = true
+          returnVersion.returnRequirements[0].twoPartTariff = true
+        })
+
+        it('returns them joined with an ", and" (Gravity fill, Transfer re-abstraction scheme, Two-part tariff, and 56 returns exception)', () => {
+          const result = ViewPresenter.go(returnVersion)
+
+          const { agreementsExceptions } = result.requirements[0]
+
+          expect(agreementsExceptions).to.equal('Gravity fill, Transfer re-abstraction scheme, Two-part tariff, and 56 returns exception')
         })
       })
     })
 
-    describe('the "requirements" property', () => {
-      describe('the requirements "abstractionPeriod" property', () => {
-        it('formats the abstraction period for display', () => {
+    describe('the requirements "points" property', () => {
+      // Formatting of the points uses GeneralLib.generateAbstractionPointDetail() so testing is 'light' here
+      it('formats the points for display', () => {
+        const result = ViewPresenter.go(returnVersion)
+
+        const { points } = result.requirements[0]
+
+        expect(points).to.equal(['At National Grid Reference SE 4044 7262 (Borehole in top field)'])
+      })
+    })
+
+    describe('the requirements "purposes" property', () => {
+      describe('when a purpose description (alias) was added to the purpose', () => {
+        beforeEach(() => {
+          returnVersion.returnRequirements[0].returnRequirementPurposes[0].alias = 'spray indiscreetly'
+        })
+
+        it('formats the purposes for display with the purpose description in brackets', () => {
           const result = ViewPresenter.go(returnVersion)
 
-          const { abstractionPeriod } = result.requirements[0]
+          const { purposes } = result.requirements[0]
 
-          expect(abstractionPeriod).to.equal('From 1 April to 31 October')
+          expect(purposes).to.equal(['Spray Irrigation - Direct (spray indiscreetly)'])
         })
       })
 
-      describe('the requirements "agreementsExceptions" property', () => {
-        describe('when no agreements or exceptions have been applied', () => {
-          it('returns "None"', () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { agreementsExceptions } = result.requirements[0]
-
-            expect(agreementsExceptions).to.equal('None')
-          })
-        })
-
-        describe('when one agreement or exception was applied', () => {
-          beforeEach(() => {
-            returnVersion.returnRequirements[0].gravityFill = true
-          })
-
-          it("returns it's display text (Gravity fill)", () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { agreementsExceptions } = result.requirements[0]
-
-            expect(agreementsExceptions).to.equal('Gravity fill')
-          })
-        })
-
-        describe('when two agreements or exceptions were selected', () => {
-          beforeEach(() => {
-            returnVersion.returnRequirements[0].gravityFill = true
-            returnVersion.returnRequirements[0].reabstraction = true
-          })
-
-          it('returns them joined with an "and" (Gravity fill and Transfer re-abstraction scheme)', () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { agreementsExceptions } = result.requirements[0]
-
-            expect(agreementsExceptions).to.equal('Gravity fill and Transfer re-abstraction scheme')
-          })
-        })
-
-        describe('when more than two options were selected', () => {
-          beforeEach(() => {
-            returnVersion.returnRequirements[0].fiftySixException = true
-            returnVersion.returnRequirements[0].gravityFill = true
-            returnVersion.returnRequirements[0].reabstraction = true
-            returnVersion.returnRequirements[0].twoPartTariff = true
-          })
-
-          it('returns them joined with an ", and" (Gravity fill, Transfer re-abstraction scheme, Two-part tariff, and 56 returns exception)', () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { agreementsExceptions } = result.requirements[0]
-
-            expect(agreementsExceptions).to.equal('Gravity fill, Transfer re-abstraction scheme, Two-part tariff, and 56 returns exception')
-          })
-        })
-      })
-
-      describe('the requirements "points" property', () => {
-        // Formatting of the points uses GeneralLib.generateAbstractionPointDetail() so testing is 'light' here
-        it('formats the points for display', () => {
+      describe('when a purpose description (alias) was not added to the purpose', () => {
+        it('formats the purposes for display with just the default description', () => {
           const result = ViewPresenter.go(returnVersion)
 
-          const { points } = result.requirements[0]
+          const { purposes } = result.requirements[0]
 
-          expect(points).to.equal(['At National Grid Reference SE 4044 7262 (Borehole in top field)'])
+          expect(purposes).to.equal(['Spray Irrigation - Direct'])
+        })
+      })
+    })
+
+    describe('the requirements "returnsCycle" property', () => {
+      describe('when the requirement is for the "summer" returns cycle', () => {
+        beforeEach(() => {
+          returnVersion.returnRequirements[0].summer = true
+        })
+
+        it('formats the cycle for display (Summer)', () => {
+          const result = ViewPresenter.go(returnVersion)
+
+          const { returnsCycle } = result.requirements[0]
+
+          expect(returnsCycle).to.equal('Summer')
         })
       })
 
-      describe('the requirements "purposes" property', () => {
-        describe('when a purpose description (alias) was added to the purpose', () => {
-          beforeEach(() => {
-            returnVersion.returnRequirements[0].returnRequirementPurposes[0].alias = 'spray indiscreetly'
-          })
+      describe('when the requirement is for the "winter-and-all-year" returns cycle', () => {
+        it('formats the cycle for display (Winter and all year)', () => {
+          const result = ViewPresenter.go(returnVersion)
 
-          it('formats the purposes for display with the purpose description in brackets', () => {
-            const result = ViewPresenter.go(returnVersion)
+          const { returnsCycle } = result.requirements[0]
 
-            const { purposes } = result.requirements[0]
-
-            expect(purposes).to.equal(['Spray Irrigation - Direct (spray indiscreetly)'])
-          })
-        })
-
-        describe('when a purpose description (alias) was not added to the purpose', () => {
-          it('formats the purposes for display with just the default description', () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { purposes } = result.requirements[0]
-
-            expect(purposes).to.equal(['Spray Irrigation - Direct'])
-          })
-        })
-      })
-
-      describe('the requirements "returnsCycle" property', () => {
-        describe('when the requirement is for the "summer" returns cycle', () => {
-          beforeEach(() => {
-            returnVersion.returnRequirements[0].summer = true
-          })
-
-          it('formats the cycle for display (Summer)', () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { returnsCycle } = result.requirements[0]
-
-            expect(returnsCycle).to.equal('Summer')
-          })
-        })
-
-        describe('when the requirement is for the "winter-and-all-year" returns cycle', () => {
-          it('formats the cycle for display (Winter and all year)', () => {
-            const result = ViewPresenter.go(returnVersion)
-
-            const { returnsCycle } = result.requirements[0]
-
-            expect(returnsCycle).to.equal('Winter and all year')
-          })
+          expect(returnsCycle).to.equal('Winter and all year')
         })
       })
     })
