5d19907a4b114b3bc63f85d24cdf47939dc7c2c8
Roble <roblesiyad123@gmail.com>
2024-08-21 16:04:25 +0100

    Add history modifier to _fetchLicenceVersions, _fetchChargeVersions, and _fetchReturnVersions methods

--- app/services/licences/fetch-licence-history.service.js
+++ app/services/licences/fetch-licence-history.service.js
@@ -1,7 +1,6 @@
 'use strict'
 
 const { db } = require('../../../db/db.js')
-const { ref } = require('objection')
 
 const ChargeVersionModel = require('../../models/charge-version.model.js')
 const LicenceModel = require('../../models/licence.model.js')
@@ -10,7 +9,6 @@ const ReturnVersionModel = require('../../models/return-version.model.js')
 
 async function go (licenceId) {
   const licence = await _fetchLicence(licenceId)
-
   const chargeVersions = await _fetchChargeVersions(licenceId)
   const licenceVersions = await _fetchLicenceVersions(licenceId)
   const returnVersions = await _fetchReturnVersions(licenceId)
@@ -34,22 +32,13 @@ async function _fetchLicence (licenceId) {
 async function _fetchChargeVersions (licenceId) {
   return ChargeVersionModel.query()
     .select(
-      'chargeVersions.licenceId as licenceId',
       db.raw("'charge-version' as entry_type"),
       'chargeVersions.id as entry_id',
-      'public.changeReasons.description as reason',
-      'chargeVersions.createdAt as created_at',
-      ref('createdBy:email').castText().as('created_by'),
-      'water.notes.text as note',
-      'versionNumber as version_number',
-      'chargeVersions.source as source',
-      db.raw("'' as mod_log")
+      'chargeVersions.createdAt as created_at'
     )
-    .leftJoin('water.notes', 'notes.noteId', '=', 'chargeVersions.noteId')
-    .leftJoin('public.changeReasons', 'changeReasons.id', '=', 'chargeVersions.changeReasonId')
+    .modify('history')
     .orderBy([
-      { column: 'chargeVersions.createdAt', order: 'desc' },
-      { column: 'chargeVersions.versionNumber', order: 'desc' }
+      { column: 'versionNumber', order: 'desc' }
     ])
     .where('chargeVersions.licenceId', licenceId)
 }
@@ -57,18 +46,12 @@ async function _fetchChargeVersions (licenceId) {
 async function _fetchLicenceVersions (licenceId) {
   return LicenceVersionModel.query()
     .select(
-      'licenceVersions.licenceId as licenceId',
       db.raw("'licence-version' as entry_type"),
       'licenceVersions.id as entry_id',
-      db.raw("'' as reason"),
-      'licenceVersions.createdAt as created_at',
-      db.raw("'' as created_by"),
-      db.raw("'' as note"),
-      'issue as version_number',
-      db.raw("'' as mod_log")
+      'licenceVersions.createdAt as created_at'
     )
+    .modify('history')
     .orderBy([
-      { column: 'createdAt', order: 'desc' },
       { column: 'issue', order: 'desc' }
     ])
     .where('licenceVersions.licenceId', licenceId)
@@ -77,19 +60,12 @@ async function _fetchLicenceVersions (licenceId) {
 async function _fetchReturnVersions (licenceId) {
   return ReturnVersionModel.query()
     .select(
-      'returnVersions.licenceId as licenceId',
       db.raw("'return-version' as entry_type"),
       'returnVersions.id as entry_id',
-      'reason',
-      'returnVersions.createdAt as created_at',
-      'users.username as created_by',
-      'notes as note',
-      'version as version_number',
-      'modLog as mod_log'
+      'returnVersions.createdAt as created_at'
     )
-    .leftJoin('public.users', 'users.id', '=', 'returnVersions.createdBy')
+    .modify('history')
     .orderBy([
-      { column: 'createdAt', order: 'desc' },
       { column: 'version', order: 'desc' }
     ])
     .where('returnVersions.licenceId', licenceId)
