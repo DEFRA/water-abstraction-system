d4c746cde30f0d760ed2ab3584a03ebdf5eb346f
Roble <roblesiyad123@gmail.com>
2024-08-22 18:08:47 +0100

    Add JSDocs to view licence history service

--- app/services/licences/view-licence-history.service.js
+++ app/services/licences/view-licence-history.service.js
@@ -1,8 +1,20 @@
 'use strict'
 
+/**
+ * Orchestrates fetching and presenting the data needed for the licence history page
+ * @module ViewLicenceHistoryService
+ */
+
 const FetchLicenceHistoryService = require('./fetch-licence-history.service.js')
 const ViewLicenceHistoryPresenter = require('../../presenters/licences/view-licence-history.presenter.js')
 
+/**
+ * Orchestrates fetching and presenting the data needed for the licence history page
+ *
+ * @param {string} licenceId - The UUID of the licence
+ *
+ * @returns {Promise<object>} an object representing the `pageData` needed by the licence history template.
+ */
 async function go (licenceId) {
   const history = await FetchLicenceHistoryService.go(licenceId)
 
