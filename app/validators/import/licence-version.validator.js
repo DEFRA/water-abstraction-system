dd90b4f8ff4c47241bb39d54eac150f38444a12e
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-23 20:01:50 +0100

    Validate single licence versions
    We make the same kind of housekeeping changes we made to the licence validator. But like the fetch, we also split out validation of the purpose.
    
    We also continue our approach to of working with single records rather than arrays of them. In this case validate just a single licence version.
    
    The idea of validating that a licence has at least one licence version is a cracking idea. But with this new approach we know we'll have to build back the licence (attach purposes to licence versions, then the licence versions to the licence). We think with that done a distinct validator that then checks the structure of the whole licence will take the idea to the next level!

---
+++ app/validators/import/licence-version.validator.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/**
+ * @module ImportLicenceVersionValidator
+ */
+
+const Joi = require('joi')
+
+const statuses = ['current', 'superseded']
+
+/**
+ * Checks that imported licence version data that has been transformed is valid for persisting to WRLS
+ *
+ * @param {object[]} licenceVersion - The transformed licence version
+ *
+ * @throws {Joi.ValidationError} - throws a Joi validation error if the validation fails
+ */
+function go (data) {
+  const schema = Joi.object({
+    endDate: Joi.date().iso().required().allow(null),
+    externalId: Joi.string().required(),
+    increment: Joi.number().required(),
+    issue: Joi.number().required(),
+    startDate: Joi.date().iso().required(),
+    status: Joi.string().required().valid(...statuses)
+  })
+
+  const result = schema.validate(data)
+
+  if (result.error) {
+    throw result.error
+  }
+}
+
+module.exports = {
+  go
+}
