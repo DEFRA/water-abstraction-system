b43935d8aa5b771f8c2ebb2beb75a31c91a99715
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-24 12:23:54 +0100

    Add a licence structure validator to the process
    Now we can validate that a licences has all the child records we expect to make it valid. This obviously doesn't validate the full structure. It just covers what we are importing now. But we can build on it as we add the other entities to the import process.

--- app/services/import/legacy/process-licence.service.js
+++ app/services/import/legacy/process-licence.service.js
@@ -5,6 +5,7 @@
  * @module ImportLegacyProcessLicenceService
  */
 
+const LicenceStructureValidator = require('../../../validators/import/licence-structure.validator.js')
 const PersistLicenceService = require('../persist-licence.service.js')
 const TransformLicenceService = require('./transform-licence.service.js')
 const TransformLicenceVersionsService = require('./transform-licence-versions.service.js')
@@ -22,14 +23,19 @@ async function go (licenceRef) {
   try {
     const startTime = currentTimeInNanoseconds()
 
+    // Transform the parent legacy licence record first
     const { naldLicenceId, regionCode, transformedLicence } = await TransformLicenceService.go(licenceRef)
 
+    // Pass the transformed licence through each transformation step, building the licence as we go
     await TransformLicenceVersionsService.go(regionCode, naldLicenceId, transformedLicence)
     await TransformLicenceVersionPurposesService.go(regionCode, naldLicenceId, transformedLicence)
 
+    // Ensure the built licence has all the valid child records we require
+    LicenceStructureValidator.go(transformedLicence)
+
     // TODO: We want to bring the persisting of the 'licence' into a single service so that we can do it within a
     // single DB transaction. This removes the risk (slight as it is admittedly) of only part of a licence being saved.
-    const savedLicence = await PersistLicenceService.go(transformedLicence)
+    // const savedLicence = await PersistLicenceService.go(transformedLicence)
 
     calculateAndLogTimeTaken(startTime, 'Process licence', { licenceRef })
   } catch (error) {
--- app/validators/import/licence.validator.js
+++ app/validators/import/licence.validator.js
@@ -9,6 +9,19 @@ const Joi = require('joi')
 /**
  * Checks that imported licence data that has been transformed is valid for persisting to WRLS
  *
+ * A valid licence structure is the following. Some properties must contain at least one child object to be valid. Those
+ * that are optional we leave empty.
+ *
+ * ```javascript
+ * {
+ *   licenceVersions: [{
+ *     licenceVersionPurposes: [{
+ *       licenceVersionPurposeConditions: []
+ *     }]
+ *   }]
+ * }
+ * ```
+ *
  * @param {object} licence - The transformed licence data
  *
  * @throws {Joi.ValidationError} - throws a Joi validation error if the validation fails
