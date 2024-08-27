f9b36c3ebc7e80e83fe27fdd132ec89ab5f182b9
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-08-27 11:32:58 +0100

    Import Licence versions (#1195)
    * Import Licence  versions
    
    We need to replace the import service logic to import a licence from NALD.
    
    We will leave the import service running to add the NALD data in the import tables, but we will slowly replace the import-licence functionality and move this into the system repo.
    
    https://eaflood.atlassian.net/browse/WATER-4575
    
    This change will add the licence versions to the import logic.
    
    The licence versions rely on the created / existing licence id.
    
    This work needs to be done first to allow us to remove the licence and licence versions from the old import logic.
    
    https://eaflood.atlassian.net/browse/WATER-4575
    
    In this instance, we felt it was more constructive to provide the feedback for the original [Import Licence versions](https://github.com/DEFRA/water-abstraction-system/pull/1195) proposed PR as a series of commits from where the code has been left.
    
    This can be used to guide suggested changes, hopefully with better context than just PR comments.
    
    ---
    
    As I went through making these changes, I realised I was aiming for three outcomes;
    
    - simplify the work of the mappers by moving casting and some data generation into the fetch service queries
    - separate as much as possible the process for importing each entity (licence, licence version etc)
    - persist as one using a DB transaction to avoid risk of partial licences
    
    The commits themselves go into more details but very happy and glad to walk through the changes I'm suggesting.
    
    ---------
    
    Co-authored-by: Robert Parkinson <robertparkinson@users.noreply.github.com>
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

---
+++ test/presenters/import/legacy/licence.presenter.test.js
@@ -0,0 +1,122 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const { generateLicenceRef } = require('../../../support/helpers/licence.helper.js')
+
+// Thing under test
+const LicencePresenter = require('../../../../app/presenters/import/legacy/licence.presenter.js')
+
+describe('Import Legacy Licence presenter', () => {
+  let legacyLicence
+
+  beforeEach(() => {
+    legacyLicence = _legacyLicence()
+  })
+
+  it('correctly transforms the data', () => {
+    const result = LicencePresenter.go(legacyLicence)
+
+    expect(result).to.equal({
+      expiredDate: null,
+      lapsedDate: null,
+      licenceRef: legacyLicence.licence_ref,
+      licenceVersions: [],
+      regionId: '82d8c1b7-0eed-43a7-a5f9-4e397c08e17e',
+      regions: {
+        historicalAreaCode: 'KAEA',
+        regionalChargeArea: 'Southern',
+        standardUnitChargeCode: 'SUCSO',
+        localEnvironmentAgencyPlanCode: 'LEME'
+      },
+      revokedDate: null,
+      startDate: new Date('1992-08-19'),
+      waterUndertaker: false
+    })
+  })
+
+  describe('the "regions" property', () => {
+    beforeEach(() => {
+      legacyLicence.environmental_improvement_unit_charge_code = 'YOOTH'
+    })
+
+    it('returns a JSON object where the "regionalChargeArea" is based on the first 2 chars of the EIUC code', () => {
+      const result = LicencePresenter.go(legacyLicence)
+
+      expect(result.regions).to.equal({
+        historicalAreaCode: 'KAEA',
+        regionalChargeArea: 'Yorkshire',
+        standardUnitChargeCode: 'SUCSO',
+        localEnvironmentAgencyPlanCode: 'LEME'
+      })
+    })
+  })
+
+  describe('the "startDate" property', () => {
+    describe('when "original_effective_date" is populated in the licence data', () => {
+      it('returns "original_effective_date" as the "startDate"', () => {
+        const result = LicencePresenter.go(legacyLicence)
+
+        expect(result.startDate).to.equal(legacyLicence.original_effective_date)
+      })
+    })
+
+    describe('when "original_effective_date" is not populated in the licence data', () => {
+      beforeEach(() => {
+        legacyLicence.original_effective_date = null
+      })
+
+      it('returns "earliest_version_start_date" as the "startDate"', () => {
+        const result = LicencePresenter.go(legacyLicence)
+
+        expect(result.startDate).to.equal(legacyLicence.earliest_version_start_date)
+      })
+    })
+  })
+
+  describe('the "waterUndertaker" property', () => {
+    describe('when the "environmental_improvement_unit_charge_code" does not end with SWC', () => {
+      it('returns false', () => {
+        const result = LicencePresenter.go(legacyLicence)
+
+        expect(result.waterUndertaker).to.be.false()
+      })
+    })
+
+    describe('when the "environmental_improvement_unit_charge_code" ends with SWC', () => {
+      beforeEach(() => {
+        legacyLicence.environmental_improvement_unit_charge_code = 'SOSWC'
+      })
+
+      it('returns true', () => {
+        const result = LicencePresenter.go(legacyLicence)
+
+        expect(result.waterUndertaker).to.be.true()
+      })
+    })
+  })
+})
+
+function _legacyLicence () {
+  return {
+    historical_area_code: 'KAEA',
+    environmental_improvement_unit_charge_code: 'SOOTH',
+    local_environment_agency_plan_code: 'LEME',
+    standard_unit_charge_code: 'SUCSO',
+    expiry_date: null,
+    region_code: '6',
+    id: '2113',
+    lapsed_date: null,
+    licence_ref: generateLicenceRef(),
+    original_effective_date: new Date('1992-08-19'),
+    revoked_date: null,
+    earliest_version_start_date: new Date('1999-01-01'),
+    region_id: '82d8c1b7-0eed-43a7-a5f9-4e397c08e17e'
+  }
+}
