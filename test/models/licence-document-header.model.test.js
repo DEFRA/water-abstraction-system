2950af5ae78c67da20f16849b38754fcd862f265
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-08-28 14:39:36 +0100

    Link LicenceDocumentHeader to LicenceEntityRole

--- test/models/licence-document-header.model.test.js
+++ test/models/licence-document-header.model.test.js
@@ -8,6 +8,8 @@ const { describe, it, before } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
+const LicenceEntityRoleHelper = require('../support/helpers/licence-entity-role.helper.js')
+const LicenceEntityRoleModel = require('../../app/models/licence-entity-role.model.js')
 const LicenceHelper = require('../support/helpers/licence.helper.js')
 const LicenceModel = require('../../app/models/licence.model.js')
 const LicenceDocumentHeaderHelper = require('../support/helpers/licence-document-header.helper.js')
@@ -16,13 +18,16 @@ const LicenceDocumentHeaderHelper = require('../support/helpers/licence-document
 const LicenceDocumentHeaderModel = require('../../app/models/licence-document-header.model.js')
 
 describe('Licence Document Header model', () => {
+  let testLicenceEntityRole
   let testLicence
   let testRecord
 
   before(async () => {
+    testLicenceEntityRole = await LicenceEntityRoleHelper.add()
     testLicence = await LicenceHelper.add()
 
     testRecord = await LicenceDocumentHeaderHelper.add({
+      companyEntityId: testLicenceEntityRole.companyEntityId,
       licenceRef: testLicence.licenceRef
     })
   })
@@ -55,6 +60,27 @@ describe('Licence Document Header model', () => {
 
         expect(result.licence).to.be.an.instanceOf(LicenceModel)
         expect(result.licence).to.equal(testLicence)
+      })
+    })
+
+    describe('when linking to licence entity role', () => {
+      it('can successfully run a related query', async () => {
+        const query = await LicenceDocumentHeaderModel.query()
+          .innerJoinRelated('licenceEntityRole')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the licence entity role', async () => {
+        const result = await LicenceDocumentHeaderModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('licenceEntityRole')
+
+        expect(result).to.be.instanceOf(LicenceDocumentHeaderModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.licenceEntityRole).to.be.an.instanceOf(LicenceEntityRoleModel)
+        expect(result.licenceEntityRole).to.equal(testLicenceEntityRole)
       })
     })
   })
