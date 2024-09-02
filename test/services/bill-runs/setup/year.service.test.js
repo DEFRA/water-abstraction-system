82d0c45c6aa4a060060b02a52aee0317576b0822
Jason Claxton <jason.claxton@defra.gov.uk>
2024-09-02 17:11:25 +0100

    Fix year service tests

--- test/services/bill-runs/setup/year.service.test.js
+++ test/services/bill-runs/setup/year.service.test.js
@@ -3,12 +3,16 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
 const { describe, it, beforeEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
 const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Things we need to stub
+const FetchLicenceSupplementaryYearsService = require('../../../../app/services/bill-runs/setup/fetch-licence-supplementary-years.service.js')
 
 // Thing under test
 const YearService = require('../../../../app/services/bill-runs/setup/year.service.js')
@@ -17,7 +21,9 @@ describe('Bill Runs Setup Year service', () => {
   let session
 
   beforeEach(async () => {
-    session = await SessionHelper.add({ data: { year: 2024 } })
+    session = await SessionHelper.add({ data: { type: 'two_part_supplementary', year: 2024 } })
+
+    Sinon.stub(FetchLicenceSupplementaryYearsService, 'go').resolves([{ financialYearEnd: 2024 }])
   })
 
   describe('when called', () => {
@@ -25,7 +31,7 @@ describe('Bill Runs Setup Year service', () => {
       const result = await YearService.go(session.id)
 
       expect(result).to.equal({
-        financialYearsData: [],
+        financialYearsData: [{ text: '2023 to 2024', value: 2024, checked: true }],
         sessionId: session.id,
         selectedYear: 2024
       })
