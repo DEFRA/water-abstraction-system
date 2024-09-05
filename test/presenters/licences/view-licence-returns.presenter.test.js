0f82f6505d4dcf7f5dac8a2e2d1d54c86c3fa8d4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-09-05 12:03:13 +0100

    Housekeeping - refactor how test data is generated
    Use a function which is our convention for larger data sets. Plus update the date to use realistic ID's (return logs don't use GUIDs).

--- test/presenters/licences/view-licence-returns.presenter.test.js
+++ test/presenters/licences/view-licence-returns.presenter.test.js
@@ -11,11 +11,85 @@ const { expect } = Code
 const ViewLicenceReturnsPresenter = require('../../../app/presenters/licences/view-licence-returns.presenter.js')
 
 describe('View Licence returns presenter', () => {
-  let returnsData
+  let returnLogs
   let hasRequirements
 
-  const returnItem = {
-    id: 'd5912c1d-3928-48e9-b2fc-e99a96d704a3',
+  beforeEach(() => {
+    hasRequirements = true
+    returnLogs = _returnLogs()
+  })
+
+  describe('when provided with returns data', () => {
+    it('correctly presents the data', () => {
+      const result = ViewLicenceReturnsPresenter.go(returnLogs, hasRequirements)
+
+      expect(result).to.equal({
+        noReturnsMessage: null,
+        returns: [
+          {
+            dates: '2 January 2020 to 1 February 2020',
+            description: 'empty description',
+            dueDate: '28 November 2012',
+            link: '/returns/return?id=v1:1:01/123:10046821:2020-01-02:2020-02-01',
+            purpose: 'SPRAY IRRIGATION',
+            reference: '10046821',
+            returnLogId: 'v1:1:01/123:10046821:2020-01-02:2020-02-01',
+            status: 'complete'
+          },
+          {
+            dates: '2 January 2020 to 1 February 2020',
+            description: 'empty description',
+            dueDate: '28 November 2012',
+            link: '/return/internal?returnId=v1:1:01/123:10046820:2020-01-02:2020-02-01',
+            purpose: 'SPRAY IRRIGATION',
+            reference: '10046820',
+            returnLogId: 'v1:1:01/123:10046820:2020-01-02:2020-02-01',
+            status: 'overdue'
+          }
+        ]
+      })
+    })
+  })
+
+  describe('the "noReturnsMessage" property', () => {
+    describe('when a licence has returns and requirements', () => {
+      it('returns null', () => {
+        const result = ViewLicenceReturnsPresenter.go(returnLogs, hasRequirements)
+
+        expect(result.noReturnsMessage).to.be.null()
+      })
+    })
+
+    describe('when a licence has NO requirements and NO returns', () => {
+      beforeEach(() => {
+        returnLogs = []
+        hasRequirements = false
+      })
+
+      it('returns the message "No requirements for returns have been set up for this licence."', () => {
+        const result = ViewLicenceReturnsPresenter.go(returnLogs, hasRequirements)
+
+        expect(result.noReturnsMessage).to.equal('No requirements for returns have been set up for this licence.')
+      })
+    })
+
+    describe('when a licence has requirements but NO returns', () => {
+      beforeEach(() => {
+        returnLogs = []
+      })
+
+      it('returns the message "No returns for this licence."', () => {
+        const result = ViewLicenceReturnsPresenter.go(returnLogs, hasRequirements)
+
+        expect(result.noReturnsMessage).to.equal('No returns for this licence.')
+      })
+    })
+  })
+})
+
+function _returnLogs () {
+  const returnLog = {
+    id: 'v1:1:01/123:10046821:2020-01-02:2020-02-01',
     dueDate: new Date('2012-11-28'),
     status: 'completed',
     startDate: new Date('2020/01/02'),
@@ -40,86 +114,16 @@ describe('View Licence returns presenter', () => {
       ],
       description: 'empty description'
     },
-    returnReference: '1068'
+    returnReference: '10046821'
   }
 
-  beforeEach(() => {
-    hasRequirements = true
-    returnsData = [
-      { ...returnItem },
-      {
-        ...returnItem,
-        id: '2fb6d1be-5d56-45de-a252-3c1e8a955991',
-        status: 'due',
-        returnReference: '1069'
-      }
-    ]
-  })
-
-  describe('when provided with returns data', () => {
-    it('correctly presents the data', () => {
-      const result = ViewLicenceReturnsPresenter.go(returnsData, hasRequirements)
-
-      expect(result).to.equal({
-        noReturnsMessage: null,
-        returns: [
-          {
-            dates: '2 January 2020 to 1 February 2020',
-            description: 'empty description',
-            dueDate: '28 November 2012',
-            link: '/returns/return?id=d5912c1d-3928-48e9-b2fc-e99a96d704a3',
-            purpose: 'SPRAY IRRIGATION',
-            reference: '1068',
-            returnLogId: 'd5912c1d-3928-48e9-b2fc-e99a96d704a3',
-            status: 'complete'
-          },
-          {
-            dates: '2 January 2020 to 1 February 2020',
-            description: 'empty description',
-            dueDate: '28 November 2012',
-            link: '/return/internal?returnId=2fb6d1be-5d56-45de-a252-3c1e8a955991',
-            purpose: 'SPRAY IRRIGATION',
-            reference: '1069',
-            returnLogId: '2fb6d1be-5d56-45de-a252-3c1e8a955991',
-            status: 'overdue'
-          }
-        ]
-      })
-    })
-  })
-
-  describe('the "noReturnsMessage" property', () => {
-    describe('when a licence has returns and requirements', () => {
-      it('returns null', () => {
-        const result = ViewLicenceReturnsPresenter.go(returnsData, hasRequirements)
-
-        expect(result.noReturnsMessage).to.be.null()
-      })
-    })
-
-    describe('when a licence has NO requirements and NO returns', () => {
-      beforeEach(() => {
-        returnsData = []
-        hasRequirements = false
-      })
-
-      it('returns the message "No requirements for returns have been set up for this licence."', () => {
-        const result = ViewLicenceReturnsPresenter.go(returnsData, hasRequirements)
-
-        expect(result.noReturnsMessage).to.equal('No requirements for returns have been set up for this licence.')
-      })
-    })
-
-    describe('when a licence has requirements but NO returns', () => {
-      beforeEach(() => {
-        returnsData = []
-      })
-
-      it('returns the message "No returns for this licence."', () => {
-        const result = ViewLicenceReturnsPresenter.go(returnsData, hasRequirements)
-
-        expect(result.noReturnsMessage).to.equal('No returns for this licence.')
-      })
-    })
-  })
-})
+  return [
+    { ...returnLog },
+    {
+      ...returnLog,
+      id: 'v1:1:01/123:10046820:2020-01-02:2020-02-01',
+      status: 'due',
+      returnReference: '10046820'
+    }
+  ]
+}
