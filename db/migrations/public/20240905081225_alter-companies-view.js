73017ac09a39470ed660ee63fd68859283e3dd5e
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2024-09-05 11:24:01 +0100

    Update db/migrations/public/20240905081225_alter-companies-view.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- db/migrations/public/20240905081225_alter-companies-view.js
+++ db/migrations/public/20240905081225_alter-companies-view.js
@@ -35,7 +35,7 @@ exports.down = function (knex) {
         'companies.name',
         'companies.type',
         'companies.company_number',
-        '// companies.external_id', // is used when importing data from nald
+        // 'companies.external_id', // is used when importing data from nald
         // 'companies.is_test', // we ignore this legacy test column in tables
         'companies.organisation_type',
         // 'companies.last_hash', // is populated but is only used by the legacy import process
