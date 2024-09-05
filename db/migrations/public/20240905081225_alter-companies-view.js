1a9dc21caa8a14a2586cb083fded342e39dd63ef
jonathangoulding <jonathangoulding21@gmail.com>
2024-09-05 09:29:18 +0100

    Add external id to companies view.
    https://eaflood.atlassian.net/browse/WATER-4659
    
    As part of the ongoing to work to migrate the import service into the water-abstraction-system, we need to add the external id for the companies (know as parties in NALD).
    
    When we import the companies (parties) we need to upsert the data. The current import service uses the external id to resolve the conflict.
    
    This follows the same approach as the other import work.

---
+++ db/migrations/public/20240905081225_alter-companies-view.js
@@ -0,0 +1,47 @@
+'use strict'
+
+const viewName = 'companies'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .dropViewIfExists(viewName)
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex(viewName).withSchema('crm_v2').select([
+        'companies.company_id AS id',
+        'companies.name',
+        'companies.type',
+        'companies.company_number',
+        'companies.external_id', // is used when importing data from nald
+        // 'companies.is_test', // we ignore this legacy test column in tables
+        'companies.organisation_type',
+        // 'companies.last_hash', // is populated but is only used by the legacy import process
+        // 'companies.current_hash', // is populated but is only used by the legacy import process
+        'companies.date_created AS created_at',
+        'companies.date_updated AS updated_at'
+      ]))
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .dropView(viewName)
+    .createView(viewName, (view) => {
+      // NOTE: We have commented out unused columns from the source table
+      view.as(knex(viewName).withSchema('crm_v2').select([
+        'companies.company_id AS id',
+        'companies.name',
+        'companies.type',
+        'companies.company_number',
+        'companies.external_id', // is used when importing data from nald
+        // 'companies.is_test', // we ignore this legacy test column in tables
+        'companies.organisation_type',
+        // 'companies.last_hash', // is populated but is only used by the legacy import process
+        // 'companies.current_hash', // is populated but is only used by the legacy import process
+        'companies.date_created AS created_at',
+        'companies.date_updated AS updated_at'
+      ]))
+    })
+}
