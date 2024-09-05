d0d7442ab7c36eac9a05e523d0250fd46cf3e666
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2024-09-05 09:51:58 +0100

    Trigger 2PT Supplementary Bill Run - Pt2 (#1262)
    https://eaflood.atlassian.net/browse/WATER-4631
    
    As we treat 2PT supplementary as a separate bill run we will need a new journey to trigger it.
    
    The first part of this work was carried out in PR https://github.com/DEFRA/water-abstraction-system/pull/1259
    
    In this PR the remainder of the journey to enable the triggering of a 2PT supplementary will be completed. This will include selecting the required financial year and triggering the bill run.

---
+++ test/services/bill-runs/setup/no-licences.service.test.js
@@ -0,0 +1,34 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Test helpers
+const RegionHelper = require('../../../support/helpers/region.helper.js')
+const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Thing under test
+const NoLicencesService = require('../../../../app/services/bill-runs/setup/no-licences.service.js')
+
+describe('Bill Runs Setup No Licences service', () => {
+  let sessionId
+
+  describe('when called with a valid session id', () => {
+    beforeEach(async () => {
+      const region = RegionHelper.select(RegionHelper.TEST_REGION_INDEX)
+      const session = await SessionHelper.add({ data: { region: region.id } })
+
+      sessionId = session.id
+    })
+
+    it('returns the regions display name', async () => {
+      const result = await NoLicencesService.go(sessionId)
+
+      expect(result).to.equal('Test Region')
+    })
+  })
+})
