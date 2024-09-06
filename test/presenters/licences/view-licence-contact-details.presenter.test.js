a33b8b23a364b17b8a20176b0003d0cecaa7dd50
Roble <roblesiyad123@gmail.com>
2024-09-06 17:55:08 +0100

    Update view licence contact details presenter test

--- test/presenters/licences/view-licence-contact-details.presenter.test.js
+++ test/presenters/licences/view-licence-contact-details.presenter.test.js
@@ -11,87 +11,80 @@ const { expect } = Code
 const ViewLicenceContactDetailsPresenter = require('../../../app/presenters/licences/view-licence-contact-details.presenter.js')
 
 describe('View Licence Contact Details presenter', () => {
-  let licenceContactDetails
+  let licenceContactDetailsData
 
   beforeEach(() => {
-    licenceContactDetails = _testFetchLicenceContactDetailsData()
+    licenceContactDetailsData = _testFetchLicenceContactDetailsData()
   })
 
-  describe('when provided with populated licence contacts data', () => {
+  describe('when provided with populated licence contact details data', () => {
     it('correctly presents the data', () => {
-      const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetails)
+      const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetailsData)
 
       expect(result).to.equal({
         licenceId: '0a4ebb93-2e90-4e35-acd5-a5aa73466508',
         licenceRef: '00/111/222',
         pageTitle: 'Licence contact details',
-        licenceContacts: [
+        licenceContactDetails: [
           {
-            address: {
-              contactAddress: [
-                'ENVIRONMENT AGENCY',
-                'HORIZON HOUSE',
-                'DEANERY ROAD',
-                'BRISTOL',
-                'BS1 5AH',
-                'United Kingdom']
-            },
-            role: 'Licence holder',
-            name: 'Acme ltd'
-          },
-          {
-            address: {
-              contactAddress: [
-                'Furland',
-                'Crewkerne',
-                'Somerset',
-                'TA18 7TT',
-                'United Kingdom'
-              ]
-            },
-            role: 'Licence contact',
-            name: 'Furland Farm'
-          },
-          {
-            address: {
-              contactAddress: [
-                'Crinkley Bottom',
-                'Cricket St Thomas',
-                'Somerset',
-                'TA20 1KL',
-                'United Kingdom'
-              ]
-            },
-            role: 'Returns to',
-            name: 'Mr N Edmonds'
-          }]
-      })
-    })
-
-    describe('the "licenceContacts" property', () => {
-      describe('the "licenceContact.address" property', () => {
-        it('returns the address of the property', () => {
-          const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetails)
-
-          expect(result.licenceContacts[0].address).to.equal({
-            contactAddress: [
+            address: [
               'ENVIRONMENT AGENCY',
               'HORIZON HOUSE',
               'DEANERY ROAD',
               'BRISTOL',
               'BS1 5AH',
               'United Kingdom'
-            ]
-          })
+            ],
+            role: 'Licence holder',
+            name: 'Acme ltd'
+          },
+          {
+            address: [
+              'Furland',
+              'Crewkerne',
+              'Somerset',
+              'TA18 7TT',
+              'United Kingdom'
+            ],
+            role: 'Licence contact',
+            name: 'Furland Farm'
+          },
+          {
+            address: [
+              'Crinkley Bottom',
+              'Cricket St Thomas',
+              'Somerset',
+              'TA20 1KL',
+              'United Kingdom'
+            ],
+            role: 'Returns to',
+            name: 'Mr N Edmonds'
+          }]
+      })
+    })
+
+    describe('the "licenceContactDetails" property', () => {
+      describe('the "licenceContactDetails.address" property', () => {
+        it('returns the address of the property', () => {
+          const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetailsData)
+
+          expect(result.licenceContactDetails[0].address).to.equal([
+            'ENVIRONMENT AGENCY',
+            'HORIZON HOUSE',
+            'DEANERY ROAD',
+            'BRISTOL',
+            'BS1 5AH',
+            'United Kingdom'
+          ])
         })
       })
 
-      describe('the "licenceContact.role" property', () => {
-        describe('when one of the licence contacts has the role type of "Enforcement officer"', () => {
-          it('returns the licenceContacts without the contact with the role type of "Enforcement officer"', () => {
-            const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetails)
+      describe('the "licenceContactDetails.role" property', () => {
+        describe('when one of the licence contact details has the role type of "Enforcement officer"', () => {
+          it('returns licenceContactDetails without the contact with the role type of "Enforcement officer"', () => {
+            const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetailsData)
 
-            const hasEnforcementOfficer = result.licenceContacts.some((contact) => {
+            const hasEnforcementOfficer = result.licenceContactDetails.some((contact) => {
               return contact.role === 'Enforcement officer'
             })
 
@@ -103,21 +96,21 @@ describe('View Licence Contact Details presenter', () => {
       describe('the "licenceContacts.name" property', () => {
         describe('when the initials are null', () => {
           beforeEach(() => {
-            licenceContactDetails.licenceDocumentHeader.metadata.contacts[3].initials = null
+            licenceContactDetailsData.licenceDocumentHeader.metadata.contacts[3].initials = null
           })
 
           it("returns the licence contact's forename and name", () => {
-            const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetails)
+            const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetailsData)
 
-            expect(result.licenceContacts[2].name).to.equal('Mr Noel Edmonds')
+            expect(result.licenceContactDetails[2].name).to.equal('Mr Noel Edmonds')
           })
         })
 
         describe('when the initials are not null', () => {
           it("returns the licence contact's forename and name", () => {
-            const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetails)
+            const result = ViewLicenceContactDetailsPresenter.go(licenceContactDetailsData)
 
-            expect(result.licenceContacts[2].name).to.equal('Mr N Edmonds')
+            expect(result.licenceContactDetails[2].name).to.equal('Mr N Edmonds')
           })
         })
       })
