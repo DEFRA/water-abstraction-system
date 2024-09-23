e0ba723e8b71c48144e3c68e6ca1007f7b414cb7
Jason Claxton <jason.claxton@defra.gov.uk>
2024-09-23 15:32:12 +0100

    Fix intermittent failures in `FetchBillsToBeReissuedService` unit tests As the unit tests were creating and checking for bill runs in real regions. When other tests were run in parallel this unit test would intermittently fail if other unit tests created bill runs in the same region.
    As this service is not reliant on having the actual region data. A randomly generated ID is used for the region to prevent other tests from effecting the results.

--- test/services/bill-runs/reissue/fetch-bills-to-be-reissued.service.test.js
+++ test/services/bill-runs/reissue/fetch-bills-to-be-reissued.service.test.js
@@ -13,6 +13,7 @@ const BillHelper = require('../../../support/helpers/bill.helper.js')
 const BillModel = require('../../../../app/models/bill.model.js')
 const BillLicenceHelper = require('../../../support/helpers/bill-licence.helper.js')
 const BillRunHelper = require('../../../support/helpers/bill-run.helper.js')
+const { generateUUID } = require('../../../../app/lib/general.lib.js')
 const TransactionHelper = require('../../../support/helpers/transaction.helper.js')
 
 // Thing under test
@@ -21,9 +22,14 @@ const FetchBillsToBeReissuedService = require('../../../../app/services/bill-run
 describe('Fetch Bills To Be Reissued service', () => {
   let billRun
   let bill
+  let regionId
 
   beforeEach(async () => {
-    billRun = await BillRunHelper.add()
+    // We use a random regionId to avoid other tests writing bill run data in the same region which would effect the
+    // tests. The service is not interested in any of the region data beyond its ID
+    regionId = generateUUID()
+
+    billRun = await BillRunHelper.add({ regionId })
     bill = await BillHelper.add({ billRunId: billRun.id })
     const { id: billLicenceId } = await BillLicenceHelper.add({ billId: bill.id })
 
@@ -32,7 +38,7 @@ describe('Fetch Bills To Be Reissued service', () => {
 
   describe('when there are no bills to be reissued', () => {
     it('returns no results', async () => {
-      const result = await FetchBillsToBeReissuedService.go(billRun.regionId)
+      const result = await FetchBillsToBeReissuedService.go(regionId)
 
       expect(result).to.be.empty()
     })
@@ -44,14 +50,14 @@ describe('Fetch Bills To Be Reissued service', () => {
     })
 
     it('returns results', async () => {
-      const result = await FetchBillsToBeReissuedService.go(billRun.regionId)
+      const result = await FetchBillsToBeReissuedService.go(regionId)
 
       expect(result).to.have.length(1)
       expect(result[0]).to.be.an.instanceOf(BillModel)
     })
 
     it('returns only the required bill fields', async () => {
-      const bill = await FetchBillsToBeReissuedService.go(billRun.regionId)
+      const bill = await FetchBillsToBeReissuedService.go(regionId)
 
       const result = Object.keys(bill[0])
 
@@ -67,7 +73,7 @@ describe('Fetch Bills To Be Reissued service', () => {
     })
 
     it('returns only the required bill licence fields', async () => {
-      const bill = await FetchBillsToBeReissuedService.go(billRun.regionId)
+      const bill = await FetchBillsToBeReissuedService.go(regionId)
 
       const { billLicences } = bill[0]
 
@@ -82,7 +88,7 @@ describe('Fetch Bills To Be Reissued service', () => {
 
     describe('and there are alcs bills to be reissued', () => {
       beforeEach(async () => {
-        const alcsBillRun = await BillRunHelper.add({ scheme: 'alcs' })
+        const alcsBillRun = await BillRunHelper.add({ regionId, scheme: 'alcs' })
         const alcsBill = await BillHelper.add({
           billRunId: alcsBillRun.id,
           flaggedForRebilling: true
@@ -95,7 +101,7 @@ describe('Fetch Bills To Be Reissued service', () => {
       })
 
       it('returns only sroc bills', async () => {
-        const result = await FetchBillsToBeReissuedService.go(billRun.regionId)
+        const result = await FetchBillsToBeReissuedService.go(regionId)
 
         expect(result).to.have.length(1)
         expect(result[0].id).to.equal(bill.id)
@@ -124,7 +130,7 @@ describe('Fetch Bills To Be Reissued service', () => {
     })
 
     it('returns an empty array', async () => {
-      const result = await FetchBillsToBeReissuedService.go(billRun.regionId)
+      const result = await FetchBillsToBeReissuedService.go(regionId)
 
       expect(result).to.be.empty()
     })
