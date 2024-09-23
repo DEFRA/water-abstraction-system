298360afcb778fc1e74c5a813d1502c5d6bbcddc
Rebecca Ransome <beckyrose200@aol.com>
2024-09-23 12:34:58 +0100

    TransformLicenceSupplementaryFlags update comments and write unit tests

---
+++ app/services/import/transform-licence-supplementary-flags.service.js
@@ -0,0 +1,28 @@
+'use strict'
+
+/**
+ * Transforms NALD licence data to determine eligibility for supplementary billing.
+ * @module TransformLicenceSupplementaryFlagsService
+ */
+
+const FetchLicenceChargeVersionsService = require('./fetch-licence-charge-versions.service.js')
+const FlagForSupplementaryBillingPresenter = require('../../presenters/import/flag-for-supplementary-billing.presenter.js')
+
+/**
+ * Transforms NALD licence data to determine eligibility for supplementary billing.
+ *
+ * When importing a licence from NALD with an end date, this service checks if the licence should be included in
+ * supplementary billing.
+ * It orchestrates these checks and applies the necessary flags when required.
+ * @param {object} transformedLicence - An object representing a valid WRLS licence
+ * @param {string} wrlsLicenceId - The WRLS licence ID
+ */
+async function go (transformedLicence, wrlsLicenceId) {
+  const wrlsLicence = await FetchLicenceChargeVersionsService.go(wrlsLicenceId)
+
+  await FlagForSupplementaryBillingPresenter.go(transformedLicence, wrlsLicence)
+}
+
+module.exports = {
+  go
+}
