ddf0a08bcdcec77561248d992397f3afeb958dec
Roble <roblesiyad123@gmail.com>
2024-09-24 17:45:31 +0100

    Add auth to view service

--- app/services/monitoring-stations/view.service.js
+++ app/services/monitoring-stations/view.service.js
@@ -11,20 +11,22 @@ const ViewMonitoringStationPresenter = require('../../presenters/monitoring-stat
 /**
  * Orchestrates fetching and presenting the data needed for the `/monitoring-stations/{id}/view` page
  *
+ * @param {string} auth - The auth object taken from `request.auth` containing user details
  * @param {string} monitoringStationId - The UUID for the monitoring station
  *
  * @returns {Promise<object>} page data needed by the view template
  */
-async function go (monitoringStationId) {
+async function go (auth, monitoringStationId) {
   const fetchLicences = await FetchMonitoringStationsService.go(monitoringStationId)
 
-  const pageData = ViewMonitoringStationPresenter.go(fetchLicences)
+  const pageData = ViewMonitoringStationPresenter.go(auth, fetchLicences)
+
   console.log('🚀🚀🚀 ~ pageData after presenter:', pageData)
+  console.log('🚀🚀🚀 ~ licences after presenter:', pageData.licences)
 
   return {
     activeNavBar: 'search',
     ...pageData
-
   }
 }
 
