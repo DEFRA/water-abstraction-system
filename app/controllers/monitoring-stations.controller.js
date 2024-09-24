759f1808e353f37b4a8383e51d9113aa57d620ce
Roble <roblesiyad123@gmail.com>
2024-09-24 17:38:54 +0100

    Pull auth from the request and pass to view service

--- app/controllers/monitoring-stations.controller.js
+++ app/controllers/monitoring-stations.controller.js
@@ -8,9 +8,9 @@
 const ViewService = require('../services/monitoring-stations/view.service.js')
 
 async function view (request, h) {
-  const { monitoringStationId } = request.params
+  const { auth, params: { monitoringStationId } } = request
 
-  const pageData = await ViewService.go(monitoringStationId)
+  const pageData = await ViewService.go(auth, monitoringStationId)
 
   return h.view('monitoring-stations/view.njk', {
     ...pageData
