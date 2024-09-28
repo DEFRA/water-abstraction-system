e9ca0d8875dcc709df8ec62892e261a1498ec052
Jason Claxton <jason.claxton@defra.gov.uk>
2024-09-28 11:56:18 +0100

    Stub a model

---
+++ test/services/can-i-stub-it.service.test.js
@@ -0,0 +1,41 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
+const { expect } = Code
+
+// Things we need to stub
+const RegionModel = require('../../app/models/region.model.js')
+
+// Thing under test
+const CanIStubItService = require('../../app/services/can-i-stub-it.service.js')
+
+describe.only('Can I Stub It (yes we can) service', () => {
+  beforeEach(() => {
+    const queryStub = Sinon.stub(RegionModel, 'query')
+    const whereStub = Sinon.stub()
+
+    whereStub.returnsThis()
+    // The `onCall` numbers work like array indexes so 2 is the third call
+    whereStub.onCall(2).resolves('Here is my mock data')
+
+    queryStub.returns({
+      select: Sinon.stub().returnsThis(),
+      where: whereStub
+    })
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  it('returns my mock data', async () => {
+    const result = await CanIStubItService.go()
+
+    expect(result).to.equal('Here is my mock data')
+  })
+})
