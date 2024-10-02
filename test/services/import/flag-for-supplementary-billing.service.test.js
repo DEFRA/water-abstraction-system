1321be4de57269c262178da5451f5b816e439084
Rebecca Ransome <beckyrose200@aol.com>
2024-10-02 16:18:40 +0100

    flag for supplementary billing unit test

--- test/services/import/flag-for-supplementary-billing.service.test.js
+++ test/services/import/flag-for-supplementary-billing.service.test.js
@@ -9,355 +9,175 @@ const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
-const LicenceSupplementaryYearHelper = require('../../support/helpers/licence-supplementary-year.helper.js')
 const LicenceHelper = require('../../support/helpers/licence.helper.js')
+
+// Things we need to stub
+const DetermineSupplementaryBillingFlagsService = require('../../../app/services/licences/supplementary/determine-supplementary-billing-flags.service.js')
 
 // Thing under test
 const FlagForSupplementaryBillingService = require('../../../app/services/import/flag-for-supplementary-billing.service.js')
 
 describe('Flag For Supplementary Billing service', () => {
   let transformedLicence
-  let wrlsLicence
   let licence
-  let clock
-  let testDate
 
   beforeEach(async () => {
-    licence = await LicenceHelper.add()
-    // The service being tested will only flag licence that have charge versions in the previous 6 years.
-    // Because of this, the current time needs to be stubbed so these tests don't break in the future.
-    testDate = new Date('2025-03-31')
-
-    clock = Sinon.useFakeTimers(testDate)
+    Sinon.stub(DetermineSupplementaryBillingFlagsService, 'go').resolves()
   })
 
-  afterEach(() => {
-    clock.restore()
+  afterEach(async () => {
+    Sinon.restore()
   })
 
-  describe('when given a wrls licence with pre sroc charge versions', () => {
-    describe('and a nald licence with an expired date', () => {
+  describe('when given a licence id that has no end dates', () => {
+    beforeEach(async () => {
+      licence = await LicenceHelper.add()
+    })
+
+    describe('and a nald licence with no end dates', () => {
       beforeEach(() => {
         transformedLicence = {
+          lapsedDate: null,
           revokedDate: null,
-          lapsedDate: null,
-          expiredDate: new Date('2024-03-31')
+          expiredDate: null
         }
       })
 
-      describe('that is different to the wrls licence', () => {
-        describe('and the licence is not already flagged for pre sroc billing', () => {
-          beforeEach(() => {
-            wrlsLicence = _wrlsPreSrocLicence(licence)
-          })
+      it('does not pass the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-          it('sets the nald licence supplementary flags correctly', async () => {
-            await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.false()
+      })
+    })
 
-            expect(transformedLicence.includeInPresrocBilling).to.equal('yes')
-            expect(transformedLicence.includeInSrocBilling).to.equal(false)
-            expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-          })
-        })
-
-        describe('and the licence is already flagged for pre sroc billing', () => {
-          beforeEach(() => {
-            wrlsLicence = _wrlsPreSrocLicence(licence)
-            wrlsLicence.includeInPresrocBilling = 'yes'
-            wrlsLicence.includeInSrocBilling = true
-          })
-
-          it('sets the nald licence supplementary flags correctly', async () => {
-            await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-            expect(transformedLicence.includeInPresrocBilling).to.equal('yes')
-            expect(transformedLicence.includeInSrocBilling).to.equal(true)
-            expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-          })
-        })
+    describe('and a nald licence with end dates', () => {
+      beforeEach(() => {
+        transformedLicence = {
+          lapsedDate: null,
+          revokedDate: new Date('2023-01-01'),
+          expiredDate: new Date('2030-01-01')
+        }
       })
 
-      describe('thats the same to the wrls licence', () => {
-        beforeEach(() => {
-          wrlsLicence = _wrlsPreSrocLicence(licence)
-          wrlsLicence.expiredDate = new Date('2024-03-31')
-        })
+      it('passes the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-        it('sets the nald licence supplementary flags correctly', async () => {
-          await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-          expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-          expect(transformedLicence.includeInSrocBilling).to.equal(false)
-          expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-        })
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.true()
       })
     })
   })
 
-  describe('when given a wrls licence with sroc charge versions', () => {
-    describe('and a nald licence with an lapsed date', () => {
+  describe('when given a licence id that has a revoked date', () => {
+    beforeEach(async () => {
+      licence = await LicenceHelper.add({ revokedDate: new Date('2023-01-01') })
+    })
+
+    describe('and a nald licence with no end dates', () => {
       beforeEach(() => {
         transformedLicence = {
+          lapsedDate: null,
           revokedDate: null,
-          lapsedDate: new Date('2024-04-01'),
           expiredDate: null
         }
       })
 
-      describe('that is different to the wrls licence', () => {
-        describe('and the licence is not already flagged for sroc billing', () => {
-          beforeEach(() => {
-            wrlsLicence = _wrlsSrocLicence(licence)
-          })
+      it('passes the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-          it('sets the nald licence supplementary flags correctly', async () => {
-            await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.true()
+      })
+    })
 
-            expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-            expect(transformedLicence.includeInSrocBilling).to.equal(true)
-            expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-          })
-        })
-
-        describe('and the licence is already flagged for sroc billing', () => {
-          beforeEach(() => {
-            wrlsLicence = _wrlsSrocLicence(licence)
-            wrlsLicence.includeInSrocBilling = true
-          })
-
-          it('sets the nald licence supplementary flags correctly', async () => {
-            await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-            expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-            expect(transformedLicence.includeInSrocBilling).to.equal(true)
-            expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-          })
-        })
+    describe('and a nald licence with the same end dates', () => {
+      beforeEach(() => {
+        transformedLicence = {
+          lapsedDate: null,
+          revokedDate: new Date('2023-01-01'),
+          expiredDate: null
+        }
       })
 
-      describe('thats the same to the wrls licence', () => {
-        beforeEach(() => {
-          wrlsLicence = _wrlsSrocLicence(licence)
-          wrlsLicence.lapsedDate = new Date('2024-04-01')
-        })
+      it('does not pass the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-        it('sets the nald licence supplementary flags correctly', async () => {
-          await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-          expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-          expect(transformedLicence.includeInSrocBilling).to.equal(false)
-          expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-        })
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.false()
       })
     })
   })
 
-  describe('when given a wrls licence with two-part tariff sroc charge versions', () => {
-    describe('and a nald licence with a revoked date', () => {
+  describe('when given a licence id that has a lapsed date', () => {
+    beforeEach(async () => {
+      licence = await LicenceHelper.add({ lapsedDate: new Date('2023-01-01') })
+    })
+
+    describe('and a nald licence with no end dates', () => {
       beforeEach(() => {
         transformedLicence = {
-          revokedDate: new Date('2024-04-01'),
           lapsedDate: null,
+          revokedDate: null,
           expiredDate: null
         }
       })
 
-      describe('that is different to the wrls licence', () => {
-        describe('and the licence is not already flagged for two-part tariff billing', () => {
-          beforeEach(() => {
-            wrlsLicence = _wrlsSrocTwoPartTariffLicence(licence)
-          })
+      it('passes the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-          it('sets the nald licence supplementary flags correctly', async () => {
-            await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-            expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-            expect(transformedLicence.includeInSrocBilling).to.equal(false)
-            expect(transformedLicence.licenceSupplementaryYears).to.equal([
-              {
-                financialYearEnd: 2023,
-                licenceId: licence.id,
-                twoPartTariff: true
-              },
-              {
-                financialYearEnd: 2024,
-                licenceId: licence.id,
-                twoPartTariff: true
-              },
-              {
-                financialYearEnd: 2025,
-                licenceId: licence.id,
-                twoPartTariff: true
-              }
-            ])
-          })
-        })
-
-        describe('and the licence is already flagged for two-part tariff billing', () => {
-          beforeEach(async () => {
-            wrlsLicence = _wrlsSrocTwoPartTariffLicence(licence)
-            wrlsLicence.chargeVersions[0].endDate = null
-
-            await LicenceSupplementaryYearHelper.add({
-              licenceId: licence.id,
-              financialYearEnd: 2023,
-              twoPartTariff: true
-            })
-          })
-
-          it('sets the nald licence supplementary flags correctly', async () => {
-            await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-            expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-            expect(transformedLicence.includeInSrocBilling).to.equal(false)
-            expect(transformedLicence.licenceSupplementaryYears).to.equal([
-              {
-                financialYearEnd: 2024,
-                licenceId: licence.id,
-                twoPartTariff: true
-              },
-              {
-                financialYearEnd: 2025,
-                licenceId: licence.id,
-                twoPartTariff: true
-              }
-            ])
-          })
-        })
-      })
-
-      describe('thats the same to the wrls licence', () => {
-        beforeEach(() => {
-          wrlsLicence = _wrlsSrocTwoPartTariffLicence(licence)
-          wrlsLicence.revokedDate = new Date('2024-04-01')
-        })
-
-        it('sets the nald licence supplementary flags correctly', async () => {
-          await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
-
-          expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-          expect(transformedLicence.includeInSrocBilling).to.equal(false)
-          expect(transformedLicence.licenceSupplementaryYears).to.equal([])
-        })
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.true()
       })
     })
 
-    describe('when the nald licence has a revoked date older than 6 years', () => {
+    describe('and a nald licence with the same end dates', () => {
       beforeEach(() => {
         transformedLicence = {
-          revokedDate: new Date('2010-04-01'),
-          lapsedDate: null,
+          lapsedDate: new Date('2023-01-01'),
+          revokedDate: null,
           expiredDate: null
         }
-
-        wrlsLicence = _wrlsPreSrocLicence(licence)
       })
 
-      it('sets the nald licence supplementary flags correctly', async () => {
-        await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
+      it('does not pass the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-        expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-        expect(transformedLicence.includeInSrocBilling).to.equal(false)
-        expect(transformedLicence.licenceSupplementaryYears).to.equal([])
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.false()
+      })
+    })
+  })
+
+  describe('when given a licence id that has an expired date', () => {
+    beforeEach(async () => {
+      licence = await LicenceHelper.add({ expiredDate: new Date('2023-01-01') })
+    })
+
+    describe('and a nald licence with no end dates', () => {
+      beforeEach(() => {
+        transformedLicence = {
+          lapsedDate: null,
+          revokedDate: null,
+          expiredDate: null
+        }
+      })
+
+      it('passes the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
+
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.true()
       })
     })
 
-    describe('when the nald licence has a revoked date set for after the current financial year', () => {
+    describe('and a nald licence with the same end dates', () => {
       beforeEach(() => {
         transformedLicence = {
-          revokedDate: new Date('2025-04-01'),
           lapsedDate: null,
-          expiredDate: null
+          revokedDate: null,
+          expiredDate: new Date('2023-01-01')
         }
-
-        wrlsLicence = _wrlsPreSrocLicence(licence)
       })
 
-      it('sets the nald licence supplementary flags correctly', async () => {
-        await FlagForSupplementaryBillingService.go(transformedLicence, wrlsLicence)
+      it('does not pass the licence on to be flagged', async () => {
+        await FlagForSupplementaryBillingService.go(transformedLicence, licence.id)
 
-        expect(transformedLicence.includeInPresrocBilling).to.equal('no')
-        expect(transformedLicence.includeInSrocBilling).to.equal(false)
-        expect(transformedLicence.licenceSupplementaryYears).to.equal([])
+        expect(DetermineSupplementaryBillingFlagsService.go.called).to.be.false()
       })
     })
   })
 })
-
-function _wrlsPreSrocLicence (licence) {
-  return {
-    id: licence.id,
-    includeInPresrocBilling: 'no',
-    includeInSrocBilling: false,
-    revokedDate: null,
-    lapsedDate: null,
-    expiredDate: null,
-    chargeVersions: [
-      {
-        id: '3a7b82d7-826c-42de-8f23-a6f5f8624fdc',
-        startDate: new Date('2019-04-01'),
-        endDate: new Date('2022-03-31'),
-        chargeReferences: [{
-          id: '4eac1d61-94e3-4582-94c6-d2fb3f13d348',
-          s127: false,
-          chargeElements: [{
-            id: '83175dc5-a057-4def-942a-501ea520e383',
-            section127Agreement: false
-          }]
-        }]
-      }
-    ]
-  }
-}
-
-function _wrlsSrocLicence (licence) {
-  return {
-    id: licence.id,
-    includeInPresrocBilling: 'no',
-    includeInSrocBilling: false,
-    revokedDate: null,
-    lapsedDate: null,
-    expiredDate: null,
-    chargeVersions: [
-      {
-        id: '3a7b82d7-826c-42de-8f23-a6f5f8624fdc',
-        startDate: new Date('2022-04-01'),
-        endDate: null,
-        chargeReferences: [{
-          id: '4eac1d61-94e3-4582-94c6-d2fb3f13d348',
-          s127: false,
-          chargeElements: [{
-            id: '83175dc5-a057-4def-942a-501ea520e383',
-            section127Agreement: false
-          }]
-        }]
-      }
-    ]
-  }
-}
-
-function _wrlsSrocTwoPartTariffLicence (licence) {
-  return {
-    id: licence.id,
-    includeInPresrocBilling: 'no',
-    includeInSrocBilling: false,
-    revokedDate: null,
-    lapsedDate: null,
-    expiredDate: null,
-    chargeVersions: [
-      {
-        id: '3a7b82d7-826c-42de-8f23-a6f5f8624fdc',
-        startDate: new Date('2022-04-01'),
-        endDate: new Date('2025-03-31'),
-        chargeReferences: [{
-          id: '4eac1d61-94e3-4582-94c6-d2fb3f13d348',
-          s127: 'true',
-          chargeElements: [{
-            id: '83175dc5-a057-4def-942a-501ea520e383',
-            section127Agreement: true
-          }]
-        }]
-      }
-    ]
-  }
-}
