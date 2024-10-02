8c93e3dbb9c4fad9181a5db705810f5d896c9291
Rebecca Ransome <beckyrose200@aol.com>
2024-10-02 15:23:51 +0100

    Flag for supplementary billing service

--- app/services/import/flag-for-supplementary-billing.service.js
+++ app/services/import/flag-for-supplementary-billing.service.js
@@ -1,78 +1,64 @@
 'use strict'
 
 /**
+ * Determines if a licence should be flagged for supplementary billing
  * @module FlagForSupplementaryBillingService
  */
 
 const DetermineSupplementaryBillingFlagsService = require('../licences/supplementary/determine-supplementary-billing-flags.service.js')
-const { determineCurrentFinancialYear } = require('../../lib/general.lib.js')
-
-const APRIL = 3
+const LicenceModel = require('../../models/licence.model.js')
 
 /**
- * I am a comment
- * @param {*} transformedLicence
- * @param {*} wrlsLicenceId
+ * Determines if a licence should be flagged for supplementary billing.
+ *
+ * This service is responsible for determining whether a licence imported from NALD should be flagged for
+ * supplementary billing. It compares the licences end dates (such as lapsed, revoked or expired dates) between WRLS
+ * and the transformed data, and if there is a change in the dates allows the licence to go on to determining the flags.
+ * @param {object} transformedLicence - the legacy NALD licence
+ * @param {object} wrlsLicenceId - the WRLS licence data
  */
 async function go (transformedLicence, wrlsLicenceId) {
-  // const naldEndDates = [
-  //   { expiredDate: transformedLicence.expiredDate },
-  //   { revokedDate: transformedLicence.revokedDate },
-  //   { lapsedDate: transformedLicence.lapsedDate }
-  // ]
+  const licenceChanged = await _licenceChanged(transformedLicence, wrlsLicenceId)
 
-  // _filterNaldEndDates(naldEndDates)
-  const naldEndDates = _getDatesInRange(transformedLicence)
-
-  // If our dates object is empty it means there are no dates that we can flag for supplementary billing
-  if (Object.keys(naldEndDates).length === 0) {
+  if (!licenceChanged) {
     return
   }
 
-  // if (naldEndDates.length === 0) {
-  //   // If there are no end dates that are not null then we can return as there is nothing to flag
-  //   return
-  // }
-
-  await DetermineSupplementaryBillingFlagsService.go(wrlsLicenceId, naldEndDates)
+  await DetermineSupplementaryBillingFlagsService.go(transformedLicence, wrlsLicenceId)
 }
 
-function _getDatesInRange (transformedLicence) {
+async function _licenceChanged (transformedLicence, wrlsLicenceId) {
+  const query = LicenceModel.query()
+    .select(['id'])
+    .where('id', wrlsLicenceId)
+
+  _whereClauses(query, transformedLicence)
+
+  const result = await query
+
+  return result.length === 0
+}
+
+function _whereClauses (query, transformedLicence) {
   const { expiredDate, lapsedDate, revokedDate } = transformedLicence
 
-  const endDates = {}
-  const { startDate, endDate } = determineCurrentFinancialYear()
-  const sixYearsAgo = new Date(startDate.getFullYear() - 6, APRIL, 1)
-
-  if (expiredDate !== null && expiredDate > sixYearsAgo && expiredDate < endDate) {
-    endDates.expiredDate = expiredDate
+  if (expiredDate) {
+    query.where('expiredDate', expiredDate)
+  } else {
+    query.whereNull('expiredDate')
   }
 
-  if (lapsedDate !== null && lapsedDate > sixYearsAgo && lapsedDate < endDate) {
-    endDates.expiredDate = expiredDate
+  if (revokedDate) {
+    query.where('revokedDate', revokedDate)
+  } else {
+    query.whereNull('revokedDate')
   }
 
-  if (revokedDate !== null && revokedDate > sixYearsAgo && revokedDate < endDate) {
-    endDates.expiredDate = expiredDate
+  if (lapsedDate) {
+    query.where('lapsedDate', lapsedDate)
+  } else {
+    query.whereNull('lapsedDate')
   }
-
-  return endDates
-}
-
-function _filterNaldEndDates (naldEndDates) {
-  const financialYear = determineCurrentFinancialYear()
-  const sixYearsAgo = new Date(financialYear.startDate.getFullYear() - 6, APRIL, 1)
-
-  // We want to filter the naldEndDates to only contain dates that are within the date range we can bill for.
-  // We remove null values as this means there is no change in the end dates.
-  // We remove values greater than the current financial year end, as this changes doesn't impact an existing bill run
-  // We remove values older than 6 years as we can only bill up to 6 years ago. Anything after that can not be re billed
-  naldEndDates.filter((endDate) => {
-    // Since naldEndDates is an array of objects, we need to access the value to compare it to our date range
-    const dateValue = Object.values(endDate)[0]
-
-    return dateValue !== null && dateValue > sixYearsAgo && dateValue < financialYear.endDate
-  })
 }
 
 module.exports = {
