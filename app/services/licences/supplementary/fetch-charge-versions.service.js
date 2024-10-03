de06a8869f8b44b1a2bf181b07450beb29783c63
Rebecca Ransome <beckyrose200@aol.com>
2024-10-03 20:58:33 +0100

    Fetch charge versions service and unit tests

--- app/services/licences/supplementary/fetch-charge-versions.service.js
+++ app/services/licences/supplementary/fetch-charge-versions.service.js
@@ -7,72 +7,22 @@
 
 const ChargeReferenceModel = require('../../../models/charge-reference.model.js')
 const ChargeVersionModel = require('../../../models/charge-version.model.js')
-const { determineCurrentFinancialYear } = require('../../../lib/general.lib.js')
 const LicenceModel = require('../../../models/licence.model.js')
-
-const APRIL = 3
 
 /**
  * Fetches and returns the charge versions, changed dates and the licence data.
  *
  * It determines which dates have changed by comparing the incoming dates from the
  * nald import to the dates we currently hold on the wrls licence
- * @param {*} naldLicence - the legacy NALD licence
  * @param {string} wrlsLicenceId - The UUID of the licence being fetched
  *
  * @returns {Promise<object>} - The data needed to determine which supplementary flags the licence needs
  */
-async function go (naldLicence, wrlsLicenceId) {
+async function go (wrlsLicenceId) {
   const licence = await _fetchLicenceData(wrlsLicenceId)
-  const changedDates = await _fetchChangedDates(licence, naldLicence)
   const chargeVersions = await _fetchChargeVersionsData(wrlsLicenceId)
 
-  return { chargeVersions, changedDates, licence }
-}
-
-function _dateToFlag (changedDates, naldDate, wrlsDate) {
-  const { startDate, endDate } = determineCurrentFinancialYear()
-  const sixYearsAgo = new Date(startDate.getFullYear() - 6, APRIL, 1)
-
-  if (naldDate) {
-    changedDates.push(naldDate)
-
-    return
-  }
-
-  if (wrlsDate > endDate) {
-    return
-  }
-
-  if (wrlsDate < endDate && wrlsDate > sixYearsAgo) {
-    changedDates.push(wrlsDate)
-
-    return
-  }
-
-  if (wrlsDate < sixYearsAgo) {
-    changedDates.push(sixYearsAgo)
-  }
-}
-
-async function _fetchChangedDates (licence, naldLicence) {
-  const { revokedDate, lapsedDate, expiredDate } = naldLicence
-
-  const changedDates = []
-
-  if (revokedDate !== licence[0].revokedDate) {
-    _dateToFlag(changedDates, revokedDate, licence[0].revokedDate)
-  }
-
-  if (lapsedDate !== licence[0].lapsedDate) {
-    _dateToFlag(changedDates, lapsedDate, licence[0].lapsedDated)
-  }
-
-  if (expiredDate !== licence[0].expiredDate) {
-    _dateToFlag(changedDates, expiredDate, licence[0].expiredDate)
-  }
-
-  return changedDates
+  return { chargeVersions, licence }
 }
 
 async function _fetchChargeVersionsData (id) {
