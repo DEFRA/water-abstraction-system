e809fb62393ffe2b959456c2717d55d0337ac2b8
Rebecca Ransome <beckyrose200@aol.com>
2024-10-04 12:54:48 +0100

    Updated determine supplementary billing flags

--- app/services/licences/supplementary/determine-supplementary-billing-flags.service.js
+++ app/services/licences/supplementary/determine-supplementary-billing-flags.service.js
@@ -10,25 +10,69 @@ const FetchChargeVersionsService = require('./fetch-charge-versions.service.js')
 const { determineCurrentFinancialYear } = require('../../../lib/general.lib.js')
 const PersistSupplementaryBillingFlagsService = require('./persist-supplementary-billing-flags.service.js')
 
+const APRIL = 3
 const SROC = new Date('2022-04-01')
 
 /**
  * Determines and persists the flags for supplementary billing
  *
- * - Fetches the relevant charge versions and changed dates for the licence.
- * - Determines which flags should be applied based on the data.
+ * - Fetches the relevant charge versions and licence data
+ * - Determines which dates have changed on the licence and which flags should be applied based on the data.
  * - Persists the flags using the `PersistSupplementaryBillingFlagsService`.
  *
  * @param {*} naldLicence - The legacy NALD licence
  * @param {string} wrlsLicenceId - The UUID of the licence being flagged for supplementary billing
  */
 async function go (naldLicence, wrlsLicenceId) {
-  const { chargeVersions, changedDates, licence } = await FetchChargeVersionsService.go(naldLicence, wrlsLicenceId)
+  const { chargeVersions, licence } = await FetchChargeVersionsService.go(wrlsLicenceId)
 
-  const { preSrocFlag, srocFlag, twoPartTariffFinancialYears } = await _determineSupplementaryFlags(
-    changedDates, chargeVersions, licence, wrlsLicenceId)
+  if (chargeVersions.length === 0) {
+    return
+  }
+
+  const datesToProcess = _datesToProcess(licence, naldLicence)
+
+  if (datesToProcess.length === 0) {
+    return
+  }
+
+  const { preSrocFlag, srocFlag, twoPartTariffFinancialYears } = await _determineSupplementaryFlags(datesToProcess, chargeVersions, licence)
 
   await PersistSupplementaryBillingFlagsService.go(twoPartTariffFinancialYears, preSrocFlag, srocFlag, wrlsLicenceId)
+}
+
+function _datesToProcess (licence, naldLicence) {
+  const changedDates = _determineChangedDates(licence, naldLicence)
+  const { endDate } = determineCurrentFinancialYear()
+
+  return changedDates.filter((changedDate) => {
+    return changedDate < endDate
+  })
+}
+
+function _dateToFlag (changedDates, naldDate, wrlsDate) {
+  const { startDate, endDate } = determineCurrentFinancialYear()
+  const sixYearsAgo = new Date(startDate.getFullYear() - 6, APRIL, 1)
+
+  if (naldDate) {
+    changedDates.push(naldDate)
+
+    return
+  }
+
+  if (wrlsDate > endDate) {
+    return
+  }
+
+  if (wrlsDate < endDate && wrlsDate > sixYearsAgo) {
+    changedDates.push(wrlsDate)
+
+    return
+  }
+
+  if (wrlsDate < sixYearsAgo) {
+    changedDates.push(sixYearsAgo)
+  }
 }
 
 function _determineBillingYears (chargeVersions) {
@@ -43,16 +87,36 @@ function _determineBillingYears (chargeVersions) {
   return [...new Set(financialYearEnds)]
 }
 
-async function _determineSupplementaryFlags (changedDates, chargeVersions, licence, wrlsLicenceId) {
+function _determineChangedDates (licence, naldLicence) {
+  const { revokedDate, lapsedDate, expiredDate } = naldLicence
+
+  const changedDates = []
+
+  if (revokedDate !== licence[0].revokedDate) {
+    _dateToFlag(changedDates, revokedDate, licence[0].revokedDate)
+  }
+
+  if (lapsedDate !== licence[0].lapsedDate) {
+    _dateToFlag(changedDates, lapsedDate, licence[0].lapsedDated)
+  }
+
+  if (expiredDate !== licence[0].expiredDate) {
+    _dateToFlag(changedDates, expiredDate, licence[0].expiredDate)
+  }
+
+  return changedDates
+}
+
+async function _determineSupplementaryFlags (datesToProcess, chargeVersions, licence) {
   const { includeInSrocBilling, includeInPresrocBilling, preSroc } = licence
 
-  const earliestDate = _earliestDate(changedDates)
+  const earliestDate = _earliestDate(datesToProcess)
   const { endDate } = determineCurrentFinancialYear()
 
   const preSrocFlag = _flagPreSrocSupplementary(earliestDate, includeInPresrocBilling, preSroc)
   const srocFlag = _flagSrocSupplementary(chargeVersions, earliestDate, endDate, includeInSrocBilling)
   const twoPartTariffFinancialYears = await _flagTwoPartTariffSupplementary(
-    chargeVersions, earliestDate, endDate, wrlsLicenceId)
+    chargeVersions, earliestDate, endDate)
 
   return { preSrocFlag, srocFlag, twoPartTariffFinancialYears }
 }
@@ -77,6 +141,8 @@ async function _flagTwoPartTariffSupplementary (chargeVersions, earliestDate, en
   if (earliestDate < endDate && srocTwoPartTariffChargeVersions.length > 0) {
     return _determineBillingYears(srocTwoPartTariffChargeVersions)
   }
+
+  return []
 }
 
 function _flagSrocSupplementary (chargeVersions, earliestDate, endDate, includeInSrocBilling) {
--- app/services/licences/supplementary/fetch-charge-versions.service.js
+++ app/services/licences/supplementary/fetch-charge-versions.service.js
@@ -29,17 +29,17 @@ async function _fetchChargeVersionsData (id) {
   return ChargeVersionModel.query()
     .select('id', 'startDate', 'endDate')
     .where('licenceId', id)
-    .andWhere('start_date', '>=', '2022-04-01')
+    .andWhere('startDate', '>=', '2022-04-01')
     .modify((builder) => {
       builder.select(
         ChargeVersionModel.relatedQuery('chargeReferences')
-          .join('charge_elements as ce', 'ce.charge_reference_id', 'charge_references.id')
-          .where('charge_versions.id', ChargeVersionModel.ref('charge_references.charge_version_id'))
+          .join('chargeElements as ce', 'ce.chargeReferenceId', 'chargeReferences.id')
+          .where('chargeVersions.id', ChargeVersionModel.ref('chargeReferences.chargeVersionId'))
           .andWhere('ce.section127Agreement', true)
           .andWhere(ChargeReferenceModel.raw("charge_references.adjustments->>'s127' = 'true'"))
           .limit(1)
           .select(ChargeReferenceModel.raw('EXISTS(SELECT 1)'))
-          .as('two_part_tariff')
+          .as('twoPartTariff')
       )
     })
 }
@@ -51,19 +51,19 @@ async function _fetchLicenceData (id) {
       'licences.expiredDate',
       'licences.lapsedDate',
       'licences.revokedDate',
-      'include_in_sroc_billing',
-      'include_in_presroc_billing'
+      'includeInSrocBilling',
+      'includeInPresrocBilling'
     ])
     .distinctOn('licences.id')
     .where('licences.id', id)
-    .leftJoin('charge_versions as cv', 'licences.id', 'cv.licence_id')
+    .leftJoin('chargeVersions as cv', 'licences.id', 'cv.licenceId')
     .modify((builder) => {
       builder.select(
         ChargeVersionModel.query()
           .count()
-          .whereColumn('charge_versions.licence_id', 'licences.id')
-          .andWhere('charge_versions.start_date', '<', '2022-04-01')
-          .as('pre_sroc')
+          .whereColumn('charge_versions.licenceId', 'licences.id')
+          .andWhere('charge_versions.startDate', '<', '2022-04-01')
+          .as('preSroc')
       )
     })
 }
