65c3bc2277131255bc235a4b9bf2631a8b224ae4
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-10-05 11:04:03 +0100

    Add test for missing site description

--- test/services/return-requirements/generate-from-existing-requirements.service.test.js
+++ test/services/return-requirements/generate-from-existing-requirements.service.test.js
@@ -22,60 +22,121 @@ describe('Return Requirements - Generate From Existing Requirements service', ()
   })
 
   describe('when a matching return version exists', () => {
-    beforeEach(() => {
-      returnVersion = _returnVersion()
+    describe('and all its return requirements have a site description', () => {
+      beforeEach(() => {
+        returnVersion = _returnVersion()
 
-      Sinon.stub(FetchExistingRequirementsService, 'go').resolves(returnVersion)
+        Sinon.stub(FetchExistingRequirementsService, 'go').resolves(returnVersion)
+      })
+
+      it('returns the details of its return requirements transformed for use in the journey', async () => {
+        const result = await GenerateFromExistingRequirementsService.go(returnVersion.id)
+
+        expect(result).to.equal([
+          {
+            points: [returnVersion.returnRequirements[0].points[0].id],
+            purposes: [{
+              alias: returnVersion.returnRequirements[0].returnRequirementPurposes[0].alias,
+              description: 'Spray Irrigation - Storage',
+              id: returnVersion.returnRequirements[0].returnRequirementPurposes[0].purposeId
+            }],
+            returnsCycle: 'winter-and-all-year',
+            siteDescription: 'FIRST BOREHOLE AT AVALON',
+            abstractionPeriod: {
+              'end-abstraction-period-day': 31,
+              'end-abstraction-period-month': 3,
+              'start-abstraction-period-day': 1,
+              'start-abstraction-period-month': 4
+            },
+            frequencyReported: 'week',
+            frequencyCollected: 'week',
+            agreementsExceptions: ['none']
+          },
+          {
+            points: [returnVersion.returnRequirements[1].points[0].id],
+            purposes: [{
+              alias: '',
+              description: 'Spray Irrigation - Storage',
+              id: returnVersion.returnRequirements[1].returnRequirementPurposes[0].purposeId
+            }],
+            returnsCycle: 'summer',
+            siteDescription: 'SECOND BOREHOLE AT AVALON',
+            abstractionPeriod: {
+              'end-abstraction-period-day': 31,
+              'end-abstraction-period-month': 3,
+              'start-abstraction-period-day': 1,
+              'start-abstraction-period-month': 4
+            },
+            frequencyReported: 'month',
+            frequencyCollected: 'week',
+            agreementsExceptions: [
+              '56-returns-exception',
+              'gravity-fill',
+              'transfer-re-abstraction-scheme',
+              'two-part-tariff'
+            ]
+          }
+        ])
+      })
     })
 
-    it('returns the details of the its return requirements transformed for use in the journey', async () => {
-      const result = await GenerateFromExistingRequirementsService.go(returnVersion.id)
+    describe('but one of its return requirements does not have a site description', () => {
+      beforeEach(() => {
+        returnVersion = _returnVersion()
+        returnVersion.returnRequirements[1].siteDescription = null
 
-      expect(result).to.equal([
-        {
-          points: [returnVersion.returnRequirements[0].points[0].id],
-          purposes: [{
-            alias: returnVersion.returnRequirements[0].returnRequirementPurposes[0].alias,
-            description: 'Spray Irrigation - Storage',
-            id: returnVersion.returnRequirements[0].returnRequirementPurposes[0].purposeId
-          }],
-          returnsCycle: 'winter-and-all-year',
-          siteDescription: 'FIRST BOREHOLE AT AVALON',
-          abstractionPeriod: {
-            'end-abstraction-period-day': 31,
-            'end-abstraction-period-month': 3,
-            'start-abstraction-period-day': 1,
-            'start-abstraction-period-month': 4
+        Sinon.stub(FetchExistingRequirementsService, 'go').resolves(returnVersion)
+      })
+
+      it('returns the details of its return requirements transformed, falling back to point description for the missing site description', async () => {
+        const result = await GenerateFromExistingRequirementsService.go(returnVersion.id)
+
+        expect(result).to.equal([
+          {
+            points: [returnVersion.returnRequirements[0].points[0].id],
+            purposes: [{
+              alias: returnVersion.returnRequirements[0].returnRequirementPurposes[0].alias,
+              description: 'Spray Irrigation - Storage',
+              id: returnVersion.returnRequirements[0].returnRequirementPurposes[0].purposeId
+            }],
+            returnsCycle: 'winter-and-all-year',
+            siteDescription: 'FIRST BOREHOLE AT AVALON',
+            abstractionPeriod: {
+              'end-abstraction-period-day': 31,
+              'end-abstraction-period-month': 3,
+              'start-abstraction-period-day': 1,
+              'start-abstraction-period-month': 4
+            },
+            frequencyReported: 'week',
+            frequencyCollected: 'week',
+            agreementsExceptions: ['none']
           },
-          frequencyReported: 'week',
-          frequencyCollected: 'week',
-          agreementsExceptions: ['none']
-        },
-        {
-          points: [returnVersion.returnRequirements[1].points[0].id],
-          purposes: [{
-            alias: '',
-            description: 'Spray Irrigation - Storage',
-            id: returnVersion.returnRequirements[1].returnRequirementPurposes[0].purposeId
-          }],
-          returnsCycle: 'summer',
-          siteDescription: 'SECOND BOREHOLE AT AVALON',
-          abstractionPeriod: {
-            'end-abstraction-period-day': 31,
-            'end-abstraction-period-month': 3,
-            'start-abstraction-period-day': 1,
-            'start-abstraction-period-month': 4
-          },
-          frequencyReported: 'month',
-          frequencyCollected: 'week',
-          agreementsExceptions: [
-            '56-returns-exception',
-            'gravity-fill',
-            'transfer-re-abstraction-scheme',
-            'two-part-tariff'
-          ]
-        }
-      ])
+          {
+            points: [returnVersion.returnRequirements[1].points[0].id],
+            purposes: [{
+              alias: '',
+              description: 'Spray Irrigation - Storage',
+              id: returnVersion.returnRequirements[1].returnRequirementPurposes[0].purposeId
+            }],
+            returnsCycle: 'summer',
+            siteDescription: 'WELL AT WELLINGTON',
+            abstractionPeriod: {
+              'end-abstraction-period-day': 31,
+              'end-abstraction-period-month': 3,
+              'start-abstraction-period-day': 1,
+              'start-abstraction-period-month': 4
+            },
+            frequencyReported: 'month',
+            frequencyCollected: 'week',
+            agreementsExceptions: [
+              '56-returns-exception',
+              'gravity-fill',
+              'transfer-re-abstraction-scheme',
+              'two-part-tariff'
+            ]
+          }
+        ])
+      })
     })
   })
 
