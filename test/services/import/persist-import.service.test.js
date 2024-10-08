3f0685fd5529481d19bca73ebc2b5f60e2950747
jonathangoulding <jonathangoulding21@gmail.com>
2024-10-08 10:14:22 +0100

    test: update persist import service

--- test/services/import/persist-import.service.test.js
+++ test/services/import/persist-import.service.test.js
@@ -3,32 +3,77 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
-const { describe, it, beforeEach } = exports.lab = Lab.script()
+const { describe, it, beforeEach, afterEach } = exports.lab = Lab.script()
 const { expect } = Code
 
 // Test helpers
+const LicenceModel = require('../../../app/models/licence.model.js')
+const PersistCompanyService = require('../../../app/services/import/persist/persist-company.service.js')
+const PersistLicenceService = require('../../../app/services/import/persist/persist-licence.service.js')
+const PersistLicenceVersionsService = require('../../../app/services/import/persist/persist-licence-versions.service.js')
 
 // Thing under test
 const PersistImportService = require('../../../app/services/import/persist-import.service.js')
 
 describe('Persist licence service', () => {
-  let transformedCompanies
-  let transformedLicence
+  const transformedCompanies = []
+  const transformedLicence = []
 
-  describe('when an error is thrown when persisting the licence', () => {
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when the services are used', () => {
     beforeEach(() => {
-      // We cause the error by making the last record to be persisted have an invalid value. This demonstrates the
-      // purpose of the DB transaction: either all records are persisted or none of them
-      transformedLicence.licenceVersions[0].licenceVersionPurposes[0].primaryPurposeId = 'boom'
+      Sinon.stub(LicenceModel, 'transaction').callsFake(async (callback) => {
+        try {
+          return await callback() // Execute the callback within the transaction
+        } catch (error) {
+          return Promise.reject(error) // Properly handle and pass through the rejection
+        }
+      })
     })
 
-    it('throws an error and persists nothing', async () => {
-      await expect(PersistImportService.go(transformedLicence, transformedCompanies)).to.reject()
+    describe('when a licence has been successfully creates or updated', () => {
+      beforeEach(() => {
+        Sinon.stub(PersistLicenceService, 'go').resolves('1234')
+        Sinon.stub(PersistLicenceVersionsService, 'go').resolves()
+        Sinon.stub(PersistCompanyService, 'go').resolves()
+      })
 
-      const newLicence = await _fetchPersistedLicence(transformedLicence.licenceRef)
+      it('should return the licence id', async () => {
+        const result = await PersistImportService.go(transformedLicence, transformedCompanies)
 
-      expect(newLicence).to.be.undefined()
+        expect(result).to.equal('1234')
+      })
+    })
+
+    describe('when a service has failed', () => {
+      beforeEach(() => {
+        Sinon.stub(PersistLicenceService, 'go').resolves('1234')
+        Sinon.stub(PersistLicenceVersionsService, 'go').resolves()
+        Sinon.stub(PersistCompanyService, 'go').rejects(new Error('boom'))
+      })
+
+      it('should throw an error', async () => {
+        await expect(PersistImportService.go(transformedLicence, transformedCompanies)).to.reject(Error, 'boom')
+      })
+    })
+  })
+
+  describe('when the transaction has failed', () => {
+    beforeEach(() => {
+      Sinon.stub(PersistLicenceService, 'go').resolves('1234')
+      Sinon.stub(PersistLicenceVersionsService, 'go').resolves()
+      Sinon.stub(PersistCompanyService, 'go').resolves()
+
+      Sinon.stub(LicenceModel, 'transaction').rejects(new Error('boom'))
+    })
+
+    it('should throw an error', async () => {
+      await expect(PersistImportService.go(transformedLicence, transformedCompanies)).to.reject(Error, 'boom')
     })
   })
 })
