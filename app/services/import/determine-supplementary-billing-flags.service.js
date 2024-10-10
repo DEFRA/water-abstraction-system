d7f6126bf305f00aee6d604e5844aef40176b142
Rebecca Ransome <beckyrose200@aol.com>
2024-10-10 09:45:25 +0100

    Update omfg call and rename naldLicence to importedLicence

--- app/services/import/determine-supplementary-billing-flags.service.js
+++ app/services/import/determine-supplementary-billing-flags.service.js
@@ -11,41 +11,41 @@ const LicenceModel = require('../../models/licence.model.js')
 /**
  * Determines if an imported licence should be flagged for supplementary billing.
  *
- * This service is responsible for determining whether a licence imported from NALD should be flagged for
+ * This service is responsible for determining whether a licence imported should be flagged for
  * supplementary billing. It compares the licences end dates (such as lapsed, revoked or expired dates) between WRLS
  * and the transformed data, and if there is a change in the dates allows the licence to go on to determining the flags.
  *
- * @param {object} naldLicence - the legacy NALD licence
+ * @param {object} importedLicence - the imported licence
  * @param {object} wrlsLicenceId - the WRLS licence data
  */
-async function go (naldLicence, wrlsLicenceId) {
+async function go (importedLicence, wrlsLicenceId) {
   try {
-    const licenceChanged = await _licenceChanged(naldLicence, wrlsLicenceId)
+    const licenceChanged = await _licenceChanged(importedLicence, wrlsLicenceId)
 
     if (!licenceChanged) {
       return
     }
 
-    await ProcessImportedLicenceService.go(naldLicence, wrlsLicenceId)
+    await ProcessImportedLicenceService.go(importedLicence, wrlsLicenceId)
   } catch (error) {
-    global.GlobalNotifier.omfg('Supplementary Billing Flag failed for licence ', wrlsLicenceId, error)
+    global.GlobalNotifier.omfg('Determine supplementary billing flags on import failed ', { wrlsLicenceId }, error)
   }
 }
 
-async function _licenceChanged (naldLicence, wrlsLicenceId) {
+async function _licenceChanged (importedLicence, wrlsLicenceId) {
   const query = LicenceModel.query()
     .select(['id'])
     .where('id', wrlsLicenceId)
 
-  _whereClauses(query, naldLicence)
+  _whereClauses(query, importedLicence)
 
   const result = await query
 
   return result.length === 0
 }
 
-function _whereClauses (query, naldLicence) {
-  const { expiredDate, lapsedDate, revokedDate } = naldLicence
+function _whereClauses (query, importedLicence) {
+  const { expiredDate, lapsedDate, revokedDate } = importedLicence
 
   if (expiredDate) {
     query.where('expiredDate', expiredDate)
