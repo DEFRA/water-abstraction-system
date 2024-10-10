47ce22367fd2c15aa35345df9d9269d05ee5a9fb
Rebecca Ransome <beckyrose200@aol.com>
2024-10-10 16:39:04 +0100

    WIP

--- app/services/licences/supplementary/determine-workflow-years.service.js
+++ app/services/licences/supplementary/determine-workflow-years.service.js
@@ -7,6 +7,7 @@
 
 const { ref } = require('objection')
 
+const { db } = require('../../../../db/db.js')
 const ReturnLogModel = require('../../../models/return-log.model.js')
 const LicenceModel = require('../../../models/licence.model.js')
 const WorkflowModel = require('../../../models/workflow.model.js')
@@ -26,10 +27,24 @@ const SROC_START_DATE = new Date('2022-04-01')
  * licence should be flagged for two-part tariff supplementary billing
  */
 async function go (chargeVersionWorkflowId) {
-  const workflowRecord = await _fetchWorkflowRecord(chargeVersionWorkflowId)
+  // const { licence } = await _fetchLicence(chargeVersionWorkflowId)
 
-  console.log('Result! :', workflowRecord)
-  if (!workflowRecord || workflowRecord.licence.includeInSrocBilling) {
+  // console.log('Licence :', licence)
+  const licence1 = await _fetchLicenceData(chargeVersionWorkflowId)
+
+  console.log('Licence :', licence1)
+
+  const result = {
+    licence,
+    startDate,
+    endDate,
+    twoPartTariff: false,
+    flagForBilling: false
+  }
+
+  // If licence doesn't exist it means it doesn't have any sroc charge versions
+  // If licence is already flagged for supplementary billing the we don't want to flag it again
+  if (!licence || licence.includeInSrocBilling) {
     console.log('Returning as there are no records')
 
     return
@@ -43,7 +58,7 @@ async function _flagForSupplementaryBilling (id) {
   return LicenceModel.query().patch({ includeInSrocBilling: true }).where('id', id)
 }
 
-async function _fetchWorkflowRecord (chargeVersionWorkflowId) {
+async function _fetchLicence (chargeVersionWorkflowId) {
   return WorkflowModel.query()
     .findById(chargeVersionWorkflowId)
     .select([
@@ -57,12 +72,60 @@ async function _fetchWorkflowRecord (chargeVersionWorkflowId) {
         'includeInSrocBilling'
       ])
     })
-    .whereExists(
-      LicenceModel.query()
-        .joinRelated('chargeVersions')
-        .where('licences.id', WorkflowModel.ref('licenceId'))
-        .andWhere('chargeVersions.startDate', '>', '2022-04-01')
-    )
+    .withGraphFetched('licence.chargeVersions')
+    .modifyGraph('licence.chargeVersions', (builder) => {
+      builder
+        .count('id AS flagForSroc')
+        .where('startDate', '>', '2024-03-31')
+        .groupBy('licenceId')
+    })
+    // .whereExists(
+    //   LicenceModel.query()
+    //     .joinRelated('chargeVersions')
+    //     .where('licences.id', WorkflowModel.ref('licenceId'))
+    //     .andWhere('chargeVersions.startDate', '>', '2022-04-01')
+    // )
+}
+
+async function _fetchLicenceData (chargeVersionWorkflowId) {
+  const query = _query()
+
+  const { rows: [row] } = await db.raw(query, [chargeVersionWorkflowId])
+
+  return row
+}
+
+function _query () {
+  return `
+    SELECT
+    l.include_in_sroc_billing,
+    EXISTS (
+      SELECT 1
+      FROM public.charge_versions cv
+      WHERE cv.licence_id = l.id
+        AND cv.start_date > '2022-04-01'
+    ) AS srocChargeVersions,
+    EXISTS (
+      SELECT 1
+      FROM public.charge_references cr
+      INNER JOIN public.charge_elements ce
+        ON ce.charge_reference_id = cr.id
+      WHERE cr.charge_version_id = (
+        SELECT cv.id
+        FROM public.charge_versions cv
+        WHERE cv.licence_id = l.id
+        LIMIT 1
+      )
+      AND ce.section_127_Agreement = TRUE
+      AND cr.adjustments->>'s127' = 'true'
+    ) AS two_part_tariff
+    FROM licences l
+    WHERE id = (
+      SELECT w.licence_id
+      FROM workflows w
+      WHERE w.id = ?
+    );
+`
 }
 
 module.exports = {
