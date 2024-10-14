b2b701ff69083a3595205cb7b932c8e2d97dec3a
Roble <roblesiyad123@gmail.com>
2024-10-14 17:56:04 +0100

    Add view licence purposes presenter

---
+++ app/presenters/licences/view-licence-purposes.presenter.js
@@ -0,0 +1,70 @@
+'use strict'
+
+const { formatAbstractionPeriod } = require('../base.presenter.js')
+
+function go (licence) {
+  return {
+    id: licence.id,
+    licenceRef: licence.licenceRef,
+    licencePurposes: _formatLicencePurposes(licence.licenceVersions[0].licenceVersionPurposes),
+    pageTitle: 'Licence purpose details'
+  }
+}
+
+function _formatLicencePurposes (licenceVersionPurposes) {
+  return licenceVersionPurposes.map((licenceVersionPurpose) => {
+    return {
+      abstractionAmounts: _formatAbstractionAmounts(licenceVersionPurpose),
+      abstractionPeriod: _abstractionPeriod(licenceVersionPurpose),
+      abstractionPoints: _formatAbstractionPoints(licenceVersionPurpose.points),
+      purposeDescription: licenceVersionPurpose.purpose.description ? licenceVersionPurpose.purpose.description : '',
+    }
+  })
+}
+
+function _abstractionPeriod (licenceVersionPurpose) {
+  const startDay = licenceVersionPurpose.abstractionPeriodStartDay
+  const startMonth = licenceVersionPurpose.abstractionPeriodStartMonth
+  const endDay = licenceVersionPurpose.abstractionPeriodEndDay
+  const endMonth = licenceVersionPurpose.abstractionPeriodEndMonth
+
+  return formatAbstractionPeriod(startDay, startMonth, endDay, endMonth)
+}
+
+function _formatAbstractionPoints (points) {
+  return points.map((point) => {
+    return point.$describe()
+  })
+}
+
+function _formatAbstractionAmounts (licenceVersionPurpose) {
+  const details = []
+
+  if (!licenceVersionPurpose) {
+    return details
+  }
+
+  const { annualQuantity, dailyQuantity, hourlyQuantity, instantQuantity } = licenceVersionPurpose
+
+  if (annualQuantity) {
+    details.push(`${parseFloat(annualQuantity).toFixed(2)} cubic metres per year`)
+  }
+
+  if (dailyQuantity) {
+    details.push(`${parseFloat(dailyQuantity).toFixed(2)} cubic metres per day`)
+  }
+
+  if (hourlyQuantity) {
+    details.push(`${parseFloat(hourlyQuantity).toFixed(2)} cubic metres per hour`)
+  }
+
+  if (instantQuantity) {
+    details.push(`${parseFloat(instantQuantity).toFixed(2)} cubic metres per second`)
+  }
+
+  return details
+}
+
+module.exports = {
+  go
+}
