ebab844e8e4f9b7041238026ae2ef9c9bb715b57
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2024-10-21 12:10:45 +0100

    Licence import flagging for supplementary billing (#1338)
    https://eaflood.atlassian.net/browse/WATER-4591
    
    During the licence import process, we need to check if a licence has been lapsed/revoked or ended. A licence can end at any time, either in the past or the future. If the licence has ended in the past then the licence bills will be incorrect. This means the licence will need to be flagged for either pre-sroc supplementary billing, sroc supplementary billing or two-part tariff supplementary billing (depending on what is affected). This PR is building the service needed to check the licence lapsed/revoked or ended date and flagging if necessary.

---
+++ app/services/licences/supplementary/persist-supplementary-billing-flags.service.js
@@ -0,0 +1,59 @@
+'use strict'
+
+/**
+ * Persists the supplementary billing flags for a licence
+ * @module PersistSupplementaryBillingFlagsService
+ */
+
+const CreateLicenceSupplementaryYearService = require('./create-licence-supplementary-year.service.js')
+const LicenceModel = require('../../../models/licence.model.js')
+
+/**
+ * Persists the supplementary billing flags for a licence
+ *
+ * Updates the licences includeInPresrocBilling and includeInSrocBilling flags.
+ * Adds financial years related to two-part tariff billing into the LicenceSupplementaryYears table.
+ *
+ * NOTE: Due to the column data type of the includeInPresrocBilling & includeInSrocBilling, one is a string value and
+ * one is a boolean.
+ *
+ * @param {object[]} twoPartTariffBillingYears - The years that need persisting in the LicenceSupplementaryYears table
+ * @param {boolean} flagForPreSrocSupplementary - `true` or `false` depending on if the licence needs to be flagged
+ * for pre sroc billing
+ * @param {boolean} flagForSrocSupplementary - `true` or `false` depending on if the licence needs to be flagged for
+ * sroc billing
+ * @param {string} licenceId - The UUID of the licence that needs the flags persisting for
+ *
+ * @returns {Promise<object>} - Resolves with the result of persisting two-part tariff billing years,
+ */
+async function go (twoPartTariffBillingYears, flagForPreSrocSupplementary, flagForSrocSupplementary, licenceId) {
+  const includeInPresrocBilling = flagForPreSrocSupplementary ? 'yes' : 'no'
+
+  await _updateLicenceFlags(includeInPresrocBilling, flagForSrocSupplementary, licenceId)
+
+  return _flagForLicenceSupplementaryYears(twoPartTariffBillingYears, licenceId)
+}
+
+/**
+ * Persists two-part tariff financial years in the LicenceSupplementaryYears table.
+ * @private
+ */
+async function _flagForLicenceSupplementaryYears (twoPartTariffBillingYears, licenceId) {
+  if (twoPartTariffBillingYears.length === 0) {
+    return
+  }
+
+  const twoPartTariff = true
+
+  return CreateLicenceSupplementaryYearService.go(licenceId, twoPartTariffBillingYears, twoPartTariff)
+}
+
+async function _updateLicenceFlags (includeInPresrocBilling, flagForSrocSupplementary, licenceId) {
+  return LicenceModel.query()
+    .patch({ includeInPresrocBilling, includeInSrocBilling: flagForSrocSupplementary })
+    .where('id', licenceId)
+}
+
+module.exports = {
+  go
+}
