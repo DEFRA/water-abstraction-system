a0f8a860e98bf5fd787aabe8503fb9daeac5cfc5
rvsiyad <roblesiyad123@gmail.com>
2024-10-23 16:40:09 +0100

    Fix failing test in view licence purposes presenter

--- test/presenters/licences/view-licence-purposes.presenter.test.js
+++ test/presenters/licences/view-licence-purposes.presenter.test.js
@@ -134,30 +134,69 @@ describe('View Licence Purpose presenter', () => {
 
     describe('when there are multiple licences with identical `licenceVersionPurposes` details', () => {
       beforeEach(() => {
-        licence.licenceVersions[0].licenceVersionPurposes.push({
-          abstractionPeriodStartDay: 1,
-          abstractionPeriodStartMonth: 4,
-          abstractionPeriodEndDay: 31,
-          abstractionPeriodEndMonth: 10,
-          annualQuantity: 180000,
-          dailyQuantity: 720,
-          hourlyQuantity: 144,
-          instantQuantity: 40,
-          purpose: {
-            id: '0316229a-e76d-4785-bc2c-65075a1a8f50',
-            description: 'Spray Irrigation - Storage'
-          },
-          points: [{
-            id: 'ab80acd6-7c2a-4f51-87f5-2c397829a0bb',
-            description: null,
-            ngr1: 'TL 23198 88603',
-            ngr2: null,
-            ngr3: null,
-            ngr4: null,
-            source: {
-              id: 'b0b12db5-e95c-44a7-8008-2389fdbba9db',
-              description: 'SURFACE WATER SOURCE OF SUPPLY'
-            }
+        licence = LicenceModel.fromJson({
+          id: '761bc44f-80d5-49ae-ab46-0a90495417b5',
+          licenceRef: '01/123',
+          licenceVersions: [{
+            createdAt: new Date('2022-06-05'),
+            id: '4c42fd78-6e68-4eaa-9c88-781c323a5a38',
+            reason: 'new-licence',
+            status: 'current',
+            startDate: new Date('2022-04-01'),
+            licenceVersionPurposes: [{
+              id: '7f5e0838-d87a-4c2e-8e9b-09d6814b9ec4',
+              abstractionPeriodStartDay: 1,
+              abstractionPeriodStartMonth: 4,
+              abstractionPeriodEndDay: 31,
+              abstractionPeriodEndMonth: 10,
+              annualQuantity: 180000,
+              dailyQuantity: 720,
+              hourlyQuantity: 144,
+              instantQuantity: 40,
+              purpose: {
+                id: '0316229a-e76d-4785-bc2c-65075a1a8f50',
+                description: 'Spray Irrigation - Storage'
+              },
+              points: [{
+                id: 'ab80acd6-7c2a-4f51-87f5-2c397829a0bb',
+                description: null,
+                ngr1: 'TL 23198 88603',
+                ngr2: null,
+                ngr3: null,
+                ngr4: null,
+                source: {
+                  id: 'b0b12db5-e95c-44a7-8008-2389fdbba9db',
+                  description: 'SURFACE WATER SOURCE OF SUPPLY'
+                }
+              }]
+            },
+            {
+              id: '7f5e0838-d87a-4c2e-8e9b-09d6814b9ec4',
+              abstractionPeriodStartDay: 1,
+              abstractionPeriodStartMonth: 4,
+              abstractionPeriodEndDay: 31,
+              abstractionPeriodEndMonth: 10,
+              annualQuantity: 180000,
+              dailyQuantity: 720,
+              hourlyQuantity: 144,
+              instantQuantity: 40,
+              purpose: {
+                id: '0316229a-e76d-4785-bc2c-65075a1a8f50',
+                description: 'Spray Irrigation - Storage'
+              },
+              points: [{
+                id: 'ab80acd6-7c2a-4f51-87f5-2c397829a0bb',
+                description: null,
+                ngr1: 'TL 23198 88603',
+                ngr2: null,
+                ngr3: null,
+                ngr4: null,
+                source: {
+                  id: 'b0b12db5-e95c-44a7-8008-2389fdbba9db',
+                  description: 'SURFACE WATER SOURCE OF SUPPLY'
+                }
+              }]
+            }]
           }]
         })
       })
