ca6338f63bf864c53e3f49b9ba29ba01d2cc4d84
rvsiyad <roblesiyad123@gmail.com>
2024-10-23 13:13:37 +0100

    Add unique check for `licenceVersionPurposes` to remove duplicates and add NOTE

--- app/presenters/licences/view-licence-purposes.presenter.js
+++ app/presenters/licences/view-licence-purposes.presenter.js
@@ -24,14 +24,30 @@ function go (licence) {
   }
 }
 
+// NOTE: Maps over the `licenceVersionPurposes` array, formatting each `licenceVersionPurpose` object to extract
+// relevant information while ensuring uniqueness. A Set is used to track already processed licence details by
+// "stringify-ing" each object. If a duplicate is found, it returns null for that entry. After mapping, it filters out
+// any null values.
 function _formatLicencePurposes (licenceVersionPurposes) {
+  const uniquePurposeDetails = new Set()
+
   return licenceVersionPurposes.map((licenceVersionPurpose) => {
+    const licenceString = JSON.stringify(licenceVersionPurpose)
+
+    if (uniquePurposeDetails.has(licenceString)) {
+      return null
+    } else {
+      uniquePurposeDetails.add(licenceString)
+    }
+
     return {
       abstractionAmounts: _formatAbstractionAmounts(licenceVersionPurpose),
       abstractionPeriod: _abstractionPeriod(licenceVersionPurpose),
       abstractionPoints: _formatAbstractionPoints(licenceVersionPurpose.points),
       purposeDescription: licenceVersionPurpose.purpose.description ? licenceVersionPurpose.purpose.description : ''
     }
+  }).filter((item) => {
+    return item !== null
   })
 }
 
