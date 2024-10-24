b263e5ab444a386397112952a435e565f3b27ffb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-10-24 12:59:11 +0100

    Enhance model and add first instance test

--- test/models/billing-account.model.test.js
+++ test/models/billing-account.model.test.js
@@ -17,6 +17,7 @@ const ChargeVersionHelper = require('../support/helpers/charge-version.helper.js
 const ChargeVersionModel = require('../../app/models/charge-version.model.js')
 const CompanyHelper = require('../support/helpers/company.helper.js')
 const CompanyModel = require('../../app/models/company.model.js')
+const ContactHelper = require('../support/helpers/contact.helper.js')
 
 // Thing under test
 const BillingAccountModel = require('../../app/models/billing-account.model.js')
@@ -158,6 +159,60 @@ describe('Billing Account model', () => {
 
         expect(result.company).to.be.an.instanceOf(CompanyModel)
         expect(result.company).to.equal(testCompany)
+      })
+    })
+  })
+
+  describe('Instance methods', () => {
+    let alternateBillingAccount
+
+    before(async () => {
+      // Link the alternate billing account we'll use in instance method testing to the same company as the test record.
+      // We'll link the 'current' billing account address to a different company to prove it takes precedence
+      alternateBillingAccount = await BillingAccountHelper.add({ companyId: testCompany.id })
+
+      // Add the first billing account address record which should be ignored as it is no longer 'current'
+      await BillingAccountAddressHelper.add({
+        billingAccountId: alternateBillingAccount.id,
+        endDate: new Date('2024-05-31'),
+        startDate: new Date('2024-04-01')
+      })
+
+      // Add the second billing account address record, which will be linked to a contact and different company to the
+      // main billing account record
+      const contact = await ContactHelper.add({ firstName: 'Bugs', lastName: 'Bunny' })
+      const alternateCompany = await CompanyHelper.add({ name: 'Acme Ltd (UK)' })
+
+      // Add two billing account address records so we can ensure the 'current' logic in the modifier is being exercised
+      await BillingAccountAddressHelper.add({
+        billingAccountId: alternateBillingAccount.id,
+        companyId: alternateCompany.id,
+        contactId: contact.id,
+        startDate: new Date('2024-06-01')
+      })
+    })
+
+    describe('$accountName', () => {
+      let fetchedRecord
+
+      describe("when the 'current' billing account address is not linked to a company", () => {
+        it('uses the name of the company linked directly to the billing account', async () => {
+          fetchedRecord = await BillingAccountModel.query().findById(testRecord.id).modify('contactDetails')
+
+          const result = fetchedRecord.$accountName()
+
+          expect(result).to.equal('Example Trading Ltd')
+        })
+      })
+
+      describe("when the 'current' billing account address is linked to a company", () => {
+        it('uses the name of the company linked to the billing account address', async () => {
+          fetchedRecord = await BillingAccountModel.query().findById(alternateBillingAccount.id).modify('contactDetails')
+
+          const result = fetchedRecord.$accountName()
+
+          expect(result).to.equal('Acme Ltd (UK)')
+        })
       })
     })
   })
