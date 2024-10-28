c57ea032c448f51f7c8bf8c6836e3fcca9e1f1fb
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-10-28 08:39:11 +0000

    Update Review licence service to use new modules

--- app/services/bill-runs/two-part-tariff/review-licence.service.js
+++ app/services/bill-runs/two-part-tariff/review-licence.service.js
@@ -1,28 +1,27 @@
 'use strict'
 
 /**
- * Orchestrates fetching and presenting the data needed for the licence review page in a two-part tariff bill run
+ * Orchestrates fetching and presenting the data needed for the two-part tariff review licence page
  * @module ReviewLicenceService
  */
 
-const FetchReviewLicenceResultsService = require('./fetch-review-licence-results.service.js')
-const ReviewLicencePresenter = require('../../../presenters/bill-runs/two-part-tariff/review-licence.presenter.js')
+const FetchReviewLicenceService = require('../review/fetch-review-licence.service.js')
+const ReviewLicencePresenter = require('../../../presenters/bill-runs/review/review-licence.presenter.js')
 
 /**
- * Orchestrates fetching and presenting the data needed for the licence review page in a two-part tariff bill run
+ * Orchestrates fetching and presenting the data needed for the two-part tariff review licence page
  *
- * @param {module:BillRunModel} billRunId - The UUID for the bill run
- * @param {module:LicenceModel} licenceId - The UUID of the licence that is being reviewed
+ * @param {string} reviewLicenceId - The UUID of the licence that is being reviewed
  * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
  *
  * @returns {Promise<object>} the 'pageData' needed for the review licence page. It contains the licence, bill run,
  * matched and unmatched returns and the licence charge data
  */
-async function go (billRunId, licenceId, yar) {
-  const { billRun, licence } = await FetchReviewLicenceResultsService.go(billRunId, licenceId)
+async function go (reviewLicenceId, yar) {
+  const reviewLicence = await FetchReviewLicenceService.go(reviewLicenceId)
 
   const [bannerMessage] = yar.flash('banner')
-  const pageData = ReviewLicencePresenter.go(billRun, licence)
+  const pageData = ReviewLicencePresenter.go(reviewLicence)
 
   return {
     bannerMessage,
