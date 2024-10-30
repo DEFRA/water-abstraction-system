32faac9d34265576114b1927306563b8b8d789f3
Rebecca Ransome <beckyrose200@aol.com>
2024-10-30 11:36:14 +0000

    Update: Determine workflow flags service comments

--- app/services/licences/supplementary/determine-workflow-flags.service.js
+++ app/services/licences/supplementary/determine-workflow-flags.service.js
@@ -16,13 +16,13 @@ const { determineCurrentFinancialYear } = require('../../../lib/general.lib.js')
  * by canceling a charge version awaiting approval), this action does not alter the licence charge information.
  * However, supplementary billing flags are necessary in this case because any licence in workflow is excluded
  * from the annual billing cycle. If a licence is in workflow when its region's annual billing run occurs, it
- * would be missed for that financial year and will need to be included in the supplementary billing run.
+ * would be missed for that financial year and will need to be included in the supplementary bill run.
  *
  * The service only focuses on the sroc and two-part tariff supplementary flags. Since only sroc annual billing is
  * active (effective from April 1, 2022), licences coming through to this service will not require pre-sroc flagging.
  *
- * The uses the workflows createdAt date and the licences regionId to check if any annual bill runs occurred while the
- * licence was in workflow.
+ * The service uses the workflows createdAt date and the licences regionId to check if any annual bill runs occurred
+ * while the licence was in workflow.
  * If the licence is lapsed, expired or revoked the existing flags are returned unchanged.
  *
  * As the service always persists the flags, we set the pre-sroc supplementary flag to what the licence already has,
@@ -32,7 +32,7 @@ const { determineCurrentFinancialYear } = require('../../../lib/general.lib.js')
  * applied to.
  *
  * NOTE: Unlike pre-sroc and sroc flags (which apply at the licence level), two-part tariff flags are year specific.
- * They are stored in the `LicenceSupplementaryYears` table for each affected year of the charge version
+ * They are stored in the `LicenceSupplementaryYears` table for each affected year.
  *
  * @param {string} workflowId - The UUID for the workflow record to fetch
  *
@@ -77,7 +77,7 @@ async function go (workflowId) {
 /**
  * Checks if a licence should be flagged for SRoC supplementary billing based on recent annual bill runs.
  *
- * Searches for any annual bills runs that have been created since the workflow records `createdAt` date. Uses the
+ * Searches for any annual bill runs that have been created since the workflow records `createdAt` date. Uses the
  * licence region, batch type and bill run status as criteria.
 
  * Flags the licence for supplementary billing if any eligible bill runs are found.
