db601f73c2970da23cfef79246a5f4b0aae30db6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-10-31 09:16:07 +0000

    Housekeeping - make Fetch service's reuse clearer

--- app/services/bill-runs/review/fetch-review-charge-reference.service.js
+++ app/services/bill-runs/review/fetch-review-charge-reference.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * Fetches the selected review charge reference instance and related data for the 2PT review charge reference page
+ * Fetches the selected review charge reference instance and related data for the 2PT review charge reference pages
  * @module FetchReviewChargeReferenceService
  */
 
@@ -10,12 +10,15 @@ const { ref } = require('objection')
 const ReviewChargeReferenceModel = require('../../../models/review-charge-reference.model.js')
 
 /**
- * Fetches the selected review charge reference instance and related data for the 2PT review charge reference page
+ * Fetches the selected review charge reference instance and related data for the 2PT review charge reference pages
+ *
+ * This fetch service fetches the data needed for the main review charge reference page, but also the authorised,
+ * factors and preview charge pages/services.
  *
  * @param {string} reviewChargeReferenceId - the UUID of the selected review charge reference
  *
  * @returns {module:ReviewChargeReferenceModel} the matching `ReviewChargeReferenceModel` instance and related data
- * needed for the two-part tariff review charge reference page
+ * needed for the two-part tariff review charge reference page and sub-pages
  */
 async function go (reviewChargeReferenceId) {
   return _fetch(reviewChargeReferenceId)
