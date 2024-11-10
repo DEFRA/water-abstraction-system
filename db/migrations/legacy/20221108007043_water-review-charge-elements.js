f0f218e4fe38c786a9df902b1694966800e0d838
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-11-10 17:07:36 +0000

    Add new test-only legacy migrations

---
+++ db/migrations/legacy/20221108007043_water-review-charge-elements.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const tableName = 'review_charge_elements'
+
+exports.up = function (knex) {
+  return knex
+    .schema
+    .withSchema('water')
+    .createTable(tableName, (table) => {
+      // Primary Key
+      table.uuid('id').primary().defaultTo(knex.raw('gen_random_uuid()'))
+
+      // Data
+      table.uuid('review_charge_reference_id').notNullable()
+      table.uuid('charge_element_id').notNullable()
+      // Specifying `null, null` creates a decimal column that can store numbers of any precision and scale
+      table.decimal('allocated', null, null).notNullable().defaultTo(0)
+      table.decimal('amended_allocated', null, null).notNullable().defaultTo(0)
+      table.boolean('charge_dates_overlap').notNullable().defaultTo(false)
+      table.text('issues')
+      table.string('status').notNullable().defaultTo('ready')
+
+      // Timestamps
+      table.timestamp('created_at', { useTz: false }).notNullable().defaultTo(knex.fn.now())
+      table.timestamp('updated_at', { useTz: false }).notNullable().defaultTo(knex.fn.now())
+    })
+}
+
+exports.down = function (knex) {
+  return knex
+    .schema
+    .withSchema('water')
+    .dropTableIfExists(tableName)
+}
