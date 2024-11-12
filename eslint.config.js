6f35c297c7b5492ed7d4b97c0fc85b9020d68654
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-11-12 18:36:03 +0000

    Add JSDoc plugin/linting back in
    This is admittedly where we could be seen to "cross the streams". prettier doesn't format JSDoc comments. There is a plugin (https://www.npmjs.com/package/prettier-plugin-jsdoc) though its not official. When we checked it we found its options were limited compared to eslint-plugin-jsdoc. Critically, there is no option to tell it to ignore @private or _myPrivateMethod() methods.
    
    It also doesn't have the same level of support as eslint-plugin-jsdoc. So, though it could be argued it is more concerned with 'style', we'll continue to use eslint-plugin-jsdoc to lint our JSDoc comments.

--- eslint.config.js
+++ eslint.config.js
@@ -1,16 +1,47 @@
 'use strict'
 
+const jsdoc = require('eslint-plugin-jsdoc')
+
 module.exports = [
   {
     languageOptions: {
       ecmaVersion: "latest",
       sourceType: "commonjs"
     },
+    plugins: {
+      jsdoc
+    },
     rules: {
       // Enforce braces around the function body of arrow functions
       "arrow-body-style": ["error", "always"],
       // Enforce 'use strict' declarations in all modules
-      strict: ['error', 'global']
+      strict: ['error', 'global'],
+      'jsdoc/check-alignment': 'warn',
+      'jsdoc/check-indentation': 'warn',
+      'jsdoc/check-types': 'warn',
+      'jsdoc/check-tag-names': 'warn',
+      'jsdoc/lines-before-block': 'warn',
+      'jsdoc/newline-after-description': 'off', // does not work with 'use strict'
+      'jsdoc/require-description': 'warn',
+      'jsdoc/require-hyphen-before-param-description': 'warn',
+      'jsdoc/require-jsdoc': ['warn', { publicOnly: true }],
+      'jsdoc/require-param': ['warn', { exemptedBy: ['private'] }],
+      'jsdoc/require-returns': ['warn', { publicOnly: true }]
+    },
+    settings: {
+      jsdoc: {
+        mode: 'jsdoc',
+        ignorePrivate: true
+      }
+    }
+  },
+  // This section works as an override to the configuration object above. It tells jsdoc to ignore any files in the
+  // `app/controllers` and `db/seeds` directories. The controllers purposefully do very little and the purpose of the
+  // seed files is obvious
+  {
+    files: ['app/controllers/**/*', 'db/seeds/**/*'],
+    rules: {
+      'jsdoc/require-jsdoc': 'off'
     }
   }
 ]
