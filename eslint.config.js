961f4d97cd119077c682e6cc7670805e93d52b96
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-11-12 15:08:51 +0000

    Add back in enforce use strict declaration
    Prettier will format it if present but won't add it as it is seen as a code rule, not a style.

--- eslint.config.js
+++ eslint.config.js
@@ -8,7 +8,9 @@ module.exports = [
     },
     rules: {
       // Enforce braces around the function body of arrow functions
-      "arrow-body-style": ["error", "always"]
+      "arrow-body-style": ["error", "always"],
+      // Enforce 'use strict' declarations in all modules
+      strict: ['error', 'global']
     }
   }
 ]
