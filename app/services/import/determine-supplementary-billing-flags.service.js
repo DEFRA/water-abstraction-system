e309c367429dba6fa83adad5dbb9d4111dfd6f41
Rebecca Ransome <beckyrose200@aol.com>
2024-11-20 21:23:10 +0000

    Update: Imported licence process to use new communal flagging service

--- app/services/import/determine-supplementary-billing-flags.service.js
+++ app/services/import/determine-supplementary-billing-flags.service.js
@@ -6,7 +6,7 @@
  */
 
 const LicenceModel = require('../../models/licence.model.js')
-const ProcessImportedLicenceService = require('../licences/supplementary/process-imported-licence.service.js')
+const ProcessBillingFlagService = require('../licences/supplementary/process-billing-flag.service.js')
 
 /**
  * Determines if an imported licence has a new end date.
@@ -30,7 +30,12 @@ async function go (importedLicence, licenceId) {
       return
     }
 
-    return ProcessImportedLicenceService.go(importedLicence, licenceId)
+    const payload = {
+      importedLicence,
+      licenceId
+    }
+
+    return ProcessBillingFlagService.go(payload)
   } catch (error) {
     global.GlobalNotifier.omfg('Determine supplementary billing flags on import failed ', { licenceId }, error)
   }
