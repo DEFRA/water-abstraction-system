87e3e9bd381a0808d495add4847027ba0aab8729
rvsiyad <roblesiyad123@gmail.com>
2024-11-20 11:10:02 +0000

    Replace ternary operations with nullish coalescing to simplify null/undefined checks

--- app/presenters/licences/view-licence-points.presenter.js
+++ app/presenters/licences/view-licence-points.presenter.js
@@ -32,21 +32,21 @@ function _formatLicencePoints (points) {
     const pointInstance = PointModel.fromJson(point)
 
     return {
-      bgsReference: pointInstance.bgsReference ? pointInstance.bgsReference : '',
-      category: pointInstance.category ? pointInstance.category : '',
+      bgsReference: pointInstance.bgsReference ?? '',
+      category: pointInstance.category ?? '',
       depth: pointInstance.depth.toString(),
-      description: pointInstance.description ? pointInstance.description : '',
+      description: pointInstance.description ?? '',
       gridReference: pointInstance.$describe(),
       hydroInterceptDistance: pointInstance.hydroInterceptDistance.toString(),
       hydroOffsetDistance: pointInstance.hydroOffsetDistance.toString(),
-      hydroReference: pointInstance.hydroReference ? pointInstance.hydroReference : '',
-      locationNote: pointInstance.locationNote ? pointInstance.locationNote : '',
-      note: pointInstance.note ? pointInstance.note : '',
-      primaryType: pointInstance.primaryType ? pointInstance.primaryType : '',
-      secondaryType: pointInstance.secondaryType ? pointInstance.secondaryType : '',
-      sourceDescription: pointInstance.sourceDescription ? pointInstance.sourceDescription : '',
-      sourceType: pointInstance.sourceType ? pointInstance.sourceType : '',
-      wellReference: pointInstance.wellReference ? pointInstance.wellReference : ''
+      hydroReference: pointInstance.hydroReference ?? '',
+      locationNote: pointInstance.locationNote ?? '',
+      note: pointInstance.note ?? '',
+      primaryType: pointInstance.primaryType ?? '',
+      secondaryType: pointInstance.secondaryType ?? '',
+      sourceDescription: pointInstance.sourceDescription ?? '',
+      sourceType: pointInstance.sourceType ?? '',
+      wellReference: pointInstance.wellReference ?? ''
     }
   })
 }
