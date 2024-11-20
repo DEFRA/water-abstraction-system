4feb7862f01c15fccc0bc8262a8409b897a43913
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2024-11-20 20:29:46 +0000

    Refactor existing supplementary billing flags service (#1483)
    https://eaflood.atlassian.net/browse/WATER-4746
    
    As part of our efforts to clean up the legacy code base and ensure that our existing functionality is working, we have been refactoring and migrating the logic for supplementary billing flags.
    This PR focuses on refactoring the existing legacy code to handle flagging for all three types (pre-sroc, sroc and two-part tariff).

---
+++ app/services/licences/supplementary/determine-return-log-flags.service.js
@@ -0,0 +1,88 @@
+'use strict'
+
+/**
+ * Determines if a licence with a change in return log should be flagged for supplementary billing
+ * @module DetermineReturnLogFlagsService
+ */
+
+const { ref } = require('objection')
+
+const ReturnLogModel = require('../../../models/return-log.model.js')
+
+const SROC_START_DATE = new Date('2022-04-01')
+
+/**
+ * Determines if a licence should be flagged for supplementary billing based on a change on a return log
+ *
+ * The service determines which flags should be added to the licence based on the returnLogId it receives.
+ * It uses the return logs dates and if the return is two-part tariff to decide the appropriate flags for supplementary
+ * billing.
+ *
+ * Before we determine which flags to add to the licence we first determine which flags the licence already has so we
+ * can maintain them. This is done when we declare our result object.
+ *
+ * If the returns start date is before the start date for sroc (1st April 2022), the licence is flagged for pre-sroc
+ * supplementary billing
+ *
+ * If the returns end date is after the start date for sroc (1st April 2022) and the return is not two-part tariff, the
+ * licence is flagged for sroc supplementary billing.
+ *
+ * If the return is two-part tariff then we flag it for twoPartTariffSupplementary and pass the start and end date of
+ * the return through to the next service, where the two-part tariff years are determined.
+ *
+ * NOTE: Unlike pre-sroc and sroc flags (which apply at the licence level), two-part tariff flags are year specific.
+ * They are stored in the `LicenceSupplementaryYears` table for each affected year of the charge version
+ *
+ * @param {string} returnLogId - The UUID for the return log to fetch
+ *
+ * @returns {object} - An object containing the related licenceId, regionId, return start and end date and licence
+ * supplementary billing flags
+ */
+async function go (returnLogId) {
+  const { twoPartTariff, licence, endDate, startDate } = await _fetchReturnLog(returnLogId)
+
+  const result = {
+    licenceId: licence.id,
+    regionId: licence.regionId,
+    startDate,
+    endDate,
+    flagForPreSrocSupplementary: licence.includeInPresrocBilling === 'yes',
+    flagForSrocSupplementary: licence.includeInSrocBilling,
+    flagForTwoPartTariffSupplementary: twoPartTariff
+  }
+
+  if (startDate < SROC_START_DATE) {
+    result.flagForPreSrocSupplementary = true
+  }
+
+  if (!twoPartTariff && endDate > SROC_START_DATE) {
+    result.flagForSrocSupplementary = true
+  }
+
+  return result
+}
+
+async function _fetchReturnLog (returnLogId) {
+  return ReturnLogModel.query()
+    .findById(returnLogId)
+    .select([
+      'id',
+      'licenceRef',
+      'startDate',
+      'endDate',
+      ref('metadata:isTwoPartTariff').castBool().as('twoPartTariff')
+    ])
+    .withGraphFetched('licence')
+    .modifyGraph('licence', (builder) => {
+      builder.select([
+        'id',
+        'regionId',
+        'includeInSrocBilling',
+        'includeInPresrocBilling'
+      ])
+    })
+}
+
+module.exports = {
+  go
+}
--- app/services/licences/supplementary/process-billing-flag.service.js
+++ app/services/licences/supplementary/process-billing-flag.service.js
@@ -5,75 +5,90 @@
  * @module ProcessBillingFlagService
  */
 
-const CreateLicenceSupplementaryYearService = require('./create-licence-supplementary-year.service.js')
 const DetermineBillingYearsService = require('./determine-billing-years.service.js')
 const DetermineExistingBillRunYearsService = require('./determine-existing-bill-run-years.service.js')
-const DetermineChargeVersionYearsService = require('./determine-charge-version-years.service.js')
-const DetermineReturnLogYearsService = require('./determine-return-log-years.service.js')
-const DetermineWorkflowYearsService = require('./determine-workflow-years.service.js')
+const DetermineChargeVersionFlagsService = require('./determine-charge-version-flags.service.js')
+const DetermineReturnLogFlagsService = require('./determine-return-log-flags.service.js')
+const DetermineWorkflowFlagsService = require('./determine-workflow-flags.service.js')
 const { calculateAndLogTimeTaken, currentTimeInNanoseconds } = require('../../../lib/general.lib.js')
+const PersistSupplementaryBillingFlagsService = require('./persist-supplementary-billing-flags.service.js')
 
 /**
  * Orchestrates flagging a licence for supplementary billing
  *
  * This service orchestrates the process of flagging a licence for supplementary billing.
- * Based on the ID the service receives (chargeVersionId, returnId, workflowId) it retrieves details of the related
- * charge version, including the licence information, start and end dates, whether the
- * charge version has two-part tariff indicators, and if it should be flagged for supplementary billing.
+ * Based on the ID the service receives, it retrieves details of the related licence and charge versions, including
+ * start and end dates (these are used to work out which two-part tariff years to flag), whether the charge version has
+ * two-part tariff indicators, and if it should be flagged for pre-sroc, sroc and two-part tariff supplementary billing.
  *
- * If the licence qualifies for flagging, the relevant dates are passed to the `DetermineBillingYearsService`, which
- * calculates the years affected by the changes to the licence.
+ * If the licence qualifies for two-part tariff flagging, the relevant dates are passed to the
+ * `DetermineBillingYearsService`, which calculates the years affected by the changes to the licence.
  *
  * If any SROC years are affected, they are passed to the `DetermineExistingBillRunYearsService`. This service checks
  * the affected years to see if they have had an annual or two-part tariff bill runs created. We avoid flagging any
  * years that haven't had an annual or two-part tariff bill run, as those changes will be captured when the bill run is
  * created.
  *
- * Finally, we call the `CreateLicenceSupplementaryYearService`, which persists our final list of years along with the
- * licence ID and whether two-part tariff is true.
+ * Finally, we call the `PersistSupplementaryBillingFlagsService`, which persists all the flags we have determined.
  *
  * @param {object} payload - The payload from the request
  */
 async function go (payload) {
   try {
     const startTime = currentTimeInNanoseconds()
+    const result = await _determineFlags(payload)
 
-    let result
+    await _setFlagForLicence(result)
 
-    if (payload.chargeVersionId) {
-      result = await DetermineChargeVersionYearsService.go(payload.chargeVersionId)
-    } else if (payload.returnId) {
-      result = await DetermineReturnLogYearsService.go(payload.returnId)
-    } else if (payload.workflowId) {
-      result = await DetermineWorkflowYearsService.go(payload.workflowId)
-    } else {
-      return
-    }
-
-    const { licence, startDate, endDate, twoPartTariff, flagForBilling } = result
-
-    if (!flagForBilling) {
-      return
-    }
-
-    const years = DetermineBillingYearsService.go(startDate, endDate)
-
-    if (!years) {
-      return
-    }
-
-    const financialYearEnds = await DetermineExistingBillRunYearsService.go(licence.regionId, years, twoPartTariff)
-
-    if (financialYearEnds.length === 0) {
-      return
-    }
-
-    await CreateLicenceSupplementaryYearService.go(licence.id, financialYearEnds, twoPartTariff)
-
-    calculateAndLogTimeTaken(startTime, 'Supplementary Billing Flag complete', { licenceId: licence.id })
+    calculateAndLogTimeTaken(startTime, 'Supplementary Billing Flag complete', { licenceId: result.licenceId })
   } catch (error) {
     global.GlobalNotifier.omfg('Supplementary Billing Flag failed', payload, error)
   }
+}
+
+async function _determineFlags (payload) {
+  if (payload.chargeVersionId) {
+    return await DetermineChargeVersionFlagsService.go(payload.chargeVersionId)
+  } else if (payload.returnId) {
+    return await DetermineReturnLogFlagsService.go(payload.returnId)
+  } else if (payload.workflowId) {
+    return await DetermineWorkflowFlagsService.go(payload.workflowId)
+  } else {
+    throw new Error('Invalid payload for process billing flags service')
+  }
+}
+
+async function _determineTwoPartTariffYears (twoPartTariffBillingYears, result) {
+  const { endDate, startDate, regionId, flagForTwoPartTariffSupplementary } = result
+  const years = DetermineBillingYearsService.go(startDate, endDate)
+
+  if (!years) {
+    return twoPartTariffBillingYears
+  }
+
+  return DetermineExistingBillRunYearsService.go(regionId, years, flagForTwoPartTariffSupplementary)
+}
+
+async function _setFlagForLicence (result) {
+  const {
+    licenceId,
+    flagForPreSrocSupplementary,
+    flagForSrocSupplementary,
+    flagForTwoPartTariffSupplementary
+  } = result
+
+  let twoPartTariffBillingYears = []
+
+  if (flagForTwoPartTariffSupplementary) {
+    twoPartTariffBillingYears = await _determineTwoPartTariffYears(twoPartTariffBillingYears, result)
+  }
+
+  await PersistSupplementaryBillingFlagsService.go(
+    twoPartTariffBillingYears,
+    flagForPreSrocSupplementary,
+    flagForSrocSupplementary,
+    licenceId
+  )
 }
 
 module.exports = {
