eca615dec5339171cc5f0180fb2d517e4a4a238a
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-11-24 08:24:52 +0000

    Prettified!

--- app/controllers/returns.controller.js
+++ app/controllers/returns.controller.js
@@ -9,7 +9,7 @@ const Boom = require('@hapi/boom')
 
 const ReturnLogService = require('../services/returns/return-log.service.js')
 
-async function returnLog (request, h) {
+async function returnLog(request, h) {
   // TODO: Consider whether we want to read the id as a query param or if we want to pass it in the route itself. This
   // would require URL-encoding and -decoding it since ids can contain slashes.
   const { id } = request.query
