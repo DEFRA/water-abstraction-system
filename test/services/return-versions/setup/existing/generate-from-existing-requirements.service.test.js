63d9a53bd05400eb281eba07beeb3896afb691be
jonathangoulding <jonathangoulding21@gmail.com>
2024-11-26 09:57:56 +0000

    test: remove duplicate test

--- test/services/return-versions/setup/existing/generate-from-existing-requirements.service.test.js
+++ test/services/return-versions/setup/existing/generate-from-existing-requirements.service.test.js
@@ -114,68 +114,6 @@ describe('Return Versions Setup - Generate From Existing Requirements service', 
       })
     })
 
-    describe('and all its return requirements have a site description', () => {
-      beforeEach(() => {
-        fetchResult = _fetchResult(returnVersionId)
-
-        Sinon.stub(FetchExistingRequirementsService, 'go').resolves(fetchResult)
-      })
-
-      it('returns the details of its return requirements transformed for use in the journey', async () => {
-        const result = await GenerateFromExistingRequirementsService.go(returnVersionId)
-
-        expect(result.requirements).to.equal([
-          {
-            points: [fetchResult.returnRequirements[0].points[0].id],
-            purposes: [
-              {
-                alias: fetchResult.returnRequirements[0].returnRequirementPurposes[0].alias,
-                description: 'Spray Irrigation - Storage',
-                id: fetchResult.returnRequirements[0].returnRequirementPurposes[0].purposeId
-              }
-            ],
-            returnsCycle: 'winter-and-all-year',
-            siteDescription: 'FIRST BOREHOLE AT AVALON',
-            abstractionPeriod: {
-              'end-abstraction-period-day': 31,
-              'end-abstraction-period-month': 3,
-              'start-abstraction-period-day': 1,
-              'start-abstraction-period-month': 4
-            },
-            frequencyReported: 'week',
-            frequencyCollected: 'week',
-            agreementsExceptions: ['none']
-          },
-          {
-            points: [fetchResult.returnRequirements[1].points[0].id],
-            purposes: [
-              {
-                alias: '',
-                description: 'Spray Irrigation - Storage',
-                id: fetchResult.returnRequirements[1].returnRequirementPurposes[0].purposeId
-              }
-            ],
-            returnsCycle: 'summer',
-            siteDescription: 'SECOND BOREHOLE AT AVALON',
-            abstractionPeriod: {
-              'end-abstraction-period-day': 31,
-              'end-abstraction-period-month': 3,
-              'start-abstraction-period-day': 1,
-              'start-abstraction-period-month': 4
-            },
-            frequencyReported: 'month',
-            frequencyCollected: 'week',
-            agreementsExceptions: [
-              '56-returns-exception',
-              'gravity-fill',
-              'transfer-re-abstraction-scheme',
-              'two-part-tariff'
-            ]
-          }
-        ])
-      })
-    })
-
     describe('but one of its return requirements does not have a site description', () => {
       beforeEach(() => {
         fetchResult = _fetchResult()
