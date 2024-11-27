6fff736660ec50d78b0fa0959e5cd67492a3154e
Rebecca Ransome <beckyrose200@aol.com>
2024-11-27 10:21:23 +0000

    Fix: Merge conflicts

--- app/services/licences/supplementary/process-billing-flag.service.js
+++ app/services/licences/supplementary/process-billing-flag.service.js
@@ -5,10 +5,11 @@
  * @module ProcessBillingFlagService
  */
 
-const DetermineBillingYearsService = require('./determine-billing-years.service.js')
 const DetermineExistingBillRunYearsService = require('./determine-existing-bill-run-years.service.js')
+const DetermineBillingYearsService = require('./determine-billing-years.service.js')
 const DetermineChargeVersionFlagsService = require('./determine-charge-version-flags.service.js')
 const DetermineImportedLicenceFlagsService = require('./determine-imported-licence-flags.service.js')
+const DetermineLicenceFlagsService = require('./determine-licence-flags.service.js')
 const DetermineReturnLogFlagsService = require('./determine-return-log-flags.service.js')
 const DetermineWorkflowFlagsService = require('./determine-workflow-flags.service.js')
 const { calculateAndLogTimeTaken, currentTimeInNanoseconds } = require('../../../lib/general.lib.js')
@@ -56,6 +57,8 @@ async function _determineFlags(payload) {
     return DetermineReturnLogFlagsService.go(payload.returnId)
   } else if (payload.workflowId) {
     return DetermineWorkflowFlagsService.go(payload.workflowId)
+  } else if (payload.licenceId) {
+    return DetermineLicenceFlagsService.go(payload.licenceId, payload.scheme)
   } else {
     throw new Error('Invalid payload for process billing flags service')
   }
