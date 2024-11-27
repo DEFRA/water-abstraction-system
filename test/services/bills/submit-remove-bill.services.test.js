1bf37eb17c64e9bc2754250337c4fe21b7004cfd
Rebecca Ransome <beckyrose200@aol.com>
2024-11-27 20:23:19 +0000

    Update: Submit remove bill service and test

--- test/services/bills/submit-remove-bill.services.test.js
+++ test/services/bills/submit-remove-bill.services.test.js
@@ -10,23 +10,30 @@ const { expect } = Code
 
 // Test helpers
 const BillHelper = require('../../support/helpers/bill.helper.js')
+const BillLicenceHelper = require('../../support/helpers/bill-licence.helper.js')
 
 // Things we need to stub
 const LegacyDeleteBillRequest = require('../../../app/requests/legacy/delete-bill.request.js')
+const ProcessBillingFlagService = require('../../../app/services/licences/supplementary/process-billing-flag.service.js')
 
 // Thing under test
 const SubmitRemoveBillService = require('../../../app/services/bills/submit-remove-bill.service.js')
 
-describe('Submit Remove Bill service', () => {
+describe.only('Submit Remove Bill service', () => {
   const user = { id: '0aa9dcaa-9a26-4a77-97ab-c17db54d38a1', useremail: 'carol.shaw@atari.com' }
 
   let bill
   let legacyDeleteBillRequestStub
+  let processBillingFlagsStub
 
   beforeEach(async () => {
     bill = await BillHelper.add()
+    // Add two licences to the bill
+    await BillLicenceHelper.add({ billId: bill.id })
+    await BillLicenceHelper.add({ billId: bill.id })
 
     legacyDeleteBillRequestStub = Sinon.stub(LegacyDeleteBillRequest, 'send').resolves()
+    processBillingFlagsStub = Sinon.stub(ProcessBillingFlagService, 'go').resolves()
   })
 
   afterEach(() => {
@@ -34,6 +41,12 @@ describe('Submit Remove Bill service', () => {
   })
 
   describe('when called', () => {
+    it('flags the two licences in the bill for supplementary billing', async () => {
+      await SubmitRemoveBillService.go(bill.id, user)
+
+      expect(processBillingFlagsStub.calledTwice).to.be.true()
+    })
+
     it('sends a request to the legacy service to delete the bill', async () => {
       await SubmitRemoveBillService.go(bill.id, user)
 
