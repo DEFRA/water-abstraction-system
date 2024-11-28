7d56d32e06a84e0aef0794d80c59a1c443485854
jonathangoulding <jonathangoulding21@gmail.com>
2024-11-28 12:27:24 +0000

    Notifications returns period options
    https://eaflood.atlassian.net/browse/WATER-4716
    
    As part of the ongoing work to migrate the legacy UI we are replacing the notification journey from the UI and rebuilding in system.
    
    This change adds the notifications returns periods option for between january.
    
    The other options will be added in following commits.
    
    The submit logic will be developed in a separate commit so the 'values' used to capture the selected options may change.

--- app/presenters/notifications/setup/returns-period.presenter.js
+++ app/presenters/notifications/setup/returns-period.presenter.js
@@ -18,7 +18,42 @@ function go() {
 }
 
 function _returnsPeriod() {
+  const today = new Date()
+  const currentYear = today.getFullYear()
+  const previousYear = currentYear - 1
+
+  if (_dayIsInJanuary(today)) {
+    return _dayIsInJanuaryOptions(currentYear, previousYear)
+  }
+
   return []
+}
+
+/*
+ *  Checks if a date is in January
+ *  A date is in January if it is between 1st January - 28th January
+ */
+function _dayIsInJanuary(date) {
+  return date.getMonth() === 0 && date.getDate() >= 1 && date.getDate() <= 28
+}
+
+function _dayIsInJanuaryOptions(currentYear, previousYear) {
+  return [
+    {
+      value: 'quarterlyJanToMarch',
+      text: `Quarterly 1st October ${previousYear} to 31st December ${previousYear}`,
+      hint: {
+        text: `Due date 28 Jan ${currentYear}`
+      }
+    },
+    {
+      value: 'quarterlyAprilToMarch',
+      text: `Quarterly 1st January ${currentYear} to 31st March ${currentYear}`,
+      hint: {
+        text: `Due date 28 April ${currentYear}`
+      }
+    }
+  ]
 }
 
 module.exports = {
