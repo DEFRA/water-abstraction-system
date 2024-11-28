1ac55b45078172c01d660c84c4e3441d85ae5d04
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2024-11-28 13:54:05 +0000

    Add determine bill flags service (#1523)
    https://eaflood.atlassian.net/browse/WATER-4746
    
    This PR contributes to cleaning up the supplementary billing flagging process, specifically focusing on handling bills removed from a bill run.
    
    When a bill run is created, users can remove bills from it. Depending on the type of bill run, the following actions are required:
    
    Annual Bill Run: The licences in the bill must be flagged for inclusion in the next supplementary bill run.
    Supplementary Bill Run: Since the licences in the bill are already flagged (which is why they were included in this bill run), the existing flag must be maintained.
    
    Previously, bill flagging logic was handled entirely in legacy code. While the endpoint in the service repository still processes other aspects of licence removal, the flagging logic has been moved to our new ProcessBillingFlagsService. This service ensures flags are correctly determined before the legacy endpoint is called.

--- app/services/bills/submit-remove-bill.service.js
+++ app/services/bills/submit-remove-bill.service.js
@@ -6,10 +6,17 @@
  */
 
 const BillModel = require('../../models/bill.model.js')
+const BillLicenceModel = require('../../models/bill-licence.model.js')
+
 const LegacyDeleteBillRequest = require('../../requests/legacy/delete-bill.request.js')
+const ProcessBillingFlagService = require('../licences/supplementary/process-billing-flag.service.js')
 
 /**
  * Orchestrates the removing of a bill from a bill run
+ *
+ * This involves calling the `ProcessBillingFlagService` for all the licences associated with the bill for the correct
+ * supplementary billing flags to be added to them and then to handle deleting the bill itself calling the legacy
+ * service
  *
  * @param {string} billId - UUID of the bill to be removed
  * @param {object} user - Instance of `UserModel` that represents the user making the request
@@ -19,6 +26,7 @@ const LegacyDeleteBillRequest = require('../../requests/legacy/delete-bill.reque
 async function go(billId, user) {
   const { billRunId } = await _fetchBill(billId)
 
+  await _flagRemovedBill(billId)
   await LegacyDeleteBillRequest.send(billRunId, billId, user)
 
   return `/billing/batch/${billRunId}/processing`
@@ -26,6 +34,22 @@ async function go(billId, user) {
 
 async function _fetchBill(billId) {
   return BillModel.query().findById(billId).select(['id', 'billRunId'])
+}
+
+async function _fetchBillLicences(billId) {
+  return BillLicenceModel.query().where('billId', billId).select('id')
+}
+
+async function _flagRemovedBill(billId) {
+  const billLicences = await _fetchBillLicences(billId)
+
+  for (const billLicence of billLicences) {
+    const payload = {
+      billLicenceId: billLicence.id
+    }
+
+    await ProcessBillingFlagService.go(payload)
+  }
 }
 
 module.exports = {
