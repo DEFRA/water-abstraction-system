600cf9199432bedd2c9f3dc3c95c351d4bfb3e72
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-12-03 09:46:11 +0000

    Remove FetchReturnCyclesService
    Having demonstrated we can get the same results with a simple query I feel we no longer need the service.

--- app/services/return-logs/process-licence-return-logs.service.js
+++ app/services/return-logs/process-licence-return-logs.service.js
@@ -1,19 +1,19 @@
 'use strict'
 
 /**
- * Process voiding and issuing return logs for a given licence reference
+ * Process voiding and reissuing return logs for a given licence reference
  * @module ProcessLicenceReturnLogsService
  */
 
 const { calculateAndLogTimeTaken, currentTimeInNanoseconds } = require('../../lib/general.lib.js')
 const CreateReturnLogsService = require('./create-return-logs.service.js')
-const FetchReturnCyclesService = require('./fetch-return-cycles.service.js')
 const FetchReturnRequirementsService = require('./fetch-return-requirements.service.js')
-const GenerateReturnLogsService = require('./generate-return-logs.service.js')
+const GenerateReturnLogService = require('./generate-return-log.service.js')
+const ReturnCycleModel = require('../../models/return-cycle.model.js')
 const VoidReturnLogsService = require('./void-return-logs.service.js')
 
 /**
- * Process voiding and issuing return logs for a given licence reference
+ * Process voiding and reissuing return logs for a given licence reference
  *
  * @param {string} licenceReference - The licence to create return logs for
  * @param {Date} [endDate] - An optional end date to use when determining which return logs to void and reissue
@@ -21,22 +21,21 @@ const VoidReturnLogsService = require('./void-return-logs.service.js')
 async function go(licenceReference, endDate = null) {
   try {
     const startTime = currentTimeInNanoseconds()
-    let returnCycles = []
 
     if (endDate) {
       await VoidReturnLogsService.go(licenceReference, endDate)
-      returnCycles = await FetchReturnCyclesService.go(endDate)
     } else {
-      returnCycles = await FetchReturnCyclesService.go(new Date())
+      endDate = new Date()
     }
 
+    const returnCycles = await _fetchReturnCycles(endDate)
     const returnLogs = []
 
     for (const returnCycle of returnCycles) {
       const returnRequirements = await FetchReturnRequirementsService.go(returnCycle, licenceReference)
 
       for (const returnRequirement of returnRequirements) {
-        const returnLog = await GenerateReturnLogsService.go(returnRequirement, returnCycle)
+        const returnLog = GenerateReturnLogService.go(returnRequirement, returnCycle)
 
         returnLogs.push(returnLog)
       }
@@ -48,6 +47,13 @@ async function go(licenceReference, endDate = null) {
   } catch (error) {
     global.GlobalNotifier.omfg('Create licence return logs job failed', { error })
   }
+}
+
+async function _fetchReturnCycles(endDate) {
+  return ReturnCycleModel.query()
+    .select(['dueDate', 'endDate', 'id', 'startDate', 'summer'])
+    .where('endDate', '>=', endDate)
+    .orderBy('endDate', 'desc')
 }
 
 module.exports = {
