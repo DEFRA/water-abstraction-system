ec96b702529bff04efca75c8a86344f04ac7b344
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-12-10 18:07:49 +0000

    Update create service to use trigger property
    The work of deciding what bill run engine to trigger has been done for the service, so we can simplify it and its tests.

--- app/services/bill-runs/setup/create.service.js
+++ app/services/bill-runs/setup/create.service.js
@@ -7,6 +7,7 @@
 
 const LegacyCreateBillRunRequest = require('../../../requests/legacy/create-bill-run.request.js')
 const StartBillRunProcessService = require('../start-bill-run-process.service.js')
+const { engineTriggers } = require('../../../lib/static-lookups.lib.js')
 
 /**
  * Used to create the new bill run at the end of the setup bill run journey
@@ -23,54 +24,18 @@ const StartBillRunProcessService = require('../start-bill-run-process.service.js
  * @param {module:UserModel} user - Instance of `UserModel` that represents the user making the request
  */
 async function go(session, existsResults, user) {
-  const { matches, toFinancialYearEnding } = existsResults
   const { region: regionId, type, summer } = session
+  const { toFinancialYearEnding, trigger } = existsResults
 
-  const existingBillRun = matches[0]
+  if (trigger === engineTriggers.current || trigger === engineTriggers.both) {
+    await StartBillRunProcessService.go(regionId, type, user.username, toFinancialYearEnding)
+  }
 
-  await _triggerBillRun(regionId, type, user, toFinancialYearEnding, existingBillRun)
-  await _triggerLegacyBillRun(regionId, type, user, toFinancialYearEnding, summer, existingBillRun)
+  if (trigger === engineTriggers.old || trigger === engineTriggers.both) {
+    await LegacyCreateBillRunRequest.send(type, regionId, toFinancialYearEnding, user, summer)
+  }
 
   await session.$query().delete()
-}
-
-async function _triggerBillRun(regionId, batchType, user, toFinancialYearEnding, existingBillRun = null) {
-  const { username: userEmail } = user
-
-  // The one case we have to handle is where the user selected supplementary and a match was found, but it was to an
-  // SROC supplementary bill run. We've got to this point in the process because it is fine to trigger the PRESROC
-  // bill run in the legacy engine. But we can't trigger the SROC one because a match already exists.
-  if (batchType === 'supplementary' && existingBillRun?.scheme === 'sroc') {
-    return null
-  }
-
-  // We do not bother to send requests for PRESROC 2PT bill runs to our engine
-  if (batchType === 'two_part_tariff' && [2021, 2022].includes(toFinancialYearEnding)) {
-    return null
-  }
-
-  return StartBillRunProcessService.go(regionId, batchType, userEmail, toFinancialYearEnding)
-}
-
-async function _triggerLegacyBillRun(regionId, batchType, user, toFinancialYearEnding, summer, existingBillRun = null) {
-  // The legacy service no longer handles annual billing
-  if (batchType === 'annual') {
-    return null
-  }
-
-  // The one case we have to handle is where the user selected supplementary and a match was found, but it was to a
-  // PRESROC supplementary bill run. We've got to this point in the process because it is fine to trigger the SROC
-  // bill run in our engine. But we can't trigger the PRESROC one because a match already exists.
-  if (batchType === 'supplementary' && existingBillRun?.scheme === 'alcs') {
-    return null
-  }
-
-  // We do not bother to send requests for SROC 2PT bill runs to the legacy service
-  if (batchType === 'two_part_tariff' && [2024, 2023].includes(toFinancialYearEnding)) {
-    return null
-  }
-
-  return LegacyCreateBillRunRequest.send(batchType, regionId, toFinancialYearEnding, user, summer)
 }
 
 module.exports = {
