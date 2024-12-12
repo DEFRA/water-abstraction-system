b0642d8f4e909d5db6ef50afea7b26779e95d407
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-12-12 10:45:27 +0000

    First refactor - Move annual checks to new service
    This is the first of our refactorings. Rather than have services that try to do one thing for all bill run types, we are moving to having a service dedicated to checking all the things for a single bill run type. That way we can see all the conditions for a bill run type in one place.
    
    This is the first service which covers annual, the simplest of the bill run types to checking for any blocks.

---
+++ app/services/bill-runs/setup/determine-blocking-annual.service.js
@@ -0,0 +1,63 @@
+'use strict'
+
+/**
+ * Determines if an existing bill run will block a user from creating a new annual bill run
+ * @module DetermineBlockingAnnualService
+ */
+
+const BillRunModel = require('../../../models/bill-run.model.js')
+const FetchLiveBillRunService = require('./fetch-live-bill-run.service.js')
+const { engineTriggers } = require('../../../lib/static-lookups.lib.js')
+
+/**
+ * Determines if an existing bill run will block a user from creating a new annual bill run
+ *
+ * You can only create one annual bill run per region, per financial year. So, our first check is to see if one already
+ * exists.
+ *
+ * If there is no match, we return the results of a check for any bill runs with a status of 'queued', 'processing',
+ * 'ready', or 'review' (a bill run is 'live').
+ *
+ * Where a region already has a 'live' bill run we do not permit users to create another one until it is completed.
+ *
+ * > We return an array because the same checks for supplementary might return multiple matches. So, we keep the results
+ * > consistent for the orchestrating service
+ *
+ * @param {string} regionId - UUID of the region a bill run is being created for
+ * @param {number} toFinancialYearEnding - The end year of the financial period the bill run will be for
+ *
+ * @returns {Promise<module:BillRunModel[]>} An array containing the matched bill run if found, otherwise the result of
+ * calling `FetchLiveBillRunsService`
+ */
+async function go(regionId, toFinancialYearEnding) {
+  let match = await _fetchMatches(regionId, toFinancialYearEnding)
+
+  if (!match) {
+    match = await FetchLiveBillRunService.go(regionId, toFinancialYearEnding)
+  }
+
+  const matches = match ? [match] : []
+  const trigger = match ? engineTriggers.neither : engineTriggers.current
+
+  return { matches, toFinancialYearEnding, trigger }
+}
+
+async function _fetchMatches(regionId, toFinancialYearEnding) {
+  return BillRunModel.query()
+    .select(['id', 'batchType', 'billRunNumber', 'createdAt', 'scheme', 'status', 'summer', 'toFinancialYearEnding'])
+    .where('regionId', regionId)
+    .where('batchType', 'annual')
+    .where('toFinancialYearEnding', toFinancialYearEnding)
+    .whereNotIn('status', ['cancel', 'empty', 'error'])
+    .orderBy([{ column: 'createdAt', order: 'desc' }])
+    .withGraphFetched('region')
+    .modifyGraph('region', (builder) => {
+      builder.select(['id', 'displayName'])
+    })
+    .limit(1)
+    .first()
+}
+
+module.exports = {
+  go
+}
---
+++ test/services/bill-runs/setup/determine-blocking-annual.service.test.js
@@ -0,0 +1,110 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const { engineTriggers } = require('../../../../app/lib/static-lookups.lib.js')
+
+// Things we need to stub
+const BillRunModel = require('../../../../app/models/bill-run.model.js')
+const FetchLiveBillRunService = require('../../../../app/services/bill-runs/setup/fetch-live-bill-run.service.js')
+
+// Thing under test
+const DetermineBlockingAnnualService = require('../../../../app/services/bill-runs/setup/determine-blocking-annual.service.js')
+
+describe('Bill Runs Setup Determine Blocking Annual Bill Run service', () => {
+  const regionId = '292fe1c3-c9d4-47dd-a01b-0ac916497af5'
+  const toFinancialYearEnding = 2025
+
+  let billRunQueryStub
+  let fetchLiveBillRunStub
+  let match
+
+  beforeEach(() => {
+    match = {
+      id: 'aadb1af8-16d5-46c3-9b80-00a6201b8196',
+      batchType: 'annual',
+      billRunNumber: 1045,
+      createdAt: new Date('2024-04-11'),
+      scheme: 'sroc',
+      status: 'sent',
+      summer: false,
+      toFinancialYearEnding
+    }
+
+    billRunQueryStub = {
+      select: Sinon.stub().returnsThis(),
+      where: Sinon.stub().returnsThis(),
+      whereNotIn: Sinon.stub().returnsThis(),
+      orderBy: Sinon.stub().returnsThis(),
+      withGraphFetched: Sinon.stub().returnsThis(),
+      modifyGraph: Sinon.stub().returnsThis(),
+      limit: Sinon.stub().returnsThis()
+    }
+
+    fetchLiveBillRunStub = Sinon.stub(FetchLiveBillRunService, 'go')
+  })
+
+  afterEach(async () => {
+    Sinon.restore()
+  })
+
+  describe('when there is a matching bill run', () => {
+    beforeEach(() => {
+      billRunQueryStub.first = Sinon.stub().resolves(match)
+
+      Sinon.stub(BillRunModel, 'query').returns(billRunQueryStub)
+    })
+
+    it('returns the match and determines that neither engine can be triggered', async () => {
+      const result = await DetermineBlockingAnnualService.go(regionId, toFinancialYearEnding)
+
+      expect(result).to.equal({ matches: [match], toFinancialYearEnding, trigger: engineTriggers.neither })
+    })
+
+    it('does not bother to check for live bill runs', async () => {
+      await DetermineBlockingAnnualService.go(regionId, toFinancialYearEnding)
+
+      expect(fetchLiveBillRunStub.called).to.be.false()
+    })
+  })
+
+  describe('when there is no matching bill run', () => {
+    beforeEach(() => {
+      billRunQueryStub.first = Sinon.stub().resolves(null)
+    })
+
+    describe('and no live bill run', () => {
+      beforeEach(() => {
+        fetchLiveBillRunStub.resolves(null)
+      })
+
+      it('returns no matches and determines that the "current" engine can be triggered', async () => {
+        const result = await DetermineBlockingAnnualService.go(regionId, toFinancialYearEnding)
+
+        expect(result).to.equal({ matches: [], toFinancialYearEnding, trigger: engineTriggers.current })
+      })
+    })
+
+    describe('but there is a live bill run', () => {
+      beforeEach(() => {
+        match.batchType = 'supplementary'
+        match.status = 'ready'
+
+        fetchLiveBillRunStub.resolves(match)
+      })
+
+      it('returns the match and determines that neither engine can be triggered', async () => {
+        const result = await DetermineBlockingAnnualService.go(regionId, toFinancialYearEnding)
+
+        expect(result).to.equal({ matches: [match], toFinancialYearEnding, trigger: engineTriggers.neither })
+      })
+    })
+  })
+})
