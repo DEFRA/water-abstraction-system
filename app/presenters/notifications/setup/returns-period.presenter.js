bb7808216b3c917e7eb15b5052c9c48f1502cd8f
jonathangoulding <jonathangoulding21@gmail.com>
2024-12-12 16:34:08 +0000

    Refactor returns period presenter to use determine upcoming periods
    https://eaflood.atlassian.net/browse/WATER-4716
    
    As part of the ongoing work to migrate the legacy UI we are replacing the notification journey from the UI and rebuilding in system.
    
    This change introduces the logic determine upcoming return periods based on the due date into the presenter.
    
    This logic was previous hard coded in the presenter but was moved out and completed in a previous piece of work - https://github.com/DEFRA/water-abstraction-system/pull/1559

--- app/presenters/notifications/setup/returns-period.presenter.js
+++ app/presenters/notifications/setup/returns-period.presenter.js
@@ -5,12 +5,8 @@
  * @module ReturnsPeriodPresenter
  */
 
-const { monthsAsIntegers } = require('../../../lib/static-lookups.lib')
-
-const currentPeriod = 'currentPeriod'
-const nextPeriod = 'nextPeriod'
-const twentyEighth = 28
-const twentyNinth = 29
+const { determineUpcomingReturnPeriods } = require('../../../lib/return-periods.lib')
+const { formatLongDate } = require('../../base.presenter')
 
 /**
  * Formats data for the `/notifications/setup/returns-period` page
@@ -26,109 +22,32 @@ function go() {
 
 function _returnsPeriod() {
   const today = new Date()
-  const currentYear = today.getFullYear()
-  const previousYear = currentYear - 1
-  const nextYear = currentYear + 1
 
-  if (_dayIsInJanuary(today)) {
-    return _dayInJanuaryOptions(currentYear, previousYear)
-  } else if (_dayIsBetweenNovemberAndDecember(today)) {
-    return _dayBetweenNovemberAndDecemberOptions(currentYear, nextYear)
-  } else if (_dayIsBetweenOctoberAndNovember(today)) {
-    return _dayBetweenOctoberAndNovemberOptions(previousYear, currentYear, nextYear)
-  } else {
-    return []
+  const [currentReturnPeriod, nextReturnPeriod] = determineUpcomingReturnPeriods(today)
+
+  const current = currentReturnPeriod.name !== 'summer' ? _quarter(currentReturnPeriod) : _summer(currentReturnPeriod)
+  const next = nextReturnPeriod.name !== 'summer' ? _quarter(nextReturnPeriod) : _summer(nextReturnPeriod)
+  return [current, next]
+}
+
+function _quarter(returnPeriod) {
+  return {
+    value: returnPeriod.name,
+    text: `Quarterly ${formatLongDate(returnPeriod.startDate)} to ${formatLongDate(returnPeriod.endDate)}`,
+    hint: {
+      text: `Due date ${formatLongDate(returnPeriod.dueDate)}`
+    }
   }
 }
 
-/*
- *  Checks if a date is in January (Before the 29th)
- *
- *  A date is in January if it is between 1st January - 28th January
- */
-function _dayIsInJanuary(date) {
-  return date.getMonth() === monthsAsIntegers.january && date.getDate() <= twentyEighth
-}
-
-function _dayInJanuaryOptions(currentYear, previousYear) {
-  return [
-    {
-      value: currentPeriod,
-      text: `Quarterly 1st October ${previousYear} to 31st December ${previousYear}`,
-      hint: {
-        text: `Due date 28 Jan ${currentYear}`
-      }
-    },
-    {
-      value: nextPeriod,
-      text: `Quarterly 1st January ${currentYear} to 31st March ${currentYear}`,
-      hint: {
-        text: `Due date 28 April ${currentYear}`
-      }
+function _summer(returnPeriod) {
+  return {
+    value: returnPeriod.name,
+    text: `Summer annual ${formatLongDate(returnPeriod.startDate)} to ${formatLongDate(returnPeriod.endDate)}`,
+    hint: {
+      text: `Due date ${formatLongDate(returnPeriod.dueDate)}`
     }
-  ]
-}
-
-/*
- *  When the date is between 29th November - 31st December
- *
- * @returns {boolean} - true if date is in range (29th November - 31st December)
- */
-function _dayIsBetweenNovemberAndDecember(date) {
-  return (
-    date.getMonth() === monthsAsIntegers.december ||
-    (date.getMonth() === monthsAsIntegers.november && date.getDate() === twentyNinth)
-  )
-}
-
-function _dayBetweenNovemberAndDecemberOptions(currentYear, nextYear) {
-  return [
-    {
-      value: currentPeriod,
-      text: `Quarterly 1st October ${currentYear} to 31st December ${currentYear}`,
-      hint: {
-        text: `Due date 28 Jan ${nextYear}`
-      }
-    },
-    {
-      value: nextPeriod,
-      text: `Quarterly 1st January ${nextYear} to 31st March ${nextYear}`,
-      hint: {
-        text: `Due date 28 April ${nextYear}`
-      }
-    }
-  ]
-}
-
-/*
- *  When the date is between 29th October - 28th November
- *
- * @returns {boolean} - true if date is in range (29th October - 28th November)
- */
-function _dayIsBetweenOctoberAndNovember(date) {
-  return (
-    (date.getMonth() === monthsAsIntegers.october && date.getDate() === twentyNinth) ||
-    (date.getMonth() === monthsAsIntegers.november && date.getDate() <= twentyEighth)
-  )
-}
-
-function _dayBetweenOctoberAndNovemberOptions(previousYear, currentYear, nextYear) {
-  return [
-    {
-      value: currentPeriod,
-      text: `Summer annual 1st November ${previousYear} to 31st October ${currentYear}`,
-      hint: {
-        text: `Due date 28 November ${currentYear}`
-      }
-    },
-    {
-      value: nextPeriod,
-      text: `Quarterly 1st October ${currentYear} to 31st December ${currentYear}`,
-      hint: {
-        text: `Due date 28 January ${nextYear}`
-      }
-    }
-  ]
+  }
 }
 
 module.exports = {
