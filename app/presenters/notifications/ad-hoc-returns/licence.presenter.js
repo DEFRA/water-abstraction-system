55abba5b383702882ee73011fce15bb1737443b0
Rebecca Ransome <beckyrose200@aol.com>
2024-12-13 14:12:56 +0000

    Add: Licence presenter

---
+++ app/presenters/notifications/ad-hoc-returns/licence.presenter.js
@@ -0,0 +1,24 @@
+'use strict'
+
+/**
+ * Formats data for the `/notifications/ad-hoc-returns/{sessionId}/licence` page
+ * @module LicencePresenter
+ */
+
+/**
+ * Formats data for the `/notifications/ad-hoc-returns/{sessionId}/licence` page
+ *
+ * @param {module:SessionModel} session - The session instance to format
+ *
+ * @returns {object} - The data formatted for the view template
+ */
+function go(session) {
+  return {
+    sessionId: session.id,
+    licenceRef: session.licenceRef ? session.licenceRef : null
+  }
+}
+
+module.exports = {
+  go
+}
