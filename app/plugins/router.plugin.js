364fb1d1847a5f8238c68557f57106fc2292067c
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2024-12-16 15:40:03 +0000

    Add ad-hoc returns enter licence page (#1555)
    https://eaflood.atlassian.net/browse/WATER-4816
    
    As part of the work for sending ad-hoc returns, this PR focuses on adding the enter a licence page to the journey.

--- app/plugins/router.plugin.js
+++ app/plugins/router.plugin.js
@@ -23,6 +23,7 @@ const DataRoutes = require('../routes/data.routes.js')
 const FilterRoutesService = require('../services/plugins/filter-routes.service.js')
 const HealthRoutes = require('../routes/health.routes.js')
 const ImportRoutes = require('../routes/import.routes.js')
+const NotificationsAdHocReturnsRoutes = require('../routes/notifications-ad-hoc-returns.route.js')
 const NotificationsSetupRoutes = require('../routes/notifications-setup.routes.js')
 const JobRoutes = require('../routes/jobs.routes.js')
 const LicenceRoutes = require('../routes/licence.routes.js')
@@ -51,7 +52,8 @@ const routes = [
   ...ReturnVersionsSetupRoutes,
   ...DataRoutes,
   ...ImportRoutes,
-  ...NotificationsSetupRoutes
+  ...NotificationsSetupRoutes,
+  ...NotificationsAdHocReturnsRoutes
 ]
 
 const RouterPlugin = {
