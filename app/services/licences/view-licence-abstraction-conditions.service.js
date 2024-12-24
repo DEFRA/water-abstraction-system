09896e09d8d9ff5ddc10600b1f21eb5b8e010380
rvsiyad <roblesiyad123@gmail.com>
2024-12-24 12:59:11 +0000

    Add console logs

--- app/services/licences/view-licence-abstraction-conditions.service.js
+++ app/services/licences/view-licence-abstraction-conditions.service.js
@@ -2,8 +2,9 @@
 
 const FetchLicenceAbstractionConditionsService = require('../../services/licences/fetch-licence-abstraction-conditions.service.js')
 const ViewLicenceAbstractionConditionsPresenter = require('../../presenters/licences/view-licence-abstraction-conditions.presenter.js')
+const FetchTestService = require('./fetch-test.service.js')
 
-// const util = require('util')
+const util = require('util')
 
 /**
  * Calls the licence abstraction conditions presenter
@@ -13,6 +14,9 @@ const ViewLicenceAbstractionConditionsPresenter = require('../../presenters/lice
  */
 async function go(licenceId) {
   const licence = await FetchLicenceAbstractionConditionsService.go(licenceId)
+  const conditionsTest = await FetchTestService.go(licenceId)
+  // console.log('🚀🚀🚀 ~ conditionsTest:', conditionsTest)
+  console.log('🚀🚀🚀 ~ conditionsTest: ', util.inspect(conditionsTest, false, null, true /* enable colors */))
 
   const pageData = ViewLicenceAbstractionConditionsPresenter.go(licence)
   // console.log('🚀🚀🚀 ~ Conditions: ', util.inspect(pageData, false, null, true /* enable colors */))
