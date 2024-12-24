13186a03b56b63b37ee1e4332d0fd1f82ec9e522
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2024-12-24 14:35:53 +0000

    Delete the endpoint trigger we built

--- app/plugins/router.plugin.js
+++ app/plugins/router.plugin.js
@@ -22,7 +22,6 @@ const CheckRoutes = require('../routes/check.routes.js')
 const DataRoutes = require('../routes/data.routes.js')
 const FilterRoutesService = require('../services/plugins/filter-routes.service.js')
 const HealthRoutes = require('../routes/health.routes.js')
-const ImportRoutes = require('../routes/import.routes.js')
 const NotificationsAdHocReturnsRoutes = require('../routes/notifications-ad-hoc-returns.route.js')
 const NotificationsSetupRoutes = require('../routes/notifications-setup.routes.js')
 const JobRoutes = require('../routes/jobs.routes.js')
@@ -51,7 +50,6 @@ const routes = [
   ...ReturnVersionsRoutes,
   ...ReturnVersionsSetupRoutes,
   ...DataRoutes,
-  ...ImportRoutes,
   ...NotificationsSetupRoutes,
   ...NotificationsAdHocReturnsRoutes
 ]
