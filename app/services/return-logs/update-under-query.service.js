f470179e4dfb3155c157b83777af91a3284ee416
Rebecca Ransome <beckyrose200@aol.com>
2024-12-31 10:23:09 +0000

    WIP

---
+++ app/services/return-logs/update-under-query.service.js
@@ -0,0 +1,26 @@
+'use strict'
+
+/**
+ * Updates the under query flag on return logs
+ * @module UpdateUnderQueryService
+ */
+
+const ReturnLogModel = require('../../models/return-log.model.js')
+
+/**
+ * Updates the under query flag on a return log
+ *
+ * @param {string} returnLogId - The UUID of the return log to update
+ * @param {boolean} underQuery - The value to update the under query flag to
+ */
+async function go(returnLogId, underQuery) {
+  await _updateReturnLog(returnLogId, underQuery)
+}
+
+async function _updateReturnLog(returnLogId, underQuery) {
+  return ReturnLogModel.query().update({ underQuery }).findById(returnLogId)
+}
+
+module.exports = {
+  go
+}
