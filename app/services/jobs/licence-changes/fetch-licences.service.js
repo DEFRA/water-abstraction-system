2d362fa3faa2738851b1f43d8911ede31397afe5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-01 20:17:46 +0000

    Add wrls dates to fetch service plus tidy

--- app/services/jobs/licence-changes/fetch-licences.service.js
+++ app/services/jobs/licence-changes/fetch-licences.service.js
@@ -17,12 +17,15 @@ const { db } = require('../../../../db/db.js')
 async function go() {
   const query = `
     SELECT DISTINCT ON (nal."LIC_NO")
-      l.id as id,
-      TO_DATE(NULLIF(nal."EXPIRY_DATE", 'null'), 'DD/MM/YYYY') AS expired_date,
-      TO_DATE(NULLIF(nal."LAPSED_DATE", 'null'), 'DD/MM/YYYY') AS lapsed_date,
-      TO_DATE(NULLIF(nal."EXPIRY_DATE", 'null'), 'DD/MM/YYYY') AS revoked_date
-    FROM import."NALD_ABS_LIC_VERSIONS" nalv
-      INNER JOIN import."NALD_ABS_LICENCES" nal
+      l.id AS id,
+      TO_DATE(NULLIF(nal."EXPIRY_DATE", 'null'), 'DD/MM/YYYY') AS nald_expired_date,
+      TO_DATE(NULLIF(nal."LAPSED_DATE", 'null'), 'DD/MM/YYYY') AS nald_lapsed_date,
+      TO_DATE(NULLIF(nal."REV_DATE", 'null'), 'DD/MM/YYYY') AS nald_revoked_date,
+      l.expired_date AS wrls_expired_date,
+      l.lapsed_date AS wrls_lapsed_date,
+      l.revoked_date AS wrls_revoked_date
+    FROM "import"."NALD_ABS_LIC_VERSIONS" nalv
+      INNER JOIN "import"."NALD_ABS_LICENCES" nal
         ON nal."ID" = nalv."AABL_ID"
         AND nal."FGAC_REGION_CODE" = nalv."FGAC_REGION_CODE"
       INNER JOIN
