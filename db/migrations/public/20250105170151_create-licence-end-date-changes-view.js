c77067de0cd09af56b1a26c9123625caa6ac5ad6
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-05 17:13:13 +0000

    Add the public view for backing the model

---
+++ db/migrations/public/20250105170151_create-licence-end-date-changes-view.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const viewName = 'licence_end_date_changes'
+
+exports.up = function (knex) {
+  return knex.schema.dropViewIfExists(viewName).createView(viewName, (view) => {
+    view.as(
+      knex('licence_end_date_changes')
+        .withSchema('water')
+        .select(['id', 'licence_id', 'date_type', 'nald_date', 'wrls_date', 'created_at', 'updated_at'])
+    )
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropViewIfExists(viewName)
+}
