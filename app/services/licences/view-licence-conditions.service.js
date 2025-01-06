8274d2a711d389e517e160f78862b53a8e04aee1
rvsiyad <roblesiyad123@gmail.com>
2025-01-06 12:05:13 +0000

    Add JSDoc for view licence conditions service

--- app/services/licences/view-licence-conditions.service.js
+++ app/services/licences/view-licence-conditions.service.js
@@ -1,18 +1,24 @@
 'use strict'
 
+/**
+ * Orchestrates fetching and presenting the data needed for the licence conditions page
+ * @module ViewLicenceConditionsService
+ */
+
 const FetchLicenceConditionsService = require('./fetch-licence-conditions.service.js')
 const ViewLicenceConditionsPresenter = require('../../presenters/licences/view-licence-conditions.presenter.js')
 
 /**
- * Calls the licence abstraction conditions presenter
+ * Orchestrates fetching and presenting the data needed for the licence conditions page
  *
- * @param {string} licenceId
- * @returns
+ * @param {string} licenceId - The UUID of the licence
+ *
+ * @returns {Promise<object>} an object representing the `pageData` needed by the licence conditions template
  */
 async function go(licenceId) {
-  const licence = await FetchLicenceConditionsService.go(licenceId)
+  const licenceVersionPurposeConditionTypes = await FetchLicenceConditionsService.go(licenceId)
 
-  const pageData = ViewLicenceConditionsPresenter.go(licence)
+  const pageData = ViewLicenceConditionsPresenter.go(licenceVersionPurposeConditionTypes)
 
   return {
     activeNavBar: 'search',
