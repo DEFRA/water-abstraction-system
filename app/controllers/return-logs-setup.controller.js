1737fae7d9249b410732fc5a13ec11a57e8803f3
Rebecca Ransome <beckyrose200@aol.com>
2025-01-07 15:39:57 +0000

    Add: Received functions to controller

--- app/controllers/return-logs-setup.controller.js
+++ app/controllers/return-logs-setup.controller.js
@@ -6,6 +6,15 @@
  */
 
 const InitiateSessionService = require('../services/return-logs/setup/initiate-session.service.js')
+const ReceivedService = require('../services/return-logs/setup/received.service.js')
+const SubmitReceivedService = require('../services/return-logs/setup/submit-received.service.js')
+
+async function received(request, h) {
+  const { sessionId } = request.params
+  const pageData = await ReceivedService.go(sessionId)
+
+  return h.view('return-logs/setup/received.njk', { ...pageData })
+}
 
 async function setup(request, h) {
   const { returnLogId } = request.query
@@ -14,6 +23,23 @@ async function setup(request, h) {
   return h.redirect(`/system/return-logs/setup/${session.id}/how-to-edit`)
 }
 
+async function submitReceived(request, h) {
+  const {
+    params: { sessionId },
+    payload
+  } = request
+
+  const pageData = await SubmitReceivedService.go(sessionId, payload)
+
+  if (pageData.error) {
+    return h.view('return-logs/setup/received.njk', pageData)
+  }
+
+  return h.redirect(`/system/return-logs/setup/${sessionId}/reported`)
+}
+
 module.exports = {
-  setup
+  received,
+  setup,
+  submitReceived
 }
