a23a0584640c1f4c57a63777a30223dcfc4a84c6
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-08 16:14:24 +0000

    Sort out how the return log status is displayed

--- app/presenters/return-logs/setup/start.presenter.js
+++ app/presenters/return-logs/setup/start.presenter.js
@@ -16,6 +16,7 @@ const { formatAbstractionPeriod, formatLongDate } = require('../../base.presente
  */
 function go(session) {
   const {
+    dueDate,
     endDate,
     licenceId,
     licenceRef,
@@ -53,9 +54,31 @@ function go(session) {
     returnReference,
     selectedOption: whatToDo ?? whatToDo,
     siteDescription,
-    status,
+    status: _status(status, dueDate),
     tariffType: twoPartTariff ? 'Two part tariff' : 'Standard tariff'
   }
+}
+
+function _status(status, dueDate) {
+  // If the return is completed we are required to display it as 'complete'. This also takes priority over the other
+  // statues
+  if (status === 'completed') {
+    return 'complete'
+  }
+
+  // Work out if the return is overdue (status is still 'due' and it is past the due date)
+  const today = new Date()
+
+  // The due date held in the record is date-only. If we compared it against 'today' without this step any return due
+  // 'today' would be flagged as overdue when it is still due (just!)
+  today.setHours(0, 0, 0, 0)
+
+  if (status === 'due' && new Date(dueDate) < today) {
+    return 'overdue'
+  }
+
+  // For all other cases we can just return the status and the return-status-tag macro will know how to display it
+  return status
 }
 
 module.exports = {
