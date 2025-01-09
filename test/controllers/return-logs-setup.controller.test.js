6af1273e2c5c63ec09f0b24f435182a81237373d
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-09 16:00:23 +0000

    Create controller unit tests

--- test/controllers/return-logs-setup.controller.test.js
+++ test/controllers/return-logs-setup.controller.test.js
@@ -8,8 +8,13 @@ const Sinon = require('sinon')
 const { describe, it, before, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
+// Test helpers
+const { postRequestOptions } = require('../support/general.js')
+
 // Things we need to stub
 const InitiateSessionService = require('../../app/services/return-logs/setup/initiate-session.service.js')
+const StartService = require('../../app/services/return-logs/setup/start.service.js')
+const SubmitStartService = require('../../app/services/return-logs/setup/submit-start.service.js')
 
 // For running our service
 const { init } = require('../../app/server.js')
@@ -23,7 +28,7 @@ describe('Return Logs Setup controller', () => {
     server = await init()
   })
 
-  beforeEach(async () => {
+  beforeEach(() => {
     // We silence any calls to server.logger.error made in the plugin to try and keep the test output as clean as
     // possible
     Sinon.stub(server.logger, 'error')
@@ -40,7 +45,7 @@ describe('Return Logs Setup controller', () => {
     describe('GET', () => {
       const session = { id: 'e0c77b74-7326-493d-be5e-0d1ad41594b5', data: {} }
 
-      beforeEach(async () => {
+      beforeEach(() => {
         options = {
           method: 'GET',
           url: '/return-logs/setup?returnLogId=v1:1:123:10021668:2022-04-01:2023-03-31',
@@ -52,7 +57,7 @@ describe('Return Logs Setup controller', () => {
       })
 
       describe('when a request is valid', () => {
-        beforeEach(async () => {
+        beforeEach(() => {
           Sinon.stub(InitiateSessionService, 'go').resolves(session)
         })
 
@@ -65,4 +70,82 @@ describe('Return Logs Setup controller', () => {
       })
     })
   })
+
+  describe('/return-logs/setup/{sessionId}/start', () => {
+    describe('GET', () => {
+      beforeEach(() => {
+        options = {
+          method: 'GET',
+          url: '/return-logs/setup/cee5b760-5619-47cf-97df-271e96b091f1/start',
+          auth: {
+            strategy: 'session',
+            credentials: { scope: ['billing'] }
+          }
+        }
+      })
+
+      describe('when the request succeeds', () => {
+        beforeEach(() => {
+          Sinon.stub(StartService, 'go').resolves({ pageTitle: 'Abstraction return' })
+        })
+
+        it('returns the page successfully', async () => {
+          const response = await server.inject(options)
+
+          expect(response.statusCode).to.equal(200)
+          expect(response.payload).to.contain('Abstraction return')
+        })
+      })
+    })
+
+    describe('POST', () => {
+      describe('when the request succeeds', () => {
+        beforeEach(() => {
+          options = _postOptions('start', { whatToDo: 'selectedOption' })
+        })
+
+        describe('and an option is selected', () => {
+          beforeEach(() => {
+            Sinon.stub(SubmitStartService, 'go').resolves({})
+          })
+
+          it('redirects to the "received" page', async () => {
+            const response = await server.inject(options)
+
+            expect(response.statusCode).to.equal(302)
+            expect(response.headers.location).to.equal(
+              '/system/return-logs/setup/cee5b760-5619-47cf-97df-271e96b091f1/received'
+            )
+          })
+        })
+      })
+
+      describe('when the request succeeds', () => {
+        beforeEach(() => {
+          options = _postOptions('start', {})
+        })
+
+        describe('and the validation fails as no option has been selected', () => {
+          beforeEach(() => {
+            Sinon.stub(SubmitStartService, 'go').resolves({
+              pageTitle: 'Abstraction return',
+              error: { text: 'Select what you want to do with this return' }
+            })
+          })
+
+          it('returns the page successfully with the error summary banner', async () => {
+            const response = await server.inject(options)
+
+            expect(response.statusCode).to.equal(200)
+            expect(response.payload).to.contain('Select what you want to do with this return')
+            expect(response.payload).to.contain('Abstraction return')
+          })
+        })
+      })
+    })
+  })
 })
+
+function _postOptions(path, payload) {
+  return postRequestOptions(`/return-logs/setup/cee5b760-5619-47cf-97df-271e96b091f1/${path}`, payload)
+}
