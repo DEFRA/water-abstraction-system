a0543f8d5fcbe10232f20711313b41014ce7dcbf
jonathangoulding <goulding.jon@icloud.com>
2025-01-09 14:08:10 +0000

    Add notification review initial page
    https://eaflood.atlassian.net/browse/WATER-4775
    
    Add the initial page, controller and router for the notification review page.
    
    The presenter and service will be added in a later change as the service will have a complex database query and we will need to format the data for the view.

---
+++ test/services/notifications/setup/review.services.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, after, before } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Thing under test
+const ReviewService = require('../../../../app/services/notifications/setup/review.service.js')
+
+describe('Notifications Setup - Review service', () => {
+  let session
+
+  before(async () => {
+    session = await SessionHelper.add()
+  })
+
+  describe('when provided no params', () => {
+    it('correctly presents the data', async () => {
+      const result = await ReviewService.go(session.id)
+
+      expect(result).to.equal({
+        activeNavBar: 'manage',
+        pageTitle: 'Review the mailing list'
+      })
+    })
+  })
+})
