3c1daf20aa3ef448525b32e87f96bfbfe4c796a7
Rebecca Ransome <beckyrose200@aol.com>
2025-01-10 15:10:26 +0000

    Add: Reported presenter

---
+++ app/presenters/return-logs/setup/reported.presenter.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/**
+ * Format data for the `/return-log/setup/{sessionId}/reported` page
+ * @module ReportedPresenter
+ */
+
+/**
+ * Format data for the `/return-log/setup/{sessionId}/reported` page
+ *
+ * @param {module:SessionModel} session - The return log setup session instance
+ *
+ * @returns {object} page data needed by the view template
+ */
+function go(session) {
+  const {
+    id: sessionId,
+    data: { returnReference },
+    reported
+  } = session
+
+  return {
+    reported: reported ?? null,
+    sessionId,
+    returnReference,
+    backLink: `/system/return-logs/setup/${session.id}/received`
+  }
+}
+
+module.exports = {
+  go
+}
