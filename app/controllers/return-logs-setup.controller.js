781d07eba32abb5d94c77bd2342a525dadba2729
Rebecca Ransome <beckyrose200@aol.com>
2025-01-10 15:10:06 +0000

    Add: Controller

--- app/controllers/return-logs-setup.controller.js
+++ app/controllers/return-logs-setup.controller.js
@@ -6,6 +6,15 @@
  */
 
 const InitiateSessionService = require('../services/return-logs/setup/initiate-session.service.js')
+const ReportedService = require('../services/return-logs/setup/reported.service.js')
+const SubmitReportedService = require('../services/return-logs/setup/submit-reported.service.js')
+
+async function reported(request, h) {
+  const { sessionId } = request.params
+  const pageData = await ReportedService.go(sessionId)
+
+  return h.view('return-logs/setup/reported.njk', { ...pageData })
+}
 
 async function setup(request, h) {
   const { returnLogId } = request.query
@@ -14,6 +23,23 @@ async function setup(request, h) {
   return h.redirect(`/system/return-logs/setup/${session.id}/how-to-edit`)
 }
 
+async function submitReported(request, h) {
+  const {
+    params: { sessionId },
+    payload
+  } = request
+
+  const pageData = await SubmitReportedService.go(sessionId, payload)
+
+  if (pageData.error) {
+    return h.view('return-logs/setup/reported.njk', pageData)
+  }
+
+  return h.redirect(`/system/return-logs/setup/${sessionId}/units`)
+}
+
 module.exports = {
-  setup
+  reported,
+  setup,
+  submitReported
 }
