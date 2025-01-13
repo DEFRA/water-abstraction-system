f8d4cababe6e080edad6c41d131a3ebda9ea3cf1
Rebecca Ransome <beckyrose200@aol.com>
2025-01-13 13:52:20 +0000

    Add: Units Validator

---
+++ app/validators/return-logs/setup/units.validator.js
@@ -0,0 +1,40 @@
+'use strict'
+
+/**
+ * Validates data submitted for the `/return-logs/setup/{sessionId}/units` page
+ * @module UnitsValidator
+ */
+
+const Joi = require('joi')
+
+/**
+ * Validates data submitted for the `/return-logs/setup/{sessionId}/units` page
+ *
+ * @param {object} payload - The payload from the request to be validated
+ *
+ * @returns {object} the result from calling Joi's schema.validate(). It will be an object with a `value:` property. If
+ * any errors are found the `error:` property will also exist detailing what the issues were
+ */
+function go(payload) {
+  const units = payload.units
+
+  const VALID_VALUES = ['cubic-meters', 'litres', 'mega-litres', 'gallons']
+  const errorMessage = 'Select which units were used'
+
+  const schema = Joi.object({
+    units: Joi.string()
+      .required()
+      .valid(...VALID_VALUES)
+      .messages({
+        'any.required': errorMessage,
+        'any.only': errorMessage,
+        'string.empty': errorMessage
+      })
+  })
+
+  return schema.validate({ units }, { abortEarly: false })
+}
+
+module.exports = {
+  go
+}
