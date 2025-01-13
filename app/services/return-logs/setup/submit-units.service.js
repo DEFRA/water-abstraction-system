0c25b0093ff57804ea53898205cafad9df7c63d7
Rebecca Ransome <beckyrose200@aol.com>
2025-01-13 13:52:11 +0000

    Add: Submit units service

---
+++ app/services/return-logs/setup/submit-units.service.js
@@ -0,0 +1,69 @@
+'use strict'
+
+/**
+ * Orchestrates validating the data for `/return-logs/setup/{sessionId}/units` page
+ * @module SubmitUnitsService
+ */
+
+const SessionModel = require('../../../models/session.model.js')
+const UnitsPresenter = require('../../../presenters/return-logs/setup/units.presenter.js')
+const UnitsValidator = require('../../../validators/return-logs/setup/units.validator.js')
+
+/**
+ * Orchestrates validating the data for `/return-logs/setup/{sessionId}/units` page
+ *
+ * It first retrieves the session instance for the return log setup session in progress.
+ *
+ * The validation result is then combined with the output of the presenter to generate the page data needed by the view.
+ * If there was a validation error the controller will re-render the page so needs this information. If all is well the
+ * controller will redirect to the next page in the journey.
+ *
+ * @param {string} sessionId - The UUID of the current session
+ * @param {object} payload - The submitted form data
+ *
+ * @returns {Promise<object>} If no errors the page data for the units page else the validation error details
+ */
+async function go(sessionId, payload) {
+  const session = await SessionModel.query().findById(sessionId)
+
+  const validationResult = _validate(payload)
+
+  if (!validationResult) {
+    await _save(session, payload)
+
+    return {}
+  }
+
+  const formattedData = UnitsPresenter.go(session)
+
+  return {
+    pageTitle: 'Which units were used?',
+    activeNavBar: 'search',
+    error: validationResult,
+    ...formattedData
+  }
+}
+
+async function _save(session, payload) {
+  session.units = payload.units
+
+  return session.$update()
+}
+
+function _validate(payload) {
+  const validation = UnitsValidator.go(payload)
+
+  if (!validation.error) {
+    return null
+  }
+
+  const { message } = validation.error.details[0]
+
+  return {
+    text: message
+  }
+}
+
+module.exports = {
+  go
+}
