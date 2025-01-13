f3dcc64dbd4b5f16e5f6d4ebb2cdec3e89890308
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-13 14:29:56 +0000

    Create validator unit tests

---
+++ test/validators/return-logs/setup/start.validator.test.js
@@ -0,0 +1,44 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const StartValidator = require('../../../../app/validators/return-logs/setup/start.validator.js')
+
+describe('Return Logs Setup - Start validator', () => {
+  describe('when valid data is provided', () => {
+    it('confirms the data is valid', () => {
+      const result = StartValidator.go({ whatToDo: 'enterReturn' })
+
+      expect(result.value).to.exist()
+      expect(result.error).not.to.exist()
+    })
+  })
+
+  describe('when invalid data is provided', () => {
+    describe('because no "whatToDo" is given', () => {
+      it('fails validation', () => {
+        const result = StartValidator.go({ whatToDo: '' })
+
+        expect(result.value).to.exist()
+        expect(result.error).to.exist()
+        expect(result.error.details[0].message).to.equal('Select what you want to do with this return')
+      })
+    })
+
+    describe('because an unknown "type" is given', () => {
+      it('fails validation', () => {
+        const result = StartValidator.go({ whatToDo: 'invalid' })
+
+        expect(result.value).to.exist()
+        expect(result.error).to.exist()
+        expect(result.error.details[0].message).to.equal('Select what you want to do with this return')
+      })
+    })
+  })
+})
