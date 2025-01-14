e8db5d8a0d2bc77be60f3df499d9e4fb64e4a84f
Rebecca Ransome <beckyrose200@aol.com>
2025-01-14 09:47:30 +0000

    Add: Meter provided service

---
+++ app/services/return-logs/setup/meter-provided.service.js
@@ -0,0 +1,35 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data for `/return-logs/setup/{sessionId}/meter-provided` page
+ * @module MeterProvidedService
+ */
+
+const MeterProvidedPresenter = require('../../../presenters/return-logs/setup/meter-provided.presenter.js')
+const SessionModel = require('../../../models/session.model.js')
+
+/**
+ * Orchestrates fetching and presenting the data for `/return-logs/setup/{sessionId}/meter-provided` page
+ *
+ * Supports generating the data needed for the meter provided page in the return log setup journey. It fetches the
+ * current session record and formats the data needed for the page.
+ *
+ * @param {string} sessionId - The UUID of the current session
+ *
+ * @returns {Promise<object>} The view data for the meter provided page
+ */
+async function go(sessionId) {
+  const session = await SessionModel.query().findById(sessionId)
+
+  const formattedData = MeterProvidedPresenter.go(session)
+
+  return {
+    pageTitle: 'Have meter details been provided?',
+    activeNavBar: 'search',
+    ...formattedData
+  }
+}
+
+module.exports = {
+  go
+}
