67aa272d66e95d319407fb11ceb7b8b7a36b4b5a
Rebecca Ransome <beckyrose200@aol.com>
2025-01-14 09:47:15 +0000

    Update: Return log setup route to add meter provided route

--- app/routes/return-logs-setup.routes.js
+++ app/routes/return-logs-setup.routes.js
@@ -38,6 +38,78 @@ const routes = [
         }
       }
     }
+  },
+  {
+    method: 'GET',
+    path: '/return-logs/setup/{sessionId}/reported',
+    options: {
+      handler: ReturnLogsSetupController.reported,
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/return-logs/setup/{sessionId}/reported',
+    options: {
+      handler: ReturnLogsSetupController.submitReported,
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/return-logs/setup/{sessionId}/units',
+    options: {
+      handler: ReturnLogsSetupController.units,
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/return-logs/setup/{sessionId}/units',
+    options: {
+      handler: ReturnLogsSetupController.submitUnits,
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
+  },
+  {
+    method: 'GET',
+    path: '/return-logs/setup/{sessionId}/meter-provided',
+    options: {
+      handler: ReturnLogsSetupController.meterProvided,
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
+  },
+  {
+    method: 'POST',
+    path: '/return-logs/setup/{sessionId}/meter-provided',
+    options: {
+      handler: ReturnLogsSetupController.submitMeterProvided,
+      auth: {
+        access: {
+          scope: ['billing']
+        }
+      }
+    }
   }
 ]
 
