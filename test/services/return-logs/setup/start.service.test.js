ee51385d856705126782f0092fc370d0b51a5b33
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-14 15:39:39 +0000

    Remove .only

--- test/services/return-logs/setup/start.service.test.js
+++ test/services/return-logs/setup/start.service.test.js
@@ -13,7 +13,7 @@ const SessionHelper = require('../../../support/helpers/session.helper.js')
 // Thing under test
 const StartService = require('../../../../app/services/return-logs/setup/start.service.js')
 
-describe.only('Return Logs Setup - Start service', () => {
+describe('Return Logs Setup - Start service', () => {
   let sessionId
 
   before(async () => {
