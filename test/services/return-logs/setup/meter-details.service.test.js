861516c104bcfd063558f7cca1aa9a0bedd11820
Rebecca Ransome <beckyrose200@aol.com>
2025-01-21 15:22:17 +0000

    Add: Meter Details service tests

---
+++ test/services/return-logs/setup/meter-details.service.test.js
@@ -0,0 +1,51 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, before } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Thing under test
+const MeterDetailsService = require('../../../../app/services/return-logs/setup/meter-details.service.js')
+
+describe('Return Logs Setup - Meter Details service', () => {
+  let session
+
+  before(async () => {
+    session = await SessionHelper.add({
+      data: {
+        returnReference: '012345'
+      }
+    })
+  })
+
+  describe('when called', () => {
+    it('fetches the current setup session record', async () => {
+      const result = await MeterDetailsService.go(session.id)
+
+      expect(result.sessionId).to.equal(session.id)
+    })
+
+    it('returns page data for the view', async () => {
+      const result = await MeterDetailsService.go(session.id)
+
+      expect(result).to.equal(
+        {
+          pageTitle: 'Meter details',
+          activeNavBar: 'search',
+          returnReference: '012345',
+          backLink: `/system/return-logs/setup/${session.id}/meter-provided`,
+          meterDetailsMake: null,
+          meterDetailsSerialNumber: null,
+          meterDetails10TimesDisplay: null
+        },
+        { skip: ['sessionId'] }
+      )
+    })
+  })
+})
