96594985a312c55a3a9546ed918e32cffc19eb6f
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-22 11:34:03 +0000

    Update submission service test

--- test/services/return-logs/setup/submission.service.test.js
+++ test/services/return-logs/setup/submission.service.test.js
@@ -17,7 +17,7 @@ describe('Return Logs Setup - Submission service', () => {
   let sessionId
 
   before(async () => {
-    const session = await _session()
+    const session = await SessionHelper.add({ data: { beenReceived: false, returnReference: '1234' } })
     sessionId = session.id
   })
 
@@ -26,46 +26,13 @@ describe('Return Logs Setup - Submission service', () => {
       const result = await SubmissionService.go(sessionId)
 
       expect(result).to.equal({
-        abstractionPeriod: 'From 1 January to 31 December',
         activeNavBar: 'search',
         backLink: `/system/return-logs/setup/${sessionId}/received`,
         beenReceived: false,
         journey: null,
-        licenceId: 'db3731ae-3dde-4778-a81e-9be549cfc0e1',
-        licenceRef: '01/111',
-        pageTitle: 'Abstraction return',
-        purposes: 'Evaporative Cooling',
-        returnsPeriod: 'From 1 April 2022 to 31 March 2023',
-        returnReference: '1234',
-        siteDescription: 'POINT A, TIDAL RIVER MEDWAY AT ISLE OF GRAIN',
-        status: 'overdue',
-        tariffType: 'Standard tariff'
+        pageTitle: 'What do you want to do with this return?',
+        returnReference: '1234'
       })
     })
   })
 })
-
-async function _session() {
-  const session = SessionHelper.add({
-    data: {
-      beenReceived: false,
-      dueDate: '2023-04-28T00:00:00.000Z',
-      endDate: '2023-03-31T00:00:00.000Z',
-      licenceId: 'db3731ae-3dde-4778-a81e-9be549cfc0e1',
-      licenceRef: '01/111',
-      periodEndDay: 31,
-      periodEndMonth: 12,
-      periodStartDay: 1,
-      periodStartMonth: 1,
-      purposes: 'Evaporative Cooling',
-      returnReference: '1234',
-      siteDescription: 'POINT A, TIDAL RIVER MEDWAY AT ISLE OF GRAIN',
-      startDate: '2022-04-01T00:00:00.000Z',
-      status: 'due',
-      twoPartTariff: false,
-      underQuery: false
-    }
-  })
-
-  return session
-}
