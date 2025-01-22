302a487915811d0444732c421ecf5f8b6eff12fb
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-22 11:25:55 +0000

    Update presenter and tests

--- app/presenters/return-logs/setup/submission.presenter.js
+++ app/presenters/return-logs/setup/submission.presenter.js
@@ -5,8 +5,6 @@
  * @module SubmissionPresenter
  */
 
-const { formatAbstractionPeriod, formatLongDate } = require('../../base.presenter.js')
-
 /**
  * Formats the data ready for presenting in the `/return-logs/setup/{sessionId}/submission` page
  *
@@ -15,63 +13,15 @@ const { formatAbstractionPeriod, formatLongDate } = require('../../base.presente
  * @returns {object} page data needed for the `/return-logs/setup/{sessionId}/submission` page
  */
 function go(session) {
-  const {
-    id: sessionId,
-    beenReceived,
-    dueDate,
-    endDate,
-    journey,
-    licenceId,
-    licenceRef,
-    periodStartDay,
-    periodStartMonth,
-    periodEndDay,
-    periodEndMonth,
-    purposes,
-    returnReference,
-    siteDescription,
-    startDate,
-    status,
-    twoPartTariff
-  } = session
+  const { id: sessionId, beenReceived, journey, returnReference } = session
 
   return {
-    abstractionPeriod: `From ${formatAbstractionPeriod(
-      periodStartDay,
-      periodStartMonth,
-      periodEndDay,
-      periodEndMonth
-    )}`,
     backLink: `/system/return-logs/setup/${sessionId}/received`,
     beenReceived,
     journey: journey ?? null,
-    licenceId,
-    licenceRef,
-    pageTitle: 'Abstraction return',
-    purposes,
-    returnsPeriod: `From ${formatLongDate(new Date(startDate))} to ${formatLongDate(new Date(endDate))}`,
-    returnReference,
-    siteDescription,
-    status: _status(status, dueDate),
-    tariffType: twoPartTariff ? 'Two part tariff' : 'Standard tariff'
+    pageTitle: 'What do you want to do with this return?',
+    returnReference
   }
-}
-
-function _status(status, dueDate) {
-  if (status === 'due') {
-    // Work out if the return is overdue (status is still 'due' and it is past the due date)
-    const today = new Date()
-    // Reset today's time to midnight to ensure only the date is compared
-    today.setHours(0, 0, 0, 0)
-
-    if (new Date(dueDate) < today) {
-      return 'overdue'
-    }
-  }
-
-  // For all other cases we can just return the status and the return-status-tag macro will know how to display it
-  // return status
-  return status
 }
 
 module.exports = {
--- test/presenters/return-logs/setup/submission.presenter.test.js
+++ test/presenters/return-logs/setup/submission.presenter.test.js
@@ -15,26 +15,22 @@ describe('Return Logs Setup - Submission presenter', () => {
 
   describe('when provided with a populated session', () => {
     beforeEach(() => {
-      session = _testSession()
+      session = {
+        id: 'e840675e-9fb9-4ce1-bf0a-d140f5c57f47',
+        beenReceived: false,
+        returnReference: '1234'
+      }
     })
 
     it('correctly presents the data', () => {
       const result = SubmissionPresenter.go(session)
 
       expect(result).to.equal({
-        abstractionPeriod: 'From 1 January to 31 December',
         backLink: '/system/return-logs/setup/e840675e-9fb9-4ce1-bf0a-d140f5c57f47/received',
         beenReceived: false,
         journey: null,
-        licenceId: 'db3731ae-3dde-4778-a81e-9be549cfc0e1',
-        licenceRef: '01/111',
-        pageTitle: 'Abstraction return',
-        purposes: 'Evaporative Cooling',
-        returnsPeriod: 'From 1 April 2022 to 31 March 2023',
-        returnReference: '1234',
-        siteDescription: 'POINT A, TIDAL RIVER MEDWAY AT ISLE OF GRAIN',
-        status: 'overdue',
-        tariffType: 'Standard tariff'
+        pageTitle: 'What do you want to do with this return?',
+        returnReference: '1234'
       })
     })
 
@@ -51,68 +47,5 @@ describe('Return Logs Setup - Submission presenter', () => {
         })
       })
     })
-
-    describe('the "status" property', () => {
-      describe('when the status is not yet "overdue"', () => {
-        beforeEach(() => {
-          session.dueDate = new Date()
-        })
-
-        it('returns the status as "due', () => {
-          const result = SubmissionPresenter.go(session)
-
-          expect(result.status).to.equal('due')
-        })
-      })
-
-      describe('when the status is "overdue"', () => {
-        beforeEach(() => {
-          // The due date is less than today's date
-          session.dueDate = new Date('2023-04-28')
-        })
-
-        it('returns the status as "overdue', () => {
-          const result = SubmissionPresenter.go(session)
-
-          expect(result.status).to.equal('overdue')
-        })
-      })
-    })
-
-    describe('the "tariffType" property', () => {
-      describe('when the tariff type is two-part tariff', () => {
-        beforeEach(() => {
-          session.twoPartTariff = true
-        })
-
-        it('returns the correct tariff type', () => {
-          const result = SubmissionPresenter.go(session)
-
-          expect(result.tariffType).to.equal('Two part tariff')
-        })
-      })
-    })
   })
 })
-
-function _testSession() {
-  return {
-    id: 'e840675e-9fb9-4ce1-bf0a-d140f5c57f47',
-    beenReceived: false,
-    dueDate: '2023-04-28T00:00:00.000Z',
-    endDate: '2023-03-31T00:00:00.000Z',
-    licenceId: 'db3731ae-3dde-4778-a81e-9be549cfc0e1',
-    licenceRef: '01/111',
-    periodEndDay: 31,
-    periodEndMonth: 12,
-    periodStartDay: 1,
-    periodStartMonth: 1,
-    purposes: 'Evaporative Cooling',
-    returnReference: '1234',
-    siteDescription: 'POINT A, TIDAL RIVER MEDWAY AT ISLE OF GRAIN',
-    startDate: '2022-04-01T00:00:00.000Z',
-    status: 'due',
-    twoPartTariff: false,
-    underQuery: false
-  }
-}
