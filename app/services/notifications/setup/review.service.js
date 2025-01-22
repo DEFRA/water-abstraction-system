360601240b176aebbb727db1b0df4aa8431412a1
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-01-22 07:31:07 +0000

    Add notification review data to page (#1624)
    https://eaflood.atlassian.net/browse/WATER-4775
    
    Previous work has been done to add the initial page, controller and router for the notification review page.
    
    This change introduces the service and presenter to render the data.
    
    The query has been developed based on the current system and has been changed to facilitate business needs.
    
    This changes uses the agreed recipient list for the quarter / annual selected return and renders a list of the recipients.
    
    Pagination and dedupe will be done in separate changes.
    
    
    ---------
    
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/notifications/setup/review.service.js
+++ app/services/notifications/setup/review.service.js
@@ -1,14 +1,17 @@
 'use strict'
 
 /**
- * Formats data for the `/notifications/setup/review` page
+ * Orchestrates fetching and presenting the data needed for the notifications setup review page
  * @module ReviewService
  */
 
+const RecipientsService = require('./fetch-recipients.service.js')
+const ReviewPresenter = require('../../../presenters/notifications/setup/review.presenter.js')
 const SessionModel = require('../../../models/session.model.js')
+const { determineUpcomingReturnPeriods } = require('../../../lib/return-periods.lib.js')
 
 /**
- * Formats data for the `/notifications/setup/review` page
+ * Orchestrates fetching and presenting the data needed for the notifications setup review page
  *
  * @param {string} sessionId - The UUID for setup ad-hoc returns notification session record
  *
@@ -17,13 +20,30 @@ const SessionModel = require('../../../models/session.model.js')
 async function go(sessionId) {
   const session = await SessionModel.query().findById(sessionId)
 
-  // eslint-disable-next-line no-unused-vars
   const { returnsPeriod } = session
+  const selectedReturnsPeriod = _extractReturnPeriod(returnsPeriod)
+  const summer = _summer(returnsPeriod)
+
+  const recipients = await RecipientsService.go(selectedReturnsPeriod.dueDate, summer)
+
+  const formattedData = ReviewPresenter.go(recipients)
 
   return {
     activeNavBar: 'manage',
-    pageTitle: 'Review the mailing list'
+    ...formattedData
   }
+}
+
+function _extractReturnPeriod(returnsPeriod) {
+  const periods = determineUpcomingReturnPeriods()
+
+  return periods.find((period) => {
+    return period.name === returnsPeriod
+  })
+}
+
+function _summer(returnsPeriod) {
+  return returnsPeriod === 'summer' ? 'true' : 'false'
 }
 
 module.exports = {
