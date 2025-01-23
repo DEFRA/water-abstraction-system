2299f85ea59024a5af5bcf9a91e3201bde6a8a2f
Becky Ransome <91424856+Beckyrose200@users.noreply.github.com>
2025-01-23 09:26:38 +0000

    Return Logs setup - Meter details (#1623)
    https://eaflood.atlassian.net/browse/WATER-4809
    
    As part of the work to migrate the return logs setup journey, this PR is focused on adding the 'Meter details' page.
    
    NOTE: This work is only for a due return. The pages will be updated in a separate PR to handle the editing of existing return logs.

--- app/controllers/return-logs-setup.controller.js
+++ app/controllers/return-logs-setup.controller.js
@@ -6,16 +6,25 @@
  */
 
 const InitiateSessionService = require('../services/return-logs/setup/initiate-session.service.js')
+const MeterDetailsService = require('../services/return-logs/setup/meter-details.service.js')
 const MeterProvidedService = require('../services/return-logs/setup/meter-provided.service.js')
 const ReceivedService = require('../services/return-logs/setup/received.service.js')
 const ReportedService = require('../services/return-logs/setup/reported.service.js')
 const StartService = require('../services/return-logs/setup/start.service.js')
+const SubmitMeterDetailsService = require('../services/return-logs/setup/submit-meter-details.service.js')
 const SubmitMeterProvidedService = require('../services/return-logs/setup/submit-meter-provided.service.js')
 const SubmitReceivedService = require('../services/return-logs/setup/submit-received.service.js')
 const SubmitReportedService = require('../services/return-logs/setup/submit-reported.service.js')
 const SubmitStartService = require('../services/return-logs/setup/submit-start.service.js')
 const SubmitUnitsService = require('../services/return-logs/setup/submit-units.service.js')
 const UnitsService = require('../services/return-logs/setup/units.service.js')
+
+async function meterDetails(request, h) {
+  const { sessionId } = request.params
+  const pageData = await MeterDetailsService.go(sessionId)
+
+  return h.view('return-logs/setup/meter-details.njk', pageData)
+}
 
 async function meterProvided(request, h) {
   const { sessionId } = request.params
@@ -50,6 +59,21 @@ async function start(request, h) {
   const pageData = await StartService.go(sessionId)
 
   return h.view('return-logs/setup/start.njk', pageData)
+}
+
+async function submitMeterDetails(request, h) {
+  const {
+    params: { sessionId },
+    payload
+  } = request
+
+  const pageData = await SubmitMeterDetailsService.go(sessionId, payload)
+
+  if (pageData.error) {
+    return h.view('return-logs/setup/meter-details.njk', pageData)
+  }
+
+  return h.redirect(`/system/return-logs/setup/${sessionId}/meter-readings`)
 }
 
 async function submitMeterProvided(request, h) {
@@ -136,11 +160,13 @@ async function units(request, h) {
 }
 
 module.exports = {
+  meterDetails,
   meterProvided,
   received,
   reported,
   setup,
   start,
+  submitMeterDetails,
   submitMeterProvided,
   submitReceived,
   submitReported,
