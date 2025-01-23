43ee238bf45c8f4169c36cd8956b46a997a0df50
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-01-23 01:25:14 -0800

    Update app/services/notifications/setup/dedupe-recipients.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/notifications/setup/dedupe-recipients.service.js
+++ app/services/notifications/setup/dedupe-recipients.service.js
@@ -6,15 +6,26 @@
  */
 
 /**
- * De Duplicates recipients for sending notifications
+ * Dedupes the recipients fetched when sending notifications
  *
- * When presenting or sending notification to recipients we need to remove duplicate contacts
+ * The recipients we fetch using `FetchRecipientsService` will contain _all_ entries. For example, if a licence has both
+ * a 'Licence holder' and 'Returns to' contact the service will fetch both.
  *
- * Deduplicates a list of recipients based on their contact hash ID, preferring certain roles if duplicates are found.
+ * But in a number of cases, the contact entries, once you cater for 'case' (lower and upper), are duplicated. Left as
+ * is it means certain users will receive two letters, or two emails, on the same day regarding the same thing.
  *
+ * Not very professional!
+ *
+ * The query `FetchRecipientsService` uses generates a hash ID for each contact entry. This service dedupes the entries
+ * based on this hash ID to avoid us spamming our users.
+ *
+ * Where we have a duplicate, we favour either the licence holder or primary user entry (depending on method type) just
+ * to be on the safe side, as it is the licence holder we are obligated to notify when sending comms.
  *
  * @param {object[]} recipients - The array of recipient objects to be deduplicated.
- * @returns {object[]} - A new array containing the deduplicated recipient objects, with the preferred roles chosen in case of duplicates.
+ * 
+ * @returns {object[]} - A new array containing the deduplicated recipient objects, with the preferred roles chosen in
+ * case of duplicates.
  */
 function go(recipients) {
   return Object.values(recipients.reduce(_removeDuplicateContactHash, {}))
