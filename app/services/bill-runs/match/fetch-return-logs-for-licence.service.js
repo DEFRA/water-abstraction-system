8612cc2203cece8e44465f5d4e731a035ddcabad
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-24 00:05:07 +0000

    Add extra logging

--- app/services/bill-runs/match/fetch-return-logs-for-licence.service.js
+++ app/services/bill-runs/match/fetch-return-logs-for-licence.service.js
@@ -19,7 +19,25 @@ const ReturnLogModel = require('../../../models/return-log.model.js')
  * `returnSubmissionLines` if they exist
  */
 async function go(licenceRef, billingPeriod) {
-  return _fetch(licenceRef, billingPeriod)
+  try {
+    return await _fetch(licenceRef, billingPeriod)
+  } catch (error) {
+    // NOTE: The try/catch was added after we found out that it is possible to set up return requirements in NALD with
+    // empty abstraction periods. This then causes the return log to also be missing abstraction period data. Our
+    // cast in the query then causes an error.
+    global.GlobalNotifier.omfg(
+      'Bill run process fetch return logs for licence failed',
+      { licenceRef, billingPeriod },
+      error
+    )
+    // NOTE: We rethrow the error so that the bill run will be set to error status. We know this will result in use
+    // having to check out the issue. But with this additional logging we should be able to quickly see which licence
+    // caused the error.
+    //
+    // If we don't rethrow the error then the bill run will complete without the return log being picked up. This would
+    // result in an incorrect bill run because the submission would not have been picked up for allocation.
+    throw error
+  }
 }
 
 async function _fetch(licenceRef, billingPeriod) {
