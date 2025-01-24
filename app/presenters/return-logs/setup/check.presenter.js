bb9e388ffaee75d2908a01ef76c4e3e7bec3617c
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-24 20:48:09 +0000

    Update the "Note" section

--- app/presenters/return-logs/setup/check.presenter.js
+++ app/presenters/return-logs/setup/check.presenter.js
@@ -13,12 +13,30 @@
  * @returns {object} page data needed for the `/return-logs/setup/{sessionId}/check` page
  */
 function go(session) {
-  const { id: sessionId, returnReference } = session
+  const { id: sessionId, note, returnReference } = session
 
   return {
+    note: _note(note),
     pageTitle: 'Check details and enter new volumes or readings',
     returnReference,
     sessionId
+  }
+}
+
+function _note(note) {
+  if (note?.content) {
+    return {
+      actions: [
+        { text: 'Change', href: 'note' },
+        { text: 'Delete', href: 'delete-note' }
+      ],
+      text: note.content
+    }
+  } else {
+    return {
+      actions: [{ text: 'Add a note', href: 'note' }],
+      text: 'No notes added'
+    }
   }
 }
 
--- test/presenters/return-logs/setup/check.presenter.test.js
+++ test/presenters/return-logs/setup/check.presenter.test.js
@@ -13,21 +13,74 @@ const CheckPresenter = require('../../../../app/presenters/return-logs/setup/che
 describe('Return Logs Setup - Check presenter', () => {
   let session
 
-  describe('when provided with a populated session', () => {
-    beforeEach(() => {
-      session = {
-        id: 'e840675e-9fb9-4ce1-bf0a-d140f5c57f47',
-        returnReference: '1234'
-      }
-    })
+  beforeEach(() => {
+    session = {
+      id: 'e840675e-9fb9-4ce1-bf0a-d140f5c57f47',
+      returnReference: '1234'
+    }
+  })
 
+  describe('when provided with a populated session', () => {
     it('correctly presents the data', () => {
       const result = CheckPresenter.go(session)
 
       expect(result).to.equal({
+        note: {
+          actions: [
+            {
+              href: 'note',
+              text: 'Add a note'
+            }
+          ],
+          text: 'No notes added'
+        },
         pageTitle: 'Check details and enter new volumes or readings',
         returnReference: '1234',
         sessionId: session.id
+      })
+    })
+  })
+
+  describe('the "note" property', () => {
+    describe('when the user has added a note', () => {
+      beforeEach(() => {
+        session.note = {
+          content: 'Note attached to requirement'
+        }
+      })
+
+      it('returns text with the note content and the change and delete a note action', () => {
+        const result = CheckPresenter.go(session)
+
+        expect(result.note).to.equal({
+          actions: [
+            {
+              href: 'note',
+              text: 'Change'
+            },
+            {
+              href: 'delete-note',
+              text: 'Delete'
+            }
+          ],
+          text: 'Note attached to requirement'
+        })
+      })
+    })
+
+    describe('when the user has not added a note', () => {
+      it('returns text with "No notes added" and the add a note action', () => {
+        const result = CheckPresenter.go(session)
+
+        expect(result.note).to.equal({
+          actions: [
+            {
+              href: 'note',
+              text: 'Add a note'
+            }
+          ],
+          text: 'No notes added'
+        })
       })
     })
   })
