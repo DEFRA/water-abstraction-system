ce282a4adedc121dc9e9e876db9f253a65844fa1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-26 20:33:09 +0000

    Update the check service to use our new presenters

--- app/services/bill-runs/setup/check.service.js
+++ app/services/bill-runs/setup/check.service.js
@@ -5,9 +5,12 @@
  * @module CheckService
  */
 
-const CheckPresenter = require('../../../presenters/bill-runs/setup/check.presenter.js')
+const AllowedBillRunPresenter = require('../../../presenters/bill-runs/setup/check/allowed-bill-run.presenter.js')
+const BlockedBillRunPresenter = require('../../../../app/presenters/bill-runs/setup/check/blocked-bill-run.presenter.js')
+const NoAnnualBillRunPresenter = require('../../../presenters/bill-runs/setup/check/no-annual-bill-run.presenter.js')
 const DetermineBlockingBillRunService = require('./determine-blocking-bill-run.service.js')
 const SessionModel = require('../../../models/session.model.js')
+const { engineTriggers } = require('../../../../app/lib/static-lookups.lib.js')
 
 /**
  * Orchestrates fetching and presenting the data for `/bill-runs/setup/{sessionId}/check` page
@@ -18,14 +21,26 @@ const SessionModel = require('../../../models/session.model.js')
  */
 async function go(sessionId) {
   const session = await SessionModel.query().findById(sessionId)
-
   const blockingResults = await DetermineBlockingBillRunService.go(session)
-  const formattedData = await CheckPresenter.go(session, blockingResults)
+
+  const formattedData = _formattedData(session, blockingResults)
 
   return {
     activeNavBar: 'bill-runs',
     ...formattedData
   }
+}
+
+function _formattedData(session, blockingResults) {
+  if (blockingResults.toFinancialYearEnding === 0) {
+    return NoAnnualBillRunPresenter.go(session)
+  }
+
+  if (blockingResults.trigger === engineTriggers.neither) {
+    return BlockedBillRunPresenter.go(session, blockingResults)
+  }
+
+  return AllowedBillRunPresenter.go(session, blockingResults)
 }
 
 module.exports = {
