98c5e039f4e12ef8cadc29530249566cbd8cf6e9
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2025-01-27 16:41:18 +0000

    Add `notes` and `created_by` to `return_submissions` (#1661)
    https://eaflood.atlassian.net/browse/WATER-4807
    
    New functionality is being created to enable the user to save a note against a return submission, previously referred to as a version.
    
    This PR will add the `notes` and `created_by` columns to the `returns.versions` table in our test DB and update the `return_submissions` view.
    
    PR https://github.com/DEFRA/water-abstraction-returns/pull/406 will make these updates to the `returns.versions` table.

---
+++ db/migrations/public/20250127153338_alter-return-submissions-view.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const viewName = 'return_submissions'
+
+exports.up = function (knex) {
+  return knex.schema.dropViewIfExists(viewName).createView(viewName, (view) => {
+    view.as(
+      knex('versions').withSchema('returns').select([
+        'version_id AS id',
+        'return_id as return_log_id',
+        'user_id',
+        'user_type',
+        'version_number as version',
+        'metadata',
+        'nil_return',
+        'current',
+        'notes', // new column
+        'created_by', // new column
+        'created_at',
+        'updated_at'
+      ])
+    )
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropViewIfExists(viewName).createView(viewName, (view) => {
+    view.as(
+      knex('versions')
+        .withSchema('returns')
+        .select([
+          'version_id AS id',
+          'return_id as return_log_id',
+          'user_id',
+          'user_type',
+          'version_number as version',
+          'metadata',
+          'nil_return',
+          'current',
+          'created_at',
+          'updated_at'
+        ])
+    )
+  })
+}
