4e586401257ddbd42f452f4050cfe0170d260c56
Jason Claxton <jason.claxton@defra.gov.uk>
2025-01-29 11:22:39 +0000

    Add notification to check service

--- app/services/return-logs/setup/check.service.js
+++ app/services/return-logs/setup/check.service.js
@@ -12,18 +12,30 @@ const CheckPresenter = require('../../../presenters/return-logs/setup/check.pres
  * Orchestrates fetching and presenting the data needed for the `/return-logs/setup/{sessionId}/check` page
  *
  * @param {string} sessionId - The UUID of the current session
+ * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
  *
  * @returns {Promise<object>} page data needed by the view template
  */
-async function go(sessionId) {
+async function go(sessionId, yar) {
   const session = await SessionModel.query().findById(sessionId)
+
+  await _markCheckPageVisited(session)
 
   const formattedData = CheckPresenter.go(session)
 
+  const notification = yar.flash('notification')[0]
+
   return {
     activeNavBar: 'search',
+    notification,
     ...formattedData
   }
+}
+
+async function _markCheckPageVisited(session) {
+  session.checkPageVisited = true
+
+  return session.$update()
 }
 
 module.exports = {
