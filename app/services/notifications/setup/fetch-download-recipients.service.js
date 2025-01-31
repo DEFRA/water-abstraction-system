739959edf4bedc09e653b0c6fd7c330c17cf3739
jonathangoulding <goulding.jon@icloud.com>
2025-01-31 14:11:59 +0000

    fix: editor

--- app/services/notifications/setup/fetch-download-recipients.service.js
+++ app/services/notifications/setup/fetch-download-recipients.service.js
@@ -27,60 +27,61 @@ async function _fetch(dueDate, summer) {
 }
 
 function _query() {
-  return `SELECT
-    contacts.licence_ref,
-    contacts.contact_type,
-    contacts.email,
-    contacts.contact
+  return `
+SELECT
+  contacts.licence_ref,
+  contacts.contact_type,
+  contacts.email,
+  contacts.contact
 FROM (
-         SELECT DISTINCT
-             ldh.licence_ref,
-             (contacts->>'role') AS contact_type,
-             (NULL) AS email,
-             contacts as contact
-         FROM public.licence_document_headers ldh
-                  INNER JOIN LATERAL jsonb_array_elements(ldh.metadata -> 'contacts') AS contacts ON TRUE
-                  INNER JOIN public.return_logs rl
-                             ON rl.licence_ref = ldh.licence_ref
-         WHERE
-             rl.status = 'due'
-           AND rl.due_date = ?
-           AND rl.metadata->>'isCurrent' = 'true'
-           AND rl.metadata->>'isSummer' = ?
-           AND contacts->>'role' IN ('Licence holder', 'Returns to')
-           AND NOT EXISTS (
-             SELECT
-                 1
-             FROM public.licence_entity_roles ler
-             WHERE
-                 ler.company_entity_id = ldh.company_entity_id
-               AND ler."role" IN ('primary_user', 'user_returns')
-         )
-         UNION ALL
-         SELECT
-             ldh.licence_ref,
-             (CASE
-                  WHEN ler."role" = 'primary_user' THEN 'Primary user'
-                  ELSE 'Returns agent'
-                 END) AS contact_type,
-             le."name" AS email,
-             (NULL) AS contact
-         FROM public.licence_document_headers ldh
-                  INNER JOIN public.licence_entity_roles ler
-                             ON ler.company_entity_id = ldh.company_entity_id
-                                 AND ler."role" IN ('primary_user', 'user_returns')
-                  INNER JOIN public.licence_entities le
-                             ON le.id = ler.licence_entity_id
-                  INNER JOIN public.return_logs rl
-                             ON rl.licence_ref = ldh.licence_ref
-         WHERE
-             rl.status = 'due'
-           AND rl.due_date = ?
-           AND rl.metadata->>'isCurrent' = 'true'
-           AND rl.metadata->>'isSummer' = ?
-     ) contacts
+  SELECT DISTINCT
+    ldh.licence_ref,
+    (contacts->>'role') AS contact_type,
+    (NULL) AS email,
+    contacts as contact
+  FROM public.licence_document_headers ldh
+    INNER JOIN LATERAL jsonb_array_elements(ldh.metadata -> 'contacts') AS contacts ON TRUE
+    INNER JOIN public.return_logs rl
+        ON rl.licence_ref = ldh.licence_ref
+    WHERE
+      rl.status = 'due'
+      AND rl.due_date = ?
+      AND rl.metadata->>'isCurrent' = 'true'
+      AND rl.metadata->>'isSummer' = ?
+      AND contacts->>'role' IN ('Licence holder', 'Returns to')
+      AND NOT EXISTS (
+        SELECT
+            1
+        FROM public.licence_entity_roles ler
+        WHERE
+          ler.company_entity_id = ldh.company_entity_id
+        AND ler."role" IN ('primary_user', 'user_returns')
+      )
+  UNION ALL
+  SELECT
+    ldh.licence_ref,
+    (CASE
+      WHEN ler."role" = 'primary_user' THEN 'Primary user'
+      ELSE 'Returns agent'
+    END) AS contact_type,
+    le."name" AS email,
+    (NULL) AS contact
+  FROM public.licence_document_headers ldh
+  INNER JOIN public.licence_entity_roles ler
+    ON ler.company_entity_id = ldh.company_entity_id
+    AND ler."role" IN ('primary_user', 'user_returns')
+  INNER JOIN public.licence_entities le
+    ON le.id = ler.licence_entity_id
+  INNER JOIN public.return_logs rl
+    ON rl.licence_ref = ldh.licence_ref
+  WHERE
+    rl.status = 'due'
+    AND rl.due_date = ?
+    AND rl.metadata->>'isCurrent' = 'true'
+    AND rl.metadata->>'isSummer' = ?
+) contacts
 ORDER BY
-    contacts.licence_ref`
+contacts.licence_ref`
 }
 
 module.exports = {
