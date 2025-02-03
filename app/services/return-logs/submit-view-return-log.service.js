c98fe5dff384b8fb95d4c9ab67b0e3e7597c2763
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-02-03 15:26:20 +0000

    Implement service to pass tests

--- app/services/return-logs/submit-view-return-log.service.js
+++ app/services/return-logs/submit-view-return-log.service.js
@@ -5,14 +5,43 @@
  * @module SubmitViewReturnLogService
  */
 
+const ReturnLogModel = require('../../models/return-log.model.js')
+
 /**
  *
  * @param returnLogId
- * @param _yar
- * @param _payload
+ * @param yar
+ * @param payload
  */
-async function go(returnLogId, _yar, _payload) {
-  console.log('🚀 ~ go ~ returnLogId:', returnLogId)
+async function go(returnLogId, yar, payload) {
+  const parsedPayload = _parsePayload(payload)
+
+  // NOTE: The YarPlugin decorates the Hapi request object with a yar property. Yar is a session manager
+  _bannerMessage(yar, parsedPayload)
+
+  await _update(returnLogId, parsedPayload)
+}
+
+function _bannerMessage(yar, parsedPayload) {
+  const { underQuery } = parsedPayload
+
+  if (underQuery) {
+    yar.flash('banner', 'This return has been marked under query.')
+  }
+}
+
+function _parsePayload(payload) {
+  const markQuery = payload['mark-query'] ?? null
+
+  return {
+    underQuery: markQuery === 'mark'
+  }
+}
+
+async function _update(returnLogId, parsedPayload) {
+  const { underQuery } = parsedPayload
+
+  await ReturnLogModel.query().findById(returnLogId).patch({ underQuery })
 }
 
 module.exports = {
