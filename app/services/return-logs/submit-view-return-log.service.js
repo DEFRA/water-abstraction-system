cac81a1de452830361fb9af72f3b8c05130bc3ad
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-02-03 17:51:52 +0000

    Refactor to simplify

--- app/services/return-logs/submit-view-return-log.service.js
+++ app/services/return-logs/submit-view-return-log.service.js
@@ -23,32 +23,11 @@ const ReturnLogModel = require('../../models/return-log.model.js')
  * @param {object} payload - The submitted form data
  */
 async function go(returnLogId, yar, payload) {
-  const parsedPayload = _parsePayload(payload)
-
-  // NOTE: The YarPlugin decorates the Hapi request object with a yar property. Yar is a session manager
-  _bannerMessage(yar, parsedPayload)
-
-  await _update(returnLogId, parsedPayload)
-}
-
-function _bannerMessage(yar, parsedPayload) {
-  const { underQuery } = parsedPayload
+  const underQuery = payload['mark-query'] === 'mark'
 
   if (underQuery) {
     yar.flash('banner', 'This return has been marked under query.')
   }
-}
-
-function _parsePayload(payload) {
-  const markQuery = payload['mark-query'] ?? null
-
-  return {
-    underQuery: markQuery === 'mark'
-  }
-}
-
-async function _update(returnLogId, parsedPayload) {
-  const { underQuery } = parsedPayload
 
   await ReturnLogModel.query().findById(returnLogId).patch({ underQuery })
 }
