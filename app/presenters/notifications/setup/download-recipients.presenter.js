914abe78c9a65d917853453e8d021a5f2763f74b
jonathangoulding <goulding.jon@icloud.com>
2025-02-03 10:33:15 +0000

    feat: handle csv special characters

--- app/presenters/notifications/setup/download-recipients.presenter.js
+++ app/presenters/notifications/setup/download-recipients.presenter.js
@@ -5,37 +5,61 @@
  * @module DownloadRecipientsPresenter
  */
 
+const ConvertToCSVService = require('../../../services/jobs/export/convert-to-csv.service.js')
+
 /**
- * Formats data for the `/notifications/setup/download` link
+ * Formats data for the `/notifications/setup/download` link.
  *
- * Will take an array of recipients and convert it into a csv.
+ * This function takes an array of recipient objects and transforms it into a CSV
+ * string suitable for download.
  *
- * @param {object[]} recipients - List of recipient objects, each containing recipient details like email or name.
+ * @param {object[]} recipients - An array of recipients
  *
- * @returns {string} - The data to a csv string
+ * @returns {string} - A CSV-formatted string that includes the recipients' data, with the first
+ * row as column headers and subsequent rows corresponding to the recipient details.
  */
 function go(recipients) {
-  const formattedData = _formatData(recipients)
-  return _jsonToCSV(formattedData)
+  const formattedData = _transformForCsv(recipients)
+  return _transformJsonToCsv(formattedData)
 }
 
-function _formatData(data) {
-  return data.map((item) => {
+/**
+ * Transforms the recipients' data into a CSV-compatible format.
+ *
+ * This function takes an array of recipient data and maps it into a structure
+ * suitable for CSV export.
+ *
+ * The order of the resulting objects will determine the column order in the CSV file.
+ *
+ * @private
+ */
+function _transformForCsv(recipients) {
+  return recipients.map((item) => {
     return {
       Licences: item.licence_ref
     }
   })
 }
 
-function _jsonToCSV(jsonArray) {
-  // Get the headers from the keys of the first object
-  const headers = Object.keys(jsonArray[0])
+/**
+ * Transforms an array of JSON objects into a CSV string.
+ *
+ * This function takes an array of JSON objects and transforms it into a CSV string.
+ * The keys of the objects become the column headers, and the values become the rows
+ * corresponding to each header. The output will be a standard CSV format with the
+ * first row containing the headers.
+ *
+ * @private
+ */
+function _transformJsonToCsv(jsonArray) {
+  const headers = ConvertToCSVService.go(Object.keys(jsonArray[0]))
 
-  // Map each object in the array to a CSV row
-  const rows = jsonArray.map((obj) => headers.map((header) => `"${obj[header]}"`).join(','))
+  const rows = jsonArray.map((obj) => {
+    const row = Object.values(obj)
+    return ConvertToCSVService.go(row)
+  })
 
-  // Join headers and rows to form the CSV string
-  return [headers.join(','), ...rows].join('\n')
+  return [headers, ...rows].join('')
 }
 
 module.exports = {
--- test/presenters/notifications/setup/download-recipients.presenter.test.js
+++ test/presenters/notifications/setup/download-recipients.presenter.test.js
@@ -10,7 +10,7 @@ const { expect } = Code
 // Thing under test
 const DownloadRecipientsPresenter = require('../../../../app/presenters/notifications/setup/download-recipients.presenter.js')
 
-describe('Notifications Setup - Download recipients presenter', () => {
+describe.only('Notifications Setup - Download recipients presenter', () => {
   let recipients
 
   beforeEach(() => {
@@ -21,7 +21,7 @@ describe('Notifications Setup - Download recipients presenter', () => {
     it('correctly formats the data to a csv string', () => {
       const result = DownloadRecipientsPresenter.go(recipients)
 
-      expect(result).to.equal('Licences\n"12323"\n"4567"')
+      expect(result).to.equal('"Licences"\n"12323"\n"4567"\n')
     })
 
     it('correctly formats the headers', () => {
@@ -29,15 +29,59 @@ describe('Notifications Setup - Download recipients presenter', () => {
 
       const [headers] = result.split('\n')
 
-      expect(headers).to.equal('Licences')
+      expect(headers).to.equal('"Licences"')
     })
 
     it('correctly formats a row', () => {
       const result = DownloadRecipientsPresenter.go(recipients)
 
-      const [, row] = result.split('\n')
+      const [, firstRow] = result.split('\n')
 
-      expect(row).to.equal('"12323"')
+      expect(firstRow).to.equal('"12323"')
+    })
+
+    describe('and there are special characters', () => {
+      describe('and the character is ","', () => {
+        beforeEach(() => {
+          recipients[0] = { licence_ref: '123,456' }
+        })
+
+        it('should handle the special character', () => {
+          const result = DownloadRecipientsPresenter.go(recipients)
+
+          const [, firstRow] = result.split('\n')
+
+          expect(firstRow).to.equal('"123,456"')
+        })
+      })
+
+      describe('and the character is ":"', () => {
+        beforeEach(() => {
+          recipients[0] = { licence_ref: '123:456' }
+        })
+
+        it('should handle the special character', () => {
+          const result = DownloadRecipientsPresenter.go(recipients)
+
+          const [, firstRow] = result.split('\n')
+
+          expect(firstRow).to.equal('"123:456"')
+        })
+      })
+
+      describe('and the character is """"', () => {
+        beforeEach(() => {
+          recipients[0] = { licence_ref: '123""456' }
+        })
+
+        it('should handle the special character', () => {
+          const result = DownloadRecipientsPresenter.go(recipients)
+
+          const [, firstRow] = result.split('\n')
+
+          expect(firstRow).to.equal('"123""""456"')
+        })
+      })
     })
   })
 })
