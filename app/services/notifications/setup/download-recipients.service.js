5165ef7ee50e5c60a2d80fb008654370bfe87476
jonathangoulding <goulding.jon@icloud.com>
2025-02-04 14:12:15 +0000

    feat: use unique code for filename

--- app/services/notifications/setup/download-recipients.service.js
+++ app/services/notifications/setup/download-recipients.service.js
@@ -5,6 +5,8 @@
  * @module DownloadRecipientsService
  */
 
+const SessionModel = require('../../../models/session.model.js')
+
 /**
  * Orchestrates fetching and formatting the data needed for the notifications setup download link
  *
@@ -12,15 +14,20 @@
  *
  * It does not seem necessary to use a data stream to create the csv as the data is relatively small.
  *
+ * @param {string} sessionId - The UUID for setup ad-hoc returns notification session record
+ *
  * @returns {Promise<object>} The data for the download link (csv string, filename and type)
  */
-async function go() {
+async function go(sessionId) {
+  const session = await SessionModel.query().findById(sessionId)
+  const { notificationType, referenceCode } = session
+
   const csv = 'Licences\n12234\n'
 
   return {
     data: csv,
     type: 'text/csv',
-    filename: 'recipients.csv'
+    filename: `${notificationType} - ${referenceCode}.csv`
   }
 }
 
--- test/services/notifications/setup/download-recipients.service.test.js
+++ test/services/notifications/setup/download-recipients.service.test.js
@@ -4,19 +4,31 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = (exports.lab = Lab.script())
+const { describe, it, before } = (exports.lab = Lab.script())
 const { expect } = Code
+
+// Test helpers
+const SessionHelper = require('../../../support/helpers/session.helper.js')
 
 // Thing under test
 const DownloadRecipientsService = require('../../../../app/services/notifications/setup/download-recipients.service.js')
 
 describe('Notifications Setup - Download recipients service', () => {
+  let session
+  const referenceCode = 'RINV-00R1MQ'
+
+  before(async () => {
+    session = await SessionHelper.add({
+      data: { returnsPeriod: 'quarterFour', referenceCode, notificationType: 'Returns invitation' }
+    })
+  })
+
   it('correctly returns the csv string, filename and type', async () => {
-    const result = await DownloadRecipientsService.go()
+    const result = await DownloadRecipientsService.go(session.id)
 
     expect(result).to.equal({
       data: 'Licences\n12234\n',
-      filename: 'recipients.csv',
+      filename: `Returns invitation - ${referenceCode}.csv`,
       type: 'text/csv'
     })
   })
