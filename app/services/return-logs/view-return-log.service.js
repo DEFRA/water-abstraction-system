229f93dfe60f2b545ad15a685682183e16d0ed00
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-02-05 10:27:22 +0000

    Add 'Mark as under query' button to View Return Logs page (#1671)
    https://eaflood.atlassian.net/browse/WATER-4893
    
    This PR adds a "Mark under query" button to the "View return log" page.
    
    If the return log is not already under query, the button text will be `Mark as under query`. Clicking it sets the `underQuery` flag on the return log to `true` and reloads the page displaying a temporary flash banner.
    
    If the return log is currently under query, the button text will be `Resolve query`. Clicking it sets the `underQuery` flag to `false` and reloads the page. No banner will be shown in this scenario.
    
    We introduce `SubmitViewReturnLogService` to handle this. The service handles checking if the button click action is to mark or resolve the query; creates a banner using `yar.flash()` if it's being marked; and updates the record.

--- app/services/return-logs/view-return-log.service.js
+++ app/services/return-logs/view-return-log.service.js
@@ -14,16 +14,20 @@ const ViewReturnLogPresenter = require('../../presenters/return-logs/view-return
  * @param {string} returnId - The ID of the return log to view
  * @param {number} version - The version number of the associated return submission to view (0 means 'current')
  * @param {object} auth - The auth object taken from `request.auth` containing user details
+ * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
  *
  * @returns {Promise<object>} an object representing the `pageData` needed by the view return log template.
  */
-async function go(returnId, version, auth) {
+async function go(returnId, version, auth, yar) {
   const returnLog = await FetchReturnLogService.go(returnId, version)
+
+  const [notificationBannerMessage] = yar.flash('banner')
 
   const pageData = ViewReturnLogPresenter.go(returnLog, auth)
 
   return {
     activeNavBar: 'search',
+    notificationBannerMessage,
     ...pageData
   }
 }
