5ca421965cc65bb3d4a55e88ac1005a3faa78008
Robert Parkinson <robertparkinson@users.noreply.github.com>
2025-02-14 10:58:35 +0000

    Support reissue return logs 4 'no returns required' (#1706)
    https://eaflood.atlassian.net/browse/WATER-4552
    
    When a user creates a no return required return version the return logs need to be updated. This PR adds in the logic to correctly void the return logs from the start date of the no returns required version up to the current date or up to the start of any future return version.

---
+++ app/services/return-logs/void-return-logs.service.js
@@ -0,0 +1,33 @@
+'use strict'
+
+/**
+ * Handles voiding the return logs for a licence after a no returns required return version has been created
+ * @module VoidNoReturnRequiredLicenceReturnLogsService
+ */
+
+const { timestampForPostgres } = require('../../lib/general.lib.js')
+const ReturnLogModel = require('../../models/return-log.model.js')
+
+/**
+ * Handles voiding the return logs for a licence after a no returns required return version has been created
+ *
+ * @param {string} licenceRef - The licenceRef of the licence
+ * @param {Date} startDate - The start date of no returns required return version
+ * @param {Date} [endDate=null] - The optional end date of no returns required return version
+ */
+async function go(licenceRef, startDate, endDate = null) {
+  const query = ReturnLogModel.query()
+    .patch({ status: 'void', updatedAt: timestampForPostgres() })
+    .where('licenceRef', licenceRef)
+    .where('startDate', '>=', startDate)
+
+  if (endDate) {
+    query.where('endDate', '<=', endDate)
+  }
+
+  await query
+}
+
+module.exports = {
+  go
+}
--- test/services/return-versions/setup/check/submit-check.service.test.js
+++ test/services/return-versions/setup/check/submit-check.service.test.js
@@ -5,7 +5,7 @@ const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 const Sinon = require('sinon')
 
-const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { describe, it, beforeEach, afterEach, before } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
@@ -15,6 +15,8 @@ const SessionHelper = require('../../../../support/helpers/session.helper.js')
 const GenerateReturnVersionService = require('../../../../../app/services/return-versions/setup/check/generate-return-version.service.js')
 const PersistReturnVersionService = require('../../../../../app/services/return-versions/setup/check/persist-return-version.service.js')
 const ProcessLicenceReturnLogsService = require('../../../../../app/services/return-logs/process-licence-return-logs.service.js')
+const VoidNoReturnRequiredLicenceReturnLogsService = require('../../../../../app/services/return-logs/void-return-logs.service.js')
+
 // Thing under test
 const SubmitCheckService = require('../../../../../app/services/return-versions/setup/check/submit-check.service.js')
 
@@ -22,54 +24,98 @@ describe('Return Versions Setup - Submit Check service', () => {
   let session
   let sessionId
 
-  beforeEach(async () => {
-    session = await SessionHelper.add({
-      data: {
-        checkPageVisited: false,
-        licence: {
-          id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-          currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-          endDate: null,
-          licenceRef: '01/ABC',
-          licenceHolder: 'Turbo Kid',
-          returnVersions: [],
+  describe('Return Versions Setup - return-required', () => {
+    beforeEach(async () => {
+      session = await SessionHelper.add({
+        data: {
+          checkPageVisited: false,
+          licence: {
+            id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+            endDate: null,
+            licenceRef: '01/ABC',
+            licenceHolder: 'Turbo Kid',
+            returnVersions: [],
+            startDate: '2022-04-01T00:00:00.000Z'
+          },
+          journey: 'returns-required',
+          requirements: [{}],
+          startDateOptions: 'licenceStartDate',
+          reason: 'major-change'
+        }
+      })
+      sessionId = session.id
+
+      Sinon.stub(GenerateReturnVersionService, 'go').resolves({
+        returnVersion: {
+          licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
           startDate: '2022-04-01T00:00:00.000Z'
-        },
-        journey: 'returns-required',
-        requirements: [{}],
-        startDateOptions: 'licenceStartDate',
-        reason: 'major-change'
-      }
+        }
+      })
+      Sinon.stub(ProcessLicenceReturnLogsService, 'go').resolves()
+      Sinon.stub(PersistReturnVersionService, 'go').resolves()
     })
-    sessionId = session.id
 
-    Sinon.stub(GenerateReturnVersionService, 'go').resolves({
-      returnVersion: {
-        licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-        startDate: '2022-04-01T00:00:00.000Z'
-      }
+    afterEach(() => {
+      Sinon.restore()
     })
-    Sinon.stub(ProcessLicenceReturnLogsService, 'go').resolves()
-    Sinon.stub(PersistReturnVersionService, 'go').resolves()
-  })
 
-  afterEach(() => {
-    Sinon.restore()
-  })
+    describe('When called with a licence that has not ended', () => {
+      it('returns a valid licence', async () => {
+        const result = await SubmitCheckService.go(sessionId)
 
-  describe('When called with a licence that has not ended', () => {
-    it('returns a valid licence', async () => {
-      const result = await SubmitCheckService.go(sessionId)
+        expect(result).to.equal(session.data.licence.id)
+      })
+    })
 
-      expect(result).to.equal(session.data.licence.id)
+    describe('When called with an licence that has ended (expired, lapsed or revoked)', () => {
+      it('returns a valid licence', async () => {
+        const result = await SubmitCheckService.go(sessionId)
+
+        expect(result).to.equal(session.data.licence.id)
+      })
     })
   })
 
-  describe('When called with an licence that has ended (expired, lapsed or revoked)', () => {
-    it('returns a valid licence', async () => {
-      const result = await SubmitCheckService.go(sessionId)
+  describe('Return Versions Setup - no-return-required', () => {
+    describe('When called with an no returns required version', () => {
+      before(async () => {
+        session = await SessionHelper.add({
+          data: {
+            checkPageVisited: false,
+            licence: {
+              id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+              currentVersionStartDate: '2023-01-01T00:00:00.000Z',
+              endDate: null,
+              licenceRef: '01/ABC',
+              licenceHolder: 'Turbo Kid',
+              returnVersions: [],
+              startDate: '2022-04-01T00:00:00.000Z'
+            },
+            journey: 'no-returns-required',
+            requirements: [{}],
+            startDateOptions: 'licenceStartDate',
+            reason: 'major-change'
+          }
+        })
+        sessionId = session.id
 
-      expect(result).to.equal(session.data.licence.id)
+        Sinon.stub(GenerateReturnVersionService, 'go').resolves({
+          returnVersion: {
+            licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
+            startDate: '2022-04-01T00:00:00.000Z'
+          }
+        })
+        Sinon.stub(ProcessLicenceReturnLogsService, 'go').resolves()
+        Sinon.stub(PersistReturnVersionService, 'go').resolves('8b7f78ba-f3ad-4cb6-a058-78abc4d1383d')
+        Sinon.stub(VoidNoReturnRequiredLicenceReturnLogsService, 'go').resolves()
+      })
+
+      it('returns a valid licence', async () => {
+        const result = await SubmitCheckService.go(sessionId)
+
+        expect(result).to.equal(session.data.licence.id)
+      })
     })
   })
 })
