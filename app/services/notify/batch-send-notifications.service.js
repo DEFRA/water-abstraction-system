56bff9678c08b03c5b52a07f6a682c77fe77333a
jonathangoulding <goulding.jon@icloud.com>
2025-02-26 15:24:45 +0000

    Spike Batch Notify
    https://eaflood.atlassian.net/browse/WATER-4904
    
    Get the batch notifications working and dive out the design / structure of the code.

---
+++ app/services/notify/batch-send-notifications.service.js
@@ -0,0 +1,137 @@
+'use strict'
+
+/**
+ * Send an email using GOV.UK Notify
+ * @module BatchSendNotificationsService
+ */
+
+const NotifyEmailService = require('./notify-email.service.js')
+const NotifyLetterService = require('./notify-letter.service.js')
+const notifyRateLimit = 3000
+const { notifyTemplates } = require('../../lib/notify-templates.lib.js')
+const { contactName, contactAddress } = require('../../presenters/crm.presenter.js')
+
+/**
+ *
+ * @param recipients
+ */
+async function go(recipients) {
+  try {
+    console.time('testTime')
+
+    // Journey -Send to notify
+    const notifications = await _promiseSettled(recipients)
+    // Add to the scheduled notification table - with notification id
+
+    // Get status ? - update in notification table
+
+    // Add error handling
+    _notificationErrors(notifications)
+
+    console.timeEnd('testTime')
+
+    return notifications
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+function _formatEmail(recipient) {
+  return {
+    templateId: notifyTemplates.returns.invitations.primaryUserEmail,
+    emailAddress: recipient.email,
+    options: {
+      personalisation: {
+        periodEndDate: '28th January 2025',
+        periodStartDate: '1st January 2025',
+        returnDueDate: '28th April 2025'
+      },
+      reference: 'developer-testing'
+    }
+  }
+}
+
+function _formatLetter(recipient) {
+  const name = contactName(recipient.contact)
+  const address = contactAddress(recipient.contact)
+
+  const addressLines = {}
+
+  // FYI - The postcode personalisation argument has been replaced. If your template still uses postcode, Notify will treat it as the last line of the address.
+
+  // address_line_1: 'Amala Bird', // required string
+  // address_line_2: '123 High Street', // required string
+  // address_line_3: 'Richmond upon Thames', // required string
+  // address_line_4: 'Middlesex',
+  // address_line_5: 'SW14 6BF', // last line of address you include must be a postcode or a country name  outside the UK
+  for (const [index, value] of address.entries()) {
+    addressLines[`address_line_${index + 1}`] = value
+  }
+
+  return {
+    templateId: notifyTemplates.returns.invitations.licenceHolderLetter,
+    options: {
+      personalisation: {
+        name,
+        ...addressLines,
+        periodEndDate: '28th January 2025',
+        periodStartDate: '1st January 2025',
+        returnDueDate: '28th April 2025'
+      },
+      reference: 'developer-testing'
+    }
+  }
+}
+
+async function _sendLetter(recipient) {
+  const data = _formatLetter(recipient)
+
+  return NotifyLetterService.go(data.templateId, data.options).then((res) => {
+    return {
+      status: res.status,
+      id: res.id,
+      contact_hash_id: recipient.contact_hash_id,
+      personalisation: data.options.personalisation
+    }
+  })
+}
+
+async function _sendEmail(recipient) {
+  const data = _formatEmail(recipient)
+
+  return NotifyEmailService.go(data.templateId, data.emailAddress, data.options).then((res) => {
+    return {
+      status: res.status,
+      id: res.id,
+      contact_hash_id: recipient.contact_hash_id,
+      personalisation: data.options.personalisation
+    }
+  })
+}
+
+async function _promiseSettled(recipients) {
+  const notifications = []
+
+  // Build an array of promises
+  for (const recipient of recipients) {
+    if (recipient.email) {
+      notifications.push(_sendEmail(recipient))
+    } else {
+      notifications.push(_sendLetter(recipient))
+    }
+  }
+
+  return Promise.allSettled(notifications)
+}
+
+function _notificationErrors(notifications) {
+  const failed = notifications.filter((response) => response.status !== 'fulfilled')
+  console.log('Failed: ', JSON.stringify(failed))
+
+  const rateLimited = notifications.filter((response) => response.value.status === 429)
+  console.log(`Failed rateLimited = ${rateLimited.length}`)
+}
+
+module.exports = {
+  go
+}
---
+++ test/services/notify/batch-send-notifications.service.test.js
@@ -0,0 +1,139 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, afterEach, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const { NotifyClient } = require('notifications-node-client')
+const { notifyTemplates } = require('../../../app/lib/notify-templates.lib.js')
+const { stubNotify } = require('../../../config/notify.config.js')
+
+// Thing under test
+const BatchSendNotificationsService = require('../../../app/services/notify/batch-send-notifications.service.js')
+
+describe.only('Notify - Batch send notifications service', () => {
+  let emailAddress
+  let options
+  let recipients
+
+  beforeEach(() => {
+    // To test a real email is delivered replace this with an email on the whitelist (and use the whitelist api key)
+    emailAddress = 'hello@example.com'
+
+    options = {
+      personalisation: {
+        periodEndDate: '28th January 2025',
+        periodStartDate: '1st January 2025',
+        returnDueDate: '28th April 2025'
+      },
+      reference: 'developer-testing'
+    }
+
+    // Determined recipients
+    recipients = [
+      {
+        contact: null,
+        contact_hash_id: '90129f6aa5bf2ad50aa3fefd3f8cf86a',
+        contact_type: 'Primary user',
+        email: 'primary.user@important.com',
+        licence_refs: '456',
+        message_type: 'Email'
+      },
+      {
+        contact: {
+          addressLine1: '1',
+          addressLine2: 'Privet Drive',
+          addressLine3: null,
+          addressLine4: null,
+          country: null,
+          county: 'Surrey',
+          forename: 'Harry',
+          initials: 'H J',
+          name: 'Licence holder',
+          postcode: 'WD25 7LR',
+          role: 'Licence holder',
+          salutation: 'Mr',
+          town: 'Little Whinging',
+          type: 'Person'
+        },
+        contact_hash_id: '22f6457b6be9fd63d8a9a8dd2ed61214',
+        contact_type: 'Licence holder',
+        email: null,
+        licence_refs: '123',
+        message_type: 'Letter'
+      }
+    ]
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when the call to "notify" is successful', () => {
+    beforeEach(() => {
+      _stubSuccessfulNotify(stubNotify, {
+        data: { id: '12345' },
+        status: 201
+      })
+    })
+
+    it('should call notify', { timeout: 120000 }, async () => {
+      const result = await BatchSendNotificationsService.go(recipients)
+
+      expect(result).to.equal([
+        // Email
+        {
+          status: 'fulfilled',
+          value: {
+            contact_hash_id: '90129f6aa5bf2ad50aa3fefd3f8cf86a',
+            id: '12345',
+            status: 201,
+            personalisation: {
+              periodEndDate: '28th January 2025',
+              periodStartDate: '1st January 2025',
+              returnDueDate: '28th April 2025'
+            }
+          }
+        },
+        // Letter
+        {
+          status: 'fulfilled',
+          value: {
+            contact_hash_id: '22f6457b6be9fd63d8a9a8dd2ed61214',
+            id: '12345',
+            status: 201,
+            personalisation: {
+              address_line_1: '1',
+              address_line_2: 'Privet Drive',
+              address_line_3: 'Little Whinging',
+              address_line_4: 'Surrey',
+              address_line_5: 'WD25 7LR',
+              name: 'Mr H J Licence holder',
+              periodEndDate: '28th January 2025',
+              periodStartDate: '1st January 2025',
+              returnDueDate: '28th April 2025'
+            }
+          }
+        }
+      ])
+    })
+  })
+})
+
+function _stubSuccessfulNotify(stub, response) {
+  if (stub) {
+    Sinon.stub(NotifyClient.prototype, 'sendEmail').resolves(response)
+    Sinon.stub(NotifyClient.prototype, 'sendLetter').resolves(response)
+  }
+}
+
+function _stubUnSuccessfulNotify(stub, response) {
+  if (stub) {
+    return Sinon.stub(NotifyClient.prototype, 'sendEmail').rejects(response)
+  }
+}
