91f2af0b1dd53ba9be19111adec9eb82fff73fb8
rvsiyad <roblesiyad123@gmail.com>
2025-02-26 17:47:35 +0000

    Add download return presenter

---
+++ app/presenters/return-logs/download-return.presenter.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const { formatDateObjectToISO } = require('../../lib/dates.lib.js')
+const { transformArrayToCSVRow } = require('../../lib/transform-to-csv.lib.js')
+
+const HEADERS = ['start date', 'reading', 'volume']
+
+function go(returnLog) {
+  const { returnSubmissions } = returnLog
+
+  const selectedReturnSubmission = _selectedReturnSubmission(returnSubmissions)
+
+  const csvData = _transformToCsv(selectedReturnSubmission)
+  const filename = _formatFileName(returnLog)
+
+  return {
+    data: [HEADERS + '\n', ...csvData].join(''),
+    filename
+  }
+}
+
+function _formatFileName(returnLog) {
+  const { returnReference, startDate, endDate, returnSubmissions } = returnLog
+  const version = returnSubmissions[0].version
+
+  return `${returnReference}_${formatDateObjectToISO(startDate)}_${formatDateObjectToISO(endDate)}_v${version}.csv`
+}
+
+function _transformToCsv(selectedReturnSubmission) {
+  const displayReadings = selectedReturnSubmission?.$method() !== 'abstractionVolumes'
+
+  const { returnSubmissionLines } = selectedReturnSubmission
+
+  return returnSubmissionLines.map((returnSubmissionLine) => {
+    const reading = displayReadings ? returnSubmissionLine.reading : ''
+
+    const row = [returnSubmissionLine.startDate, reading, returnSubmissionLine.quantity]
+
+    return transformArrayToCSVRow(row)
+  })
+}
+
+function _selectedReturnSubmission(returnSubmissions) {
+  if (!returnSubmissions) {
+    return null
+  }
+
+  return returnSubmissions[0]
+}
+module.exports = {
+  go
+}
