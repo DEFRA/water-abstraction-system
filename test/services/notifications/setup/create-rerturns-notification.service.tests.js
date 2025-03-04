f19538ce6a12397a63de076cc762c32d51b57b99
jonathangoulding <goulding.jon@icloud.com>
2025-03-04 15:04:28 +0000

    Add notifications setup create notification service
    https://eaflood.atlassian.net/browse/WATER-4778
    
    As part of the ongoing work to replace the legacy notifications code. We need to persist notification into 'water.scheduled_notification'.
    
    This requires an adherence to the existing schema.
    
    This change introduces the create returns notification service to persist notifications into the database.

--- test/services/notifications/setup/create-rerturns-notification.service.tests.js
+++ test/services/notifications/setup/create-rerturns-notification.service.tests.js
@@ -10,47 +10,85 @@ const { expect } = Code
 // Test helpers
 const EventHelper = require('../../../support/helpers/event.helper.js')
 const ScheduledNotificationModel = require('../../../../app/models/scheduled-notification.model.js')
+const { timestampForPostgres } = require('../../../../app/lib/general.lib.js')
 
 // Thing under test
-const CreateReturnsNotificationService = require('../../../../app/services/notifications/setup/create-returns-notification.service.js')
+const CreateNotificationService = require('../../../../app/services/notifications/setup/create-notification.service.js')
 
-describe.only('Notifications Setup - Create event service', () => {
-  let event
+describe('Notifications Setup - Create notification service', () => {
+  let eventId
+  let notifications
 
   beforeEach(async () => {
-    event = await EventHelper.add({
+    const event = await EventHelper.add({
       type: 'notification',
       subtype: 'returnsInvitation'
     })
+
+    eventId = event.id
   })
 
-  it('should create the event (required values)', async () => {
-    const result = await CreateReturnsNotificationService.go(event.id)
+  describe('when inserting a single notification', () => {
+    beforeEach(async () => {
+      notifications = [{ eventId, metadata: {}, createdAt: timestampForPostgres() }]
+    })
 
-    const createdResult = await ScheduledNotificationModel.query().findById(result.id)
+    it('should create a single notification (and only set the required values)', async () => {
+      const result = await CreateNotificationService.go(notifications)
 
-    expect(createdResult).equal({
-      companyId: null,
-      createdAt: createdResult.createdAt,
-      eventId: null,
-      id: result.id,
-      individualId: null,
-      jobId: null,
-      licences: null,
-      log: null,
-      messageRef: null,
-      messageType: null,
-      metadata: null,
-      nextStatusCheck: null,
-      notificationType: null,
-      notifyId: null,
-      notifyStatus: null,
-      personalisation: null,
-      plaintext: null,
-      recipient: null,
-      sendAfter: null,
-      status: null,
-      statusChecks: null
+      const createdResult = await ScheduledNotificationModel.query().findById(result[0].id)
+
+      expect(createdResult).equal({
+        companyId: null,
+        createdAt: createdResult.createdAt,
+        eventId,
+        id: result[0].id,
+        individualId: null,
+        jobId: null,
+        licences: null,
+        log: null,
+        messageRef: null,
+        messageType: null,
+        metadata: {},
+        nextStatusCheck: null,
+        notificationType: null,
+        notifyId: null,
+        notifyStatus: null,
+        personalisation: null,
+        plaintext: null,
+        recipient: null,
+        sendAfter: null,
+        status: null,
+        statusChecks: null
+      })
+    })
+  })
+
+  describe('when inserting a multiples notifications', () => {
+    beforeEach(() => {
+      notifications = [
+        { eventId, metadata: {}, createdAt: timestampForPostgres() },
+        { eventId, metadata: {}, createdAt: timestampForPostgres() }
+      ]
+    })
+
+    it('should return the saved notifications', async () => {
+      const result = await CreateNotificationService.go(notifications)
+
+      expect(result).equal([
+        {
+          createdAt: result[0].createdAt,
+          eventId,
+          id: result[0].id,
+          metadata: {}
+        },
+        {
+          createdAt: result[1].createdAt,
+          eventId,
+          id: result[1].id,
+          metadata: {}
+        }
+      ])
     })
   })
 })
