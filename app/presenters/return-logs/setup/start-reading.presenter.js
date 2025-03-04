0d329e7ed6fe08f2d4b4bb85df2615c4610b4e81
Rebecca Ransome <beckyrose200@aol.com>
2025-03-04 12:37:17 +0000

    Add: Start reading presenter

---
+++ app/presenters/return-logs/setup/start-reading.presenter.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * Formats the data ready for presenting in the `/return-logs/setup/{sessionId}/start-reading` page
+ * @module StartReadingPresenter
+ */
+
+/**
+ * Formats the data ready for presenting in the `/return-logs/setup/{sessionId}/start-reading` page
+ *
+ * @param {module:SessionModel} session - The session instance to format
+ *
+ * @returns {object} page data needed for the `/return-logs/setup/{sessionId}/start-reading` page
+ */
+function go(session) {
+  const { id: sessionId, returnReference, startReading } = session
+  return {
+    backLink: `/system/return-logs/setup/${sessionId}/check`,
+    pageTitle: 'Enter the start meter reading',
+    returnReference,
+    startReading: startReading ?? null
+  }
+}
+
+module.exports = {
+  go
+}
