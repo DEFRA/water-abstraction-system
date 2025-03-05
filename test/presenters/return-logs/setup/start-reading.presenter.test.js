21326fa3a2a3a857f91e48cc874f96f0d3fd0fd3
Rebecca Ransome <beckyrose200@aol.com>
2025-03-05 10:11:42 +0000

    Add: Start reading presenter test

---
+++ test/presenters/return-logs/setup/start-reading.presenter.test.js
@@ -0,0 +1,50 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const StartReadingPresenter = require('../../../../app/presenters/return-logs/setup/start-reading.presenter.js')
+
+describe('Return Logs Setup - Start Reading presenter', () => {
+  let session
+
+  beforeEach(() => {
+    session = {
+      id: '61e07498-f309-4829-96a9-72084a54996d',
+      returnReference: '012345'
+    }
+  })
+
+  describe('when provided with a session', () => {
+    it('correctly presents the data', () => {
+      const result = StartReadingPresenter.go(session)
+
+      expect(result).to.equal({
+        pageTitle: 'Enter the start meter reading',
+        sessionId: '61e07498-f309-4829-96a9-72084a54996d',
+        returnReference: '012345',
+        startReading: null,
+        backLink: '/system/return-logs/setup/61e07498-f309-4829-96a9-72084a54996d/check'
+      })
+    })
+  })
+
+  describe('the "startReading" property', () => {
+    describe('when the user has previously entered a start reading', () => {
+      beforeEach(() => {
+        session.startReading = '156000'
+      })
+
+      it('returns the "startReading" property populated to display the input', () => {
+        const result = StartReadingPresenter.go(session)
+
+        expect(result.startReading).to.equal('156000')
+      })
+    })
+  })
+})
