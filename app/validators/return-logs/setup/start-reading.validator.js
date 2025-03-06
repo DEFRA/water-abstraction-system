481c1d98e89335eff84a3236f984de60d4ce3314
Rebecca Ransome <beckyrose200@aol.com>
2025-03-06 16:17:59 +0000

    Update: Start reading validator and test

--- app/validators/return-logs/setup/start-reading.validator.js
+++ app/validators/return-logs/setup/start-reading.validator.js
@@ -11,21 +11,24 @@ const Joi = require('joi')
  * Validates data submitted for the `/return-logs/{sessionId}/start-reading` page
  *
  * @param {object} payload - The payload from the request to be validated
+ * @param {object[]} lines - The lines of the return log
  *
  * @returns {object} the result from calling Joi's schema.validate(). It will be an object with a `value:` property. If
  * any errors are found the `error:` property will also exist detailing what the issues were
  */
-function go(payload) {
-  const errorMessage = 'Enter a start meter reading'
-  const positiveErrorMessage = 'Start meter reading must be a positive number'
+function go(payload, lines) {
+  console.log('🚀 lines:', lines)
+  console.log('🚀 payload:', payload)
+  // Use the maximum possible number if no meter reading exists in the return submission
+  const maxMeterReading = _maxMeterReading(lines) || Number.MAX_SAFE_INTEGER
 
   const schema = Joi.object({
-    startReading: Joi.number().positive().integer().required().messages({
-      'any.required': errorMessage,
-      'number.base': positiveErrorMessage,
-      'number.max': errorMessage,
-      'number.unsafe': errorMessage,
-      'number.positive': positiveErrorMessage,
+    startReading: Joi.number().positive().integer().required().max(maxMeterReading).messages({
+      'any.required': 'Enter a start meter reading',
+      'number.base': 'Start meter reading must be a positive number',
+      'number.max': 'Please enter a reading which is equal to or lower than the next reading',
+      'number.unsafe': 'Enter a reading that is 9007199254740991 or fewer',
+      'number.positive': 'Start meter reading must be a positive number',
       'number.integer': 'Enter a whole number'
     })
   })
@@ -33,6 +36,26 @@ function go(payload) {
   return schema.validate(payload, { abortEarly: false })
 }
 
+/**
+ * Finds the first line with a reading and returns that reading as a number or undefined
+ *
+ * @private
+ */
+function _maxMeterReading(lines) {
+  const firstLine = lines.find((line) => {
+    return line.reading
+  })
+
+  return Number(firstLine?.reading)
+}
+
 module.exports = {
   go
 }
+
+// REBECCA
+// Tidy up console logs
+// Validation error messages for the max number
+// Fix the unit tests for the routes that you have broken
+// Make sure in the controller to test the new way the pages can redirect
+// Need to test the check answers page stuff that has been added to the page
--- test/validators/return-logs/setup/start-reading.validator.test.js
+++ test/validators/return-logs/setup/start-reading.validator.test.js
@@ -12,17 +12,46 @@ const StartReadingValidator = require('../../../../app/validators/return-logs/se
 
 describe('Return Logs Setup - Start Reading validator', () => {
   let payload
+  let lines
+
+  beforeEach(() => {
+    lines = [
+      {
+        endDate: '2019-04-30T00:00:00.000Z',
+        startDate: '2019-04-01T00:00:00.000Z'
+      },
+      {
+        endDate: '2019-05-31T00:00:00.000Z',
+        startDate: '2019-05-01T00:00:00.000Z'
+      }
+    ]
+  })
 
   describe('when a valid payload is provided', () => {
     describe('because the user entered a value', () => {
-      beforeEach(() => {
-        payload = { startReading: '156000' }
+      describe('and the return lines do not have any readings', () => {
+        beforeEach(() => {
+          payload = { startReading: '156000' }
+        })
+
+        it('confirms the payload is valid', () => {
+          const result = StartReadingValidator.go(payload, lines)
+
+          expect(result.error).not.to.exist()
+        })
       })
 
-      it('confirms the payload is valid', () => {
-        const result = StartReadingValidator.go(payload)
+      describe('and the return lines do have an initial reading', () => {
+        beforeEach(() => {
+          payload = { startReading: '156000' }
+          lines[0].reading = '160000'
+        })
 
-        expect(result.error).not.to.exist()
+        it('confirms the payload is valid', () => {
+          const result = StartReadingValidator.go(payload, lines)
+
+          expect(result.error).not.to.exist()
+        })
       })
     })
   })
@@ -34,49 +63,82 @@ describe('Return Logs Setup - Start Reading validator', () => {
       })
 
       it('fails validation with the message "Enter a start meter reading"', () => {
-        const result = StartReadingValidator.go(payload)
+        const result = StartReadingValidator.go(payload, lines)
 
         expect(result.error).to.exist()
         expect(result.error.details[0].message).to.equal('Enter a start meter reading')
       })
     })
 
-    describe('but entered text', () => {
+    describe('because the user entered text', () => {
       beforeEach(() => {
-        payload.startReading = 'Test'
+        payload = { startReading: 'Test' }
       })
 
       it('fails validation with the message "Start meter reading must be a positive number"', () => {
-        const result = StartReadingValidator.go(payload)
+        const result = StartReadingValidator.go(payload, lines)
 
         expect(result.error).to.exist()
         expect(result.error.details[0].message).to.equal('Start meter reading must be a positive number')
       })
     })
 
-    describe('but entered a negative start reading', () => {
+    describe('because the user entered a negative start reading', () => {
       beforeEach(() => {
-        payload.startReading = '-0.1'
+        payload = { startReading: '-1' }
       })
 
       it('fails validation with the message "Start meter reading must be a positive number"', () => {
-        const result = StartReadingValidator.go(payload)
+        const result = StartReadingValidator.go(payload, lines)
 
         expect(result.error).to.exist()
         expect(result.error.details[0].message).to.equal('Start meter reading must be a positive number')
       })
     })
 
-    describe('but entered a decimal number', () => {
+    describe('because the user entered a decimal number', () => {
       beforeEach(() => {
-        payload.startReading = '1.1'
+        payload = { startReading: '1.1' }
       })
 
       it('fails validation with the message "Enter a whole number"', () => {
-        const result = StartReadingValidator.go(payload)
+        const result = StartReadingValidator.go(payload, lines)
 
         expect(result.error).to.exist()
         expect(result.error.details[0].message).to.equal('Enter a whole number')
+      })
+    })
+
+    describe('because the return lines have an initial reading', () => {
+      describe('and the user entered a number greater than the initial reading', () => {
+        beforeEach(() => {
+          payload = { startReading: '10010' }
+          lines[0].reading = '10000'
+        })
+
+        it('fails validation with the message "Please enter a reading which is equal to or lower than the next reading"', () => {
+          const result = StartReadingValidator.go(payload, lines)
+
+          expect(result.error).to.exist()
+          expect(result.error.details[0].message).to.equal(
+            'Please enter a reading which is equal to or lower than the next reading'
+          )
+        })
+      })
+    })
+
+    describe('because the return lines do not have an initial reading', () => {
+      describe('and the user entered a number greater than the default maximum', () => {
+        beforeEach(() => {
+          payload = { startReading: '9007199254740995' }
+        })
+
+        it('fails validation with the message "Enter a reading that is 9007199254740991 or fewer"', () => {
+          const result = StartReadingValidator.go(payload, lines)
+
+          expect(result.error).to.exist()
+          expect(result.error.details[0].message).to.equal('Enter a reading that is 9007199254740991 or fewer')
+        })
       })
     })
   })
