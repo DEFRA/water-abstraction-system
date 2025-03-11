91fa89285c51099c06d2b6b53550d6bc39f7e80d
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-03-11 14:03:29 +0000

    Update notification setup submit check to user batch notifications service (#1791)
    * Update notification setup submit check to user batch notifications service
    
    https://eaflood.atlassian.net/browse/WATER-4778
    
    A previous refactor missed updating notifications setup submit check service to use the batch notifications service.
    
    This change updates the call to the batch notifications service. And renames the required presenter to be correct.

--- app/services/notifications/setup/batch-notifications.service.js
+++ app/services/notifications/setup/batch-notifications.service.js
@@ -7,7 +7,7 @@
 
 const NotifyEmailService = require('../../notify/notify-email.service.js')
 const NotifyLetterService = require('../../notify/notify-letter.service.js')
-const ReturnsNotificationPresenter = require('../../../presenters/notifications/setup/scheduled-notifications.presenter.js')
+const ScheduledNotificationsPresenter = require('../../../presenters/notifications/setup/scheduled-notifications.presenter.js')
 
 const CREATED = 201
 
@@ -29,11 +29,18 @@ const CREATED = 201
  * @param {object} determinedReturnsPeriod
  * @param {string} referenceCode
  * @param {string} journey
+ * @param {string} eventId - the event id to link all the notifications to an event
  *
  * @returns {object} - the number of sent and errored notifications
  */
-async function go(recipients, determinedReturnsPeriod, referenceCode, journey) {
-  const notifications = ReturnsNotificationPresenter.go(recipients, determinedReturnsPeriod, referenceCode, journey)
+async function go(recipients, determinedReturnsPeriod, referenceCode, journey, eventId) {
+  const notifications = ScheduledNotificationsPresenter.go(
+    recipients,
+    determinedReturnsPeriod,
+    referenceCode,
+    journey,
+    eventId
+  )
 
   const toSendNotifications = _toSendNotifications(notifications)
 
