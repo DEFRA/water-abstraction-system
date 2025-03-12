0b673366f2233b57e4bdf4f23250874d11a72748
jonathangoulding <goulding.jon@icloud.com>
2025-03-12 16:17:05 +0000

    chore: sonar issues

--- config/notify.config.js
+++ config/notify.config.js
@@ -5,13 +5,15 @@
  * @module NotifyConfig
  */
 
+const defaultBatchSize = 250
+
 // We require dotenv directly in each config file to support unit tests that depend on this subset of config.
 // Requiring dotenv in multiple places has no effect on the app when running for real.
 require('dotenv').config()
 
 const config = {
   apiKey: process.env.GOV_UK_NOTIFY_API_KEY,
-  batchSize: process.env.NOTIFICATIONS_BATCH_SIZE || 250,
+  batchSize: process.env.NOTIFICATIONS_BATCH_SIZE || defaultBatchSize,
   stubNotify: process.env.STUB_NOTIFY || true // Used to perform integration tests with notify,
 }
 
