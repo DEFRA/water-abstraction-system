0613d331c6acd4d31ec6fe4f9b87ae3b816ece34
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-03-12 23:49:03 +0000

    Remove licences filters from assign bill run service
    Previously, we'd let match and allocate do its work, and then return the licences it processed. These would be what we used to determine which licence supplementary year records to assign to the bill run.
    
    But now we have to deal with licences that won't be processed by match and allocate.
    
    So, we're switching the order of things when processing a two-part tariff supplementary bill run. Now, we'll assign _all_ matching licence supplementary year records to the bill run, at the time it is triggered.
    
    We'll revert match & allocate back to its previous form (returning a boolean true or false if anything was populated).
    
    This means we'll have something that ties those licences which no longer have two-part tariff charge versions to the bill run. They won't appear in review, but we can deal with them afterwards when we come to generate the bill run.

--- test/services/bill-runs/assign-bill-run-to-licences.service.test.js
+++ test/services/bill-runs/assign-bill-run-to-licences.service.test.js
@@ -17,22 +17,18 @@ const AssignBillRunToLicencesService = require('../../../app/services/bill-runs/
 describe('Bill Runs - Assign Bill Run To Licences service', () => {
   const billingPeriod = { startDate: new Date('2022-04-01'), endDate: new Date('2023-03-31') }
   const billRunId = '091c3d3f-0328-4b10-b1a1-3eccf55416a0'
-  const licences = [{ id: '098e0fff-1c3e-4be3-83b9-8f483ae5b41e' }, { id: '406b8ba4-63b6-442a-86f3-a144f1f63ca9' }]
   const twoPartTariff = true
 
   let licenceSupplementaryYearPatch
-  let licenceSupplementaryYearWhereIn
   let licenceSupplementaryYearWhere
 
   beforeEach(() => {
     licenceSupplementaryYearPatch = Sinon.stub().returnsThis()
     licenceSupplementaryYearWhere = Sinon.stub().returnsThis()
-    licenceSupplementaryYearWhereIn = Sinon.stub()
 
     Sinon.stub(LicenceSupplementaryYearModel, 'query').returns({
       patch: licenceSupplementaryYearPatch,
-      where: licenceSupplementaryYearWhere,
-      whereIn: licenceSupplementaryYearWhereIn
+      where: licenceSupplementaryYearWhere
     })
   })
 
@@ -42,7 +38,7 @@ describe('Bill Runs - Assign Bill Run To Licences service', () => {
 
   describe('when called', () => {
     it('assigns the bill run ID to the matching "LicenceSupplementaryYear" records', async () => {
-      await AssignBillRunToLicencesService.go(billRunId, licences, billingPeriod, twoPartTariff)
+      await AssignBillRunToLicencesService.go(billRunId, billingPeriod, twoPartTariff)
 
       const patchArgs = licenceSupplementaryYearPatch.args[0][0]
 
@@ -55,13 +51,6 @@ describe('Bill Runs - Assign Bill Run To Licences service', () => {
       const twoPartTariffWhereArgs = licenceSupplementaryYearWhere.secondCall.args
 
       expect(twoPartTariffWhereArgs).to.equal(['twoPartTariff', true])
-
-      const whereInArgs = licenceSupplementaryYearWhereIn.args[0]
-
-      expect(whereInArgs).to.equal([
-        'licenceId',
-        ['098e0fff-1c3e-4be3-83b9-8f483ae5b41e', '406b8ba4-63b6-442a-86f3-a144f1f63ca9']
-      ])
     })
   })
 })
