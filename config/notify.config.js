48ba6b2ff22f69f022f70563340b100120411c0d
jonathangoulding <goulding.jon@icloud.com>
2025-03-12 17:37:04 +0000

    chore: update config

--- config/notify.config.js
+++ config/notify.config.js
@@ -5,14 +5,16 @@
  * @module NotifyConfig
  */
 
-const defaultBatchSize = 250
-
 // We require dotenv directly in each config file to support unit tests that depend on this subset of config.
 // Requiring dotenv in multiple places has no effect on the app when running for real.
 require('dotenv').config()
 
 const config = {
   apiKey: process.env.GOV_UK_NOTIFY_API_KEY,
+  // The Notify service imposes a rate limit of 3,000 requests per minute and restricts the number of statuses returned
+  // per API call to a maximum of 250. Given these constraints, our batch processing mechanism should handle the lowest
+  // of the two limitations, which in this case is the 250-message status retrieval limit from Notify.
+  // https://docs.notifications.service.gov.uk/node.html#get-the-status-of-multiple-messages
   batchSize: process.env.NOTIFICATIONS_BATCH_SIZE || 250,
   stubNotify: process.env.STUB_NOTIFY || true // Used to perform integration tests with notify,
 }
