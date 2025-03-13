bb425469a4b7bbeafb188832db10e8f529311cb7
Jason Claxton <jason.claxton@defra.gov.uk>
2025-03-13 16:04:00 +0000

    Create readings service

---
+++ app/services/return-logs/setup/readings.service.js
@@ -0,0 +1,32 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting the data needed for the `/return-logs/setup/{sessionId}/readings/{yearMonth}` page
+ * @module ReadingsService
+ */
+
+const ReadingsPresenter = require('../../../presenters/return-logs/setup/readings.presenter.js')
+const SessionModel = require('../../../models/session.model.js')
+
+/**
+ * Orchestrates fetching and presenting the data needed for the `/return-logs/setup/{sessionId}/readings/{yearMonth}` page
+ *
+ * @param {string} sessionId - The UUID of the current session
+ * @param {string} yearMonth - The year and zero-indexed month to view, eg. `2014-0` for January 2014
+ *
+ * @returns {Promise<object>} an object representing the data needed for the readings template
+ */
+async function go(sessionId, yearMonth) {
+  const session = await SessionModel.query().findById(sessionId)
+
+  const pageData = ReadingsPresenter.go(session, yearMonth)
+
+  return {
+    activeNavBar: 'search',
+    ...pageData
+  }
+}
+
+module.exports = {
+  go
+}
