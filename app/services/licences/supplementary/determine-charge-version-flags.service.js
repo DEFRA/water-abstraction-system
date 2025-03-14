046786283b85d7d449d613e37523fe662dbcac59
Rebecca Ransome <beckyrose200@aol.com>
2025-03-14 15:38:31 +0000

    Add: New scenarios for determine charge version flags

--- app/services/licences/supplementary/determine-charge-version-flags.service.js
+++ app/services/licences/supplementary/determine-charge-version-flags.service.js
@@ -5,12 +5,9 @@
  * @module DetermineChargeVersionFlagsService
  */
 
-const { ref } = require('objection')
+const { determineCurrentFinancialYear } = require('../../../lib/general.lib.js')
 
-const BillRunModel = require('../../../models/bill-run.model.js')
-const ChargeVersionModel = require('../../../models/charge-version.model.js')
-
-const APRIL = 3
+const FetchChargeVersionBillingDataService = require('./fetch-charge-version-billing-data.service.js')
 
 /**
  * Determines if a licence should be flagged for supplementary billing based on a change in charge version
@@ -29,95 +26,63 @@ const APRIL = 3
  * They are stored in the `LicenceSupplementaryYears` table for each affected year of the charge version
  *
  * @param {string} chargeVersionId - The UUID for the charge version which has changed
- * @param {string} licenceId - The UUID for the licence associated with the charge version
- * @param {Date} changeDate - The date the charge version change starts on
  *
  * @returns {object} - An object containing the related licenceId, regionId, charge version start and end date and
  * licence supplementary billing flags
  */
-async function go(chargeVersionId, licenceId, changeDate) {
-  const changeDateFinancialYearEnd = _determineFinancialYearEnd(new Date(changeDate))
-  const licenceBillRuns = await _fetchLicenceBillRuns(changeDateFinancialYearEnd, licenceId)
+async function go(chargeVersionId) {
+  const { chargeVersion, srocBillRuns } = await FetchChargeVersionBillingDataService.go(chargeVersionId)
+  const { chargeReferences, licence, endDate, startDate, scheme } = chargeVersion
 
-  if (licenceBillRuns.length === 0) {
-    return _accessChargeVersion(chargeVersionId)
-  }
-
-  return _accessBillRuns(licenceBillRuns, licenceId, changeDate)
-}
-
-function _accessBillRuns(licenceBillRuns, licenceId, changeDate) {
-  const flagForPreSrocSupplementary = licenceBillRuns.some((billRun) => {
-    return billRun.scheme === 'alcs'
-  })
-
-  const flagForSrocSupplementary = licenceBillRuns.some((billRun) => {
-    return billRun.scheme === 'sroc' && ['annual', 'supplementary'].includes(billRun.batchType)
-  })
-
-  const flagForTwoPartTariffSupplementary = licenceBillRuns.some((billRun) => {
-    return billRun.scheme === 'sroc' && ['two_part_tariff', 'two_part_supplementary'].includes(billRun.batchType)
-  })
-
-  return {
-    licenceId,
-    startDate: changeDate,
-    endDate: new Date(),
-    regionId: licenceBillRuns[0].regionId,
-    flagForPreSrocSupplementary,
-    flagForSrocSupplementary,
-    flagForTwoPartTariffSupplementary
-  }
-}
-
-async function _accessChargeVersion(chargeVersionId) {
-  const { chargeReferences, licence, endDate, startDate, scheme } = await _fetchChargeVersion(chargeVersionId)
-
-  return {
+  const result = {
     licenceId: licence.id,
-    regionId: licence.regionId,
     startDate,
     endDate,
+    regionId: chargeVersion.licence.regionId,
     flagForPreSrocSupplementary: licence.includeInPresrocBilling === 'yes' || scheme === 'alcs',
-    flagForSrocSupplementary: licence.includeInSrocBilling || scheme === 'sroc',
-    flagForTwoPartTariffSupplementary: _twoPartTariffSrocIndicators(chargeReferences)
-  }
-}
-
-function _determineFinancialYearEnd(date) {
-  let year = date.getFullYear()
-
-  if (date.getMonth() >= APRIL) {
-    year++
+    flagForSrocSupplementary: licence.includeInSrocBilling,
+    flagForTwoPartTariffSupplementary: false
   }
 
-  return year
+  const currentFinancialYear = determineCurrentFinancialYear()
+  const futureChargeVersion = chargeVersion.startDate > currentFinancialYear.endDate
+
+  // If the charge version is pre-sroc, we don’t need to add any sroc or two-part tariff supplementary flags.
+  // If the charge version starts after the current financial year, it’s a future charge version, so supplementary flags
+  // aren’t needed.
+  if (futureChargeVersion || scheme === 'alcs') {
+    return result
+  }
+
+  result.flagForSrocSupplementary = _flagForSrocSupplementary(srocBillRuns, chargeReferences)
+  result.flagForTwoPartTariffSupplementary = _flagForTwoPartTariffSupplementary(srocBillRuns)
+
+  return result
 }
 
-async function _fetchChargeVersion(chargeVersionId) {
-  return ChargeVersionModel.query()
-    .findById(chargeVersionId)
-    .select(['id', 'scheme', 'startDate', 'endDate'])
-    .withGraphFetched('chargeReferences')
-    .modifyGraph('chargeReferences', (builder) => {
-      builder.select(['id', 'scheme', ref('adjustments:s127').castBool().as('twoPartTariff')])
-    })
-    .withGraphFetched('licence')
-    .modifyGraph('licence', (builder) => {
-      builder.select(['id', 'regionId', 'includeInSrocBilling', 'includeInPresrocBilling'])
-    })
+function _flagForSrocSupplementary(srocBillRuns, chargeReferences) {
+  const flagForSrocSupplementary = srocBillRuns.some((billRun) => {
+    return ['annual', 'supplementary'].includes(billRun.batchType)
+  })
+
+  if (flagForSrocSupplementary) {
+    return true
+  }
+  // If there are no charge references attached to the charge version it means its a non-chargeable charge version
+  // So if its not been included in a bill run then we don't need to flag it
+  return chargeReferences.length > 0
 }
 
-async function _fetchLicenceBillRuns(changeDateFinancialYearEnd, licenceId) {
-  return BillRunModel.query()
-    .join('bills', 'bills.billRunId', 'billRuns.id')
-    .join('billLicences', 'billLicences.billId', 'bills.id')
-    .where('billLicences.licenceId', licenceId)
-    .whereIn('billRuns.status', ['sent', 'ready', 'review'])
-    .where('billRuns.toFinancialYearEnding', '>=', changeDateFinancialYearEnd)
-    .select('billRuns.regionId', 'billRuns.scheme', 'billRuns.toFinancialYearEnding', 'billRuns.batchType')
-    .distinct()
-    .orderBy('billRuns.toFinancialYearEnding', 'asc')
+function _flagForTwoPartTariffSupplementary(srocBillRuns, chargeReferences) {
+  const flagForTwoPartTariffSupplementary = srocBillRuns.some((billRun) => {
+    return ['two_part_tariff', 'two_part_supplementary'].includes(billRun.batchType)
+  })
+
+  if (flagForTwoPartTariffSupplementary) {
+    return true
+  }
+
+  return _twoPartTariffSrocIndicators(chargeReferences)
 }
 
 function _twoPartTariffSrocIndicators(chargeReferences) {
