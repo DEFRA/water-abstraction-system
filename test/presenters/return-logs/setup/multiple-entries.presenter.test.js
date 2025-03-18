3afc7e1541b760f08353fb15082baf60dc3074f2
Rebecca Ransome <beckyrose200@aol.com>
2025-03-18 19:24:54 +0000

    Add: Multiple entries presenter tests

---
+++ test/presenters/return-logs/setup/multiple-entries.presenter.test.js
@@ -0,0 +1,125 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const MultipleEntriesPresenter = require('../../../../app/presenters/return-logs/setup/multiple-entries.presenter.js')
+
+describe('Return Logs Setup - Multiple Entries presenter', () => {
+  let session
+
+  beforeEach(() => {
+    session = {
+      id: '61e07498-f309-4829-96a9-72084a54996d',
+      returnReference: '012345',
+      lines: [
+        { startDate: new Date('2023-04-01').toISOString(), endDate: new Date('2023-04-30').toISOString() },
+        { startDate: new Date('2023-05-01').toISOString(), endDate: new Date('2023-05-31').toISOString() }
+      ],
+      returnsFrequency: 'month',
+      reported: 'abstraction-volumes'
+    }
+  })
+
+  describe('when provided with a session', () => {
+    it('correctly presents the data', () => {
+      const result = MultipleEntriesPresenter.go(session)
+
+      expect(result).to.equal({
+        pageTitle: 'Enter multiple monthly volumes',
+        sessionId: '61e07498-f309-4829-96a9-72084a54996d',
+        returnReference: '012345',
+        multipleEntries: null,
+        backLink: '/system/return-logs/setup/61e07498-f309-4829-96a9-72084a54996d/check',
+        lineCount: 2,
+        frequency: 'monthly',
+        measurementType: 'volumes',
+        startDate: '1 April 2023',
+        endDate: '1 May 2023'
+      })
+    })
+  })
+
+  describe('the "measurementType" property', () => {
+    describe('when the user has previously selected "abstraction-volumes" as the reported type', () => {
+      beforeEach(() => {
+        session.reported = 'abstraction-volumes'
+      })
+
+      it('returns "measurementType" back as "volumes"', () => {
+        const result = MultipleEntriesPresenter.go(session)
+
+        expect(result.measurementType).to.equal('volumes')
+      })
+    })
+
+    describe('when the user has previously selected "meter-readings" as the reported type', () => {
+      beforeEach(() => {
+        session.reported = 'meter-readings'
+      })
+
+      it('returns "measurementType" back as "readings"', () => {
+        const result = MultipleEntriesPresenter.go(session)
+
+        expect(result.measurementType).to.equal('readings')
+      })
+    })
+  })
+
+  describe('the "frequency" property', () => {
+    describe('when the return frequency is "day"', () => {
+      beforeEach(() => {
+        session.returnsFrequency = 'day'
+      })
+
+      it('returns "frequency" back as "daily"', () => {
+        const result = MultipleEntriesPresenter.go(session)
+
+        expect(result.frequency).to.equal('daily')
+      })
+    })
+
+    describe('when the return frequency is "week"', () => {
+      beforeEach(() => {
+        session.returnsFrequency = 'week'
+      })
+
+      it('returns "frequency" back as "weekly"', () => {
+        const result = MultipleEntriesPresenter.go(session)
+
+        expect(result.frequency).to.equal('weekly')
+      })
+    })
+
+    describe('when the return frequency is "month"', () => {
+      beforeEach(() => {
+        session.returnsFrequency = 'month'
+      })
+
+      it('returns "frequency" back as "monthly"', () => {
+        const result = MultipleEntriesPresenter.go(session)
+
+        expect(result.frequency).to.equal('monthly')
+      })
+    })
+  })
+
+  describe('the "multipleEntries" property', () => {
+    describe('when the user has previously entered "multiple entries"', () => {
+      beforeEach(() => {
+        session.multipleEntries = '1,2,3,4,5,6,7,8,9,10,11,12'
+      })
+
+      it('returns the "multipleEntries" property populated', () => {
+        const result = MultipleEntriesPresenter.go(session)
+
+        expect(result.multipleEntries).to.equal('1,2,3,4,5,6,7,8,9,10,11,12')
+      })
+    })
+  })
+})
