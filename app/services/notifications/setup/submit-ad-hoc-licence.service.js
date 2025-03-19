81da9975efbb3b080213d82cf6fb84630e5699a3
jonathangoulding <goulding.jon@icloud.com>
2025-03-19 15:17:06 +0000

    Update notifications setup submit ad-hoc licence
    https://eaflood.atlassian.net/browse/WATER-4962
    
    The 'ad-hoc' journey does not exist in the legacy code.
    
    This change updates the notification setup submit ad-hoc- licence service to provide a 'determinedReturnsPeriod' in the session.
    
    This is required to allow the 'ad-hoc' journey to use the existing 'reminders' GOV.UK Notify templates to send the notifications.
    
    A previous change has implemented the ability to send notifications for ad-hoc licences -  https://github.com/DEFRA/water-abstraction-system/pull/1828.

--- app/services/notifications/setup/submit-ad-hoc-licence.service.js
+++ app/services/notifications/setup/submit-ad-hoc-licence.service.js
@@ -10,6 +10,7 @@ const AdHocLicencePresenter = require('../../../presenters/notifications/setup/a
 const ReturnLogModel = require('../../../models/return-log.model.js')
 const SessionModel = require('../../../models/session.model.js')
 const AdHocLicenceValidator = require('../../../validators/notifications/setup/ad-hoc-licence.validator.js')
+const DetermineReturnsPeriodService = require('./determine-returns-period.service.js')
 
 /**
  * Orchestrates validating the data for `/notifications/setup/{sessionId}/ad-hoc-licence` page
@@ -45,6 +46,34 @@ async function go(sessionId, payload) {
   return {}
 }
 
+/**
+ * Ad-hoc licence notifications has highlighted an issue with using the returns period dates.
+ *
+ * This code is intended as a temporary 'fix' to allow ad-hoc returns to use the returns reminders Notify template
+ * (which requires the 'determinedReturnsPeriod' dates)
+ *
+ * This function alters the 'dueDate' but adding 28 days (this may not be the final state and should be regarded as a
+ * placeholder).
+ *
+ * This also, manually, sets gets the 'startDate' and 'endDate' for 'allYear' which represents the current financial
+ * kyear.
+ *
+ * @private
+ */
+function _determinedReturnsPeriod() {
+  const { returnsPeriod, summer } = DetermineReturnsPeriodService.go('allYear')
+
+  const dueDate = new Date()
+  const daysToAdd = 28
+  dueDate.setDate(dueDate.getDate() + daysToAdd)
+
+  return {
+    ...returnsPeriod,
+    summer,
+    dueDate
+  }
+}
+
 async function _dueReturnsExist(licenceRef) {
   const dueReturns = await ReturnLogModel.query().where('licenceRef', licenceRef).where('status', 'due').first()
 
@@ -59,6 +88,8 @@ async function _licenceExists(licenceRef) {
 
 async function _save(session, payload) {
   session.licenceRef = payload.licenceRef
+
+  session.determinedReturnsPeriod = _determinedReturnsPeriod()
 
   return session.$update()
 }
