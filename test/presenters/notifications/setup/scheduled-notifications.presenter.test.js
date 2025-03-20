b3ca1533d19207c792fe145020aaafa24f4fd1a8
jonathangoulding <goulding.jon@icloud.com>
2025-03-20 11:14:24 +0000

    chore: pre pr checks

--- test/presenters/notifications/setup/scheduled-notifications.presenter.test.js
+++ test/presenters/notifications/setup/scheduled-notifications.presenter.test.js
@@ -661,7 +661,7 @@ describe('Notifications Setup - Scheduled Notifications Presenter', () => {
               recipient: 'primary.user@important.com',
               reference: 'TEST-123',
               sendAfter: '2025-01-01T00:00:00.000Z',
-              templateId: 'f1144bc7-8bdc-4e82-87cb-1a6c69445836'
+              templateId: '2fa7fc83-4df1-4f52-bccf-ff0faeb12b6f'
             }
           ])
         })
@@ -696,7 +696,7 @@ describe('Notifications Setup - Scheduled Notifications Presenter', () => {
               recipient: 'returns.agent@important.com',
               reference: 'TEST-123',
               sendAfter: '2025-01-01T00:00:00.000Z',
-              templateId: '038e1807-d1b5-4f09-a5a6-d7eee9030a7a'
+              templateId: '41c45bd4-8225-4d7e-a175-b48b613b5510'
             }
           ])
         })
@@ -731,7 +731,7 @@ describe('Notifications Setup - Scheduled Notifications Presenter', () => {
               recipient: 'primary.user@important.com',
               reference: 'TEST-123',
               sendAfter: '2025-01-01T00:00:00.000Z',
-              templateId: 'f1144bc7-8bdc-4e82-87cb-1a6c69445836'
+              templateId: '2fa7fc83-4df1-4f52-bccf-ff0faeb12b6f'
             }
           ])
         })
@@ -773,7 +773,7 @@ describe('Notifications Setup - Scheduled Notifications Presenter', () => {
               },
               reference: 'TEST-123',
               sendAfter: '2025-01-01T00:00:00.000Z',
-              templateId: 'c01c808b-094b-4a3a-ab9f-a6e86bad36ba'
+              templateId: '4fe80aed-c5dd-44c3-9044-d0289d635019'
             }
           ])
         })
@@ -813,7 +813,7 @@ describe('Notifications Setup - Scheduled Notifications Presenter', () => {
               },
               reference: 'TEST-123',
               sendAfter: '2025-01-01T00:00:00.000Z',
-              templateId: 'e9f132c7-a550-4e18-a5c1-78375f07aa2d'
+              templateId: '0e535549-99a2-44a9-84a7-589b12d00879'
             }
           ])
         })
@@ -853,7 +853,7 @@ describe('Notifications Setup - Scheduled Notifications Presenter', () => {
               },
               reference: 'TEST-123',
               sendAfter: '2025-01-01T00:00:00.000Z',
-              templateId: 'c01c808b-094b-4a3a-ab9f-a6e86bad36ba'
+              templateId: '4fe80aed-c5dd-44c3-9044-d0289d635019'
             }
           ])
         })
