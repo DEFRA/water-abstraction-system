dbe7be2642ae2ba3ec6e3d1f2a446fe1fc960347
jonathangoulding <goulding.jon@icloud.com>
2025-03-21 14:00:24 +0000

    feat: use notify id

--- app/services/notifications/setup/update-notifications.service.js
+++ app/services/notifications/setup/update-notifications.service.js
@@ -14,12 +14,32 @@ const ScheduledNotificationModel = require('../../../../app/models/scheduled-not
  * https://www.postgresql.org/docs/current/postgres-fdw.html#POSTGRES-FDW-OPTIONS-REMOTE-EXECUTION. The calling
  * service will handle the batch process.
  *
+ * We have used an insert with onConflict to allow us to provide an array of notifications to reduce the calls needed to
+ * the database.
+ *
+ * ```javascript
+ * [
+ *   {
+ *     notifyId: '1234',
+ *     notifyStatus: 'received',
+ *     status: 'sent'
+ *   },
+ *   {
+ *     notifyId: '123',
+ *     status: 'sent'
+ *   }
+ * ]
+ * ```
+ *
  * @param {object} notifications
  *
  * @returns {object} - the Updated notifications
  */
 async function go(notifications) {
-  return ScheduledNotificationModel.query().insert(notifications).onConflict('id').merge(['status', 'notifyStatus'])
+  return ScheduledNotificationModel.query()
+    .insert(notifications)
+    .onConflict('notifyId')
+    .merge(['status', 'notifyStatus'])
 }
 
 module.exports = {
