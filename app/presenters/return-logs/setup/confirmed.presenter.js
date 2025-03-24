fbd9510ece886c8714df53b7f405801b57668b6d
Rebecca Ransome <beckyrose200@aol.com>
2025-03-24 14:29:47 +0000

    Add: Confirmed presenter

---
+++ app/presenters/return-logs/setup/confirmed.presenter.js
@@ -0,0 +1,57 @@
+'use strict'
+
+/**
+ * Formats return log data for the `/return-log/setup/confirmed` page
+ * @module ConfirmedPresenter
+ */
+
+/**
+ * Formats return log data for the `/return-log/setup/confirmed` page
+ *
+ * @param {module:ReturnLogModel} returnLog - The return log instance
+ *
+ * @returns {object} page data needed by the view template
+ */
+function go(returnLog) {
+  const { licenceId, licenceRef, purposes, returnReference, siteDescription, status, id } = returnLog
+
+  return {
+    returnLogId: id,
+    licenceId,
+    licenceRef,
+    pageTitle: _pageTitle(returnReference, status),
+    purposeDetails: _purposeDetails(purposes),
+    siteDescription,
+    status
+  }
+}
+
+function _pageTitle(returnReference, status) {
+  if (status === 'received') {
+    return `Return ${returnReference} received`
+  }
+
+  return `Return ${returnReference} submitted`
+}
+
+function _purposeDetails(purposes) {
+  const formattedPurposes = purposes.map((purpose) => {
+    return purpose.tertiary.description
+  })
+
+  if (formattedPurposes.length === 1) {
+    return {
+      label: 'Purpose',
+      value: formattedPurposes[0]
+    }
+  }
+
+  return {
+    label: 'Purposes',
+    value: formattedPurposes.join(', ')
+  }
+}
+
+module.exports = {
+  go
+}
