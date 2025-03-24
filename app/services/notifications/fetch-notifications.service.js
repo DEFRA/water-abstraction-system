5cafe6011ba0da88c0abfdcd88991cce1918c22f
rvsiyad <roblesiyad123@gmail.com>
2025-03-24 11:58:07 +0000

    Add _fetchLicence to fetch notifications service

--- app/services/notifications/fetch-notifications.service.js
+++ app/services/notifications/fetch-notifications.service.js
@@ -5,23 +5,30 @@
  * @module FetchNotificationsService
  */
 
+const LicenceModel = require('../../models/licence.model.js')
 const ScheduledNotificationsModel = require('../../models/scheduled-notification.model.js')
 
 /**
  * Fetches the matching notification and licence data needed for the view
  *
- * @param {string} id - The scheduledNotifications ID
+ * @param {string} notificationId - The UUID for the scheduledNotification
+ * @param {string} licenceId - The UUID for the related licence
  *
  * @returns {Promise<module:ScheduledNotificationsModel>} the matching `ScheduledNotificationsModel` instance and
  * licence data
  */
-async function go(id) {
-  const notification = await _fetch(id)
+async function go(notificationId, licenceId) {
+  const licence = await _fetchLicence(licenceId)
+  const notification = await _fetchNotification(notificationId)
 
-  return notification
+  return { licence, notification }
 }
 
-async function _fetch(id) {
+async function _fetchLicence(licenceId) {
+  return LicenceModel.query().findById(licenceId).select('id', 'licenceRef')
+}
+
+async function _fetchNotification(notificationId) {
   return ScheduledNotificationsModel.query()
     .findById(id)
     .select(['messageType', 'messageRef', 'personalisation', 'plaintext'])
