cf035050bf016c87bed50ab14ff7fbffe716e360
Rebecca Ransome <beckyrose200@aol.com>
2025-03-24 15:12:12 +0000

    Add: Unit tests for new services

---
+++ test/presenters/return-logs/setup/confirmed.presenter.test.js
@@ -0,0 +1,109 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const ConfirmedPresenter = require('../../../../app/presenters/return-logs/setup/confirmed.presenter.js')
+
+describe('Return Logs - Setup - Confirmed presenter', () => {
+  let returnLog
+
+  beforeEach(() => {
+    returnLog = {
+      licenceId: '91aff99a-3204-4727-86bd-7bdf3ef24533',
+      licenceRef: '01/117',
+      returnLogId: 'v1:6:01/117:10032788:2019-04-01:2019-05-12',
+      returnReference: '10032788',
+      status: 'received',
+      purposes: [
+        {
+          alias: 'SPRAY IRRIGATION',
+          primary: {
+            code: 'I',
+            description: 'Industrial, Commercial And Public Services'
+          },
+          tertiary: {
+            code: '400',
+            description: 'Spray Irrigation - Direct'
+          },
+          secondary: {
+            code: 'GOF',
+            description: 'Golf Courses'
+          }
+        }
+      ],
+      siteDescription: 'Addington Sandpits'
+    }
+  })
+
+  describe('when provided with a return log', () => {
+    it('correctly presents the data', () => {
+      const result = ConfirmedPresenter.go(returnLog)
+
+      expect(result).to.equal({
+        licenceId: '91aff99a-3204-4727-86bd-7bdf3ef24533',
+        licenceRef: '01/117',
+        returnLogId: 'v1:6:01/117:10032788:2019-04-01:2019-05-12',
+        pageTitle: 'Return 10032788 received',
+        purposeDetails: {
+          label: 'Purpose',
+          value: 'Spray Irrigation - Direct'
+        },
+        siteDescription: 'Addington Sandpits',
+        status: 'received'
+      })
+    })
+  })
+
+  describe('the "pageTitle" property', () => {
+    describe('when the return log has a status of received', () => {
+      it('returns pageTitle as "Return received"', () => {
+        const result = ConfirmedPresenter.go(returnLog)
+
+        expect(result.pageTitle).to.equal('Return 10032788 received')
+      })
+    })
+
+    describe('when the return log does not have a status of received', () => {
+      beforeEach(() => {
+        returnLog.status = 'completed'
+      })
+
+      it('returns pageTitle as "Return submitted"', () => {
+        const result = ConfirmedPresenter.go(returnLog)
+
+        expect(result.pageTitle).to.equal('Return 10032788 submitted')
+      })
+    })
+  })
+
+  describe('the "purposeDetails" property', () => {
+    describe('when the return log has a single purpose', () => {
+      it('returns an object with a label property set to "Purpose" and a value property containing the single purpose', () => {
+        const result = ConfirmedPresenter.go(returnLog)
+
+        expect(result.purposeDetails).to.equal({ label: 'Purpose', value: 'Spray Irrigation - Direct' })
+      })
+    })
+
+    describe('when the return log session has multiple purposes', () => {
+      beforeEach(() => {
+        returnLog.purposes.push({ tertiary: { code: '420', description: 'Spray Irrigation - Storage' } })
+      })
+
+      it('returns an object with a label property set to "Purposes" and a value property containing the comma separated purposes as a string', () => {
+        const result = ConfirmedPresenter.go(returnLog)
+
+        expect(result.purposeDetails).to.equal({
+          label: 'Purposes',
+          value: 'Spray Irrigation - Direct, Spray Irrigation - Storage'
+        })
+      })
+    })
+  })
+})
---
+++ test/services/return-logs/setup/submit-confirmed.service.test.js
@@ -0,0 +1,46 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const LicenceHelper = require('../../../support/helpers/licence.helper.js')
+const ReturnLogHelper = require('../../../support/helpers/return-log.helper.js')
+
+// Things we need to stub
+const ProcessBillingFlagService = require('../../../../app/services/licences/supplementary/process-billing-flag.service.js')
+
+// Thing under test
+const SubmitConfirmedService = require('../../../../app/services/return-logs/setup/submit-confirmed.service.js')
+
+describe('Return Logs Setup - Submit Confirmed service', () => {
+  let licence
+  let returnLog
+
+  beforeEach(async () => {
+    licence = await LicenceHelper.add()
+
+    returnLog = await ReturnLogHelper.add({ licenceRef: licence.licenceRef })
+
+    Sinon.stub(ProcessBillingFlagService, 'go').resolves()
+  })
+
+  describe('when a user submits the confirmed return to be marked for supplementary billing', () => {
+    it('returns the licenceId for the redirect', async () => {
+      const result = await SubmitConfirmedService.go(returnLog.id)
+
+      expect(result).to.equal(licence.id)
+    })
+
+    it('sends the return to be processed by the "processBillingFlagsService"', async () => {
+      await SubmitConfirmedService.go(returnLog.id)
+
+      expect(ProcessBillingFlagService.called).to.be.true()
+    })
+  })
+})
