853cea0b9e831cbc95e0c88f9d0ceec016148cb9
jonathangoulding <goulding.jon@icloud.com>
2025-03-26 16:01:44 +0000

    chore: pre pr checks

--- app/services/notifications/setup/update-notifications.service.js
+++ app/services/notifications/setup/update-notifications.service.js
@@ -18,7 +18,6 @@ const ScheduledNotificationModel = require('../../../../app/models/scheduled-not
  *
  */
 async function go(notifications) {
-  // update update at ?
   await ScheduledNotificationModel.query().insert(notifications).onConflict('id').merge(['status', 'notifyStatus'])
 }
 
