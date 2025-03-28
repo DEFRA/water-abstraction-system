3304cb4cef329e3c29cb89e9a926827959b1e54a
jonathangoulding <goulding.jon@icloud.com>
2025-03-28 14:10:06 +0000

    Update notifications status update to batch scheduled notifications
    https://eaflood.atlassian.net/browse/WATER-4961
    
    There was a previous assumption to use the scheduled notifications event as part of the batch process. This idea introduced some complex logic to handle counting how many request we have made to Notify.
    
    This change updates the existing fetch logic to return an array of scheduled notifications rather than an event with scheduled notifications.
    
    This allows the batch mechanism to function the same as it does in other places.
    
    This change also updates the notification status update service to use the batch mechanism previously mentioned.
    
    As we are using an upsert style query (for batch inserting) the 'water.scheduledNotifications' schema wants us to provide a 'createdAt' even though all our 'scheduledNotifications' should have a 'createdAt' date.
    
    We should not be creating any new 'scheduledNotifications' just updating them. So we provide the 'createdAt' date to be reinserted.

---
+++ app/services/jobs/notifications/fetch-scheduled-notifications.service.js
@@ -0,0 +1,34 @@
+'use strict'
+
+/**
+ * Fetches the scheduled notifications with the status 'sending'.
+ * @module FetchScheduledNotificationsService
+ */
+
+const ScheduledNotificationModel = require('../../../models/scheduled-notification.model.js')
+
+const SEVEN_DAYS = 7
+
+/**
+ * Fetches the scheduled notifications with the status 'sending'.
+ *
+ * The function returns a list of 'scheduledNotifications' that have a status of 'sending' and are lees than 7 days old.
+ *
+ * Notify has a retention period of 7 days, so we only want 'scheduledNotifications' created within the last 7 days.
+ *
+ * @returns {Promise<object[]>} - an array of 'scheduledNotifications'
+ */
+async function go() {
+  const today = new Date()
+  const sevenDaysAgo = new Date()
+  sevenDaysAgo.setDate(today.getDate() - SEVEN_DAYS)
+
+  return ScheduledNotificationModel.query()
+    .select(['id', 'notifyId', 'status', 'notifyStatus', 'log', 'eventId', 'createdAt'])
+    .where('status', 'sending')
+    .andWhere('createdAt', '>=', sevenDaysAgo)
+}
+
+module.exports = {
+  go
+}
