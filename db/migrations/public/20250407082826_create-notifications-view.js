2889ee00221a1c89b674d2d3c2f348d340aa5913
jonathangoulding <goulding.jon@icloud.com>
2025-04-07 09:39:49 +0000

    Add notification view and model https://eaflood.atlassian.net/browse/WATER-4961
    We have previously used a 'scheduledNotifications' view for the 'water.scheduledNotifications' table.
    We have decided to move to a new view called 'notifications' this is, simply, because we do not schedule notifications to be sent, we just send them.
    
    This change adds the 'notifications' table view, with the associated model, test and test helper.

---
+++ db/migrations/public/20250407082826_create-notifications-view.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const viewName = 'notifications'
+
+exports.up = function (knex) {
+  return knex.schema.createView(viewName, (view) => {
+    // NOTE: We have commented out unused columns from the source table
+    view.as(
+      knex('scheduled_notification').withSchema('water').select([
+        'id',
+        'recipient',
+        'message_type',
+        'message_ref',
+        'personalisation',
+        // 'send_after',
+        'status',
+        'log AS notify_error_message',
+        'licences',
+        // 'individual_entity_id AS individual_id',
+        // 'company_entity_id AS company_id',
+        // 'medium',
+        'notify_id',
+        'notify_status',
+        'plaintext',
+        'event_id',
+        // 'metadata',
+        // 'status_checks',
+        // 'next_status_check',
+        // 'notification_type',
+        // 'job_id',
+        'date_created AS created_at'
+      ])
+    )
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropViewIfExists(viewName)
+}
---
+++ test/models/notification.model.test.js
@@ -0,0 +1,61 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const EventHelper = require('../support/helpers/event.helper.js')
+const EventModel = require('../../app/models/event.model.js')
+const NotificationHelper = require('../support/helpers/notification.helper.js')
+
+// Thing under test
+const NotificationModel = require('../../app/models/notification.model.js')
+
+describe.only('Notification model', () => {
+  let testRecord
+
+  describe('Basic query', () => {
+    beforeEach(async () => {
+      testRecord = await NotificationHelper.add()
+    })
+
+    it('can successfully run a basic query', async () => {
+      const result = await NotificationModel.query().findById(testRecord.id)
+
+      expect(result).to.be.an.instanceOf(NotificationModel)
+      expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to event', () => {
+      let testEvent
+
+      beforeEach(async () => {
+        testEvent = await EventHelper.add()
+
+        testRecord = await NotificationHelper.add({ eventId: testEvent.id })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await NotificationModel.query().innerJoinRelated('event')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the event', async () => {
+        const result = await NotificationModel.query().findById(testRecord.id).withGraphFetched('event')
+
+        expect(result).to.be.instanceOf(NotificationModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.event).to.be.an.instanceOf(EventModel)
+        expect(result.event).to.include(testEvent)
+      })
+    })
+  })
+})
