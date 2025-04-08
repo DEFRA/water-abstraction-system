4a6dfe6af0188aabfa4ec7b329c2faa9928d85e7
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-04-08 09:00:06 +0100

    Split returns view feature toggle into view & edit (#1876)
    https://eaflood.atlassian.net/browse/WATER-4898
    https://eaflood.atlassian.net/browse/WATER-4836
    
    We've been working on replacing the 'view return log page' and submitting/editing a return. We put access to this new functionality behind the feature toggle `ENABLE_SYSTEM_RETURNS_VIEW`.
    
    However, we're now at a point where the new 'view return log' is done, but the submit/edit is still being worked on. Ideally, we'd like to make the new view available but continue to hide our new edit/submit journey.
    
    To make this happen, we must split what `ENABLE_SYSTEM_RETURNS_VIEW` protects. We'll keep it, but now it only controls access to the new 'view return log' page. We'll add a new `ENABLE_SYSTEM_RETURNS_SUBMIT` to control access to our new submit/edit journey.

--- test/services/return-logs/setup/initiate-session.service.test.js
+++ test/services/return-logs/setup/initiate-session.service.test.js
@@ -3,8 +3,9 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
-const { describe, it, before } = (exports.lab = Lab.script())
+const { describe, it, before, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
@@ -13,6 +14,9 @@ const ReturnLogHelper = require('../../../support/helpers/return-log.helper.js')
 const ReturnSubmissionHelper = require('../../../support/helpers/return-submission.helper.js')
 const ReturnSubmissionLineHelper = require('../../../support/helpers/return-submission-line.helper.js')
 const SessionModel = require('../../../../app/models/session.model.js')
+
+// Things we need to stub
+const FeatureFlagsConfig = require('../../../../config/feature-flags.config.js')
 
 // Thing under test
 const InitiateSessionService = require('../../../../app/services/return-logs/setup/initiate-session.service.js')
@@ -46,6 +50,10 @@ describe('Return Logs - Setup - Initiate Session service', () => {
     licence = await LicenceHelper.add()
   })
 
+  afterEach(() => {
+    Sinon.restore()
+  })
+
   describe('when the return log has been received and submitted', () => {
     let returnLog
     let returnSubmission
@@ -63,6 +71,11 @@ describe('Return Logs - Setup - Initiate Session service', () => {
         metadata: { method: 'abstractionVolumes' }
       })
       await ReturnSubmissionLineHelper.add({ returnSubmissionId: returnSubmission.id })
+    })
+
+    beforeEach(() => {
+      // Enable our new submit/edit journey
+      Sinon.stub(FeatureFlagsConfig, 'enableSystemReturnsSubmit').value(true)
     })
 
     it('creates a new session record containing details of the return log', async () => {
@@ -265,6 +278,11 @@ describe('Return Logs - Setup - Initiate Session service', () => {
       })
     })
 
+    beforeEach(() => {
+      // Enable our new submit/edit journey
+      Sinon.stub(FeatureFlagsConfig, 'enableSystemReturnsSubmit').value(true)
+    })
+
     it('sets beenReceived to true', async () => {
       const result = await InitiateSessionService.go(returnLog.id)
 
@@ -307,6 +325,11 @@ describe('Return Logs - Setup - Initiate Session service', () => {
       })
     })
 
+    beforeEach(() => {
+      // Enable our new submit/edit journey
+      Sinon.stub(FeatureFlagsConfig, 'enableSystemReturnsSubmit').value(true)
+    })
+
     it('sets beenReceived to false', async () => {
       const result = await InitiateSessionService.go(returnLog.id)
 
@@ -339,6 +362,23 @@ describe('Return Logs - Setup - Initiate Session service', () => {
         'startReading',
         'units'
       ])
+    })
+  })
+
+  describe('when the system submit/edit feature is not enabled', () => {
+    let returnLog
+
+    beforeEach(() => {
+      // Disable our new submit/edit journey
+      Sinon.stub(FeatureFlagsConfig, 'enableSystemReturnsSubmit').value(false)
+
+      returnLog = { id: ReturnLogHelper.generateReturnLogId() }
+    })
+
+    it('the redirect URL it returns points to the legacy edit page', async () => {
+      const result = await InitiateSessionService.go(returnLog.id)
+
+      expect(result).to.equal(`/return/internal?returnId=${returnLog.id}`)
     })
   })
 })
