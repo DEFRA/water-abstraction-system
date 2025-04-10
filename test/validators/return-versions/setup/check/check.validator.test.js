7150ce85eb4e6b0c2ae31c48d7a0004575b3d91d
rvsiyad <roblesiyad123@gmail.com>
2025-04-10 13:23:40 +0100

    Refactor check validator test to validate against requirements

--- test/validators/return-versions/setup/check/check.validator.test.js
+++ test/validators/return-versions/setup/check/check.validator.test.js
@@ -4,18 +4,22 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = (exports.lab = Lab.script())
+const { describe, it, before } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Thing under test
 const CheckValidator = require('../../../../../app/validators/return-versions/setup/check/check.validator.js')
 
 describe('Check validator', () => {
-  describe('when valid data is provided', () => {
-    const returnCycles = ['winter-and-all-year', 'winter-and-all-year', 'winter-and-all-year']
+  let requirements
 
+  before(() => {
+    requirements = testRequirements()
+  })
+
+  describe('when valid data is provided', () => {
     it('confirms the data is valid', () => {
-      const result = CheckValidator.go(returnCycles)
+      const result = CheckValidator.go(requirements)
 
       expect(result.value).to.exist()
       expect(result.error).not.to.exist()
@@ -23,10 +27,12 @@ describe('Check validator', () => {
   })
 
   describe('when invalid data is provided', () => {
-    const returnCycles = ['summer', 'winter-and-all-year', 'winter-and-all-year']
+    before(() => {
+      requirements[0].returnsCycle = 'summer'
+    })
 
     it('it fails validation', () => {
-      const result = CheckValidator.go(returnCycles)
+      const result = CheckValidator.go(requirements)
 
       expect(result.value).to.exist()
       expect(result.error).to.exist()
@@ -36,3 +42,38 @@ describe('Check validator', () => {
     })
   })
 })
+
+function testRequirements() {
+  return [
+    {
+      points: ['9a1a0916-f45e-4e86-ab7a-8d9c773699b3'],
+      purposes: [],
+      returnsCycle: 'winter-and-all-year',
+      siteDescription: 'Addington Sandpits',
+      abstractionPeriod: {
+        'end-abstraction-period-day': 31,
+        'end-abstraction-period-month': 12,
+        'start-abstraction-period-day': 1,
+        'start-abstraction-period-month': 1
+      },
+      frequencyReported: 'month',
+      frequencyCollected: 'month',
+      agreementsExceptions: ['none']
+    },
+    {
+      points: ['0f3da7ba-86d1-4360-8705-c7d85e2f6549'],
+      purposes: [],
+      returnsCycle: 'winter-and-all-year',
+      siteDescription: 'Addington Sandpits',
+      abstractionPeriod: {
+        'end-abstraction-period-day': 27,
+        'end-abstraction-period-month': 1,
+        'start-abstraction-period-day': 1,
+        'start-abstraction-period-month': 12
+      },
+      frequencyReported: 'week',
+      frequencyCollected: 'week',
+      agreementsExceptions: ['none']
+    }
+  ]
+}
