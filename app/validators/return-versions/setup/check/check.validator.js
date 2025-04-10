93fdde00653d6874f09957d28768e1092012682d
rvsiyad <roblesiyad123@gmail.com>
2025-04-10 13:08:31 +0100

    Refactor check validator to validate the requirements

--- app/validators/return-versions/setup/check/check.validator.js
+++ app/validators/return-versions/setup/check/check.validator.js
@@ -1,30 +1,34 @@
 'use strict'
 
 /**
- * Validates data submitted for the `/return-requirements/{sessionId}/check` page
+ * Validates data submitted for the check page
  * @module CheckValidator
  */
 
 const Joi = require('joi')
 
 /**
- * Validates data submitted for the `/return-requirements/{sessionId}/check` page
+ * Validates data submitted for the check page
  *
  * When submitting requirements for returns, users can specify if the return is a quarterly return submission. If a user
  * defines the return as a quarterly return submission, the requirements associated cannot contain a summer return
  * cycle. If these conditions are not met, the validation will return an error.
  *
- * @param {Array} returnCycles - The returns cycles from the requirements for returns
+ * @param {Array} requirements - The requirements for returns
  *
  * @returns {object} The result from calling Joi's schema.validate(). If any errors are found the
  * `error:` property will also exist detailing what the issue is.
  */
-function go(returnCycles) {
+function go(requirements) {
   const errorMessage = "Quarterly returns submissions can't be set for returns in the summer cycle."
 
-  const schema = Joi.array().items(Joi.string().valid('winter-and-all-year')).messages({ 'any.only': errorMessage })
+  const schema = Joi.array().items(
+    Joi.object({
+      returnsCycle: Joi.string().valid('winter-and-all-year').messages({ 'any.only': errorMessage })
+    }).unknown(true)
+  )
 
-  return schema.validate(returnCycles, { abortEarly: true })
+  return schema.validate(requirements, { abortEarly: true })
 }
 
 module.exports = {
