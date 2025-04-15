b0c22aeff5a4b4f3383bb242b5ac4d02148a1a04
Roble <roblesiyad123@gmail.com>
2025-04-15 14:34:02 +0100

    Prevent requirements marked summer & quarterly submission (#1872)
    * Prevent requirements marked summer & quarterly submission
    
    https://eaflood.atlassian.net/browse/WATER-4955
    
    Quarterly return submissions can only be approved for licences with return in a winter/all year return cycle only.
    Currently, when a return requirement with a summer cycle is selected for quarterly submissions, the system does not prevent the approval of the return.
    We want to prevent this combination from being approved for returns.
    
    This PR will implement validation preventing approval of requirement for return for summer cycle and quarterly submission.

--- app/services/return-versions/setup/check/submit-check.service.js
+++ app/services/return-versions/setup/check/submit-check.service.js
@@ -5,11 +5,17 @@
  * @module SubmitCheckService
  */
 
+const CheckPresenter = require('../../../../presenters/return-versions/setup/check/check.presenter.js')
+const CheckValidation = require('../../../../validators/return-versions/setup/check/check.validator.js')
+const FetchPointsService = require('../fetch-points.service.js')
 const GenerateReturnVersionService = require('./generate-return-version.service.js')
 const PersistReturnVersionService = require('./persist-return-version.service.js')
 const ProcessLicenceReturnLogsService = require('../../../return-logs/process-licence-return-logs.service.js')
+const ReturnRequirementsPresenter = require('../../../../presenters/return-versions/setup/check/returns-requirements.presenter.js')
 const SessionModel = require('../../../../models/session.model.js')
 const VoidReturnLogsService = require('../../../return-logs/void-return-logs.service.js')
+
+const ONE_DAY_IN_MILLISECONDS = 24 * 60 * 60 * 1000
 
 /**
  * Manages converting the session data to return requirement records when check return requirements is confirmed
@@ -22,18 +28,40 @@ const VoidReturnLogsService = require('../../../return-logs/void-return-logs.ser
  * @param {string} sessionId - The UUID for return requirement setup session record
  * @param {number} userId - The id of the logged in user
  *
- * @returns {string} The licence ID
+ * @returns {Promise<object>} An object containing the licenceId if there are no errors else the page data for the
+ * check page including the validation error details
  */
 async function go(sessionId, userId) {
   const session = await SessionModel.query().findById(sessionId)
 
+  const validationResult = _validate(session)
+
+  if (!validationResult) {
+    await _processReturnVersion(session, userId)
+
+    await SessionModel.query().deleteById(sessionId)
+
+    return { licenceId: session.licence.id }
+  }
+
+  const returnRequirements = await _returnRequirements(session)
+  const submittedSessionData = CheckPresenter.go(session)
+
+  return {
+    activeNavBar: 'search',
+    error: validationResult,
+    ...returnRequirements,
+    ...submittedSessionData
+  }
+}
+
+async function _processReturnVersion(session, userId) {
   const returnVersionData = await GenerateReturnVersionService.go(session.data, userId)
 
   await PersistReturnVersionService.go(returnVersionData)
 
-  const oneDayInMilliseconds = 24 * 60 * 60 * 1000
   const changeDate = new Date(returnVersionData.returnVersion.startDate)
-  changeDate.setTime(changeDate.getTime() - oneDayInMilliseconds)
+  changeDate.setTime(changeDate.getTime() - ONE_DAY_IN_MILLISECONDS)
 
   if (session.data.journey === 'no-returns-required') {
     await VoidReturnLogsService.go(
@@ -44,10 +72,34 @@ async function go(sessionId, userId) {
   }
 
   await ProcessLicenceReturnLogsService.go(returnVersionData.returnVersion.licenceId, changeDate)
+}
 
-  await SessionModel.query().deleteById(sessionId)
+async function _returnRequirements(session) {
+  const { licence, requirements, journey } = session
 
-  return session.licence.id
+  const points = await FetchPointsService.go(licence.id)
+
+  return ReturnRequirementsPresenter.go(requirements, points, journey)
+}
+
+function _validate(session) {
+  const { requirements, quarterlyReturns } = session
+
+  if (!quarterlyReturns) {
+    return null
+  }
+
+  const validation = CheckValidation.go(requirements)
+
+  if (!validation.error) {
+    return null
+  }
+
+  const { message } = validation.error.details[0]
+
+  return {
+    text: message
+  }
 }
 
 module.exports = {
