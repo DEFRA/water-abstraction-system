47d4256768be7665baf16fef1721f06e976eb231
Jason Claxton <jason.claxton@defra.gov.uk>
2025-04-17 10:54:11 +0100

    Fix another comment

--- app/services/return-logs/setup/submit-readings.service.js
+++ app/services/return-logs/setup/submit-readings.service.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * Orchestrates validating the data for `/return-logs/setup/{sessionId}/readings/{yearMonth}` page
+ * Orchestrates validating the data for the `/return-logs/setup/{sessionId}/readings/{yearMonth}` page
  * @module SubmitReadingsService
  */
 
@@ -11,7 +11,7 @@ const ReadingsValidator = require('../../../validators/return-logs/setup/reading
 const SessionModel = require('../../../models/session.model.js')
 
 /**
- * Orchestrates validating the data for `/return-logs/setup/{sessionId}/readings/{yearMonth}` page
+ * Orchestrates validating the data for the `/return-logs/setup/{sessionId}/readings/{yearMonth}` page
  *
  * @param {string} sessionId - The UUID of the current session
  * @param {object} payload - The submitted form data
