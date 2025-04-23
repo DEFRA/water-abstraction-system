ae050615c82f5edd21d832bcb0efb456737a7b25
rvsiyad <roblesiyad123@gmail.com>
2025-04-23 16:27:55 +0100

    Add tests for preventing summer selection when return is marked for quarterly submission

--- test/validators/return-versions/setup/returns-cycle.validator.test.js
+++ test/validators/return-versions/setup/returns-cycle.validator.test.js
@@ -4,7 +4,7 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, before } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Thing under test
@@ -12,44 +12,106 @@ const ReturnsCycleValidator = require('../../../../app/validators/return-version
 
 describe('Return Versions Setup - Returns Cycle validator', () => {
   let payload
+  let session
+
+  before(() => {
+    payload = {
+      returnsCycle: 'winter-and-all-year'
+    }
+
+    session = _testSession()
+  })
 
   describe('when valid data is provided', () => {
-    beforeEach(() => {
-      payload = {
-        returnsCycle: 'winter-and-all-year'
-      }
-    })
-
     it('confirms the data is valid', async () => {
-      const result = ReturnsCycleValidator.go(payload)
+      const result = ReturnsCycleValidator.go(payload, session)
 
       expect(result.value.returnsCycle).to.exist()
     })
   })
 
   describe('when invalid data is provided', () => {
-    beforeEach(() => {
+    before(() => {
       payload = {
         returnsCycle: 'ABC123'
       }
     })
 
-    it('fails validation', () => {
-      const result = ReturnsCycleValidator.go(payload)
+    it('fails validation with the error message "Select the returns cycle for the requirements for returns"', () => {
+      const result = ReturnsCycleValidator.go(payload, session)
 
       expect(result.error.details[0].message).to.equal('Select the returns cycle for the requirements for returns')
     })
   })
 
+  describe('when a user selects summer but quarterly returns is marked true', () => {
+    before(() => {
+      payload = {
+        returnsCycle: 'summer'
+      }
+
+      session.data.quarterlyReturns = true
+    })
+
+    it("fails validation with the error message 'Quarterly returns submissions can't be set for returns in the summer cycle'", () => {
+      const result = ReturnsCycleValidator.go(payload, session)
+
+      expect(result.error.details[0].message).to.equal(
+        "Quarterly returns submissions can't be set for returns in the summer cycle"
+      )
+    })
+  })
+
   describe('when no data is provided', () => {
-    beforeEach(() => {
+    before(() => {
       payload = {}
     })
 
     it('fails validation', () => {
-      const result = ReturnsCycleValidator.go(payload)
+      const result = ReturnsCycleValidator.go(payload, session)
 
       expect(result.error.details[0].message).to.equal('Select the returns cycle for the requirements for returns')
     })
   })
 })
+
+function _testSession() {
+  return {
+    id: 'bbfb8568-cd71-4ca1-b56b-2d50dae2b2b9',
+    data: {
+      method: 'use-existing-requirements',
+      reason: 'new-licence',
+      journey: 'returns-required',
+      licence: {
+        id: '91aff99a-3204-4727-86bd-7bdf3ef24533',
+        endDate: null,
+        startDate: '1992-08-19T00:00:00.000Z',
+        licenceRef: '01/117',
+        licenceHolder: 'Ferns Surfacing Limited',
+        returnVersions: [],
+        waterUndertaker: false,
+        currentVersionStartDate: '2019-05-13T00:00:00.000Z'
+      },
+      requirements: [
+        {
+          points: [],
+          purposes: [],
+          returnsCycle: 'winter-and-all-year',
+          siteDescription: 'POINT A, ADDINGTON SANDPITS',
+          abstractionPeriod: [],
+          frequencyReported: 'month',
+          frequencyCollected: 'month',
+          agreementsExceptions: []
+        }
+      ],
+      startDateDay: '28',
+      startDateYear: '2025',
+      multipleUpload: false,
+      startDateMonth: '12',
+      checkPageVisited: true,
+      quarterlyReturns: false,
+      startDateOptions: 'anotherStartDate',
+      returnVersionStartDate: '2025-12-28T00:00:00.000Z'
+    }
+  }
+}
