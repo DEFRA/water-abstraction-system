921196f9f377f3f80d4f1415c82f8d48b508d708
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-04-24 23:30:04 +0100

    Remove the stubbing duplication in the tests

--- test/services/jobs/return-logs/check-return-cycle.service.test.js
+++ test/services/jobs/return-logs/check-return-cycle.service.test.js
@@ -5,7 +5,7 @@ const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 const Sinon = require('sinon')
 
-const { describe, it, before, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Things to stub
@@ -14,14 +14,31 @@ const ReturnCycleModel = require('../../../../app/models/return-cycle.model.js')
 // Thing under test
 const CheckReturnCycleService = require('../../../../app/services/jobs/return-logs/check-return-cycle.service.js')
 
-describe('Jobs - Return Logs - Check Return Cycle service', () => {
+describe('Return Logs - Check Return Cycle service', () => {
   const currentDate = new Date('2024-05-01')
+  const id = '0055799f-8b6a-4753-ac78-57c61a6ef80b'
 
   let clock
+  let cycleData
+  let firstStub
   let insertStub
+  let returningStub
   let summer
 
   beforeEach(() => {
+    firstStub = Sinon.stub()
+    insertStub = Sinon.stub().returnsThis()
+    returningStub = Sinon.stub()
+
+    Sinon.stub(ReturnCycleModel, 'query').returns({
+      select: Sinon.stub().returnsThis(),
+      where: Sinon.stub().returnsThis(),
+      limit: Sinon.stub().returnsThis(),
+      first: firstStub,
+      returning: returningStub,
+      insert: insertStub
+    })
+
     clock = Sinon.useFakeTimers(currentDate)
   })
 
@@ -32,29 +49,23 @@ describe('Jobs - Return Logs - Check Return Cycle service', () => {
 
   describe('when summer is true', () => {
     beforeEach(() => {
+      cycleData = {
+        id,
+        dueDate: new Date('2024-11-28'),
+        endDate: new Date('2024-10-31'),
+        startDate: new Date('2023-11-01'),
+        summer: true
+      }
       summer = true
     })
 
     describe('and there is no matching return cycle for the change date', () => {
-      before(() => {
-        insertStub = Sinon.stub().returnsThis()
-        Sinon.stub(ReturnCycleModel, 'query').returns({
-          select: Sinon.stub().returnsThis(),
-          where: Sinon.stub().returnsThis(),
-          limit: Sinon.stub().returnsThis(),
-          first: Sinon.stub().resolves(undefined),
-          returning: Sinon.stub().resolves({
-            id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-            dueDate: new Date('2024-11-28T00:00:00.000Z'),
-            endDate: new Date('2024-10-31T00:00:00.000Z'),
-            startDate: new Date('2023-11-01T00:00:00.000Z'),
-            summer: true
-          }),
-          insert: insertStub
-        })
+      beforeEach(() => {
+        firstStub.resolves(undefined)
+        returningStub.resolves(cycleData)
       })
 
-      it('creates summer return cycle', async () => {
+      it('creates and then returns the new summer return cycle', async () => {
         const result = await CheckReturnCycleService.go(summer)
 
         const [insertObject] = insertStub.args[0]
@@ -62,81 +73,50 @@ describe('Jobs - Return Logs - Check Return Cycle service', () => {
         expect(insertStub.callCount).to.equal(1)
         expect(insertObject).to.equal(
           {
-            dueDate: new Date('2024-11-28T00:00:00.000Z'),
-            endDate: new Date('2024-10-31T00:00:00.000Z'),
-            startDate: new Date('2023-11-01T00:00:00.000Z'),
+            dueDate: cycleData.dueDate,
+            endDate: cycleData.endDate,
+            startDate: cycleData.startDate,
             summer: true,
             submittedInWrls: true
           },
           { skip: ['createdAt', 'updatedAt'] }
         )
-        expect(result).to.equal({
-          id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-          dueDate: new Date('2024-11-28T00:00:00.000Z'),
-          endDate: new Date('2024-10-31T00:00:00.000Z'),
-          startDate: new Date('2023-11-01T00:00:00.000Z'),
-          summer: true
-        })
+        expect(result).to.equal(cycleData)
       })
     })
 
     describe('and there is a matching summer return cycle for the change date', () => {
-      before(() => {
-        insertStub = Sinon.stub().returnsThis()
-        Sinon.stub(ReturnCycleModel, 'query').returns({
-          select: Sinon.stub().returnsThis(),
-          where: Sinon.stub().returnsThis(),
-          limit: Sinon.stub().returnsThis(),
-          first: Sinon.stub().resolves({
-            id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-            dueDate: new Date('2024-11-28T00:00:00.000Z'),
-            endDate: new Date('2024-10-31T00:00:00.000Z'),
-            startDate: new Date('2023-11-01T00:00:00.000Z'),
-            summer: true
-          }),
-          insert: insertStub
-        })
+      beforeEach(() => {
+        firstStub.resolves(cycleData)
       })
 
-      it('returns the summer cycles', async () => {
+      it('returns the matching summer cycle', async () => {
         const result = await CheckReturnCycleService.go(summer)
 
-        expect(result).to.equal({
-          id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-          dueDate: new Date('2024-11-28T00:00:00.000Z'),
-          endDate: new Date('2024-10-31T00:00:00.000Z'),
-          startDate: new Date('2023-11-01T00:00:00.000Z'),
-          summer: true
-        })
+        expect(result).to.equal(cycleData)
       })
     })
   })
 
   describe('when summer is false', () => {
     beforeEach(() => {
+      cycleData = {
+        id,
+        dueDate: new Date('2025-04-28'),
+        endDate: new Date('2025-03-31'),
+        startDate: new Date('2024-04-01'),
+        summer: false
+      }
       summer = false
     })
 
     describe('and there is no matching return cycle for the change date', () => {
-      before(() => {
-        insertStub = Sinon.stub().returnsThis()
-        Sinon.stub(ReturnCycleModel, 'query').returns({
-          select: Sinon.stub().returnsThis(),
-          where: Sinon.stub().returnsThis(),
-          limit: Sinon.stub().returnsThis(),
-          first: Sinon.stub().resolves(undefined),
-          returning: Sinon.stub().resolves({
-            id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-            dueDate: new Date('2025-04-28T00:00:00.000Z'),
-            endDate: new Date('2025-03-31T00:00:00.000Z'),
-            startDate: new Date('2024-04-01T00:00:00.000Z'),
-            summer: false
-          }),
-          insert: insertStub
-        })
+      beforeEach(() => {
+        firstStub.resolves(undefined)
+        returningStub.resolves(cycleData)
       })
 
-      it('creates all year return cycle', async () => {
+      it('creates and then returns the new summer return cycle', async () => {
         const result = await CheckReturnCycleService.go(summer)
 
         const [insertObject] = insertStub.args[0]
@@ -144,52 +124,27 @@ describe('Jobs - Return Logs - Check Return Cycle service', () => {
         expect(insertStub.callCount).to.equal(1)
         expect(insertObject).to.equal(
           {
-            dueDate: new Date('2025-04-28T00:00:00.000Z'),
-            endDate: new Date('2025-03-31T00:00:00.000Z'),
-            startDate: new Date('2024-04-01T00:00:00.000Z'),
+            dueDate: cycleData.dueDate,
+            endDate: cycleData.endDate,
+            startDate: cycleData.startDate,
             summer: false,
             submittedInWrls: true
           },
           { skip: ['createdAt', 'updatedAt'] }
         )
-        expect(result).to.equal({
-          id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-          dueDate: new Date('2025-04-28T00:00:00.000Z'),
-          endDate: new Date('2025-03-31T00:00:00.000Z'),
-          startDate: new Date('2024-04-01T00:00:00.000Z'),
-          summer: false
-        })
+        expect(result).to.equal(cycleData)
       })
     })
 
     describe('when there is a matching all year return cycle for the change date', () => {
-      before(() => {
-        insertStub = Sinon.stub().returnsThis()
-        Sinon.stub(ReturnCycleModel, 'query').returns({
-          select: Sinon.stub().returnsThis(),
-          where: Sinon.stub().returnsThis(),
-          limit: Sinon.stub().returnsThis(),
-          first: Sinon.stub().resolves({
-            id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-            dueDate: new Date('2025-04-28T00:00:00.000Z'),
-            endDate: new Date('2025-03-31T00:00:00.000Z'),
-            summer: false,
-            startDate: new Date('2024-04-01T00:00:00.000Z')
-          }),
-          insert: insertStub
-        })
+      beforeEach(() => {
+        firstStub.resolves(cycleData)
       })
 
-      it('creates the all year return cycles', async () => {
+      it('returns the matching all year return cycle', async () => {
         const result = await CheckReturnCycleService.go(summer)
 
-        expect(result).to.equal({
-          id: '0055799f-8b6a-4753-ac78-57c61a6ef80b',
-          dueDate: new Date('2025-04-28T00:00:00.000Z'),
-          endDate: new Date('2025-03-31T00:00:00.000Z'),
-          summer: false,
-          startDate: new Date('2024-04-01T00:00:00.000Z')
-        })
+        expect(result).to.equal(cycleData)
       })
     })
   })
