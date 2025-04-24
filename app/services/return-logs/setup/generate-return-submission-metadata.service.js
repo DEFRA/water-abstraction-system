516135df230cd226b1db2c918bade7296fe1c454
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-04-24 15:46:10 +0100

    Implement `GenerateReturnSubmissionMetadataService`

---
+++ app/services/return-logs/setup/generate-return-submission-metadata.service.js
@@ -0,0 +1,53 @@
+'use strict'
+
+/**
+ * Generates metadata for a new return submission
+ * @module GenerateReturnSubmissionMetadata
+ */
+
+const UNIT_NAMES = {
+  'cubic-metres': 'm³',
+  litres: 'l',
+  megalitres: 'Ml',
+  gallons: 'gal'
+}
+
+/**
+ * TODO: Implement and document
+ *
+ * @param session
+ *
+ * @param trx
+ * @returns
+ */
+function go(session) {
+  return {
+    type: session.reported === 'abstraction-volumes' ? 'estimated' : 'measured',
+    method: session.reported === 'abstraction-volumes' ? 'abstractionVolumes' : 'oneMeter',
+    units: UNIT_NAMES[session.units],
+    meters: [], // TODO: Implement this
+    ..._total(session)
+  }
+}
+
+function _total(session) {
+  const total = {
+    totalFlag: !!session.singleVolume,
+    total: session.singleVolume ? 12345 : null, // TODO: Dummy value, check where we get this from
+    totalCustomDates: session.singleVolume && session.periodDateUsedOptions === 'custom-dates'
+  }
+
+  const totalCustomDates = {
+    totalCustomDateStart: total.totalCustomDates ? session.fromFullDate : undefined,
+    totalCustomDateEnd: total.totalCustomDates ? session.toFullDate : undefined
+  }
+
+  return {
+    ...total,
+    ...totalCustomDates
+  }
+}
+
+module.exports = {
+  go
+}
