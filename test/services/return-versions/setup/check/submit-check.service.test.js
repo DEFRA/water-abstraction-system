8867932f1c959f12c826757c58c71c0bf4c89b00
Roble <roblesiyad123@gmail.com>
2025-04-25 15:40:15 +0100

    Amend validation for returns marked quarterly submissions (#1903)
    * Amend validation for returns marked quarterly submissions
    
    https://eaflood.atlassian.net/browse/WATER-4955
    
    While working on a [bug fix](https://github.com/DEFRA/water-abstraction-system/pull/1900) for the returns journey check page,
    a problem was identified with the current validation. Other check services within WRLS do not currently involve validation as they are meant to only
    contain validated data from the user. The returns check page breaks this and involves validation to prevent requirements marked summer and returns checked
    for quarterly submission.
    
    In response to this, we have decided to incorporate validation into both the 'additional submission options' and 'returns cycle' page. This will prevent users from
    attaining incorrect data onto the check page, removing unnecessary validation.
    
    This PR will add validation to the 'additional submission options' and 'returns cycle' pages to prevent requirements marked summer and returns checked
    for quarterly submission.

--- test/services/return-versions/setup/check/submit-check.service.test.js
+++ test/services/return-versions/setup/check/submit-check.service.test.js
@@ -12,7 +12,6 @@ const { expect } = Code
 const SessionHelper = require('../../../../support/helpers/session.helper.js')
 
 // Things we need to stub
-const FetchPointsService = require('../../../../../app/services/return-versions/setup/fetch-points.service.js')
 const GenerateReturnVersionService = require('../../../../../app/services/return-versions/setup/check/generate-return-version.service.js')
 const PersistReturnVersionService = require('../../../../../app/services/return-versions/setup/check/persist-return-version.service.js')
 const ProcessLicenceReturnLogsService = require('../../../../../app/services/return-logs/process-licence-return-logs.service.js')
@@ -63,7 +62,7 @@ describe('Return Versions Setup - Submit Check service', () => {
       it('returns a valid licence', async () => {
         const result = await SubmitCheckService.go(session.id)
 
-        expect(result).to.equal({ licenceId: session.data.licence.id })
+        expect(result).to.equal(session.data.licence.id)
       })
     })
 
@@ -71,109 +70,7 @@ describe('Return Versions Setup - Submit Check service', () => {
       it('returns a valid licence', async () => {
         const result = await SubmitCheckService.go(session.id)
 
-        expect(result).to.equal({ licenceId: session.data.licence.id })
-      })
-    })
-
-    describe('When called with a return version marked quarterly return submissions', () => {
-      describe('and requirements have a return cycle of summer', () => {
-        beforeEach(async () => {
-          session = await SessionHelper.add({
-            data: {
-              checkPageVisited: false,
-              licence: {
-                id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-                currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-                endDate: null,
-                licenceRef: '01/ABC',
-                licenceHolder: 'Turbo Kid',
-                returnVersions: [],
-                startDate: '2022-04-01T00:00:00.000Z'
-              },
-              journey: 'returns-required',
-              requirements: [
-                {
-                  returnsCycle: 'summer'
-                },
-                {
-                  returnsCycle: 'winter_and_all_year'
-                }
-              ],
-              startDateOptions: 'licenceStartDate',
-              reason: 'major-change',
-              quarterlyReturns: true
-            }
-          })
-
-          Sinon.stub(FetchPointsService, 'go').resolves()
-        })
-
-        it('returns the page data needed to reload the check page and the error message', async () => {
-          const result = await SubmitCheckService.go(session.id)
-
-          expect(result).to.equal({
-            activeNavBar: 'search',
-            error: {
-              text: "Quarterly returns submissions can't be set for returns in the summer cycle."
-            },
-            licenceRef: '01/ABC',
-            multipleUpload: undefined,
-            note: {
-              actions: [
-                {
-                  href: 'note',
-                  text: 'Add a note'
-                }
-              ],
-              text: 'No notes added'
-            },
-            pageTitle: 'Check the requirements for returns for Turbo Kid',
-            quarterlyReturnSubmissions: false,
-            quarterlyReturns: true,
-            reason: 'Major change',
-            reasonLink: `/system/return-versions/setup/${session.id}/reason`,
-            requirements: [],
-            returnsRequired: true,
-            sessionId: session.id,
-            startDate: 'Invalid Date'
-          })
-        })
-      })
-
-      describe('and requirements do not have a return cycle of summer', () => {
-        before(async () => {
-          session = await SessionHelper.add({
-            data: {
-              checkPageVisited: false,
-              licence: {
-                id: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
-                currentVersionStartDate: '2023-01-01T00:00:00.000Z',
-                endDate: null,
-                licenceRef: '01/ABC',
-                licenceHolder: 'Turbo Kid',
-                returnVersions: [],
-                startDate: '2022-04-01T00:00:00.000Z'
-              },
-              journey: 'returns-required',
-              requirements: [
-                {
-                  returnsCycle: 'winter_and_all_year'
-                }
-              ],
-              startDateOptions: 'licenceStartDate',
-              reason: 'major-change',
-              quarterlyReturns: true
-            }
-          })
-
-          Sinon.stub(FetchPointsService, 'go').resolves()
-        })
-
-        it('returns a valid licence', async () => {
-          const result = await SubmitCheckService.go(session.id)
-
-          expect(result).to.equal({ licenceId: session.data.licence.id })
-        })
+        expect(result).to.equal(session.data.licence.id)
       })
     })
   })
@@ -214,7 +111,7 @@ describe('Return Versions Setup - Submit Check service', () => {
       it('returns a valid licence', async () => {
         const result = await SubmitCheckService.go(session.id)
 
-        expect(result).to.equal({ licenceId: session.data.licence.id })
+        expect(result).to.equal(session.data.licence.id)
       })
     })
   })
