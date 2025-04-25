5d5cff46fd357d5d70dc3a2a5b34d7e860690882
jonathangoulding <goulding.jon@icloud.com>
2025-04-25 15:52:13 +0000

    chore: pr fixes

--- test/services/notices/setup/abstraction-alerts/determine-licence-monitoring-stations.service.test.js
+++ test/services/notices/setup/abstraction-alerts/determine-licence-monitoring-stations.service.test.js
@@ -3,35 +3,47 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
-const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, afterEach, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const LicenceHelper = require('../../../../support/helpers/licence.helper.js')
-const LicenceMonitoringStationModel = require('../../../../support/helpers/licence-monitoring-station.helper.js')
-const MonitoringStationHelper = require('../../../../support/helpers/monitoring-station.helper.js')
+const { generateUUID } = require('../../../../../app/lib/general.lib.js')
+
+// Things we need to stub
+const FetchLicenceMonitoringStationsService = require('../../../../../app/services/notices/setup/abstraction-alerts/fetch-licence-monitoring-stations.service.js')
 
 // Thing under test
 const DetermineLicenceMonitoringStationsService = require('../../../../../app/services/notices/setup/abstraction-alerts/determine-licence-monitoring-stations.service.js')
 
 describe('Notices Setup - Abstraction alerts - Determine licence monitoring stations service', () => {
-  let licence
-  let monitoringStation
+  const monitoringStation = generateUUID()
 
   beforeEach(async () => {
-    monitoringStation = await MonitoringStationHelper.add()
+    Sinon.stub(FetchLicenceMonitoringStationsService, 'go').resolves([
+      {
+        abstraction_period_end_day: 1,
+        abstraction_period_end_month: 1,
+        abstraction_period_start_day: 1,
+        abstraction_period_start_month: 2,
+        label: 'MONITOR PLACE',
+        licence_id: '123',
+        licence_ref: '123/456',
+        licence_version_purpose_condition_id: null,
+        measure_type: 'flow',
+        restriction_type: 'reduce',
+        start_date: new Date('2022-01-01'),
+        status: 'resume',
+        status_updated_at: null,
+        threshold_unit: 'm3/s',
+        threshold_value: 100
+      }
+    ])
+  })
 
-    licence = await LicenceHelper.add()
-
-    await LicenceMonitoringStationModel.add({
-      licenceId: licence.id,
-      monitoringStationId: monitoringStation.id,
-      abstraction_period_end_day: '01',
-      abstraction_period_end_month: '01',
-      abstraction_period_start_day: '01',
-      abstraction_period_start_month: '02'
-    })
+  afterEach(() => {
+    Sinon.restore()
   })
 
   it('correctly returns the data', async () => {
@@ -44,8 +56,8 @@ describe('Notices Setup - Abstraction alerts - Determine licence monitoring stat
           abstraction_period_end_month: 1,
           abstraction_period_start_day: 1,
           abstraction_period_start_month: 2,
-          licence_id: licence.id,
-          licence_ref: licence.licenceRef,
+          licence_id: '123',
+          licence_ref: '123/456',
           licence_version_purpose_condition_id: null,
           measure_type: 'flow',
           restriction_type: 'reduce',
