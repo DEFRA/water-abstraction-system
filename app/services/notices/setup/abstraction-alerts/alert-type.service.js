ed39aa43a55c24a132e33544a39e3fa743e60388
jonathangoulding <goulding.jon@icloud.com>
2025-04-28 16:28:59 +0000

    chore: sonar

--- app/services/notices/setup/abstraction-alerts/alert-type.service.js
+++ app/services/notices/setup/abstraction-alerts/alert-type.service.js
@@ -13,7 +13,8 @@ const SessionModel = require('../../../../models/session.model.js')
  * Orchestrates presenting the data for `/notices/setup/{sessionId}/abstraction-alert/alert-type` page
  *
  * @param {string} sessionId
- * @returns {object} - The data formatted for the view template
+ *
+ * @returns {Promise<{object}>} - The data formatted for the view template
  */
 async function go(sessionId) {
   const session = await SessionModel.query().findById(sessionId)
