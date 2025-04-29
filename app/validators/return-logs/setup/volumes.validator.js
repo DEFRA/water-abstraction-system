8e18f6b512521ecd643b247e7e2bfb5f18c0a4c2
Jason Claxton <jason.claxton@defra.gov.uk>
2025-04-29 16:51:19 +0100

    Update volumes validator errors

--- app/validators/return-logs/setup/volumes.validator.js
+++ app/validators/return-logs/setup/volumes.validator.js
@@ -16,14 +16,18 @@ const Joi = require('joi')
  * any errors are found the `error:` property will also exist detailing what the issues were
  */
 function go(payload) {
+  const maxAllowedVolume = 9999999999
+
   const schema = Joi.object().pattern(
     /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/, // Regex to match keys like '2024-04-01T00:00:00.000Z'
     Joi.number()
       .min(0)
+      .max(maxAllowedVolume)
       .messages({
-        'number.base': 'Volumes must be a number or blank',
-        'number.min': 'Volumes must be a positive number',
-        'number.unsafe': `Volume entered exceeds the maximum safe number ${Number.MAX_SAFE_INTEGER}`
+        'number.base': 'Volume must be a number or blank',
+        'number.min': 'Volume must be a positive number',
+        'number.max': `Volume entered exceeds the maximum of ${maxAllowedVolume}`,
+        'number.unsafe': `Volume entered exceeds the maximum of ${maxAllowedVolume}`
       })
   )
 
