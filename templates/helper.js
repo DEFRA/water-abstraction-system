1fb0d9023619d219b626c5650792df017b830250
jonathangoulding <goulding.jon@icloud.com>
2025-04-30 15:50:08 +0000

    fix: lint

--- templates/helper.js
+++ templates/helper.js
@@ -1,4 +1,4 @@
-/* eslint-disable camelcase, no-unused-vars, strict */
+/* eslint-disable camelcase, no-unused-vars, strict, no-undef */
 
 // Controller code
 const __SERVICE_NAME__ = require('__SERVICE_PATH__')
