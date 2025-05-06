c54bd148952268ed9ba710d0706927ec0f1cf42d
jonathangoulding <goulding.jon@icloud.com>
2025-05-06 08:42:15 +0000

    Update notifications setup abstraction alerts check licence matches view
    https://eaflood.atlassian.net/browse/WATER-5026
    
    We are replacing the legacy notification abstraction alert journey. We are going through page by page and adding roughly similar page for some parts of the journey. With the intention of reusing the existing send notification logic we built previously.
    
    This changes updates the check licence matches controller, view, presenter and service to render the licences previously selected on the alert thresholds page, with the same information as the legacy page.
    
    When multiple thresholds for a licence are present they are grouped by licence, but show their own abstraction period, flow and level restrictions and the last alert type.
    
    The remove / exclude logic will be added in another change.

--- app/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.js
+++ app/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.js
@@ -8,10 +8,21 @@
 /**
  * Formats data for the `/notices/setup/{sessionId}/abstraction-alert/check-licence-matches` page
  *
+ * @param {module:SessionModel} session - The session instance
+ *
  * @returns {object} - The data formatted for the view template
  */
-function go() {
-  return {}
+function go(session) {
+  const { alertThresholds, licenceMonitoringStations } = session
+  return {
+    licences: _licences(alertThresholds, licenceMonitoringStations)
+  }
+}
+
+function _licences(alertThresholds, licenceMonitoringStations) {
+  return licenceMonitoringStations.filter((lms) => {
+    return alertThresholds.includes(lms.id)
+  })
 }
 
 module.exports = {
--- test/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.test.js
+++ test/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.test.js
@@ -7,14 +7,22 @@ const Code = require('@hapi/code')
 const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
+// Test helpers
+const AbstractionAlertSessionData = require('../../../../fixtures/abstraction-alert-session-data.fixture.js')
+
 // Thing under test
 const CheckLicenceMatchesPresenter = require('../../../../../app/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.js')
 
 describe('Notices Setup - Abstraction Alerts - Check Licence Matches Presenter', () => {
+  let abstractionAlertSessionData
   let session
 
   beforeEach(() => {
-    session = {}
+    abstractionAlertSessionData = AbstractionAlertSessionData.monitoringStation()
+    session = {
+      ...abstractionAlertSessionData,
+      alertThresholds: [abstractionAlertSessionData.licenceMonitoringStations[0].id]
+    }
   })
 
   describe('when called', () => {
@@ -22,6 +30,12 @@ describe('Notices Setup - Abstraction Alerts - Check Licence Matches Presenter',
       const result = CheckLicenceMatchesPresenter.go(session)
 
       expect(result).to.equal({})
+    })
+
+    it.only('returns the selected licence monitoring stations from the "alertThresholds"', () => {
+      const result = CheckLicenceMatchesPresenter.go(session)
+
+      expect(result.licences).to.equal([abstractionAlertSessionData.licenceMonitoringStations[0]])
     })
   })
 })
