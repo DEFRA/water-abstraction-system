faaa9caaf1dfe014a8b84d56062dc557ee3408ce
Rebecca Ransome <beckyrose200@aol.com>
2025-05-07 16:22:52 +0100

    Add: Submit stop or reduce service and test

--- app/services/licence-monitoring-station/setup/submit-stop-or-reduce.service.js
+++ app/services/licence-monitoring-station/setup/submit-stop-or-reduce.service.js
@@ -1,19 +1,18 @@
 'use strict'
 
 /**
- * Orchestrates validating the data for `` page
- *
+ * Orchestrates validating the data for `/licence-monitoring-station/setup/{sessionId}/stop-or-reduce` page
  * @module SubmitStopOrReduceService
  */
 
+const SessionModel = require('../../../models/session.model.js')
 const StopOrReducePresenter = require('../../../presenters/licence-monitoring-station/setup/stop-or-reduce.presenter.js')
 const StopOrReduceValidator = require('../../../validators/licence-monitoring-station/setup/stop-or-reduce.validator.js')
-const SessionModel = require('../../../models/session.model.js')
 
 /**
- * Orchestrates validating the data for `` page
+ * Orchestrates validating the data for `/licence-monitoring-station/setup/{sessionId}/stop-or-reduce` page
  *
- * @param {string} sessionId
+ * @param {string} sessionId - The UUID of the current session
  * @param {object} payload - The submitted form data
  *
  * @returns {Promise<object>} - The data formatted for the view template
@@ -26,19 +25,32 @@ async function go(sessionId, payload) {
   if (!validationResult) {
     await _save(session, payload)
 
-    return {}
+    return {
+      checkPageVisited: session.checkPageVisited
+    }
   }
 
-  const pageData = StopOrReducePresenter.go(session)
+  const formattedData = _submittedSessionData(session, payload)
 
   return {
+    activeNavBar: 'search',
     error: validationResult,
-    ...pageData
+    ...formattedData
   }
 }
 
 async function _save(session, payload) {
+  session.stopOrReduce = payload.stopOrReduce
+  session.reduceAtThreshold = payload.reduceAtThreshold ?? null
+
   return session.$update()
+}
+
+function _submittedSessionData(session, payload) {
+  session.stopOrReduce = payload.stopOrReduce ?? null
+  session.reduceAtThreshold = payload.reduceAtThreshold ?? null
+
+  return StopOrReducePresenter.go(session)
 }
 
 function _validate(payload) {
@@ -48,11 +60,15 @@ function _validate(payload) {
     return null
   }
 
-  const { message } = validation.error.details[0]
+  const { message, path } = validation.error.details[0]
 
-  return {
-    text: message
+  const result = {
+    message,
+    reduceAtThresholdRadioElement: path[0] === 'reduceAtThreshold' ? { text: message } : null,
+    stopOrReduceRadioElement: path[0] === 'stopOrReduce' ? { text: message } : null
   }
+
+  return result
 }
 
 module.exports = {
--- test/services/licence-monitoring-station/setup/submit-stop-or-reduce.service.test.js
+++ test/services/licence-monitoring-station/setup/submit-stop-or-reduce.service.test.js
@@ -13,39 +13,116 @@ const SessionHelper = require('../../../support/helpers/session.helper.js')
 // Thing under test
 const SubmitStopOrReduceService = require('../../../../app/services/licence-monitoring-station/setup/submit-stop-or-reduce.service.js')
 
-describe('Stop Or Reduce Service', () => {
+describe('Licence Monitoring Station Setup - Stop Or Reduce service', () => {
   let payload
   let session
   let sessionData
 
   beforeEach(async () => {
-    payload = {}
-    sessionData = {}
+    sessionData = {
+      data: {
+        label: 'Monitoring Station Label',
+        monitoringStationId: 'e1c44f9b-51c2-4aee-a518-5509d6f05869'
+      }
+    }
 
-    session = await SessionHelper.add({ data: sessionData })
+    session = await SessionHelper.add(sessionData)
   })
 
   describe('when called', () => {
-    it('saves the submitted value', async () => {
-      await SubmitStopOrReduceService.go(session.id, payload)
+    describe('with a valid payload', () => {
+      beforeEach(async () => {
+        payload = { stopOrReduce: 'stop' }
+      })
 
-      const refreshedSession = await session.$query()
+      it('saves the submitted option', async () => {
+        await SubmitStopOrReduceService.go(session.id, payload)
 
-      expect(refreshedSession).to.equal()
+        const refreshedSession = await session.$query()
+
+        expect(refreshedSession.stopOrReduce).to.equal('stop')
+        expect(refreshedSession.reduceAtThreshold).to.equal(null)
+      })
+
+      describe('and the page has been not been visited', () => {
+        it('returns the correct details the controller needs to redirect the journey', async () => {
+          const result = await SubmitStopOrReduceService.go(session.id, payload)
+
+          expect(result).to.equal({
+            checkPageVisited: undefined
+          })
+        })
+      })
+
+      describe('and the page has been visited', () => {
+        beforeEach(async () => {
+          session = await SessionHelper.add({ data: { ...sessionData.data, checkPageVisited: true } })
+        })
+
+        it('returns the correct details the controller needs to redirect the journey to the check page', async () => {
+          const result = await SubmitStopOrReduceService.go(session.id, payload)
+
+          expect(result).to.equal({
+            checkPageVisited: true
+          })
+        })
+      })
     })
 
-    it('continues the journey', async () => {
-      const result = await SubmitStopOrReduceService.go(session.id, payload)
+    describe('with an invalid payload', () => {
+      beforeEach(async () => {
+        payload = {}
+      })
 
-      expect(result).to.equal({})
-    })
-  })
+      it('returns the page data for the view', async () => {
+        const result = await SubmitStopOrReduceService.go(session.id, payload)
 
-  describe('when validation fails', () => {
-    it('returns page data for the view, with errors', async () => {
-      const result = await SubmitStopOrReduceService.go(session.id, payload)
+        expect(result).to.equal(
+          {
+            activeNavBar: 'search',
+            backLink: `/system/licence-monitoring-station/setup/${session.id}/threshold-and-unit`,
+            monitoringStationLabel: 'Monitoring Station Label',
+            pageTitle: 'Does the licence holder need to stop or reduce at this threshold?',
+            stopOrReduce: null,
+            reduceAtThreshold: null
+          },
+          { skip: ['sessionId', 'error'] }
+        )
+      })
 
-      expect(result).to.equal({})
+      describe('because the user has not entered or selected anything', () => {
+        it('includes an error for both input elements and radio elements', async () => {
+          const result = await SubmitStopOrReduceService.go(session.id, payload)
+
+          expect(result.error).to.equal({
+            message: 'Select if the licence holder needs to stop or reduce',
+            stopOrReduceRadioElement: {
+              text: 'Select if the licence holder needs to stop or reduce'
+            },
+            reduceAtThresholdRadioElement: null
+          })
+        })
+      })
+
+      describe('because the user has not entered the "reduceAtThreshold"', () => {
+        beforeEach(async () => {
+          payload = {
+            stopOrReduce: 'reduce'
+          }
+        })
+
+        it('includes an error for the reduceAtThreshold input elements', async () => {
+          const result = await SubmitStopOrReduceService.go(session.id, payload)
+
+          expect(result.error).to.equal({
+            message: 'Select if the licence holder needs to stop abstraction when they reach a certain amount',
+            stopOrReduceRadioElement: null,
+            reduceAtThresholdRadioElement: {
+              text: 'Select if the licence holder needs to stop abstraction when they reach a certain amount'
+            }
+          })
+        })
+      })
     })
   })
 })
