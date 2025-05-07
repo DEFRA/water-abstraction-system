874b3d7caf1df0bb661d914b7b165ab339dd3802
Rebecca Ransome <beckyrose200@aol.com>
2025-05-07 11:46:53 +0100

    Add: Stop or reduce presenter and test

--- app/presenters/licence-monitoring-station/setup/stop-or-reduce.presenter.js
+++ app/presenters/licence-monitoring-station/setup/stop-or-reduce.presenter.js
@@ -1,17 +1,38 @@
 'use strict'
 
 /**
- * Formats data for the `` page
+ * Formats data for the `/licence-monitoring-station/setup/{sessionId}/stop-or-reduce` page
  * @module StopOrReducePresenter
  */
 
 /**
- * Formats data for the `` page
+ * Formats data for the `/licence-monitoring-station/setup/{sessionId}/stop-or-reduce` page
  *
- * @returns {object} - The data formatted for the view template
+ * @param {module:SessionModel} session - The licence monitoring station setup session instance
+ *
+ * @returns {object} page data needed by the view template
  */
-function go() {
-  return {}
+function go(session) {
+  const { id: sessionId, label, stopOrReduce, reduceAtThreshold } = session
+
+  return {
+    backLink: _backLink(session),
+    monitoringStationLabel: label,
+    pageTitle: 'Does the licence holder need to stop or reduce at this threshold?',
+    reduceAtThreshold: reduceAtThreshold ?? null,
+    sessionId,
+    stopOrReduce: stopOrReduce ?? null
+  }
+}
+
+function _backLink(session) {
+  const { checkPageVisited, id } = session
+
+  if (checkPageVisited) {
+    return `/system/licence-monitoring-station/setup/${id}/check`
+  }
+
+  return `/system/licence-monitoring-station/setup/${id}/threshold-and-unit`
 }
 
 module.exports = {
