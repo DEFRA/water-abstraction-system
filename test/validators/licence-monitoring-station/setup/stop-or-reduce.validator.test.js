068de991589d6891ab8250dd6d7079572e158057
Rebecca Ransome <beckyrose200@aol.com>
2025-05-07 16:30:25 +0100

    Add: Validator and tests

--- test/validators/licence-monitoring-station/setup/stop-or-reduce.validator.test.js
+++ test/validators/licence-monitoring-station/setup/stop-or-reduce.validator.test.js
@@ -10,29 +10,69 @@ const { expect } = Code
 // Thing under test
 const StopOrReduceValidator = require('../../../../app/validators/licence-monitoring-station/setup/stop-or-reduce.validator.js')
 
-describe('Stop Or Reduce Validator', () => {
+describe('Licence Monitoring Station Setup - Stop Or Reduce validator', () => {
   let payload
 
-  beforeEach(() => {
-    payload = {}
-  })
+  describe('when valid data is provided', () => {
+    describe('because the user selected the "stop" option', () => {
+      beforeEach(() => {
+        payload = {
+          stopOrReduce: 'stop'
+        }
+      })
 
-  describe('when called with valid data', () => {
-    it('returns with no errors', () => {
-      const result = StopOrReduceValidator.go(payload)
+      it('confirms the data is valid', () => {
+        const result = StopOrReduceValidator.go(payload)
 
-      expect(result.value).to.exist()
-      expect(result.error).not.to.exist()
+        expect(result.error).not.to.exist()
+      })
+    })
+
+    describe('because the user selected the "reduce" option', () => {
+      beforeEach(() => {
+        payload = {
+          stopOrReduce: 'reduce',
+          reduceAtThreshold: 'yes'
+        }
+      })
+
+      it('confirms the data is valid', () => {
+        const result = StopOrReduceValidator.go(payload)
+
+        expect(result.error).not.to.exist()
+      })
     })
   })
 
-  describe('when called with invalid data', () => {
-    it('returns with errors', () => {
-      const result = StopOrReduceValidator.go(payload)
+  describe('when an invalid payload is provided', () => {
+    describe('because the user did not select anything', () => {
+      beforeEach(() => {
+        payload = {}
+      })
 
-      expect(result.value).to.exist()
-      expect(result.error).to.exist()
-      expect(result.error.details[0].message).to.equal('')
+      it('fails validation', () => {
+        const result = StopOrReduceValidator.go(payload)
+
+        expect(result.error).to.exist()
+        expect(result.error.details[0].message).to.equal('Select if the licence holder needs to stop or reduce')
+      })
+    })
+
+    describe('because the user selected "reduce" but did not select if the licence holder should stop abstracting water', () => {
+      beforeEach(() => {
+        payload = {
+          stopOrReduce: 'reduce'
+        }
+      })
+
+      it('fails validation', () => {
+        const result = StopOrReduceValidator.go(payload)
+
+        expect(result.error).to.exist()
+        expect(result.error.details[0].message).to.equal(
+          'Select if the licence holder needs to stop abstraction when they reach a certain amount'
+        )
+      })
     })
   })
 })
