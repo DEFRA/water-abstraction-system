532bb4d376320b2bf75f2793038aa0f53bacb039
Robert Parkinson <robertparkinson@users.noreply.github.com>
2025-05-08 14:29:05 +0100

    Setup scaffold pages for view notice (#1962)
    https://eaflood.atlassian.net/browse/WATER-4788
    
    This PR creates the scaffolds for the view notice page. This page replaces an existing legacy UI page.

---
+++ app/presenters/notices/view.presenter.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * Formats data for the 'notices/{noticeId}/view' page
+ * @module ViewPresenter
+ */
+
+/**
+ * Formats data for the 'notices/{noticeId}/view' page
+ *
+ * @returns {object} - The data formatted for the view template
+ */
+function go() {
+  return {}
+}
+
+module.exports = {
+  go
+}
