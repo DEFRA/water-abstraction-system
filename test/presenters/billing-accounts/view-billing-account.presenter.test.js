f60de5e6f38cb7b64f0d056f4495031ea8cd4163
rvsiyad <roblesiyad123@gmail.com>
2025-05-12 10:05:24 +0100

    Update view billing account presenter test to use billing account fixture file

--- test/presenters/billing-accounts/view-billing-account.presenter.test.js
+++ test/presenters/billing-accounts/view-billing-account.presenter.test.js
@@ -7,6 +7,9 @@ const Code = require('@hapi/code')
 const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
+// Test helpers
+const BillingAccountsFixture = require('../../fixtures/billing-accounts.fixtures.js')
+
 // Thing under test
 const ViewBillingAccountPresenter = require('../../../app/presenters/billing-account/view-billing-account.presenter.js')
 
@@ -14,7 +17,7 @@ describe('View Billing Account presenter', () => {
   let billingAccountData
 
   beforeEach(() => {
-    billingAccountData = _testBillingAccountData()
+    billingAccountData = BillingAccountsFixture.billingAccount()
   })
 
   describe('when provided with a populated billing account', () => {
@@ -153,54 +156,3 @@ describe('View Billing Account presenter', () => {
     })
   })
 })
-
-function _testBillingAccountData() {
-  return {
-    billingAccount: {
-      id: '9b03843e-848b-497e-878e-4a6628d4f683',
-      accountNumber: 'S88897992A',
-      createdAt: new Date('2023-12-14T18:42:59.659Z'),
-      lastTransactionFile: null,
-      lastTransactionFileCreatedAt: null,
-      billingAccountAddresses: [
-        {
-          id: '04ba8291-fb58-40a9-9581-cfedc136eef7',
-          address: {
-            id: '310ae9a7-69c1-49b3-a29f-0ba46e6cfa7b',
-            address1: 'Tutsham Farm',
-            address2: 'West Farleigh',
-            address3: null,
-            address4: null,
-            address5: 'Maidstone',
-            address6: 'Kent',
-            postcode: 'ME15 0NE'
-          }
-        }
-      ],
-      company: {
-        id: '55a71eb5-e0e1-443e-9a25-c529cccfd6df',
-        name: 'Ferns Surfacing Limited'
-      }
-    },
-    bills: [
-      {
-        id: '3d1b5d1f-9b57-4a28-bde1-1d57cd77b203',
-        createdAt: new Date('2023-12-14T18:42:59.659Z'),
-        credit: false,
-        invoiceNumber: false,
-        netAmount: 10384,
-        financialYear: 2021,
-        billRun: {
-          id: 'eee30072-ad12-426a-9d69-c712f38e581d',
-          batchType: 'annual',
-          billRunNumber: 607,
-          scheme: 'alcs',
-          source: 'nald',
-          summer: false
-        }
-      }
-    ],
-    licenceId: '91aff99a-3204-4727-86bd-7bdf3ef24533',
-    pagination: { total: 1 }
-  }
-}
