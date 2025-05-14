f60e3250dec05cd6e9f7e6f999f55c281b2bce06
jonathangoulding <goulding.jon@icloud.com>
2025-05-14 14:26:55 +0000

    feat: add remove threshold to session with licence monitoring station id

--- app/services/notices/setup/abstraction-alerts/remove-threshold.service.js
+++ app/services/notices/setup/abstraction-alerts/remove-threshold.service.js
@@ -12,13 +12,26 @@ const SessionModel = require('../../../../models/session.model.js')
  * Orchestrates fetching and presenting the data for the `/notices/setup/{sessionId}/abstraction-alert/remove-threshold/{licenceMonitoringStationId}` page
  *
  * @param {string} sessionId
+ * @param {string} licenceMonitoringStationId
  *
  * @returns {Promise<object>} - The data formatted for the view template
  */
-async function go(sessionId) {
+async function go(sessionId, licenceMonitoringStationId) {
   const session = await SessionModel.query().findById(sessionId)
 
+  await _save(session, licenceMonitoringStationId)
+
   return {}
+}
+
+async function _save(session, licenceMonitoringStationId) {
+  if (session.removedThresholds) {
+    session.removedThresholds = [...session.removedThresholds, licenceMonitoringStationId]
+  } else {
+    session.removedThresholds = [licenceMonitoringStationId]
+  }
+
+  return session.$update()
 }
 
 module.exports = {
--- test/services/notices/setup/abstraction-alerts/remove-threshold.service.test.js
+++ test/services/notices/setup/abstraction-alerts/remove-threshold.service.test.js
@@ -9,25 +9,50 @@ const { expect } = Code
 
 // Test helpers
 const SessionHelper = require('../../../../support/helpers/session.helper.js')
+const { generateUUID } = require('../../../../../app/lib/general.lib.js')
 
 // Thing under test
 const RemoveThresholdService = require('../../../../../app/services/notices/setup/abstraction-alerts/remove-threshold.service.js')
 
 describe('Remove Threshold Service', () => {
   let session
-  let sessionData
-
-  beforeEach(async () => {
-    sessionData = {}
-
-    session = await SessionHelper.add({ data: sessionData })
-  })
+  let licenceMonitoringStationId
 
   describe('when called', () => {
-    it('returns page data for the view', async () => {
-      const result = await RemoveThresholdService.go(session.id)
+    describe('and there are no thresholds currently removed', () => {
+      beforeEach(async () => {
+        licenceMonitoringStationId = generateUUID()
 
-      expect(result).to.equal({})
+        session = await SessionHelper.add()
+      })
+
+      it('saves the "licenceMonitoringStationId" to the session to be excluded from the list', async () => {
+        await RemoveThresholdService.go(session.id, licenceMonitoringStationId)
+
+        const refreshedSession = await session.$query()
+
+        expect(refreshedSession.removedThresholds).to.equal([licenceMonitoringStationId])
+      })
+    })
+
+    describe('and there are existing "removedThresholds"', () => {
+      beforeEach(async () => {
+        licenceMonitoringStationId = generateUUID()
+
+        session = await SessionHelper.add({
+          data: {
+            removedThresholds: [licenceMonitoringStationId]
+          }
+        })
+      })
+
+      it('saves the "licenceMonitoringStationId" to the session with the existing "removedThresholds"', async () => {
+        await RemoveThresholdService.go(session.id, licenceMonitoringStationId)
+
+        const refreshedSession = await session.$query()
+
+        expect(refreshedSession.removedThresholds).to.equal([licenceMonitoringStationId, licenceMonitoringStationId])
+      })
     })
   })
 })
