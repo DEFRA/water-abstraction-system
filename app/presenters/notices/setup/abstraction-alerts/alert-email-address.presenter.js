e4ec0bb0589826cdcb726781f7edbdf97b02e37b
rvsiyad <roblesiyad123@gmail.com>
2025-05-15 09:54:08 +0100

    Add alert email address presenter

---
+++ app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * Formats data for the `/notices/setup/{sessionId}/abstraction-alert/alert-email-address` page
+ * @module AlertEmailAddressPresenter
+ */
+
+/**
+ * Formats data for the `/notices/setup/{sessionId}/abstraction-alert/alert-email-address` page
+ *
+ * @returns {object} - The data formatted for the view template
+ */
+function go() {
+  return {}
+}
+
+module.exports = {
+  go
+}
