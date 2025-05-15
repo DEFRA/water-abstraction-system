29fe752ae1df414dcf5dc82bec8f1afc7bb41e75
rvsiyad <roblesiyad123@gmail.com>
2025-05-15 09:54:29 +0100

    Add alert email address validator

---
+++ app/validators/notices/setup/abstraction-alerts/alert-email-address.validator.js
@@ -0,0 +1,27 @@
+'use strict'
+
+/**
+ * Validates data submitted for the `/notices/setup/{sessionId}/abstraction-alert/alert-email-address` page
+ *
+ * @module AlertEmailAddressValidator
+ */
+
+const Joi = require('joi')
+
+/**
+ * Validates data submitted for the `/notices/setup/{sessionId}/abstraction-alert/alert-email-address` page
+ *
+ * @param {object} payload - The payload from the request to be validated
+ *
+ * @returns {object} the result from calling Joi's schema.validate(). It will be an object with a `value:` property. If
+ * any errors are found the `error:` property will also exist detailing what the issues were
+ */
+function go(payload) {
+  const schema = Joi.object({})
+
+  return schema.validate(payload, { abortEarly: false })
+}
+
+module.exports = {
+  go
+}
