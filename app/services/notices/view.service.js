2e704e974b3a0e7254026180fc2fd1565e60b72f
robertparkinson <robert@daedalusft.com>
2025-05-20 16:41:47 +0100

    view notice view, presenter and refactor

--- app/services/notices/view.service.js
+++ app/services/notices/view.service.js
@@ -6,21 +6,40 @@
  * @module ViewService
  */
 
+const DatabaseConfig = require('../../../config/database.config.js')
+const FetchNoticeService = require('../../services/notices/fetch-notice.service.js')
+const PaginatorPresenter = require('../../presenters/paginator.presenter.js')
 const ViewPresenter = require('../../presenters/notices/view.presenter.js')
 
 /**
  * Orchestrates fetching and presenting the data for the 'notices/{id}' page
  *
- * @param {string} noticeId - The uuid of the notice we are viewing
+ * @param {string} id - The uuid of the notice we are viewing
  * @param {number|string} page - The current page for the pagination service
  *
  * @returns {Promise<object>} - The data formatted for the view template
  */
-async function go(noticeId, page) {
-  const pageData = ViewPresenter.go()
+async function go(id, page) {
+  const notices = await FetchNoticeService.go(id, page)
+
+  const { results, total: numberOfRecipients } = notices
+  const selectedPageNumber = page ? Number(page) : 1
+  const defaultPageSize = DatabaseConfig.defaultPageSize
+  const numberShowing = results.length < defaultPageSize ? results.length : defaultPageSize * selectedPageNumber
+
+  const pagination = PaginatorPresenter.go(
+    numberOfRecipients,
+    selectedPageNumber,
+    `/system/notices/${results[0].event.id}`
+  )
+  const pageData = ViewPresenter.go(results, page)
 
   return {
-    ...pageData
+    ...pageData,
+    numberOfRecipients,
+    numberShowing,
+    pageTitle: 'Water abstraction alert',
+    pagination
   }
 }
 
