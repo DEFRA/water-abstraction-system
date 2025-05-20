84e1056e7765c4b3ff56079c1e89d8f576729eb0
jonathangoulding <goulding.jon@icloud.com>
2025-05-20 07:25:35 +0000

    fix: revert redirect

--- test/controllers/notices-setup.controller.test.js
+++ test/controllers/notices-setup.controller.test.js
@@ -526,9 +526,7 @@ describe('Notices Setup controller', () => {
             const response = await server.inject(postOptions)
 
             expect(response.statusCode).to.equal(302)
-            expect(response.headers.location).to.equal(
-              `/system/notices/setup/${session.id}/abstraction-alerts/alert-email-address`
-            )
+            expect(response.headers.location).to.equal(`/system/notices/setup/${session.id}/abstraction-alerts/check`)
           })
         })
       })
