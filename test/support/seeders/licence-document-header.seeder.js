0e52e726d8bb75d7bcbc28ec52bd3725cfe5ee8c
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-05-20 06:44:52 -0700

    Update test/support/seeders/licence-document-header.seeder.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- test/support/seeders/licence-document-header.seeder.js
+++ test/support/seeders/licence-document-header.seeder.js
@@ -16,7 +16,8 @@ const ReturnLogHelper = require('../helpers/return-log.helper.js')
  * to be included in the recipients list
  * @param {string} returnLogDueDate - defaulted to the same due date set by the returnsLogHelper
  *
- * @returns {Promise<object>} - an object of the added licenceDocumentHeaders
+ * @returns {Promise<object>} an object containing different licence document header instances and related entities
+ * representing different scenarios
  */
 async function seed(enableReturnLog = true, returnLogDueDate = '2023-04-28') {
   return {
