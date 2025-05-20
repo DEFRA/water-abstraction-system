6069691fe81392e5b86b0e8e2eb909d6d99258e8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-05-20 23:12:51 +0100

    Update SubmitIndexService
    This follows on the changes made to `IndexService` to move the validation into the submit service, before we save anything to the session.
    
    The behaviour I would also expect to see when something is invalid, is that the previous results are still displayed, along with what I entered as a filter and the errors with it. I wouldn't expect the results to disappear.
    
    Because of this, we also need to bring into this page all the logic needed to fetch results and present them ready for passing to the view.
    
    It does mean duplication, and we should look at that once the work is complete. This page is likely to serve as a template for others so worth getting right. But for now, the user experience is paramount so the DRYness of our code will have to take a back seat.
    
    We also see the benefit of the work to keep the references to the filters consistent (the mapping can be deleted) as well as sticking with standard Joi errors and validating the payload object (simplified validation).
    
    The errors work as GOV.UK patterns require: a message in the summary with a working link to the control with an issue.

--- app/services/notices/submit-index.service.js
+++ app/services/notices/submit-index.service.js
@@ -1,43 +1,153 @@
 'use strict'
 
 /**
- * Updates the session cookie with the filter data needed for the notices page
- * @module SubmitNoticesIndexService
+ * Handles validation of the requested filters, saving them to the session else re-rendering the page if invalid
+ * @module SubmitIndexService
  */
 
+const FetchNoticesService = require('./fetch-notices.service.js')
+const NoticesIndexPresenter = require('../../presenters/notices/index-notices.presenter.js')
+const IndexValidator = require('../../validators/notices/index.validator.js')
+const PaginatorPresenter = require('../../presenters/paginator.presenter.js')
+
 /**
- * Updates the session cookie with the filter data needed for the notices page
+ * Handles validation of the requested filters, saving them to the session else re-rendering the page if invalid
+ *
+ * Users can also opt to clear any filters applied.
  *
  * @param {object} payload - The `request.payload` containing the filter data.
  * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
+ * @param {number|string} page - The current page for the pagination service
  *
- * @returns {Promise<object>} The error details if any
+ * @returns {Promise<object>} If no errors an empty object signifying the request can be redirected to the index page
+ * else the data needed to re-render the page
  */
-async function go(payload, yar) {
-  const clearFilters = payload?.clearFilters
-  const sentFromDay = payload['sent-from-day'] ? payload['sent-from-day'] : null
-  const sentFromMonth = payload['sent-from-month'] ? payload['sent-from-month'] : null
-  const sentFromYear = payload['sent-from-year'] ? payload['sent-from-year'] : null
-  const sentToDay = payload['sent-to-day'] ? payload['sent-to-day'] : null
-  const sentToMonth = payload['sent-to-month'] ? payload['sent-to-month'] : null
-  const sentToYear = payload['sent-to-year'] ? payload['sent-to-year'] : null
-  const filterNotificationTypes = payload?.filterNotificationTypes
-  const sentBy = payload?.sentBy
+async function go(payload, yar, page = 1) {
+  const clearFilters = _clearFilters(payload, yar)
 
   if (clearFilters) {
-    yar.clear('notices-filter')
-  } else {
-    yar.set('notices-filter', {
-      filterNotificationTypes,
-      sentBy,
-      sentFromDay,
-      sentFromMonth,
-      sentFromYear,
-      sentToDay,
-      sentToMonth,
-      sentToYear
-    })
+    return {}
   }
+
+  _handleOneOptionSelected(payload)
+
+  const validationResult = _validate(payload)
+
+  if (!validationResult) {
+    _save(payload, yar)
+
+    return {}
+  }
+
+  // When the page comes from the request via the controller then it will be a string. For consistency we want it as a
+  // number
+  const selectedPageNumber = Number(page)
+
+  const savedFilters = _savedFilters(yar)
+  const { results, total: numberOfNotices } = await FetchNoticesService.go(savedFilters, selectedPageNumber)
+
+  const pageData = await NoticesIndexPresenter.go(results, numberOfNotices)
+  const pagination = PaginatorPresenter.go(numberOfNotices, selectedPageNumber, `/system/notices`)
+  const pageTitle = _pageTitle(pagination.numberOfPages, selectedPageNumber)
+
+  return {
+    activeNavBar: 'manage',
+    error: validationResult,
+    filters: { ...savedFilters, ...payload },
+    ...pageData,
+    pagination,
+    pageTitle
+  }
+}
+
+function _clearFilters(payload, yar) {
+  const clearFilters = payload.clearFilters
+
+  if (clearFilters) {
+    yar.clear('noticesFilter')
+
+    return true
+  }
+
+  return false
+}
+
+/**
+ * When a single type is checked by the user, it returns as a string. When multiple types are checked, the 'types' is
+ * returned as an array. When nothing is checked then the property doesn't exist in the payload.
+ *
+ * This function works to handle these discrepancies.
+ *
+ * @private
+ */
+function _handleOneOptionSelected(payload) {
+  if (!payload?.noticeTypes) {
+    payload.noticeTypes = []
+
+    return
+  }
+
+  if (!Array.isArray(payload?.noticeTypes)) {
+    payload.noticeTypes = [payload?.noticeTypes]
+  }
+}
+
+function _pageTitle(numberOfPages, selectedPageNumber) {
+  if (numberOfPages < 2) {
+    return 'Notices'
+  }
+
+  return `Notices (page ${selectedPageNumber} of ${numberOfPages})`
+}
+
+function _save(payload, yar) {
+  yar.set('noticesFilter', {
+    noticeTypes: payload.noticeTypes,
+    fromDate: payload.fromDate,
+    sentBy: payload.sentBy ?? null,
+    sentFromDay: payload.sentFromDay ?? null,
+    sentFromMonth: payload.sentFromMonth ?? null,
+    sentFromYear: payload.sentFromYear ?? null,
+    sentToDay: payload.sentToDay ?? null,
+    sentToMonth: payload.sentToMonth ?? null,
+    sentToYear: payload.sentToYear ?? null,
+    toDate: payload.toDate
+  })
+}
+
+function _savedFilters(yar) {
+  const savedFilters = yar.get('noticesFilter')
+
+  return {
+    fromDate: null,
+    noticeTypes: [],
+    openFilter: true,
+    sentBy: null,
+    toDate: null,
+    ...savedFilters
+  }
+}
+
+function _validate(payload) {
+  const validation = IndexValidator.go(payload)
+
+  if (!validation.error) {
+    return null
+  }
+
+  const result = {
+    errorList: []
+  }
+
+  validation.error.details.forEach((detail) => {
+    const path = detail.path[0]
+
+    result.errorList.push({ href: `#${path}`, text: detail.message })
+
+    result[path] = { message: detail.message }
+  })
+
+  return result
 }
 
 module.exports = {
--- test/services/notices/submit-index.service.test.js
+++ test/services/notices/submit-index.service.test.js
@@ -8,98 +8,382 @@ const Sinon = require('sinon')
 const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
-// Thing under test
-const SubmitNoticesIndexService = require('../../../app/services/notices/submit-index.service.js')
+// Test helpers
+const NoticesFixture = require('../../fixtures/notices.fixture.js')
 
-describe('Notices - Submit', () => {
+// Things to stub
+const FetchNoticesService = require('../../../app/services/notices/fetch-notices.service.js')
+
+// Thing under test
+const SubmitIndexService = require('../../../app/services/notices/submit-index.service.js')
+
+describe('Notices - Submit Index service', () => {
+  let notice
   let payload
   let yarStub
 
   beforeEach(async () => {
-    yarStub = { clear: Sinon.stub().returns(), set: Sinon.stub().returns() }
+    yarStub = {
+      clear: Sinon.stub().returns(),
+      get: Sinon.stub(),
+      set: Sinon.stub().returns()
+    }
   })
 
   afterEach(() => {
     Sinon.restore()
   })
 
-  describe('when called with a clear in the payload', () => {
-    it('clears them from yar', async () => {
-      payload = {
-        clearFilters: 'reset'
-      }
-      await SubmitNoticesIndexService.go(payload, yarStub)
+  describe('when called', () => {
+    describe('with the instruction to clear filters', () => {
+      beforeEach(() => {
+        payload = {
+          clearFilters: 'reset'
+        }
+      })
 
-      expect(yarStub.clear.called).to.be.true()
-      expect(yarStub.set.called).to.be.false()
+      it('returns a result that tells the controller to redirect to the index page', async () => {
+        const result = await SubmitIndexService.go(payload, yarStub)
+
+        expect(result).to.equal({})
+      })
+
+      it('clears the "noticesFilter" object from the session', async () => {
+        await SubmitIndexService.go(payload, yarStub)
+
+        expect(yarStub.clear.called).to.be.true()
+      })
     })
-  })
 
-  describe('when called with a valid payload', () => {
-    it('saves the submitted payload into yar', async () => {
-      payload = {
-        filterNotificationTypes: [
-          'legacy-notifications',
-          'returns-paper-form',
-          'returns-reminders',
-          'returns-invitation',
-          'water-abstraction-alert-resume',
-          'water-abstraction-alert-stop',
-          'water-abstraction-alert-reduce',
-          'water-abstraction-alert-warning'
-        ],
-        sentBy: 'test@test.com',
-        'sent-from-day': '1',
-        'sent-from-month': '1',
-        'sent-from-year': '2025',
-        'sent-to-day': '31',
-        'sent-to-month': '12',
-        'sent-to-year': '2025'
-      }
-      await SubmitNoticesIndexService.go(payload, yarStub)
+    describe('with an empty payload', () => {
+      beforeEach(() => {
+        payload = {}
+      })
 
-      expect(yarStub.clear.called).to.be.false()
-      expect(yarStub.set.called).to.be.true()
-      expect(yarStub.set.args[0][1]).to.equal({
-        filterNotificationTypes: [
-          'legacy-notifications',
-          'returns-paper-form',
-          'returns-reminders',
-          'returns-invitation',
-          'water-abstraction-alert-resume',
-          'water-abstraction-alert-stop',
-          'water-abstraction-alert-reduce',
-          'water-abstraction-alert-warning'
-        ],
-        sentBy: 'test@test.com',
-        sentFromDay: '1',
-        sentFromMonth: '1',
-        sentFromYear: '2025',
-        sentToDay: '31',
-        sentToMonth: '12',
-        sentToYear: '2025'
+      it('returns a result that tells the controller to redirect to the index page', async () => {
+        const result = await SubmitIndexService.go(payload, yarStub)
+
+        expect(result).to.equal({})
+      })
+
+      it('saves a default "noticesFilter" object in the session', async () => {
+        await SubmitIndexService.go(payload, yarStub)
+
+        const setArgs = yarStub.set.args[0]
+
+        expect(setArgs[0]).to.equal('noticesFilter')
+        expect(setArgs[1]).to.equal({
+          noticeTypes: [],
+          fromDate: undefined,
+          sentBy: null,
+          sentFromDay: null,
+          sentFromMonth: null,
+          sentFromYear: null,
+          sentToDay: null,
+          sentToMonth: null,
+          sentToYear: null,
+          toDate: undefined
+        })
+      })
+    })
+
+    describe('with a valid payload', () => {
+      beforeEach(() => {
+        payload = {
+          sentBy: 'carol.shaw@wrls.gov.uk',
+          sentFromDay: '1',
+          sentFromMonth: '4',
+          sentFromYear: '2023',
+          sentToDay: '31',
+          sentToMonth: '3',
+          sentToYear: '2024'
+        }
+      })
+
+      describe('but no notice types included', () => {
+        it('returns a result that tells the controller to redirect to the index page', async () => {
+          const result = await SubmitIndexService.go(payload, yarStub)
+
+          expect(result).to.equal({})
+        })
+
+        it('saves the submitted filters as the "noticesFilter" object in the session', async () => {
+          await SubmitIndexService.go(payload, yarStub)
+
+          const setArgs = yarStub.set.args[0]
+
+          expect(setArgs[0]).to.equal('noticesFilter')
+          expect(setArgs[1]).to.equal({
+            noticeTypes: [],
+            fromDate: '2023-04-01',
+            sentBy: payload.sentBy,
+            sentFromDay: payload.sentFromDay,
+            sentFromMonth: payload.sentFromMonth,
+            sentFromYear: payload.sentFromYear,
+            sentToDay: payload.sentToDay,
+            sentToMonth: payload.sentToMonth,
+            sentToYear: payload.sentToYear,
+            toDate: '2024-03-31'
+          })
+        })
+      })
+
+      describe('including a single notice type selected', () => {
+        beforeEach(() => {
+          payload.noticeTypes = 'stop'
+        })
+
+        it('returns a result that tells the controller to redirect to the index page', async () => {
+          const result = await SubmitIndexService.go(payload, yarStub)
+
+          expect(result).to.equal({})
+        })
+
+        it('saves the submitted filters as the "noticesFilter" object in the session', async () => {
+          await SubmitIndexService.go(payload, yarStub)
+
+          const setArgs = yarStub.set.args[0]
+
+          expect(setArgs[0]).to.equal('noticesFilter')
+          expect(setArgs[1]).to.equal({
+            noticeTypes: ['stop'],
+            fromDate: '2023-04-01',
+            sentBy: payload.sentBy,
+            sentFromDay: payload.sentFromDay,
+            sentFromMonth: payload.sentFromMonth,
+            sentFromYear: payload.sentFromYear,
+            sentToDay: payload.sentToDay,
+            sentToMonth: payload.sentToMonth,
+            sentToYear: payload.sentToYear,
+            toDate: '2024-03-31'
+          })
+        })
+      })
+
+      describe('including a multiple notice types selected', () => {
+        beforeEach(() => {
+          payload.noticeTypes = ['resume', 'stop']
+        })
+
+        it('returns a result that tells the controller to redirect to the index page', async () => {
+          const result = await SubmitIndexService.go(payload, yarStub)
+
+          expect(result).to.equal({})
+        })
+
+        it('saves the submitted filters as the "noticesFilter" object in the session', async () => {
+          await SubmitIndexService.go(payload, yarStub)
+
+          const setArgs = yarStub.set.args[0]
+
+          expect(setArgs[0]).to.equal('noticesFilter')
+          expect(setArgs[1]).to.equal({
+            noticeTypes: payload.noticeTypes,
+            fromDate: '2023-04-01',
+            sentBy: payload.sentBy,
+            sentFromDay: payload.sentFromDay,
+            sentFromMonth: payload.sentFromMonth,
+            sentFromYear: payload.sentFromYear,
+            sentToDay: payload.sentToDay,
+            sentToMonth: payload.sentToMonth,
+            sentToYear: payload.sentToYear,
+            toDate: '2024-03-31'
+          })
+        })
+      })
+    })
+
+    describe('with an invalid payload', () => {
+      beforeEach(() => {
+        payload = {
+          sentFromDay: '1',
+          sentFromMonth: '4'
+        }
+
+        yarStub.get.returns({ sentBy: 'billing.data@wrls.gov.uk' })
+
+        notice = NoticesFixture.alertStop()
+      })
+
+      describe('and the results are paginated', () => {
+        beforeEach(() => {
+          Sinon.stub(FetchNoticesService, 'go').resolves({ results: [notice], total: 70 })
+        })
+
+        it('extracts the previously saved filters', async () => {
+          await SubmitIndexService.go(payload, yarStub)
+
+          expect(yarStub.get.called).to.be.true()
+          expect(yarStub.set.called).to.be.false()
+        })
+
+        it('returns the page data for the view, including any errors', async () => {
+          const result = await SubmitIndexService.go(payload, yarStub, '2')
+
+          expect(result).to.equal(
+            {
+              activeNavBar: 'manage',
+              error: {
+                errorList: [{ href: '#fromDate', text: 'Enter a valid from date' }],
+                fromDate: { message: 'Enter a valid from date' }
+              },
+              filters: {
+                noticeTypes: [],
+                fromDate: '-04-01',
+                openFilter: true,
+                sentBy: 'billing.data@wrls.gov.uk',
+                sentFromDay: payload.sentFromDay,
+                sentFromMonth: payload.sentFromMonth,
+                toDate: undefined
+              },
+              notices: [
+                {
+                  createdDate: '25 March 2025',
+                  link: `/notifications/report/${notice.id}`,
+                  recipients: notice.recipientCount,
+                  sentBy: 'billing.data@wrls.gov.uk',
+                  status: 'sent',
+                  type: 'Stop - Water abstraction alert'
+                }
+              ],
+              numberOfNoticesDisplayed: 1,
+              totalNumberOfNotices: '70',
+              pageTitle: 'Notices (page 2 of 3)'
+            },
+            { skip: ['pagination'] }
+          )
+        })
+      })
+
+      describe('and the results are not paginated', () => {
+        beforeEach(() => {
+          Sinon.stub(FetchNoticesService, 'go').resolves({ results: [notice], total: 1 })
+        })
+
+        it('extracts the previously saved filters', async () => {
+          await SubmitIndexService.go(payload, yarStub)
+
+          expect(yarStub.get.called).to.be.true()
+          expect(yarStub.set.called).to.be.false()
+        })
+
+        it('returns the page data for the view, including any errors', async () => {
+          const result = await SubmitIndexService.go(payload, yarStub)
+
+          expect(result).to.equal(
+            {
+              activeNavBar: 'manage',
+              error: {
+                errorList: [{ href: '#fromDate', text: 'Enter a valid from date' }],
+                fromDate: { message: 'Enter a valid from date' }
+              },
+              filters: {
+                noticeTypes: [],
+                fromDate: '-04-01',
+                openFilter: true,
+                sentBy: 'billing.data@wrls.gov.uk',
+                sentFromDay: payload.sentFromDay,
+                sentFromMonth: payload.sentFromMonth,
+                toDate: undefined
+              },
+              notices: [
+                {
+                  createdDate: '25 March 2025',
+                  link: `/notifications/report/${notice.id}`,
+                  recipients: notice.recipientCount,
+                  sentBy: 'billing.data@wrls.gov.uk',
+                  status: 'sent',
+                  type: 'Stop - Water abstraction alert'
+                }
+              ],
+              numberOfNoticesDisplayed: 1,
+              totalNumberOfNotices: '1',
+              pageTitle: 'Notices'
+            },
+            { skip: ['pagination'] }
+          )
+        })
       })
     })
   })
 
-  describe('when called with an empty valid payload', () => {
-    it('saves the submitted payload into yar', async () => {
-      payload = {}
+  // describe('when called with a clear in the payload', () => {
+  //   it('clears them from yar', async () => {
+  //     payload = {
+  //       clearFilters: 'reset'
+  //     }
+  //     await SubmitIndexService.go(payload, yarStub)
 
-      await SubmitNoticesIndexService.go(payload, yarStub)
+  //     expect(yarStub.clear.called).to.be.true()
+  //     expect(yarStub.set.called).to.be.false()
+  //   })
+  // })
 
-      expect(yarStub.clear.called).to.be.false()
-      expect(yarStub.set.called).to.be.true()
-      expect(yarStub.set.args[0][1]).to.equal({
-        filterNotificationTypes: undefined,
-        sentBy: undefined,
-        sentFromDay: null,
-        sentFromMonth: null,
-        sentFromYear: null,
-        sentToDay: null,
-        sentToMonth: null,
-        sentToYear: null
-      })
-    })
-  })
+  // describe('when called with a valid payload', () => {
+  //   it('saves the submitted payload into yar', async () => {
+  //     payload = {
+  //       filterNotificationTypes: [
+  //         'legacy-notifications',
+  //         'returns-paper-form',
+  //         'returns-reminders',
+  //         'returns-invitation',
+  //         'water-abstraction-alert-resume',
+  //         'water-abstraction-alert-stop',
+  //         'water-abstraction-alert-reduce',
+  //         'water-abstraction-alert-warning'
+  //       ],
+  //       sentBy: 'test@test.com',
+  //       'sent-from-day': '1',
+  //       'sent-from-month': '1',
+  //       'sent-from-year': '2025',
+  //       'sent-to-day': '31',
+  //       'sent-to-month': '12',
+  //       'sent-to-year': '2025'
+  //     }
+  //     await SubmitIndexService.go(payload, yarStub)
+
+  //     expect(yarStub.clear.called).to.be.false()
+  //     expect(yarStub.set.called).to.be.true()
+  //     expect(yarStub.set.args[0][1]).to.equal({
+  //       filterNotificationTypes: [
+  //         'legacy-notifications',
+  //         'returns-paper-form',
+  //         'returns-reminders',
+  //         'returns-invitation',
+  //         'water-abstraction-alert-resume',
+  //         'water-abstraction-alert-stop',
+  //         'water-abstraction-alert-reduce',
+  //         'water-abstraction-alert-warning'
+  //       ],
+  //       sentBy: 'test@test.com',
+  //       sentFromDay: '1',
+  //       sentFromMonth: '1',
+  //       sentFromYear: '2025',
+  //       sentToDay: '31',
+  //       sentToMonth: '12',
+  //       sentToYear: '2025'
+  //     })
+  //   })
+  // })
+
+  // describe('when called with an empty valid payload', () => {
+  //   it('saves the submitted payload into yar', async () => {
+  //     payload = {}
+
+  //     await SubmitIndexService.go(payload, yarStub)
+
+  //     expect(yarStub.clear.called).to.be.false()
+  //     expect(yarStub.set.called).to.be.true()
+  //     expect(yarStub.set.args[0][1]).to.equal({
+  //       filterNotificationTypes: undefined,
+  //       sentBy: undefined,
+  //       sentFromDay: null,
+  //       sentFromMonth: null,
+  //       sentFromYear: null,
+  //       sentToDay: null,
+  //       sentToMonth: null,
+  //       sentToYear: null
+  //     })
+  //   })
+  // })
 })
