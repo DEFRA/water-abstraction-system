533a3f3e0f3b04dfbe15284c350501c4f1199119
jonathangoulding <goulding.jon@icloud.com>
2025-05-21 20:28:55 +0000

    Add notices determines relevant licence monitoring stations
    https://eaflood.atlassian.net/browse/WATER-5067
    
    We previously added functionality to save the licence refs to the session in the 'SubmitCheckLicenceMatchesService'. This was to make the code simpler on the recipients part of the journey.
    
    We have found another use case of adding the 'relevant' licence monitoring stations to the session.
    
    This change adds a new session object 'relevantLicenceMonitoringStations' to the session for the 'abstraction-alert' journey. This will be used by the recipient to send notifications to the stations recipients.
    
    As this has complex functionality and needed in two places it has been added / tested as its own service.
    
    A few tweaks to the wording has been made to better align with what it is actually doing. They are not thresholds but licence monitoring stations.

--- app/services/notices/setup/abstraction-alerts/submit-check-licence-matches.service.js
+++ app/services/notices/setup/abstraction-alerts/submit-check-licence-matches.service.js
@@ -8,6 +8,7 @@
 
 const CheckLicenceMatchesPresenter = require('../../../../presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.js')
 const SessionModel = require('../../../../models/session.model.js')
+const DetermineRelevantLicenceMonitoringStationsService = require('./determine-relevant-licence-monitoring-stations.service.js')
 
 /**
  * Orchestrates saving the data for the `/notices/setup/{sessionId}/abstraction-alerts/check-licence-matches` page
@@ -23,12 +24,21 @@ async function go(sessionId) {
   await _save(session, pageData)
 }
 
-async function _save(session, pageData) {
-  const relevantLicenceRefs = pageData.restrictions.map((restriction) => {
-    return restriction.licenceRef
+async function _save(session) {
+  const { alertThresholds, licenceMonitoringStations, removedThresholds } = session
+
+  const relevantLicenceMonitoringStations = DetermineRelevantLicenceMonitoringStationsService.go(
+    licenceMonitoringStations,
+    alertThresholds,
+    removedThresholds
+  )
+
+  const relevantLicenceRefs = relevantLicenceMonitoringStations.map((station) => {
+    return station.licence.licenceRef
   })
 
   session.licenceRefs = Array.from(new Set(relevantLicenceRefs))
+  session.relevantLicenceMonitoringStations = relevantLicenceMonitoringStations
 
   return session.$update()
 }
