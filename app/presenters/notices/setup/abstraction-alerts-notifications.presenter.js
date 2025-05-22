d64e9f2b0761f5f6d62f9208e36663fe8a35662b
jonathangoulding <goulding.jon@icloud.com>
2025-05-22 10:27:28 +0000

    feat: iterate each licence monitoring station to create notification for each recipient

--- app/presenters/notices/setup/abstraction-alerts-notifications.presenter.js
+++ app/presenters/notices/setup/abstraction-alerts-notifications.presenter.js
@@ -10,10 +10,13 @@ const { notifyTemplates } = require('../../../lib/notify-templates.lib.js')
 const { contactName, contactAddress } = require('../../crm.presenter.js')
 
 /**
+ * Formats recipients into notifications for an abstraction alert
  *
- * @param recipients
- * @param session
- * @param eventId
+ * @param {Array<object>} recipients
+ * @param {SessionModel} session - The session instance
+ * @param {string} eventId
+ *
+ * @returns {Array<object>} notifications
  */
 function go(recipients, session, eventId) {
   const notifications = []
@@ -106,22 +109,6 @@ function _letter(recipient, referenceCode, eventId) {
 function _letterTemplate() {
   return notifyTemplates['abstraction-alerts'].reduceWarning
 }
-
-//      water_abstraction_alert_reduce_warning: '27499bbd-e854-4f13-884e-30e0894526b6',
-//       water_abstraction_alert_reduce_or_stop_warning: '8c77274f-6a61-46a5-82d8-66863320d608',
-//       water_abstraction_alert_stop_warning: '7ab10c86-2c23-4376-8c72-9419e7f982bb',
-//       water_abstraction_alert_reduce: 'fafe7d77-7710-46c8-b870-3b5c1e3816d2',
-//       water_abstraction_alert_reduce_or_stop: '2d81eaa7-0c34-463b-8ac2-5ff37d5bd800',
-//       water_abstraction_alert_stop: 'c2635893-0dd7-4fff-a152-774707e2175e',
-//       water_abstraction_alert_resume: 'ba6b11ad-41fc-4054-87eb-7e9a168ceec2',
-//
-//       water_abstraction_alert_reduce_warning_email: '6ec7265d-8ebb-4217-a62b-9bf0216f8c9f',
-//       water_abstraction_alert_reduce_or_stop_warning_email: 'bf32327a-f170-4854-8abb-3068aee9cdec',
-//       water_abstraction_alert_stop_warning_email: 'a51ace39-3224-4c18-bbb8-c803a6da9a21',
-//       water_abstraction_alert_reduce_email: 'd94bf110-b173-4f77-8e9a-cf7b4f95dc00',
-//       water_abstraction_alert_reduce_or_stop_email: '4ebf29e1-f819-4d88-b7e4-ee47df302b9a',
-//       water_abstraction_alert_stop_email: 'd7468ba1-ac65-42c4-9785-8998f9c34e01',
-//       water_abstraction_alert_resume_email: '5eae5e5b-4f9a-4e2e-8d1e-c8d083533fbf',
 
 module.exports = {
   go
