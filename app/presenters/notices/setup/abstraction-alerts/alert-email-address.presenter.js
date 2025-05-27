3162c08e3c23d18a8294b162843906432007cf50
Roble <roblesiyad123@gmail.com>
2025-05-27 13:31:17 +0100

    Update abstraction alert email address (#2027)
    * Update abstraction alert email address
    
    https://eaflood.atlassian.net/browse/WATER-5058
    
    Previously, we added the scaffolding for the abstraction alert email address. We now have to add the logic to get the
    users email address, with validation and saving it to the session.
    
    This PR adds the logic for the abstraction alert email address.

--- app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
+++ app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
@@ -8,10 +8,17 @@
 /**
  * Formats data for the `/notices/setup/{sessionId}/abstraction-alerts/alert-email-address` page
  *
+ * @param {module:SessionModel} session - The session instance
+ * @param {object} auth - The auth object taken from `request.auth` containing user details
+ *
  * @returns {object} - The data formatted for the view template
  */
-function go() {
-  return {}
+function go(session, auth) {
+  return {
+    caption: session.monitoringStationName,
+    pageTitle: 'Select an email address to include in the alerts',
+    username: auth.credentials.user.username
+  }
 }
 
 module.exports = {
