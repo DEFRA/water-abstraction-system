7070b958531c921bd6165a372eadab78445ab3fd
rvsiyad <roblesiyad123@gmail.com>
2025-06-03 14:52:37 +0100

    Add validationResult to alert email address presenter to determine anchor link

--- app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
+++ app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
@@ -10,15 +10,30 @@
  *
  * @param {module:SessionModel} session - The session instance
  * @param {object} auth - The auth object taken from `request.auth` containing user details
+ * @param {object} validationResult - The validation results from the payload
  *
  * @returns {object} - The data formatted for the view template
  */
-function go(session, auth) {
+function go(session, auth, validationResult) {
   return {
+    anchor: _anchor(validationResult),
+    backLink: `/system/notices/setup/${session.id}/abstraction-alerts/check-licence-matches`,
     caption: session.monitoringStationName,
     pageTitle: 'Select an email address to include in the alerts',
     username: auth.credentials.user.username
   }
+}
+
+function _anchor(validationResult) {
+  if (!validationResult) {
+    return null
+  }
+
+  if (validationResult.radioFormError) {
+    return '#alertEmailAddress'
+  }
+
+  return '#otherUser'
 }
 
 module.exports = {
