e7036ddaa5ab80d3045fca57c06ec2f1ef3999e8
robertparkinson <robert@daedalusft.com>
2025-06-05 09:02:03 +0100

    add pagination and unit tests

--- test/presenters/notices/view.presenter.test.js
+++ test/presenters/notices/view.presenter.test.js
@@ -4,99 +4,163 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it } = (exports.lab = Lab.script())
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
+
+const EventModel = require('../../../app/models/event.model.js')
 
 // Thing under test
 const ViewPresenter = require('../../../app/presenters/notices/view.presenter.js')
 
 describe('Notices - View Notice presenter', () => {
-  const notices = [
-    {
-      messageType: 'letter',
-      messageRef: 'returns_reminder_returns_to_letter',
-      event: {
-        referenceCode: 'PRTF-VPV3J9',
-        issuer: 'test@wrls.gov.uk',
-        createdAt: new Date('2025-02-21T14:52:18.000Z'),
-        status: 'completed'
-      },
-      licences: ['AT/TST/MONTHLY/02'],
-      personalisation: {
-        address_line_1: 'Big Farm Co Ltd',
-        address_line_2: 'Big Farm',
-        address_line_3: 'Windy road',
-        address_line_4: 'Buttercup meadow',
-        address_line_5: 'Buttercup Village',
-        postcode: 'TT1 1TT'
-      },
-      recipient: 'n/a',
-      status: 'completed'
-    },
-    {
-      messageType: 'letter',
-      messageRef: 'returns_reminder_returns_to_letter',
-      event: {
-        referenceCode: 'PRTF-VPV3J9',
-        issuer: 'test@wrls.gov.uk',
-        createdAt: new Date('2025-02-21T14:52:18.000Z'),
-        status: 'completed'
-      },
-      licences: ['AT/TST/MONTHLY/02'],
-      recipient: 'test@wrls.gov.uk',
-      status: 'completed'
-    },
-    {
-      messageType: 'letter',
-      messageRef: 'returns_reminder_returns_to_letter',
-      event: {
-        referenceCode: 'PRTF-VPV3J9',
-        issuer: 'test@wrls.gov.uk',
-        createdAt: new Date('2025-02-21T14:52:18.000Z'),
-        status: 'completed'
-      },
-      licences: ['AT/TST/MONTHLY/02'],
-      personalisation: {
-        address_line_1: 'The Farm Co Ltd',
-        address_line_2: 'Big Farm',
-        address_line_3: 'Windy road',
-        address_line_4: 'Buttercup meadow',
-        address_line_5: 'Buttercup Village',
-        postcode: 'TT1 1TT'
-      },
-      recipient: null,
-      status: 'completed'
-    }
-  ]
-
-  it('correctly presents the dataw', () => {
-    const result = ViewPresenter.go(notices)
-
-    expect(result).to.equal({
-      createdBy: 'test@wrls.gov.uk',
-      dateCreated: '21 February 2025',
-      reference: 'PRTF-VPV3J9',
-      notices: [
-        {
-          recipient: ['Big Farm Co Ltd', 'Big Farm', 'Windy road', 'Buttercup meadow', 'Buttercup Village', 'TT1 1TT'],
-          licenceRefs: ['AT/TST/MONTHLY/02'],
-          messageType: 'letter',
-          status: 'completed'
+  const notices = {
+    event: EventModel.fromJson({
+      id: 'a40dcb94-cb01-4fce-9a46-94b49eca2057',
+      referenceCode: 'PRTF-VPV3J9',
+      issuer: 'test@wrls.gov.uk',
+      createdAt: new Date('2025-02-21T14:52:18.000Z'),
+      status: 'completed',
+      subtype: 'return reminder'
+    }),
+    results: [
+      {
+        recipient_name: 'Big Farm Co Ltd',
+        message_type: 'letter',
+        personalisation: {
+          address_line_1: 'Big Farm Co Ltd',
+          address_line_2: 'Big Farm',
+          address_line_3: 'Windy road',
+          address_line_4: 'Buttercup meadow',
+          address_line_5: 'Buttercup Village',
+          postcode: 'TT1 1TT'
         },
-        {
-          recipient: 'test@wrls.gov.uk',
-          licenceRefs: ['AT/TST/MONTHLY/02'],
-          messageType: 'letter',
-          status: 'completed'
+        status: 'completed',
+        licences: "['AT/TST/MONTHLY/02']",
+        recipient: 'n/a'
+      },
+      {
+        recipient_name: 'test@wrls.gov.uk',
+        message_type: 'email',
+        status: 'completed',
+        licences: "['AT/TST/MONTHLY/02']",
+        recipient: 'test@wrls.gov.uk'
+      },
+      {
+        recipient_name: 'Big Farm Co Ltd',
+        message_type: 'letter',
+        personalisation: {
+          address_line_1: 'Big Farm Co Ltd',
+          address_line_2: 'Big Farm',
+          address_line_3: 'Windy road',
+          address_line_4: 'Buttercup meadow',
+          address_line_5: 'Buttercup Village',
+          postcode: 'TT1 1TT'
         },
-        {
-          recipient: ['The Farm Co Ltd', 'Big Farm', 'Windy road', 'Buttercup meadow', 'Buttercup Village', 'TT1 1TT'],
-          licenceRefs: ['AT/TST/MONTHLY/02'],
-          messageType: 'letter',
-          status: 'completed'
-        }
-      ],
-      status: 'completed'
+        status: 'completed',
+        licences: "['AT/TST/MONTHLY/02']",
+        recipient: null
+      }
+    ]
+  }
+
+  describe('when a notice exists', () => {
+    it('correctly presents the data for display', () => {
+      const result = ViewPresenter.go(notices, 1)
+
+      expect(result).to.equal({
+        createdBy: 'test@wrls.gov.uk',
+        dateCreated: '21 February 2025',
+        reference: 'PRTF-VPV3J9',
+        notices: [
+          {
+            recipient: [
+              'Big Farm Co Ltd',
+              'Big Farm',
+              'Windy road',
+              'Buttercup meadow',
+              'Buttercup Village',
+              'TT1 1TT'
+            ],
+            licenceRefs: "['AT/TST/MONTHLY/02']",
+            messageType: 'letter',
+            status: 'completed'
+          },
+          {
+            recipient: 'test@wrls.gov.uk',
+            licenceRefs: "['AT/TST/MONTHLY/02']",
+            messageType: 'email',
+            status: 'completed'
+          },
+          {
+            recipient: [
+              'Big Farm Co Ltd',
+              'Big Farm',
+              'Windy road',
+              'Buttercup meadow',
+              'Buttercup Village',
+              'TT1 1TT'
+            ],
+            licenceRefs: "['AT/TST/MONTHLY/02']",
+            messageType: 'letter',
+            status: 'completed'
+          }
+        ],
+        status: 'completed'
+      })
+    })
+  })
+
+  describe('when a notice does not exist and has paginated results', () => {
+    beforeEach(async () => {
+      const emptyObjects = Array.from({ length: 25 }, () => {
+        return {}
+      })
+      notices.results.unshift(...emptyObjects)
+    })
+
+    it('correctly presents the data for display', () => {
+      const result = ViewPresenter.go(notices, 2)
+
+      expect(result).to.equal({
+        createdBy: 'test@wrls.gov.uk',
+        dateCreated: '21 February 2025',
+        reference: 'PRTF-VPV3J9',
+        notices: [
+          {
+            recipient: [
+              'Big Farm Co Ltd',
+              'Big Farm',
+              'Windy road',
+              'Buttercup meadow',
+              'Buttercup Village',
+              'TT1 1TT'
+            ],
+            licenceRefs: "['AT/TST/MONTHLY/02']",
+            messageType: 'letter',
+            status: 'completed'
+          },
+          {
+            recipient: 'test@wrls.gov.uk',
+            licenceRefs: "['AT/TST/MONTHLY/02']",
+            messageType: 'email',
+            status: 'completed'
+          },
+          {
+            recipient: [
+              'Big Farm Co Ltd',
+              'Big Farm',
+              'Windy road',
+              'Buttercup meadow',
+              'Buttercup Village',
+              'TT1 1TT'
+            ],
+            licenceRefs: "['AT/TST/MONTHLY/02']",
+            messageType: 'letter',
+            status: 'completed'
+          }
+        ],
+        status: 'completed'
+      })
     })
   })
 })
--- test/services/notices/view.service.test.js
+++ test/services/notices/view.service.test.js
@@ -3,23 +3,194 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
-const { describe, it, afterEach } = (exports.lab = Lab.script())
+const { describe, it, afterEach, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
+const EventModel = require('../../../app/models/event.model.js')
+
+// Things to stub
+const FetchNoticeService = require('../../../app/services/notices/fetch-notice.service.js')
 
 // Thing under test
 const ViewService = require('../../../app/services/notices/view.service.js')
 
-describe('Notices - Index Notices service', () => {
-  afterEach(() => {})
+describe('Notices - View Notice service', () => {
+  const fetchResults = {
+    event: EventModel.fromJson({
+      id: 'a40dcb94-cb01-4fce-9a46-94b49eca2058',
+      referenceCode: 'RREM-RD2KF4',
+      issuer: 'test.user@defra.gov.uk',
+      createdAt: new Date('2025-02-21T14:52:18.000Z'),
+      status: 'sent',
+      subtype: 'returnReminder'
+    }),
+    results: [
+      {
+        recipient_name: 'Big Farm Co Ltd',
+        message_type: 'letter',
+        personalisation: {
+          address_line_1: 'Big Farm Co Ltd',
+          address_line_2: 'Big Farm',
+          address_line_3: 'Windy road',
+          address_line_4: 'Buttercup meadow',
+          address_line_5: 'Buttercup Village',
+          postcode: 'TT1 1TT'
+        },
+        status: 'completed',
+        licences: "['AT/TST/MONTHLY/02']",
+        recipient: 'n/a'
+      }
+    ]
+  }
 
-  describe('when called', () => {
+  // The presenter tests cover the different variations of what can be displayed
+  const expectedResultCommon = {
+    activeNavBar: 'manage',
+    createdBy: 'test.user@defra.gov.uk',
+    reference: 'RREM-RD2KF4',
+    dateCreated: '21 February 2025',
+    status: 'sent',
+    notices: [
+      {
+        messageType: 'letter',
+        status: 'completed',
+        licenceRefs: "['AT/TST/MONTHLY/02']",
+        recipient: ['Big Farm Co Ltd', 'Big Farm', 'Windy road', 'Buttercup meadow', 'Buttercup Village', 'TT1 1TT']
+      }
+    ],
+    numberOfRecipients: 1,
+    numberShowing: 1,
+    pagination: { numberOfPages: 1 },
+    pageNumbers: 'Showing all notifications',
+    pageTitle: 'Returns reminders'
+  }
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when called with a valid id, returnInvitation type and no page number', () => {
+    beforeEach(() => {
+      fetchResults.event.subtype = 'returnInvitation'
+      Sinon.stub(FetchNoticeService, 'go').resolves(fetchResults)
+    })
+
     it('returns page data for the view', async () => {
-      const result = await ViewService.go()
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2058')
 
-      expect(result).to.equal({})
+      expect(result).to.equal({ ...expectedResultCommon, pageTitle: 'Returns invitations' })
+    })
+  })
+
+  describe('when called with a valid id, returnReminder type and no page number', () => {
+    beforeEach(() => {
+      fetchResults.event.subtype = 'returnReminder'
+      Sinon.stub(FetchNoticeService, 'go').resolves(fetchResults)
+    })
+
+    it('returns page data for the view', async () => {
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2058')
+
+      expect(result).to.equal({ ...expectedResultCommon, pageTitle: 'Returns reminders' })
+    })
+  })
+
+  describe('when called with a valid id, adHocReminder type and page 1', () => {
+    beforeEach(() => {
+      fetchResults.event.subtype = 'adHocReminder'
+      Sinon.stub(FetchNoticeService, 'go').resolves(fetchResults, 1)
+    })
+
+    it('returns page data for the view', async () => {
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2058')
+
+      expect(result).to.equal({ ...expectedResultCommon, pageTitle: 'Ad-hoc notice' })
+    })
+  })
+
+  describe('when called with a valid id, waterAbstractionAlerts type and page 1', () => {
+    beforeEach(() => {
+      fetchResults.event.subtype = 'waterAbstractionAlerts'
+      Sinon.stub(FetchNoticeService, 'go').resolves(fetchResults, 1)
+    })
+
+    it('returns page data for the view', async () => {
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2058')
+
+      expect(result).to.equal({ ...expectedResultCommon, pageTitle: 'Water abstraction alert' })
+    })
+  })
+
+  describe('when called with a valid id, undefinded subtype and page 1', () => {
+    beforeEach(() => {
+      fetchResults.event.subtype = undefined
+      Sinon.stub(FetchNoticeService, 'go').resolves(fetchResults, 1)
+    })
+
+    it('returns page data for the view', async () => {
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2058')
+
+      expect(result).to.equal({ ...expectedResultCommon, pageTitle: 'Notifications' })
+    })
+  })
+
+  describe('when called with a valid id and the second page', () => {
+    beforeEach(() => {
+      fetchResults.event.subtype = 'returnReminder'
+      Sinon.stub(FetchNoticeService, 'go').resolves(fetchResults)
+      const emptyObjects = Array.from({ length: 25 }, () => {
+        return {}
+      })
+      fetchResults.results.unshift(...emptyObjects)
+    })
+
+    it('returns page data for the view', async () => {
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2058', '2')
+
+      expect(result).to.equal({
+        ...expectedResultCommon,
+        numberOfRecipients: 26,
+        numberShowing: 25,
+        pagination: {
+          numberOfPages: 2,
+          component: {
+            previous: { href: '/system/notices/a40dcb94-cb01-4fce-9a46-94b49eca2058?page=1' },
+            items: [
+              {
+                number: 1,
+                visuallyHiddenText: 'Page 1',
+                href: '/system/notices/a40dcb94-cb01-4fce-9a46-94b49eca2058',
+                current: false
+              },
+              {
+                number: 2,
+                visuallyHiddenText: 'Page 2',
+                href: '/system/notices/a40dcb94-cb01-4fce-9a46-94b49eca2058?page=2',
+                current: true
+              }
+            ]
+          }
+        },
+        pageNumbers: 'Showing page 2 of 2 notifications'
+      })
+    })
+  })
+
+  describe('when called with an id that returns no results', () => {
+    beforeEach(() => {
+      Sinon.stub(FetchNoticeService, 'go').resolves(undefined)
+    })
+
+    it('returns bare minimum page data for the view', async () => {
+      const result = await ViewService.go('a40dcb94-cb01-4fce-9a46-94b49eca2057', 1)
+
+      expect(result).to.equal({
+        activeNavBar: 'manage',
+        pageTitle: 'Notifications'
+      })
     })
   })
 })
