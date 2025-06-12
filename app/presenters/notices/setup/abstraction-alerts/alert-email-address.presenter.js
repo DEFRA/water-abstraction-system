4ae82b1831d0f0b925b61ece04e455feb4f769f1
Roble <roblesiyad123@gmail.com>
2025-06-12 17:21:33 +0100

    Update AA email address page to check submitted values (#2045)
    * Update AA email address to check submitted values
    
    https://eaflood.atlassian.net/browse/WATER-5058
    
    Previously we added the logic for validating the payload and displaying errors for the abstraction alert email address page. We now have to add the logic to check previously selected values saved in the session.
    
    This PR adds the logic to redisplay checked radio buttons for the abstraction alert email address.

--- app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
+++ app/presenters/notices/setup/abstraction-alerts/alert-email-address.presenter.js
@@ -11,16 +11,33 @@
  * @param {module:SessionModel} session - The session instance
  * @param {object} auth - The auth object taken from `request.auth` containing user details
  * @param {object} validationResult - The validation results from the payload
+ * @param {object} payload - The submitted form data
  *
  * @returns {object} - The data formatted for the view template
  */
 function go(session, auth, validationResult) {
+  const { alertEmailAddress, alertEmailAddressType, monitoringStationName, id: sessionId } = session
+
   return {
+    alertEmailAddressOptions: _alertEmailAddressOptions(alertEmailAddress, alertEmailAddressType),
     anchor: _anchor(validationResult),
-    backLink: `/system/notices/setup/${session.id}/abstraction-alerts/check-licence-matches`,
-    caption: session.monitoringStationName,
+    backLink: `/system/notices/setup/${sessionId}/abstraction-alerts/check-licence-matches`,
+    caption: monitoringStationName,
     pageTitle: 'Select an email address to include in the alerts',
     username: auth.credentials.user.username
+  }
+}
+
+function _alertEmailAddressOptions(alertEmailAddress, alertEmailAddressType) {
+  const usernameChecked = alertEmailAddressType === 'username'
+  const otherUserChecked = alertEmailAddressType === 'other'
+
+  const otherUserEmailAddressInput = otherUserChecked && alertEmailAddress ? alertEmailAddress : ''
+
+  return {
+    otherUserChecked,
+    otherUserEmailAddressInput,
+    usernameChecked
   }
 }
 
