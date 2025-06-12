eab0f7da20c75ef412fca34b7d4c10de3ff4b681
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-12 22:30:58 +0100

    Update process orchestrator

--- app/services/jobs/clean/process-clean.service.js
+++ app/services/jobs/clean/process-clean.service.js
@@ -6,6 +6,7 @@
  */
 
 const CleanExpiredSessionsService = require('./clean-expired-sessions.service.js')
+const CleanEmptyVoidReturnLogsService = require('./clean-empty-void-return-logs.service.js')
 const { calculateAndLogTimeTaken, currentTimeInNanoseconds } = require('../../../lib/general.lib.js')
 
 /**
@@ -15,10 +16,11 @@ async function go() {
   try {
     const startTime = currentTimeInNanoseconds()
 
+    const emptyVoidReturnLogsCount = await CleanEmptyVoidReturnLogsService.go()
     const expiredSessionsCount = await CleanExpiredSessionsService.go()
 
     calculateAndLogTimeTaken(startTime, 'Clean job complete', {
-      counts: { expiredSessions: expiredSessionsCount }
+      counts: { emptyVoidReturnLogs: emptyVoidReturnLogsCount, expiredSessions: expiredSessionsCount }
     })
   } catch (error) {
     global.GlobalNotifier.omfg('Clean job failed', {}, error)
