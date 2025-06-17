2d7849337d6540ec46edadcdec0bf4e1a3433d1d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-17 08:27:03 +0100

    Update FetchPurposes to use relevant licence ver
    This is the update to FetchPurposesService to get it to find the relevant licence version in order to pick purposes to show to the user.
    
    However, this change has made us realise this is not the way to go. We now need to update the unit tests for this service to ensure they cover this complexity. We realised if we continue with this approach, updating each service with the logic needed, not only do they risk looking as complex as this, but it means lots of work and duplication when it comes to the testing.
    
    So, we are going in a different direction!

--- app/services/return-versions/setup/fetch-purposes.service.js
+++ app/services/return-versions/setup/fetch-purposes.service.js
@@ -10,22 +10,53 @@ const PurposeModel = require('../../../models/purpose.model.js')
 /**
  * Fetches a licence's purposes needed for `/return-versions/setup/{sessionId}/purpose` page
  *
+ * Specifically, we need to look for the 'relevant' licence version for the start date the user has entered. That will
+ * then give us the licence version purposes, which we use in a WHERE EXISTS clause to determine which purposes to
+ * fetch and show to the user.
+ *
+ * We find the 'relevant' licence version for the start date the user has entered, by filtering for those where the end
+ * date is null or greater then sorting them ascending order (oldest at the top).
+ *
+ * If a licence only has one 'current' licence version (it will have a null end date) then it will be the one selected.
+ *
+ * If it has a superseded licence version, but the start date is greater than its end date, we still just get the
+ * 'current' version returned. Else the first licence version with an end date equal to or greater than our start date
+ * is the version that will be used.
+ *
  * @param {string} licenceId - The UUID for the licence to fetch
+ * @param {Date} startDate - The start date the user has selected for the new return version, needed to find the
+ * relevant licence version
  *
  * @returns {Promise<object>} The distinct purposes for the matching licence's current version
  */
-async function go(licenceId) {
-  return _fetch(licenceId)
-}
-
-async function _fetch(licenceId) {
+async function go(licenceId, startDate) {
   return PurposeModel.query()
     .select(['purposes.id', 'purposes.description'])
-    .whereExists(
-      PurposeModel.relatedQuery('licenceVersionPurposes')
-        .innerJoin('licenceVersions', 'licenceVersions.id', 'licenceVersionPurposes.licenceVersionId')
-        .where('licenceVersions.licenceId', licenceId)
-        .where('licenceVersions.status', 'current')
+    .whereRaw(
+      `
+        EXISTS (
+          SELECT
+            1
+          FROM
+            public.licence_version_purposes lvp
+          INNER JOIN (
+            SELECT
+              lv.id
+            FROM
+              public.licence_versions lv
+            WHERE
+              lv.licence_id = ?
+              AND (
+                lv.end_date IS NULL OR lv.end_date >= ?
+              )
+            ORDER BY lv.end_date ASC
+            LIMIT 1
+          ) relevant_lic_ver ON relevant_lic_ver.id = lvp.licence_version_id
+          WHERE
+            lvp.purpose_id = purposes.id
+        )
+      `,
+      [licenceId, startDate]
     )
     .orderBy([{ column: 'purposes.description', order: 'asc' }])
 }
