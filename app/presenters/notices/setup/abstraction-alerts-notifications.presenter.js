cb37a7542be027973ec118d4d8fdfe22dc924474
jonathangoulding <goulding.jon@icloud.com>
2025-06-17 13:00:15 +0000

    Fix abstraction alerts multiple licence refs
    https://eaflood.atlassian.net/browse/WATER-5067
    
    When we get the 'recipients', we receive the associated licence refs in a comma separate string e.g 'AB12,BC,56'.
    
    When this occurred we were not handling this a comma seperated string but as a licence ref. The download logic had already solved this problem but the implementation was missed in the notices.
    
    This change updates the abstraction alert presenter to check if the licence monitoring stations licence ref is in the recipients  licence refs.

--- app/presenters/notices/setup/abstraction-alerts-notifications.presenter.js
+++ app/presenters/notices/setup/abstraction-alerts-notifications.presenter.js
@@ -248,9 +248,12 @@ function _licences(licenceRefs) {
 }
 
 /**
- * Each licence monitoring station has a licence ref. Multiple stations could have the licence ref.
+ * Matches a recipient to a licence monitoring station by the licence ref.
  *
- * When finding the recipients for the station we do so by the licence ref.
+ * 'recipient.licence_refs' will be a comma seperated string: 'licenceOne,licenceTwo'.
+ *
+ * To match a recipient to a licence monitoring station we use the licence monitoring stations licenceRef and check if
+ * the licence ref is present in the 'recipient.licence_refs' string.
  *
  * This does mean that a recipient can / will receive multiple notifications from different licence monitoring stations.
  *
@@ -258,7 +261,7 @@ function _licences(licenceRefs) {
  */
 function _matchingRecipients(recipients, station) {
   return recipients.filter((recipient) => {
-    return recipient.licence_refs === station.licence.licenceRef
+    return recipient.licence_refs.split(',').includes(station.licence.licenceRef)
   })
 }
 
