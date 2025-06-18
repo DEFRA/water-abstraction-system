fe1288785d063b74fbdb9a0d98bad25269af6067
jonathangoulding <goulding.jon@icloud.com>
2025-06-18 09:05:22 +0000

    feat: handle failed notice

--- app/services/jobs/notifications/update-abstraction-alerts.service.js
+++ app/services/jobs/notifications/update-abstraction-alerts.service.js
@@ -12,22 +12,37 @@ const { timestampForPostgres } = require('../../../lib/general.lib.js')
  * Orchestrates the process of updating the licence monitoring stations last abstraction alert.
  *
  * When a notification is for water abstraction alerts it will have the 'messageRef' set as something like:
- * water_abstraction_alert_resume_email. When this is present we need to update the licence monitoring stations 'status'
+ * 'water_abstraction_alert_resume_email'. When this is present we need to update the licence monitoring stations 'status'
  * and 'statusUpdatedAt'. This signifies the last abstraction alert sent.
+ *
+ * If a notification has failed to send (Notify has sent an error) then we do not update.
  *
  * @param {object[]} notifications
  */
 async function go(notifications) {
-  for (const notification of notifications) {
-    if (notification.messageRef?.includes('water_abstraction_alert') && notification.status !== 'error')
-      await _update(notification.personalisation.licenceMonitoringStationId, notification.personalisation.alertType)
-  }
+  const toUpdateStations = _stations(notifications)
+
+  await Promise.all(toUpdateStations)
 }
 
 async function _update(id, status) {
   await LicenceMonitoringStationModel.query().findById(id).patch({
     status,
     statusUpdatedAt: timestampForPostgres()
+  })
+}
+
+/**
+ * Determine which notification are for the water abstraction alerts and have not failed to send.
+ * @private
+ */
+function _stations(notifications) {
+  const stationsToUpdate = notifications.filter((notification) => {
+    return notification.messageRef?.includes('water_abstraction_alert') && notification.status !== 'error'
+  })
+
+  return stationsToUpdate.map((notification) => {
+    return _update(notification.personalisation.licenceMonitoringStationId, notification.personalisation.alertType)
   })
 }
 
