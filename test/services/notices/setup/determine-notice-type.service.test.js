d0fbe68059638ae8f4356ff11ffc2efbbc66b8e4
jonathangoulding <goulding.jon@icloud.com>
2025-06-20 13:23:01 +0000

    Update setup initiate session to use determine notice type
    https://eaflood.atlassian.net/browse/WATER-5101
    
    We previously implemented 'ad-hoc' returns as a journey for notices. This is going to change.
    
    We will introduce a page to allow the user to select a notice type when they have chosen a licence for notices.
    
    As part of this upcoming page we need to extract our existing journey logic into a reusable function.
    
    This change introduces the Determine Notice Type Service. This function will set the notice type for all journeys.
    
    Our initial entry to setting up the session data is still based on the query string, but this may change.

---
+++ test/services/notices/setup/determine-notice-type.service.test.js
@@ -0,0 +1,102 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const DetermineNoticeTypeService = require('../../../../app/services/notices/setup/determine-notice-type.service.js')
+
+describe('Notices - Setup - Initiate Session service', () => {
+  describe('when called', () => {
+    describe('when the "notificationType" is "invitations"', () => {
+      it('creates a new session record', () => {
+        const result = DetermineNoticeTypeService.go('invitations')
+
+        expect(result).to.equal({
+          journey: 'invitations',
+          name: 'Returns: invitation',
+          notificationType: 'Returns invitation',
+          redirectPath: 'returns-period',
+          referenceCode: result.referenceCode, // randomly generated
+          subType: 'returnInvitation'
+        })
+      })
+
+      describe('the "referenceCode" property', () => {
+        it('returns a reference code for "invitations" notifications', () => {
+          const result = DetermineNoticeTypeService.go('invitations')
+
+          expect(result.referenceCode).to.include('RINV-')
+          expect(result.referenceCode.length).to.equal(11)
+        })
+      })
+    })
+
+    describe('when the "notificationType" is "reminders"', () => {
+      it('creates a new session record', () => {
+        const result = DetermineNoticeTypeService.go('reminders')
+
+        expect(result).to.equal({
+          journey: 'reminders',
+          name: 'Returns: reminder',
+          notificationType: 'Returns reminder',
+          redirectPath: 'returns-period',
+          referenceCode: result.referenceCode, // randomly generated
+          subType: 'returnReminder'
+        })
+      })
+
+      describe('the "referenceCode" property', () => {
+        it('returns a reference code for "reminders" notifications', () => {
+          const result = DetermineNoticeTypeService.go('reminders')
+
+          expect(result.referenceCode).to.include('RREM-')
+          expect(result.referenceCode.length).to.equal(11)
+        })
+      })
+    })
+
+    describe('when the "notificationType" is "ad-hoc"', () => {
+      it('creates a new session record', () => {
+        const result = DetermineNoticeTypeService.go('ad-hoc')
+
+        expect(result).to.equal({
+          journey: 'ad-hoc',
+          name: 'Returns: ad-hoc',
+          notificationType: 'Ad hoc',
+          redirectPath: 'licence',
+          referenceCode: result.referenceCode, // randomly generated
+          subType: 'adHocReminder'
+        })
+      })
+
+      describe('the "referenceCode" property', () => {
+        it('returns a reference code for an "ad-hoc" notification', () => {
+          const result = DetermineNoticeTypeService.go('ad-hoc')
+
+          expect(result.referenceCode).to.include('ADHC-')
+          expect(result.referenceCode.length).to.equal(11)
+        })
+      })
+    })
+
+    describe('when the "notificationType" is "abstraction-alert"', () => {
+      it('creates a new session record', () => {
+        const result = DetermineNoticeTypeService.go('abstraction-alert')
+
+        expect(result).to.equal({
+          journey: 'abstraction-alert',
+          name: 'Water abstraction alert',
+          notificationType: 'Abstraction alert',
+          redirectPath: 'abstraction-alerts/alert-type',
+          referenceCode: result.referenceCode, // randomly generated
+          subType: 'waterAbstractionAlerts'
+        })
+      })
+    })
+  })
+})
