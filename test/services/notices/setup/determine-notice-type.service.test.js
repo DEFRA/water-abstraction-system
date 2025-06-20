2b11947caa1eee112c82e63fd30b7300abebed33
jonathangoulding <goulding.jon@icloud.com>
2025-06-20 14:00:45 +0000

    tests: add missing test

--- test/services/notices/setup/determine-notice-type.service.test.js
+++ test/services/notices/setup/determine-notice-type.service.test.js
@@ -84,7 +84,7 @@ describe('Notices - Setup - Initiate Session service', () => {
       })
     })
 
-    describe('when the "notificationType" is "abstraction-alert"', () => {
+    describe.only('when the "notificationType" is "abstraction-alert"', () => {
       it('creates a new session record', () => {
         const result = DetermineNoticeTypeService.go('abstraction-alert')
 
@@ -95,6 +95,15 @@ describe('Notices - Setup - Initiate Session service', () => {
           redirectPath: 'abstraction-alerts/alert-type',
           referenceCode: result.referenceCode, // randomly generated
           subType: 'waterAbstractionAlerts'
+        })
+      })
+
+      describe('the "referenceCode" property', () => {
+        it('returns a reference code for an "abstraction-alert" notification', () => {
+          const result = DetermineNoticeTypeService.go('abstraction-alert')
+
+          expect(result.referenceCode).to.include('WAA-')
+          expect(result.referenceCode.length).to.equal(10)
         })
       })
     })
