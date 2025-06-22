b591fd742d69a940837e73fafe60f8f07d1f6dcf
jonathangoulding <goulding.jon@icloud.com>
2025-06-22 16:51:22 +0000

    Scaffold the notices setup select notice page
    https://eaflood.atlassian.net/browse/WATER-5011
    
    This is the initial scaffolded code for the 'Select the notice type' page.
    
    The page title has been added and the payload variable 'noticeType'

---
+++ app/services/notices/setup/submit-notice-type.service.js
@@ -0,0 +1,60 @@
+'use strict'
+
+/**
+ * Orchestrates validating the data for `/notices/setup/{sessionId}/notice-type` page
+ *
+ * @module SubmitNoticeTypeService
+ */
+
+const NoticeTypePresenter = require('../../../presenters/notices/setup/notice-type.presenter.js')
+const NoticeTypeValidator = require('../../../validators/notices/setup/notice-type.validator.js')
+const SessionModel = require('../../../models/session.model.js')
+
+/**
+ * Orchestrates validating the data for `/notices/setup/{sessionId}/notice-type` page
+ *
+ * @param {string} sessionId
+ * @param {object} payload - The submitted form data
+ *
+ * @returns {Promise<object>} - The data formatted for the view template
+ */
+async function go(sessionId, payload) {
+  const session = await SessionModel.query().findById(sessionId)
+
+  const validationResult = _validate(payload)
+
+  if (!validationResult) {
+    await _save(session, payload)
+
+    return {}
+  }
+
+  const pageData = NoticeTypePresenter.go(session)
+
+  return {
+    error: validationResult,
+    ...pageData
+  }
+}
+
+async function _save(session, payload) {
+  return session.$update()
+}
+
+function _validate(payload) {
+  const validation = NoticeTypeValidator.go(payload)
+
+  if (!validation.error) {
+    return null
+  }
+
+  const { message } = validation.error.details[0]
+
+  return {
+    text: message
+  }
+}
+
+module.exports = {
+  go
+}
---
+++ test/validators/notices/setup/notice-type.validator.test.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const NoticeTypeValidator = require('../../../../app/validators/notices/setup/notice-type.validator.js')
+
+describe('Notice Type Validator', () => {
+  let payload
+
+  beforeEach(() => {
+    payload = { noticeType: '' }
+  })
+
+  describe('when called with valid data', () => {
+    it('returns with no errors', () => {
+      const result = NoticeTypeValidator.go(payload)
+
+      expect(result.value).to.exist()
+      expect(result.error).not.to.exist()
+    })
+  })
+
+  describe('when called with invalid data', () => {
+    beforeEach(() => {
+      payload = {}
+    })
+
+    it('returns with errors', () => {
+      const result = NoticeTypeValidator.go(payload)
+
+      expect(result.value).to.exist()
+      expect(result.error).to.exist()
+      expect(result.error.details[0].message).to.equal('"noticeType" is required')
+    })
+  })
+})
