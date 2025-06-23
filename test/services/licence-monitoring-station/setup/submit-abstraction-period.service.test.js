dda865f09146917f1387558db2cf98e408bad69e
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-06-23 18:33:09 +0100

    Unit tests for `SubmitAbstractionPeriodService`

--- test/services/licence-monitoring-station/setup/submit-abstraction-period.service.test.js
+++ test/services/licence-monitoring-station/setup/submit-abstraction-period.service.test.js
@@ -19,8 +19,17 @@ describe('Abstraction Period Service', () => {
   let sessionData
 
   beforeEach(async () => {
-    payload = {}
-    sessionData = {}
+    payload = {
+      'abstraction-period-start-day': '1',
+      'abstraction-period-start-month': '2',
+      'abstraction-period-end-day': '3',
+      'abstraction-period-end-month': '4'
+    }
+
+    sessionData = {
+      label: 'LABEL',
+      licenceRef: 'LICENCE_REF'
+    }
 
     session = await SessionHelper.add({ data: sessionData })
   })
@@ -31,10 +40,13 @@ describe('Abstraction Period Service', () => {
 
       const refreshedSession = await session.$query()
 
-      expect(refreshedSession).to.equal(session)
+      expect(refreshedSession.abstractionPeriodStartDay).to.equal('1')
+      expect(refreshedSession.abstractionPeriodStartMonth).to.equal('2')
+      expect(refreshedSession.abstractionPeriodEndDay).to.equal('3')
+      expect(refreshedSession.abstractionPeriodEndMonth).to.equal('4')
     })
 
-    it('continues the journey', async () => {
+    it('returns an empty object in order to continue the journey', async () => {
       const result = await SubmitAbstractionPeriodService.go(session.id, payload)
 
       expect(result).to.equal({})
@@ -42,10 +54,37 @@ describe('Abstraction Period Service', () => {
   })
 
   describe('when validation fails', () => {
-    it('returns page data for the view, with errors', async () => {
+    beforeEach(() => {
+      payload['abstraction-period-start-day'] = 'INVALID_START_DAY'
+      payload['abstraction-period-end-day'] = 'INVALID_END_DAY'
+    })
+
+    it('returns page data for the view', async () => {
       const result = await SubmitAbstractionPeriodService.go(session.id, payload)
 
-      expect(result).to.equal({})
+      expect(result).to.equal(
+        {
+          abstractionPeriodStartDay: 'INVALID_START_DAY',
+          abstractionPeriodStartMonth: '2',
+          abstractionPeriodEndDay: 'INVALID_END_DAY',
+          abstractionPeriodEndMonth: '4',
+          backLink: `/system/licence-monitoring-station/setup/${session.id}/licence-number`,
+          monitoringStationLabel: 'LABEL',
+          pageTitle: 'Enter an abstraction period for licence LICENCE_REF'
+        },
+        { skip: ['error'] }
+      )
+    })
+
+    it('returns the validation error', async () => {
+      const result = await SubmitAbstractionPeriodService.go(session.id, payload)
+
+      expect(result.error).to.equal({
+        text: {
+          startResult: 'Enter a valid start date',
+          endResult: 'Enter a valid end date'
+        }
+      })
     })
   })
 })
