a38b25efc012fba05f0e353185436f16c44fdb24
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-06-23 16:10:52 +0100

    Scaffold the notices setup select notice page (#2114)
    * Scaffold the notices setup select notice page
    
    https://eaflood.atlassian.net/browse/WATER-5011
    
    This is the initial scaffolded code for the 'Select the notice type' page.
    
    The page title has been added and the payload variable 'noticeType'

---
+++ test/services/notices/setup/notice-type.service.test.js
@@ -0,0 +1,33 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Thing under test
+const NoticeTypeService = require('../../../../app/services/notices/setup/notice-type.service.js')
+
+describe('Notice Type Service', () => {
+  let session
+  let sessionData
+
+  beforeEach(async () => {
+    sessionData = {}
+
+    session = await SessionHelper.add({ data: sessionData })
+  })
+
+  describe('when called', () => {
+    it('returns page data for the view', async () => {
+      const result = await NoticeTypeService.go(session.id)
+
+      expect(result).to.equal({ pageTitle: 'Select the notice type' })
+    })
+  })
+})
