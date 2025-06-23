fe669a6b55233a39f00e7029af32c47c585d4a49
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-06-23 19:49:14 +0100

    Update clean empty bill runs
    Oh my oh my! We _really_ had it wrong on our first iteration of cleaning the empty bill runs!
    
    When we recreated an empty presroc bill run, we realised that it will generate not just the bill run and the charge version years, but volumes, bills, bill licences, transactions, plus there is the bill run recorded in the Charging Module!
    
    Fortunately, we've already done all the work to handle all this with our 'cancel bill run' feature.
    
    So, we completely re-wrote this clean task to first identify the bill runs that are empty, then use this information to feed the existing delete bill run services, using the same process as the cancel bill run page.

--- app/services/jobs/clean/clean-empty-bill-runs.service.js
+++ app/services/jobs/clean/clean-empty-bill-runs.service.js
@@ -6,6 +6,9 @@
  */
 
 const BillRunModel = require('../../../models/bill-run.model.js')
+const CancelBillBunService = require('../../bill-runs/cancel/cancel-bill-run.service.js')
+const DeleteBillRunService = require('../../bill-runs/cancel/delete-bill-run.service.js')
+const UnassignBillRunToLicencesService = require('../../bill-runs/unassign-bill-run-to-licences.service.js')
 
 /**
  * Deletes empty bill runs
@@ -13,11 +16,42 @@ const BillRunModel = require('../../../models/bill-run.model.js')
  * @returns {Promise<number>} The number of rows deleted
  */
 async function go() {
+  let billRunId
+  let deletedCount = 0
+
   try {
-    return await BillRunModel.query().delete().where('status', 'empty')
+    const emptyBillRuns = await _fetch()
+
+    for (const emptyBillRun of emptyBillRuns) {
+      billRunId = emptyBillRun.id
+
+      const deleted = await _deleteEmptyBillRun(billRunId)
+
+      // Up the count if the bill run was deleted
+      deletedCount = deleted ? deletedCount + 1 : deletedCount
+    }
+
+    return deletedCount
   } catch (error) {
-    global.GlobalNotifier.omfg('Clean job failed', { job: 'clean-empty-bill-runs' }, error)
+    global.GlobalNotifier.omfg('Clean job failed', { billRunId, job: 'clean-empty-bill-runs' }, error)
   }
+}
+
+async function _deleteEmptyBillRun(billRunId) {
+  const billRun = await CancelBillBunService.go(billRunId)
+
+  if (billRun.status !== 'cancel') {
+    return false
+  }
+
+  await UnassignBillRunToLicencesService.go(billRun.id)
+  await DeleteBillRunService.go(billRun)
+
+  return true
+}
+
+async function _fetch() {
+  return BillRunModel.query().select(['id']).where('status', 'empty')
 }
 
 module.exports = {
--- test/services/jobs/clean/clean-empty-bill-runs.service.test.js
+++ test/services/jobs/clean/clean-empty-bill-runs.service.test.js
@@ -9,17 +9,40 @@ const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const BillRunHelper = require('../../../support/helpers/bill-run.helper.js')
 const BillRunModel = require('../../../../app/models/bill-run.model.js')
+
+// Things we need to stub
+const CancelBillRunService = require('../../../../app/services/bill-runs/cancel/cancel-bill-run.service.js')
+const DeleteBillRunService = require('../../../../app/services/bill-runs/cancel/delete-bill-run.service.js')
+const UnassignBillRunToLicencesService = require('../../../../app/services/bill-runs/unassign-bill-run-to-licences.service.js')
 
 // Thing under test
 const CleanEmptyBillRunsService = require('../../../../app/services/jobs/clean/clean-empty-bill-runs.service.js')
 
 describe('Jobs - Clean - Clean Empty Bill Runs service', () => {
-  let billRun
+  const emptyBillRuns = [{ id: 'b1c10417-77bb-421e-a9ef-15a0d1bc05d8' }, { id: 'ddc7f25f-8b83-4ef1-9b10-bf1d968e2f13' }]
+
+  let cancelBillRunStub
+  let deleteBillRunStub
+  let emptyBillRunFetchStub
   let notifierStub
+  let unassignBillRunStub
 
   beforeEach(async () => {
+    emptyBillRunFetchStub = Sinon.stub()
+
+    Sinon.stub(BillRunModel, 'query').returns({
+      select: Sinon.stub().returnsThis(),
+      where: emptyBillRunFetchStub
+    })
+
+    // We'll control whether this one succeeds or not in the tests
+    cancelBillRunStub = Sinon.stub(CancelBillRunService, 'go')
+
+    // These we stub to always resolve
+    deleteBillRunStub = Sinon.stub(DeleteBillRunService, 'go').resolves()
+    unassignBillRunStub = Sinon.stub(UnassignBillRunToLicencesService, 'go').resolves()
+
     // The service depends on GlobalNotifier to have been set. This happens in app/plugins/global-notifier.plugin.js
     // when the app starts up and the plugin is registered. As we're not creating an instance of Hapi server in this
     // test we recreate the condition by setting it directly with our own stub
@@ -31,56 +54,127 @@ describe('Jobs - Clean - Clean Empty Bill Runs service', () => {
     Sinon.restore()
   })
 
-  describe('when the clean is successful', () => {
-    describe('when there is an "empty" bill run', () => {
-      beforeEach(async () => {
-        billRun = await BillRunHelper.add({ status: 'empty' })
+  describe('when no bill runs are flagged as "empty"', () => {
+    beforeEach(async () => {
+      emptyBillRunFetchStub.resolves([])
+    })
+
+    it('does not attempt to delete any bill runs', async () => {
+      await CleanEmptyBillRunsService.go()
+
+      expect(cancelBillRunStub.called).to.be.false()
+
+      expect(unassignBillRunStub.called).to.be.false()
+      expect(deleteBillRunStub.called).to.be.false()
+    })
+  })
+
+  describe('when there are bill runs flagged as "empty"', () => {
+    beforeEach(async () => {
+      emptyBillRunFetchStub.resolves(emptyBillRuns)
+    })
+
+    describe('and they can be cancelled (CancelBillRunService updates status to "cancel")', () => {
+      beforeEach(() => {
+        cancelBillRunStub.onFirstCall().resolves({
+          id: emptyBillRuns[0].id,
+          externalId: 'd704cb32-a309-4a04-9b0e-f316614a5927',
+          status: 'cancel'
+        })
+
+        cancelBillRunStub.onSecondCall().resolves({
+          id: emptyBillRuns[1].id,
+          externalId: '4f64f905-94b4-460d-aefc-098f57834085',
+          status: 'cancel'
+        })
       })
 
-      it('removes the empty bill run', async () => {
+      it('removes the empty bill runs', async () => {
         await CleanEmptyBillRunsService.go()
 
-        const results = await BillRunModel.query().whereIn('id', [billRun.id])
+        expect(cancelBillRunStub.calledTwice).to.be.true()
+        expect(cancelBillRunStub.firstCall.calledWith(emptyBillRuns[0].id)).to.be.true()
+        expect(cancelBillRunStub.secondCall.calledWith(emptyBillRuns[1].id)).to.be.true()
 
-        expect(results).to.have.length(0)
+        expect(unassignBillRunStub.calledTwice).to.be.true()
+        expect(unassignBillRunStub.firstCall.calledWith(emptyBillRuns[0].id)).to.be.true()
+        expect(unassignBillRunStub.secondCall.calledWith(emptyBillRuns[1].id)).to.be.true()
+
+        expect(deleteBillRunStub.calledTwice).to.be.true()
       })
     })
 
-    describe('when the bill run is not flagged as "empty"', () => {
-      beforeEach(async () => {
-        billRun = await BillRunHelper.add({ status: 'processing' })
+    describe('but one cannot be cancelled (CancelBillRunService does not update status)', () => {
+      beforeEach(() => {
+        cancelBillRunStub.onFirstCall().resolves({
+          id: emptyBillRuns[0].id,
+          externalId: 'd704cb32-a309-4a04-9b0e-f316614a5927',
+          status: 'cancel'
+        })
+
+        cancelBillRunStub.onSecondCall().resolves({
+          id: emptyBillRuns[1].id,
+          externalId: '4f64f905-94b4-460d-aefc-098f57834085',
+          status: 'sending'
+        })
       })
 
-      it('does not remove the empty bill run', async () => {
+      it('removes only the one that could be cancelled', async () => {
         await CleanEmptyBillRunsService.go()
 
-        const results = await BillRunModel.query().whereIn('id', [billRun.id])
+        expect(cancelBillRunStub.calledTwice).to.be.true()
+        expect(cancelBillRunStub.firstCall.calledWith(emptyBillRuns[0].id)).to.be.true()
+        expect(cancelBillRunStub.secondCall.calledWith(emptyBillRuns[1].id)).to.be.true()
 
-        expect(results).to.have.length(1)
+        expect(unassignBillRunStub.calledOnce).to.be.true()
+        expect(unassignBillRunStub.firstCall.calledWith(emptyBillRuns[0].id)).to.be.true()
+
+        expect(deleteBillRunStub.calledOnce).to.be.true()
       })
     })
   })
 
   describe('when the clean errors', () => {
-    beforeEach(() => {
-      Sinon.stub(BillRunModel, 'query').returns({
-        delete: Sinon.stub().returnsThis(),
-        where: Sinon.stub().rejects()
+    describe('whilst fetching the empty bill runs', () => {
+      beforeEach(() => {
+        emptyBillRunFetchStub.rejects()
+      })
+
+      it('does not throw an error', async () => {
+        await expect(CleanEmptyBillRunsService.go()).not.to.reject()
+      })
+
+      it('logs the error with no bill run ID', async () => {
+        await CleanEmptyBillRunsService.go()
+
+        const errorLogArgs = notifierStub.omfg.firstCall.args
+
+        expect(notifierStub.omfg.calledWith('Clean job failed')).to.be.true()
+        expect(errorLogArgs[1]).to.equal({ billRunId: undefined, job: 'clean-empty-bill-runs' })
+        expect(errorLogArgs[2]).to.be.instanceOf(Error)
       })
     })
 
-    it('does not throw an error', async () => {
-      await expect(CleanEmptyBillRunsService.go()).not.to.reject()
-    })
+    describe('whilst cancelling a bill run', () => {
+      beforeEach(() => {
+        emptyBillRunFetchStub.resolves(emptyBillRuns)
 
-    it('logs the error', async () => {
-      await CleanEmptyBillRunsService.go()
+        cancelBillRunStub.rejects()
+      })
 
-      const errorLogArgs = notifierStub.omfg.firstCall.args
+      it('does not throw an error', async () => {
+        await expect(CleanEmptyBillRunsService.go()).not.to.reject()
+      })
 
-      expect(notifierStub.omfg.calledWith('Clean job failed')).to.be.true()
-      expect(errorLogArgs[1]).to.equal({ job: 'clean-empty-bill-runs' })
-      expect(errorLogArgs[2]).to.be.instanceOf(Error)
+      it('logs the error including the ID of the bill run that errored', async () => {
+        await CleanEmptyBillRunsService.go()
+
+        const errorLogArgs = notifierStub.omfg.firstCall.args
+
+        expect(notifierStub.omfg.calledWith('Clean job failed')).to.be.true()
+        expect(errorLogArgs[1]).to.equal({ billRunId: emptyBillRuns[0].id, job: 'clean-empty-bill-runs' })
+        expect(errorLogArgs[2]).to.be.instanceOf(Error)
+      })
     })
   })
 })
