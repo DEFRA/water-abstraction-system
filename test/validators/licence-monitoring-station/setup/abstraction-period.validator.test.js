f40f490f366dcdb4fbf9132ef09963cbfe522e0c
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-06-24 09:46:38 +0100

    Unit tests for `AbstractionPeriodValidator`

--- test/validators/licence-monitoring-station/setup/abstraction-period.validator.test.js
+++ test/validators/licence-monitoring-station/setup/abstraction-period.validator.test.js
@@ -13,30 +13,129 @@ const AbstractionPeriodValidator = require('../../../../app/validators/licence-m
 describe('Abstraction Period Validator', () => {
   let payload
 
-  beforeEach(() => {
-    payload = { placeholder: '' }
-  })
-
   describe('when called with valid data', () => {
-    it('returns with no errors', () => {
-      const result = AbstractionPeriodValidator.go(payload)
+    beforeEach(() => {
+      payload = {
+        'abstraction-period-start-day': '1',
+        'abstraction-period-start-month': '2',
+        'abstraction-period-end-day': '3',
+        'abstraction-period-end-month': '4'
+      }
+    })
 
-      expect(result.value).to.exist()
-      expect(result.error).not.to.exist()
+    it('returns with no errors', () => {
+      const { startResult, endResult } = AbstractionPeriodValidator.go(payload)
+
+      expect(startResult.value).to.exist()
+      expect(startResult.error).not.to.exist()
+      expect(endResult.value).to.exist()
+      expect(endResult.error).not.to.exist()
     })
   })
 
   describe('when called with invalid data', () => {
-    beforeEach(() => {
-      payload = {}
+    describe('like an invalid date', () => {
+      beforeEach(() => {
+        payload = {
+          'abstraction-period-start-day': '99',
+          'abstraction-period-start-month': '2',
+          'abstraction-period-end-day': '99',
+          'abstraction-period-end-month': '4'
+        }
+      })
+
+      it('returns with errors', () => {
+        const { startResult, endResult } = AbstractionPeriodValidator.go(payload)
+
+        expect(startResult.value).to.exist()
+        expect(startResult.error).to.exist()
+        expect(startResult.error.details[0].message).to.equal('Enter a valid start date')
+
+        expect(endResult.value).to.exist()
+        expect(endResult.error).to.exist()
+        expect(endResult.error.details[0].message).to.equal('Enter a valid end date')
+      })
     })
 
-    it('returns with errors', () => {
-      const result = AbstractionPeriodValidator.go(payload)
+    describe('like invalid chars', () => {
+      beforeEach(() => {
+        payload = {
+          'abstraction-period-start-day': 'INVALID',
+          'abstraction-period-start-month': 'INVALID',
+          'abstraction-period-end-day': 'INVALID',
+          'abstraction-period-end-month': 'INVALID'
+        }
+      })
 
-      expect(result.value).to.exist()
-      expect(result.error).to.exist()
-      expect(result.error.details[0].message).to.equal('"placeholder" is required')
+      it('returns with errors', () => {
+        const { startResult, endResult } = AbstractionPeriodValidator.go(payload)
+
+        expect(startResult.value).to.exist()
+        expect(startResult.error).to.exist()
+        expect(startResult.error.details[0].message).to.equal('Enter a valid start date')
+
+        expect(endResult.value).to.exist()
+        expect(endResult.error).to.exist()
+        expect(endResult.error.details[0].message).to.equal('Enter a valid end date')
+      })
+    })
+
+    describe('like a missing date', () => {
+      beforeEach(() => {
+        payload = {}
+      })
+
+      it('returns with errors', () => {
+        const { startResult, endResult } = AbstractionPeriodValidator.go(payload)
+
+        expect(startResult.value).to.exist()
+        expect(startResult.error).to.exist()
+        expect(startResult.error.details[0].message).to.equal('Enter the abstraction period start date')
+
+        expect(endResult.value).to.exist()
+        expect(endResult.error).to.exist()
+        expect(endResult.error.details[0].message).to.equal('Enter the abstraction period end date')
+      })
+    })
+
+    describe('and only the start date is invalid', () => {
+      it('only returns an error for the start date', () => {
+        payload = {
+          'abstraction-period-start-day': '99',
+          'abstraction-period-start-month': '2',
+          'abstraction-period-end-day': '3',
+          'abstraction-period-end-month': '4'
+        }
+
+        const { startResult, endResult } = AbstractionPeriodValidator.go(payload)
+
+        expect(startResult.value).to.exist()
+        expect(startResult.error).to.exist()
+        expect(startResult.error.details[0].message).to.equal('Enter a valid start date')
+
+        expect(endResult.value).to.exist()
+        expect(endResult.error).not.to.exist()
+      })
+    })
+
+    describe('and only the end date is invalid', () => {
+      it('only returns an error for the end date', () => {
+        payload = {
+          'abstraction-period-start-day': '1',
+          'abstraction-period-start-month': '2',
+          'abstraction-period-end-day': '99',
+          'abstraction-period-end-month': '4'
+        }
+
+        const { startResult, endResult } = AbstractionPeriodValidator.go(payload)
+
+        expect(startResult.value).to.exist()
+        expect(startResult.error).not.to.exist()
+
+        expect(endResult.value).to.exist()
+        expect(endResult.error).to.exist()
+        expect(endResult.error.details[0].message).to.equal('Enter a valid end date')
+      })
     })
   })
 })
