16e3c3b1180d7f8579b873f2b636d52345082478
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-06-25 13:14:38 +0100

    Create initial version of page

--- app/presenters/licence-monitoring-station/setup/check.presenter.js
+++ app/presenters/licence-monitoring-station/setup/check.presenter.js
@@ -8,10 +8,17 @@
 /**
  * Formats data for `/licence-monitoring-station/setup/{sessionId}/check`
  *
- * @returns {object} - The data formatted for the view template
+ * @param {module:SessionModel} session - The licence monitoring station setup session instance
+ *
+ * @returns {object} The data formatted for the view template
  */
-function go() {
-  return {}
+function go(session) {
+  const { label } = session
+
+  return {
+    monitoringStationLabel: label,
+    pageTitle: 'Check the restriction details'
+  }
 }
 
 module.exports = {
