3477b611fac60636f659fa56ea9694625df15c2e
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-06-26 16:05:19 +0100

    Remove individual back link `TODO` as we will tackle back links as a whole

--- app/presenters/licence-monitoring-station/setup/abstraction-period.presenter.js
+++ app/presenters/licence-monitoring-station/setup/abstraction-period.presenter.js
@@ -33,12 +33,6 @@ function go(session) {
   }
 }
 
-// TODO: Fix logic for all 3 possible scenarios:
-// - User came from select condition page, in which case back link should go to select condition page; this is done.
-// - User came directly from check page, in which case back link should go to check page; this is also done.
-// - User came from select condition page but prior to that they came from the check page; this is _NOT_ done as
-//   currently, checkPageVisited will be true and they will therefore be returned to the check page. We need to fix this
-//   but will do this once check page is in place so we can more easily develop and test this logic.
 function _backLink(session) {
   const { checkPageVisited, id } = session
 
