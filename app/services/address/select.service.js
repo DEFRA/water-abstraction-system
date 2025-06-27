41cc82128fa3543d8f7517a4f6a70a806769d7a6
robertparkinson <robert@daedalusft.com>
2025-06-27 13:42:23 +0100

    add fetch address service

--- app/services/address/select.service.js
+++ app/services/address/select.service.js
@@ -6,8 +6,10 @@
  * @module SelectService
  */
 
+const BaseRequest = require('../../requests/base.request.js')
 const SelectPresenter = require('../../presenters/address/select.presenter.js')
 const SessionModel = require('../../models/session.model.js')
+const servicesConfig = require('../../../config/services.config.js')
 
 /**
  * Orchestrates fetching and presenting the data for the `address/{sessionId}/select` page
@@ -19,9 +21,22 @@ const SessionModel = require('../../models/session.model.js')
 async function go(sessionId) {
   const session = await SessionModel.query().findById(sessionId)
 
+  try {
+    const statusUrl = new URL(
+      'address-service/v1/addresses/postcode?query-string=sw2%201an&key=client1',
+      servicesConfig.addressFacade.url
+    )
+    const result = await BaseRequest.get(statusUrl.href)
+    const resultJson = JSON.parse(result.response.body)
+    console.log(resultJson.results[0])
+  } catch (error) {
+    console.log(error)
+  }
+
   const pageData = SelectPresenter.go(session)
 
   return {
+    pageTitle: 'Select the address',
     ...pageData
   }
 }
