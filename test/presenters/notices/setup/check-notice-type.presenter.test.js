0910478d6fd23544a5a13874a4e4b5d59210b72e
jonathangoulding <goulding.jon@icloud.com>
2025-07-01 16:14:26 +0100

    Update notice setup returns paper forms change link
    https://eaflood.atlassian.net/browse/WATER-5024
    
    This change adds the returns data to the view and the associated change link.
    
    Returns only shows for the 'paper-forms' journey and at least one will have been selected to show.
    
    When multiple are selected we need to render the returns in a list in the UI. To achieve this we set the summary list value to html and add a <br> after each return.

--- test/presenters/notices/setup/check-notice-type.presenter.test.js
+++ test/presenters/notices/setup/check-notice-type.presenter.test.js
@@ -9,6 +9,7 @@ const { expect } = Code
 
 // Test helpers
 const { generateLicenceRef } = require('../../../support/helpers/licence.helper.js')
+const { generateUUID } = require('../../../../app/lib/general.lib.js')
 
 // Thing under test
 const CheckNoticeTypePresenter = require('../../../../app/presenters/notices/setup/check-notice-type.presenter.js')
@@ -122,8 +123,31 @@ describe('Notices - Setup - Check Notice Type Presenter', () => {
     })
 
     describe('and the notice type is "paper-forms"', () => {
+      let dueReturnOne
+      let dueReturnTwo
+
       beforeEach(() => {
+        dueReturnOne = {
+          description: 'Potable Water Supply - Direct',
+          endDate: '2003-03-31',
+          returnId: generateUUID(),
+          returnReference: '3135',
+          startDate: '2002-04-01'
+        }
+
+        dueReturnTwo = {
+          description: 'Potable Water Supply - Direct',
+          endDate: '2004-03-31',
+          returnId: generateUUID(),
+          returnReference: '3135',
+          startDate: '2003-04-01'
+        }
+
         session.noticeType = 'paper-forms'
+
+        session.dueReturns = [dueReturnOne, dueReturnTwo]
+
+        session.selectedReturns = [dueReturnOne.returnId]
       })
 
       it('returns the summary list', () => {
@@ -163,8 +187,39 @@ describe('Notices - Setup - Check Notice Type Presenter', () => {
             value: {
               text: 'Submit using a paper form invitation'
             }
+          },
+          {
+            actions: {
+              items: [
+                {
+                  href: '/system/notices/setup/123/returns-for-paper-forms',
+                  text: 'Change',
+                  visuallyHiddenText: 'returns for paper forms'
+                }
+              ]
+            },
+            key: {
+              text: 'Returns'
+            },
+            value: {
+              html: '3135 - 1 April 2002 to 31 March 2003'
+            }
           }
         ])
+      })
+
+      describe('and there are more than one "selectedReturns"', () => {
+        beforeEach(() => {
+          session.selectedReturns = [dueReturnOne.returnId, dueReturnTwo.returnId]
+        })
+
+        it('returns the html with a line break', () => {
+          const result = CheckNoticeTypePresenter.go(session)
+
+          expect(result.summaryList[2].value.html).to.equal(
+            '3135 - 1 April 2002 to 31 March 2003<br>3135 - 1 April 2003 to 31 March 2004'
+          )
+        })
       })
     })
   })
