27e350e61faa3dbdad67db507ca37813de9fe70f
jonathangoulding <goulding.jon@icloud.com>
2025-07-01 15:12:45 +0100

    Update notice setup returns paper forms notification
    https://eaflood.atlassian.net/browse/WATER-5024
    
    This change adds a notification message when the user has come from the check page and makes a change.
    
    If the previously selected returns do not match the currently selected returns then something has changed, we show the notification.

--- app/services/notices/setup/submit-returns-for-paper-forms.service.js
+++ app/services/notices/setup/submit-returns-for-paper-forms.service.js
@@ -9,16 +9,18 @@
 const ReturnsForPaperFormsPresenter = require('../../../presenters/notices/setup/returns-for-paper-forms.presenter.js')
 const ReturnsForPaperFormsValidator = require('../../../validators/notices/setup/returns-for-paper-forms.validator.js')
 const SessionModel = require('../../../models/session.model.js')
+const GeneralLib = require('../../../lib/general.lib.js')
 
 /**
  * Orchestrates validating the data for `/notices/setup/{sessionId}/returns-for-paper-forms` page
  *
  * @param {string} sessionId
  * @param {object} payload - The submitted form data
+ * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
  *
  * @returns {Promise<object>} - The data formatted for the view template
  */
-async function go(sessionId, payload) {
+async function go(sessionId, payload, yar) {
   const session = await SessionModel.query().findById(sessionId)
 
   _handleOneOptionSelected(payload)
@@ -26,6 +28,10 @@ async function go(sessionId, payload) {
   const validationResult = _validate(payload)
 
   if (!validationResult) {
+    if (session.checkPageVisited && _arraysDiffer(payload.returns, session.selectedReturns)) {
+      GeneralLib.flashNotification(yar, 'Updated', 'Returns updated')
+    }
+
     await _save(session, payload)
 
     return {}
@@ -37,6 +43,33 @@ async function go(sessionId, payload) {
     error: validationResult,
     ...pageData
   }
+}
+
+/**
+ * Checks whether two arrays of primitive values differ.
+ *
+ * Sorts the two arrays to assure elements match
+ *
+ * Returns `true` if arrays have different lengths or different elements
+ * Returns `false` if arrays contain the same elements
+ *
+ * Example:
+ *   arraysDiffer([1, 2], [2, 1]) // false
+ *   arraysDiffer([1, 2], [1, 3]) // true
+ *
+ * @private
+ */
+function _arraysDiffer(a, b) {
+  if (a.length !== b.length) return true
+
+  const sortedA = [...a].sort()
+  const sortedB = [...b].sort()
+
+  for (let i = 0; i < sortedA.length; i++) {
+    if (sortedA[i] !== sortedB[i]) return true
+  }
+
+  return false
 }
 
 function _handleOneOptionSelected(payload) {
