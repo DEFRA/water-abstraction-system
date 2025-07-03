6e0ef3a5aadc533e6978c1d8e19309bcb6b3f32a
robertparkinson <robert@daedalusft.com>
2025-07-03 14:17:20 +0100

    setup scaffolding for manual address page

---
+++ test/services/address/submit-manual.service.test.js
@@ -0,0 +1,51 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const SessionHelper = require('../../support/helpers/session.helper.js')
+
+// Thing under test
+const SubmitManualService = require('../../../app/services/address/submit-manual.service.js')
+
+describe('Manual Service', () => {
+  let payload
+  let session
+  let sessionData
+
+  beforeEach(async () => {
+    payload = {}
+    sessionData = {}
+
+    session = await SessionHelper.add({ data: sessionData })
+  })
+
+  describe('when called', () => {
+    it('saves the submitted value', async () => {
+      await SubmitManualService.go(session.id, payload)
+
+      const refreshedSession = await session.$query()
+
+      expect(refreshedSession).to.equal(session)
+    })
+
+    it('continues the journey', async () => {
+      const result = await SubmitManualService.go(session.id, payload)
+
+      expect(result).to.equal({})
+    })
+  })
+
+  describe('when validation fails', () => {
+    it('returns page data for the view, with errors', async () => {
+      const result = await SubmitManualService.go(session.id, payload)
+
+      expect(result).to.equal({})
+    })
+  })
+})
