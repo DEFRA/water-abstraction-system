8fd58c98cbe1f1a7f503055fa065bf3c1517c5c9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-07-08 19:26:23 +0100

    Update LookupPostcodeRequest to use new base request

--- app/requests/address-facade/lookup-postcode.request.js
+++ app/requests/address-facade/lookup-postcode.request.js
@@ -5,47 +5,19 @@
  * @module LookupPostcodeRequest
  */
 
-const BaseRequest = require('../base.request.js')
-
-const requestConfig = require('../../../config/request.config.js')
-const servicesConfig = require('../../../config/services.config.js')
+const AddressFacadeRequest = require('../address-facade.request.js')
 
 /**
  * Sends a request to the address facade for the provided postcode
  *
  * @param {string} postcode - The postcode to look up addresses for
  *
- * @returns {Promise<object[]>} An array of found addresses
+ * @returns {Promise<object>} The result of the request; whether it succeeded and the response or error returned
  */
 async function send(postcode) {
-  const uri = `address-service/v1/addresses/postcode?query-string=${postcode}&key=client1`
-  const result = await BaseRequest.get(uri, _requestOptions())
+  const path = `address-service/v1/addresses/postcode?query-string=${postcode}&key=client1`
 
-  return {
-    succeeded: result.succeeded,
-    results: result?.response?.body?.results || []
-  }
-}
-
-/**
- * Additional options that will be added to the default options used by BaseRequest
- *
- * We use it to set
- *
- * - the base address facade URL for all requests
- * - the option to tell Got that we expect JSON responses. This means Got will automatically handle parsing the
- *   response to a JSON object for us
- *
- * @private
- */
-function _requestOptions() {
-  return {
-    prefixUrl: servicesConfig.addressFacade.url,
-    responseType: 'json',
-    timeout: {
-      request: requestConfig.timeout
-    }
-  }
+  return AddressFacadeRequest.get(path)
 }
 
 module.exports = {
--- test/requests/address-facade/lookup-postcode.request.test.js
+++ test/requests/address-facade/lookup-postcode.request.test.js
@@ -8,18 +8,25 @@ const Sinon = require('sinon')
 const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
-// Test helpers
-const servicesConfig = require('../../../config/services.config.js')
-
 // Things we need to stub
-const BaseRequest = require('../../../app/requests/base.request.js')
+const AddressFacadeRequest = require('../../../app/requests/address-facade.request.js')
 
 // Thing under test
 const LookupPostcodeRequest = require('../../../app/requests/address-facade/lookup-postcode.request.js')
 
 describe('Requests - Address Facade - Lookup Postcode request', () => {
-  const postcode = 'SW1A 1AA'
-  const postcodePath = `address-service/v1/addresses/postcode?query-string=${postcode}&key=client1`
+  const match = {
+    uprn: 340116,
+    address: 'ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH',
+    organisation: 'ENVIRONMENT AGENCY',
+    premises: 'HORIZON HOUSE',
+    street_address: 'DEANERY ROAD',
+    locality: null,
+    city: 'BRISTOL',
+    postcode: 'BS1 5AH',
+    country: 'United Kingdom'
+  }
+  const postcode = 'BS1 5AH'
 
   afterEach(() => {
     Sinon.restore()
@@ -27,52 +34,107 @@ describe('Requests - Address Facade - Lookup Postcode request', () => {
 
   describe('when the request succeeds', () => {
     beforeEach(async () => {
-      Sinon.stub(BaseRequest, 'get').resolves({
+      Sinon.stub(AddressFacadeRequest, 'get').resolves({
         succeeded: true,
         response: {
           statusCode: 200,
           body: {
             results: []
           }
-        }
+        },
+        matches: [match]
       })
     })
 
-    it('calls the address lookup url with the required options', async () => {
+    it('hits the correct endpoint', async () => {
       await LookupPostcodeRequest.send(postcode)
 
-      const requestArgs = BaseRequest.get.firstCall.args
+      const requestArgs = AddressFacadeRequest.get.firstCall.args
 
-      expect(requestArgs[0]).to.equal(postcodePath)
-      expect(requestArgs[1].prefixUrl).to.equal(`${servicesConfig.addressFacade.url}`)
-      expect(requestArgs[1].responseType).to.equal('json')
+      expect(requestArgs[0]).to.equal(`address-service/v1/addresses/postcode?query-string=${postcode}&key=client1`)
     })
 
-    it('returns a valid repsonse', async () => {
+    it('returns a "true" success status', async () => {
       const result = await LookupPostcodeRequest.send(postcode)
 
       expect(result.succeeded).to.be.true()
-      expect(result.results).to.equal([])
+    })
+
+    it('returns the matching addresses', async () => {
+      const result = await LookupPostcodeRequest.send(postcode)
+
+      expect(result.matches).to.equal([match])
     })
   })
 
-  describe('when the request fails', () => {
-    beforeEach(async () => {
-      Sinon.stub(BaseRequest, 'get').resolves({
-        succeeded: false,
-        response: {
+  describe('when the request cannot lookup a postcode', () => {
+    describe('because the request did not return a 2xx/3xx response', () => {
+      beforeEach(async () => {
+        Sinon.stub(AddressFacadeRequest, 'get').resolves({
+          succeeded: false,
+          response: {
+            statusCode: 404,
+            body: { statusCode: 404, error: 'Not Found', message: 'Not Found' }
+          },
+          matches: []
+        })
+      })
+
+      it('returns a "false" success status', async () => {
+        const result = await LookupPostcodeRequest.send(postcode)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error in the "response"', async () => {
+        const result = await LookupPostcodeRequest.send(postcode)
+
+        expect(result.response.body).to.equal({
           statusCode: 404,
-          statusMessage: 'Not Found',
-          body: { statusCode: 404, error: 'Not Found', message: 'Not Found' }
-        }
+          error: 'Not Found',
+          message: 'Not Found'
+        })
+      })
+
+      it('does not returns any matches', async () => {
+        const result = await LookupPostcodeRequest.send(postcode)
+
+        expect(result.matches).to.exist()
+        expect(result.matches).to.be.instanceOf(Array)
+        expect(result.matches).to.be.empty()
       })
     })
 
-    it('returns a "false" success status and an empty array', async () => {
-      const result = await LookupPostcodeRequest.send(postcode)
+    describe('because the request attempt returned an error, for example, TimeoutError', () => {
+      beforeEach(async () => {
+        Sinon.stub(AddressFacadeRequest, 'get').resolves({
+          succeeded: false,
+          response: new Error("Timeout awaiting 'request' for 5000ms"),
+          matches: []
+        })
+      })
 
-      expect(result.succeeded).to.be.false()
-      expect(result.results).to.equal([])
+      it('returns a "false" success status', async () => {
+        const result = await LookupPostcodeRequest.send(postcode)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error in the "response"', async () => {
+        const result = await LookupPostcodeRequest.send(postcode)
+
+        expect(result.response.statusCode).not.to.exist()
+        expect(result.response.body).not.to.exist()
+        expect(result.response.message).to.equal("Timeout awaiting 'request' for 5000ms")
+      })
+
+      it('does not returns any matches', async () => {
+        const result = await LookupPostcodeRequest.send(postcode)
+
+        expect(result.matches).to.exist()
+        expect(result.matches).to.be.instanceOf(Array)
+        expect(result.matches).to.be.empty()
+      })
     })
   })
 })
