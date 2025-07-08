e37191702a18a44d126a13eeadbad531c8dc494d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-07-08 22:24:25 +0100

    Housekeeping - correct comments

--- app/requests/address-facade/lookup-uprn.request.js
+++ app/requests/address-facade/lookup-uprn.request.js
@@ -10,9 +10,9 @@ const AddressFacadeRequest = require('../address-facade.request.js')
 /**
  * Sends a request to the address facade for the provided uprn
  *
- * @param {string} uprn - The UPRN to look up
+ * @param {string} uprn - The UPRN to look up addresses for
  *
- * @returns {Promise<object>} An array with the address information
+ * @returns {Promise<object>} The result of the request; whether it succeeded and the response or error returned
  */
 async function send(uprn) {
   const path = `address-service/v1/addresses/${uprn}?key=client1`
