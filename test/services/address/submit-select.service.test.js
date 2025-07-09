2fce8a108e363dd6617c5f344475c056253bc27d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-07-09 09:09:26 +0100

    Update Address lookup requests to convention (#2176)
    https://eaflood.atlassian.net/browse/WATER-5119
    
    We recently migrated the send return invitations and reminders notification journeys, which required sending requests to [GOV.UK Notify](https://www.notifications.service.gov.uk/) for the first time.
    
    Building on that, we are adding the ability to send notices to specific licences and amending the address used. That meant talking to the [ea-address-facade](https://github.com/DEFRA/ea-address-facade).
    
    We added the requests, but have only now just noted that they don't follow the conventions used when we first added 'requests' for the [Charging Module API](https://github.com/DEFRA/sroc-charging-module-api) and the other legacy services to this project.
    
    We have our `app/requests/base.request.js`, which handles using [Got](https://github.com/sindresorhus/got) to make HTTP requests and deal with the responses.
    
    We then have 'base' service requests, for example, `app/requests/charging-module.request.js`. These handle setting the domain and any general operations, for example, applying authentication. They also control how exactly we can communicate with a service. Perhaps only `post` requests are supported, so only a Charging Module API `post` method is exposed.
    
    Finally, you have the individual requests themselves. These are specific and are used to set the path to use and handle anything related to the request.
    
    The **Notify** and **Address** requests have not followed this pattern. Instead, the specific requests are referencing `base.request.js` directly.
    
    This change isn't critical. However, we have conventions and patterns, so it is best to follow them until we _purposely_ need to break them.
    
    This change updates the address-based requests.

--- test/services/address/submit-select.service.test.js
+++ test/services/address/submit-select.service.test.js
@@ -12,55 +12,56 @@ const { expect } = Code
 const SessionHelper = require('../../support/helpers/session.helper.js')
 
 // Things to stub
-const LookupPostcodeRequest = require('../../../app/requests/address-lookup/lookup-postcode.request.js')
-const LookupUPRNRequest = require('../../../app/requests/address-lookup/lookup-uprn.request.js')
+const LookupPostcodeRequest = require('../../../app/requests/address-facade/lookup-postcode.request.js')
+const LookupUPRNRequest = require('../../../app/requests/address-facade/lookup-uprn.request.js')
 
 // Thing under test
 const SubmitSelectService = require('../../../app/services/address/submit-select.service.js')
 
 describe('Address - Submit Select Service', () => {
+  const match = {
+    uprn: 340116,
+    address: 'ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH',
+    organisation: 'ENVIRONMENT AGENCY',
+    premises: 'HORIZON HOUSE',
+    street_address: 'DEANERY ROAD',
+    locality: null,
+    city: 'BRISTOL',
+    postcode: 'BS1 5AH',
+    country: 'United Kingdom'
+  }
+
   let getByPostcodeStub
   let getByUPRNStub
   let payload
   let session
-  let sessionData
 
   beforeEach(async () => {
-    sessionData = {
-      address: {
-        postcode: 'SW1A 1AA'
-      }
-    }
+    session = await SessionHelper.add({ data: { address: { postcode: 'BS1 5AH' } } })
 
-    session = await SessionHelper.add({ data: sessionData })
+    getByPostcodeStub = Sinon.stub(LookupPostcodeRequest, 'send')
+    getByUPRNStub = Sinon.stub(LookupUPRNRequest, 'send')
   })
 
   afterEach(() => {
     Sinon.restore()
   })
 
-  describe('when called with a valid UPRN', () => {
+  describe('when an address has been selected', () => {
     beforeEach(async () => {
-      session = await SessionHelper.add({ data: sessionData })
-      getByPostcodeStub = Sinon.stub(LookupPostcodeRequest, 'send')
-      getByUPRNStub = Sinon.stub(LookupUPRNRequest, 'send')
       payload = {
-        addresses: '123456789'
+        addresses: '340116'
       }
+
       getByUPRNStub.resolves({
         succeeded: true,
-        results: [
-          {
-            uprn: '123456789',
-            organisation: 'organisation',
-            premises: 'premises',
-            street_address: 'street_address',
-            locality: 'locality',
-            city: 'city',
-            postcode: 'postcode',
-            country: 'country'
+        response: {
+          statusCode: 200,
+          body: {
+            results: [match]
           }
-        ]
+        },
+        matches: [match]
       })
     })
 
@@ -70,14 +71,14 @@ describe('Address - Submit Select Service', () => {
       const refreshedSession = await session.$query()
 
       expect(refreshedSession.address).to.equal({
-        uprn: '123456789',
-        addressLine1: 'organisation',
-        addressLine2: 'premises',
-        addressLine3: 'street_address',
-        addressLine4: 'locality',
-        town: 'city',
-        postcode: 'postcode',
-        country: 'country'
+        uprn: 340116,
+        addressLine1: 'ENVIRONMENT AGENCY',
+        addressLine2: 'HORIZON HOUSE',
+        addressLine3: 'DEANERY ROAD',
+        addressLine4: null,
+        town: 'BRISTOL',
+        postcode: 'BS1 5AH',
+        country: 'United Kingdom'
       })
     })
 
@@ -90,21 +91,19 @@ describe('Address - Submit Select Service', () => {
 
   describe('when an address has not been selected', () => {
     beforeEach(async () => {
-      session = await SessionHelper.add({ data: sessionData })
-      getByPostcodeStub = Sinon.stub(LookupPostcodeRequest, 'send')
-      getByUPRNStub = Sinon.stub(LookupUPRNRequest, 'send')
       payload = {
         addresses: 'select'
       }
+
       getByPostcodeStub.resolves({
         succeeded: true,
-        results: [
-          {
-            address: 'address 1',
-            postcode: 'SW1A 1AA',
-            uprn: '123456789'
+        response: {
+          statusCode: 200,
+          body: {
+            results: [match]
           }
-        ]
+        },
+        matches: [match]
       })
     })
 
@@ -123,12 +122,12 @@ describe('Address - Submit Select Service', () => {
             text: `1 address found`
           },
           {
-            text: 'address 1',
-            value: '123456789'
+            text: 'ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH',
+            value: 340116
           }
         ],
         pageTitle: 'Select the address',
-        postcode: 'SW1A 1AA',
+        postcode: 'BS1 5AH',
         sessionId: session.id
       })
     })
@@ -136,104 +135,104 @@ describe('Address - Submit Select Service', () => {
 
   describe('when called with a invalid UPRN', () => {
     beforeEach(async () => {
-      session = await SessionHelper.add({ data: sessionData })
-      getByPostcodeStub = Sinon.stub(LookupPostcodeRequest, 'send')
-      getByUPRNStub = Sinon.stub(LookupUPRNRequest, 'send')
       payload = {
-        addresses: '123456789'
+        addresses: 'invalid'
       }
+
       getByUPRNStub.resolves({
         succeeded: false,
-        results: []
-      })
-      getByPostcodeStub.resolves({
-        succeeded: true,
-        results: [
-          {
-            address: 'address 1',
-            postcode: 'SW1A 1AA',
-            uprn: '123456789'
+        response: {
+          statusCode: 404,
+          body: {
+            facade_status_code: 404,
+            facade_error_message: 'HTTP 404 Not Found',
+            facade_error_code: 'address_service_error_11',
+            supplier_was_called: null,
+            supplier_status_code: null,
+            supplier_response: null
           }
-        ]
-      })
-    })
-
-    it('renders the page with an error', async () => {
-      const result = await SubmitSelectService.go(session.id, payload)
-
-      expect(result).to.equal({
-        backLink: `/system/address/${session.id}/postcode`,
-        error: {
-          text: 'Address not found'
         },
-        addresses: [
-          {
-            value: 'select',
-            selected: true,
-            text: `1 address found`
+        matches: []
+      })
+    })
+
+    describe('and the postcode lookup succeeds and finds a match', () => {
+      beforeEach(() => {
+        getByPostcodeStub.resolves({
+          succeeded: true,
+          response: {
+            statusCode: 200,
+            body: {
+              results: []
+            }
           },
-          {
-            text: 'address 1',
-            value: '123456789'
-          }
-        ],
-        pageTitle: 'Select the address',
-        postcode: 'SW1A 1AA',
-        sessionId: session.id
+          matches: [match]
+        })
       })
-    })
-  })
 
-  describe('when called with a invalid UPRN but the postcode lookup fails', () => {
-    beforeEach(async () => {
-      session = await SessionHelper.add({ data: sessionData })
-      getByPostcodeStub = Sinon.stub(LookupPostcodeRequest, 'send')
-      getByUPRNStub = Sinon.stub(LookupUPRNRequest, 'send')
-      payload = {
-        addresses: '123456789'
-      }
-      getByUPRNStub.resolves({
-        succeeded: false,
-        results: []
-      })
-      getByPostcodeStub.resolves({
-        succeeded: false,
-        results: []
-      })
-    })
+      it('returns the page data with an error', async () => {
+        const result = await SubmitSelectService.go(session.id, payload)
 
-    it('returns page data that causes a redirect to the manual page', async () => {
-      const result = await SubmitSelectService.go(session.id, payload)
-
-      expect(result).to.equal({
-        redirect: true
-      })
-    })
-  })
-
-  describe('when called with a invalid UPRN but the postcode lookup succeeds but has no results', () => {
-    beforeEach(async () => {
-      session = await SessionHelper.add({ data: sessionData })
-      getByPostcodeStub = Sinon.stub(LookupPostcodeRequest, 'send')
-      getByUPRNStub = Sinon.stub(LookupUPRNRequest, 'send')
-      payload = {
-        addresses: '123456789'
-      }
-      getByUPRNStub.resolves({
-        succeeded: false,
-        results: []
-      })
-      getByPostcodeStub.resolves({
-        succeeded: true,
-        results: []
+        expect(result).to.equal({
+          backLink: `/system/address/${session.id}/postcode`,
+          error: {
+            text: 'Address not found'
+          },
+          addresses: [
+            {
+              value: 'select',
+              selected: true,
+              text: `1 address found`
+            },
+            {
+              text: 'ENVIRONMENT AGENCY, HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH',
+              value: 340116
+            }
+          ],
+          pageTitle: 'Select the address',
+          postcode: 'BS1 5AH',
+          sessionId: session.id
+        })
       })
     })
 
-    it('returns page data that causes a redirect to the manual page', async () => {
-      const result = await SubmitSelectService.go(session.id, payload)
+    describe('and the postcode lookup succeeds but has no matches', () => {
+      beforeEach(async () => {
+        getByPostcodeStub.resolves({
+          succeeded: true,
+          response: {
+            statusCode: 200,
+            body: {
+              results: []
+            }
+          },
+          matches: []
+        })
+      })
 
-      expect(result).to.equal({
-        redirect: true
+      it('returns page data that causes a redirect to the manual page', async () => {
+        const result = await SubmitSelectService.go(session.id, payload)
+
+        expect(result).to.equal({ redirect: true })
+      })
+    })
+
+    describe('but the postcode lookup fails', () => {
+      beforeEach(async () => {
+        getByPostcodeStub.resolves({
+          succeeded: false,
+          response: {
+            statusCode: 404,
+            body: { statusCode: 404, error: 'Not Found', message: 'Not Found' }
+          },
+          matches: []
+        })
+      })
+
+      it('returns page data that causes a redirect to the manual page', async () => {
+        const result = await SubmitSelectService.go(session.id, payload)
+
+        expect(result).to.equal({ redirect: true })
       })
     })
   })
