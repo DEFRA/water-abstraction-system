f06e48ac7fa1bcb81f900fe78388b0ad7db9226a
Jason Claxton <jason.claxton@defra.gov.uk>
2025-07-10 15:05:04 +0100

    Create service to select alert

---
+++ app/services/notices/setup/preview-select-alert.service.js
@@ -0,0 +1,49 @@
+'use strict'
+
+/**
+ * Orchestrates presenting the data for the `/notices/setup/{sessionId}/preview/{contactHashId}/select-alert` page
+ *
+ * @module PreviewSelectAlertService
+ */
+
+const DetermineRecipientsService = require('./determine-recipients.service.js')
+const FetchAbstractionAlertRecipientsService = require('./fetch-abstraction-alert-recipients.service.js')
+const PreviewSelectAlertPresenter = require('../../../presenters/notices/setup/preview-select-alert.presenter.js')
+const SessionModel = require('../../../models/session.model.js')
+
+/**
+ * Orchestrates presenting the data for the `/notices/setup/{sessionId}/preview/{contactHashId}/select-alert` page
+ *
+ * @param {string} contactHashId - The recipients unique identifier
+ * @param {string} sessionId - The UUID of the current session
+ *
+ * @returns {Promise<object>} - The data formatted for the view template
+ */
+async function go(contactHashId, sessionId) {
+  const session = await SessionModel.query().findById(sessionId)
+
+  const recipientLicenceRefs = await _recipientLicenceRefs(contactHashId, session)
+
+  const pageData = PreviewSelectAlertPresenter.go(contactHashId, recipientLicenceRefs, session)
+
+  return {
+    activeNavBar: 'manage',
+    ...pageData
+  }
+}
+
+async function _recipientLicenceRefs(contactHashId, session) {
+  const recipientsData = await FetchAbstractionAlertRecipientsService.go(session)
+
+  const recipients = DetermineRecipientsService.go(recipientsData)
+
+  const recipient = recipients.find((recipient) => {
+    return recipient.contact_hash_id === contactHashId
+  })
+
+  return recipient.licence_refs.split(',')
+}
+
+module.exports = {
+  go
+}
