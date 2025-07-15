47bba7360baf7b8b0237ebe9605ee18401374c14
Jason Claxton <jason.claxton@defra.gov.uk>
2025-07-15 15:09:58 +0100

    Fix test heading

--- test/presenters/notices/setup/preview/preview.presenter.test.js
+++ test/presenters/notices/setup/preview/preview.presenter.test.js
@@ -14,7 +14,7 @@ const NotifyPreviewRequest = require('../../../../../app/requests/notify/notify-
 // Thing under test
 const PreviewPresenter = require('../../../../../app/presenters/notices/setup/preview/preview.presenter.js')
 
-describe('Preview presenter', () => {
+describe('Notices Setup - Preview - Preview presenter', () => {
   const contactHashId = '9df5923f179a0ed55c13173c16651ed9'
   const licenceMonitoringStationId = 'a4d15f69-5005-4b6e-ab50-3fbae2deec9c'
   const sessionId = '7334a25e-9723-4732-a6e1-8e30c5f3732e'
