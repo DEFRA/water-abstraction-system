8e972c3a6b75d54b4aad56614a4bbe589c26fee5
Robert Parkinson <robertparkinson@users.noreply.github.com>
2025-07-22 15:16:39 +0100

    Select how to contact recipient page scaffolding (#2211)
    https://eaflood.atlassian.net/browse/WATER-45043
    
    When a licence version has been changed either by conditions or someone else taking it over we some times need to send out ad hoc notifications to users. This page is part of the journey to create those notification and send them.
    
    This PR sets up the scaffolding for the select a recipient contact type page.

---
+++ app/presenters/notices/setup/contact-type.presenter.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * Formats data for the `/notices/setup/{sessionId}/abstraction-alerts/contact-type` page
+ * @module ContactTypePresenter
+ */
+
+/**
+ * Formats data for the `/notices/setup/{sessionId}/abstraction-alerts/contact-type` page
+ *
+ * @returns {object} - The data formatted for the view template
+ */
+function go() {
+  return {}
+}
+
+module.exports = {
+  go
+}
---
+++ test/presenters/notices/setup/contact-type.presenter.test.js
@@ -0,0 +1,27 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const ContactTypePresenter = require('../../../../app/presenters/notices/setup/contact-type.presenter.js')
+
+describe('Contact Type Presenter', () => {
+  let session
+
+  beforeEach(() => {
+    session = {}
+  })
+
+  describe('when called', () => {
+    it('returns page data for the view', () => {
+      const result = ContactTypePresenter.go(session)
+
+      expect(result).to.equal({})
+    })
+  })
+})
