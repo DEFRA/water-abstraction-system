35aabe5deab471ab651ca6be93a7ffa458f1134d
jonathangoulding <goulding.jon@icloud.com>
2025-07-30 15:32:11 +0000

    chore: pre pr checks

--- test/services/notices/setup/recipients.service.test.js
+++ test/services/notices/setup/recipients.service.test.js
@@ -9,10 +9,12 @@ const { describe, it, afterEach, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const FetchAbstractionAlertRecipientsService = require('../../../../app/services/notices/setup/fetch-abstraction-alert-recipients.service.js')
-const FetchRecipientsService = require('../../../../app/services/notices/setup/fetch-recipients.service.js')
 const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
 const SessionHelper = require('../../../support/helpers/session.helper.js')
+
+// Things we need to stub
+const FetchAbstractionAlertRecipientsService = require('../../../../app/services/notices/setup/fetch-abstraction-alert-recipients.service.js')
+const FetchRecipientsService = require('../../../../app/services/notices/setup/fetch-recipients.service.js')
 
 // Thing under test
 const RecipientsService = require('../../../../app/services/notices/setup/recipients.service.js')
