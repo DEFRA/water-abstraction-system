8fbf6d9e0f7799adad26db5d54773f75ab40f77e
jonathangoulding <goulding.jon@icloud.com>
2025-08-01 09:04:05 +0000

    Update notices setup recipients to use selected recipients
    https://eaflood.atlassian.net/browse/WATER-5041
    
    This change updates the recipients to use the selected recipients selected by the suer for the 'adHoc' journey.

--- app/services/notices/setup/recipients.service.js
+++ app/services/notices/setup/recipients.service.js
@@ -14,20 +14,41 @@ const DetermineRecipientsService = require('./determine-recipients.service.js')
  *
  * Fetches the recipients based on the journey type and determines recipients (remove duplicates).
  *
+ * Most upstream services require only fetching and
+ *
  * @param {module:SessionModel} session - The session instance
+ * @param {boolean} allRecipients - flag to decide if all recipients are required
  *
  * @returns {Promise<object[]>} - recipients
  */
-async function go(session) {
+async function go(session, allRecipients = false) {
   let recipientsData
 
   if (session.journey === 'alerts') {
     recipientsData = await FetchAbstractionAlertRecipientsService.go(session)
+  } else if (session.journey === 'adhoc') {
+    recipientsData = await _adHoc(session, allRecipients)
   } else {
     recipientsData = await FetchRecipientsService.go(session)
   }
 
   return DetermineRecipientsService.go(recipientsData)
+}
+
+async function _adHoc(session, allRecipients) {
+  const recipientsData = await FetchRecipientsService.go(session)
+
+  if (!allRecipients && session.selectedRecipients) {
+    return _selectedRecipients(session.selectedRecipients, recipientsData)
+  }
+
+  return recipientsData
+}
+
+function _selectedRecipients(selectedRecipients, recipientsData) {
+  return recipientsData.filter((recipient) => {
+    return selectedRecipients.includes(recipient.contact_hash_id)
+  })
 }
 
 module.exports = {
