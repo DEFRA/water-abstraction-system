bca51db66fcaabc59e2b6fb818e4e98aa6b1c06e
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-08-05 06:50:41 -0700

    Update test/services/notices/setup/recipients.service.test.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- test/services/notices/setup/recipients.service.test.js
+++ test/services/notices/setup/recipients.service.test.js
@@ -84,7 +84,7 @@ describe('Notices - Setup - Recipients service', () => {
           Sinon.restore()
         })
 
-        it('correctly presents the data', async () => {
+        it('returns only the selected recipients formatted for display', async () => {
           const result = await RecipientsService.go(session, false)
 
           expect(result).to.equal([
