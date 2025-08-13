497d1852f7d1de648a81bab31ebde599f0be4ac8
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-08-13 14:09:47 +0100

    Update return forms PDF to use real return data (#2265)
    * Update return forms PDF to use real return data
    
    https://eaflood.atlassian.net/browse/WATER-5162
    
    This change updates the return forms PDF to use data from the selected return.
    
    This change adds the return id to the path / controller. This allows the preview to be specific to the return selected on the selected returns page. When we implement the sending of the notice, we can pass in the return id to generate the PDF per return id (and recipient if required).
    
    We needed to update the exciting fetch logic to return the additional data and check the return passed in has been selected from the journey.
    
    Some general renaming has been done to update a previous assumption, The main one being the removal of the 'formatId' this is guaranteed to be the same as the 'returnReference' and has been removed.
    
    The 'regionAndArea' will be updated in another change. This has some additional complexity to big for this change.

--- app/presenters/notices/setup/prepare-return-forms.presenter.js
+++ app/presenters/notices/setup/prepare-return-forms.presenter.js
@@ -6,6 +6,7 @@
  */
 
 const { formatLongDate } = require('../../base.presenter.js')
+const { returnRequirementFrequencies } = require('../../../lib/static-lookups.lib.js')
 
 /**
  * Formats data for the return form
@@ -15,22 +16,29 @@ const { formatLongDate } = require('../../base.presenter.js')
  * Each page will be assigned a corresponding object to isolate the data to each page where possible. Those pages are:
  * - The "cover" page, this is the first page. The address is on this page.
  *
+ * @param {SessionModel} session - The session instance
+ * @param {object} dueReturnLog
+ *
  * @returns {object} - The data formatted for the return form
  */
-function go() {
+function go(session, dueReturnLog) {
+  const { licenceRef } = session
+
+  const { dueDate, endDate, purpose, returnsFrequency, returnReference, siteDescription, startDate, twoPartTariff } =
+    dueReturnLog
+
   return {
     address: _address(),
-    description: 'mock site',
-    dueDate: formatLongDate(new Date('2023-10-01')),
-    endDate: formatLongDate(new Date('2023-09-30')),
-    licenceRef: '123',
-    purpose: 'a purpose',
+    siteDescription,
+    dueDate: formatLongDate(new Date(dueDate)),
+    endDate: formatLongDate(new Date(endDate)),
+    licenceRef,
+    purpose,
     regionAndArea: 'A place / in the sun',
-    returnRef: '7646',
-    startDate: formatLongDate(new Date('2023-09-01')),
-    title: _title(),
-    twoPartTariff: true,
-    formatId: 'format id 123'
+    returnReference,
+    startDate: formatLongDate(new Date(startDate)),
+    pageTitle: _pageTitle(returnsFrequency),
+    twoPartTariff
   }
 }
 
@@ -43,15 +51,9 @@ function _address() {
     addressLine5: 'United Kingdom'
   }
 }
-/*
- * {{ 'Water abstraction daily return ' if frequency === 'day' }}
- * {{ 'Water abstraction weekly return ' if frequency === 'week' }}
- * {{ 'Water abstraction monthly return ' if frequency === 'month' }}
- * {{ 'Water abstraction quarterly return ' if frequency === 'quarter' }}
- * {{ 'Water abstraction yearly return ' if frequency === 'year' }}
- */
-function _title() {
-  return 'Water abstraction daily return'
+
+function _pageTitle(returnsFrequency) {
+  return `Water abstraction ${returnRequirementFrequencies[returnsFrequency]} return`
 }
 
 module.exports = {
