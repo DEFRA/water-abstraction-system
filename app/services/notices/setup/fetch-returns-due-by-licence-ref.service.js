01904a8fd748e88c4ded3f99df54e44b11f142a8
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-08-14 01:11:18 -0700

    Update app/services/notices/setup/fetch-returns-due-by-licence-ref.service.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/services/notices/setup/fetch-returns-due-by-licence-ref.service.js
+++ app/services/notices/setup/fetch-returns-due-by-licence-ref.service.js
@@ -13,7 +13,7 @@ const { timestampForPostgres } = require('../../../lib/general.lib.js')
  *
  * @param {string} licenceRef - The licence reference to fetch 'due' return logs for
  *
- * @returns {Promise<module:ReturnLogModel[]>}
+ * @returns {Promise<object[]>}
  */
 async function go(licenceRef) {
   const { rows } = await _fetch(licenceRef)
