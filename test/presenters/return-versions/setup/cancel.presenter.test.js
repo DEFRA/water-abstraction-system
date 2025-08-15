c104e145ef267ca6c02c6176d7bb2ad8b0b5506f
robertparkinson <robert@daedalusft.com>
2025-08-15 15:00:54 +0100

    View templates standardise - cancel view

--- test/presenters/return-versions/setup/cancel.presenter.test.js
+++ test/presenters/return-versions/setup/cancel.presenter.test.js
@@ -57,6 +57,7 @@ describe('Return Versions Setup - Cancel presenter', () => {
         licenceId: '8b7f78ba-f3ad-4cb6-a058-78abc4d1383d',
         licenceRef: '01/ABC',
         pageTitle: 'You are about to cancel these requirements for returns',
+        pageTitleCaption: 'Licence 01/ABC',
         reason: 'Major change',
         returnRequirements: ['Winter and all year monthly requirements for returns, Bore hole in rear field.'],
         sessionId: '61e07498-f309-4829-96a9-72084a54996d',
