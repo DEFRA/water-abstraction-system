24d56e57b4f0fcda22751c0656e4c098ad1af035
jonathangoulding <goulding.jon@icloud.com>
2025-08-15 15:14:25 +0000

    Add dynamic meter readings to the return forms PDF
    https://eaflood.atlassian.net/browse/WATER-5162
    
    When generating meter readings, we need to handle them spilling over onto another page.
    
    The meter reading uses boxes in the PDF form, this is presented as rows and columns.
    
    This change calculates the number of dates we put on a single page; when this limit is reached, a new page will be added below the existing, and this continues.
    
    We use the legacy codes CSS and some of the HTML to show the pages correctly, legacy code has stipulated the columns and rows as the expected format when rendering the PDF.

--- app/presenters/notices/setup/prepare-return-forms.presenter.js
+++ app/presenters/notices/setup/prepare-return-forms.presenter.js
@@ -9,6 +9,21 @@ const NotifyAddressPresenter = require('./notify-address.presenter.js')
 const { formatLongDate } = require('../../base.presenter.js')
 const { naldAreaCodes, returnRequirementFrequencies } = require('../../../lib/static-lookups.lib.js')
 
+const RETURN_TYPE = {
+  week: {
+    columns: 2,
+    columnSize: 14
+  },
+  month: {
+    columns: 2,
+    columnSize: 12
+  },
+  day: {
+    columns: 3,
+    columnSize: 1
+  }
+}
+
 /**
  * Formats data for the return form
  *
@@ -41,21 +56,62 @@ function go(session, dueReturnLog, recipient) {
 
   return {
     address: _address(recipient),
-    siteDescription,
     dueDate: formatLongDate(new Date(dueDate)),
     endDate: formatLongDate(new Date(endDate)),
     licenceRef,
+    meterReadings: _meterReadings(returnsFrequency),
+    pageTitle: _pageTitle(returnsFrequency),
     purpose,
     regionAndArea: _regionAndArea(regionName, naldAreaCode),
     returnReference,
+    returnsFrequency,
+    siteDescription,
     startDate: formatLongDate(new Date(startDate)),
-    pageTitle: _pageTitle(returnsFrequency),
     twoPartTariff
   }
 }
 
 function _address(recipient) {
   return NotifyAddressPresenter.go(recipient.contact)
+}
+
+/**
+ * Splits a list of page items into up to two columns.
+ *
+ * If the number of items exceeds the specified `columnSize`, the overflow
+ * is placed in the second column. If there are fewer items than `columnSize`,
+ * the second column will be empty.
+ *
+ * The page layout will roughly be:
+ *
+ * | Column 1 | Column 2 |
+ * | -------- | -------- |
+ * | item 1 | item 3 |
+ * | item 2 | item 4 |
+ *
+ * @private
+ */
+function _columns(pageItems, columnSize) {
+  const [firstColumn, secondColumn] = _formatArrayIntoGroups(pageItems, columnSize)
+
+  return [firstColumn || [], secondColumn || []]
+}
+
+/**
+ * Splits an array into smaller subarrays (groups) of a specified size.
+ *
+ * Used when calculating the number of dates can be displayed per page.
+ *
+ * Used to calculate the columns, it will split the grouped dates into linear arrays
+ *
+ * @private
+ */
+function _formatArrayIntoGroups(dates, columnSize) {
+  const result = []
+  for (let i = 0; i < dates.length; i += columnSize) {
+    result.push(dates.slice(i, i + columnSize))
+  }
+  return result
 }
 
 /**
@@ -72,8 +128,68 @@ function _regionAndArea(regionName, naldAreaCode) {
   return regionName
 }
 
+function _meterReadings(returnsFrequency) {
+  const { columns, columnSize } = RETURN_TYPE[returnsFrequency]
+
+  const dates = _generateFormattedDates(new Date('2021-01-01'), 64)
+
+  const totalItemsPerPage = columnSize * columns
+
+  return _paginate(dates, totalItemsPerPage, columnSize)
+}
+
+/**
+ * The meter readings use boxes to allow the user to fill in the reading.
+ *
+ * These boxes can span multiple pages, we need to handle this use case.
+ *
+ * We first format the array of dates into groups suitable to render on the page.
+ *
+ * In the case of a 'week' we want to have 2 columns with 14 'rows' per column. This means 28 total dates on the page.
+ * When this limit is exceeded, we continue onto the next page. Therefore, given 65 dates, we would get 3 groups in our
+ * array:
+ * - The first page would be an array of 28
+ * - The second page would be an array of 28
+ * - The third page would be an array of 8
+ *
+ * Total = 65 dates.
+ *
+ * Once the page count has been established, we need to format the dates into columns. The 'week' example of 2 columns
+ * with 14 'rows' per column would result in two columns with 14 'rows' in each column.
+ *
+ * When the column does not reach the maximum row count, it will be the remaining dates.
+ *
+ * This is driven from the UI where the columns are fixed. So it is expected to only have one column with 12 rows (when
+ * the 'returnFrequency' is for months and the date range is a year).
+ *
+ * @private
+ */
+function _paginate(dates, perPage, columnSize) {
+  const pageChunks = _formatArrayIntoGroups(dates, perPage)
+
+  return pageChunks.map((pageItems) => {
+    return _columns(pageItems, columnSize)
+  })
+}
+
 function _pageTitle(returnsFrequency) {
   return `Water abstraction ${returnRequirementFrequencies[returnsFrequency]} return`
+}
+
+/**
+ * Temp date range - will be replaced by start date to end date calculations
+ * @private
+ */
+function _generateFormattedDates(startDate, count) {
+  const dates = []
+
+  for (let i = 0; i < count; i++) {
+    const date = new Date(startDate)
+    date.setDate(startDate.getDate() + i)
+    dates.push(formatLongDate(date))
+  }
+
+  return dates
 }
 
 module.exports = {
--- test/presenters/notices/setup/prepare-return-forms.presenter.test.js
+++ test/presenters/notices/setup/prepare-return-forms.presenter.test.js
@@ -30,7 +30,7 @@ describe('Notices - Setup - Prepare Return Forms Presenter', () => {
       purpose: 'A purpose',
       regionName: 'North West',
       returnReference: '123456',
-      returnsFrequency: 'day',
+      returnsFrequency: 'week',
       siteDescription: 'Water park',
       startDate: '2025-01-01',
       twoPartTariff: false
@@ -54,11 +54,13 @@ describe('Notices - Setup - Prepare Return Forms Presenter', () => {
         dueDate: '6 July 2025',
         endDate: '6 June 2025',
         licenceRef: '123',
+        meterReadings: result.meterReadings,
         purpose: 'A purpose',
         regionAndArea: 'North West / Lower Trent',
         returnReference: '123456',
+        returnsFrequency: 'week',
         startDate: '1 January 2025',
-        pageTitle: 'Water abstraction daily return',
+        pageTitle: 'Water abstraction weekly return',
         twoPartTariff: false
       })
     })
@@ -89,24 +91,26 @@ describe('Notices - Setup - Prepare Return Forms Presenter', () => {
       })
     })
 
-    describe('the "pageTitle" property', () => {
-      describe('when the "returnsFrequency" is "day"', () => {
-        beforeEach(() => {
-          dueReturnLog.returnsFrequency = 'day'
-        })
+    describe('when the "returnsFrequency" is "day"', () => {
+      beforeEach(() => {
+        dueReturnLog.returnsFrequency = 'day'
+      })
 
+      describe('the "pageTitle" property', () => {
         it('should return the relevant title', () => {
           const result = PrepareReturnFormsPresenter.go(session, dueReturnLog, recipient)
 
           expect(result.pageTitle).to.equal('Water abstraction daily return')
         })
       })
+    })
 
-      describe('when the "returnsFrequency" is "month"', () => {
-        beforeEach(() => {
-          dueReturnLog.returnsFrequency = 'month'
-        })
+    describe('when the "returnsFrequency" is "month"', () => {
+      beforeEach(() => {
+        dueReturnLog.returnsFrequency = 'month'
+      })
 
+      describe('the "pageTitle" property', () => {
         it('should return the relevant title', () => {
           const result = PrepareReturnFormsPresenter.go(session, dueReturnLog, recipient)
 
@@ -114,23 +118,99 @@ describe('Notices - Setup - Prepare Return Forms Presenter', () => {
         })
       })
 
-      describe('when the "returnsFrequency" is "quarter"', () => {
-        beforeEach(() => {
-          dueReturnLog.returnsFrequency = 'quarter'
-        })
-
-        it('should return the relevant title', () => {
+      describe('the "meterReadings" property', () => {
+        it('should return meter readings', () => {
           const result = PrepareReturnFormsPresenter.go(session, dueReturnLog, recipient)
 
-          expect(result.pageTitle).to.equal('Water abstraction quarterly return')
+          expect(result.meterReadings).to.equal([
+            [
+              [
+                '1 January 2021',
+                '2 January 2021',
+                '3 January 2021',
+                '4 January 2021',
+                '5 January 2021',
+                '6 January 2021',
+                '7 January 2021',
+                '8 January 2021',
+                '9 January 2021',
+                '10 January 2021',
+                '11 January 2021',
+                '12 January 2021'
+              ],
+              [
+                '13 January 2021',
+                '14 January 2021',
+                '15 January 2021',
+                '16 January 2021',
+                '17 January 2021',
+                '18 January 2021',
+                '19 January 2021',
+                '20 January 2021',
+                '21 January 2021',
+                '22 January 2021',
+                '23 January 2021',
+                '24 January 2021'
+              ]
+            ],
+            [
+              [
+                '25 January 2021',
+                '26 January 2021',
+                '27 January 2021',
+                '28 January 2021',
+                '29 January 2021',
+                '30 January 2021',
+                '31 January 2021',
+                '1 February 2021',
+                '2 February 2021',
+                '3 February 2021',
+                '4 February 2021',
+                '5 February 2021'
+              ],
+              [
+                '6 February 2021',
+                '7 February 2021',
+                '8 February 2021',
+                '9 February 2021',
+                '10 February 2021',
+                '11 February 2021',
+                '12 February 2021',
+                '13 February 2021',
+                '14 February 2021',
+                '15 February 2021',
+                '16 February 2021',
+                '17 February 2021'
+              ]
+            ],
+            [
+              [
+                '18 February 2021',
+                '19 February 2021',
+                '20 February 2021',
+                '21 February 2021',
+                '22 February 2021',
+                '23 February 2021',
+                '24 February 2021',
+                '25 February 2021',
+                '26 February 2021',
+                '27 February 2021',
+                '28 February 2021',
+                '1 March 2021'
+              ],
+              ['2 March 2021', '3 March 2021', '4 March 2021', '5 March 2021']
+            ]
+          ])
         })
       })
+    })
 
-      describe('when the "returnsFrequency" is "week"', () => {
-        beforeEach(() => {
-          dueReturnLog.returnsFrequency = 'week'
-        })
+    describe('when the "returnsFrequency" is "week"', () => {
+      beforeEach(() => {
+        dueReturnLog.returnsFrequency = 'week'
+      })
 
+      describe('the "pageTitle" property', () => {
         it('should return the relevant title', () => {
           const result = PrepareReturnFormsPresenter.go(session, dueReturnLog, recipient)
 
@@ -138,15 +218,93 @@ describe('Notices - Setup - Prepare Return Forms Presenter', () => {
         })
       })
 
-      describe('when the "returnsFrequency" is "year"', () => {
-        beforeEach(() => {
-          dueReturnLog.returnsFrequency = 'year'
-        })
-
-        it('should return the relevant title', () => {
+      describe('the "meterReadings" property', () => {
+        it('should return meter readings', () => {
           const result = PrepareReturnFormsPresenter.go(session, dueReturnLog, recipient)
 
-          expect(result.pageTitle).to.equal('Water abstraction yearly return')
+          expect(result.meterReadings).to.equal([
+            [
+              [
+                '1 January 2021',
+                '2 January 2021',
+                '3 January 2021',
+                '4 January 2021',
+                '5 January 2021',
+                '6 January 2021',
+                '7 January 2021',
+                '8 January 2021',
+                '9 January 2021',
+                '10 January 2021',
+                '11 January 2021',
+                '12 January 2021',
+                '13 January 2021',
+                '14 January 2021'
+              ],
+              [
+                '15 January 2021',
+                '16 January 2021',
+                '17 January 2021',
+                '18 January 2021',
+                '19 January 2021',
+                '20 January 2021',
+                '21 January 2021',
+                '22 January 2021',
+                '23 January 2021',
+                '24 January 2021',
+                '25 January 2021',
+                '26 January 2021',
+                '27 January 2021',
+                '28 January 2021'
+              ]
+            ],
+            [
+              [
+                '29 January 2021',
+                '30 January 2021',
+                '31 January 2021',
+                '1 February 2021',
+                '2 February 2021',
+                '3 February 2021',
+                '4 February 2021',
+                '5 February 2021',
+                '6 February 2021',
+                '7 February 2021',
+                '8 February 2021',
+                '9 February 2021',
+                '10 February 2021',
+                '11 February 2021'
+              ],
+              [
+                '12 February 2021',
+                '13 February 2021',
+                '14 February 2021',
+                '15 February 2021',
+                '16 February 2021',
+                '17 February 2021',
+                '18 February 2021',
+                '19 February 2021',
+                '20 February 2021',
+                '21 February 2021',
+                '22 February 2021',
+                '23 February 2021',
+                '24 February 2021',
+                '25 February 2021'
+              ]
+            ],
+            [
+              [
+                '26 February 2021',
+                '27 February 2021',
+                '28 February 2021',
+                '1 March 2021',
+                '2 March 2021',
+                '3 March 2021',
+                '4 March 2021',
+                '5 March 2021'
+              ],
+              []
+            ]
+          ])
         })
       })
     })
