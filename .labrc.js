26f105a78079f4dbed79cea48589d55fe9bb6d18
jonathangoulding <goulding.jon@icloud.com>
2025-08-19 10:54:15 +0000

    feat: use exisiting period logic

--- .labrc.js
+++ .labrc.js
@@ -2,23 +2,23 @@
 
 module.exports = {
   verbose: true,
-  // coverage: true,
-  // 'coverage-exclude': [
-  //   'db/seeds',
-  //   'app/controllers/check.controller.js', // Test helper endpoints
-  //   // Most plugins are not tested directly (but are inherently tested by other tests loading the web server)
-  //   // The plugins below do no get 100% coverage, this comment is here to highlight this.
-  //   'app/plugins/airbrake.plugin.js',
-  //   'app/plugins/auth.plugin.js',
-  //   'app/plugins/hapi-pino.plugin.js',
-  //   'app/plugins/stop.plugin.js',
-  //   'app/plugins/views.plugin.js',
-  //   'config/notify.config.js',
-  //   'templates'
-  // ],
-  // // lcov reporter required for SonarQube
-  // reporter: ['console', 'html', 'lcov'],
-  // output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],
+  coverage: true,
+  'coverage-exclude': [
+    'db/seeds',
+    'app/controllers/check.controller.js', // Test helper endpoints
+    // Most plugins are not tested directly (but are inherently tested by other tests loading the web server)
+    // The plugins below do no get 100% coverage, this comment is here to highlight this.
+    'app/plugins/airbrake.plugin.js',
+    'app/plugins/auth.plugin.js',
+    'app/plugins/hapi-pino.plugin.js',
+    'app/plugins/stop.plugin.js',
+    'app/plugins/views.plugin.js',
+    'config/notify.config.js',
+    'templates'
+  ],
+  // lcov reporter required for SonarQube
+  reporter: ['console', 'html', 'lcov'],
+  output: ['stdout', 'coverage/coverage.html', 'coverage/lcov.info'],
   // @aws-sdk/s3 exposes global variables which cause errors during test if we don't ignore them. lab expects the list
   // of globals to ignore to be a single comma-delimited string; for ease of management we define them in an array then
   // join them.
