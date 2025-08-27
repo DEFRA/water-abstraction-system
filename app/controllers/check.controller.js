b24e8b56bf121808f3620bebbd6c128946868cfc
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-08-27 12:54:30 +0100

    Update controller and route back to /placeholder

--- app/controllers/check.controller.js
+++ app/controllers/check.controller.js
@@ -5,19 +5,7 @@
  * @module CheckController
  */
 
-const GenerateNotifyAddressService = require('../services/check/address/generate-notify-address.service.js')
-const ValidateAddressService = require('../services/check/address/validate-address.service.js')
-
-const SUCCESS_STATUS_CODE = 200
 const NO_CONTENT_STATUS_CODE = 204
-
-async function generateAddress(request, h) {
-  const { licenceRef } = request.payload
-
-  const result = await GenerateNotifyAddressService.go(licenceRef)
-
-  return h.response(result).code(SUCCESS_STATUS_CODE)
-}
 
 /**
  * A test end point for checking functionality
@@ -37,14 +25,6 @@ async function placeholder(request, h) {
   return h.response().code(NO_CONTENT_STATUS_CODE)
 }
 
-async function validateAddress(request, h) {
-  const result = await ValidateAddressService.go(request.payload)
-
-  return h.response(result).code(SUCCESS_STATUS_CODE)
-}
-
 module.exports = {
-  generateAddress,
-  placeholder,
-  validateAddress
+  placeholder
 }
