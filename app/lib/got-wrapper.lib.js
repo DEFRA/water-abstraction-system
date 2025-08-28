a19b5c464fa55e6e0b1daf19ce1d490e6c583530
Stuart Adair <43574728+StuAA78@users.noreply.github.com>
2025-08-28 11:09:19 +0100

    Small refactor for clarity

--- app/lib/got-wrapper.lib.js
+++ app/lib/got-wrapper.lib.js
@@ -25,38 +25,44 @@ async function gotWrapper(defaults = {}) {
   // We use the `await import` workaround to allow us to use the ESM `got`
   const { got } = await import('got')
 
-  // Return a function that mimics the `request` interface
-  return async function requestLike(options, callback) {
-    // `request` can be called with either a string URL for a GET request, or an options object
-    if (typeof options === 'string') {
-      options = { url: options }
+  // Return a request-like function bound to `got` and the provided defaults
+  return (options, callback) => {
+    return requestLike(options, callback, got, defaults)
+  }
+}
+
+// Our `got` wrapper which conforms to the `request` API. Note that we don't name it with an underscore as the function
+// name is exposed elsewhere so we keep it as `requestLike`
+async function requestLike(options, callback, got, defaults) {
+  // `request` can be called with either a string URL for a GET request, or an options object
+  if (typeof options === 'string') {
+    options = { url: options }
+  }
+
+  const gotOptions = {
+    method: options.method || defaults.method || 'GET',
+    headers: { ...defaults.headers, ...options.headers },
+    body: options.body || defaults.body,
+    url: options.url || options.uri,
+    agent: {
+      http: new HttpProxyAgent({ proxy: defaults.proxy }),
+      https: new HttpsProxyAgent({ proxy: defaults.proxy })
+    },
+    responseType: 'text'
+  }
+
+  try {
+    const response = await got(gotOptions)
+
+    const returnedResponse = {
+      statusCode: response.statusCode,
+      headers: response.headers,
+      body: response.body
     }
 
-    const gotOptions = {
-      method: options.method || defaults.method || 'GET',
-      headers: { ...defaults.headers, ...options.headers },
-      body: options.body || defaults.body,
-      url: options.url || options.uri,
-      agent: {
-        http: new HttpProxyAgent({ proxy: defaults.proxy }),
-        https: new HttpsProxyAgent({ proxy: defaults.proxy })
-      },
-      responseType: 'text'
-    }
-
-    try {
-      const response = await got(gotOptions)
-
-      const returnedResponse = {
-        statusCode: response.statusCode,
-        headers: response.headers,
-        body: response.body
-      }
-
-      callback(null, returnedResponse, response.body)
-    } catch (error) {
-      callback(error, null, null)
-    }
+    callback(null, returnedResponse, response.body)
+  } catch (error) {
+    callback(error, null, null)
   }
 }
 
