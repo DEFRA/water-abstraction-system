2157ec9b784dc91ac4d61ec636547ac6bd39df62
robertparkinson <robert@daedalusft.com>
2025-08-29 15:42:53 +0100

    fix failing tests

--- test/services/licences/view-licence-set-up.service.test.js
+++ test/services/licences/view-licence-set-up.service.test.js
@@ -153,6 +153,7 @@ describe('View Licence Set Up service', () => {
           {
             action: [
               {
+                dataTest: 'return-version-0',
                 link: '/system/return-versions/0312e5eb-67ae-44fb-922c-b1a0b81bc08d',
                 text: 'View'
               }
