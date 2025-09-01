cadce6d89942d87f9c518403a0449869f4a8f7c5
jonathangoulding <goulding.jon@icloud.com>
2025-09-01 14:35:46 +0100

    test: all recipients

---
+++ test/services/notices/setup/all-recipients.service.test.js
@@ -0,0 +1,195 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
+
+// Thing under test
+const AllRecipientsService = require('../../../../app/services/notices/setup/all-recipients.service.js')
+
+describe('Notices - Setup - All Recipients service', () => {
+  let recipients
+  let recipientsFixture
+  let session
+
+  beforeEach(() => {
+    recipientsFixture = RecipientsFixture.recipients()
+
+    recipients = [...Object.values(recipientsFixture)]
+
+    session = {}
+  })
+
+  describe('when getting the recipients', () => {
+    describe('and all recipients are required', () => {
+      beforeEach(() => {
+        recipients = [recipientsFixture.primaryUser, recipientsFixture.returnsAgent, recipientsFixture.licenceHolder]
+      })
+
+      it('returns all the recipients formatted for display', () => {
+        const result = AllRecipientsService.go(session, recipients)
+
+        expect(result).to.equal([
+          {
+            contact: null,
+            contact_hash_id: '90129f6aa5bf2ad50aa3fefd3f8cf86a',
+            contact_type: 'Primary user',
+            email: 'primary.user@important.com',
+            licence_refs: recipientsFixture.primaryUser.licence_refs
+          },
+          {
+            contact: null,
+            contact_hash_id: '2e6918568dfbc1d78e2fbe279aaee990',
+            contact_type: 'Returns agent',
+            email: 'returns.agent@important.com',
+            licence_refs: recipientsFixture.returnsAgent.licence_refs
+          },
+          {
+            contact: {
+              addressLine1: '1',
+              addressLine2: 'Privet Drive',
+              addressLine3: null,
+              addressLine4: null,
+              country: null,
+              county: 'Surrey',
+              forename: 'Harry',
+              initials: 'H J',
+              name: 'Licence holder',
+              postcode: 'WD25 7LR',
+              role: 'Licence holder',
+              salutation: 'Mr',
+              town: 'Little Whinging',
+              type: 'Person'
+            },
+            contact_hash_id: '22f6457b6be9fd63d8a9a8dd2ed61214',
+            contact_type: 'Licence holder',
+            licence_refs: recipientsFixture.licenceHolder.licence_refs
+          }
+        ])
+      })
+    })
+
+    describe('and all recipients are not required', () => {
+      describe('when there are selected recipients', () => {
+        beforeEach(() => {
+          session = {
+            selectedRecipients: [recipientsFixture.licenceHolder.contact_hash_id]
+          }
+        })
+
+        it('returns only the selected recipients formatted for display', () => {
+          const result = AllRecipientsService.go(session, recipients, false)
+
+          expect(result).to.equal([
+            {
+              contact: {
+                addressLine1: '1',
+                addressLine2: 'Privet Drive',
+                addressLine3: null,
+                addressLine4: null,
+                country: null,
+                county: 'Surrey',
+                forename: 'Harry',
+                initials: 'H J',
+                name: 'Licence holder',
+                postcode: 'WD25 7LR',
+                role: 'Licence holder',
+                salutation: 'Mr',
+                town: 'Little Whinging',
+                type: 'Person'
+              },
+              contact_hash_id: '22f6457b6be9fd63d8a9a8dd2ed61214',
+              contact_type: 'Licence holder',
+              licence_refs: recipientsFixture.licenceHolder.licence_refs
+            }
+          ])
+        })
+      })
+
+      describe('when there are no selected recipients', () => {
+        beforeEach(() => {
+          recipients = [recipientsFixture.primaryUser, recipientsFixture.returnsAgent]
+        })
+
+        it('returns all the recipients formatted for display', () => {
+          const result = AllRecipientsService.go(session, recipients, false)
+
+          expect(result).to.equal([
+            {
+              contact: null,
+              contact_hash_id: '90129f6aa5bf2ad50aa3fefd3f8cf86a',
+              contact_type: 'Primary user',
+              email: 'primary.user@important.com',
+              licence_refs: recipientsFixture.primaryUser.licence_refs
+            },
+            {
+              contact: null,
+              contact_hash_id: '2e6918568dfbc1d78e2fbe279aaee990',
+              contact_type: 'Returns agent',
+              email: 'returns.agent@important.com',
+              licence_refs: recipientsFixture.returnsAgent.licence_refs
+            }
+          ])
+        })
+      })
+    })
+
+    describe('and there are "additionalRecipients"', () => {
+      beforeEach(() => {
+        session = {
+          additionalRecipients: [recipientsFixture.licenceHolder]
+        }
+
+        recipients = [recipientsFixture.primaryUser, recipientsFixture.returnsAgent]
+      })
+
+      it('returns all the recipients (including "additionalRecipients") formatted for display', () => {
+        const result = AllRecipientsService.go(session, recipients, false)
+
+        expect(result).to.equal([
+          {
+            contact: null,
+            contact_hash_id: '90129f6aa5bf2ad50aa3fefd3f8cf86a',
+            contact_type: 'Primary user',
+            email: 'primary.user@important.com',
+            licence_refs: recipientsFixture.primaryUser.licence_refs
+          },
+          {
+            contact: null,
+            contact_hash_id: '2e6918568dfbc1d78e2fbe279aaee990',
+            contact_type: 'Returns agent',
+            email: 'returns.agent@important.com',
+            licence_refs: recipientsFixture.returnsAgent.licence_refs
+          },
+          {
+            contact: {
+              addressLine1: '1',
+              addressLine2: 'Privet Drive',
+              addressLine3: null,
+              addressLine4: null,
+              country: null,
+              county: 'Surrey',
+              forename: 'Harry',
+              initials: 'H J',
+              name: 'Licence holder',
+              postcode: 'WD25 7LR',
+              role: 'Licence holder',
+              salutation: 'Mr',
+              town: 'Little Whinging',
+              type: 'Person'
+            },
+            contact_hash_id: '22f6457b6be9fd63d8a9a8dd2ed61214',
+            contact_type: 'Licence holder',
+            licence_refs: recipientsFixture.licenceHolder.licence_refs
+          }
+        ])
+      })
+    })
+  })
+})
