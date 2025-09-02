3eb40244f5f4047b07fdf726f380ffc3fdc6777d
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-02 17:36:33 +0100

    Update the legacy migration to default the date

--- db/migrations/legacy/20221108007033_water-scheduled-notification.js
+++ db/migrations/legacy/20221108007033_water-scheduled-notification.js
@@ -30,7 +30,7 @@ exports.up = function (knex) {
     table.string('job_id')
 
     // Legacy timestamps
-    table.timestamp('date_created', { useTz: false })
+    table.timestamp('date_created', { useTz: false }).defaultTo(knex.fn.now())
 
     // Constraints
     table.unique(['job_id'], { useConstraint: true })
