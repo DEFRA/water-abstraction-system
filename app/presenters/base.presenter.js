d7256356ff3e1a9fa145d6469e58bfcb55b0413c
Jason Claxton <jason.claxton@defra.gov.uk>
2025-09-04 11:13:57 +0100

    Add `formatReturnLogStatus` to base presenter This function previously existed in `app/presenters/return-logs/base-return-logs.presenter.js` but is being moved to the base presenter as it's going to also be used in the licence returns tab

--- app/presenters/base.presenter.js
+++ app/presenters/base.presenter.js
@@ -256,6 +256,59 @@ function formatQuantity(units, quantity) {
 }
 
 /**
+ * Formats the status for a return log, adjusting for specific conditions.
+ *
+ * If the return log's status is 'completed', it will be displayed as 'complete'. If the status is 'due', but there are
+ * 28 days before the returns due date, it will display 'not due yet'. If the status is 'due' and the due date has
+ * passed, it will be displayed as 'overdue'. For all other cases, it will return the status as is.
+ *
+ * @param {module:ReturnLogModel} returnLog - The return log containing status and due date information
+ *
+ * @returns {string} The formatted status for display.
+ */
+function formatReturnLogStatus(returnLog) {
+  const { status, dueDate } = returnLog
+
+  // If the return is completed we are required to display it as 'complete'. This also takes priority over the other
+  // statues
+  if (status === 'completed') {
+    return 'complete'
+  }
+
+  // Work out if the return is overdue (status is still 'due' and it is past the due date)
+  const today = new Date()
+
+  // The due date held in the record is date-only. If we compared it against 'today' without this step any return due
+  // 'today' would be flagged as overdue when it is still due (just!)
+  today.setHours(0, 0, 0, 0)
+
+  if (status === 'due') {
+    if (!dueDate) {
+      return 'not due yet'
+    }
+
+    if (dueDate < today) {
+      return 'overdue'
+    }
+
+    // A return is considered "due" for 28 days, starting 28 days before the due date
+    // Any date before this period should be marked as "not due yet"
+    const notDueUntil = new Date(dueDate)
+
+    // Calculate the start of the "due" period, which begins 27 days before the due date
+    notDueUntil.setDate(notDueUntil.getDate() - DUE_PERIOD_DAYS)
+
+    // If today is before the "due" period starts, the return is "not due yet"
+    if (today < notDueUntil) {
+      return 'not due yet'
+    }
+  }
+
+  // For all other cases we can just return the status and the return-status-tag macro will know how to display it
+  return status
+}
+
+/**
  * Format a Joi validation result into a format that can be used by our views
  *
  * If {@link https://joi.dev/ | Joi} found no validation errors the result will not contain an `error:` property.
@@ -458,6 +511,7 @@ module.exports = {
   formatPounds,
   formatPurposes,
   formatQuantity,
+  formatReturnLogStatus,
   formatValidationResult,
   formatValueUnit,
   leftPadZeroes,
