aa9305965c95776e042a92f69fad176838841e8c
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-05 00:10:35 +0100

    Update select presenter based on changes

--- test/presenters/address/select.presenter.test.js
+++ test/presenters/address/select.presenter.test.js
@@ -12,60 +12,72 @@ const SelectPresenter = require('../../../app/presenters/address/select.presente
 
 describe('Address - Select Presenter', () => {
   let addresses
+  let session
 
-  describe('when called with one address', () => {
-    beforeEach(() => {
-      addresses = [
+  beforeEach(async () => {
+    addresses = [
+      {
+        address: 'address 1',
+        postcode: 'SW1A 1AA',
+        uprn: '123456789'
+      },
+      {
+        address: 'address 2',
+        postcode: 'SW1A 1AA',
+        uprn: '123456780'
+      }
+    ]
+
+    session = {
+      id: 'fecd5f15-bacf-4b3d-bdcd-ef279a97b061',
+      addressJourney: {
+        activeNavBar: 'manage',
+        address: { postcode: 'SW1A 1AA' },
+        backLink: {
+          href: '/system/notices/setup/fecd5f15-bacf-4b3d-bdcd-ef279a97b061/contact-type',
+          text: 'Back'
+        },
+        redirectUrl: '/system/notices/setup/fecd5f15-bacf-4b3d-bdcd-ef279a97b061/add-recipient'
+      }
+    }
+  })
+
+  it('correctly presents the data', () => {
+    const result = SelectPresenter.go(session, addresses)
+
+    expect(result).to.equal({
+      activeNavBar: 'manage',
+      addresses: [
         {
-          address: 'address 1',
-          postcode: 'SW1A 1AA',
-          uprn: '123456789'
+          value: 'select',
+          selected: true,
+          text: `2 addresses found`
+        },
+        {
+          text: 'address 1',
+          value: '123456789'
+        },
+        {
+          text: 'address 2',
+          value: '123456780'
         }
-      ]
-    })
-
-    it('returns page data for the view', () => {
-      const result = SelectPresenter.go(addresses)
-
-      expect(result).to.equal({
-        addresses: [
-          {
-            value: 'select',
-            selected: true,
-            text: `1 address found`
-          },
-          {
-            text: 'address 1',
-            value: '123456789'
-          }
-        ],
-        pageTitle: 'Select the address',
-        postcode: addresses[0].postcode
-      })
+      ],
+      backLink: {
+        href: '/system/address/fecd5f15-bacf-4b3d-bdcd-ef279a97b061/postcode',
+        text: 'Back'
+      },
+      pageTitle: 'Select the address',
+      postcode: 'SW1A 1AA',
+      sessionId: 'fecd5f15-bacf-4b3d-bdcd-ef279a97b061'
     })
   })
 
-  describe('when called with multiple addresses', () => {
-    beforeEach(() => {
-      addresses = [
-        {
-          address: 'address 1',
-          postcode: 'SW1A 1AA',
-          uprn: '123456789'
-        },
-        {
-          address: 'address 2',
-          postcode: 'SW1A 1AA',
-          uprn: '123456780'
-        }
-      ]
-    })
+  describe('the "addresses" property', () => {
+    describe('when multiple addresses were found', () => {
+      it('returns all the addresses, and the inserted description entry is pluralised and selected', () => {
+        const result = SelectPresenter.go(session, addresses)
 
-    it('returns page data for the view', () => {
-      const result = SelectPresenter.go(addresses)
-
-      expect(result).to.equal({
-        addresses: [
+        expect(result.addresses).to.equal([
           {
             value: 'select',
             selected: true,
@@ -79,9 +91,25 @@ describe('Address - Select Presenter', () => {
             text: 'address 2',
             value: '123456780'
           }
-        ],
-        pageTitle: 'Select the address',
-        postcode: addresses[0].postcode
+        ])
+      })
+    })
+
+    describe('when one address was found', () => {
+      it('returns the one address, and the inserted description entry is singular and selected', () => {
+        const result = SelectPresenter.go(session, [addresses[0]])
+
+        expect(result.addresses).to.equal([
+          {
+            value: 'select',
+            selected: true,
+            text: `1 address found`
+          },
+          {
+            text: 'address 1',
+            value: '123456789'
+          }
+        ])
       })
     })
   })
