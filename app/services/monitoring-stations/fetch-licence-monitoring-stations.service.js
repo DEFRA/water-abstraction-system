9236883cec643b4f1e35c84f79f755c76eea2c75
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-06 11:10:15 +0100

    Update fetch service
    It now returns the data we need and drops the call to notifications. Also, to simplify things rather than do it in a single objection query (which results in many behind the scenes), we use 3, which better reflects the data we are after.

--- app/services/monitoring-stations/fetch-licence-monitoring-stations.service.js
+++ app/services/monitoring-stations/fetch-licence-monitoring-stations.service.js
@@ -1,97 +1,115 @@
 'use strict'
 
 /**
- * Fetches the matching monitoring station and additional records needed for the view licence page
+ * Fetches the licence monitoring stations, plus the parent licence and monitoring station for view licence page
  * @module FetchLicenceMonitoringStationsService
  */
 
-const { ref } = require('objection')
-
+const LicenceModel = require('../../models/licence.model.js')
+const LicenceMonitoringStationModel = require('../../models/licence-monitoring-station.model.js')
 const MonitoringStationModel = require('../../models/monitoring-station.model.js')
-const NotificationModel = require('../../models/notification.model.js')
 
 /**
- * Fetches the matching monitoring station and additional records needed for the view licence page
+ * Fetches the licence monitoring stations, plus the parent licence and monitoring station for view licence page
  *
  * @param {string} licenceId - The UUID of the licence
  * @param {string} monitoringStationId - The UUID of the monitoring station
  *
- * @returns {Promise<object>} the matching instance of the `MonitoringStationModel` and `NotificationModel` populated
- * with the data needed for the view licence page
+ * @returns {Promise<object>} the matching instance of the `MonitoringStationModel`, `LicenceModel` and associated
+ * `LicenceMonitoringStationModel` instances
  */
 async function go(licenceId, monitoringStationId) {
-  const lastAlert = await _fetchLastAlert(licenceId)
-  const monitoringStationLicenceTags = await _fetchMonitoringStationLicenceTags(licenceId, monitoringStationId)
+  const licence = await _fetchLicence(licenceId)
+  const monitoringStation = await _fetchMonitoringStation(monitoringStationId)
+  const licenceMonitoringStations = await _fetchLicenceMonitoringStations(licenceId, monitoringStationId)
 
-  return { lastAlert, monitoringStationLicenceTags }
+  return { licence, licenceMonitoringStations, monitoringStation }
 }
 
-async function _fetchLastAlert(licenceId) {
-  return NotificationModel.query()
-    .select([
-      'id',
-      ref('personalisation:alertType').castText().as('alertType'),
-      ref('personalisation:address_line_1').castText().as('contact'),
-      'createdAt',
-      'messageRef',
-      'messageType',
-      'recipient',
-      ref('personalisation:sending_alert_type').castText().as('sendingAlertType'),
-      'status'
-    ])
-    .where(ref('personalisation:licenceId').castText(), licenceId)
-    .where('status', 'sent')
-    .whereLike('messageRef', 'water_abstraction_alert%')
-    .orderBy('createdAt', 'desc') // Order by most recent first
-    .first() // Return only the first result
+async function _fetchLicence(monitoringStationId) {
+  return LicenceModel.query().findById(monitoringStationId).select(['id', 'licenceRef'])
 }
 
-async function _fetchMonitoringStationLicenceTags(licenceId, monitoringStationId) {
-  return MonitoringStationModel.query()
-    .findById(monitoringStationId)
-    .select(['id', 'label', 'riverName'])
-    .withGraphFetched('licenceMonitoringStations')
-    .modifyGraph('licenceMonitoringStations', (licenceMonitoringStationsBuilder) => {
-      licenceMonitoringStationsBuilder
-        .select([
-          'licenceMonitoringStations.id',
-          'licenceMonitoringStations.createdAt',
-          'licenceMonitoringStations.licenceId',
-          'licenceMonitoringStations.restrictionType',
-          'licenceMonitoringStations.thresholdUnit',
-          'licenceMonitoringStations.thresholdValue'
-        ])
-        .whereNull('licenceMonitoringStations.deletedAt')
-        .where('licenceMonitoringStations.licenceId', licenceId)
-        .orderBy([{ column: 'licenceMonitoringStations.thresholdValue', order: 'desc' }])
-        .withGraphFetched('licence')
-        .modifyGraph('licence', (licenceBuilder) => {
-          licenceBuilder.select(['licenceRef'])
-        })
-        .withGraphFetched('licenceVersionPurposeCondition')
-        .modifyGraph('licenceVersionPurposeCondition', (licenceVersionPurposeConditionBuilder) => {
-          licenceVersionPurposeConditionBuilder
-            .select(['externalId', 'notes'])
-            .withGraphFetched('licenceVersionPurpose')
-            .modifyGraph('licenceVersionPurpose', (licenceVersionPurposeBuilder) => {
-              licenceVersionPurposeBuilder
-                .select(['id'])
-                .withGraphFetched('licenceVersion')
-                .modifyGraph('licenceVersion', (licenceVersionBuilder) => {
-                  licenceVersionBuilder.select(['status'])
-                })
-            })
-            .withGraphFetched('licenceVersionPurposeConditionType')
-            .modifyGraph('licenceVersionPurposeConditionType', (licenceVersionPurposeConditionTypeBuilder) => {
-              licenceVersionPurposeConditionTypeBuilder.select(['displayTitle'])
+async function _fetchLicenceMonitoringStations(licenceId, monitoringStationId) {
+  return LicenceMonitoringStationModel.query()
+    .select(['createdAt', 'id', 'restrictionType', 'status', 'statusUpdatedAt', 'thresholdUnit', 'thresholdValue'])
+    .where('licenceId', licenceId)
+    .where('monitoringStationId', monitoringStationId)
+    .orderBy([{ column: 'thresholdValue', order: 'desc' }])
+    .withGraphFetched('licenceVersionPurposeCondition')
+    .modifyGraph('licenceVersionPurposeCondition', (licenceVersionPurposeConditionBuilder) => {
+      licenceVersionPurposeConditionBuilder
+        .select(['externalId', 'id', 'notes'])
+        .withGraphFetched('licenceVersionPurpose')
+        .modifyGraph('licenceVersionPurpose', (licenceVersionPurposeBuilder) => {
+          licenceVersionPurposeBuilder
+            .select(['id'])
+            .withGraphFetched('licenceVersion')
+            .modifyGraph('licenceVersion', (licenceVersionBuilder) => {
+              licenceVersionBuilder.select(['id', 'status'])
             })
         })
-        .withGraphFetched('user')
-        .modifyGraph('user', (userBuilder) => {
-          userBuilder.select(['username'])
+        .withGraphFetched('licenceVersionPurposeConditionType')
+        .modifyGraph('licenceVersionPurposeConditionType', (licenceVersionPurposeConditionTypeBuilder) => {
+          licenceVersionPurposeConditionTypeBuilder.select(['id', 'displayTitle'])
         })
     })
+    .withGraphFetched('user')
+    .modifyGraph('user', (userBuilder) => {
+      userBuilder.select(['id', 'username'])
+    })
 }
+
+async function _fetchMonitoringStation(monitoringStationId) {
+  return MonitoringStationModel.query().findById(monitoringStationId).select(['id', 'label', 'riverName'])
+}
+
+// async function _fetchMonitoringStationLicenceTags(licenceId, monitoringStationId) {
+//   return MonitoringStationModel.query()
+//     .findById(monitoringStationId)
+//     .select(['id', 'label', 'riverName'])
+//     .withGraphFetched('licenceMonitoringStations')
+//     .modifyGraph('licenceMonitoringStations', (licenceMonitoringStationsBuilder) => {
+//       licenceMonitoringStationsBuilder
+//         .select([
+//           'licenceMonitoringStations.id',
+//           'licenceMonitoringStations.createdAt',
+//           'licenceMonitoringStations.licenceId',
+//           'licenceMonitoringStations.restrictionType',
+//           'licenceMonitoringStations.thresholdUnit',
+//           'licenceMonitoringStations.thresholdValue'
+//         ])
+//         .whereNull('licenceMonitoringStations.deletedAt')
+//         .where('licenceMonitoringStations.licenceId', licenceId)
+//         .orderBy([{ column: 'licenceMonitoringStations.thresholdValue', order: 'desc' }])
+//         .withGraphFetched('licence')
+//         .modifyGraph('licence', (licenceBuilder) => {
+//           licenceBuilder.select(['licenceRef'])
+//         })
+//         .withGraphFetched('licenceVersionPurposeCondition')
+//         .modifyGraph('licenceVersionPurposeCondition', (licenceVersionPurposeConditionBuilder) => {
+//           licenceVersionPurposeConditionBuilder
+//             .select(['externalId', 'notes'])
+//             .withGraphFetched('licenceVersionPurpose')
+//             .modifyGraph('licenceVersionPurpose', (licenceVersionPurposeBuilder) => {
+//               licenceVersionPurposeBuilder
+//                 .select(['id'])
+//                 .withGraphFetched('licenceVersion')
+//                 .modifyGraph('licenceVersion', (licenceVersionBuilder) => {
+//                   licenceVersionBuilder.select(['status'])
+//                 })
+//             })
+//             .withGraphFetched('licenceVersionPurposeConditionType')
+//             .modifyGraph('licenceVersionPurposeConditionType', (licenceVersionPurposeConditionTypeBuilder) => {
+//               licenceVersionPurposeConditionTypeBuilder.select(['displayTitle'])
+//             })
+//         })
+//         .withGraphFetched('user')
+//         .modifyGraph('user', (userBuilder) => {
+//           userBuilder.select(['username'])
+//         })
+//     })
+// }
 
 module.exports = {
   go
--- test/services/monitoring-stations/fetch-licence-monitoring-stations.service.test.js
+++ test/services/monitoring-stations/fetch-licence-monitoring-stations.service.test.js
@@ -4,11 +4,10 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, before, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, before, beforeEach, afterEach, after } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const { generateUUID } = require('../../../app/lib/general.lib.js')
 const LicenceHelper = require('../../support/helpers/licence.helper.js')
 const LicenceMonitoringStationHelper = require('../../support/helpers/licence-monitoring-station.helper.js')
 const LicenceVersionHelper = require('../../support/helpers/licence-version.helper.js')
@@ -16,174 +15,243 @@ const LicenceVersionPurposeHelper = require('../../support/helpers/licence-versi
 const LicenceVersionPurposeConditionHelper = require('../../support/helpers/licence-version-purpose-condition.helper.js')
 const LicenceVersionPurposeConditionTypeHelper = require('../../support/helpers/licence-version-purpose-condition-type.helper.js')
 const MonitoringStationHelper = require('../../support/helpers/monitoring-station.helper.js')
-const NotificationHelper = require('../../support/helpers/notification.helper.js')
 const UserHelper = require('../../support/helpers/user.helper.js')
 
 // Thing under test
 const FetchLicenceMonitoringStationsService = require('../../../app/services/monitoring-stations/fetch-licence-monitoring-stations.service.js')
 
 describe('Monitoring Stations - Fetch Licence Monitoring Stations service', () => {
+  let licence
+  let licenceMonitoringStations
+  let licenceVersion
+  let licenceVersionPurpose
+  let licenceVersionPurposeCondition
+  let licenceVersionPurposeConditionType
   let monitoringStation
+  let user
 
   before(async () => {
+    licenceVersionPurposeConditionType = await LicenceVersionPurposeConditionTypeHelper.select()
+    user = UserHelper.select()
+
     monitoringStation = await MonitoringStationHelper.add({
       label: 'The Monitoring Station',
       riverName: 'The River'
     })
+
+    licence = await LicenceHelper.add()
+    licenceVersion = await LicenceVersionHelper.add({ licenceId: licence.id })
+    licenceVersionPurpose = await LicenceVersionPurposeHelper.add({ licenceVersionId: licenceVersion.id })
+    licenceVersionPurposeCondition = await LicenceVersionPurposeConditionHelper.add({
+      licenceVersionPurposeConditionTypeId: licenceVersionPurposeConditionType.id,
+      licenceVersionPurposeId: licenceVersionPurpose.id,
+      notes: 'This is the effect of restriction'
+    })
   })
 
-  describe('when fetching details of the last alert sent', () => {
-    let licenceId
+  beforeEach(() => {
+    licenceMonitoringStations = []
+  })
 
-    beforeEach(() => {
-      licenceId = generateUUID()
-    })
+  afterEach(async () => {
+    for (const licenceMonitoringStation of licenceMonitoringStations) {
+      await licenceMonitoringStation.$query().delete()
+    }
+  })
 
-    describe('and NO alerts exist for the licence', () => {
-      it('returns nothing', async () => {
-        const result = await FetchLicenceMonitoringStationsService.go(licenceId, monitoringStation.id)
+  after(async () => {
+    await licenceVersionPurposeCondition.$query().delete()
+    await licenceVersionPurpose.$query().delete()
+    await licenceVersion.$query().delete()
+    await licence.$query().delete()
+    await monitoringStation.$query().delete()
+  })
 
-        expect(result.lastAlert).to.be.undefined()
-      })
-    })
-
-    describe('and alerts exist for the licence', () => {
+  describe('when a matching monitoring station and licence exists', () => {
+    describe('and licence monitoring station records exist for them', () => {
       beforeEach(async () => {
-        const licence = await LicenceHelper.add()
-        licenceId = licence.id
+        let licenceMonitoringStation = await LicenceMonitoringStationHelper.add({
+          createdBy: user.id,
+          licenceId: licence.id,
+          licenceVersionPurposeConditionId: licenceVersionPurposeCondition.id,
+          monitoringStationId: monitoringStation.id,
+          restrictionType: 'stop',
+          status: 'resume',
+          statusUpdatedAt: new Date('2025-08-26 21:22:05')
+        })
+        licenceMonitoringStations.push(licenceMonitoringStation)
 
-        await _createAlerts(licenceId)
+        licenceMonitoringStation = await LicenceMonitoringStationHelper.add({
+          abstractionPeriodStartDay: 1,
+          abstractionPeriodStartMonth: 4,
+          abstractionPeriodEndDay: 31,
+          abstractionPeriodEndMonth: 3,
+          createdBy: user.id,
+          licenceId: licence.id,
+          monitoringStationId: monitoringStation.id,
+          restrictionType: 'reduce',
+          thresholdValue: 500
+        })
+        licenceMonitoringStations.push(licenceMonitoringStation)
       })
 
-      it('returns the latest sent water abstraction alert', async () => {
-        const result = await FetchLicenceMonitoringStationsService.go(licenceId, monitoringStation.id)
+      it('returns the licence, monitoring station and licence monitoring station records', async () => {
+        const result = await FetchLicenceMonitoringStationsService.go(licence.id, monitoringStation.id)
 
-        expect(result.lastAlert).to.equal(
+        expect(result.licence).to.equal({ id: licence.id, licenceRef: licence.licenceRef })
+        expect(result.monitoringStation).to.equal({
+          id: monitoringStation.id,
+          label: 'The Monitoring Station',
+          riverName: 'The River'
+        })
+
+        expect(result.licenceMonitoringStations).to.equal([
           {
-            alertType: 'stop',
-            contact: 'Big Farm Two',
-            createdAt: new Date('2025-04-17'),
-            messageRef: 'water_abstraction_alert_stop',
-            messageType: 'letter',
-            recipient: null,
-            sendingAlertType: 'stop',
-            status: 'sent'
+            createdAt: licenceMonitoringStations[1].createdAt,
+            id: licenceMonitoringStations[1].id,
+            licenceVersionPurposeCondition: null,
+            restrictionType: licenceMonitoringStations[1].restrictionType,
+            status: licenceMonitoringStations[1].status,
+            statusUpdatedAt: licenceMonitoringStations[1].statusUpdatedAt,
+            thresholdUnit: licenceMonitoringStations[1].thresholdUnit,
+            thresholdValue: licenceMonitoringStations[1].thresholdValue,
+            user: { id: user.id, username: user.username }
           },
-          { skip: 'id' }
-        )
-      })
-    })
-  })
-
-  describe('when a matching licence monitoring station exists', () => {
-    let licence
-    let licenceMonitoringStation
-    let licenceVersionPurposeCondition
-    let licenceVersionPurposeId
-
-    beforeEach(async () => {
-      licence = await LicenceHelper.add()
-
-      const { id: licenceVersionId } = await LicenceVersionHelper.add({ licenceId: licence.id })
-
-      const licenceVersionPurpose = await LicenceVersionPurposeHelper.add({ licenceVersionId })
-      licenceVersionPurposeId = licenceVersionPurpose.id
-
-      const { id: licenceVersionPurposeConditionTypeId } = LicenceVersionPurposeConditionTypeHelper.select(22)
-
-      licenceVersionPurposeCondition = await LicenceVersionPurposeConditionHelper.add({
-        licenceVersionPurposeId,
-        licenceVersionPurposeConditionTypeId,
-        notes: 'This is the effect of restriction'
-      })
-
-      const { id: userId } = await UserHelper.add({ username: 'station-monitor@wrls.gov.uk' })
-
-      licenceMonitoringStation = await LicenceMonitoringStationHelper.add({
-        createdAt: new Date('2025-05-20'),
-        createdBy: userId,
-        licenceId: licence.id,
-        licenceVersionPurposeConditionId: licenceVersionPurposeCondition.id,
-        monitoringStationId: monitoringStation.id,
-        restrictionType: 'reduce'
-      })
-    })
-
-    it('returns the matching data', async () => {
-      const result = await FetchLicenceMonitoringStationsService.go(licence.id, monitoringStation.id)
-
-      expect(result.monitoringStationLicenceTags).to.equal({
-        id: monitoringStation.id,
-        label: 'The Monitoring Station',
-        riverName: 'The River',
-        licenceMonitoringStations: [
           {
-            id: licenceMonitoringStation.id,
-            createdAt: new Date('2025-05-20'),
-            licenceId: licence.id,
-            restrictionType: 'reduce',
-            thresholdUnit: 'm3/s',
-            thresholdValue: 100,
-            licence: { licenceRef: licence.licenceRef },
+            createdAt: licenceMonitoringStations[0].createdAt,
+            id: licenceMonitoringStations[0].id,
             licenceVersionPurposeCondition: {
               externalId: licenceVersionPurposeCondition.externalId,
-              notes: 'This is the effect of restriction',
+              id: licenceVersionPurposeCondition.id,
+              notes: licenceVersionPurposeCondition.notes,
               licenceVersionPurpose: {
-                id: licenceVersionPurposeId,
-                licenceVersion: { status: 'current' }
+                id: licenceVersionPurpose.id,
+                licenceVersion: { id: licenceVersion.id, status: licenceVersion.status }
               },
               licenceVersionPurposeConditionType: {
-                displayTitle: 'Flow cessation condition'
+                displayTitle: licenceVersionPurposeConditionType.displayTitle,
+                id: licenceVersionPurposeConditionType.id
               }
             },
-            user: { username: 'station-monitor@wrls.gov.uk' }
+            restrictionType: licenceMonitoringStations[0].restrictionType,
+            status: licenceMonitoringStations[0].status,
+            statusUpdatedAt: licenceMonitoringStations[0].statusUpdatedAt,
+            thresholdUnit: licenceMonitoringStations[0].thresholdUnit,
+            thresholdValue: licenceMonitoringStations[0].thresholdValue,
+            user: { id: user.id, username: user.username }
           }
-        ]
+        ])
+      })
+    })
+
+    describe('but no licence monitoring station records exist for them', () => {
+      it('returns the licence, monitoring station but no licence monitoring station records', async () => {
+        const result = await FetchLicenceMonitoringStationsService.go(licence.id, monitoringStation.id)
+
+        expect(result.licence).to.equal({ id: licence.id, licenceRef: licence.licenceRef })
+        expect(result.monitoringStation).to.equal({
+          id: monitoringStation.id,
+          label: 'The Monitoring Station',
+          riverName: 'The River'
+        })
+
+        expect(result.licenceMonitoringStations).to.be.empty()
       })
     })
   })
+
+  describe('when a matching monitoring station does not exist', () => {
+    it('returns only the licence record populated', async () => {
+      const result = await FetchLicenceMonitoringStationsService.go(licence.id, '1dcbafad-a1c6-43ec-9313-7149b40ffa57')
+
+      expect(result.licence).to.equal({ id: licence.id, licenceRef: licence.licenceRef })
+      expect(result.monitoringStation).to.be.undefined()
+      expect(result.licenceMonitoringStations).to.be.empty()
+    })
+  })
+
+  describe('when a matching licence does not exist', () => {
+    it('returns only the monitoring station record populated', async () => {
+      const result = await FetchLicenceMonitoringStationsService.go(
+        '86cb402a-5122-407a-beea-3f5422133e55',
+        monitoringStation.id
+      )
+
+      expect(result.licence).to.be.undefined()
+      expect(result.monitoringStation).to.equal({
+        id: monitoringStation.id,
+        label: 'The Monitoring Station',
+        riverName: 'The River'
+      })
+      expect(result.licenceMonitoringStations).to.be.empty()
+    })
+  })
+
+  // describe('when a matching licence monitoring station exists', () => {
+  //   let licence
+  //   let licenceMonitoringStation
+  //   let licenceVersionPurposeCondition
+  //   let licenceVersionPurposeId
+
+  //   beforeEach(async () => {
+  //     licence = await LicenceHelper.add()
+
+  //     const { id: licenceVersionId } = await LicenceVersionHelper.add({ licenceId: licence.id })
+
+  //     const licenceVersionPurpose = await LicenceVersionPurposeHelper.add({ licenceVersionId })
+  //     licenceVersionPurposeId = licenceVersionPurpose.id
+
+  //     const { id: licenceVersionPurposeConditionTypeId } = LicenceVersionPurposeConditionTypeHelper.select(22)
+
+  //     licenceVersionPurposeCondition = await LicenceVersionPurposeConditionHelper.add({
+  //       licenceVersionPurposeId,
+  //       licenceVersionPurposeConditionTypeId,
+  //       notes: 'This is the effect of restriction'
+  //     })
+
+  //     const { id: userId } = await UserHelper.add({ username: 'station-monitor@wrls.gov.uk' })
+
+  //     licenceMonitoringStation = await LicenceMonitoringStationHelper.add({
+  //       createdAt: new Date('2025-05-20'),
+  //       createdBy: userId,
+  //       licenceId: licence.id,
+  //       licenceVersionPurposeConditionId: licenceVersionPurposeCondition.id,
+  //       monitoringStationId: monitoringStation.id,
+  //       restrictionType: 'reduce'
+  //     })
+  //   })
+
+  //   it('returns the matching data', async () => {
+  //     const result = await FetchLicenceMonitoringStationsService.go(licence.id, monitoringStation.id)
+
+  //     expect(result.monitoringStationLicenceTags).to.equal({
+  //       id: monitoringStation.id,
+  //       label: 'The Monitoring Station',
+  //       riverName: 'The River',
+  //       licenceMonitoringStations: [
+  //         {
+  //           id: licenceMonitoringStation.id,
+  //           createdAt: new Date('2025-05-20'),
+  //           licenceId: licence.id,
+  //           restrictionType: 'reduce',
+  //           thresholdUnit: 'm3/s',
+  //           thresholdValue: 100,
+  //           licence: { licenceRef: licence.licenceRef },
+  //           licenceVersionPurposeCondition: {
+  //             externalId: licenceVersionPurposeCondition.externalId,
+  //             notes: 'This is the effect of restriction',
+  //             licenceVersionPurpose: {
+  //               id: licenceVersionPurposeId,
+  //               licenceVersion: { status: 'current' }
+  //             },
+  //             licenceVersionPurposeConditionType: {
+  //               displayTitle: 'Flow cessation condition'
+  //             }
+  //           },
+  //           user: { username: 'station-monitor@wrls.gov.uk' }
+  //         }
+  //       ]
+  //     })
+  //   })
+  // })
 })
-
-async function _createAlerts(licenceId) {
-  // Won't get picked up as not the latest
-  await NotificationHelper.add({
-    createdAt: new Date('2024-08-13'),
-    messageRef: 'water_abstraction_alert_stop_warning',
-    messageType: 'letter',
-    personalisation: {
-      alertType: 'reduce',
-      address_line_1: 'Big Farm One',
-      licenceId,
-      sending_alert_type: 'warning'
-    },
-    status: 'sent'
-  })
-
-  // Will get picked up as it is the latest
-  await NotificationHelper.add({
-    createdAt: new Date('2025-04-17'),
-    messageRef: 'water_abstraction_alert_stop',
-    messageType: 'letter',
-    personalisation: {
-      alertType: 'stop',
-      address_line_1: 'Big Farm Two',
-      licenceId,
-      sending_alert_type: 'stop'
-    },
-    status: 'sent'
-  })
-
-  // Won't get picked up as the status is draft
-  await NotificationHelper.add({
-    createdAt: new Date('2025-04-16'),
-    messageRef: 'water_abstraction_alert_stop',
-    messageType: 'letter',
-    personalisation: {
-      alertType: 'stop',
-      address_line_1: 'Big Farm Three',
-      licenceId,
-      sending_alert_type: 'stop'
-    },
-    status: 'draft'
-  })
-}
