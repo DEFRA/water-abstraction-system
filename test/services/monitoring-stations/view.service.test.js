9a54c47bdb1df9639ffc95105fbd97d55973996f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-10 14:45:29 +0100

    Update View service to match changes

--- test/services/monitoring-stations/view.service.test.js
+++ test/services/monitoring-stations/view.service.test.js
@@ -9,21 +9,18 @@ const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Things we need to stub
-const FeatureFlagsConfig = require('../../../config/feature-flags.config.js')
-const FetchMonitoringStationService = require('../../../app/services/monitoring-stations/fetch-monitoring-station.service.js')
+const FetchMonitoringStationDetailsService = require('../../../app/services/monitoring-stations/fetch-monitoring-station-details.service.js')
 
 // Thing under test
 const ViewService = require('../../../app/services/monitoring-stations/view.service.js')
 
 describe('Monitoring Stations - View service', () => {
   let auth
+  let licenceMonitoringStations
   let monitoringStation
   let yarStub
 
   beforeEach(() => {
-    Sinon.stub(FeatureFlagsConfig, 'enableLicenceMonitoringStationsSetup').value(true)
-    Sinon.stub(FeatureFlagsConfig, 'enableLicenceMonitoringStationsView').value(true)
-
     auth = auth = {
       credentials: {
         scope: ['billing', 'hof_notifications', 'manage_gauging_station_licence_links']
@@ -31,39 +28,38 @@ describe('Monitoring Stations - View service', () => {
     }
 
     monitoringStation = {
-      id: 'f122d4bb-42bd-4af9-a081-1656f5a30b63',
       catchmentName: null,
       gridReference: 'TL2664640047',
+      id: 'f122d4bb-42bd-4af9-a081-1656f5a30b63',
       label: 'BUSY POINT',
       riverName: null,
       stationReference: null,
-      wiskiId: null,
-      licenceMonitoringStations: [
-        {
-          id: '3ee344db-784c-4d21-8d53-e50833f7e848',
-          abstractionPeriodEndDay: '31',
-          abstractionPeriodEndMonth: '08',
-          abstractionPeriodStartDay: '01',
-          abstractionPeriodStartMonth: '04',
-          licence: {
-            id: '3cd1481c-e96a-45fc-8f2b-1849564b95a5',
-            licenceRef: 'AT/TEST'
-          },
-          licenceId: '3cd1481c-e96a-45fc-8f2b-1849564b95a5',
-          licenceVersionPurposeCondition: null,
-          measureType: 'flow',
-          restrictionType: 'reduce',
-          status: 'resume',
-          statusUpdatedAt: null,
-          thresholdUnit: 'm3/s',
-          thresholdValue: 100
-        }
-      ]
+      wiskiId: null
     }
+
+    licenceMonitoringStations = [
+      {
+        abstractionPeriodEndDay: '31',
+        abstractionPeriodEndMonth: '08',
+        abstractionPeriodStartDay: '01',
+        abstractionPeriodStartMonth: '04',
+        id: '3ee344db-784c-4d21-8d53-e50833f7e848',
+        measureType: 'flow',
+        restrictionType: 'reduce',
+        thresholdUnit: 'm3/s',
+        thresholdValue: 100,
+        latestNotification: null,
+        licence: {
+          id: '3cd1481c-e96a-45fc-8f2b-1849564b95a5',
+          licenceRef: 'AT/TEST'
+        },
+        licenceVersionPurposeCondition: null
+      }
+    ]
+
     yarStub = { flash: Sinon.stub().returns(['Tag removed for 99/999/9999']) }
 
-    Sinon.stub(FeatureFlagsConfig, 'enableMonitoringStationsAlertNotifications').value(true)
-    Sinon.stub(FetchMonitoringStationService, 'go').resolves(monitoringStation)
+    Sinon.stub(FetchMonitoringStationDetailsService, 'go').resolves({ licenceMonitoringStations, monitoringStation })
   })
 
   afterEach(() => {
@@ -76,17 +72,17 @@ describe('Monitoring Stations - View service', () => {
 
       expect(result).to.equal({
         activeNavBar: 'search',
-        catchmentName: null,
-        enableLicenceMonitoringStationsSetup: true,
-        gridReference: 'TL2664640047',
-        links: {
-          createAlert: `/system/notices/setup/alerts?monitoringStationId=${monitoringStation.id}`
-        },
-        monitoringStationId: 'f122d4bb-42bd-4af9-a081-1656f5a30b63',
         notification: 'Tag removed for 99/999/9999',
+        backLink: { href: '/licences', text: 'Go back to search' },
+        buttons: {
+          createAlert: {
+            href: '/system/notices/setup/alerts?monitoringStationId=f122d4bb-42bd-4af9-a081-1656f5a30b63'
+          },
+          tagLicence: { value: 'f122d4bb-42bd-4af9-a081-1656f5a30b63' }
+        },
+        gridReference: 'TL2664640047',
         pageTitle: 'BUSY POINT',
-        permissionToManageLinks: true,
-        permissionToSendAlerts: true,
+        pageTitleCaption: null,
         restrictionHeading: 'Flow restriction type and threshold',
         restrictions: [
           {
@@ -95,8 +91,8 @@ describe('Monitoring Stations - View service', () => {
               text: 'View'
             },
             abstractionPeriod: '1 April to 31 August',
-            alert: null,
-            alertDate: null,
+            alert: '',
+            alertDate: '',
             licenceId: '3cd1481c-e96a-45fc-8f2b-1849564b95a5',
             licenceRef: 'AT/TEST',
             restriction: 'Reduce',
@@ -104,24 +100,8 @@ describe('Monitoring Stations - View service', () => {
             threshold: '100m3/s'
           }
         ],
-        showRemoveTagButton: false,
         stationReference: '',
-        tableCaption: 'Licences linked to this monitoring station',
         wiskiId: ''
-      })
-    })
-  })
-
-  describe('when the "enableMonitoringStationsAlertNotifications" is false', () => {
-    beforeEach(() => {
-      Sinon.stub(FeatureFlagsConfig, 'enableMonitoringStationsAlertNotifications').value(false)
-    })
-
-    it('returns the link for the legacy water abstraction alert page', async () => {
-      const result = await ViewService.go(auth, monitoringStation.id, yarStub)
-
-      expect(result.links).to.equal({
-        createAlert: `/monitoring-stations/${monitoringStation.id}/send-alert/alert-type`
       })
     })
   })
