2e9630e3855c2282336a8809032fbe4a42034fca
Jason Claxton <jason.claxton@defra.gov.uk>
2025-09-12 16:53:26 +0100

    Create ReSP request

---
+++ test/requests/resp.request.test.js
@@ -0,0 +1,113 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, before, beforeEach, after, afterEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Things we need to stub
+const BaseRequest = require('../../app/requests/base.request.js')
+const respConfig = require('../../config/resp.config.js')
+const serverConfig = require('../../config/server.config.js')
+
+// Thing under test
+const RespRequest = require('../../app/requests/resp.request.js')
+
+describe('ReSP API Request', () => {
+  const testRoute = 'TEST_ROUTE'
+
+  before(() => {
+    // RespRequest makes use of the getRespToken() server method, which we therefore need to stub
+    // Note that we only need to do this once as it is unaffected by the Sinon.restore() in our afterEach()
+    global.HapiServerMethods = {
+      getRespToken: Sinon.stub().resolves({
+        accessToken: 'ACCESS_TOKEN',
+        expiresIn: 3600
+      })
+    }
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  after(() => {
+    // Tidy up our global server methods stub once done
+    delete global.HapiServerMethods
+  })
+
+  describe('#get', () => {
+    describe('when the request succeeds', () => {
+      beforeEach(() => {
+        Sinon.stub(BaseRequest, 'get').resolves({
+          succeeded: true,
+          response: {
+            statusCode: 200,
+            body: { testObject: { test: 'yes' } }
+          }
+        })
+      })
+
+      it('calls the ReSP API with the required options', async () => {
+        await RespRequest.get(testRoute)
+
+        const requestArgs = BaseRequest.get.firstCall.args
+
+        expect(requestArgs[0]).to.endWith('TEST_ROUTE')
+        expect(requestArgs[1].headers).to.include({ authorization: 'Bearer ACCESS_TOKEN' })
+      })
+
+      it('returns a "true" success status', async () => {
+        const result = await RespRequest.get(testRoute)
+
+        expect(result.succeeded).to.be.true()
+      })
+
+      it('returns the response body as an object', async () => {
+        const result = await RespRequest.get(testRoute)
+
+        expect(result.response.body.testObject.test).to.equal('yes')
+      })
+
+      it('returns the status code', async () => {
+        const result = await RespRequest.get(testRoute)
+
+        expect(result.response.statusCode).to.equal(200)
+      })
+    })
+
+    describe('when the request fails', () => {
+      beforeEach(() => {
+        Sinon.stub(BaseRequest, 'get').resolves({
+          succeeded: false,
+          response: {
+            statusCode: 404,
+            statusMessage: 'Not Found',
+            body: { statusCode: 404, error: 'Not Found', message: 'Not Found' }
+          }
+        })
+      })
+
+      it('returns a "false" success status', async () => {
+        const result = await RespRequest.get(testRoute)
+
+        expect(result.succeeded).to.be.false()
+      })
+
+      it('returns the error response', async () => {
+        const result = await RespRequest.get(testRoute)
+
+        expect(result.response.body.message).to.equal('Not Found')
+      })
+
+      it('returns the status code', async () => {
+        const result = await RespRequest.get(testRoute)
+
+        expect(result.response.statusCode).to.equal(404)
+      })
+    })
+  })
+})
