37bbd0c9e9e54b14b3ec99b928132b5d31ae60e7
Jason Claxton <jason.claxton@defra.gov.uk>
2025-09-15 11:24:30 +0100

    Correct API path

--- app/requests/resp/view-health.request.js
+++ app/requests/resp/view-health.request.js
@@ -13,7 +13,7 @@ const RespRequest = require('../resp.request.js')
  * @returns {Promise<object>} The result of the request; whether it succeeded and the response or error returned
  */
 async function send() {
-  const path = 'monitoringFrequency'
+  const path = 'rsp/v1/monitoringFrequency'
 
   return RespRequest.get(path)
 }
