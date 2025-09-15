3da17f69de36a518a6697ccf0187bd69523999a8
Jason Claxton <jason.claxton@defra.gov.uk>
2025-09-15 10:54:58 +0100

    Create `view-health` request

---
+++ app/requests/resp/view-health.request.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * View the health of the ReSP API service
+ * @module ViewHealthRequest
+ */
+
+const RespRequest = require('../resp.request.js')
+
+/**
+ * View the health of the ReSP API service
+ *
+ * @returns {Promise<object>} The result of the request; whether it succeeded and the response or error returned
+ */
+async function send() {
+  const path = 'monitoringFrequency'
+
+  return RespRequest.get(path)
+}
+
+module.exports = {
+  send
+}
