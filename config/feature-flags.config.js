384226ed693f5263578a47adccecd96f8989a5e0
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-23 23:28:50 +0100

    Housekeeping - Order feature flags alphabetically

--- config/feature-flags.config.js
+++ config/feature-flags.config.js
@@ -12,22 +12,22 @@ require('dotenv').config()
 const config = {
   // Credit to https://stackoverflow.com/a/323546/6117745 for how to handle
   // converting the env var to a boolean
+  enableAdHocNotifications: String(process.env.ENABLE_AD_HOC_NOTIFICATIONS) === 'true' || false,
+  enableBillingAccountView: String(process.env.ENABLE_BILLING_ACCOUNT_VIEW) === 'true' || false,
   enableLicenceConditionsView: String(process.env.ENABLE_LICENCE_CONDITIONS_VIEW) === 'true' || false,
   enableLicencePointsView: String(process.env.ENABLE_LICENCE_POINTS_VIEW) === 'true' || false,
   enableLicencePurposesView: String(process.env.ENABLE_LICENCE_PURPOSES_VIEW) === 'true' || false,
   enableMonitoringStationsView: String(process.env.ENABLE_MONITORING_STATIONS_VIEW) === 'true' || false,
-  enableNullDueDate: String(process.env.ENABLE_NULL_DUE_DATE) === 'true' || false,
   enableNotificationsView: String(process.env.ENABLE_NOTIFICATIONS_VIEW) === 'true' || false,
+  enableNullDueDate: String(process.env.ENABLE_NULL_DUE_DATE) === 'true' || false,
   enableReissuingBillingBatches: String(process.env.ENABLE_REISSUING_BILLING_BATCHES) === 'true' || false,
   enableRequirementsForReturns: String(process.env.ENABLE_REQUIREMENTS_FOR_RETURNS) === 'true' || false,
   enableSystemLicenceView: String(process.env.ENABLE_SYSTEM_LICENCE_VIEW) === 'true' || false,
+  enableSystemManageView: String(process.env.ENABLE_SYSTEM_MANAGE_VIEW) === 'true' || false,
   enableSystemReturnsSubmit: String(process.env.ENABLE_SYSTEM_RETURNS_SUBMIT) === 'true' || false,
   enableSystemReturnsView: String(process.env.ENABLE_SYSTEM_RETURNS_VIEW) === 'true' || false,
-  enableTwoPartTariffSupplementary: String(process.env.ENABLE_TWO_PART_TARIFF_SUPPLEMENTARY) === 'true' || false,
-  enableBillingAccountView: String(process.env.ENABLE_BILLING_ACCOUNT_VIEW) === 'true' || false,
-  enableSystemManageView: String(process.env.ENABLE_SYSTEM_MANAGE_VIEW) === 'true' || false,
   enableSystemProfiles: String(process.env.ENABLE_SYSTEM_PROFILES) === 'true' || false,
-  enableAdHocNotifications: String(process.env.ENABLE_AD_HOC_NOTIFICATIONS) === 'true' || false
+  enableTwoPartTariffSupplementary: String(process.env.ENABLE_TWO_PART_TARIFF_SUPPLEMENTARY) === 'true' || false
 }
 
 module.exports = config
