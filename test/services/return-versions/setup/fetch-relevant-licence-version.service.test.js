ed7d681ebb71a66c99c972bdbe923d29893af280
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-09-26 00:21:28 +0100

    Implement change
    Includes extra unit tests to cover both our example scenarios.

--- test/services/return-versions/setup/fetch-relevant-licence-version.service.test.js
+++ test/services/return-versions/setup/fetch-relevant-licence-version.service.test.js
@@ -29,16 +29,32 @@ describe('Return Versions - Setup - Fetch Relevant Licence Version service', () 
       status: 'superseded'
     })
 
-    licenceVersions.secondLicenceVersion = await LicenceVersionHelper.add({
+    licenceVersions.oopsSecondLicenceVersion = await LicenceVersionHelper.add({
       endDate: new Date('2019-05-12'),
       issue: 101,
+      licenceId,
+      startDate: new Date('2019-05-12'),
+      status: 'superseded'
+    })
+
+    licenceVersions.secondLicenceVersion = await LicenceVersionHelper.add({
+      endDate: new Date('2019-05-12'),
+      issue: 102,
       licenceId,
       startDate: new Date('2012-08-13'),
       status: 'superseded'
     })
 
+    licenceVersions.oopsCurrentLicenceVersion = await LicenceVersionHelper.add({
+      endDate: new Date('2019-05-13'),
+      issue: 103,
+      licenceId,
+      startDate: new Date('2019-05-13'),
+      status: 'superseded'
+    })
+
     licenceVersions.currentLicence = await LicenceVersionHelper.add({
-      issue: 102,
+      issue: 104,
       licenceId,
       startDate: new Date('2019-05-13'),
       status: 'current'
@@ -93,15 +109,54 @@ describe('Return Versions - Setup - Fetch Relevant Licence Version service', () 
     })
   })
 
+  // NOTE: WATER-5251 was an issue with a licence that had two licence versions. The first covered a single day: 1 April
+  // 2025 and originally had the wrong purposes linked to it. Rather than delete it, the users created a second licence
+  // version that also started on 1 April 2025 (hence the previous only covering a single day). The issue exposed that
+  // when this is the case, we select the earlier (single day) rather than later licence version. We fixed it, but we
+  // these tests to ensure we handle these exceptions.
+  describe('when the selected start date is the same as the start date of multiple licence versions', () => {
+    describe('and both have end dates (they are both superseded)', () => {
+      before(() => {
+        startDate = new Date('2019-05-12')
+      })
+
+      it.only('returns the licence version with the latest end date', async () => {
+        const result = await FetchRelevantLicenceVersionService.go(licenceId, startDate)
+
+        expect(result).to.equal({
+          endDate: licenceVersions.secondLicenceVersion.endDate,
+          id: licenceVersions.secondLicenceVersion.id,
+          startDate: licenceVersions.secondLicenceVersion.startDate
+        })
+      })
+    })
+
+    describe('and one has an end date and the other is the "current" licence version', () => {
+      before(() => {
+        startDate = new Date('2019-05-13')
+      })
+
+      it('returns the "current" licence version', async () => {
+        const result = await FetchRelevantLicenceVersionService.go(licenceId, startDate)
+
+        expect(result).to.equal({
+          endDate: licenceVersions.currentLicence.endDate,
+          id: licenceVersions.currentLicence.id,
+          startDate: licenceVersions.currentLicence.startDate
+        })
+      })
+    })
+  })
+
   describe('when the licence ID is unknown', () => {
     before(() => {
       startDate = new Date('2024-04-01')
     })
 
-    it('returns "undefined"', async () => {
+    it('returns "null"', async () => {
       const result = await FetchRelevantLicenceVersionService.go('1d7f6806-43b1-4cce-9ab1-adb28448aef2', startDate)
 
-      expect(result).to.be.undefined()
+      expect(result).to.be.null()
     })
   })
 })
