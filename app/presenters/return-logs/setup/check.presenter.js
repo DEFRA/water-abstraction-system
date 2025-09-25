d01c4b5a6284506432a32355a6f988b58431e042
Jason Claxton <jason.claxton@defra.gov.uk>
2025-09-25 15:56:22 +0100

    Fix volume being null when meter reading is 0

--- app/presenters/return-logs/setup/check.presenter.js
+++ app/presenters/return-logs/setup/check.presenter.js
@@ -138,7 +138,7 @@ function _formatLines(lines, meter10TimesDisplay, reported, startReading, unitNa
     } else {
       formattedLine.reading = line.reading ?? null
 
-      if (line.reading) {
+      if (typeof line.reading === 'number') {
         const multiplier = meter10TimesDisplay === 'yes' ? 10 : 1
 
         formattedLine.quantity = (line.reading - previousReading) * multiplier
