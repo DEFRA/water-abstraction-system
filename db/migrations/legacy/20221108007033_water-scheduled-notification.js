999911ca4a1899b3669f8d0bd3e4bd750d4b957f
jonathangoulding <goulding.jon@icloud.com>
2025-09-25 15:52:17 +0100

    Add migration to add a PDF column to notifications
    https://eaflood.atlassian.net/browse/WATER-5280
    
    The Return forms where not previously stored.
    
    This migration adds the column to record PDFs sent via Notify.
    
    Notify states a 'precompiled letter must be a PDF file'
    
    https://docs.notifications.service.gov.uk/node.html#send-a-precompiled-letter

--- db/migrations/legacy/20221108007033_water-scheduled-notification.js
+++ db/migrations/legacy/20221108007033_water-scheduled-notification.js
@@ -31,6 +31,7 @@ exports.up = function (knex) {
     table.uuid('licence_gauging_station_id')
     table.jsonb('return_log_ids')
     table.uuid('template_id')
+    table.binary('pdf')
 
     // Legacy timestamps
     table.timestamp('date_created', { useTz: false }).defaultTo(knex.fn.now())
