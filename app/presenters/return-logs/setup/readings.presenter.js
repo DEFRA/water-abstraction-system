f9cd713dfd2728eb08652e36ee654ce696515987
Jason Claxton <jason.claxton@defra.gov.uk>
2025-09-26 14:55:20 +0100

    Update readings/volumes presenters to return the reading/quantity instead of an empty string fi the value isn't a number. This change ensures that if a validation error occurs on the page, what the user actually entered is displayed on the line.

--- app/presenters/return-logs/setup/readings.presenter.js
+++ app/presenters/return-logs/setup/readings.presenter.js
@@ -47,7 +47,7 @@ function _inputLines(lines, returnsFrequency) {
     const lineData = {
       endDate,
       label: _lineLabel(endDate, returnsFrequency),
-      reading: typeof line.reading === 'number' ? reading.toString() : ''
+      reading: typeof line.reading === 'number' ? reading.toString() : line.reading
     }
 
     if (line.error) {
