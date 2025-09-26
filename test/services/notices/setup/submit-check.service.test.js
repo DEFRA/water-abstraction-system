0c773a5ebe7688862b524e3e1c66dd691a82ff93
jonathangoulding <goulding.jon@icloud.com>
2025-09-26 15:24:35 +0100

    Updated sending notification to save / fetch and update
    https://eaflood.atlassian.net/browse/WATER-5280
    
    Currently, when a user clicks Confirm on the check-recipients page, as part of sending a notice, we follow this process.
    
    - Convert the notice setup and recipient data into package to be sent to Notify
    - We send the package to Notify
    - We insert the result and the data into the DB as a ‘notification’.
    
    Because there may be many notifications to send, or in the case of return forms, each can take some time to process, we do this behind the scenes to avoid users seeing any spinning wheels.
    
    So, they will be immediately taken to the ‘Confirmation’ page, which includes a link to view the notice.
    
    If the user is keen, and the process is going to take a moment to complete, when they view the notice and its notifications, the page will be empty. If they were to wait a moment and refresh, then the records would appear. However, we do not consider this a reasonable workaround.
    
    This change updates the notification flow to save a notification, then fetch the notifications and then update the notifications.
    
    We create and fetch the notifications before batching and pass the notifications into the batch process (they will now have a notification id).
    
    The batch process iterates over the notifications and updates (upserts) the saved notification record with Notifies responses.

--- test/services/notices/setup/submit-check.service.test.js
+++ test/services/notices/setup/submit-check.service.test.js
@@ -10,22 +10,21 @@ const { expect } = Code
 
 // Test helpers
 const EventModel = require('../../../../app/models/event.model.js')
+const NotificationModel = require('../../../../app/models/notification.model.js')
 const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
 const SessionHelper = require('../../../support/helpers/session.helper.js')
+const { generateReferenceCode } = require('../../../support/helpers/notification.helper.js')
 
 // Things we need to stub
 const BatchNotificationsService = require('../../../../app/services/notices/setup/batch-notifications.service.js')
-const DetermineNotificationsService = require('../../../../app/services/notices/setup/determine-notifications.service.js')
 const DetermineRecipientsService = require('../../../../app/services/notices/setup/determine-recipients.service.js')
 const FetchReturnsRecipientsService = require('../../../../app/services/notices/setup/fetch-returns-recipients.service.js')
-const { generateReferenceCode } = require('../../../support/helpers/notification.helper.js')
 
 // Thing under test
 const SubmitCheckService = require('../../../../app/services/notices/setup/submit-check.service.js')
 
 describe('Notices - Setup - Submit Check service', () => {
   let auth
-  let mockNotifications
   let notifierStub
   let referenceCode
   let session
@@ -40,8 +39,6 @@ describe('Notices - Setup - Submit Check service', () => {
       }
     }
 
-    mockNotifications = [{ text: 'mock notification' }]
-
     const recipients = RecipientsFixture.recipients()
 
     testRecipients = [recipients.primaryUser]
@@ -50,7 +47,7 @@ describe('Notices - Setup - Submit Check service', () => {
 
     session = await SessionHelper.add({
       data: {
-        journey: 'invitations',
+        journey: 'standard',
         referenceCode,
         returnsPeriod: 'quarterFour',
         determinedReturnsPeriod: {
@@ -59,11 +56,11 @@ describe('Notices - Setup - Submit Check service', () => {
           endDate: '2023-03-31',
           summer: 'false',
           startDate: '2022-04-01'
-        }
+        },
+        noticeType: 'invitations'
       }
     })
 
-    Sinon.stub(DetermineNotificationsService, 'go').resolves(mockNotifications)
     Sinon.stub(DetermineRecipientsService, 'go').returns(testRecipients)
     Sinon.stub(FetchReturnsRecipientsService, 'go').resolves(testRecipients)
 
@@ -81,29 +78,40 @@ describe('Notices - Setup - Submit Check service', () => {
       Sinon.stub(BatchNotificationsService, 'go').resolves({ sent: 1, error: 0 })
     })
 
-    it('should determine notifications', async () => {
-      const result = await SubmitCheckService.go(session.id, auth)
-
-      const args = DetermineNotificationsService.go.firstCall.args
-
-      expect(args[0]).to.equal(session)
-      expect(args[1]).to.equal(testRecipients)
-      expect(args[2]).to.equal(result)
-    })
-
     it('should batch notifications', async () => {
       const result = await SubmitCheckService.go(session.id, auth)
 
+      const notifications = await NotificationModel.query().where({
+        eventId: result
+      })
+
       const args = BatchNotificationsService.go.firstCall.args
 
-      expect(args[0]).to.equal(mockNotifications)
+      expect(args[0]).to.equal([
+        {
+          id: notifications[0].id,
+          messageRef: 'returns_invitation_primary_user_email',
+          messageType: 'email',
+          pdf: null,
+          personalisation: {
+            periodEndDate: '31 March 2023',
+            returnDueDate: '28 April 2025',
+            periodStartDate: '1 April 2022'
+          },
+          recipient: 'primary.user@important.com',
+          templateId: '2fa7fc83-4df1-4f52-bccf-ff0faeb12b6f'
+        }
+      ])
+
       expect(args[1]).to.equal(result)
+
+      expect(args[2]).to.equal(referenceCode)
     })
 
     it('correctly returns the event id', async () => {
       const result = await SubmitCheckService.go(session.id, auth)
 
-      const event = await EventModel.query().where('reference_code', referenceCode).first()
+      const event = await EventModel.query().findById(result)
 
       expect(result).to.equal(event.id)
     })
