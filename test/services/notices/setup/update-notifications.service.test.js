fd6086b36c325775d16b6e82f6535486cba752e2
jonathangoulding <goulding.jon@icloud.com>
2025-09-26 09:18:21 +0100

    test: add test to ensure upsert works

--- test/services/notices/setup/update-notifications.service.test.js
+++ test/services/notices/setup/update-notifications.service.test.js
@@ -18,8 +18,12 @@ const UpdateNotificationsService = require('../../../../app/services/notices/set
 
 describe('Notices - Setup - Update notification service', () => {
   let eventId
+  let notUpdatedNotification
   let notification
+  let notificationTwo
   let notifications
+  let notifyId
+  let notifyIdTwo
 
   beforeEach(async () => {
     const event = await EventHelper.add({
@@ -28,16 +32,19 @@ describe('Notices - Setup - Update notification service', () => {
     })
 
     eventId = event.id
+
+    notification = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
+    notificationTwo = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
+
+    notifyId = generateUUID()
+    notifyIdTwo = generateUUID()
+
+    // This is used to test the upsert is not affecting other notifications related to the same event
+    notUpdatedNotification = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
   })
 
   describe('when a notification to Notify is successful', () => {
-    let notifyId
-
-    beforeEach(async () => {
-      notification = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
-
-      notifyId = generateUUID()
-
+    beforeEach(() => {
       notifications = [
         {
           id: notification.id,
@@ -45,225 +52,128 @@ describe('Notices - Setup - Update notification service', () => {
           notifyStatus: 'sent',
           plaintext: 'some text',
           status: 'pending'
+        },
+        {
+          id: notificationTwo.id,
+          notifyId: notifyIdTwo,
+          notifyStatus: 'sent',
+          plaintext: 'some text',
+          status: 'pending'
         }
       ]
     })
 
-    describe('and it is for a single notification', () => {
-      it('should update a single notification (and only update the new values)', async () => {
-        await UpdateNotificationsService.go(notifications)
+    it('should update notifications (and only update the new values)', async () => {
+      await UpdateNotificationsService.go(notifications)
 
-        const updatedResult = await NotificationModel.query().where('event_id', eventId)
+      const updatedResult = await NotificationModel.query().where('event_id', eventId)
 
-        expect(updatedResult).equal([
-          {
-            createdAt: updatedResult[0].createdAt,
-            eventId,
-            id: notification.id,
-            licenceMonitoringStationId: null,
-            licences: null,
-            notifyError: null,
-            messageRef: null,
-            messageType: null,
-            notifyId,
-            notifyStatus: 'sent',
-            pdf: null,
-            personalisation: null,
-            plaintext: 'some text',
-            recipient: null,
-            returnLogIds: null,
-            status: 'pending',
-            templateId: null
-          }
-        ])
-      })
-    })
-
-    describe('and it is for multiple notifications', () => {
-      let notificationTwo
-      let notifyIdTwo
-
-      beforeEach(async () => {
-        notificationTwo = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
-
-        notifyIdTwo = generateUUID()
-
-        notifications = [
-          {
-            id: notification.id,
-            notifyId,
-            notifyStatus: 'sent',
-            plaintext: 'some text',
-            status: 'pending'
-          },
-          {
-            id: notificationTwo.id,
-            notifyId: notifyIdTwo,
-            notifyStatus: 'sent',
-            plaintext: 'some text',
-            status: 'pending'
-          }
-        ]
-      })
-
-      it('should update notifications (and only update the new values)', async () => {
-        await UpdateNotificationsService.go(notifications)
-
-        const updatedResult = await NotificationModel.query().where('event_id', eventId)
-
-        expect(updatedResult).equal([
-          {
-            createdAt: updatedResult[0].createdAt,
-            eventId,
-            id: notification.id,
-            licenceMonitoringStationId: null,
-            licences: null,
-            notifyError: null,
-            messageRef: null,
-            messageType: null,
-            notifyId,
-            notifyStatus: 'sent',
-            pdf: null,
-            personalisation: null,
-            plaintext: 'some text',
-            recipient: null,
-            returnLogIds: null,
-            status: 'pending',
-            templateId: null
-          },
-          {
-            createdAt: updatedResult[1].createdAt,
-            eventId,
-            id: notificationTwo.id,
-            licenceMonitoringStationId: null,
-            licences: null,
-            notifyError: null,
-            messageRef: null,
-            messageType: null,
-            notifyId: notifyIdTwo,
-            notifyStatus: 'sent',
-            pdf: null,
-            personalisation: null,
-            plaintext: 'some text',
-            recipient: null,
-            returnLogIds: null,
-            status: 'pending',
-            templateId: null
-          }
-        ])
-      })
+      expect(updatedResult).equal([
+        notUpdatedNotification,
+        {
+          createdAt: notification.createdAt,
+          eventId,
+          id: notification.id,
+          licenceMonitoringStationId: null,
+          licences: null,
+          notifyError: null,
+          messageRef: null,
+          messageType: null,
+          notifyId,
+          notifyStatus: 'sent',
+          pdf: null,
+          personalisation: null,
+          plaintext: 'some text',
+          recipient: null,
+          returnLogIds: null,
+          status: 'pending',
+          templateId: null
+        },
+        {
+          createdAt: notificationTwo.createdAt,
+          eventId,
+          id: notificationTwo.id,
+          licenceMonitoringStationId: null,
+          licences: null,
+          notifyError: null,
+          messageRef: null,
+          messageType: null,
+          notifyId: notifyIdTwo,
+          notifyStatus: 'sent',
+          pdf: null,
+          personalisation: null,
+          plaintext: 'some text',
+          recipient: null,
+          returnLogIds: null,
+          status: 'pending',
+          templateId: null
+        }
+      ])
     })
   })
 
   describe('when a notification to Notify is unsuccessful', () => {
     beforeEach(async () => {
-      notification = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
-
       notifications = [
         {
           id: notification.id,
+          notifyError: JSON.stringify({ message: 'some error' }),
+          status: 'error'
+        },
+        {
+          id: notificationTwo.id,
           notifyError: JSON.stringify({ message: 'some error' }),
           status: 'error'
         }
       ]
     })
 
-    describe('and it is for a single notification', () => {
-      it('should update a single notification (and only update the new values)', async () => {
-        await UpdateNotificationsService.go(notifications)
+    it('should update notifications (and only update the new values)', async () => {
+      await UpdateNotificationsService.go(notifications)
 
-        const updatedResult = await NotificationModel.query().where('event_id', eventId)
+      const updatedResult = await NotificationModel.query().where('event_id', eventId)
 
-        expect(updatedResult).equal([
-          {
-            createdAt: updatedResult[0].createdAt,
-            eventId,
-            id: notification.id,
-            licenceMonitoringStationId: null,
-            licences: null,
-            notifyError: '{"message":"some error"}',
-            messageRef: null,
-            messageType: null,
-            notifyId: null,
-            notifyStatus: null,
-            pdf: null,
-            personalisation: null,
-            plaintext: null,
-            recipient: null,
-            returnLogIds: null,
-            status: 'error',
-            templateId: null
-          }
-        ])
-      })
-    })
-
-    describe('and it is for multiple notifications', () => {
-      let notificationTwo
-
-      beforeEach(async () => {
-        notificationTwo = await NotificationHelper.add({ eventId, createdAt: timestampForPostgres() })
-
-        notifications = [
-          {
-            id: notification.id,
-            notifyError: JSON.stringify({ message: 'some error' }),
-            status: 'error'
-          },
-          {
-            id: notificationTwo.id,
-            notifyError: JSON.stringify({ message: 'some error' }),
-            status: 'error'
-          }
-        ]
-      })
-
-      it('should update notifications (and only update the new values)', async () => {
-        await UpdateNotificationsService.go(notifications)
-
-        const updatedResult = await NotificationModel.query().where('event_id', eventId)
-
-        expect(updatedResult).equal([
-          {
-            createdAt: updatedResult[0].createdAt,
-            eventId,
-            id: notification.id,
-            licenceMonitoringStationId: null,
-            licences: null,
-            notifyError: '{"message":"some error"}',
-            messageRef: null,
-            messageType: null,
-            notifyId: null,
-            notifyStatus: null,
-            pdf: null,
-            personalisation: null,
-            plaintext: null,
-            recipient: null,
-            returnLogIds: null,
-            status: 'error',
-            templateId: null
-          },
-          {
-            createdAt: updatedResult[1].createdAt,
-            eventId,
-            id: notificationTwo.id,
-            licenceMonitoringStationId: null,
-            licences: null,
-            notifyError: '{"message":"some error"}',
-            messageRef: null,
-            messageType: null,
-            notifyId: null,
-            notifyStatus: null,
-            pdf: null,
-            personalisation: null,
-            plaintext: null,
-            recipient: null,
-            returnLogIds: null,
-            status: 'error',
-            templateId: null
-          }
-        ])
-      })
+      expect(updatedResult).equal([
+        notUpdatedNotification,
+        {
+          createdAt: notification.createdAt,
+          eventId,
+          id: notification.id,
+          licenceMonitoringStationId: null,
+          licences: null,
+          notifyError: '{"message":"some error"}',
+          messageRef: null,
+          messageType: null,
+          notifyId: null,
+          notifyStatus: null,
+          pdf: null,
+          personalisation: null,
+          plaintext: null,
+          recipient: null,
+          returnLogIds: null,
+          status: 'error',
+          templateId: null
+        },
+        {
+          createdAt: notificationTwo.createdAt,
+          eventId,
+          id: notificationTwo.id,
+          licenceMonitoringStationId: null,
+          licences: null,
+          notifyError: '{"message":"some error"}',
+          messageRef: null,
+          messageType: null,
+          notifyId: null,
+          notifyStatus: null,
+          pdf: null,
+          personalisation: null,
+          plaintext: null,
+          recipient: null,
+          returnLogIds: null,
+          status: 'error',
+          templateId: null
+        }
+      ])
     })
   })
 })
