2aa5595191464c06fe5f9f76c9c63054b2528286
Robert Parkinson <robertparkinson@users.noreply.github.com>
2025-09-30 09:28:38 +0100

    Use new standard for abstraction periods (#2457)
    https://eaflood.atlassian.net/browse/WATER-5200
    
    This PR continues our tech debt of standardising how we use the layouts and views. This PR updates the abstraction period pages to use the new standard. This involves moving the back link into the presenter and updating the relevant tests.

--- test/presenters/return-versions/setup/abstraction-period.presenter.test.js
+++ test/presenters/return-versions/setup/abstraction-period.presenter.test.js
@@ -57,10 +57,10 @@ describe('Return Versions Setup - Abstraction Period presenter', () => {
     describe('when the user has previously submitted an abstraction period', () => {
       beforeEach(() => {
         session.requirements[0].abstractionPeriod = {
-          'abstraction-period-start-day': '07',
-          'abstraction-period-start-month': '12',
-          'abstraction-period-end-day': '22',
-          'abstraction-period-end-month': '07'
+          abstractionPeriodStartDay: '07',
+          abstractionPeriodStartMonth: '12',
+          abstractionPeriodEndDay: '22',
+          abstractionPeriodEndMonth: '07'
         }
       })
 
@@ -68,10 +68,10 @@ describe('Return Versions Setup - Abstraction Period presenter', () => {
         const result = AbstractionPeriodPresenter.go(session, requirementIndex)
 
         expect(result.abstractionPeriod).to.equal({
-          'abstraction-period-start-day': '07',
-          'abstraction-period-start-month': '12',
-          'abstraction-period-end-day': '22',
-          'abstraction-period-end-month': '07'
+          abstractionPeriodStartDay: '07',
+          abstractionPeriodStartMonth: '12',
+          abstractionPeriodEndDay: '22',
+          abstractionPeriodEndMonth: '07'
         })
       })
     })
