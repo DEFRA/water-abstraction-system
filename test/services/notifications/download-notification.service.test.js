06a57646c8aaf72d40310a3c3e25e0578c1b4f23
jonathangoulding <goulding.jon@icloud.com>
2025-10-01 14:01:20 +0100

    chore: pre pr checks

--- test/services/notifications/download-notification.service.test.js
+++ test/services/notifications/download-notification.service.test.js
@@ -3,31 +3,37 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
-const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, afterEach, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
-// Test helpers
-const NotificationHelper = require('../../support/helpers/notification.helper.js')
+// Things we need to stub
+const FetchDownloadNotificationService = require('../../../app/services/notifications/fetch-notification-download.service.js')
 
 // Thing under test
 const DownloadNotificationService = require('../../../app/services/notifications/download-notification.service.js')
+const { generateUUID } = require('../../../app/lib/general.lib.js')
 
 describe('Notifications - Download Notification service', () => {
-  let notification
+  let notificationId
   let pdf
 
   beforeEach(async () => {
+    notificationId = generateUUID()
+
     pdf = Buffer.from('mock file')
 
-    notification = await NotificationHelper.add({
-      pdf
-    })
+    Sinon.stub(FetchDownloadNotificationService, 'go').resolves({ pdf })
+  })
+
+  afterEach(() => {
+    Sinon.restore()
   })
 
   describe('when called', () => {
     it('returns pdf data', async () => {
-      const result = await DownloadNotificationService.go(notification.id)
+      const result = await DownloadNotificationService.go(notificationId)
 
       expect(result).to.equal(pdf)
     })
