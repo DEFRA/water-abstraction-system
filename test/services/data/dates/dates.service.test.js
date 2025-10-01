c2e17e707128b2db522ce27b44f11342a32f8d20
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-01 17:35:39 +0100

    Add a new data dates endpoint
    One of the known issues with the acceptance tests is that the return invite and reminder journeys rely on you having actual data in your environment.
    
    If you don't then they cannot find due return logs so error. We wanted to fix this as part of stabilising the tests, but to do so the acceptance tests will need to know what the current return periods are.
    
    That's a significant amount of code we don't want to just copy and paste into the acceptance-tests project.
    
    So, we've come up with a data endpoint that returns all the calculated dates the service has to come up with. We know about the current return periods, but there are also the billing periods and current financial year. Should we spot or add any more, we can add them to this endpoint and avoid having to duplicate the code in our test suite.

---
+++ test/services/data/dates/dates.service.test.js
@@ -0,0 +1,66 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Thing under test
+const DatesService = require('../../../../app/services/data/dates/dates.service.js')
+
+// NOTE: The service is calling a series of other services that actually determine the dates we return. So, we are not
+// interested in duplicating that testing here. Instead, we focus on confirming the structure is as we expect whilst
+// trying to avoid asserting the values (else it will just keep breaking!)
+describe('Data - Dates service', () => {
+  it('returns the current billing periods', () => {
+    const result = DatesService.go()
+
+    expect(result.billingPeriods).to.exist()
+
+    expect(result.billingPeriods.annual).to.exist()
+    expect(result.billingPeriods.annual).to.have.length(1)
+    expect(result.billingPeriods.annual[0].startDate).to.be.instanceOf(Date)
+    expect(result.billingPeriods.annual[0].endDate).to.be.instanceOf(Date)
+
+    expect(result.billingPeriods.supplementary).to.exist()
+    expect(result.billingPeriods.supplementary.length).to.be.between(1, 6)
+    expect(result.billingPeriods.supplementary[0].startDate).to.be.instanceOf(Date)
+    expect(result.billingPeriods.supplementary[0].endDate).to.be.instanceOf(Date)
+
+    expect(result.billingPeriods.twoPartTariff).to.exist()
+    expect(result.billingPeriods.twoPartTariff).to.have.length(1)
+    expect(result.billingPeriods.twoPartTariff[0].startDate).to.be.instanceOf(Date)
+    expect(result.billingPeriods.twoPartTariff[0].endDate).to.be.instanceOf(Date)
+
+    expect(result.billingPeriods.twoPartSupplementary).to.exist()
+    expect(result.billingPeriods.twoPartSupplementary).to.have.length(1)
+    expect(result.billingPeriods.twoPartSupplementary[0].startDate).to.be.instanceOf(Date)
+    expect(result.billingPeriods.twoPartSupplementary[0].endDate).to.be.instanceOf(Date)
+  })
+
+  it('returns the current financial year', () => {
+    const result = DatesService.go()
+
+    expect(result.currentFinancialYear).to.exist()
+    expect(result.currentFinancialYear.startDate).to.be.instanceOf(Date)
+    expect(result.currentFinancialYear.endDate).to.be.instanceOf(Date)
+  })
+
+  it('returns the current returns periods', () => {
+    const result = DatesService.go()
+
+    expect(result.firstReturnPeriod).to.exist()
+    expect(result.firstReturnPeriod.name).to.exist()
+    expect(result.firstReturnPeriod.startDate).to.be.instanceOf(Date)
+    expect(result.firstReturnPeriod.endDate).to.be.instanceOf(Date)
+    expect(result.firstReturnPeriod.dueDate).to.be.instanceOf(Date)
+
+    expect(result.secondReturnPeriod).to.exist()
+    expect(result.secondReturnPeriod.name).to.exist()
+    expect(result.secondReturnPeriod.startDate).to.be.instanceOf(Date)
+    expect(result.secondReturnPeriod.endDate).to.be.instanceOf(Date)
+    expect(result.secondReturnPeriod.dueDate).to.be.instanceOf(Date)
+  })
+})
