1da4f8660df9f595296dd8276852e539ddc2cba9
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-02 10:29:00 +0100

    Housekeeping - refactor the existing tests
    Ensure we clean up after ourselves, plus do things like create the return logs and their data in one.
    
    We want to add another return log to confirm the order is both by start date and return reference, so we needed to re-jig things ahead of this.

--- test/services/licences/fetch-licence-returns.service.test.js
+++ test/services/licences/fetch-licence-returns.service.test.js
@@ -4,81 +4,82 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, before, after } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
 const LicenceHelper = require('../../support/helpers/licence.helper.js')
 const ReturnLogHelper = require('../../support/helpers/return-log.helper.js')
-const { generateUUID } = require('../../../app/lib/general.lib.js')
 
 // Thing under test
 const FetchLicenceReturnsService = require('../../../app/services/licences/fetch-licence-returns.service.js')
 
-describe('Fetch licence returns service', () => {
-  let licenceId
+describe('Licences - Fetch licence returns service', () => {
+  let licence
+  let returnLogs
+
+  before(async () => {
+    returnLogs = []
+
+    licence = await LicenceHelper.add()
+
+    let returnLog = await ReturnLogHelper.add({
+      dueDate: new Date('2020-06-28'),
+      endDate: new Date('2020-06-01'),
+      licenceRef: licence.licenceRef,
+      metadata: { nald: { formatId: 32 } },
+      returnReference: '32',
+      startDate: new Date('2020-02-01'),
+      status: 'due'
+    })
+    returnLogs.push(returnLog)
+
+    returnLog = await ReturnLogHelper.add({
+      dueDate: null,
+      endDate: new Date('2020-06-01'),
+      licenceRef: licence.licenceRef,
+      metadata: { nald: { formatId: 123 } },
+      returnReference: '123',
+      startDate: new Date('2020-05-01'),
+      status: 'due'
+    })
+    returnLogs.push(returnLog)
+  })
+
+  after(async () => {
+    await licence.$query().delete()
+
+    for (const returnLog of returnLogs) {
+      returnLog.$query().delete()
+    }
+  })
 
   describe('when the licence has return logs', () => {
-    const dueDate = new Date('2020-04-01')
-    const endDate = new Date('2020-06-01')
-    const latestStartDate = new Date('2020-05-01')
-    const startDate = new Date('2020-02-01')
-
-    const firstReturn = {
-      id: generateUUID(),
-      dueDate,
-      endDate,
-      metadata: '323',
-      returnReference: '32',
-      startDate,
-      status: 'due'
-    }
-
-    const latestReturn = {
-      ...firstReturn,
-      id: generateUUID(),
-      returnReference: '123',
-      startDate: latestStartDate,
-      dueDate: null
-    }
-
-    beforeEach(async () => {
-      const license = await LicenceHelper.add()
-
-      licenceId = license.id
-
-      firstReturn.licenceRef = license.licenceRef
-      latestReturn.licenceRef = license.licenceRef
-
-      await ReturnLogHelper.add(firstReturn)
-      await ReturnLogHelper.add(latestReturn)
-    })
-
     it('returns results', async () => {
-      const result = await FetchLicenceReturnsService.go(licenceId, 1)
+      const result = await FetchLicenceReturnsService.go(licence.id, 1)
 
       expect(result.pagination).to.equal({
         total: 2
       })
-      //  This should be ordered by due date
+      //  This should be ordered first by start date, then by return reference
       expect(result.returns).to.equal([
         {
-          dueDate: null,
-          endDate,
-          id: latestReturn.id,
-          metadata: 323,
-          returnReference: '123',
-          startDate: latestStartDate,
-          status: 'due'
+          dueDate: returnLogs[1].dueDate,
+          endDate: returnLogs[1].endDate,
+          id: returnLogs[1].id,
+          metadata: returnLogs[1].metadata,
+          returnReference: returnLogs[1].returnReference,
+          startDate: returnLogs[1].startDate,
+          status: returnLogs[1].status
         },
         {
-          dueDate,
-          endDate,
-          id: firstReturn.id,
-          metadata: 323,
-          returnReference: '32',
-          startDate,
-          status: 'due'
+          dueDate: returnLogs[0].dueDate,
+          endDate: returnLogs[0].endDate,
+          id: returnLogs[0].id,
+          metadata: returnLogs[0].metadata,
+          returnReference: returnLogs[0].returnReference,
+          startDate: returnLogs[0].startDate,
+          status: returnLogs[0].status
         }
       ])
     })
