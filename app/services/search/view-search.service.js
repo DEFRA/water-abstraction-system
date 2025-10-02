10cb154326403346b748ce878ff19e3626fd945e
ben-sagar <ben.sagar1@defra.gov.uk>
2025-10-02 17:33:12 +0100

    Migrate search to system
    https://eaflood.atlassian.net/browse/WATER-5304
    
    We are migrating the internal search page (the root page of the internal service) from the legacy apps to our new `water-abstraction-system app`.
    
    The existing page allows users to search using different types of values, for example, licence references, licence names, and return references.
    
    We are implementing these different types of search into the new ‘system’ page one at a time to manage the complexity.
    
    This change is to implement searching for licences.

---
+++ app/services/search/view-search.service.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const SearchPresenter = require('../../presenters/search/search.presenter.js')
+
+/**
+ * Handles initial display of the /search page
+ * @module ViewSearchService
+ */
+
+/**
+ * Handles initial display of the /search page
+ *
+ * This service just displays a blank search page, actual search queries are handled by the `QuerySearchService`.
+ *
+ * @returns {Promise<object>} The view data for the search page
+ */
+async function go() {
+  const formattedData = SearchPresenter.go()
+
+  return {
+    activeNavBar: 'search',
+    ...formattedData
+  }
+}
+
+module.exports = {
+  go
+}
