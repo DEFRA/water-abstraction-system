8e02e89d6ca9b0cdaa60c7474fa3e9a30d53020d
Jason Claxton <jason.claxton@defra.gov.uk>
2025-10-02 11:19:42 +0100

    Update return log model and tests

--- test/models/return-log.model.test.js
+++ test/models/return-log.model.test.js
@@ -13,6 +13,8 @@ const LicenceModel = require('../../app/models/licence.model.js')
 const ReturnCycleHelper = require('../support/helpers/return-cycle.helper.js')
 const ReturnCycleModel = require('../../app/models/return-cycle.model.js')
 const ReturnLogHelper = require('../support/helpers/return-log.helper.js')
+const ReturnRequirementHelper = require('../support/helpers/return-requirement.helper.js')
+const ReturnRequirementModel = require('../../app/models/return-requirement.model.js')
 const ReturnSubmissionHelper = require('../support/helpers/return-submission.helper.js')
 const ReturnSubmissionModel = require('../../app/models/return-submission.model.js')
 
@@ -64,6 +66,58 @@ describe('Return Log model', () => {
       })
     })
 
+    describe('when linking to return cycles', () => {
+      let returnCycle
+      let testRecord
+
+      before(async () => {
+        returnCycle = await ReturnCycleHelper.select()
+        testRecord = await ReturnLogHelper.add({ returnCycleId: returnCycle.id })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ReturnLogModel.query().innerJoinRelated('returnCycle')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the return submissions', async () => {
+        const result = await ReturnLogModel.query().findById(testRecord.id).withGraphFetched('returnCycle')
+
+        expect(result).to.be.instanceOf(ReturnLogModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.returnCycle).to.be.an.instanceOf(ReturnCycleModel)
+        expect(result.returnCycle).to.equal(returnCycle, { skip: ['createdAt', 'updatedAt'] })
+      })
+    })
+
+    describe('when linking to return requirements', () => {
+      let testReturnRequirements
+
+      beforeEach(async () => {
+        testReturnRequirements = await ReturnRequirementHelper.add()
+
+        testRecord = await ReturnLogHelper.add({ returnRequirementId: testReturnRequirements.id })
+      })
+
+      it('can successfully run a related query', async () => {
+        const query = await ReturnLogModel.query().innerJoinRelated('returnRequirement')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the return requirement', async () => {
+        const result = await ReturnLogModel.query().findById(testRecord.id).withGraphFetched('returnRequirement')
+
+        expect(result).to.be.instanceOf(ReturnLogModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.returnRequirement).to.be.an.instanceOf(ReturnRequirementModel)
+        expect(result.returnRequirement).to.equal(testReturnRequirements)
+      })
+    })
+
     describe('when linking to return submissions', () => {
       let returnSubmissions
 
@@ -95,32 +149,6 @@ describe('Return Log model', () => {
         expect(result.returnSubmissions[0]).to.be.an.instanceOf(ReturnSubmissionModel)
         expect(result.returnSubmissions).to.include(returnSubmissions[0])
         expect(result.returnSubmissions).to.include(returnSubmissions[1])
-      })
-    })
-
-    describe('when linking to return cycles', () => {
-      let testRecord
-      let returnCycle
-
-      before(async () => {
-        returnCycle = await ReturnCycleHelper.select()
-        testRecord = await ReturnLogHelper.add({ returnCycleId: returnCycle.id })
-      })
-
-      it('can successfully run a related query', async () => {
-        const query = await ReturnLogModel.query().innerJoinRelated('returnCycle')
-
-        expect(query).to.exist()
-      })
-
-      it('can eager load the return submissions', async () => {
-        const result = await ReturnLogModel.query().findById(testRecord.id).withGraphFetched('returnCycle')
-
-        expect(result).to.be.instanceOf(ReturnLogModel)
-        expect(result.id).to.equal(testRecord.id)
-
-        expect(result.returnCycle).to.be.an.instanceOf(ReturnCycleModel)
-        expect(result.returnCycle).to.equal(returnCycle, { skip: ['createdAt', 'updatedAt'] })
       })
     })
   })
