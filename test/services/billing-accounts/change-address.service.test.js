08ea1c64148ed2a8c7404f04fb0b47cf362eacf1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-07 13:09:43 +0100

    Housekeeping - Kill flakey assertion
    We just want to know the record has been updated and we have that confirmation from the fact the other fields have been overwritten.
    
    The problem is if the test suite runs too fast the creation and update are so close that 'updatedAt' is the same, not greater than the createdAt.

--- test/services/billing-accounts/change-address.service.test.js
+++ test/services/billing-accounts/change-address.service.test.js
@@ -90,7 +90,6 @@ describe('Change address service', () => {
             expect(reFetchedExistingAddress.id).to.equal(existingAddress.id)
             expect(reFetchedExistingAddress.createdAt).to.equal(existingAddress.createdAt)
             expect(reFetchedExistingAddress.address1).to.equal(address.addressLine1)
-            expect(reFetchedExistingAddress.updatedAt).not.to.equal(existingAddress.updatedAt)
           })
 
           it('links the billing account address record to the existing address', async () => {
