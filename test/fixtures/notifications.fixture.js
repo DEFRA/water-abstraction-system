e3ec564f87239af09ad56c746e58894f656451aa
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-10-07 17:48:36 +0100

    Expand NotificationsFixture with realistic examples
    Now we are going to need to test a bunch of notification types, especially when it comes to the presenter.
    
    We want to enhance the data we display, so ideally we want to be working with as many 'realistic' examples as possible.
    
    This update provides 'happy-path' examples for each of the notice types we currently filter by on view notices page (not _every_ abstraction alert type, but we don't need to go _that_ far!)
    
    These are compatible with `NotificationHelper.add()`, or can be amended depending on the needs of the test.
    
    We may enhance, amend and expand them in subsequent commits. But this will help us get started.

--- test/fixtures/notifications.fixture.js
+++ test/fixtures/notifications.fixture.js
@@ -3,6 +3,209 @@
 const EventModel = require('../../app/models/event.model.js')
 const LicenceModel = require('../../app/models/licence.model.js')
 const NotificationModel = require('../../app/models/notification.model.js')
+const { generateUUID } = require('../../app/lib/general.lib.js')
+
+const ADDRESS = {
+  address_line_1: 'ACME Services Ltd',
+  address_line_2: 'ACME Operations Centre',
+  address_line_3: 'Bakersfield Road',
+  address_line_4: 'Bakersfield',
+  address_line_5: 'Bath',
+  address_line_6: 'SOMERSET',
+  address_line_7: 'BA1 1AA'
+}
+
+/**
+ * Generates an abstraction alert email notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the abstraction alert email notification
+ *
+ * @returns {object} The generated abstraction alert email object
+ */
+function abstractionAlertEmail(notice) {
+  const licenceMonitoringStationId = generateUUID()
+
+  const notification = {
+    createdAt: new Date('2025-10-09'),
+    eventId: notice.id,
+    licenceMonitoringStationId,
+    licences: notice.licences,
+    messageRef: 'water_abstraction_alert_stop_email',
+    messageType: 'email',
+    notifyId: generateUUID(),
+    notifyStatus: 'delivered',
+    pdf: null,
+    personalisation: {
+      alertType: 'stop',
+      condition_text: 'Effect of restriction: I have a bad feeling about this',
+      flow_or_level: 'flow',
+      issuer_email_address: 'luke.skywalker@rebelmail.test',
+      label: 'Death star',
+      licenceGaugingStationId: licenceMonitoringStationId,
+      licenceId: generateUUID(),
+      licenceRef: notice.licences[0],
+      monitoring_station_name: 'Death star',
+      sending_alert_type: 'stop',
+      source: '* Source of supply: Meridian Trench',
+      thresholdUnit: 'm3/s',
+      thresholdValue: 100
+    },
+    plaintext:
+      'Dear licence contact\n' +
+      '\n' +
+      'The flow at Death star monitoring station is now at 100 m3/s or less.\n' +
+      '\n' +
+      'This means that you must stop taking water now. \n',
+    recipient: 'grace.hopper@acme.co.uk',
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates an abstraction alert letter notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the abstraction alert letter notification
+ *
+ * @returns {object} The generated abstraction alert letter object
+ */
+function abstractionAlertLetter(notice) {
+  const licenceMonitoringStationId = generateUUID()
+
+  const notification = {
+    createdAt: new Date('2025-10-09'),
+    eventId: notice.id,
+    licenceMonitoringStationId,
+    licences: notice.licences,
+    messageRef: 'water_abstraction_alert_stop',
+    messageType: 'letter',
+    notifyId: generateUUID(),
+    notifyStatus: 'received',
+    pdf: null,
+    personalisation: {
+      ...ADDRESS,
+      alertType: 'stop',
+      condition_text: 'Effect of restriction: I have a bad feeling about this',
+      flow_or_level: 'flow',
+      issuer_email_address: 'luke.skywalker@rebelmail.test',
+      label: 'Death star',
+      licenceGaugingStationId: licenceMonitoringStationId,
+      licenceId: generateUUID(),
+      licenceRef: notice.licences[0],
+      monitoring_station_name: 'Death star',
+      name: 'ACME Services Ltd',
+      sending_alert_type: 'stop',
+      source: '* Source of supply: Meridian Trench',
+      thresholdUnit: 'm3/s',
+      thresholdValue: 100
+    },
+    plaintext:
+      'Dear licence contact\n' +
+      '\n' +
+      'The flow at Death star monitoring station is now at 100 m3/s or less.\n' +
+      '\n' +
+      'This means that you must stop taking water now.\n',
+    recipient: null,
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates a legacy hands off flow letter notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the legacy hands off flow letter notification
+ *
+ * @returns {object} The generated legacy hands off flow letter object
+ */
+function legacyHandsOfFlow(notice) {
+  const notification = {
+    createdAt: new Date('2022-10-09'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'notification_letter',
+    messageType: 'letter',
+    notifyId: generateUUID(),
+    notifyStatus: 'received',
+    pdf: null,
+    personalisation: {
+      address_line_1: ADDRESS.address_line_1,
+      address_line_2: ADDRESS.address_line_2,
+      address_line_3: ADDRESS.address_line_3,
+      address_line_4: ADDRESS.address_line_4,
+      address_line_5: ADDRESS.address_line_5,
+      address_line_6: ADDRESS.address_line_6,
+      postcode: ADDRESS.address_line_7,
+      body: `Water Resources Act 1991  \nOur reference: ${notice.referenceCode}\n\nDear licence holder,\n\n`,
+      heading: 'Warning: Abstraction restrictions may happen soon',
+      licenceId: generateUUID(),
+      subject: 'Warning: Abstraction restrictions may happen soon'
+    },
+    plaintext:
+      'Water Resources Act 1991  \n' +
+      `Our reference: ${notice.referenceCode}\n` +
+      '\n' +
+      'Dear licence holder,\n' +
+      '\n' +
+      '# This is an advance warning that you may be asked to stop or reduce your water abstraction soon.\n',
+    recipient: 'n/a',
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates a legacy renewal letter notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the legacy renewal letter notification
+ *
+ * @returns {object} The generated legacy renewal letter object
+ */
+function legacyRenewal(notice) {
+  const notification = {
+    createdAt: new Date('2022-10-09'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'notification_letter',
+    messageType: 'letter',
+    notifyId: generateUUID(),
+    notifyStatus: 'received',
+    pdf: null,
+    personalisation: {
+      address_line_1: ADDRESS.address_line_1,
+      address_line_2: ADDRESS.address_line_2,
+      address_line_3: ADDRESS.address_line_3,
+      address_line_4: ADDRESS.address_line_4,
+      address_line_5: ADDRESS.address_line_5,
+      address_line_6: ADDRESS.address_line_6,
+      postcode: ADDRESS.address_line_7,
+      body: `Water Resources Act 1991  \nOur reference: ${notice.referenceCode}\n\nDear licence holder,\n\n`,
+      heading: 'Invitation to apply for a water abstraction licence renewal',
+      licenceId: generateUUID(),
+      subject: 'Invitation to apply for a water abstraction licence renewal'
+    },
+    plaintext:
+      'Water Resources Act 1991  \n' +
+      `Our reference: ${notice.referenceCode}\n` +
+      '\n' +
+      'Dear licence holder,\n' +
+      '\n' +
+      '# All or part of the following abstraction licence will expire soon:\n',
+    recipient: 'n/a',
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
 
 /**
  * Represents a complete response from `FetchNotificationsService`
@@ -60,6 +263,211 @@ function notification() {
   }
 }
 
+/**
+ * Generates a paper return notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the paper return notification
+ *
+ * @returns {object} The generated paper return notification object
+ */
+function paperReturn(notice) {
+  const notification = {
+    createdAt: new Date('2025-10-09'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'pdf.return_form',
+    messageType: 'letter',
+    notifyId: generateUUID(),
+    notifyStatus: 'received',
+    pdf: Buffer.from('I am a paper return'),
+    personalisation: {
+      ...ADDRESS,
+      qr_url: `v1:5:${notice.licences[0]}:12345678:2024-04-01:2025-03-31`,
+      purpose: 'Potable Water Supply - Direct',
+      due_date: '28 April 2025',
+      end_date: '31 March 2025',
+      format_id: '12345678',
+      start_date: '1 April 2024',
+      licence_ref: notice.licences[0],
+      region_code: 5,
+      region_name: 'South West',
+      naldAreaCode: 'AASWX',
+      site_description: 'Wiley Coyote Borehole No 1',
+      returns_frequency: 'day',
+      is_two_part_tariff: false
+    },
+    plaintext: null,
+    returnLogIds: [generateUUID()],
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates a returns invitation email notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the returns invitation email notification
+ *
+ * @returns {object} The generated returns invitation email object
+ */
+function returnsInvitationEmail(notice) {
+  const notification = {
+    createdAt: new Date('2025-04-02'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'returns_invitation_primary_user_email',
+    messageType: 'email',
+    notifyId: generateUUID(),
+    notifyStatus: 'delivered',
+    pdf: null,
+    personalisation: {
+      periodEndDate: '31 March 2025',
+      returnDueDate: '28 April 2025',
+      periodStartDate: '1 April 2024'
+    },
+    plaintext:
+      'Dear licence holder\n' +
+      '\n' +
+      '^ You must submit a record of your water abstraction from 1 April 2024 to 31 March 2025. \n' +
+      '\n' +
+      '^ You’ll need to submit your returns by 1 April 2025.\n',
+    recipient: 'grace.hopper@acme.co.uk',
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates a returns invitation letter notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the returns invitation letter notification
+ *
+ * @returns {object} The generated returns invitation letter object
+ */
+function returnsInvitationLetter(notice) {
+  const notification = {
+    createdAt: new Date('2025-04-02'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'returns_invitation_licence_holder_letter',
+    messageType: 'letter',
+    notifyId: generateUUID(),
+    notifyStatus: 'received',
+    pdf: null,
+    personalisation: {
+      ...ADDRESS,
+      name: 'ACME Services Ltd',
+      periodEndDate: '31 March 2025',
+      returnDueDate: '28 April 2025',
+      periodStartDate: '1 April 2024'
+    },
+    plaintext:
+      'Dear ACME Services Ltd\n' +
+      '\n' +
+      '^ You must submit a record of your water abstraction from 1 April 2024 to 31 March 2025. \n' +
+      '\n' +
+      '^ You’ll need to submit your returns by 1 April 2025.\n',
+    recipient: null,
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates a returns reminder email notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the returns reminder email notification
+ *
+ * @returns {object} The generated returns reminder email object
+ */
+function returnsReminderEmail(notice) {
+  const notification = {
+    createdAt: new Date('2025-04-18'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'returns_reminder_primary_user_email',
+    messageType: 'email',
+    notifyId: generateUUID(),
+    notifyStatus: 'delivered',
+    pdf: null,
+    personalisation: {
+      periodEndDate: '31 March 2025',
+      returnDueDate: '28 April 2025',
+      periodStartDate: '1 April 2024'
+    },
+    plaintext:
+      'Dear licence holder\n' +
+      '\n' +
+      '^ You must submit a record of your water abstraction from 1 April 2024 to 31 March 2025. \n' +
+      '\n' +
+      '^ You’ll need to submit your returns by 1 April 2025.\n',
+    recipient: 'grace.hopper@acme.co.uk',
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
+/**
+ * Generates a returns reminder letter notification object associated to the provided notice
+ *
+ * @param {object} notice - The notice to associate with the returns reminder letter notification
+ *
+ * @returns {object} The generated returns reminder letter object
+ */
+function returnsReminderLetter(notice) {
+  const notification = {
+    createdAt: new Date('2025-04-18'),
+    eventId: notice.id,
+    licenceMonitoringStationId: null,
+    licences: notice.licences,
+    messageRef: 'returns_reminder_licence_holder_letter',
+    messageType: 'letter',
+    notifyId: generateUUID(),
+    notifyStatus: 'received',
+    pdf: null,
+    personalisation: {
+      ...ADDRESS,
+      name: 'ACME Services Ltd',
+      periodEndDate: '31 March 2025',
+      returnDueDate: '28 April 2025',
+      periodStartDate: '1 April 2024'
+    },
+    plaintext:
+      'Dear ACME Services Ltd\n' +
+      '\n' +
+      '# We do not have your returns for 1 April 2025 to 30 June 2025 \n' +
+      '\n' +
+      'If you’ve submitted them in the past few days, you can ignore this reminder.\n' +
+      '\n' +
+      'We asked you to submit your returns by 1 April 2025.\n',
+    recipient: null,
+    returnLogIds: null,
+    status: 'sent'
+  }
+
+  return notification
+}
+
 module.exports = {
-  notification
+  abstractionAlertEmail,
+  abstractionAlertLetter,
+  legacyHandsOfFlow,
+  legacyRenewal,
+  notification,
+  paperReturn,
+  returnsInvitationEmail,
+  returnsInvitationLetter,
+  returnsReminderEmail,
+  returnsReminderLetter
 }
