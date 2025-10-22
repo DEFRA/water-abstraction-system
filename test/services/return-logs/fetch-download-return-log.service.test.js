f75b21e005f0b2d1caf7d844db784576afd4e50c
Jason Claxton <jason.claxton@defra.gov.uk>
2025-10-22 15:35:38 +0100

    Update unit tests to catch readings issue

--- test/services/return-logs/fetch-download-return-log.service.test.js
+++ test/services/return-logs/fetch-download-return-log.service.test.js
@@ -3,15 +3,13 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
-const Sinon = require('sinon')
 
-const { describe, it, before, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { describe, it, before } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
 const ReturnLogHelper = require('../../support/helpers/return-log.helper.js')
 const ReturnSubmissionHelper = require('../../support/helpers/return-submission.helper.js')
-const ReturnSubmissionModel = require('../../../app/models/return-submission.model.js')
 const ReturnSubmissionLineHelper = require('../../support/helpers/return-submission-line.helper.js')
 
 // Thing under test
@@ -19,46 +17,38 @@ const FetchDownloadReturnLogService = require('../../../app/services/return-logs
 
 describe('Fetch Download Return Log service', () => {
   let returnLog
-  let returnSubmissions = []
+  let returnSubmissions
 
-  before(async () => {
-    returnLog = await ReturnLogHelper.add()
+  describe('when a return log exists that used abstraction volumes', () => {
+    before(async () => {
+      returnLog = await ReturnLogHelper.add()
 
-    returnSubmissions = await Promise.all([
-      ReturnSubmissionHelper.add({ returnLogId: returnLog.id, version: 1 }),
-      ReturnSubmissionHelper.add({ returnLogId: returnLog.id, version: 2 }),
-      ReturnSubmissionHelper.add({ returnLogId: returnLog.id, version: 3 })
-    ])
+      returnSubmissions = await Promise.all([
+        ReturnSubmissionHelper.add({ returnLogId: returnLog.id, version: 1 }),
+        ReturnSubmissionHelper.add({ returnLogId: returnLog.id, version: 2 }),
+        ReturnSubmissionHelper.add({ returnLogId: returnLog.id, version: 3 })
+      ])
 
-    await Promise.all([
-      ReturnSubmissionLineHelper.add({
-        returnSubmissionId: returnSubmissions[0].id,
-        endDate: '2023-03-31',
-        quantity: 10
-      }),
-      ReturnSubmissionLineHelper.add({
-        returnSubmissionId: returnSubmissions[0].id,
-        endDate: '2023-01-31',
-        quantity: 5
-      })
-    ])
-  })
+      await Promise.all([
+        ReturnSubmissionLineHelper.add({
+          returnSubmissionId: returnSubmissions[0].id,
+          startDate: '2023-02-01',
+          endDate: '2023-02-28',
+          quantity: 10
+        }),
+        ReturnSubmissionLineHelper.add({
+          returnSubmissionId: returnSubmissions[0].id,
+          startDate: '2023-01-01',
+          endDate: '2023-01-31',
+          quantity: 5
+        })
+      ])
+    })
 
-  beforeEach(async () => {
-    // We stub on the model prototype so that any created instances have $applyReadings stubbed. We don't set any return
-    // value as we don't need it to actually do anything; we just want to be able to assert that it was called.
-    Sinon.stub(ReturnSubmissionModel.prototype, '$applyReadings')
-  })
-
-  afterEach(() => {
-    Sinon.restore()
-  })
-
-  describe('when a return log exists', () => {
     it('returns the return log with its related return submission and return submission lines', async () => {
-      const results = await FetchDownloadReturnLogService.go(returnLog.id, 2)
+      const result = await FetchDownloadReturnLogService.go(returnLog.id, 2)
 
-      expect(results).to.equal({
+      expect(result).to.equal({
         id: returnLog.id,
         returnReference: returnLog.returnReference,
         startDate: returnLog.startDate,
@@ -73,21 +63,75 @@ describe('Fetch Download Return Log service', () => {
         ]
       })
     })
+
+    it('orders submission lines by start date', async () => {
+      const result = await FetchDownloadReturnLogService.go(returnLog.id, 1)
+      const lines = result.returnSubmissions[0].returnSubmissionLines
+
+      expect(lines).to.have.length(2)
+      expect(lines[0].startDate.toISOString()).to.equal('2023-01-01T00:00:00.000Z')
+      expect(lines[0].endDate.toISOString()).to.equal('2023-01-31T00:00:00.000Z')
+      expect(lines[1].startDate.toISOString()).to.equal('2023-02-01T00:00:00.000Z')
+      expect(lines[1].endDate.toISOString()).to.equal('2023-02-28T00:00:00.000Z')
+    })
   })
 
-  it('orders submission lines by start date', async () => {
-    const result = await FetchDownloadReturnLogService.go(returnLog.id, 1)
-    const lines = result.returnSubmissions[0].returnSubmissionLines
+  describe('when a return log exists that used meter readings', () => {
+    before(async () => {
+      returnLog = await ReturnLogHelper.add()
 
-    expect(lines).to.have.length(2)
-    expect(lines[0].endDate.toISOString()).to.equal('2023-01-31T00:00:00.000Z')
-    expect(lines[1].endDate.toISOString()).to.equal('2023-03-31T00:00:00.000Z')
-  })
+      returnSubmissions = await ReturnSubmissionHelper.add({
+        metadata: {
+          type: 'measured',
+          units: 'm³',
+          meters: [
+            {
+              units: 'm³',
+              readings: {
+                '2023-01-01_2023-01-31': 100,
+                '2023-02-01_2023-02-28': 170
+              },
+              multiplier: 1,
+              startReading: 0,
+              meterDetailsProvided: false
+            }
+          ],
+          method: 'oneMeter',
+          totalFlag: false
+        },
+        returnLogId: returnLog.id,
+        version: 1
+      })
 
-  it('applies readings to selected submission', async () => {
-    const result = await FetchDownloadReturnLogService.go(returnLog.id, 1)
-    const selectedSubmission = result.returnSubmissions[0]
+      await Promise.all([
+        ReturnSubmissionLineHelper.add({
+          returnSubmissionId: returnSubmissions.id,
+          startDate: '2023-02-01',
+          endDate: '2023-02-28',
+          quantity: 100
+        }),
+        ReturnSubmissionLineHelper.add({
+          returnSubmissionId: returnSubmissions.id,
+          startDate: '2023-01-01',
+          endDate: '2023-01-31',
+          quantity: 70
+        })
+      ])
+    })
 
-    expect(selectedSubmission.$applyReadings.calledOnce).to.be.true()
+    it('returns the return log with the submission lines populated with the meter readings', async () => {
+      const result = await FetchDownloadReturnLogService.go(returnLog.id, 1)
+      const lines = result.returnSubmissions[0].returnSubmissionLines
+
+      expect(lines).to.have.length(2)
+      expect(lines[0].startDate.toISOString()).to.equal('2023-01-01T00:00:00.000Z')
+      expect(lines[0].endDate.toISOString()).to.equal('2023-01-31T00:00:00.000Z')
+      expect(lines[0].quantity).to.equal(70)
+      expect(lines[0].reading).to.equal(100)
+      expect(lines[1].startDate.toISOString()).to.equal('2023-02-01T00:00:00.000Z')
+      expect(lines[1].endDate.toISOString()).to.equal('2023-02-28T00:00:00.000Z')
+      expect(lines[1].quantity).to.equal(100)
+      expect(lines[1].reading).to.equal(170)
+    })
   })
 })
