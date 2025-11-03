ada7727b90483343162ee6cc32c5015108542376
jonathangoulding <goulding.jon@icloud.com>
2025-11-03 12:07:43 +0000

    Update fetch returns due to null due dates
    https://eaflood.atlassian.net/browse/WATER-5360
    
    We recently implemented null due dates on return logs.
    
    This change updates the fetch returns due logic to handle a null due date for a return log.

--- app/services/notices/setup/fetch-returns-due.service.js
+++ app/services/notices/setup/fetch-returns-due.service.js
@@ -6,6 +6,8 @@
  */
 
 const ReturnLogModel = require('../../../models/return-log.model.js')
+const featureFlagsConfig = require('../../../../config/feature-flags.config.js')
+const { NoticeType } = require('../../../lib/static-lookups.lib.js')
 
 /**
  * Fetches the returns due for the `/notices/setup/remove-licences` page
@@ -16,23 +18,36 @@ const ReturnLogModel = require('../../../models/return-log.model.js')
  * with those provided (making them a valid licence in the context of 'the licence exists with the given due date).
  *
  * @param {string[]} licenceRefs - the licences to find
- * @param {string} dueDate - The 'due' date for outstanding return logs to fetch contacts for
- * @param {string} summer - Whether we are looking for outstanding summer or all year return logs
+ * @param {object} returnsPeriod - containing the 'startDate', 'endDate', 'dueDate' and 'summer'
+ * @param {string} noticeType - the notice type
  *
  * @returns {Promise<object[]>}
  */
-async function go(licenceRefs, dueDate, summer) {
-  return _fetch(licenceRefs, dueDate, summer)
+async function go(licenceRefs, returnsPeriod, noticeType) {
+  return _fetch(licenceRefs, returnsPeriod, noticeType)
 }
 
-async function _fetch(licenceRefs, dueDate, summer) {
-  return ReturnLogModel.query()
+async function _fetch(licenceRefs, returnsPeriod, noticeType) {
+  const query = ReturnLogModel.query()
     .select('licenceRef')
     .whereIn('licenceRef', [...licenceRefs])
     .andWhere('status', 'due')
-    .andWhere('dueDate', dueDate)
-    .whereJsonPath('metadata', '$.isSummer', '=', summer)
+    .andWhere('startDate', '>=', returnsPeriod.startDate)
+    .andWhere('endDate', '<=', returnsPeriod.endDate)
+    .whereJsonPath('metadata', '$.isSummer', '=', returnsPeriod.summer)
     .distinctOn('licenceRef')
+
+  if (!featureFlagsConfig.enableNullDueDate) {
+    query.andWhere('dueDate', returnsPeriod.dueDate)
+  } else {
+    if (noticeType === NoticeType.REMINDERS) {
+      query.whereNotNull('dueDate')
+    } else {
+      query.whereNull('dueDate')
+    }
+  }
+
+  return query
 }
 
 module.exports = {
