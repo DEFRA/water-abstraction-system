7213af3a8611caca181275708876adef3fb74c65
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-11-04 15:24:43 +0000

    Update CreateNoticeService to do all work
    Otherwise known as Ronseal!
    
    Other than updating SubmitCheckService itself, this is the final step in moving the work of creating the notice away from the submit and into the CreateNoticeService.

--- test/services/notices/setup/create-notice.service.test.js
+++ test/services/notices/setup/create-notice.service.test.js
@@ -9,85 +9,68 @@ const { expect } = Code
 
 // Test helpers
 const EventModel = require('../../../../app/models/event.model.js')
+const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
 const { generateReferenceCode } = require('../../../support/helpers/notification.helper.js')
 
 // Thing under test
 const CreateNoticeService = require('../../../../app/services/notices/setup/create-notice.service.js')
 
 describe('Notices - Setup - Create Notice service', () => {
-  let event
+  const issuer = 'hello@world.com'
+
+  let recipients
+  let session
 
   beforeEach(() => {
-    event = {}
+    const fixtureData = RecipientsFixture.recipients()
+
+    recipients = [fixtureData.primaryUser, fixtureData.returnsAgent]
+
+    session = {
+      returnsPeriod: 'quarterFour',
+      removeLicences: [],
+      journey: 'invitations',
+      referenceCode: generateReferenceCode(),
+      determinedReturnsPeriod: {
+        dueDate: new Date(`2025-07-28`),
+        endDate: new Date(`2025-06-30`),
+        startDate: new Date(`2025-04-01`),
+        summer: 'true'
+      },
+      subType: 'returnInvitation',
+      name: 'Returns: invitation'
+    }
   })
 
-  it('should create the notice (required values)', async () => {
-    const result = await CreateNoticeService.go(event)
+  it('creates the notice', async () => {
+    const result = await CreateNoticeService.go(session, recipients, issuer)
 
-    const createdResult = await EventModel.query().findById(result.id)
-
-    expect(result).equal({
-      createdAt: result.createdAt,
-      id: createdResult.id,
-      type: 'notification',
-      updatedAt: result.updatedAt
-    })
-  })
-
-  describe('when the notice is for a returns notification', () => {
-    const referenceCode = generateReferenceCode()
-
-    beforeEach(() => {
-      event = {
-        licences: JSON.stringify(['123', '456']),
+    expect(result).to.be.instanceOf(EventModel)
+    expect(result).equal(
+      {
+        issuer: 'hello@world.com',
+        licences: [...recipients[0].licence_refs, ...recipients[1].licence_refs],
         metadata: {
           name: 'Returns: invitation',
-          sent: 2783,
-          error: 18,
+          recipients: 2,
           options: { excludeLicences: [] },
-          recipients: 2801,
-          returnCycle: { dueDate: '2024-11-28', endDate: '2024-10-31', isSummer: true, startDate: '2023-11-01' }
-        },
-        referenceCode,
-        status: 'started',
-        subtype: 'returnInvitation'
-      }
-    })
-
-    it('should create the notice', async () => {
-      const result = await CreateNoticeService.go(event)
-
-      const createdResult = await EventModel.query().findById(result.id)
-
-      expect(createdResult).equal({
-        createdAt: createdResult.createdAt,
-        entities: null,
-        id: createdResult.id,
-        issuer: null,
-        licences: ['123', '456'],
-        metadata: {
-          error: 18,
-          name: 'Returns: invitation',
-          options: {
-            excludeLicences: []
-          },
-          recipients: 2801,
           returnCycle: {
-            dueDate: '2024-11-28',
-            endDate: '2024-10-31',
-            isSummer: true,
-            startDate: '2023-11-01'
-          },
-          sent: 2783
+            dueDate: '2025-07-28',
+            endDate: '2025-06-30',
+            startDate: '2025-04-01',
+            isSummer: true
+          }
         },
-        overallStatus: null,
-        referenceCode,
-        status: 'started',
-        statusCounts: null,
+        overallStatus: 'pending',
+        referenceCode: session.referenceCode,
+        status: 'completed',
+        statusCounts: { cancelled: 0, error: 0, pending: 2, returned: 0, sent: 0 },
         subtype: 'returnInvitation',
-        type: 'notification',
-        updatedAt: createdResult.updatedAt
-      })
-    })
+        type: 'notification'
+      },
+      {
+        skip: ['createdAt', 'id', 'updatedAt']
+      }
+    )
   })
 })
