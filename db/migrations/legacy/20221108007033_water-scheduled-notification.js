80d0e4d1d1142b2a40590e4983944cae0a282218
robertparkinson <robert@daedalusft.com>
2025-11-04 14:03:22 +0000

    leave in exisitng column

--- db/migrations/legacy/20221108007033_water-scheduled-notification.js
+++ db/migrations/legacy/20221108007033_water-scheduled-notification.js
@@ -33,6 +33,7 @@ exports.up = function (knex) {
     table.uuid('template_id')
     table.binary('pdf')
     table.date('returned_at')
+    table.uuid('alternate_notification_id')
     table.uuid('alternate_notice_id')
 
     // Legacy timestamps
