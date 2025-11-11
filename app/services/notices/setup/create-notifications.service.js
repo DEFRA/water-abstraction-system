8efad1db292b41bd46852fe074ae76538700b3e1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-11-11 16:13:52 +0000

    Update CreateNotificationsService to do all work
    Otherwise known as Ronseal!
    
    Other than updating SubmitCheckService itself, this is the final step in moving the work of creating the notifications away from the submit and into the CreateNotificationsService.
    
    We've simplified the logic around the presenters so they are used directly and consistently.
    
    We can also use PostgreSQL's `returning()` ability to return all the information we need to pass back to SubmitCheckService. This means we can make another service, `FetchNotificationsService` redundant.

--- app/services/notices/setup/create-notifications.service.js
+++ app/services/notices/setup/create-notifications.service.js
@@ -1,27 +1,70 @@
 'use strict'
 
 /**
- * Create notifications
+ * Create the notifications for a new notice based on the notice setup data and then return them
  * @module CreateNotificationsService
  */
 
+const AbstractionAlertNotificationsPresenter = require('../../../presenters/notices/setup/abstraction-alert-notifications.presenter.js')
+const PaperReturnNotificationsPresenter = require('../../../presenters/notices/setup/paper-return-notifications.presenter.js')
 const NotificationModel = require('../../../../app/models/notification.model.js')
+const NotificationsPresenter = require('../../../presenters/notices/setup/notifications.presenter.js')
+const { timestampForPostgres } = require('../../../lib/general.lib.js')
+const { NoticeType } = require('../../../lib/static-lookups.lib.js')
 
 /**
  * Create notifications
  *
  * This query uses postgresql's ability to insert with batches. There is a limitation on the batch size
- * https://www.postgresql.org/docs/current/postgres-fdw.html#POSTGRES-FDW-OPTIONS-REMOTE-EXECUTION. The calling
- * service will handle the batch process.
+ * https://www.postgresql.org/docs/current/postgres-fdw.html#POSTGRES-FDW-OPTIONS-REMOTE-EXECUTION.
  *
- * > By using the batch insert, we need the upstream service to set the 'createdAt' field.
- *
- * @param {object} notifications
+ * @param {SessionModel} session - The session instance
+ * @param {object[]} recipients - List of recipients, each containing details like email or address of the recipient
+ * @param {string} noticeId - The UUID of the notice these notifications should be linked to
  *
  * @returns {Promise<object>} - the created notifications
  */
-async function go(notifications) {
-  return NotificationModel.query().insert(notifications)
+async function go(session, recipients, noticeId) {
+  const notifications = _notifications(session, recipients, noticeId)
+  const persistedNotifications = []
+
+  for (const notification of notifications) {
+    const persistedNotification = await NotificationModel.query()
+      .insert({
+        ...notification,
+        createdAt: timestampForPostgres()
+      })
+      .returning([
+        'createdAt',
+        'id',
+        'licenceMonitoringStationId',
+        'messageRef',
+        'messageType',
+        'pdf',
+        'personalisation',
+        'recipient',
+        'returnLogIds',
+        'templateId'
+      ])
+
+    persistedNotifications.push(persistedNotification)
+  }
+
+  return persistedNotifications
+}
+
+function _notifications(session, recipients, noticeId) {
+  const { noticeType } = session
+
+  if (noticeType === NoticeType.ABSTRACTION_ALERTS) {
+    return AbstractionAlertNotificationsPresenter.go(session, recipients, noticeId)
+  }
+
+  if (noticeType === NoticeType.PAPER_RETURN) {
+    return PaperReturnNotificationsPresenter.go(session, recipients, noticeId)
+  }
+
+  return NotificationsPresenter.go(session, recipients, noticeId)
 }
 
 module.exports = {
--- test/services/notices/setup/create-notifications.service.test.js
+++ test/services/notices/setup/create-notifications.service.test.js
@@ -8,81 +8,451 @@ const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const EventHelper = require('../../../support/helpers/event.helper.js')
-const NotificationModel = require('../../../../app/models/notification.model.js')
-const { timestampForPostgres } = require('../../../../app/lib/general.lib.js')
+const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
+const { generateUUID } = require('../../../../app/lib/general.lib.js')
+const { generateLicenceRef } = require('../../../support/helpers/licence.helper.js')
+const { generateReferenceCode } = require('../../../support/helpers/notification.helper.js')
 
 // Thing under test
 const CreateNotificationsService = require('../../../../app/services/notices/setup/create-notifications.service.js')
 
-describe('Notices - Setup - Create notification service', () => {
-  let eventId
-  let notifications
+describe('Notices - Setup - Create Notifications service', () => {
+  let noticeId
+  let recipients
+  let session
 
   beforeEach(async () => {
-    const event = await EventHelper.add({
-      type: 'notification',
-      subtype: 'returnsInvitation'
-    })
-
-    eventId = event.id
-
-    notifications = [
-      { eventId, createdAt: timestampForPostgres() },
-      { eventId, createdAt: timestampForPostgres() }
-    ]
+    noticeId = generateUUID()
   })
 
-  it('should return the saved notifications', async () => {
-    const result = await CreateNotificationsService.go(notifications)
+  describe('when the notice is an abstraction alert', () => {
+    beforeEach(() => {
+      const fixtureData = RecipientsFixture.alertsRecipients()
 
-    const createdNotifications = await NotificationModel.query().where({
-      event_id: eventId
+      recipients = [fixtureData.additionalContact, fixtureData.primaryUser]
+
+      const licenceMonitoringStations = [
+        {
+          abstractionPeriodEndDay: 31,
+          abstractionPeriodEndMonth: 10,
+          abstractionPeriodStartDay: 1,
+          abstractionPeriodStartMonth: 4,
+          id: generateUUID(),
+          latestNotification: null,
+          licence: {
+            id: generateUUID(),
+            licenceRef: recipients[0].licence_refs[0]
+          },
+          measureType: 'flow',
+          notes: null,
+          restrictionType: 'stop',
+          thresholdGroup: 'flow-50-m3/s',
+          thresholdValue: 50,
+          thresholdUnit: 'm3/s'
+        },
+        {
+          abstractionPeriodEndDay: 31,
+          abstractionPeriodEndMonth: 10,
+          abstractionPeriodStartDay: 1,
+          abstractionPeriodStartMonth: 4,
+          id: generateUUID(),
+          latestNotification: null,
+          licence: {
+            id: generateUUID(),
+            licenceRef: recipients[1].licence_refs[0]
+          },
+          measureType: 'flow',
+          notes: null,
+          restrictionType: 'stop',
+          thresholdGroup: 'flow-500-Ml/d',
+          thresholdValue: 500,
+          thresholdUnit: 'Ml/d'
+        }
+      ]
+
+      session = {
+        alertEmailAddress: 'admin-internal@wrls.gov.uk',
+        alertEmailAddressType: 'username',
+        alertThresholds: ['flow-50-m3/s', 'flow-500-Ml/d'],
+        alertType: 'stop',
+        licenceRefs: [...recipients[0].licence_refs, ...recipients[1].licence_refs],
+        licenceMonitoringStations,
+        journey: 'alerts',
+        monitoringStationId: generateUUID(),
+        monitoringStationName: 'DEATH STAR',
+        monitoringStationRiverName: '',
+        name: 'Water abstraction alert',
+        noticeType: 'abstractionAlerts',
+        notificationType: 'Abstraction alert',
+        referenceCode: generateReferenceCode('WAA-'),
+        relevantLicenceMonitoringStations: [...licenceMonitoringStations],
+        removedThresholds: [],
+        selectedRecipients: [recipients[0].contact_hash_id, recipients[1].contact_hash_id],
+        subType: 'waterAbstractionAlerts'
+      }
     })
 
-    expect(createdNotifications).equal([
-      {
-        alternateNoticeId: null,
-        createdAt: createdNotifications[0].createdAt,
-        eventId,
-        id: result[0].id,
-        licenceMonitoringStationId: null,
-        licences: null,
-        messageRef: null,
-        messageType: null,
-        notifyError: null,
-        notifyId: null,
-        notifyStatus: null,
-        pdf: null,
-        personalisation: null,
-        plaintext: null,
-        recipient: null,
-        returnedAt: null,
-        returnLogIds: null,
-        status: null,
-        templateId: null
-      },
-      {
-        alternateNoticeId: null,
-        createdAt: createdNotifications[1].createdAt,
-        eventId,
-        id: result[1].id,
-        licenceMonitoringStationId: null,
-        licences: null,
-        messageRef: null,
-        messageType: null,
-        notifyError: null,
-        notifyId: null,
-        notifyStatus: null,
-        pdf: null,
-        personalisation: null,
-        plaintext: null,
-        recipient: null,
-        returnedAt: null,
-        returnLogIds: null,
-        status: null,
-        templateId: null
+    it('returns the created the abstraction alert notifications', async () => {
+      const results = await CreateNotificationsService.go(session, recipients, noticeId)
+
+      expect(results).to.have.length(2)
+
+      expect(results[0]).to.equal(
+        {
+          eventId: noticeId,
+          licenceMonitoringStationId: session.licenceMonitoringStations[0].id,
+          licences: recipients[0].licence_refs,
+          messageRef: 'water_abstraction_alert_stop_email',
+          messageType: 'email',
+          pdf: null,
+          personalisation: {
+            alertType: 'stop',
+            condition_text: '',
+            flow_or_level: 'flow',
+            issuer_email_address: 'admin-internal@wrls.gov.uk',
+            label: 'DEATH STAR',
+            licenceGaugingStationId: session.licenceMonitoringStations[0].id,
+            licenceId: session.licenceMonitoringStations[0].licence.id,
+            licenceRef: session.licenceMonitoringStations[0].licence.licenceRef,
+            monitoring_station_name: 'DEATH STAR',
+            sending_alert_type: 'stop',
+            source: '',
+            thresholdUnit: 'm3/s',
+            thresholdValue: 50
+          },
+          recipient: 'additional.contact@important.com',
+          returnLogIds: null,
+          status: 'pending',
+          templateId: 'd7468ba1-ac65-42c4-9785-8998f9c34e01'
+        },
+        { skip: ['createdAt', 'id'] }
+      )
+
+      expect(results[1]).to.equal(
+        {
+          eventId: noticeId,
+          licenceMonitoringStationId: session.licenceMonitoringStations[1].id,
+          licences: recipients[1].licence_refs,
+          messageRef: 'water_abstraction_alert_stop_email',
+          messageType: 'email',
+          pdf: null,
+          personalisation: {
+            alertType: 'stop',
+            condition_text: '',
+            flow_or_level: 'flow',
+            issuer_email_address: 'admin-internal@wrls.gov.uk',
+            label: 'DEATH STAR',
+            licenceGaugingStationId: session.licenceMonitoringStations[1].id,
+            licenceId: session.licenceMonitoringStations[1].licence.id,
+            licenceRef: session.licenceMonitoringStations[1].licence.licenceRef,
+            monitoring_station_name: 'DEATH STAR',
+            sending_alert_type: 'stop',
+            source: '',
+            thresholdUnit: 'Ml/d',
+            thresholdValue: 500
+          },
+          recipient: 'primary.user@important.com',
+          returnLogIds: null,
+          status: 'pending',
+          templateId: 'd7468ba1-ac65-42c4-9785-8998f9c34e01'
+        },
+        { skip: ['createdAt', 'id'] }
+      )
+    })
+  })
+
+  describe('when the notice is a paper return', () => {
+    beforeEach(() => {
+      const fixtureData = RecipientsFixture.recipients()
+
+      const licenceRef = generateLicenceRef()
+
+      recipients = [
+        { ...fixtureData.licenceHolder, licence_refs: [licenceRef] },
+        { ...fixtureData.returnsTo, licence_refs: [licenceRef] }
+      ]
+
+      const referenceCode = generateReferenceCode('PRTF-')
+      const selectedReturnId = generateUUID()
+      const sessionId = generateUUID()
+
+      session = {
+        addressJourney: {
+          address: {},
+          backLink: {
+            href: `/system/notices/setup/${sessionId}/recipient-name`,
+            text: 'Back'
+          },
+          redirectUrl: `/system/notices/setup/${sessionId}/add-recipient`,
+          activeNavBar: 'notices',
+          pageTitleCaption: `Notice ${referenceCode}`
+        },
+        checkPageVisited: true,
+        dueReturns: [
+          {
+            dueDate: '2025-04-28T00:00:00.000Z',
+            endDate: '2025-03-31T00:00:00.000Z',
+            naldAreaCode: 'RIDIN',
+            purpose: 'Spray Irrigation - Direct',
+            regionCode: 3,
+            regionName: 'North East',
+            returnId: selectedReturnId,
+            returnLogId: `v1:3:${licenceRef}:10059610:2024-04-01:2025-03-31`,
+            returnReference: '10059610',
+            returnsFrequency: 'month',
+            siteDescription: 'BOREHOLE AT AVALON',
+            startDate: '2024-04-01T00:00:00.000Z',
+            twoPartTariff: false
+          },
+          {
+            dueDate: '2024-04-28T00:00:00.000Z',
+            endDate: '2024-03-31T00:00:00.000Z',
+            purpose: 'Spray Irrigation - Direct',
+            returnId: generateUUID(),
+            startDate: '2023-04-01T00:00:00.000Z',
+            regionCode: 3,
+            regionName: 'North East',
+            returnLogId: `v1:3:${licenceRef}:10044001:2023-04-01:2024-03-31`,
+            naldAreaCode: 'RIDIN',
+            twoPartTariff: false,
+            returnReference: '10044001',
+            siteDescription: 'PUMP AT TINTAGEL',
+            returnsFrequency: 'month'
+          }
+        ],
+        id: sessionId,
+        licenceRef,
+        journey: 'adhoc',
+        name: 'Paper returns',
+        notificationType: 'Paper returns',
+        noticeType: 'paperReturn',
+        referenceCode,
+        selectedRecipients: [recipients[0].contact_hash_id, recipients[1].contact_hash_id],
+        selectedReturns: [selectedReturnId],
+        subType: 'paperReturnForms'
       }
-    ])
+    })
+
+    it('returns the created the paper return notifications', async () => {
+      const results = await CreateNotificationsService.go(session, recipients, noticeId)
+
+      expect(results).to.have.length(2)
+
+      expect(results[0]).to.equal(
+        {
+          eventId: noticeId,
+          licenceMonitoringStationId: null,
+          licences: recipients[0].licence_refs,
+          messageRef: 'pdf.return_form',
+          messageType: 'letter',
+          pdf: null,
+          personalisation: {
+            address_line_1: 'Mr H J Potter',
+            address_line_2: '1',
+            address_line_3: 'Privet Drive',
+            address_line_4: 'Little Whinging',
+            address_line_5: 'Surrey',
+            address_line_6: 'WD25 7LR',
+            due_date: '28 April 2025',
+            end_date: '31 March 2025',
+            format_id: '10059610',
+            is_two_part_tariff: false,
+            licence_ref: session.licenceRef,
+            naldAreaCode: 'RIDIN',
+            purpose: 'Spray Irrigation - Direct',
+            qr_url: session.dueReturns[0].returnId,
+            region_code: 3,
+            region_name: 'North East',
+            returns_frequency: 'month',
+            site_description: 'BOREHOLE AT AVALON',
+            start_date: '1 April 2024'
+          },
+          recipient: null,
+          returnLogIds: session.selectedReturns,
+          status: 'pending',
+          templateId: null
+        },
+        { skip: ['createdAt', 'id'] }
+      )
+
+      expect(results[1]).to.equal(
+        {
+          eventId: noticeId,
+          licenceMonitoringStationId: null,
+          licences: recipients[1].licence_refs,
+          messageRef: 'pdf.return_form',
+          messageType: 'letter',
+          pdf: null,
+          personalisation: {
+            address_line_1: 'Mr H J Weasley',
+            address_line_2: 'INVALID ADDRESS - Needs a valid postcode or country outside the UK',
+            address_line_3: '2',
+            address_line_4: 'Privet Drive',
+            address_line_5: 'Little Whinging',
+            address_line_6: 'Surrey',
+            due_date: '28 April 2025',
+            end_date: '31 March 2025',
+            format_id: '10059610',
+            is_two_part_tariff: false,
+            licence_ref: session.licenceRef,
+            naldAreaCode: 'RIDIN',
+            purpose: 'Spray Irrigation - Direct',
+            qr_url: session.dueReturns[0].returnId,
+            region_code: 3,
+            region_name: 'North East',
+            returns_frequency: 'month',
+            site_description: 'BOREHOLE AT AVALON',
+            start_date: '1 April 2024'
+          },
+          recipient: null,
+          returnLogIds: session.selectedReturns,
+          status: 'pending',
+          templateId: null
+        },
+        { skip: ['createdAt', 'id'] }
+      )
+    })
+  })
+
+  describe('when the notice is a returns invitation or reminder', () => {
+    beforeEach(() => {
+      const fixtureData = RecipientsFixture.recipients()
+
+      const licenceRef = generateLicenceRef()
+      const dueReturns = [
+        {
+          dueDate: '2025-04-28T00:00:00.000Z',
+          endDate: '2025-03-31T00:00:00.000Z',
+          naldAreaCode: 'RIDIN',
+          purpose: 'Spray Irrigation - Direct',
+          regionCode: 3,
+          regionName: 'North East',
+          returnId: generateUUID(),
+          returnLogId: `v1:3:${licenceRef}:10059610:2024-04-01:2025-03-31`,
+          returnReference: '10059610',
+          returnsFrequency: 'month',
+          siteDescription: 'BOREHOLE AT AVALON',
+          startDate: '2024-04-01T00:00:00.000Z',
+          twoPartTariff: false
+        },
+        {
+          dueDate: '2024-04-28T00:00:00.000Z',
+          endDate: '2024-03-31T00:00:00.000Z',
+          purpose: 'Spray Irrigation - Direct',
+          returnId: generateUUID(),
+          startDate: '2023-04-01T00:00:00.000Z',
+          regionCode: 3,
+          regionName: 'North East',
+          returnLogId: `v1:3:${licenceRef}:10044001:2023-04-01:2024-03-31`,
+          naldAreaCode: 'RIDIN',
+          twoPartTariff: false,
+          returnReference: '10044001',
+          siteDescription: 'PUMP AT TINTAGEL',
+          returnsFrequency: 'month'
+        }
+      ]
+
+      recipients = [
+        {
+          ...fixtureData.licenceHolder,
+          licence_refs: [licenceRef],
+          return_log_ids: [dueReturns[0].returnId, dueReturns[1].returnId]
+        },
+        {
+          ...fixtureData.returnsTo,
+          licence_refs: [licenceRef],
+          return_log_ids: [dueReturns[0].returnId, dueReturns[1].returnId]
+        }
+      ]
+
+      const referenceCode = generateReferenceCode('RINV-')
+      const sessionId = generateUUID()
+
+      session = {
+        addressJourney: {
+          address: {},
+          backLink: {
+            href: `/system/notices/setup/${sessionId}/recipient-name`,
+            text: 'Back'
+          },
+          redirectUrl: `/system/notices/setup/${sessionId}/add-recipient`,
+          activeNavBar: 'notices',
+          pageTitleCaption: `Notice ${referenceCode}`
+        },
+        checkPageVisited: true,
+        dueReturns,
+        id: sessionId,
+        licenceRef,
+        journey: 'adhoc',
+        name: 'Returns: invitation',
+        notificationType: 'Returns invitation',
+        noticeType: 'invitations',
+        referenceCode,
+        selectedRecipients: [recipients[0].contact_hash_id, recipients[1].contact_hash_id],
+        subType: 'returnInvitation'
+      }
+    })
+
+    it('returns the created the returns notifications', async () => {
+      const results = await CreateNotificationsService.go(session, recipients, noticeId)
+
+      expect(results).to.have.length(2)
+
+      expect(results[0]).to.equal(
+        {
+          eventId: noticeId,
+          licenceMonitoringStationId: null,
+          licences: recipients[0].licence_refs,
+          messageRef: 'returns_invitation_licence_holder_letter',
+          messageType: 'letter',
+          pdf: null,
+          personalisation: {
+            address_line_1: 'Mr H J Potter',
+            address_line_2: '1',
+            address_line_3: 'Privet Drive',
+            address_line_4: 'Little Whinging',
+            address_line_5: 'Surrey',
+            address_line_6: 'WD25 7LR',
+            name: 'Mr H J Potter',
+            periodEndDate: null,
+            periodStartDate: null,
+            returnDueDate: '10 December 2025'
+          },
+          recipient: null,
+          returnLogIds: recipients[0].return_log_ids,
+          status: 'pending',
+          templateId: '4b47cf1c-043c-4a0c-8659-5be06cb2b860'
+        },
+        { skip: ['createdAt', 'id'] }
+      )
+
+      expect(results[1]).to.equal(
+        {
+          eventId: noticeId,
+          licenceMonitoringStationId: null,
+          licences: recipients[1].licence_refs,
+          messageRef: 'returns_invitation_returns_to_letter',
+          messageType: 'letter',
+          pdf: null,
+          personalisation: {
+            address_line_1: 'Mr H J Weasley',
+            address_line_2: 'INVALID ADDRESS - Needs a valid postcode or country outside the UK',
+            address_line_3: '2',
+            address_line_4: 'Privet Drive',
+            address_line_5: 'Little Whinging',
+            address_line_6: 'Surrey',
+            name: 'Mr H J Weasley',
+            periodEndDate: null,
+            periodStartDate: null,
+            returnDueDate: '10 December 2025'
+          },
+          recipient: null,
+          returnLogIds: recipients[1].return_log_ids,
+          status: 'pending',
+          templateId: '73b4c395-4423-4976-8ab4-c82e2cb6beee'
+        },
+        { skip: ['createdAt', 'id'] }
+      )
+    })
   })
 })
