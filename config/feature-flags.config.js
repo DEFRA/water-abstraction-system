75b3b616d1e1933eaaa8540d22131cd599f67bcf
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2025-11-17 12:25:58 +0000

    Remove feature flag `ENABLE_REISSUING_BILLING_BATCHES` (#2655)
    We currently have several feature flags that are no longer required so we are going to remove them.
    
    In this PR the `ENABLE_REISSUING_BILLING_BATCHES` feature flag will be removed.

--- config/feature-flags.config.js
+++ config/feature-flags.config.js
@@ -19,7 +19,6 @@ const config = {
   enableLicencePurposesView: String(process.env.ENABLE_LICENCE_PURPOSES_VIEW) === 'true' || false,
   enableMonitoringStationsView: String(process.env.ENABLE_MONITORING_STATIONS_VIEW) === 'true' || false,
   enableNullDueDate: String(process.env.ENABLE_NULL_DUE_DATE) === 'true' || false,
-  enableReissuingBillingBatches: String(process.env.ENABLE_REISSUING_BILLING_BATCHES) === 'true' || false,
   enableRequirementsForReturns: String(process.env.ENABLE_REQUIREMENTS_FOR_RETURNS) === 'true' || false,
   enableSystemLicenceView: String(process.env.ENABLE_SYSTEM_LICENCE_VIEW) === 'true' || false,
   enableSystemProfiles: String(process.env.ENABLE_SYSTEM_PROFILES) === 'true' || false
