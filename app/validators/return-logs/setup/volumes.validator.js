be53c5bd7dce8f999a61f756b95a04ff7e44a017
Jason Claxton <jason.claxton@defra.gov.uk>
2025-11-19 18:43:55 +0000

    Update `VolumesValidator` to only allow up to 6 DP

--- app/validators/return-logs/setup/volumes.validator.js
+++ app/validators/return-logs/setup/volumes.validator.js
@@ -23,7 +23,9 @@ function go(payload) {
     Joi.number()
       .min(0)
       .max(MAX_ALLOWED_VOLUME)
+      .custom(_maxDecimals, 'Max decimals')
       .messages({
+        'any.invalid': 'Enter a number with no more than 6 decimal places',
         'number.base': 'Volume must be a number or blank',
         'number.min': 'Volume must be a positive number',
         'number.max': `Volume entered exceeds the maximum of ${MAX_ALLOWED_VOLUME}`,
@@ -32,6 +34,35 @@ function go(payload) {
   )
 
   return schema.validate(payload, { abortEarly: false })
+}
+
+/**
+ * Custom JOI validator to check a value does not have more than 6 decimal places
+ *
+ * Due to limitations in Joi validation for decimals, achieving validation for numbers with more than 6 decimal places
+ * requires a custom approach.
+ *
+ * See {@link https://github.com/hapijs/joi/blob/master/API.md#anycustommethod-description | Joi custom validation}.
+ *
+ * @param {number} value - the value to be validated
+ * @param {object} helpers - a Joi object containing a numbers of helpers
+ *
+ * @returns {number|object} if valid the original value else a Joi 'any.invalid' error. Knowing we return this means
+ * you can assign what error message to use when a number has too many decimals.
+ */
+function _maxDecimals(value, helpers) {
+  // Guard clause to ensure we don't try and interact with a null or undefined value
+  if (!value) {
+    return value
+  }
+
+  const parts = value.toString().split('.')
+
+  if (parts.length === 1 || parts[1].length <= 6) {
+    return value
+  }
+
+  return helpers.error('any.invalid')
 }
 
 module.exports = {
