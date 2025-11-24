8940b8000682fbbbbe03864dcfe96ad6104b3596
jonathangoulding <goulding.jon@icloud.com>
2025-11-24 15:53:04 +0000

    chore: pr fix

--- test/services/licences/view-purposes.service.test.js
+++ test/services/licences/view-purposes.service.test.js
@@ -5,7 +5,7 @@ const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 const Sinon = require('sinon')
 
-const { describe, it, afterEach, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
@@ -41,10 +41,6 @@ describe('Licences - View Purposes service', () => {
       licence,
       purposes
     })
-  })
-
-  afterEach(() => {
-    Sinon.restore()
   })
 
   describe('when a licence with a matching ID exists', () => {
