2acdd872c9eb7c66bd2d197e44c8a5dc1aaf34b6
Jason Claxton <jason.claxton@defra.gov.uk>
2025-11-26 15:40:48 +0000

    Create `UpdateQuantitiesService`

---
+++ app/services/return-logs/setup/update-quantities.service.js
@@ -0,0 +1,66 @@
+'use strict'
+
+/**
+ * Updates the quantities saved in the session following a change of UOM or the submission of readings
+ * @module UpdateQuantitiesService
+ */
+
+const { convertFromCubicMetres, convertToCubicMetres } = require('../../../lib/general.lib.js')
+
+const METER_READINGS_METHOD = 'meterReadings'
+
+/**
+ * Updates the quantities saved in the session following a change of UOM or the submission of readings
+ *
+ * When "volumes" are used the line.quantity is calculated from line.quantityCubicMetres in the sessions data as this
+ * will always be populated with the correct volume in cubic metres. Whereas the line.quantity may require updating if
+ * the UOM has been changed.
+ *
+ * When "meter readings" are used, the line.quantity and line.quantityCubicMetres both need to be calculated based on
+ * the difference between the current and previous meter readings. If the meter has a x10 display, the calculated
+ * quantity will be multiplied by 10.
+ *
+ * @param {object} session - Session object containing the return submission data
+ */
+async function go(session) {
+  if (session.reported === METER_READINGS_METHOD) {
+    return _readingsUpdate(session)
+  }
+
+  return _volumesUpdate(session)
+}
+
+async function _readingsUpdate(session) {
+  const multiplier = session.meter10TimesDisplay === 'yes' ? 10 : 1
+  let previousReading = session.startReading ?? 0
+
+  session.lines.forEach((line) => {
+    if (typeof line.reading === 'number') {
+      const userQuantity = (line.reading - previousReading) * multiplier
+
+      line.quantityCubicMetres = convertToCubicMetres(userQuantity, session.unitSymbol)
+      line.quantity = convertFromCubicMetres(line.quantityCubicMetres, session.unitSymbol)
+      previousReading = line.reading
+    } else {
+      // We null the quantities to ensure that any previously saved quantities are removed if the reading is now missing
+      line.quantity = null
+      line.quantityCubicMetres = null
+    }
+  })
+
+  return session.$update()
+}
+
+async function _volumesUpdate(session) {
+  session.lines.forEach((line) => {
+    line.quantity = line.quantityCubicMetres
+      ? convertFromCubicMetres(line.quantityCubicMetres, session.unitSymbol)
+      : line.quantityCubicMetres
+  })
+
+  return session.$update()
+}
+
+module.exports = {
+  go
+}
