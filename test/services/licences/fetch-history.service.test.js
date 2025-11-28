3a7f5b7c389b04d330ccd60c5e117632a7810e5d
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-11-28 13:41:20 +0000

    Update licence history to only licence versions (#2716)
    * Update licence history to only licence versions
    
    https://eaflood.atlassian.net/browse/WATER-5071
    
    The updated history page only shows licence versions. So we need to remove the returns and charges from the fetch query and the view.
    
    This change strips the history page back to showing only the licence versions.
    
    It only shows the start and end date in the view (and the view action button).
    
    The UI has been updated to use the updated table, the other fields have been left null and will be implemented in a later change

--- test/services/licences/fetch-history.service.test.js
+++ test/services/licences/fetch-history.service.test.js
@@ -8,26 +8,16 @@ const { describe, it, before } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const ChargeVersionHelper = require('../../support/helpers/charge-version.helper.js')
-const ChargeVersionNoteHelper = require('../../support/helpers/charge-version-note.helper.js')
 const LicenceHelper = require('../../support/helpers/licence.helper.js')
 const LicenceVersionHelper = require('../../support/helpers/licence-version.helper.js')
-const ModLogHelper = require('../../support/helpers/mod-log.helper.js')
-const ReturnVersionHelper = require('../../support/helpers/return-version.helper.js')
 
 // Thing under test
 const FetchHistoryService = require('../../../app/services/licences/fetch-history.service.js')
 
 describe('Licences - Fetch History service', () => {
-  let chargeVersion
-  let chargeVersionModLog
-  let chargeVersionNote
   let licence
   let licenceId
   let licenceVersion
-  let licenceVersionModLog
-  let returnVersion
-  let returnVersionModLog
 
   describe('when the licence has licence versions, charge versions and return versions', () => {
     before(async () => {
@@ -35,16 +25,7 @@ describe('Licences - Fetch History service', () => {
 
       licenceId = licence.id
 
-      const { licenceRef } = licence
-
-      chargeVersionNote = await ChargeVersionNoteHelper.add()
-      chargeVersion = await ChargeVersionHelper.add({ licenceId, licenceRef, noteId: chargeVersionNote.id })
       licenceVersion = await LicenceVersionHelper.add({ licenceId })
-      returnVersion = await ReturnVersionHelper.add({ licenceId })
-
-      chargeVersionModLog = await ModLogHelper.add({ chargeVersionId: chargeVersion.id })
-      licenceVersionModLog = await ModLogHelper.add({ licenceVersionId: licenceVersion.id })
-      returnVersionModLog = await ModLogHelper.add({ returnVersionId: returnVersion.id })
     })
 
     it('returns the matching licence versions, charge versions and return versions', async () => {
@@ -52,62 +33,11 @@ describe('Licences - Fetch History service', () => {
 
       expect(result).to.equal({
         id: licence.id,
-        chargeVersions: [
-          {
-            id: chargeVersion.id,
-            createdAt: chargeVersion.createdAt,
-            createdBy: null,
-            changeReason: null,
-            chargeVersionNote: {
-              id: chargeVersionNote.id,
-              note: 'This is a test note'
-            },
-            startDate: chargeVersion.startDate,
-            modLogs: [
-              {
-                id: chargeVersionModLog.id,
-                naldDate: chargeVersionModLog.naldDate,
-                note: chargeVersionModLog.note,
-                reasonDescription: chargeVersionModLog.reasonDescription,
-                userId: chargeVersionModLog.userId
-              }
-            ]
-          }
-        ],
         licenceVersions: [
           {
-            createdAt: licenceVersion.createdAt,
+            endDate: licenceVersion.endDate,
             id: licenceVersion.id,
-            startDate: licenceVersion.startDate,
-            modLogs: [
-              {
-                id: licenceVersionModLog.id,
-                naldDate: licenceVersionModLog.naldDate,
-                note: licenceVersionModLog.note,
-                reasonDescription: licenceVersionModLog.reasonDescription,
-                userId: licenceVersionModLog.userId
-              }
-            ]
-          }
-        ],
-        returnVersions: [
-          {
-            createdAt: returnVersion.createdAt,
-            id: returnVersion.id,
-            notes: null,
-            createdBy: null,
-            reason: 'new-licence',
-            startDate: returnVersion.startDate,
-            modLogs: [
-              {
-                id: returnVersionModLog.id,
-                naldDate: returnVersionModLog.naldDate,
-                note: returnVersionModLog.note,
-                reasonDescription: returnVersionModLog.reasonDescription,
-                userId: returnVersionModLog.userId
-              }
-            ],
-            user: null
+            startDate: licenceVersion.startDate
           }
         ]
       })
