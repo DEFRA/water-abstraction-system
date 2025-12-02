5fa64b2a6b668b16b4491d004e177f469b3010e7
jonathangoulding <goulding.jon@icloud.com>
2025-12-02 14:59:32 +0000

    Add change type and licence ref to licence version page
    https://eaflood.atlassian.net/browse/WATER-5189
    
    We recently added the licence version page (scaffolded).
    
    This change adds the change type to the licence version page, it also adds the data to render the page title and caption.

--- app/presenters/licence-versions/view.presenter.js
+++ app/presenters/licence-versions/view.presenter.js
@@ -5,19 +5,26 @@
  * @module ViewPresenter
  */
 
+const { formatLongDate } = require('../base.presenter.js')
+
 /**
  * Formats data for the `/licence-versions/{id}` page
  *
+ * @param {object} licenceVersion - the licence version with the licence
+ *
  * @returns {object} The data formatted for the view template
  */
-function go() {
+function go(licenceVersion) {
+  const { licence } = licenceVersion
+
   return {
     backLink: {
-      href: '',
-      text: 'Back'
+      href: `/system/licences/${licence.id}/history`,
+      text: 'Back to history'
     },
-    pageTitle: 'Licence version starting',
-    pageTitleCaption: 'Licence'
+    pageTitle: `Licence version starting ${formatLongDate(licenceVersion.startDate)}`,
+    pageTitleCaption: `Licence ${licence.licenceRef}`,
+    changeType: licenceVersion.administrative ? 'no licence issued' : 'licence issued'
   }
 }
 
