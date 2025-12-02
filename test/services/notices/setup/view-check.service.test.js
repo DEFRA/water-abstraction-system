66e3ea49ebc15daabb05c10f8e4e54c91d539d83
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-12-02 18:10:00 +0000

    Update ViewCheckService to use just FetchRecipients
    FetchRecipientsService is now the one place other services have to go to to fetch recipients, irrespective of whether we are checking, sending or downloading the data.

--- test/services/notices/setup/view-check.service.test.js
+++ test/services/notices/setup/view-check.service.test.js
@@ -10,39 +10,35 @@ const { expect } = Code
 
 // Test helpers
 const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
-const SessionHelper = require('../../../support/helpers/session.helper.js')
+const NoticeSessionFixture = require('../../../fixtures/notice-session.fixture.js')
 
 // Things we need to stub
-const FetchAbstractionAlertRecipientsService = require('../../../../app/services/notices/setup/abstraction-alerts/fetch-abstraction-alert-recipients.service.js')
-const FetchReturnsRecipientsService = require('../../../../app/services/notices/setup/fetch-returns-recipients.service.js')
+const FetchRecipientsService = require('../../../../app/services/notices/setup/fetch-recipients.service.js')
+const SessionModel = require('../../../../app/models/session.model.js')
 
 // Thing under test
 const ViewCheckService = require('../../../../app/services/notices/setup/view-check.service.js')
 
 describe('Notices - Setup - View Check service', () => {
-  let removeLicences
   let session
-  let testRecipients
+  let sessionUpdateStub
+  let recipient
   let yarStub
 
   beforeEach(async () => {
-    removeLicences = ''
+    recipient = RecipientsFixture.returnsNoticePrimaryUser()
 
-    session = await SessionHelper.add({
-      data: {
-        returnsPeriod: 'quarterFour',
-        removeLicences,
-        journey: 'standard',
-        noticeType: 'invitations',
-        referenceCode: 'RINV-123'
-      }
+    session = NoticeSessionFixture.standardInvitation(recipient.licence_refs[0])
+    sessionUpdateStub = Sinon.stub().resolves()
+    session.$update = sessionUpdateStub
+
+    Sinon.stub(SessionModel, 'query').returns({
+      findById: Sinon.stub().resolves(session)
     })
-
-    testRecipients = RecipientsFixture.recipients()
 
     yarStub = { flash: Sinon.stub().returns([{ title: 'Test', text: 'Notification' }]) }
 
-    Sinon.stub(FetchReturnsRecipientsService, 'go').resolves([testRecipients.primaryUser])
+    Sinon.stub(FetchRecipientsService, 'go').resolves([recipient])
   })
 
   afterEach(() => {
@@ -65,18 +61,18 @@ describe('Notices - Setup - View Check service', () => {
         title: 'Test'
       },
       page: 1,
+      pageTitle: 'Check the recipients',
+      pageTitleCaption: session.addressJourney.pageTitleCaption,
       pagination: {
         numberOfPages: 1
       },
-      pageTitle: 'Check the recipients',
-      pageTitleCaption: 'Notice RINV-123',
       readyToSend: 'Returns invitations are ready to send.',
       recipients: [
         {
-          contact: ['primary.user@important.com'],
-          licences: testRecipients.primaryUser.licence_refs,
+          contact: [recipient.email],
+          licences: recipient.licence_refs,
           method: 'Email - Primary user',
-          previewLink: `/system/notices/setup/${session.id}/preview/${testRecipients.primaryUser.contact_hash_id}`
+          previewLink: `/system/notices/setup/${session.id}/preview/${recipient.contact_hash_id}`
         }
       ],
       tableCaption: 'Showing all 1 recipients',
@@ -84,95 +80,23 @@ describe('Notices - Setup - View Check service', () => {
     })
   })
 
-  describe('the "selectedRecipients" property', () => {
-    describe('when there are no "selectedRecipients"', () => {
-      it('adds the "selectedRecipients" array to the session', async () => {
-        await ViewCheckService.go(session.id, yarStub)
+  describe('when this is the first time visiting the check page', () => {
+    it('initialises the "selectedRecipients" property in the session', async () => {
+      await ViewCheckService.go(session.id, yarStub)
 
-        const refreshedSession = await session.$query()
-
-        expect(refreshedSession).to.equal({
-          ...session,
-          data: {
-            ...session.data,
-            selectedRecipients: [testRecipients.primaryUser.contact_hash_id]
-          },
-          selectedRecipients: [testRecipients.primaryUser.contact_hash_id]
-        })
-      })
-    })
-
-    describe('when there are "selectedRecipients"', () => {
-      beforeEach(async () => {
-        session = await SessionHelper.add({
-          data: {
-            returnsPeriod: 'quarterFour',
-            removeLicences,
-            journey: 'standard',
-            noticeType: 'invitations',
-            referenceCode: 'RINV-123',
-            selectedRecipients: [testRecipients.primaryUser.contact_hash_id]
-          }
-        })
-      })
-
-      it('does not affect the "selectedRecipients"', async () => {
-        await ViewCheckService.go(session.id, yarStub)
-
-        const refreshedSession = await session.$query()
-        expect(refreshedSession).to.equal(session)
-      })
+      expect(sessionUpdateStub.called).to.be.true()
     })
   })
 
-  describe('when the journey is "alerts"', () => {
-    beforeEach(async () => {
-      session = await SessionHelper.add({
-        data: {
-          journey: 'alerts',
-          monitoringStationId: '456',
-          noticeType: 'abstractionAlerts',
-          referenceCode: 'WAA-123'
-        }
-      })
-
-      testRecipients = RecipientsFixture.alertsRecipients()
-
-      Sinon.stub(FetchAbstractionAlertRecipientsService, 'go').resolves([testRecipients.additionalContact])
+  describe('when the page has been visited before', () => {
+    beforeEach(() => {
+      session.selectedRecipients = [recipient.contact_hash_id]
     })
 
-    it('correctly presents the data', async () => {
-      const result = await ViewCheckService.go(session.id, yarStub)
+    it('leaves the "selectedRecipients" property alone', async () => {
+      await ViewCheckService.go(session.id, yarStub)
 
-      expect(result).to.equal({
-        activeNavBar: 'notices',
-        canSendNotice: true,
-        links: {
-          cancel: `/system/notices/setup/${session.id}/cancel`,
-          download: `/system/notices/setup/${session.id}/download`
-        },
-        notification: {
-          text: 'Notification',
-          title: 'Test'
-        },
-        page: 1,
-        pagination: {
-          numberOfPages: 1
-        },
-        pageTitle: 'Check the recipients',
-        pageTitleCaption: 'Notice WAA-123',
-        readyToSend: 'Abstraction alerts are ready to send.',
-        recipients: [
-          {
-            contact: ['additional.contact@important.com'],
-            licences: testRecipients.additionalContact.licence_refs,
-            method: 'Email - Additional contact',
-            previewLink: `/system/notices/setup/${session.id}/preview/${testRecipients.additionalContact.contact_hash_id}/check-alert`
-          }
-        ],
-        tableCaption: 'Showing all 1 recipients',
-        warning: null
-      })
+      expect(sessionUpdateStub.called).to.be.false()
     })
   })
 })
