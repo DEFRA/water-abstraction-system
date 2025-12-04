4526666325b6e97e96b69f379dbfc16a9dd5550c
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-12-04 12:04:59 +0000

    Add next and previous presenter (#2733)
    https://eaflood.atlassian.net/browse/WATER-5190
    
    As part of the work to add the 'previous' and 'next' pagination links to the licence versions page we have decided to make a common component to support a common implementation for future work.
    
    This change introduces the 'PaginatorPreviousAndNextPresenter', which will receive a sorted array and an id and will return the items either side of the ids index in the array.

---
+++ test/presenters/previous-and-next.presenter.test.js
@@ -0,0 +1,123 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const { generateUUID } = require('../../app/lib/general.lib.js')
+
+// Thing under test
+const PreviousAndNextPresenter = require('../../app/presenters/previous-and-next.presenter.js')
+
+describe('Previous and next presenter', () => {
+  let anchorElement
+  let arrayOfElements
+  let nextElement
+  let previousElement
+
+  beforeEach(() => {
+    anchorElement = { id: generateUUID() }
+
+    previousElement = {
+      id: generateUUID()
+    }
+
+    nextElement = {
+      id: generateUUID()
+    }
+
+    arrayOfElements = [
+      { id: generateUUID() },
+      { id: generateUUID() },
+      previousElement,
+      anchorElement,
+      nextElement,
+      { id: generateUUID() },
+      { id: generateUUID() }
+    ]
+  })
+
+  describe('when the anchor element is in the array', () => {
+    describe('and there is a "previous" and "next" element', () => {
+      it('returns the populated "previous" and "next" elements', () => {
+        const result = PreviousAndNextPresenter.go(arrayOfElements, anchorElement)
+
+        expect(result).to.equal({
+          previous: previousElement,
+          next: nextElement
+        })
+      })
+    })
+
+    describe('and there is only a "previous" element', () => {
+      beforeEach(() => {
+        arrayOfElements = [previousElement, anchorElement]
+      })
+
+      it('returns the populated "previous" element', () => {
+        const result = PreviousAndNextPresenter.go(arrayOfElements, anchorElement)
+
+        expect(result).to.equal({
+          previous: previousElement,
+          next: null
+        })
+      })
+    })
+
+    describe('and there is only a "next" element', () => {
+      beforeEach(() => {
+        arrayOfElements = [anchorElement, nextElement]
+      })
+
+      it('returns the populated "next" element', () => {
+        const result = PreviousAndNextPresenter.go(arrayOfElements, anchorElement)
+
+        expect(result).to.equal({
+          previous: null,
+          next: nextElement
+        })
+      })
+    })
+
+    describe('and there is only the anchor element', () => {
+      beforeEach(() => {
+        arrayOfElements = [anchorElement]
+      })
+
+      it('returns the "previous" and "next" elements as null', () => {
+        const result = PreviousAndNextPresenter.go(arrayOfElements, anchorElement)
+
+        expect(result).to.equal({
+          previous: null,
+          next: null
+        })
+      })
+    })
+  })
+
+  describe('when the anchor element is not in the array', () => {
+    beforeEach(() => {
+      arrayOfElements = [
+        {
+          id: generateUUID()
+        },
+        {
+          id: generateUUID()
+        }
+      ]
+    })
+
+    it('returns the "previous" and "next" as null', () => {
+      const result = PreviousAndNextPresenter.go(arrayOfElements, anchorElement)
+
+      expect(result).to.equal({
+        previous: null,
+        next: null
+      })
+    })
+  })
+})
