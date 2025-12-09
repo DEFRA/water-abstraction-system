bc29bd68a95878278ec4954f5b1fad5ffe3158f1
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-12-09 12:46:21 +0000

    Fix missing due date for paper returns with nulls (#2755)
    https://eaflood.atlassian.net/browse/WATER-5218
    
    In [Fix assigning rtn log due dates from notifications](https://github.com/DEFRA/water-abstraction-system/pull/2747), we updated how due dates are assigned to return logs when notifications are successful. This included setting them when a paper return is successful.
    
    Because of issues with additional contacts in ad-hoc notices, our testing focused on that. So, we were adding additional contacts to our paper returns and confirming that the due date was assigned when they were successful.
    
    However, we hadn't spotted that when no additional contacts are added, the `due_date` in the notification is not set. We thought it was because we could see it in the preview. However, this is something specific to the preview. We're not setting it when persisting the paper return.
    
    This change ensures we do, which means when we come to assign the due date, we have a value to work with!

--- test/presenters/notices/setup/paper-return-notifications.presenter.test.js
+++ test/presenters/notices/setup/paper-return-notifications.presenter.test.js
@@ -11,6 +11,7 @@ const { expect } = Code
 const RecipientsFixture = require('../../../fixtures/recipients.fixtures.js')
 const ReturnLogFixture = require('../../../fixtures/return-logs.fixture.js')
 const { formatLongDate } = require('../../../../app/presenters/base.presenter.js')
+const { futureDueDate } = require('../../../../app/presenters/notices/base.presenter.js')
 const { generateLicenceRef } = require('../../../support/helpers/licence.helper.js')
 
 // Thing under test
@@ -20,32 +21,57 @@ describe('Notices - Setup - Paper Return Notifications presenter', () => {
   const noticeId = '32f4cab2-ce0c-4711-aac8-fb4941f3b59a'
 
   let dueReturns
+  let dynamicLetterDueDate
   let licenceRef
   let recipients
   let session
 
   beforeEach(async () => {
+    dynamicLetterDueDate = futureDueDate('letter')
+
     licenceRef = generateLicenceRef()
 
-    const fixtureData = RecipientsFixture.recipients()
-
-    recipients = [fixtureData.licenceHolder, fixtureData.returnsTo]
-
     dueReturns = [
+      {
+        ...ReturnLogFixture.dueReturn(),
+        dueDate: null,
+        licenceRef
+      },
       {
         ...ReturnLogFixture.dueReturn(),
         licenceRef
       },
       {
         ...ReturnLogFixture.dueReturn(),
+        dueDate: new Date('2025-04-28'),
         licenceRef
+      }
+    ]
+
+    const dueReturnIds = dueReturns.map((dueReturn) => {
+      return dueReturn.returnId
+    })
+
+    const licenceHolder = RecipientsFixture.returnsNoticeLicenceHolder()
+    const returnsTo = RecipientsFixture.returnsNoticeReturnsTo()
+
+    recipients = [
+      {
+        ...licenceHolder,
+        licence_refs: [licenceRef],
+        return_log_ids: dueReturnIds
+      },
+      {
+        ...returnsTo,
+        licence_refs: [licenceRef],
+        return_log_ids: dueReturnIds
       }
     ]
 
     session = {
       licenceRef,
       dueReturns,
-      selectedReturns: [dueReturns[0].returnId]
+      selectedReturns: [dueReturns[0].returnId, dueReturns[2].returnId]
     }
   })
 
@@ -55,19 +81,19 @@ describe('Notices - Setup - Paper Return Notifications presenter', () => {
     expect(result).to.equal([
       {
         contactType: 'licence holder',
-        dueDate: dueReturns[0].dueDate,
+        dueDate: dynamicLetterDueDate,
         eventId: noticeId,
         licences: [licenceRef],
         messageRef: 'paper return',
         messageType: 'letter',
         personalisation: {
-          address_line_1: 'Mr H J Potter',
-          address_line_2: '1',
+          address_line_1: 'J Returnsholder',
+          address_line_2: '4',
           address_line_3: 'Privet Drive',
           address_line_4: 'Little Whinging',
           address_line_5: 'Surrey',
           address_line_6: 'WD25 7LR',
-          due_date: formatLongDate(dueReturns[0].dueDate),
+          due_date: formatLongDate(dynamicLetterDueDate),
           end_date: '31 March 2023',
           format_id: dueReturns[0].returnReference,
           is_two_part_tariff: false,
@@ -86,19 +112,19 @@ describe('Notices - Setup - Paper Return Notifications presenter', () => {
       },
       {
         contactType: 'returns to',
-        dueDate: dueReturns[0].dueDate,
+        dueDate: dynamicLetterDueDate,
         eventId: noticeId,
         licences: [licenceRef],
         messageRef: 'paper return',
         messageType: 'letter',
         personalisation: {
-          address_line_1: 'Mr H J Weasley',
-          address_line_2: 'INVALID ADDRESS - Needs a valid postcode or country outside the UK',
-          address_line_3: '2',
-          address_line_4: 'Privet Drive',
-          address_line_5: 'Little Whinging',
-          address_line_6: 'Surrey',
-          due_date: formatLongDate(dueReturns[0].dueDate),
+          address_line_1: 'J Returnsto',
+          address_line_2: '4',
+          address_line_3: 'Privet Drive',
+          address_line_4: 'Little Whinging',
+          address_line_5: 'Surrey',
+          address_line_6: 'WD25 7LR',
+          due_date: formatLongDate(dynamicLetterDueDate),
           end_date: '31 March 2023',
           format_id: dueReturns[0].returnReference,
           is_two_part_tariff: false,
@@ -113,6 +139,68 @@ describe('Notices - Setup - Paper Return Notifications presenter', () => {
           start_date: '1 April 2022'
         },
         returnLogIds: [dueReturns[0].returnId],
+        status: 'pending'
+      },
+      {
+        contactType: 'licence holder',
+        dueDate: dueReturns[2].dueDate,
+        eventId: noticeId,
+        licences: [licenceRef],
+        messageRef: 'paper return',
+        messageType: 'letter',
+        personalisation: {
+          address_line_1: 'J Returnsholder',
+          address_line_2: '4',
+          address_line_3: 'Privet Drive',
+          address_line_4: 'Little Whinging',
+          address_line_5: 'Surrey',
+          address_line_6: 'WD25 7LR',
+          due_date: formatLongDate(dueReturns[2].dueDate),
+          end_date: '31 March 2023',
+          format_id: dueReturns[2].returnReference,
+          is_two_part_tariff: false,
+          licence_ref: licenceRef,
+          naldAreaCode: 'MIDLT',
+          purpose: 'Mineral Washing',
+          qr_url: dueReturns[2].returnId,
+          region_code: '1',
+          region_name: 'North West',
+          returns_frequency: 'month',
+          site_description: 'BOREHOLE AT AVALON',
+          start_date: '1 April 2022'
+        },
+        returnLogIds: [dueReturns[2].returnId],
+        status: 'pending'
+      },
+      {
+        contactType: 'returns to',
+        dueDate: dueReturns[2].dueDate,
+        eventId: noticeId,
+        licences: [licenceRef],
+        messageRef: 'paper return',
+        messageType: 'letter',
+        personalisation: {
+          address_line_1: 'J Returnsto',
+          address_line_2: '4',
+          address_line_3: 'Privet Drive',
+          address_line_4: 'Little Whinging',
+          address_line_5: 'Surrey',
+          address_line_6: 'WD25 7LR',
+          due_date: formatLongDate(dueReturns[2].dueDate),
+          end_date: '31 March 2023',
+          format_id: dueReturns[2].returnReference,
+          is_two_part_tariff: false,
+          licence_ref: licenceRef,
+          naldAreaCode: 'MIDLT',
+          purpose: 'Mineral Washing',
+          qr_url: dueReturns[2].returnId,
+          region_code: '1',
+          region_name: 'North West',
+          returns_frequency: 'month',
+          site_description: 'BOREHOLE AT AVALON',
+          start_date: '1 April 2022'
+        },
+        returnLogIds: [dueReturns[2].returnId],
         status: 'pending'
       }
     ])
