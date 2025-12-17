93d6bb95c98a3113003fd3f131c754e63c7dd82a
Jason Claxton <jason.claxton@defra.gov.uk>
2025-12-17 14:39:19 +0000

    Fix linting

--- db/migrations/public/20251217142724_alter-return-submissions-view.js
+++ db/migrations/public/20251217142724_alter-return-submissions-view.js
@@ -27,20 +27,22 @@ exports.up = function (knex) {
 exports.down = function (knex) {
   return knex.schema.dropViewIfExists(viewName).createView(viewName, (view) => {
     view.as(
-      knex('versions').withSchema('returns').select([
-        'version_id AS id',
-        'return_id as return_log_id',
-        'user_id',
-        'user_type',
-        'version_number as version',
-        'metadata',
-        'nil_return',
-        'current',
-        'notes',
-        'created_by',
-        'created_at',
-        'updated_at'
-      ])
+      knex('versions')
+        .withSchema('returns')
+        .select([
+          'version_id AS id',
+          'return_id as return_log_id',
+          'user_id',
+          'user_type',
+          'version_number as version',
+          'metadata',
+          'nil_return',
+          'current',
+          'notes',
+          'created_by',
+          'created_at',
+          'updated_at'
+        ])
     )
   })
 }
