504525abb4e0b635ba93485bb3c80b4731ea63bc
Jason Claxton <jason.claxton@defra.gov.uk>
2025-12-17 16:57:31 +0000

    Fix linting issue

--- app/services/return-logs/fetch-return-log.service.js
+++ app/services/return-logs/fetch-return-log.service.js
@@ -83,13 +83,7 @@ async function _fetch(returnId, selectedReturnSubmission) {
 
 async function _fetchAllReturnSubmissions(returnId) {
   return ReturnSubmissionModel.query()
-    .select([
-      'createdAt',
-      'id',
-      'notes',
-      'version',
-      'userId'
-    ])
+    .select(['createdAt', 'id', 'notes', 'version', 'userId'])
     .where('returnLogId', returnId)
     .orderBy('version', 'desc')
 }
