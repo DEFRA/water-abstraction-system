cefdb0a78ed94d50b613d742bd6e8a4e98ffc298
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2025-12-22 17:12:08 +0000

    Update customer licence page to show customer licences (#2799)
    https://eaflood.atlassian.net/browse/WATER-5384
    
    We recently scaffolded the customer licences page.
    
    This change introduces the logic to show the customers' licence on the page. This is based on the legacy page, and the fetch request represents what the legacy page is doing.

--- app/presenters/customers/licences.presenter.js
+++ app/presenters/customers/licences.presenter.js
@@ -5,22 +5,38 @@
  * @module LicencesPresenter
  */
 
+const { formatLongDate } = require('../base.presenter.js')
+
 /**
  * Formats data for the 'customers/{id}/licences' page
  *
  * @param {module:CompanyModel} customer - The customer from the companies table
+ * @param {object} licences - the licences for the customer
  *
  * @returns {object} The data formatted for the view template
  */
-function go(customer) {
+function go(customer, licences) {
   return {
     backLink: {
       href: '/',
       text: 'Back to search'
     },
     pageTitleCaption: customer.name,
-    pageTitle: 'Licences'
+    pageTitle: 'Licences',
+    licences: _licences(licences)
   }
+}
+
+function _licences(licences) {
+  return licences.map((licence) => {
+    return {
+      startDate: formatLongDate(licence.licenceDocument.startDate),
+      endDate: formatLongDate(licence.licenceDocument.endDate),
+      licenceRef: licence.licenceDocument.licence.licenceRef,
+      licenceName: licence.licenceDocument.licence.$licenceName(),
+      id: licence.licenceDocument.licence.id
+    }
+  })
 }
 
 module.exports = {
