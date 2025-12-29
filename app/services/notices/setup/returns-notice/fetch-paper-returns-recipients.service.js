ce908abbac8638fa0e04ede75f86b442d6bbe768
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-12-29 00:18:45 +0000

    Update FetchPaperReturnsRecipientsService
    To be consistent with the new FetchReturnsInvitationRecipients and FetchReturnsReminderRecipients services, and to ensure we support new fields we'll be adding to the download, we need to update FetchPaperReturnsRecipientsService to also ensure `notificationDueDate` is set
    by the service.
    
    It is a little different in that the presenter still needs to determine the due date when it comes to _sending_ the notification, because when sending it is not until later that the recipients and return logs are combined.
    
    But when fetching the data for the download, the results are per recipient and return log combination, so we can set the value.

--- app/services/notices/setup/returns-notice/fetch-paper-returns-recipients.service.js
+++ app/services/notices/setup/returns-notice/fetch-paper-returns-recipients.service.js
@@ -7,6 +7,7 @@
 
 const GenerateReturnLogsByIdQueryService = require('./generate-return-logs-by-id-query.service.js')
 const GenerateRecipientsQueryService = require('./generate-recipients-query.service.js')
+const { futureDueDate } = require('../../../../presenters/notices/base.presenter.js')
 
 const { db } = require('../../../../../db/db.js')
 
@@ -26,7 +27,27 @@ async function go(session, download) {
 
   const { rows } = await db.raw(query, bindings)
 
+  _applyNotificationDueDate(rows, download)
+
   return rows
+}
+
+function _applyNotificationDueDate(rows, download) {
+  for (const row of rows) {
+    const { due_date: dueDate } = row
+
+    // We can set the notificationDueDate for the download because each row is DISTINCT to a recipient and return log.
+    // We can't set it when not downloading because a recipient might be linked to multiple return logs and unlike the
+    // other notice types, when we send the notification we create one for each recipient/return log combination. So
+    // there is no recipient-level notification date. But that's fine because the /check page only shows the recipient
+    // once, and the PaperReturnNotificationsPresenter handles determining the notificationDueDate for each
+    // notification.
+    if (download) {
+      row.notificationDueDate = dueDate ?? futureDueDate('letter')
+    } else {
+      row.notificationDueDate = null
+    }
+  }
 }
 
 module.exports = {
