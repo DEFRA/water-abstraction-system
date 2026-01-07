4d8015b058c1fe705453c33722551e6212d072e9
jonathangoulding <goulding.jon@icloud.com>
2026-01-07 12:00:35 +0000

    Add Billing account data to the Customer billing accounts page
    https://eaflood.atlassian.net/browse/WATER-5385
    
    This change adds the services to fetch and present the billing account page data for the customer billing account page.
    
    Pagination has beed added per standard practice.

--- app/services/customers/billing-accounts.service.js
+++ app/services/customers/billing-accounts.service.js
@@ -7,24 +7,38 @@
  */
 
 const BillingAccountsPresenter = require('../../presenters/customers/billing-accounts.presenter.js')
+const FetchBillingAccountsService = require('./fetch-billing-accounts.service.js')
 const FetchCustomerService = require('./fetch-customer.service.js')
+const PaginatorPresenter = require('../../presenters/paginator.presenter.js')
 
 /**
  * Orchestrates fetching and presenting the data for the 'customers/{id}/billing-accounts' page
  *
  * @param {string} customerId - the UUID of the customer
+ * @param {number} page - The current page for the pagination service
  *
  * @returns {Promise<object>} The data formatted for the view template
  */
-async function go(customerId) {
+async function go(customerId, page) {
   const customer = await FetchCustomerService.go(customerId)
 
-  const pageData = BillingAccountsPresenter.go(customer)
+  const { billingAccounts, pagination } = await FetchBillingAccountsService.go(customerId, page)
+
+  const pageData = BillingAccountsPresenter.go(customer, billingAccounts)
+
+  const paginationData = PaginatorPresenter.go(
+    pagination.total,
+    Number(page),
+    `/system/customers/${customerId}/billing-accounts`,
+    billingAccounts.length,
+    'billing accounts'
+  )
 
   return {
     activeNavBar: 'search',
     activeSecondaryNav: 'billing-accounts',
-    ...pageData
+    ...pageData,
+    pagination: paginationData
   }
 }
 
--- test/services/customers/billing-accounts.service.test.js
+++ test/services/customers/billing-accounts.service.test.js
@@ -12,18 +12,34 @@ const { expect } = Code
 const CustomersFixtures = require('../../fixtures/customers.fixture.js')
 
 // Things we need to stub
+const FetchBillingAccountsService = require('../../../app/services/customers/fetch-billing-accounts.service.js')
 const FetchCustomerService = require('../../../app/services/customers/fetch-customer.service.js')
 
 // Thing under test
 const BillingAccountsService = require('../../../app/services/customers/billing-accounts.service.js')
+const CustomerFixtures = require('../../fixtures/customers.fixture.js')
 
 describe('Customers - Billing Accounts Service', () => {
+  let billingAccount
+  let billingAccounts
   let customer
+  let page
 
   beforeEach(async () => {
     customer = CustomersFixtures.customer()
 
+    billingAccounts = CustomerFixtures.billingAccounts()
+
+    billingAccount = billingAccounts[0]
+
     Sinon.stub(FetchCustomerService, 'go').returns(customer)
+
+    Sinon.stub(FetchBillingAccountsService, 'go').returns({
+      billingAccounts,
+      pagination: { total: 1 }
+    })
+
+    page = 1
   })
 
   afterEach(() => {
@@ -32,7 +48,7 @@ describe('Customers - Billing Accounts Service', () => {
 
   describe('when called', () => {
     it('returns page data for the view', async () => {
-      const result = await BillingAccountsService.go(customer.id)
+      const result = await BillingAccountsService.go(customer.id, page)
 
       expect(result).to.equal({
         activeNavBar: 'search',
@@ -41,8 +57,27 @@ describe('Customers - Billing Accounts Service', () => {
           href: '/',
           text: 'Back to search'
         },
+        billingAccounts: [
+          {
+            accountNumber: billingAccount.accountNumber,
+            address: [
+              'Tyrell Corporation',
+              'Environment Agency',
+              'Horizon House',
+              'Deanery Road',
+              'Bristol',
+              'BS1 5AH'
+            ],
+            id: billingAccount.id,
+            link: `/system/billing-accounts/${billingAccount.id}?company-id=${billingAccount.company.id}`
+          }
+        ],
         pageTitle: 'Billing accounts',
-        pageTitleCaption: 'Tyrell Corporation'
+        pageTitleCaption: 'Tyrell Corporation',
+        pagination: {
+          numberOfPages: 1,
+          showingMessage: 'Showing all 1 billing accounts'
+        }
       })
     })
   })
