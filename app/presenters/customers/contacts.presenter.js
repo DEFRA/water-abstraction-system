db680bcdc8445884a4bf69f2c9800887bfb8eb50
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-01-08 18:35:45 +0000

    Update customer contacts page to show buttons (#2831)
    * Update customer contacts page to show buttons
    
    https://eaflood.atlassian.net/browse/WATER-5385
    
    This change adds the create/remove contact buttons to the page.
    
    This has some complexity in constructing the URl that we have not seen in the legacy code before.
    
    When the legacy UI navigates to the create page, it uses a 'key' from the URL (https://github.com/DEFRA/water-abstraction-ui/blob/1ffa5f2a9ac481b306506776d43cd63c4ea9143c/src/internal/modules/customers/controllers.js#L260_.
    
    The legacy UI builds this key like so:
    ```js
     const key = `newCompanyContact.${request.params.companyId}.${request.defra.userId}`
    ```
    
    It then uses this key for a redirect:
    ```js
     redirectPath: `/customer/${request.params.companyId}?newContactKey=${key}`,
     ```
    
     Why it does this ? We do not know. But we need to use the create contact page so we have implemented the URl as is.
    
    Permissions will be added in a later change.

--- app/presenters/customers/contacts.presenter.js
+++ app/presenters/customers/contacts.presenter.js
@@ -9,17 +9,38 @@
  * Formats data for the 'customers/{id}/contacts' page
  *
  * @param {module:CompanyModel} customer - The customer from the companies table
+ * @param {object} auth - The auth object taken from `request.auth` containing user details
  *
  * @returns {object} The data formatted for the view template
  */
-function go(customer) {
+function go(customer, auth) {
   return {
     backLink: {
       href: '/',
       text: 'Back to search'
     },
+    links: _links(customer, auth),
     pageTitle: 'Contacts',
     pageTitleCaption: customer.name
+  }
+}
+
+/**
+ * When the legacy UI navigates to the create page, it uses a 'key' from the URL (https://github.com/DEFRA/water-abstraction-ui/blob/1ffa5f2a9ac481b306506776d43cd63c4ea9143c/src/internal/modules/customers/controllers.js#L260_.
+ *
+ * The legacy UI builds this key like so:
+ * ```js
+ *  const key = `newCompanyContact.${request.params.companyId}.${request.defra.userId}`
+ * ```
+ * We replicate this logic.
+ *
+ * @private
+ */
+function _links(customer, auth) {
+  return {
+    createContact: `/contact-entry/newCompanyContact.${customer.id}.${auth.credentials.user.id}/select-contact`,
+
+    removeContact: `/customer/${customer.id}/contacts/remove`
   }
 }
 
--- test/presenters/customers/contacts.presenter.test.js
+++ test/presenters/customers/contacts.presenter.test.js
@@ -14,20 +14,29 @@ const CustomersFixtures = require('../../fixtures/customers.fixture.js')
 const ContactsPresenter = require('../../../app/presenters/customers/contacts.presenter.js')
 
 describe('Customers - Contacts Presenter', () => {
+  const userId = '1000'
+
+  let auth
   let customer
 
   beforeEach(() => {
+    auth = { credentials: { user: { id: userId } } }
+
     customer = CustomersFixtures.customer()
   })
 
   describe('when called', () => {
     it('returns page data for the view', () => {
-      const result = ContactsPresenter.go(customer)
+      const result = ContactsPresenter.go(customer, auth)
 
       expect(result).to.equal({
         backLink: {
           href: '/',
           text: 'Back to search'
+        },
+        links: {
+          createContact: `/contact-entry/newCompanyContact.${customer.id}.${userId}/select-contact`,
+          removeContact: `/customer/${customer.id}/contacts/remove`
         },
         pageTitle: 'Contacts',
         pageTitleCaption: 'Tyrell Corporation'
