a1ff716ed374a6878680bf0ca91416d168f94ec9
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-01-20 15:03:19 +0000

    Update the customer contact manage page to only render links for authorised users (#2882)
    * Update the customer contact manage page to only render links for authorised users
    
    https://eaflood.atlassian.net/browse/WATER-5457
    
    A user with the 'hof_notifications' role can change contact details and remove the contact, those without cannot see the change link or remove contact button.
    
    This change adds a check on the user role to determine if we show the change link and the remove button.
    
    The remove button has been added as part of this change, but the page does not exist yet.

--- app/services/customers-contacts/view-manage.service.js
+++ app/services/customers-contacts/view-manage.service.js
@@ -9,15 +9,17 @@
 const FetchCompanyContactService = require('./fetch-company-contact.service.js')
 const FetchCustomerService = require('../customers/fetch-customer.service.js')
 const ViewManagePresenter = require('../../presenters/customers-contacts/view-manage.presenter.js')
+const { userRoles } = require('../../presenters/licences/base-licences.presenter.js')
 
 /**
  * Orchestrates fetching and presenting the data for the '/customers-contacts/{id}/manage' page
  *
  * @param {string} id - the UUID of the customer contact
+ * @param {object} auth - The auth object taken from `request.auth` containing user details
  *
  * @returns {Promise<object>} The data formatted for the view template
  */
-async function go(id) {
+async function go(id, auth) {
   const companyContact = await FetchCompanyContactService.go(id)
 
   const company = await FetchCustomerService.go(companyContact.companyId)
@@ -25,8 +27,9 @@ async function go(id) {
   const pageData = ViewManagePresenter.go(company, companyContact)
 
   return {
+    ...pageData,
     activeNavBar: 'search',
-    ...pageData
+    roles: userRoles(auth)
   }
 }
 
