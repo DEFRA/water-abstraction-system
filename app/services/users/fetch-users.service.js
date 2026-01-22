9f08dd121362d92ac47b2803676b4801ca36c7f5
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-01-22 09:23:16 +0000

    Add the fetch service that backs the view users pg
    Includes what is needed to support filtering.

---
+++ app/services/users/fetch-users.service.js
@@ -0,0 +1,182 @@
+'use strict'
+
+/**
+ * Fetches the selected page of users for /users
+ * @module FetchUsersService
+ */
+
+const UserModel = require('../../models/user.model.js')
+const { userPermissions } = require('../../lib/static-lookups.lib.js')
+
+const DatabaseConfig = require('../../../config/database.config.js')
+
+/**
+ * Fetches the selected page of users for /users
+ *
+ * We can filter the results by
+ *
+ * - **Email** - A case-insensitive `LIKE` search on the username so partial values can be used
+ * - **Permissions** - Only applies to internal users so if set, we automatically exclude external users. Then
+ * _Basic access_ is a user not linked to any groups. A _Digitise!_ user is linked to the 'nps' group and has either the
+ * `ar_approver` or `ar_user` role. All other permissions the user is linked to the specified group. This includes users
+ * linked to 'nps' but that have no user roles.
+ * - **Status** - Where the user is disabled, enabled (has logged in) or awaiting (enabled but has not yet logged in)
+ * - **Type** - The application the user belongs to i.e. internal or external
+ *
+ * @param {object} filters - an object containing the different filters to apply to the query
+ * @param {number} page - The current page for the pagination service
+ *
+ * @returns {Promise<module:UserModel[]>} an array of users that match the selected 'page in the data
+ */
+async function go(filters, page) {
+  const query = _fetchQuery()
+
+  _applyFilters(query, filters)
+
+  query.orderBy('users.username', 'asc').page(page - 1, DatabaseConfig.defaultPageSize)
+
+  query.whereRaw('')
+
+  return query
+}
+
+function _applyFilters(query, filters) {
+  const { email, permissions, status, type } = filters
+
+  if (email) {
+    query.whereILike('users.username', `%${email}%`)
+  }
+
+  if (type) {
+    query.where('users.application', type)
+  }
+
+  if (permissions) {
+    _applyPermissionsFilter(query, permissions)
+  }
+
+  if (status) {
+    _applyStatusFilter(query, status)
+  }
+}
+
+function _applyPermissionsFilter(query, permissions) {
+  const permissionDetails = userPermissions[permissions]
+
+  // Permissions only apply to internal users, so we can focus on those only
+  query.where('users.application', 'water_admin')
+
+  if (['billing_and_data', 'environment_officer', 'psc', 'super', 'wirs'].includes(permissions)) {
+    _applyStandardPermissionFilter(query, permissionDetails.groups)
+
+    return
+  }
+
+  if (permissions === 'basic') {
+    _applyBasicPermissionFilter(query)
+
+    return
+  }
+
+  if (permissions === 'nps') {
+    _applyNpsPermissionFilter(query, permissionDetails.groups)
+
+    return
+  }
+
+  _applyDigitisePermissionFilter(query, permissionDetails)
+}
+
+/**
+ * The basic permission is that the user is not linked to _any_ groups
+ *
+ * @private
+ */
+function _applyBasicPermissionFilter(query) {
+  query.whereRaw(`NOT EXISTS (SELECT 1 FROM public.user_groups ug WHERE ug.user_id = users.id)`)
+}
+
+/**
+ * The digitise permission is that the user is linked to the 'nps' group and has either the `ar_approver` or
+ * `ar_user` role.
+ *
+ * So, we can reuse `_applyStandardPermissionFilter` to check for the group link, then add an additional condition to
+ * check for the roles.
+ *
+ * @private
+ */
+function _applyDigitisePermissionFilter(query, permissionDetails) {
+  const { groups, roles } = permissionDetails
+
+  _applyStandardPermissionFilter(query, groups)
+
+  query.whereRaw(
+    `EXISTS (
+  SELECT 1 FROM public.user_roles ur
+  INNER JOIN public."roles" r ON r.id = ur.role_id
+  WHERE ur.user_id = users.id AND r."role" IN (?)
+)
+  `,
+    roles
+  )
+}
+
+/**
+ * The nps permission is that the user is linked to the 'nps' group but does NOT have either the `ar_approver` or
+ * `ar_user` role.
+ *
+ * So, we can reuse `_applyStandardPermissionFilter` to check for the group link, then add an additional condition to
+ * exclude those with those user roles.
+ *
+ * @private
+ */
+function _applyNpsPermissionFilter(query, groups) {
+  _applyStandardPermissionFilter(query, groups)
+
+  query.whereRaw(
+    `NOT EXISTS (
+  SELECT 1 FROM public.user_roles ur
+  INNER JOIN public."roles" r ON r.id = ur.role_id
+  WHERE ur.user_id = users.id AND r."role" IN ('ar_approver', 'ar_user')
+)
+  `
+  )
+}
+
+function _applyStandardPermissionFilter(query, groups) {
+  query.whereRaw(
+    `EXISTS (
+  SELECT 1 FROM public.user_groups ug
+  INNER JOIN public."groups" g ON g.id = ug.group_id
+  WHERE ug.user_id = users.id AND g."group" IN (?)
+)
+  `,
+    groups
+  )
+}
+
+function _applyStatusFilter(query, status) {
+  if (status === 'disabled') {
+    query.where('users.enabled', false)
+
+    return
+  }
+
+  // The remaining filters relate to enabled users
+  query.where('users.enabled', true)
+
+  if (status === 'awaiting') {
+    query.whereNull('users.lastLogin')
+  } else {
+    // Status is assumed to be `enabled`
+    query.whereNotNull('users.lastLogin')
+  }
+}
+
+function _fetchQuery() {
+  return UserModel.query().select(['id', 'username']).modify('status').modify('permissions')
+}
+
+module.exports = {
+  go
+}
---
+++ test/services/users/fetch-users.service.test.js
@@ -0,0 +1,409 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Things we need to stub
+const databaseConfig = require('../../../config/database.config.js')
+
+// Test helpers
+const GroupModel = require('../../../app/models/group.model.js')
+const RoleModel = require('../../../app/models/role.model.js')
+const UserHelper = require('../../support/helpers/user.helper.js')
+const UserModel = require('../../../app/models/user.model.js')
+const UsersFixture = require('../../fixtures/users.fixture.js')
+
+// Thing under test
+const FetchUsersService = require('../../../app/services/users/fetch-users.service.js')
+
+// NOTE: The users are seeded as part of setting up the test database, along with with their groups and roles. So,
+// unlike other fetch tests we don't create any test records and assert they are in our results as we already have
+// sufficient data to work with.
+describe('Notices - Fetch Users service', () => {
+  // We refer to this in all our tests. Pulling it in here means we only do it once
+  const userSeedData = UserHelper.data
+
+  let filters
+  let pageNumber
+
+  beforeEach(() => {
+    // NOTE: _filters() generates an empty filters object as used by the services that call FetchNotices when no filter
+    // has been applied by the user
+    filters = _filters()
+
+    pageNumber = 1
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when no filter is applied', () => {
+    beforeEach(() => {
+      // NOTE: We set the default page size to 1000 to ensure we get all records and avoid failed tests when run as
+      // part of the full suite, and the risk our test record is returned in the second page of results.
+      Sinon.stub(databaseConfig, 'defaultPageSize').value(1000)
+    })
+
+    it('returns all users ordered by their username (email) ascending', async () => {
+      const { results, total } = await FetchUsersService.go(filters, pageNumber)
+
+      // Assert the total is equal to or greater than our seeded count (other tests may create records) so we don't
+      // assert exactly
+      expect(total).to.be.at.least(userSeedData.length)
+
+      // Assert the results contain all our seeded users
+      for (let i = 0; i < userSeedData.length; i++) {
+        expect(results).contains(_transformToResult(UsersFixture.user(i)))
+      }
+    })
+  })
+
+  describe('when a filter is applied', () => {
+    beforeEach(() => {
+      Sinon.stub(databaseConfig, 'defaultPageSize').value(1000)
+    })
+
+    describe('and "Email" has been set', () => {
+      beforeEach(() => {
+        // NOTE: We use uppercase to test that the service is using case insensitive LIKE where clause
+        filters.email = 'LEE'
+      })
+
+      it('returns the matching users', async () => {
+        const { results } = await FetchUsersService.go(filters, pageNumber)
+
+        // Assert the results contain only users with 'lee' in their username
+        for (let i = 0; i < userSeedData.length; i++) {
+          if (userSeedData[i].username.toUpperCase().includes(filters.email)) {
+            expect(results).contains(_transformToResult(UsersFixture.user(i)))
+          }
+        }
+      })
+
+      it('excludes those that do not match', async () => {
+        const { results } = await FetchUsersService.go(filters, pageNumber)
+
+        // Assert the results do not contain any users with 'lee' on their username
+        for (let i = 0; i < userSeedData.length; i++) {
+          if (!userSeedData[i].username.toUpperCase().includes(filters.email)) {
+            expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+          }
+        }
+      })
+    })
+
+    // NOTE: Because 'permissions' can be based on one or more properties depending on the value selected, we assert the
+    // fetch can handle all possible scenarios (we don't have to check every permission because most are evaluated in
+    // the same way)
+    describe('and "Permissions" has been set', () => {
+      // An internal user has "basic access" permissions if they are not linked to any groups
+      describe('as "Basic access"', () => {
+        beforeEach(() => {
+          filters.permissions = 'basic'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded basic user
+          expect(results).contains(_transformToResult(UsersFixture.basicAccess()))
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our other seeded users
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (userSeedData[i].username !== 'basic.access@wrls.gov.uk') {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+
+      // An internal user has "[Group]" permissions if they are linked to that group and have no `ar_*` user roles. We
+      // use billing and data as an example of this
+      describe('as "Billing and data"', () => {
+        beforeEach(() => {
+          filters.permissions = 'billing_and_data'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded billing and data user
+          expect(results).contains(_transformToResult(UsersFixture.billingAndData()))
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our other seeded users
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (userSeedData[i].username !== 'billing.data@wrls.gov.uk') {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+
+      // We check the "National Permitting Service" permission, because for a user to have this they must be linked to
+      // the nps group, but NOT any `ar_*` user roles
+      describe('as "National Permitting Service"', () => {
+        beforeEach(() => {
+          filters.permissions = 'nps'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded national permitting service user
+          expect(results).contains(_transformToResult(UsersFixture.nationalPermittingService()))
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our other seeded users
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (userSeedData[i].username !== 'national.permitting.service@wrls.gov.uk') {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+
+      // An internal user has "National Permitting Service and Digitise! approver" permissions if they are linked to the
+      // group 'nps' and have the `ar_approver` user role.
+      describe('as "National Permitting Service and Digitise! approver"', () => {
+        beforeEach(() => {
+          filters.permissions = 'nps_ar_approver'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded national permitting service user
+          expect(results).contains(_transformToResult(UsersFixture.digitiseApprover()))
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our other seeded users
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (userSeedData[i].username !== 'digitise.approver@wrls.gov.uk') {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+
+      // An internal user has "National Permitting Service and Digitise! editor" permissions if they are linked to the
+      // group 'nps' and have the `ar_user` user role.
+      describe('as "National Permitting Service and Digitise! editor"', () => {
+        beforeEach(() => {
+          filters.permissions = 'nps_ar_user'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded national permitting service user
+          expect(results).contains(_transformToResult(UsersFixture.digitiseEditor()))
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our other seeded users
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (userSeedData[i].username !== 'digitise.editor@wrls.gov.uk') {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+    })
+
+    // NOTE: Because 'status' can be based on one or more properties depending on the value selected, we assert the
+    // fetch can handle all possible status values
+    describe('and "Status" has been set', () => {
+      describe('as "Awaiting"', () => {
+        beforeEach(() => {
+          filters.status = 'awaiting'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded users where enabled is true and lastLogin is null
+          for (let i = 0; i < userSeedData.length; i++) {
+            const userData = userSeedData[i]
+
+            if (userData.enabled && !userData.lastLogin) {
+              expect(results).contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our seeded users where enabled is false, or enabled is true but lastLogin
+          // is NOT null
+          for (let i = 0; i < userSeedData.length; i++) {
+            const userData = userSeedData[i]
+
+            if (!userData.enabled || userData.lastLogin) {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+
+      describe('as "Disabled"', () => {
+        beforeEach(() => {
+          filters.status = 'disabled'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded users where enabled is false
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (!userSeedData[i].enabled) {
+              expect(results).contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our seeded users where enabled is true
+          for (let i = 0; i < userSeedData.length; i++) {
+            if (userSeedData[i].enabled) {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+
+      describe('as "Enabled"', () => {
+        beforeEach(() => {
+          filters.status = 'enabled'
+        })
+
+        it('returns the matching users', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results contain our seeded users where enabled is true and lastLogin is NOT null
+          for (let i = 0; i < userSeedData.length; i++) {
+            const userData = userSeedData[i]
+
+            if (userData.enabled && userData.lastLogin) {
+              expect(results).contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+
+        it('excludes those that do not match', async () => {
+          const { results } = await FetchUsersService.go(filters, pageNumber)
+
+          // Assert the results do not contain our seeded users where enabled is false, or enabled is true but lastLogin
+          // is null
+          for (let i = 0; i < userSeedData.length; i++) {
+            const userData = userSeedData[i]
+
+            if (!userData.enabled || !userData.lastLogin) {
+              expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+            }
+          }
+        })
+      })
+    })
+
+    describe('and "Type" has been set', () => {
+      beforeEach(() => {
+        filters.type = 'water_vml'
+      })
+
+      it('returns the matching users', async () => {
+        const { results } = await FetchUsersService.go(filters, pageNumber)
+
+        // Assert the results contain our seeded external users
+        for (let i = 0; i < userSeedData.length; i++) {
+          if (userSeedData[i].application === 'water_vml') {
+            expect(results).contains(_transformToResult(UsersFixture.user(i)))
+          }
+        }
+      })
+
+      it('excludes those that do not match', async () => {
+        const { results } = await FetchUsersService.go(filters, pageNumber)
+
+        // Assert the results do not contain our seeded internal users
+        for (let i = 0; i < userSeedData.length; i++) {
+          if (userSeedData[i].application !== 'water_vml') {
+            expect(results).not.contains(_transformToResult(UsersFixture.user(i)))
+          }
+        }
+      })
+    })
+  })
+
+  describe('when the results are paginated', () => {
+    beforeEach(() => {
+      pageNumber = 2
+
+      // NOTE: We know we create 3 records so we set the value to 2 to ensure the results are paginated
+      Sinon.stub(databaseConfig, 'defaultPageSize').value(2)
+    })
+
+    it('can return the selected page', async () => {
+      const result = await FetchUsersService.go(filters, pageNumber)
+
+      expect(result.results).not.to.be.empty()
+    })
+  })
+})
+
+function _filters() {
+  return {
+    email: null,
+    permissions: null,
+    status: null,
+    type: null
+  }
+}
+
+function _transformToResult(userInstance) {
+  const { application, enabled, id, groups, lastLogin, roles, username } = userInstance
+
+  return UserModel.fromJson({
+    application,
+    enabled,
+    id,
+    groups: groups.map((group) => {
+      return GroupModel.fromJson({
+        group: group.group,
+        id: group.id
+      })
+    }),
+    lastLogin,
+    roles: roles.map((role) => {
+      return RoleModel.fromJson({
+        id: role.id,
+        role: role.role
+      })
+    }),
+    username
+  })
+}
