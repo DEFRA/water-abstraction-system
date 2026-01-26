8d13cfe4ffe60e73d843bb34c81810b5dd33813e
jonathangoulding <goulding.jon@icloud.com>
2026-01-26 15:50:28 +0000

    chore: pre pr check

--- test/services/companies/fetch-company-contacts.service.test.js
+++ test/services/companies/fetch-company-contacts.service.test.js
@@ -4,7 +4,7 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, before } = (exports.lab = Lab.script())
+const { describe, it, before, after, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
@@ -14,38 +14,50 @@ const ContactHelper = require('../../support/helpers/contact.helper.js')
 const LicenceRoleHelper = require('../../support/helpers/licence-role.helper.js')
 
 // Thing under test
-const FetchContactsService = require('../../../app/services/companies/fetch-company-contacts.service.js')
+const FetchCompanyContactsService = require('../../../app/services/companies/fetch-company-contacts.service.js')
 
 describe('Companies - Fetch Company Contacts service', () => {
+  let additionalCompanyContact
   let company
   let companyContact
   let contact
   let licenceRole
 
-  describe('when there is a company contact', () => {
-    before(async () => {
-      company = await CompanyHelper.add()
+  before(async () => {
+    contact = await ContactHelper.add()
+    company = await CompanyHelper.add()
 
-      contact = await ContactHelper.add()
+    licenceRole = LicenceRoleHelper.select('additionalContact')
 
-      licenceRole = LicenceRoleHelper.select('additionalContact')
-
-      companyContact = await CompanyContactHelper.add({
-        companyId: company.id,
-        contactId: contact.id,
-        licenceRoleId: licenceRole.id
-      })
-
-      // Add additional contact - not related to the company
-      const additionalContact = await ContactHelper.add()
-
-      await CompanyContactHelper.add({
-        contactId: additionalContact.id
-      })
+    companyContact = await CompanyContactHelper.add({
+      companyId: company.id,
+      contactId: contact.id,
+      licenceRoleId: licenceRole.id
     })
 
+    // Add additional contact - not related to the company
+    const additionalContact = await ContactHelper.add()
+
+    await CompanyContactHelper.add({
+      contactId: additionalContact.id
+    })
+  })
+
+  afterEach(async () => {
+    await companyContact.$query().delete()
+
+    if (additionalCompanyContact) {
+      await additionalCompanyContact.$query().delete()
+    }
+  })
+
+  after(async () => {
+    await contact.$query().delete()
+  })
+
+  describe('when there is a company contact', () => {
     it('returns the matching company', async () => {
-      const result = await FetchContactsService.go(company.id)
+      const result = await FetchCompanyContactsService.go(company.id)
 
       expect(result).to.equal({
         companyContacts: [
--- test/services/company-contacts/fetch-company-contact.service.test.js
+++ test/services/company-contacts/fetch-company-contact.service.test.js
@@ -4,7 +4,7 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, before, beforeEach, after, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
@@ -15,87 +15,120 @@ const ContactHelper = require('../../support/helpers/contact.helper.js')
 const FetchCompanyContactService = require('../../../app/services/company-contacts/fetch-company-contact.service.js')
 
 describe('Company Contacts - Fetch Company Contact service', () => {
+  let additionalCompanyContact
   let companyContact
   let contact
 
-  describe('when there is a company contact', () => {
-    describe('when a company contact is not marked for "abstractionAlerts"', () => {
-      beforeEach(async () => {
-        contact = await ContactHelper.add()
+  before(async () => {
+    contact = await ContactHelper.add()
+  })
 
-        companyContact = await CompanyContactHelper.add({
-          contactId: contact.id
+  afterEach(async () => {
+    await companyContact.$query().delete()
+
+    if (additionalCompanyContact) {
+      await additionalCompanyContact.$query().delete()
+    }
+  })
+
+  after(async () => {
+    await contact.$query().delete()
+  })
+
+  describe('when there is a company contact', () => {
+    describe('and it is the only "additional contact" for the company', () => {
+      describe('and it is not marked for "abstractionAlerts"', () => {
+        beforeEach(async () => {
+          companyContact = await CompanyContactHelper.add({
+            abstractionAlerts: false,
+            contactId: contact.id
+          })
+        })
+
+        it('returns the matching company contact with "abstractionAlertsCount" as 0', async () => {
+          const result = await FetchCompanyContactService.go(companyContact.id)
+
+          expect(result).to.equal(_transformToFetchResult(companyContact, contact, 0))
         })
       })
 
-      it('returns the matching company', async () => {
-        const result = await FetchCompanyContactService.go(companyContact.id)
+      describe('and it is marked for "abstractionAlerts"', () => {
+        beforeEach(async () => {
+          companyContact = await CompanyContactHelper.add({
+            abstractionAlerts: true,
+            contactId: contact.id
+          })
+        })
 
-        expect(result).to.equal({
-          id: companyContact.id,
-          abstractionAlerts: false,
-          abstractionAlertsCount: 0,
-          companyId: companyContact.companyId,
-          contact: {
-            id: contact.id,
-            salutation: null,
-            firstName: 'Amara',
-            middleInitials: null,
-            lastName: 'Gupta',
-            initials: null,
-            contactType: 'person',
-            suffix: null,
-            department: null,
-            email: 'amara.gupta@example.com'
-          }
+        it('returns the matching company contact with "abstractionAlertsCount" as 1', async () => {
+          const result = await FetchCompanyContactService.go(companyContact.id)
+
+          expect(result).to.equal(_transformToFetchResult(companyContact, contact, 1))
         })
       })
     })
 
-    describe('when a company contact is marked for "abstractionAlerts"', () => {
-      beforeEach(async () => {
-        contact = await ContactHelper.add()
-
-        companyContact = await CompanyContactHelper.add({
-          contactId: contact.id,
-          abstractionAlerts: true
+    describe('and there are other "additional contacts" for the company', () => {
+      describe('when it is not marked for "abstractionAlerts"', () => {
+        beforeEach(async () => {
+          companyContact = await CompanyContactHelper.add({
+            contactId: contact.id
+          })
         })
 
-        // Add a company contact to the company
-        await CompanyContactHelper.add({
-          contactId: contact.id,
-          abstractionAlerts: true
+        describe('and nor are the other contacts', () => {
+          beforeEach(async () => {
+            additionalCompanyContact = await CompanyContactHelper.add({
+              abstractionAlerts: false,
+              companyId: companyContact.companyId,
+              contactId: contact.id
+            })
+          })
+
+          it('returns the matching company contact with "abstractionAlertsCount" as 0', async () => {
+            const result = await FetchCompanyContactService.go(companyContact.id)
+
+            expect(result).to.equal(_transformToFetchResult(companyContact, contact, 0))
+          })
         })
-      })
 
-      it('returns the matching company (with the "abstractionAlertsCount")', async () => {
-        const result = await FetchCompanyContactService.go(companyContact.id)
+        describe('but the other contacts are', () => {
+          beforeEach(async () => {
+            additionalCompanyContact = await CompanyContactHelper.add({
+              abstractionAlerts: true,
+              companyId: companyContact.companyId,
+              contactId: contact.id
+            })
+          })
 
-        expect(result).to.equal({
-          id: companyContact.id,
-          abstractionAlerts: true,
-          abstractionAlertsCount: 1,
-          companyId: companyContact.companyId,
-          contact: {
-            id: contact.id,
-            salutation: null,
-            firstName: 'Amara',
-            middleInitials: null,
-            lastName: 'Gupta',
-            initials: null,
-            contactType: 'person',
-            suffix: null,
-            department: null,
-            email: 'amara.gupta@example.com'
-          }
+          it('returns the matching company contact with "abstractionAlertsCount" as 1', async () => {
+            const result = await FetchCompanyContactService.go(companyContact.id)
+
+            expect(result).to.equal(_transformToFetchResult(companyContact, contact, 1))
+          })
         })
-      })
-
-      it('returns the "abstractionAlertsCount"', async () => {
-        const result = await FetchCompanyContactService.go(companyContact.id)
-
-        expect(result.abstractionAlertsCount).to.equal(1)
       })
     })
   })
 })
+
+function _transformToFetchResult(companyContact, contact, abstractionAlertsCount = 0) {
+  return {
+    id: companyContact.id,
+    abstractionAlerts: companyContact.abstractionAlerts,
+    abstractionAlertsCount,
+    companyId: companyContact.companyId,
+    contact: {
+      id: contact.id,
+      salutation: null,
+      firstName: 'Amara',
+      middleInitials: null,
+      lastName: 'Gupta',
+      initials: null,
+      contactType: 'person',
+      suffix: null,
+      department: null,
+      email: 'amara.gupta@example.com'
+    }
+  }
+}
