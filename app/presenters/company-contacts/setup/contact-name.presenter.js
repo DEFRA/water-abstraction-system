180f807d5d31964f62e82caefa321fab4c72c2fb
jonathangoulding <goulding.jon@icloud.com>
2026-01-27 16:23:11 +0000

    Add company contact setup contact name logic
    https://eaflood.atlassian.net/browse/WATER-5469
    
    This change updates previous scaffolding code.
    
     It implements the name field for the page, with associated validation, saving the name to the session and re-rendering the page with an error.

--- app/presenters/company-contacts/setup/contact-name.presenter.js
+++ app/presenters/company-contacts/setup/contact-name.presenter.js
@@ -8,15 +8,21 @@
 /**
  * Formats data for the '/company-contacts/setup/{sessionId}/contact-name' page
  *
+ * @param {module:SessionModel} session - The session instance
+ *
  * @returns {object} The data formatted for the view template
  */
-function go() {
+function go(session) {
+  const { company } = session
+
   return {
     backLink: {
-      href: '',
+      href: `/system/companies/${company.id}/contacts`,
       text: 'Back'
     },
-    pageTitle: 'Enter a name for the contact'
+    pageTitle: 'Enter a name for the contact',
+    pageTitleCaption: company.name,
+    name: session.name ?? ''
   }
 }
 
--- test/presenters/company-contacts/setup/contact-name.presenter.test.js
+++ test/presenters/company-contacts/setup/contact-name.presenter.test.js
@@ -7,14 +7,20 @@ const Code = require('@hapi/code')
 const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
+// Test helpers
+const CustomersFixtures = require('../../../fixtures/customers.fixture.js')
+
 // Thing under test
 const ContactNamePresenter = require('../../../../app/presenters/company-contacts/setup/contact-name.presenter.js')
 
 describe('Company Contacts - Setup - Contact Name Presenter', () => {
+  let company
   let session
 
   beforeEach(() => {
-    session = {}
+    company = CustomersFixtures.company()
+
+    session = { company }
   })
 
   describe('when called', () => {
@@ -23,10 +29,34 @@ describe('Company Contacts - Setup - Contact Name Presenter', () => {
 
       expect(result).to.equal({
         backLink: {
-          href: '',
+          href: `/system/companies/${company.id}/contacts`,
           text: 'Back'
         },
-        pageTitle: 'Enter a name for the contact'
+        name: '',
+        pageTitle: 'Enter a name for the contact',
+        pageTitleCaption: 'Tyrell Corporation'
+      })
+    })
+
+    describe('the "name" property', () => {
+      describe('when the name has previously been saved', () => {
+        beforeEach(() => {
+          session.name = 'Eric'
+        })
+
+        it('returns the name from the session', () => {
+          const result = ContactNamePresenter.go(session)
+
+          expect(result.name).to.equal('Eric')
+        })
+      })
+
+      describe('when the name has not previously been saved', () => {
+        it('returns an empty string', () => {
+          const result = ContactNamePresenter.go(session)
+
+          expect(result.name).to.equal('')
+        })
       })
     })
   })
