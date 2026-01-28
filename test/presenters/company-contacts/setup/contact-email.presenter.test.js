c7ac085bb3029adfa67961af1a2196eb8e596994
jonathangoulding <goulding.jon@icloud.com>
2026-01-28 13:37:49 +0000

    chore: pre pr checks

--- test/presenters/company-contacts/setup/contact-email.presenter.test.js
+++ test/presenters/company-contacts/setup/contact-email.presenter.test.js
@@ -8,7 +8,7 @@ const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const CustomersFixtures = require('../../../fixtures/customers.fixture.js')
+const CustomersFixtures = require('../../../support/fixtures/customers.fixture.js')
 const { generateUUID } = require('../../../../app/lib/general.lib.js')
 
 // Thing under test
