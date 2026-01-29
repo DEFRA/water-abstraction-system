9c0437c021aabb6ca0709c4425b7c556a9109ac5
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-01-29 09:28:20 +0000

    Scaffold company contacts setup abstraction alerts (#2926)
    * Scaffold company contacts setup abstraction alerts
    
    https://eaflood.atlassian.net/browse/WATER-5473
    
    This change scaffolds the company contacts setup abstraction alerts page.

---
+++ app/services/company-contacts/setup/submit-abstraction-alerts.service.js
@@ -0,0 +1,54 @@
+'use strict'
+
+/**
+ * Orchestrates validating the data for the '/company-contacts/setup/{sessionId}/abstraction-alerts' page
+ *
+ * @module SubmitAbstractionAlertsService
+ */
+
+const AbstractionAlertsPresenter = require('../../../presenters/company-contacts/setup/abstraction-alerts.presenter.js')
+const AbstractionAlertsValidator = require('../../../validators/company-contacts/setup/abstraction-alerts.validator.js')
+const SessionModel = require('../../../models/session.model.js')
+const { formatValidationResult } = require('../../../presenters/base.presenter.js')
+
+/**
+ * Orchestrates validating the data for the '/company-contacts/setup/{sessionId}/abstraction-alerts' page
+ *
+ * @param {string} sessionId - The UUID of the current session
+ * @param {object} payload - The submitted form data
+ *
+ * @returns {Promise<object>} The data formatted for the view template
+ */
+async function go(sessionId, payload) {
+  const session = await SessionModel.query().findById(sessionId)
+
+  const validationResult = _validate(payload)
+
+  if (!validationResult) {
+    await _save(session, payload)
+
+    return {}
+  }
+
+  const pageData = AbstractionAlertsPresenter.go(session)
+
+  return {
+    activeNavBar: 'search',
+    error: validationResult,
+    ...pageData
+  }
+}
+
+async function _save(session, payload) {
+  return session.$update()
+}
+
+function _validate(payload) {
+  const validationResult = AbstractionAlertsValidator.go(payload)
+
+  return formatValidationResult(validationResult)
+}
+
+module.exports = {
+  go
+}
