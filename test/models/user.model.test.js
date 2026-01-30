a3542ffe39a0afb0706aee37a2655ddcbaea88bf
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-01-30 17:37:27 +0000

    Housekeeping - Allow licence entity to be reused
    We are going to be adding a modifier and instance method to the UserModel that depend on licence entity and licence entity role records.
    
    So, ahead of that we make the existing licence entity and the test user it creates accessible to other tests. We also start cleaning up after ourselves.

--- test/models/user.model.test.js
+++ test/models/user.model.test.js
@@ -4,7 +4,7 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, before, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, before, beforeEach, afterEach, after } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
@@ -40,8 +40,10 @@ describe('User model', () => {
   let testChargeVersionNoteOne
   let testChargeVersionNoteTwo
   let testGroup
+  let testLicenceEntity
   let testRecord
   let testRole
+  let testUser
   let testUserRole
   let testUserGroup
 
@@ -55,6 +57,21 @@ describe('User model', () => {
     testChargeVersionNoteOne = await ChargeVersionNoteHelper.add({ userId: testRecord.id, note: '1st test note' })
     testChargeVersionNoteTwo = await ChargeVersionNoteHelper.add({ userId: testRecord.id, note: '2nd test note' })
     testUserRole = UserRoleHelper.select(USER_ROLE_AR_USER_INDEX)
+  })
+
+  afterEach(async () => {
+    if (testLicenceEntity) {
+      await testLicenceEntity.$query().delete()
+    }
+
+    if (testUser) {
+      await testUser.$query().delete()
+    }
+  })
+
+  after(async () => {
+    await testChargeVersionNoteOne.$query().delete()
+    await testChargeVersionNoteTwo.$query().delete()
   })
 
   describe('Basic query', () => {
@@ -115,10 +132,7 @@ describe('User model', () => {
     })
 
     describe('when linking to licence entity', () => {
-      let testAddedRecord
-      let testLicenceEntity
-
-      before(async () => {
+      beforeEach(async () => {
         testLicenceEntity = await LicenceEntityHelper.add()
 
         const { id: licenceEntityId } = testLicenceEntity
@@ -128,7 +142,7 @@ describe('User model', () => {
         // linked to a licence using the external UI.
         //
         // So, for this test we have to fall back to generating a user against which we assign the licence entity ID.
-        testAddedRecord = await UserHelper.add({ licenceEntityId })
+        testUser = await UserHelper.add({ licenceEntityId })
       })
 
       it('can successfully run a related query', async () => {
@@ -138,10 +152,10 @@ describe('User model', () => {
       })
 
       it('can eager load the licence entity', async () => {
-        const result = await UserModel.query().findById(testAddedRecord.id).withGraphFetched('licenceEntity')
+        const result = await UserModel.query().findById(testUser.id).withGraphFetched('licenceEntity')
 
         expect(result).to.be.instanceOf(UserModel)
-        expect(result.id).to.equal(testAddedRecord.id)
+        expect(result.id).to.equal(testUser.id)
 
         expect(result.licenceEntity).to.be.an.instanceOf(LicenceEntityModel)
         expect(result.licenceEntity).to.equal(testLicenceEntity)
