a0281442fb4fcbeb537250bdd52b2725f3b7f953
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-01-30 14:57:13 +0000

    Update app/lib/check-page.lib.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/lib/check-page.lib.js
+++ app/lib/check-page.lib.js
@@ -6,16 +6,23 @@
  */
 
 /**
- * Checks if the check page has been visited and updates the url accordingly.
- * If session.checkPageVisited is true, the last segment of the path is replaced
- * with 'check'.
+ * Checks if the '/check' page of a journey has been visited and returns the appropriate url
+ *
+ * If `session.checkPageVisited` is true, the last segment of the path is replaced with 'check'.
  *
  * For example, a url of '/setup/123/contact-email' becomes '/setup/123/check'.
+ *
+ * Our journeys have to support users working through the journey once, but then being able to return to a page from the
+ * '/check' page to edit the value. This method helps manage the back or redirect URLs for those pages.
+ *
+ * The 'Back' link is expected to return the user to the '/check' page if they have already visited it, else return them
+ * to the previous page in the journey. The same applies when the user submits a page - they should be redirected back
+ * to or the next page in the journey depending on whether they came from the '/check' page.
  *
  * @param {object} session - The session instance
  * @param {string} url - The url
  *
- * @returns {string} The updated redirectUrl, or provided redirectUrl if no update is required.
+ * @returns {string} The url to use, modified to point to the '/check' page if it has been visited
  */
 function checkUrl(session, url) {
   const { checkPageVisited } = session
