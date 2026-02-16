25f6370fb6e92b5a58582e7f9f612860cacde10d
Ben Sagar <6583598+ben-sagar@users.noreply.github.com>
2026-02-16 15:43:49 +0000

    Create user verification views (#2999)
    https://eaflood.atlassian.net/browse/WATER-5467
    
    Whilst working on migrating the page to view external users over to the new system, we identified that there were a couple of related tables missing from our data model that are required.
    
    - the first table `crm.verification` contains the verification codes for users who are attempting to claim licences, which will allow them to manage those licences
    
    - the second table `crm.verification_documents` is simply the list of licences that they are attempting to claim with that verification code
    
    This change is to add those two tables to our views and models.

---
+++ test/models/user-verification.model.test.js
@@ -0,0 +1,134 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, before, after } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const LicenceDocumentHeaderHelper = require('../support/helpers/licence-document-header.helper.js')
+const LicenceDocumentHeaderModel = require('../../app/models/licence-document-header.model.js')
+const LicenceEntityHelper = require('../support/helpers/licence-entity.helper.js')
+const LicenceEntityModel = require('../../app/models/licence-entity.model.js')
+const UserVerificationDocumentHelper = require('../support/helpers/user-verification-document.helper.js')
+const UserVerificationHelper = require('../support/helpers/user-verification.helper.js')
+
+// Thing under test
+const UserVerificationModel = require('../../app/models/user-verification.model.js')
+
+describe('User Verification model', () => {
+  let testLicenceDocumentHeaders
+  let testUserVerificationDocuments
+  let testCompanyEntity
+  let testLicenceEntity
+  let testRecord
+
+  before(async () => {
+    testCompanyEntity = await LicenceEntityHelper.add({ type: 'company' })
+    testLicenceEntity = await LicenceEntityHelper.add()
+    testLicenceDocumentHeaders = []
+    for (let i = 0; i < 2; i++) {
+      const testLicenceDocumentHeader = await LicenceDocumentHeaderHelper.add()
+
+      testLicenceDocumentHeaders.push(testLicenceDocumentHeader)
+    }
+
+    testRecord = await UserVerificationHelper.add({
+      companyEntityId: testCompanyEntity.id,
+      licenceEntityId: testLicenceEntity.id
+    })
+
+    testUserVerificationDocuments = []
+    for (const testLicenceDocumentHeader of testLicenceDocumentHeaders) {
+      const testUserVerificationDocument = await UserVerificationDocumentHelper.add({
+        licenceDocumentHeaderId: testLicenceDocumentHeader.id,
+        userVerificationId: testRecord.id
+      })
+
+      testUserVerificationDocuments.push(testUserVerificationDocument)
+    }
+  })
+
+  after(async () => {
+    for (const testUserVerificationDocument of testUserVerificationDocuments) {
+      await testUserVerificationDocument.$query().delete()
+    }
+    await testRecord.$query().delete()
+    for (const testLicenceDocumentHeader of testLicenceDocumentHeaders) {
+      await testLicenceDocumentHeader.$query().delete()
+    }
+    await testLicenceEntity.$query().delete()
+    await testCompanyEntity.$query().delete()
+  })
+
+  describe('Basic query', () => {
+    it('can successfully run a basic query', async () => {
+      const result = await UserVerificationModel.query().findById(testRecord.id)
+
+      expect(result).to.be.an.instanceOf(UserVerificationModel)
+      expect(result.id).to.equal(testRecord.id)
+    })
+  })
+
+  describe('Relationships', () => {
+    describe('when linking to company entity', () => {
+      it('can successfully run a related query', async () => {
+        const query = await UserVerificationModel.query().innerJoinRelated('companyEntity')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the company entity', async () => {
+        const result = await UserVerificationModel.query().findById(testRecord.id).withGraphFetched('companyEntity')
+
+        expect(result).to.be.instanceOf(UserVerificationModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.companyEntity).to.be.an.instanceOf(LicenceEntityModel)
+        expect(result.companyEntity).to.equal(testCompanyEntity)
+      })
+    })
+
+    describe('when linking to licence entity', () => {
+      it('can successfully run a related query', async () => {
+        const query = await UserVerificationModel.query().innerJoinRelated('licenceEntity')
+
+        expect(query).to.exist()
+      })
+
+      it('can eager load the licence entity', async () => {
+        const result = await UserVerificationModel.query().findById(testRecord.id).withGraphFetched('licenceEntity')
+
+        expect(result).to.be.instanceOf(UserVerificationModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.licenceEntity).to.be.an.instanceOf(LicenceEntityModel)
+        expect(result.licenceEntity).to.equal(testLicenceEntity)
+      })
+    })
+
+    describe('when linking to licence document headers', () => {
+      it('can successfully run a related query', async () => {
+        const query = await UserVerificationModel.query().innerJoinRelated('licenceDocumentHeaders')
+
+        expect(query).to.be.an.array()
+      })
+
+      it('can eager load the licence document headers', async () => {
+        const result = await UserVerificationModel.query()
+          .findById(testRecord.id)
+          .withGraphFetched('licenceDocumentHeaders')
+
+        expect(result).to.be.instanceOf(UserVerificationModel)
+        expect(result.id).to.equal(testRecord.id)
+
+        expect(result.licenceDocumentHeaders).to.be.an.array()
+        expect(result.licenceDocumentHeaders[0]).to.be.an.instanceOf(LicenceDocumentHeaderModel)
+        expect(result.licenceDocumentHeaders).to.include(testLicenceDocumentHeaders[0])
+        expect(result.licenceDocumentHeaders).to.include(testLicenceDocumentHeaders[1])
+      })
+    })
+  })
+})
