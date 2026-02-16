ea6fc7e64d14e92973aec1f77bd8ed4a8a096aa8
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-02-16 15:18:33 +0000

    Revert Customer search to Licence holder  (#3013)
    https://eaflood.atlassian.net/browse/WATER-5507
    
    We are migrating the internal search page (the root page of the internal service) from the legacy apps to our new water-abstraction-system app.
    
    The existing page allows users to search using different types of values, for example, licence references, licence names, and return references.
    
    First, we [Migrated licence holder search](https://github.com/DEFRA/water-abstraction-system/pull/2667). Behind the scenes, this search found all 'companies' with a licence holder role (`crm_v2.document_roles`).
    
    Then we [Changed it to search Company (customer)](https://github.com/DEFRA/water-abstraction-system/pull/2943). Locking it to just licence holders prevents users from finding other companies that may not be linked to a licence via 'licence holder', but might still have contacts and billing accounts.
    
    **Clarification**
    
    Having received further user feedback and discussed 'Customer' in greater depth as a team, we now have a clearer definition going forward.
    
    A 'Customer' is a licence holder, and can be thought of as the 'parent' entity in our CRM. A licence holder may have additional contacts, such as billing, returns, and abstraction alerts, which may be linked to different 'organisations', for example, when a licence holder employs an agent. But these should not be considered at the same level as 'licence holder'.
    
    Unfortunately, because of the way data is currently structured in `crm_v2`, all these different contact types have a parent `crm_v2.companies` record. Hence, we assumed that all companies are customers and may not link directly to a licence holder.
    
    The goal over the coming months is to 'fix' our CRM so that it supports the structure the business believes we actually have.
    
    In the meantime, to avoid confusion, we'll no longer use the generic term 'Customers' and instead stick with 'Licence holders'.
    
    **Change**
    
    Now we know this, we're reverting the change we made to 'search'. We'll rename things back to 'licenceHolder' and update the queries to only include those companies with a 'licence holder' role. We'll also update the query used to count licences to only include those where the company is a licence holder.

--- app/services/search/fetch-search-results-details.service.js
+++ app/services/search/fetch-search-results-details.service.js
@@ -11,6 +11,7 @@ const LicenceModel = require('../../models/licence.model.js')
 const MonitoringStationModel = require('../../models/monitoring-station.model.js')
 const ReturnLogModel = require('../../models/return-log.model.js')
 const UserModel = require('../../models/user.model.js')
+const { db } = require('../../../db/db.js')
 
 /**
  * Fetches the item details for a set of search results on the /search page
@@ -50,8 +51,8 @@ async function _findByType(type, ids) {
   switch (type) {
     case 'billingAccount':
       return _billingAccount(ids)
-    case 'company':
-      return _company(ids)
+    case 'licenceHolder':
+      return _licenceHolder(ids)
     case 'licence':
       return _licence(ids)
     case 'monitoringStation':
@@ -75,14 +76,39 @@ async function _licence(ids) {
     .findByIds(ids)
 }
 
-async function _company(ids) {
+/**
+ * Because licence holders are often duplicated (try searching for Anglian water!) we want to give the user additional
+ * detail to help them find the right one.
+ *
+ * All data in NALD is held at a regional level. So any national organisation with a licence in each region will have 8
+ * licence holder records as a minimum. So, we fetch the region to help distinguish them.
+ *
+ * When there are actual duplicates, the primary record becomes clear as it typically has the most licences linked to
+ * it. Displaying the count of licences helps distinguish this. We only care about licence document role records where
+ * the role is 'licence holder'. They also have start and end dates so there may be multiple records for the same
+ * company and licence. We only want a count of the licences, not entries, hence the `DISTINCT`.
+ * @private
+ */
+async function _licenceHolder(ids) {
   return CompanyModel.query()
-    .select(['companies.id', 'companies.name', 'region.displayName AS region'])
+    .select([
+      'companies.id',
+      'companies.name',
+      'region.displayName AS region',
+      db.raw(`(
+        SELECT COUNT(DISTINCT ldr.licence_document_id) FROM public.licence_document_roles ldr
+        INNER JOIN public.licence_roles lr ON lr.id = ldr.licence_role_id
+        WHERE ldr.company_id = companies.id AND lr."name" = 'licenceHolder'
+      ) AS "licenceCount"`)
+    ])
     .leftJoinRelated('region')
     .whereNotNull('externalId')
     .withGraphFetched('licenceDocumentRoles')
     .modifyGraph('licenceDocumentRoles', (builder) => {
-      builder.select('licenceDocumentId')
+      builder
+        .select(['licenceDocumentRoles.licenceDocumentId'])
+        .innerJoinRelated('licenceRole', { alias: 'lr' })
+        .where('lr.name', 'licenceHolder')
     })
     .findByIds(ids)
 }
