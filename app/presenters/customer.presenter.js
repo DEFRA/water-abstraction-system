d3920c126a6b330ae55ea5ea0c1a0a7402ec0609
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-02-17 08:18:37 +0000

    Update app/presenters/customer.presenter.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/presenters/customer.presenter.js
+++ app/presenters/customer.presenter.js
@@ -26,13 +26,27 @@ function _communicationType(companyContact) {
 }
 
 /**
- * All emails in WRLS should be lowercase.
+ * Converts an email to lowercase, if one has been set
  *
- * The email should be lowercase when saved in WRLS, however, the legacy data still contains mix case emails.
+ * Technically, email addresses, specifically the bit before the '@' symbol, are case-sensitive. However, nearly all
+ * email providers ignore case in email addresses, and treat `FOO@example.com` and `foo@example.com` as the same
+ * address.
  *
- * @param {string | undefined} email - The email
+ * Therefore, the accepted convention is to always store email addresses in lowercase to avoid any confusion.
  *
- * @returns {string|null} The email formatted for to lowercase or null
+ * Surprise-surprise, the legacy service did not follow this convention!
+ *
+ * So, this function ensures any email addresses we collect are formatted as expected before being persisted.
+ *
+ * Also, until we can clean up the existing data, it ensures that any legacy email addresses are processed as lowercase.
+ *
+ * To avoid repeating the check of whether an email exists before passing it to this function, this function
+ * deals with that. Calling code can just pass in, for example, `formatEmail(contact.email)` without worrying about an
+ * error.
+ *
+ * @param {string} email - The email address to lowercase
+ *
+ * @returns {string|null} The email formatted to lowercase or null
  */
 function formatEmail(email) {
   if (email) {
