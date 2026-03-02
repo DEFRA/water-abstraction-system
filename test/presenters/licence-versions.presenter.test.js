32f4c870502ec0c6a73335a4e399786009ba3f6d
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-03-02 08:24:13 +0000

    Update companies licences page to show licence versions (#3052)
    * Update companies licences page to show licence versions
    
    https://eaflood.atlassian.net/browse/WATER-5516
    
    This change update the companies licences page to show licence versions.
    
    This means fetching the licences and licence versions; we have a link between a licence version and a company through the licence version holder. This is used to fetch the licence / licence versions for the company.
    
    This logic, and the way the data is structured, can lead to fetching licence versions for a licence, where the company can be different.
    
    This means the displayed licence versions could appear to be missing date between licence versions, this is not the case, and the licence holder has likely been updated.

---
+++ test/presenters/licence-versions.presenter.test.js
@@ -0,0 +1,242 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const { generateUUID } = require('../../app/lib/general.lib.js')
+
+// Thing under test
+const LicenceVersionsPresenter = require('../../app/presenters/licence-versions.presenter.js')
+
+describe('Licences versions presenter', () => {
+  describe('#formatLicenceVersions()', () => {
+    let licenceVersions
+
+    describe('when the licence versions', () => {
+      describe('have contiguous start and end dates', () => {
+        beforeEach(() => {
+          licenceVersions = [
+            {
+              id: generateUUID(),
+              startDate: new Date('2015-11-06'),
+              endDate: new Date('2023-10-02')
+            },
+            {
+              id: generateUUID(),
+              startDate: new Date('2002-04-01'),
+              endDate: new Date('2015-11-05')
+            },
+            {
+              id: generateUUID(),
+              startDate: new Date('1977-04-01'),
+              endDate: new Date('2002-03-31')
+            }
+          ]
+        })
+
+        it('returns all the licence versions merged', () => {
+          const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+          expect(result).to.equal([
+            {
+              id: licenceVersions[0].id,
+              endDate: new Date('2023-10-02'),
+              startDate: new Date('1977-04-01')
+            }
+          ])
+        })
+
+        describe('that are one day apart', () => {
+          beforeEach(() => {
+            licenceVersions = [
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-02'),
+                endDate: new Date('2022-01-02')
+              },
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-01'),
+                endDate: new Date('2022-01-01')
+              }
+            ]
+          })
+
+          it('returns the licence versions merged', () => {
+            const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+            expect(result).to.equal([
+              {
+                id: licenceVersions[0].id,
+                startDate: new Date('2022-01-01'),
+                endDate: new Date('2022-01-02')
+              }
+            ])
+          })
+        })
+      })
+
+      describe('have overlapping dates (the same start date, end date)', () => {
+        beforeEach(() => {
+          licenceVersions = [
+            {
+              id: generateUUID(),
+              endDate: new Date('2022-01-03'),
+              startDate: new Date('2022-01-02')
+            },
+            {
+              id: generateUUID(),
+              endDate: new Date('2022-01-02'),
+              startDate: new Date('2022-01-01')
+            }
+          ]
+        })
+
+        it('returns all the licence versions merged', () => {
+          const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+          expect(result).to.equal([
+            {
+              id: licenceVersions[0].id,
+              endDate: new Date('2022-01-03'),
+              startDate: new Date('2022-01-01')
+            }
+          ])
+        })
+      })
+
+      describe('have non-contiguous start and end dates', () => {
+        beforeEach(() => {
+          licenceVersions = [
+            {
+              id: generateUUID(),
+              startDate: new Date('2016-10-06'),
+              endDate: new Date('2023-10-02')
+            },
+            {
+              id: generateUUID(),
+              startDate: new Date('2002-04-01'),
+              endDate: new Date('2015-11-05')
+            },
+            {
+              id: generateUUID(),
+              startDate: new Date('1977-04-01'),
+              endDate: new Date('2002-03-31')
+            }
+          ]
+        })
+
+        it('returns the licence versions merged into groups', () => {
+          const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+          expect(result).to.equal([
+            {
+              id: licenceVersions[0].id,
+              startDate: new Date('2016-10-06'),
+              endDate: new Date('2023-10-02')
+            },
+            {
+              id: licenceVersions[1].id,
+              startDate: new Date('1977-04-01'),
+              endDate: new Date('2015-11-05')
+            }
+          ])
+        })
+      })
+
+      describe('have the same start date', () => {
+        describe('but the first ends before the second ends', () => {
+          beforeEach(() => {
+            licenceVersions = [
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-05'), // Starts SAME as the one above
+                endDate: new Date('2022-01-10') // Ends EARLIER than the one above
+              },
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-05'),
+                endDate: new Date('2022-01-15')
+              }
+            ]
+          })
+
+          it('returns the earliest start and latest end date, and details from the latest', () => {
+            const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+            expect(result).to.equal([
+              {
+                id: licenceVersions[1].id,
+                startDate: new Date('2022-01-05'),
+                endDate: new Date('2022-01-15')
+              }
+            ])
+          })
+        })
+
+        describe('but the first ends after the second ends', () => {
+          beforeEach(() => {
+            licenceVersions = [
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-05'),
+                endDate: new Date('2022-01-15')
+              },
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-05'), // Starts SAME as the one above
+                endDate: new Date('2022-01-10') // Ends EARLIER than the one above
+              }
+            ]
+          })
+
+          it('returns the details of the wrapping licence version', () => {
+            const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+            expect(result).to.equal([
+              {
+                id: licenceVersions[0].id,
+                startDate: new Date('2022-01-05'),
+                endDate: new Date('2022-01-15')
+              }
+            ])
+          })
+        })
+
+        describe('and the same end date', () => {
+          beforeEach(() => {
+            licenceVersions = [
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-01'),
+                endDate: new Date('2022-01-01')
+              },
+              {
+                id: generateUUID(),
+                startDate: new Date('2022-01-01'),
+                endDate: new Date('2022-01-01')
+              }
+            ]
+          })
+
+          it('returns the details of the merged licence version', () => {
+            const result = LicenceVersionsPresenter.formatLicenceVersions(licenceVersions)
+
+            expect(result).to.equal([
+              {
+                id: licenceVersions[0].id,
+                startDate: new Date('2022-01-01'),
+                endDate: new Date('2022-01-01')
+              }
+            ])
+          })
+        })
+      })
+    })
+  })
+})
