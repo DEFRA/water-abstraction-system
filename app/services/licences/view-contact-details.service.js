ce59a77cbdd1f966073b63e16a5ebb779065b86f
Jason Claxton <jason.claxton@defra.gov.uk>
2026-03-11 15:44:41 +0000

    Remove "Customer contacts" data from `ViewContactDetailsService` and fix tests

--- app/services/licences/view-contact-details.service.js
+++ app/services/licences/view-contact-details.service.js
@@ -5,10 +5,8 @@
  * @module ViewContactDetailsService
  */
 
-const CompanyContactsPresenter = require('../../presenters/licences/company-contacts.presenter.js')
 const ContactDetailsPresenter = require('../../presenters/licences/contact-details.presenter.js')
 const FetchContactDetailsService = require('./fetch-contact-details.service.js')
-const FetchCustomerContactsService = require('./fetch-company-contacts.service.js')
 const FetchLicenceService = require('./fetch-licence.service.js')
 const { userRoles } = require('../../presenters/licences/base-licences.presenter.js')
 
@@ -24,15 +22,11 @@ async function go(licenceId, auth) {
   const licence = await FetchLicenceService.go(licenceId)
 
   const licenceContacts = await FetchContactDetailsService.go(licenceId)
-  const licenceContactsData = ContactDetailsPresenter.go(licenceContacts, licence)
-
-  const companyContacts = await FetchCustomerContactsService.go(licenceId)
-  const companyContactsData = CompanyContactsPresenter.go(companyContacts)
+  const pageData = ContactDetailsPresenter.go(licenceContacts, licence)
 
   return {
     activeSecondaryNav: 'contact-details',
-    ...companyContactsData,
-    ...licenceContactsData,
+    ...pageData,
     roles: userRoles(auth)
   }
 }
