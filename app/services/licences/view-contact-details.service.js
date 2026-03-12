490173d57f2c8a37d4ff87b00013968a2d1a4349
Jason Claxton <jason.claxton@defra.gov.uk>
2026-03-12 14:17:34 +0000

    Add pagination to `ViewContactDetailsService`

--- app/services/licences/view-contact-details.service.js
+++ app/services/licences/view-contact-details.service.js
@@ -5,29 +5,42 @@
  * @module ViewContactDetailsService
  */
 
+const { userRoles } = require('../../presenters/licences/base-licences.presenter.js')
 const ContactDetailsPresenter = require('../../presenters/licences/contact-details.presenter.js')
 const FetchContactDetailsService = require('./fetch-contact-details.service.js')
 const FetchLicenceService = require('./fetch-licence.service.js')
-const { userRoles } = require('../../presenters/licences/base-licences.presenter.js')
+const PaginatorPresenter = require('../../presenters/paginator.presenter.js')
 
 /**
  * Orchestrates fetching and presenting the data needed for the licence contact details page
  *
  * @param {string} licenceId - The UUID of the licence
  * @param {object} auth - The auth object taken from `request.auth` containing user details
+ * @param {string} page - The current page for the pagination service
  *
  * @returns {Promise<object>} an object representing the `pageData` needed by the licence contact details template.
  */
-async function go(licenceId, auth) {
+async function go(licenceId, auth, page) {
   const licence = await FetchLicenceService.go(licenceId)
+  const roles = userRoles(auth)
 
-  const licenceContacts = await FetchContactDetailsService.go(licenceId)
+  const { licenceContacts, totalNumber } = await FetchContactDetailsService.go(licenceId, page)
+
   const pageData = ContactDetailsPresenter.go(licenceContacts, licence)
+
+  const pagination = PaginatorPresenter.go(
+    totalNumber,
+    page,
+    `/system/licences/${licenceId}/contact-details`,
+    licenceContacts.length,
+    'contacts'
+  )
 
   return {
     activeSecondaryNav: 'contact-details',
     ...pageData,
-    roles: userRoles(auth)
+    pagination,
+    roles
   }
 }
 
