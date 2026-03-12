b7f3a3102ae7dc218ad37e46907fd353913d85da
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-03-12 00:49:31 +0000

    Update Fetch to return groups and roles for the user

--- app/services/users/fetch-user-internal.service.js
+++ app/services/users/fetch-user-internal.service.js
@@ -15,7 +15,30 @@ const UserModel = require('../../models/user.model.js')
  * @returns {Promise<module:UserModel>} the requested user
  */
 async function go(id) {
-  return UserModel.query().select(['id', 'username']).modify('status').modify('permissions').findById(id)
+  return UserModel.query()
+    .select(['id', 'username'])
+    .modify('status')
+    .modify('permissions')
+    .findById(id)
+    .withGraphFetched('groups')
+    .modifyGraph('groups', (groupsBuilder) => {
+      groupsBuilder
+        .select(['groups.id'])
+        .withGraphFetched('roles')
+        .modifyGraph('roles', (rolesBuilder) => {
+          rolesBuilder.select(['description', 'roles.id', 'role']).orderBy([
+            { column: 'role', order: 'asc' },
+            { column: 'description', order: 'asc' }
+          ])
+        })
+    })
+    .withGraphFetched('roles')
+    .modifyGraph('roles', (rolesBuilder) => {
+      rolesBuilder.select(['description', 'roles.id', 'role']).orderBy([
+        { column: 'role', order: 'asc' },
+        { column: 'description', order: 'asc' }
+      ])
+    })
 }
 
 module.exports = {
--- test/services/users/fetch-user-internal.service.test.js
+++ test/services/users/fetch-user-internal.service.test.js
@@ -8,6 +8,8 @@ const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
+const GroupRoleHelper = require('../../support/helpers/group-role.helper.js')
+const RoleHelper = require('../../support/helpers/role.helper.js')
 const UsersFixture = require('../../support/fixtures/users.fixture.js')
 
 // Thing under test
@@ -17,17 +19,67 @@ const FetchUserInternalService = require('../../../app/services/users/fetch-user
 // unlike other fetch tests we don't create any test records and assert they are in our results as we already have
 // sufficient data to work with.
 describe('Users - Fetch User Internal service', () => {
+  let groupRoles
+  let roles
   let user
 
   describe('when called', () => {
     beforeEach(() => {
-      user = UsersFixture.adminInternal()
+      user = UsersFixture.digitiseApprover()
+
+      // Grab the groupRole records for the group attached to our seeded user
+      groupRoles = GroupRoleHelper.data.filter((groupRole) => {
+        return groupRole.groupId === user.groups[0].id
+      })
+
+      const seededRoles = RoleHelper.data
+
+      // From those determine which roles should be attached to the user. Be aware, these are distinct from 'user roles'
+      // which is why we have picked the Digitise Approver for this test, as it has both group and user roles.
+      roles = groupRoles.map((groupRole) => {
+        const matchingRole = seededRoles.find((seededRole) => {
+          return seededRole.id === groupRole.roleId
+        })
+
+        return {
+          description: matchingRole.description,
+          id: matchingRole.id,
+          role: matchingRole.role
+        }
+      })
+
+      // Sort the roles to into alphabetical order
+      roles.sort((a, b) => {
+        return a.role.localeCompare(b.role)
+      })
     })
 
     it('returns the requested user', async () => {
       const result = await FetchUserInternalService.go(user.id)
 
-      expect(result.username).to.equal(user.username)
+      expect(result).to.equal({
+        id: user.id,
+        username: user.username,
+        enabled: true,
+        lastLogin: user.lastLogin,
+        statusPassword: null,
+        application: 'water_admin',
+        licenceEntity: null,
+        groups: [
+          {
+            group: user.groups[0].group,
+            id: user.groups[0].id,
+            roles
+          }
+        ],
+        roles: [
+          {
+            id: user.roles[0].id,
+            role: user.roles[0].role,
+            description: user.roles[0].description
+          }
+        ]
+      })
     })
   })
 })
