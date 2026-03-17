6473fc9d04cab8c55dc083b91f555393f3334c0f
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-03-17 09:47:35 +0000

    Update companies contacts page to show name and contact type (#3097)
    * Update companies contacts page to show name and contact type
    
    https://eaflood.atlassian.net/browse/WATER-5513
    
    This change updates the companies contacts page to show all the contacts, this includes the users (primary, user returns and basic users), billing etc.
    
    This means updating the query to return all the possible contacts.
    
    Pagination has been manually implemented as we need to use a raw query.

--- test/presenters/companies/contacts.presenter.test.js
+++ test/presenters/companies/contacts.presenter.test.js
@@ -10,6 +10,7 @@ const { expect } = Code
 
 // Test helpers
 const CustomersFixtures = require('../../support/fixtures/customers.fixture.js')
+const { generateUUID } = require('../../../app/lib/general.lib.js')
 
 // Things we need to stub
 const FeatureFlagsConfig = require('../../../config/feature-flags.config.js')
@@ -19,12 +20,18 @@ const ContactsPresenter = require('../../../app/presenters/companies/contacts.pr
 
 describe('Companies - Contacts presenter', () => {
   let company
-  let companyContacts
+  let contacts
 
   beforeEach(() => {
     company = CustomersFixtures.company()
 
-    companyContacts = CustomersFixtures.companyContacts()
+    contacts = [
+      {
+        id: generateUUID(),
+        contactType: 'additional-contact',
+        contactName: 'Rachael Tyrell'
+      }
+    ]
 
     Sinon.stub(FeatureFlagsConfig, 'enableCustomerManage').value(true)
   })
@@ -35,19 +42,18 @@ describe('Companies - Contacts presenter', () => {
 
   describe('when called', () => {
     it('returns page data for the view', () => {
-      const result = ContactsPresenter.go(company, companyContacts)
+      const result = ContactsPresenter.go(company, contacts)
 
       expect(result).to.equal({
         backLink: {
           href: '/',
           text: 'Back to search'
         },
-        companyContacts: [
+        contacts: [
           {
-            action: `/system/company-contacts/${companyContacts[0].id}`,
-            communicationType: 'Additional Contact',
-            name: 'Rachael Tyrell',
-            email: 'rachael.tyrell@tyrellcorp.com'
+            action: `/system/company-contacts/${contacts[0].id}`,
+            type: 'Additional contact',
+            name: 'Rachael Tyrell'
           }
         ],
         links: {
@@ -56,6 +62,200 @@ describe('Companies - Contacts presenter', () => {
         },
         pageTitle: 'Contacts',
         pageTitleCaption: 'Tyrell Corporation'
+      })
+    })
+
+    describe('and there is a company contact with the type', () => {
+      describe('"abstraction-alerts"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'abstraction-alerts',
+              contactName: 'Rachael Tyrell'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/company-contacts/${contacts[0].id}`,
+              type: 'Abstraction alerts',
+              name: 'Rachael Tyrell'
+            }
+          ])
+        })
+      })
+
+      describe('"additional-contact"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'additional-contact',
+              contactName: 'Rachael Tyrell'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/company-contacts/${contacts[0].id}`,
+              type: 'Additional contact',
+              name: 'Rachael Tyrell'
+            }
+          ])
+        })
+      })
+
+      describe('"billing"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'billing',
+              contactName: 'Rachael Tyrell'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/billing-accounts/${contacts[0].id}?company-id=${company.id}`,
+              type: 'Billing',
+              name: 'Rachael Tyrell'
+            }
+          ])
+        })
+      })
+
+      describe('"basic-user"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'basic-user',
+              contactName: 'user@test.com'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/users/external/${contacts[0].id}`,
+              type: 'Basic user',
+              name: 'user@test.com'
+            }
+          ])
+        })
+      })
+
+      describe('"primary-user"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'primary-user',
+              contactName: 'user@test.com'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/users/external/${contacts[0].id}`,
+              type: 'Primary user',
+              name: 'user@test.com'
+            }
+          ])
+        })
+      })
+
+      describe('"returns-user"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'returns-user',
+              contactName: 'user@test.com'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/users/external/${contacts[0].id}`,
+              type: 'Returns user',
+              name: 'user@test.com'
+            }
+          ])
+        })
+      })
+
+      describe('"licence-holder"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'licence-holder',
+              contactName: 'Rachael Tyrell'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/companies/${contacts[0].id}/licence-holder`,
+              type: 'Licence holder',
+              name: 'Rachael Tyrell'
+            }
+          ])
+        })
+      })
+
+      describe('"returns-to"', () => {
+        beforeEach(() => {
+          contacts = [
+            {
+              id: generateUUID(),
+              contactType: 'returns-to',
+              contactName: 'Rachael Tyrell'
+            }
+          ]
+        })
+
+        it('returns the correct contact', () => {
+          const result = ContactsPresenter.go(company, contacts)
+
+          expect(result.contacts).to.equal([
+            {
+              action: `/system/companies/${contacts[0].id}/returns-to`,
+              type: 'Returns to',
+              name: 'Rachael Tyrell'
+            }
+          ])
+        })
       })
     })
   })
---
+++ test/support/seeders/crm.seeder.js
@@ -0,0 +1,396 @@
+'use strict'
+
+/**
+ * @module CRMSeeder
+ */
+
+const BillingAccountHelper = require('../helpers/billing-account.helper.js')
+const CompanyContactHelper = require('../../support/helpers/company-contact.helper.js')
+const CompanyHelper = require('../helpers/company.helper.js')
+const ContactHelper = require('../../support/helpers/contact.helper.js')
+const LicenceDocumentHeaderHelper = require('../helpers/licence-document-header.helper.js')
+const LicenceDocumentRoleHelper = require('../helpers/licence-document-role.helper.js')
+const LicenceEntityHelper = require('../helpers/licence-entity.helper.js')
+const LicenceEntityRoleHelper = require('../helpers/licence-entity-role.helper.js')
+const LicenceHelper = require('../helpers/licence.helper.js')
+const LicenceRoleHelper = require('../../support/helpers/licence-role.helper.js')
+const LicenceVersionHelper = require('../helpers/licence-version.helper.js')
+const LicenceVersionHolderHelper = require('../helpers/licence-version-holder.helper.js')
+const UserModel = require('../helpers/user.helper.js')
+
+/**
+ * Seed CRM data
+ *
+ * The crm data has multiple sources of contacts.
+ *
+ * We need to get contacts for a company from multiple sources. Theses can be grouped into two categories. External
+ * users and those directly related to the company.
+ *
+ * Directly related to the company:
+ * > Contacts directly related to the company are pretty straight forwards,
+ * you can use the company id and return the record.
+ *
+ * These contacts are:
+ * - abstraction-alerts - An additional contact marked to receive abstraction alerts
+ * - licence-holder - The company that holds the licence
+ * - returns-to - A company to handle the returns
+ * - additional-contact - An additional contact for the licence
+ * - billing - The billing accounts related to the company
+ *
+ * External users:
+ * > External contacts are a little more complicated, they are not directly linked to the company. We need to get to the
+ * licence, we do this from the licence version holder. Once we have the licence, we need to join the licence to the
+ * licence document header using the licence ref. Then we need to return the external contacts from the licence entities.
+ *
+ * These contacts are:
+ * - primary-user - An external user who a licence is registered to
+ * - basic-user - A user with basic access to the system
+ * - returns-user - A user with returns access to the system
+ *
+ * To clean all the contacts from the database, call the '.clean()' method
+ *
+ * @returns {object} - all the contacts
+ */
+async function seed() {
+  // Setup company - The company will be the licence holder
+  const company = await _company('Hogwarts')
+  const companyId = company.record.id
+
+  // Licence entity users are linked through the 'companyEntityId'
+  const companyEntity = await _licenceCompanyEntity(company.record.name)
+  const companyEntityId = companyEntity.record.id
+
+  // Set up a licence linked to the company
+  const licence = await _licence(company)
+  const licenceDocumentHeader = await _licenceDocumentHeader(companyEntityId, licence.record.licenceRef)
+
+  // Set up external users - linked to the company by the licence
+  const basicUser = await _basicUser(companyEntityId, 'minerva.mchonagall@hogwarts.com')
+  const primaryUser = await _primaryUser(companyEntityId, 'albus.dumbledore@hogwarts.com')
+  const returnsUser = await _returnsUser(companyEntityId, 'severus.snape@hogwarts.com')
+
+  // Set up additional contacts - linked to the company via the company contacts
+  const abstractionAlerts = await _abstractionAlertsContact(companyId)
+  const additionalContact = await _additionalContact(companyId, 'Horace Slughorn')
+
+  // Set up billing accounts
+  const billing = await _billing(companyId)
+
+  // Set up returns to - a contact linked through the licence document role to the company.
+  const returnsTo = await _returnsTo(company)
+  const otherReturnsTo = await _returnsTo(company, new Date('2020-01-01'))
+
+  // Extra contacts - ensures the 'FetchCompanyCRMDataService' returns all licences related to a company; we need to
+  // add an extra contact; this contact is linked to the company.
+  const extraCompanyEntity = await _licenceCompanyEntity('The Leaky Cauldron')
+  const extraLicence = await _licence(company)
+  const extraLicenceDocumentHeader = await _licenceDocumentHeader(
+    extraCompanyEntity.record.id,
+    extraLicence.record.licenceRef
+  )
+  const extraBasicUser = await _basicUser(extraCompanyEntity.record.id, 'rubeus.hagrid@hogwarts.com')
+
+  // Other contacts - are set up similarly to the 'extra' contact, but the company is different. The query should not
+  // return this as the company is different. This contact is not linked to the company.
+  const otherCompany = await _company("Weasleys' Wizard Wheezes")
+  const otherCompanyEntity = await _licenceCompanyEntity(otherCompany.record.name)
+  const otherLicence = await _licence(otherCompany)
+  const otherLicenceDocumentHeader = await _licenceDocumentHeader(
+    otherCompanyEntity.record.id,
+    otherLicence.record.licenceRef
+  )
+  const otherBasicUser = await _basicUser(otherCompanyEntity.record.id, 'Draco Malfoy')
+  const otherCompanyContact = await _additionalCompanyContact(otherCompany, additionalContact)
+
+  return {
+    abstractionAlerts,
+    additionalContact,
+    basicUser,
+    billing,
+    company,
+    extraBasicUser,
+    licence,
+    primaryUser,
+    returnsTo,
+    returnsUser,
+    clean: async () => {
+      await abstractionAlerts.clean()
+      await additionalContact.clean()
+      await basicUser.clean()
+      await billing.clean()
+      await company.clean()
+      await companyEntity.clean()
+      await extraBasicUser.clean()
+      await extraBasicUser.clean()
+      await extraCompanyEntity.clean()
+      await extraLicence.clean()
+      await extraLicenceDocumentHeader.clean()
+      await licence.clean()
+      await licenceDocumentHeader.clean()
+      await otherBasicUser.clean()
+      await otherCompany.clean()
+      await otherCompanyContact.clean()
+      await otherCompanyEntity.clean()
+      await otherLicence.clean()
+      await otherLicenceDocumentHeader.clean()
+      await otherReturnsTo.clean()
+      await primaryUser.clean()
+      await returnsTo.clean()
+      await returnsUser.clean()
+    }
+  }
+}
+
+async function _abstractionAlertsContact(companyId) {
+  const licenceRole = LicenceRoleHelper.select('additionalContact')
+
+  const contact = await ContactHelper.add({
+    contactType: 'person',
+    department: null,
+    firstName: 'Gilderoy',
+    lastName: 'Lockhart',
+    salutation: 'Prof'
+  })
+
+  const companyContact = await CompanyContactHelper.add({
+    contactId: contact.id,
+    licenceRoleId: licenceRole.id,
+    abstractionAlerts: true,
+    companyId
+  })
+
+  return {
+    record: companyContact,
+    clean: async () => {
+      await companyContact.$query().delete()
+      await contact.$query().delete()
+    }
+  }
+}
+
+async function _additionalContact(companyId, name) {
+  const licenceRole = LicenceRoleHelper.select('additionalContact')
+
+  const contact = await ContactHelper.add({
+    contactType: 'department',
+    department: name
+  })
+
+  const companyContact = await CompanyContactHelper.add({
+    contactId: contact.id,
+    licenceRoleId: licenceRole.id,
+    abstractionAlerts: false,
+    companyId
+  })
+
+  return {
+    record: companyContact,
+    clean: async () => {
+      await companyContact.$query().delete()
+      await contact.$query().delete()
+    }
+  }
+}
+
+/**
+ * An additional company contact using the contact id from the original company.
+ *
+ * This highlights the same contact can be associated with multiple companies.
+ *
+ * @private
+ */
+async function _additionalCompanyContact(company, additionalContact) {
+  const additionalCompanyContact = await CompanyContactHelper.add({
+    company: company.record.contactId,
+    contactId: additionalContact.record.contactId
+  })
+
+  return {
+    record: additionalCompanyContact,
+    clean: async () => {
+      await additionalCompanyContact.$query().delete()
+    }
+  }
+}
+
+async function _basicUser(companyEntityId, name) {
+  const licenceEntity = await LicenceEntityHelper.add({
+    name,
+    type: 'individual'
+  })
+
+  const licenceEntityRole = await LicenceEntityRoleHelper.add({
+    role: 'user',
+    companyEntityId,
+    licenceEntityId: licenceEntity.id
+  })
+
+  const user = await UserModel.add({
+    licenceEntityId: licenceEntity.id,
+    username: name
+  })
+
+  return {
+    record: user,
+    clean: async () => {
+      await licenceEntity.$query().delete()
+      await licenceEntityRole.$query().delete()
+      await user.$query().delete()
+    }
+  }
+}
+
+async function _billing(companyId) {
+  const billingAccount = await BillingAccountHelper.add({ companyId })
+
+  return {
+    record: billingAccount,
+    clean: async () => {
+      await billingAccount.$query().delete()
+    }
+  }
+}
+
+async function _company(name) {
+  const company = await CompanyHelper.add({
+    name
+  })
+
+  return {
+    record: company,
+    clean: async () => {
+      await company.$query().delete()
+    }
+  }
+}
+
+async function _licence(company) {
+  const licence = await LicenceHelper.add()
+
+  const licenceVersion = await LicenceVersionHelper.add({
+    licenceId: licence.id
+  })
+
+  const licenceVersionHolder = await LicenceVersionHolderHelper.add({
+    licenceVersionId: licenceVersion.id,
+    companyId: company.record.id
+  })
+
+  return {
+    record: licence,
+    clean: async () => {
+      await licence.$query().delete()
+      await licenceVersion.$query().delete()
+      await licenceVersionHolder.$query().delete()
+    }
+  }
+}
+
+async function _licenceDocumentHeader(companyEntityId, licenceRef) {
+  const licenceDocumentHeader = await LicenceDocumentHeaderHelper.add({
+    companyEntityId,
+    licenceRef
+  })
+
+  return {
+    record: licenceDocumentHeader,
+    clean: async () => {
+      await licenceDocumentHeader.$query().delete()
+    }
+  }
+}
+
+async function _licenceCompanyEntity(name) {
+  const licenceEntity = await LicenceEntityHelper.add({
+    name,
+    type: 'company'
+  })
+
+  return {
+    record: licenceEntity,
+    clean: async () => {
+      await licenceEntity.$query().delete()
+    }
+  }
+}
+
+async function _primaryUser(companyEntityId, name) {
+  const licenceEntity = await LicenceEntityHelper.add({
+    name,
+    type: 'individual'
+  })
+
+  const licenceEntityRole = await LicenceEntityRoleHelper.add({
+    role: 'primary_user',
+    companyEntityId,
+    licenceEntityId: licenceEntity.id
+  })
+
+  const user = await UserModel.add({
+    licenceEntityId: licenceEntity.id,
+    username: name
+  })
+
+  return {
+    record: user,
+    clean: async () => {
+      await licenceEntity.$query().delete()
+      await licenceEntityRole.$query().delete()
+      await user.$query().delete()
+    }
+  }
+}
+
+async function _returnsTo(company, endDate = null) {
+  const licenceRole = await LicenceRoleHelper.select('returnsTo')
+
+  const licenceDocumentRole = await LicenceDocumentRoleHelper.add({
+    companyId: company.record.id,
+    licenceRoleId: licenceRole.id,
+    endDate
+  })
+
+  return {
+    record: company,
+    clean: async () => {
+      await licenceDocumentRole.$query().delete()
+    }
+  }
+}
+
+async function _returnsUser(companyEntityId, name) {
+  const licenceEntity = await LicenceEntityHelper.add({
+    name,
+    type: 'individual'
+  })
+
+  const licenceEntityRole = await LicenceEntityRoleHelper.add({
+    role: 'user_returns',
+    companyEntityId,
+    licenceEntityId: licenceEntity.id
+  })
+
+  await LicenceEntityRoleHelper.add({
+    role: 'user',
+    companyEntityId,
+    licenceEntityId: licenceEntity.id
+  })
+
+  const user = await UserModel.add({
+    licenceEntityId: licenceEntity.id,
+    username: name
+  })
+
+  return {
+    record: user,
+    clean: async () => {
+      await licenceEntity.$query().delete()
+      await licenceEntityRole.$query().delete()
+      await user.$query().delete()
+    }
+  }
+}
+
+module.exports = {
+  seed
+}
