a24ac7a3739252773a3041d5af14d9e7fc272d84
jonathangoulding <goulding.jon@icloud.com>
2026-03-20 16:10:52 +0000

    chore: pre pr checks

--- app/services/company-contacts/setup/view-check.service.js
+++ app/services/company-contacts/setup/view-check.service.js
@@ -45,7 +45,7 @@ async function go(sessionId, yar) {
 async function _save(session, matchingContact) {
   if (matchingContact) {
     session.matchingContact = matchingContact
-    return session.$update()
+    await session.$update()
   }
 }
 
