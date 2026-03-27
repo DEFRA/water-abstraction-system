97026b8f6cd167649774d9b67a10941b6b80c617
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-03-27 12:54:31 +0000

    Update FetchLicenceService

--- test/services/return-versions/setup/fetch-licence.service.test.js
+++ test/services/return-versions/setup/fetch-licence.service.test.js
@@ -4,12 +4,12 @@
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
 
-const { describe, it, before } = (exports.lab = Lab.script())
+const { describe, it, before, after } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
+const CompanyHelper = require('../../../support/helpers/company.helper.js')
 const LicenceHelper = require('../../../support/helpers/licence.helper.js')
-const LicenceHolderSeeder = require('../../../support/seeders/licence-holder.seeder.js')
 const LicenceVersionHelper = require('../../../support/helpers/licence-version.helper.js')
 const ModLogHelper = require('../../../support/helpers/mod-log.helper.js')
 const ReturnRequirementHelper = require('../../../support/helpers/return-requirement.helper.js')
@@ -19,46 +19,58 @@ const ReturnVersionHelper = require('../../../support/helpers/return-version.hel
 const FetchLicenceService = require('../../../../app/services/return-versions/setup/fetch-licence.service.js')
 
 describe('Return Versions - Setup - Fetch Licence service', () => {
+  let company
+  let ignoredReturnVersion
   let licence
-  let licenceHolderSeedData
   let licenceVersion
   let modLog
+  let oldLicenceVersion
+  let returnRequirement
   let returnVersion
+  let supersededReturnVersion
+  let supersededReturnRequirement
 
   before(async () => {
     // Create a licence
     licence = await LicenceHelper.add()
 
+    // Create a company
+    company = await CompanyHelper.add()
+
     // Create two licence versions so we can test the service only gets the 'current' version
-    await LicenceVersionHelper.add({
+    oldLicenceVersion = await LicenceVersionHelper.add({
+      companyId: company.id,
+      endDate: new Date('2025-04-30'),
+      increment: 0,
+      issue: 1,
       licenceId: licence.id,
       startDate: licence.startDate,
       status: 'superseded'
     })
 
     licenceVersion = await LicenceVersionHelper.add({
+      companyId: company.id,
+      increment: 0,
+      issue: 2,
       licenceId: licence.id,
       startDate: new Date('2022-05-01')
     })
 
-    // Create a licence holder for the licence with the default name 'Licence Holder Ltd'
-    licenceHolderSeedData = await LicenceHolderSeeder.seed(licence.licenceRef)
-
     // Create a return version without return requirements: we'll test it is ignored
-    await ReturnVersionHelper.add({
+    ignoredReturnVersion = await ReturnVersionHelper.add({
       licenceId: licence.id,
       startDate: licence.startDate,
       version: 100
     })
 
     // Next create a superseded return version: we'll test it is ignore
-    const supersededReturnVersion = await ReturnVersionHelper.add({
+    supersededReturnVersion = await ReturnVersionHelper.add({
       licenceId: licence.id,
       startDate: licenceVersion.startDate,
       status: 'superseded',
       version: 101
     })
-    await ReturnRequirementHelper.add({ returnVersionId: supersededReturnVersion.id })
+    supersededReturnRequirement = await ReturnRequirementHelper.add({ returnVersionId: supersededReturnVersion.id })
 
     // Now create the current return version: we'll test its included in the result
     returnVersion = await ReturnVersionHelper.add({
@@ -66,13 +78,32 @@ describe('Return Versions - Setup - Fetch Licence service', () => {
       startDate: licenceVersion.startDate,
       version: 102
     })
+    returnRequirement = await ReturnRequirementHelper.add({ returnVersionId: returnVersion.id })
 
     // Add a mod log for the return version we'll return linked to the licence
     modLog = await ModLogHelper.add({
       reasonDescription: 'Record Loaded During Migration',
       returnVersionId: returnVersion.id
     })
-    await ReturnRequirementHelper.add({ returnVersionId: returnVersion.id })
+  })
+
+  after(async () => {
+    await modLog.$query().delete()
+
+    await returnRequirement.$query().delete()
+    await returnVersion.$query().delete()
+
+    await supersededReturnRequirement.$query().delete()
+    await supersededReturnVersion.$query().delete()
+
+    await ignoredReturnVersion.$query().delete()
+
+    await licenceVersion.$query().delete()
+    await oldLicenceVersion.$query().delete()
+
+    await company.$query().delete()
+
+    await licence.$query().delete()
   })
 
   describe('when the matching licence exists', () => {
@@ -90,7 +121,15 @@ describe('Return Versions - Setup - Fetch Licence service', () => {
         licenceVersions: [
           {
             id: licenceVersion.id,
-            startDate: licenceVersion.startDate
+            licenceId: licence.id,
+            issueDate: licenceVersion.issueDate,
+            startDate: licenceVersion.startDate,
+            status: licenceVersion.status,
+            company: {
+              id: company.id,
+              name: 'Example Trading Ltd',
+              type: 'organisation'
+            }
           }
         ],
         returnVersions: [
@@ -105,21 +144,7 @@ describe('Return Versions - Setup - Fetch Licence service', () => {
               }
             ]
           }
-        ],
-        licenceDocument: {
-          id: licenceHolderSeedData.licenceDocumentId,
-          licenceDocumentRoles: [
-            {
-              id: licenceHolderSeedData.licenceDocumentRoleId,
-              contact: null,
-              company: {
-                id: licenceHolderSeedData.companyId,
-                name: 'Licence Holder Ltd',
-                type: 'organisation'
-              }
-            }
-          ]
-        }
+        ]
       })
     })
 
