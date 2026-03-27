98426b227e2ee56a95fcf3b1109f9ac7f8e8e383
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-03-27 12:57:20 +0000

    Update the LicenceHolderSeeder

--- test/support/seeders/licence-holder.seeder.js
+++ test/support/seeders/licence-holder.seeder.js
@@ -5,44 +5,30 @@
  */
 
 const CompanyHelper = require('../helpers/company.helper.js')
-const LicenceDocumentHelper = require('../helpers/licence-document.helper.js')
-const LicenceDocumentRoleHelper = require('../helpers/licence-document-role.helper.js')
-const LicenceRoleHelper = require('../helpers/licence-role.helper.js')
+const LicenceVersionHelper = require('../helpers/licence-version.helper.js')
 
 /**
  * Adds a company to the provided licence that is set up to be the licence holder
  *
- * @param {string} licenceRef - The licence reference that the company will be the licence holder for
+ * @param {object} licence - The licence or an object representing it, with an `id` and `licenceRef` property
  * @param {string} [name] - The name of the company that will be the licence holder
  * @param {string} [regionId] - The ID of the region to assign to the company that will be created
  *
- * @returns {Promise<module:LicenceDocumentRoleHelper>} the licence document role which will link through to all the
- * entities that make up the licence holder
+ * @returns {Promise<object>} an object containing the created `company` and `licenceVersion` records
  */
-async function seed(licenceRef, name = 'Licence Holder Ltd', regionId = null) {
-  // Create a licence role (the default is licenceHolder)
-  const { id: licenceRoleId } = await LicenceRoleHelper.select()
-
+async function seed(licence, name = 'Licence Holder Ltd', regionId = null) {
   // Create a company record
-  const { id: companyId } = await CompanyHelper.add({ externalId: CompanyHelper.generateExternalId(), name, regionId })
+  const company = await CompanyHelper.add({ externalId: CompanyHelper.generateExternalId(), name, regionId })
 
-  // We have to create a licence document to link our licence record to (eventually!) the company or contact record that
-  // is the 'licence holder'
-  const { id: licenceDocumentId } = await LicenceDocumentHelper.add({ licenceRef })
-
-  // Create the licence document role record that _is_ linked to the correct licence holder record
-  const { id: licenceDocumentRoleId } = await LicenceDocumentRoleHelper.add({
-    licenceDocumentId,
-    licenceRoleId,
-    companyId,
-    startDate: new Date('2022-04-01')
-  })
+  const licenceVersion = await LicenceVersionHelper.add({ companyId: company.id, licenceId: licence.id })
 
   return {
-    companyId,
-    licenceDocumentId,
-    licenceDocumentRoleId,
-    licenceRoleId
+    company,
+    licenceVersion,
+    clean: async () => {
+      await company.$query().delete()
+      await licenceVersion.$query().delete()
+    }
   }
 }
 
