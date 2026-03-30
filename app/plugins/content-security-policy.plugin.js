3a234a1340fecaa95b3f2e3d1d5f399f055af8a9
Jason Claxton <jason.claxton@defra.gov.uk>
2026-03-30 15:30:36 +0100

    Implement `ContentSecurityPolicyPlugin` with default settings

---
+++ app/plugins/content-security-policy.plugin.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/**
+ * Plugin to set Content Security Policy headers on responses. This is a security measure to help prevent cross-site
+ * scripting (XSS) and other code injection attacks.
+ *
+ * {@link https://github.com/nlf/blankie|blankie}
+ *
+ * @module ContentSecurityPolicyPlugin
+ */
+
+const Blankie = require('blankie')
+
+const ContentSecurityPolicyPlugin = () => {
+  return {
+    plugin: Blankie,
+    options: {}
+  }
+}
+
+module.exports = ContentSecurityPolicyPlugin
