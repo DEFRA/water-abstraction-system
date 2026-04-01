f45e71af483d3d7ab0110307f6e88bbfbf9ab67e
jonathangoulding <goulding.jon@icloud.com>
2026-04-01 17:20:37 +0100

    chore: fix merge issues

--- test/support/seeders/crm-contacts.seeder.js
+++ test/support/seeders/crm-contacts.seeder.js
@@ -63,6 +63,45 @@ async function licenceHolder(licenceSeedData, name, existingRegionId = null) {
 }
 
 /**
+ * Adds a primary user
+ *
+ * A primary user is linked to the licence by the company entity id on the licence document header.
+ *
+ * We need to update the licence document header company entity id to the newly created company entity
+ *
+ * @param {object} licenceSeedData - The licence seed data
+ * @param {string} email - The email for the user
+ *
+ * @returns {Promise<object>} an object containing all records related to a primary user
+ */
+async function primaryUser(licenceSeedData, email) {
+  const individualEntity = await LicenceEntityHelper.add({ name: email, type: 'individual' })
+
+  const companyEntity = await LicenceEntityHelper.add({ type: 'company' })
+
+  await licenceSeedData.licenceDocumentHeader.$query().patch({
+    companyEntityId: companyEntity.id
+  })
+
+  const licenceEntityRole = await LicenceEntityRoleHelper.add({
+    companyEntityId: companyEntity.id,
+    licenceEntityId: individualEntity.id,
+    role: 'primary_user'
+  })
+
+  return {
+    companyEntity,
+    individualEntity,
+    licenceEntityRole,
+    clean: async () => {
+      await companyEntity.$query().delete()
+      await individualEntity.$query().delete()
+      await licenceEntityRole.$query().delete()
+    }
+  }
+}
+
+/**
  * Adds a 'returnsTo'
  *
  * We need to handle both a 'returnsTo' linked to the same licence holder as the licence and a 'returnsTo' not related to the licence holder
@@ -115,45 +154,6 @@ async function returnsTo(licenceSeedData, licenceHolderSeedData, name) {
       await licenceDocumentRole.$query().delete()
       await licenceHolderSeedData.clean()
       await licenceRole.$query().delete()
-    }
-  }
-}
-
-/**
- * Adds a primary user
- *
- * A primary user is linked to the licence by the company entity id on the licence document header.
- *
- * We need to update the licence document header company entity id to the newly created company entity
- *
- * @param {object} licenceSeedData - The licence seed data
- * @param {string} email - The email for the user
- *
- * @returns {Promise<object>} an object containing all records related to a primary user
- */
-async function primaryUser(licenceSeedData, email) {
-  const individualEntity = await LicenceEntityHelper.add({ name: email, type: 'individual' })
-
-  const companyEntity = await LicenceEntityHelper.add({ type: 'company' })
-
-  await licenceSeedData.licenceDocumentHeader.$query().patch({
-    companyEntityId: companyEntity.id
-  })
-
-  const licenceEntityRole = await LicenceEntityRoleHelper.add({
-    companyEntityId: companyEntity.id,
-    licenceEntityId: individualEntity.id,
-    role: 'primary_user'
-  })
-
-  return {
-    companyEntity,
-    individualEntity,
-    licenceEntityRole,
-    clean: async () => {
-      await companyEntity.$query().delete()
-      await individualEntity.$query().delete()
-      await licenceEntityRole.$query().delete()
     }
   }
 }
