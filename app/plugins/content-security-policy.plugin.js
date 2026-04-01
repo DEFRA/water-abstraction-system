5e3ca99242030220c99a54e5e9e2ca860d5db78a
Jason Claxton <jason.claxton@defra.gov.uk>
2026-04-01 12:07:21 +0100

    Update `ContentSecurityPolicyPlugin` JSDoc comment to better explain what it is doing

--- app/plugins/content-security-policy.plugin.js
+++ app/plugins/content-security-policy.plugin.js
@@ -4,6 +4,23 @@
  * Plugin to set Content Security Policy headers on responses. This is a security measure to help prevent cross-site
  * scripting (XSS) and other code injection attacks.
  *
+ * With the exception of the `fontSrc` and `generateNonces` options, the configuration for this plugin is based on the
+ * default configuration provided by the `blankie` package. The default options have been explicitly declared to make it
+ * clear what the defaults are. The `fontSrc` option is set to allow fonts to be loaded from the same origin as the
+ * application.
+ *
+ * In this application, `generateNonces: 'script'` instructs blankie to generate a unique nonce for each request and
+ * include it in the `Content-Security-Policy` header as part of the `script-src` directive. During request handling,
+ * blankie exposes this value at `request.plugins.blankie.nonces.script`. The views plugin then maps that value to
+ * `cspNonce`.
+ *
+ * The `cspNonce` view variable is consumed by `govuk/template.njk`, which uses it to add `nonce` attributes to the
+ * template's inline script tags. Our `layout.njk` extends that template and also applies `cspNonce` to its inline
+ * script that calls `window.GOVUKFrontend.initAll()`.
+ *
+ * This flow allows that specific inline script to run only when its nonce matches the value in the CSP header for the
+ * current response. Any other inline script without the correct nonce is blocked by the browser.
+ *
  * {@link https://github.com/nlf/blankie|blankie}
  *
  * @module ContentSecurityPolicyPlugin
