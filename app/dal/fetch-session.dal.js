5cf512cc38979b34550342c23a5dac90e0b7a09d
jonathangoulding <goulding.jon@icloud.com>
2026-04-02 10:57:16 +0100

    chore: pre pr checks

--- app/dal/fetch-session.dal.js
+++ app/dal/fetch-session.dal.js
@@ -1,7 +1,7 @@
 'use strict'
 
 /**
- * Fetches the session
+ * Fetches a session by its unique identifier.
  * @module FetchSessionDal
  */
 
@@ -9,12 +9,16 @@ const SessionModel = require('../models/session.model.js')
 const SessionNotFoundError = require('../errors/session-not-found.error.js')
 
 /**
- * Fetches the session
+ * Fetches a session by its unique identifier.
  *
- * @param {string} sessionId - The UUID of the session
+ * When the session does not exist, it throws a SessionNotFoundError. This is caught
+ * upstream and used to show the user a specific error message related to the session
+ * being invalid, rather than the generic 'There's a problem with the service'.
  *
- * @returns {Promise<object[]>} an object containing the session
- * @throws {SessionNotFoundError} If no session is found with the provided session id
+ * @param {string} sessionId - The UUID of the session to retrieve.
+ *
+ * @returns {Promise<object>} The session record found in the database.
+ * @throws {SessionNotFoundError} If no session is found with the provided sessionId.
  */
 async function go(sessionId) {
   const session = await SessionModel.query().findById(sessionId)
