48dad6b2ab786b825eeb71d609480f426a15d07d
robertparkinson <robert@daedalusft.com>
2026-04-07 13:27:56 +0100

    Update billing account address

--- app/services/billing-accounts/setup/submit-check.service.js
+++ app/services/billing-accounts/setup/submit-check.service.js
@@ -6,6 +6,10 @@
  * @module SubmitCheckService
  */
 
+const AddressModel = require('../../../models/address.model.js')
+const ChangeAddressService = require('../change-address.service.js')
+const FetchCompanyService = require('./fetch-company.service.js')
+const FetchCompanyContactsService = require('./fetch-company-contacts.service.js')
 const SessionModel = require('../../../models/session.model.js')
 
 /**
@@ -17,14 +21,88 @@ const SessionModel = require('../../../models/session.model.js')
  */
 async function go(sessionId) {
   const session = await SessionModel.query().findById(sessionId)
+  const { billingAccount } = session
 
-  await _save(session)
+  const existingAccount = !!session.existingAccount && session.existingAccount !== 'new'
+  const id = existingAccount ? session.existingAccount : session.billingAccount.company.id
+  const companyContacts = await FetchCompanyContactsService.go(id)
+  console.log(companyContacts)
+  const address = await _address(session)
+  const agentCompany = await _agentCompany(session, companyContacts, existingAccount)
+  const contact = _contact(session, companyContacts)
 
-  return {}
+  // await ChangeAddressService.go(billingAccount.id, address, agentCompany, contact)
+
+  return {
+    address,
+    agentCompany,
+    contact
+  }
 }
 
-async function _save(session) {
-  return session.$update()
+async function _address(session) {
+  const addressSelected = !!session.addressSelected && session.addressSelected !== 'new'
+
+  if (!addressSelected) {
+    return session.addressJourney.address
+  }
+
+  const existingAddress = await AddressModel.query().select(['addresses.id']).findById(session.addressSelected)
+
+  return {
+    id: existingAddress.id
+  }
+}
+
+async function _agentCompany(session, companyContacts, existingAccount) {
+  let companyId
+  let companyName = companyContacts.company.name
+
+  const anotherAccountSelected = session.accountSelected === 'another'
+
+  if (session.companiesHouseNumber) {
+    const companysHouseResult = await FetchCompanyService.go(session.companiesHouseNumber)
+
+    companyName = companysHouseResult.name
+  }
+
+  if (!anotherAccountSelected) {
+    companyId = session.billingAccount.company.id
+  } else if (existingAccount) {
+    companyId = session.existingAccount
+  }
+
+  return {
+    id: companyId,
+    name: companyName,
+    type: session.accountType === 'company' ? 'organisation' : 'person',
+    companyNumber: session.companiesHouseNumber ?? null
+  }
+}
+
+function _contact(session, companyContacts) {
+  if (session.fao === 'no') {
+    return {}
+  }
+
+  const { contacts } = companyContacts
+
+  const existingContact = !!session.contactSelected && session.contactSelected !== 'new'
+  let name = session.contactName
+
+  if (existingContact) {
+    const selectedContact = contacts.find((contact) => {
+      return contact.id === session.contactSelected
+    })
+
+    name = selectedContact.$name()
+  }
+
+  return {
+    ...(existingContact && { id: session.contactSelected }),
+    contactType: 'department',
+    department: name
+  }
 }
 
 module.exports = {
--- test/services/billing-accounts/setup/submit-check.service.test.js
+++ test/services/billing-accounts/setup/submit-check.service.test.js
@@ -3,34 +3,522 @@
 // Test framework dependencies
 const Lab = require('@hapi/lab')
 const Code = require('@hapi/code')
+const Sinon = require('sinon')
 
-const { describe, it, beforeEach } = (exports.lab = Lab.script())
+const { describe, it, before, beforeEach, afterEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
 const BillingAccountsFixture = require('../../../support/fixtures/billing-accounts.fixture.js')
 const SessionHelper = require('../../../support/helpers/session.helper.js')
 
+// Things to stub
+const AddressHelper = require('../../../support/helpers/address.helper.js')
+const ChangeAddressService = require('../../../../app/services/billing-accounts/change-address.service.js')
+const ContactHelper = require('../../../support/helpers/contact.helper.js')
+const CustomersFixture = require('../../../support/fixtures/customers.fixture.js')
+const FetchCompanyContactsService = require('../../../../app/services/billing-accounts/setup/fetch-company-contacts.service.js')
+const FetchCompanyService = require('../../../../app/services/billing-accounts/setup/fetch-company.service.js')
+const FetchCompaniesService = require('../../../../app/services/billing-accounts/setup/fetch-companies.service.js')
+
 // Thing under test
 const SubmitCheckService = require('../../../../app/services/billing-accounts/setup/submit-check.service.js')
 
 describe('Billing Accounts - Setup - Submit Check Service', () => {
+  let address
+  let contact
   let session
   let sessionData
+
+  const billingAccount = BillingAccountsFixture.billingAccount().billingAccount
+  const exampleContacts = CustomersFixture.companyContacts()
+  const companyContacts = {
+    company: billingAccount.company,
+    contacts: []
+  }
+
+  const companies = [
+    {
+      address: 'HORIZON HOUSE, DEANERY ROAD, BRISTOL, BS1 5AH',
+      number: '12345678',
+      title: 'ENVIRONMENT AGENCY'
+    }
+  ]
+
+  before(async () => {
+    address = await AddressHelper.add()
+    contact = await ContactHelper.add({
+      ...exampleContacts[0].contact
+    })
+
+    companyContacts.contacts.push(contact)
+  })
 
   beforeEach(async () => {
     sessionData = {
       billingAccount: BillingAccountsFixture.billingAccount().billingAccount
     }
 
-    session = await SessionHelper.add({ data: sessionData })
+    Sinon.stub(FetchCompanyService, 'go').returns(companies)
+    Sinon.stub(FetchCompanyContactsService, 'go').resolves(companyContacts)
   })
 
-  describe('when called', () => {
-    it('continues the journey', async () => {
-      const result = await SubmitCheckService.go(session.id)
+  afterEach(async () => {
+    await session.$query().delete()
+    Sinon.restore()
+  })
 
-      expect(result).to.equal({})
+  describe.only('when called', () => {
+    describe('and the user has selected the existing billing account', () => {
+      beforeEach(async () => {
+        sessionData.accountSelected = billingAccount.company.id
+      })
+
+      describe('and selected an existing address', () => {
+        beforeEach(async () => {
+          sessionData.addressSelected = address.id
+        })
+
+        describe('and added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactName = 'Contact Name'
+            sessionData.contactSelected = 'new'
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it.only('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and has not added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'no'
+            sessionData.contactName = null
+            sessionData.contactSelected = null
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and selected an existing contact as an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactSelected = contact.id
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it.only('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+      })
+
+      describe('and selected to create a new address', () => {
+        beforeEach(async () => {
+          sessionData.addressSelected = 'new'
+          sessionData.addressJourney = _addressJourney()
+        })
+
+        describe('and added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactName = 'Contact Name'
+            sessionData.contactSelected = 'new'
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and has not added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'no'
+            sessionData.contactName = null
+            sessionData.contactSelected = null
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and selected an existing contact as an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactSelected = contact.id
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it.only('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+      })
+    })
+
+    describe('and the user has searched for and chosen an existing billing account', () => {
+      beforeEach(async () => {
+        sessionData.accountSelected = 'another'
+        sessionData.existingAccount = billingAccount.company.id
+      })
+
+      describe('and selected an existing address', () => {
+        beforeEach(async () => {
+          sessionData.addressSelected = address.id
+        })
+
+        describe('and added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactName = 'Contact Name'
+            sessionData.contactSelected = 'new'
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and has not added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'no'
+            sessionData.contactName = null
+            sessionData.contactSelected = null
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and selected an existing contact as an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactSelected = contact.id
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it.only('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+      })
+
+      describe('and selected to create a new address', () => {
+        beforeEach(async () => {
+          sessionData.addressSelected = 'new'
+          sessionData.addressJourney = _addressJourney()
+        })
+
+        describe('and added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactName = 'Contact Name'
+            sessionData.contactSelected = 'new'
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and has not added an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'no'
+            sessionData.contactName = null
+            sessionData.contactSelected = null
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+
+        describe('and selected an existing contact as an FAO', () => {
+          beforeEach(async () => {
+            sessionData.fao = 'yes'
+            sessionData.contactSelected = contact.id
+
+            session = await SessionHelper.add({ data: sessionData })
+          })
+
+          it.only('creates a new billing account address', async () => {
+            const result = await SubmitCheckService.go(session.id)
+
+            expect(result).to.equal({})
+          })
+        })
+      })
+    })
+
+    describe('and the user has searched for an existing billing account but chose to create a new one', () => {
+      beforeEach(async () => {
+        sessionData.accountSelected = 'another'
+        sessionData.existingAccount = 'new'
+      })
+
+      describe('and selected "company" as the account type', () => {
+        beforeEach(async () => {
+          sessionData.accountType = 'company'
+          sessionData.existingAccount = 'new'
+        })
+
+        describe('and selected an existing address', () => {
+          describe('and added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactName = 'Contact Name'
+              sessionData.contactSelected = 'new'
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and has not added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'no'
+              sessionData.contactName = null
+              sessionData.contactSelected = null
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and selected an existing contact as an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactSelected = contact.id
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it.only('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+        })
+
+        describe('and selected to create a new address', () => {
+          describe('and added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactName = 'Contact Name'
+              sessionData.contactSelected = 'new'
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and has not added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'no'
+              sessionData.contactName = null
+              sessionData.contactSelected = null
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and selected an existing contact as an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactSelected = contact.id
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it.only('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+        })
+      })
+
+      describe('and selected "individual" as the account type', () => {
+        describe('and selected an existing address', () => {
+          describe('and added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactName = 'Contact Name'
+              sessionData.contactSelected = 'new'
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and has not added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'no'
+              sessionData.contactName = null
+              sessionData.contactSelected = null
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and selected an existing contact as an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactSelected = contact.id
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it.only('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+        })
+
+        describe('and selected to create a new address', () => {
+          describe('and added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactName = 'Contact Name'
+              sessionData.contactSelected = 'new'
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and has not added an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'no'
+              sessionData.contactName = null
+              sessionData.contactSelected = null
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+
+          describe('and selected an existing contact as an FAO', () => {
+            beforeEach(async () => {
+              sessionData.fao = 'yes'
+              sessionData.contactSelected = contact.id
+
+              session = await SessionHelper.add({ data: sessionData })
+            })
+
+            it.only('creates a new billing account address', async () => {
+              const result = await SubmitCheckService.go(session.id)
+
+              expect(result).to.equal({})
+            })
+          })
+        })
+      })
     })
   })
 })
+
+function _addressJourney() {
+  return {
+    address: {
+      uprn: 12345678,
+      postcode: 'BS1 5AH',
+      addressLine1: 'ENVIRONMENT AGENCY',
+      addressLine2: 'HORIZON HOUSE',
+      addressLine3: null,
+      addressLine4: 'BRISTOL'
+    }
+  }
+}
