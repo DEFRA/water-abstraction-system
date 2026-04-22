2df003beb60b2b1b72c9f3e31be2cda4a4f419e8
jonathangoulding <goulding.jon@icloud.com>
2026-04-22 14:36:37 +0100

    Update bill runs session delete
    https://eaflood.atlassian.net/browse/WATER-5573
    
    We're seeing a bug on the tst env where the user can immediately press back after the bills runs check page. This is an issue on test which is running with low resources.
    
    This change lifts the delete session immediately after the fetch to avoid this issue.
    
    This change also updates the session error message.

--- test/services/bill-runs/setup/create.service.test.js
+++ test/services/bill-runs/setup/create.service.test.js
@@ -13,7 +13,6 @@ const SessionModelStub = require('../../../support/stubs/session.stub.js')
 const { engineTriggers } = require('../../../../app/lib/static-lookups.lib.js')
 
 // Things we need to stub
-const DeleteSessionDal = require('../../../../app/dal/delete-session.dal.js')
 const LegacyCreateBillRunRequest = require('../../../../app/requests/legacy/create-bill-run.request.js')
 const StartBillRunProcessService = require('../../../../app/services/bill-runs/start-bill-run-process.service.js')
 
@@ -33,8 +32,6 @@ describe('Bill Runs - Setup - Create service', () => {
   beforeEach(() => {
     legacyCreateBillRunRequestStub = Sinon.stub(LegacyCreateBillRunRequest, 'send')
     startBillRunProcessServiceStub = Sinon.stub(StartBillRunProcessService, 'go')
-
-    Sinon.stub(DeleteSessionDal, 'go').resolves()
   })
 
   afterEach(() => {
@@ -58,12 +55,6 @@ describe('Bill Runs - Setup - Create service', () => {
       ).to.be.true()
       expect(legacyCreateBillRunRequestStub.calledWith('supplementary', regionId, 2025, user, false)).to.be.true()
     })
-
-    it('deletes the setup session', async () => {
-      await CreateService.go(session, blockingResults, user)
-
-      expect(DeleteSessionDal.go.calledWith(session.id)).to.be.true()
-    })
   })
 
   describe('when the "blockingResults" determines only the "current" bill run should be triggered', () => {
@@ -81,12 +72,6 @@ describe('Bill Runs - Setup - Create service', () => {
       expect(startBillRunProcessServiceStub.calledWith(regionId, 'annual', 'carol.shaw@atari.com', 2025)).to.be.true()
       expect(legacyCreateBillRunRequestStub.called).to.be.false()
     })
-
-    it('deletes the setup session', async () => {
-      await CreateService.go(session, blockingResults, user)
-
-      expect(DeleteSessionDal.go.calledWith(session.id)).to.be.true()
-    })
   })
 
   describe('when the "blockingResults" determines only the "old" bill run should be triggered', () => {
@@ -103,12 +88,6 @@ describe('Bill Runs - Setup - Create service', () => {
 
       expect(startBillRunProcessServiceStub.called).to.be.false()
       expect(legacyCreateBillRunRequestStub.calledWith('two_part_tariff', regionId, 2022, user, true)).to.be.true()
-    })
-
-    it('deletes the setup session', async () => {
-      await CreateService.go(session, blockingResults, user)
-
-      expect(DeleteSessionDal.go.calledWith(session.id)).to.be.true()
     })
   })
 })
