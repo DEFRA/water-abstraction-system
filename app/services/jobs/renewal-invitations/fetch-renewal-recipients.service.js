6ed8e6c46345e78ba4fc7ebcc422e4d6168f57b2
jonathangoulding <goulding.jon@icloud.com>
2026-04-22 11:15:07 +0100

    feat: use generate query patterns

--- app/services/jobs/renewal-invitations/fetch-renewal-recipients.service.js
+++ app/services/jobs/renewal-invitations/fetch-renewal-recipients.service.js
@@ -5,17 +5,11 @@
  * @module FetchRenewalRecipients
  */
 
+const GenerateRenewalRecipientsQueryService = require('./generate-renewal-recipients-query.service.js')
 const { db } = require('../../../../db/db.js')
 
 /**
  * Fetches recipients for renewal invitations
- *
- * When fetching renewal invitation recipients, we are only concerned with the licence holder and/or primary user.
- *
- * When a licence is registered, then a primary user will exist; when this is the case, we return the primary user and
- * not the licence holder.
- *
- * If the licence is not registered, then we return the licence holder.
  *
  * The licence expired date is calculated and provided upstream.
  *
@@ -24,149 +18,13 @@ const { db } = require('../../../../db/db.js')
  * @returns {Promise<object[]>} - An array of recipients for with an
  */
 async function go(expiredDate) {
-  const licenceHolderQuery = _licenceHolderQuery()
-  const primaryUserQuery = _primaryUserQuery()
-  const processQuery = _processForSending()
+  // cte licences = revoked, expired or lapsed <= (no on expiry date) - and have as the expiry
 
-  const { rows } = await db.raw(
-    `
-    WITH
-        primary_user as (
-          ${primaryUserQuery}
-        ),
-        -- Which licences are registered (have a primary user). This CTE is used in the next CTE to filter out
-        -- records linked to licences that are registered.
-        registered_licences AS (
-          SELECT DISTINCT licence_ref FROM primary_user
-        ),
-        licence_holder as (
-          ${licenceHolderQuery}
-        ),
-        all_contacts AS (
-          SELECT * FROM primary_user
-          UNION ALL
-          SELECT * FROM licence_holder
-        ),
-        ${processQuery}
+  const query = GenerateRenewalRecipientsQueryService.go()
 
-      SELECT * FROM results;
-    `,
-    [expiredDate, expiredDate]
-  )
+  const { rows } = await db.raw(`${query}`, [expiredDate, expiredDate])
 
   return rows
-}
-
-function _licenceHolderQuery() {
-  return `
-    SELECT
-      ('licence holder') AS contact_type,
-      2 AS priority,
-      jsonb_build_object(
-        'name', c.name,
-        'address1', a.address_1,
-        'address2', a.address_2,
-        'address3', a.address_3,
-        'address4', a.address_4,
-        'address5', a.address_5,
-        'address6', a.address_6,
-        'postcode', a.postcode,
-        'country', a.country
-      ) AS contact,
-      MD5(LOWER(CONCAT(
-        c.name,
-        a.address_1,
-        a.address_2,
-        a.address_3,
-        a.address_4,
-        a.address_5,
-        a.address_6,
-        a.postcode,
-        a.country
-      ))) AS contact_hash_id,
-      NULL::TEXT AS email,
-      l.licence_ref,
-      ('Letter') AS message_type
-    FROM
-      public.licences l
-    INNER JOIN (
-      SELECT DISTINCT ON (lv.licence_id)
-        lv.licence_id,
-        lv.company_id,
-        lv.address_id
-      FROM
-        public.licence_versions lv
-      WHERE
-        lv.start_date <= CURRENT_DATE
-      ORDER BY
-        lv.licence_id ASC,
-        lv."issue" DESC,
-        lv."increment" DESC,
-        lv.end_date DESC NULLS FIRST
-    ) AS llv ON llv.licence_id = l.id
-    INNER JOIN public.companies c ON c.id = llv.company_id
-    INNER JOIN public.addresses a ON a.id = llv.address_id
-    LEFT JOIN registered_licences rl
-      ON rl.licence_ref = l.licence_ref
-    WHERE
-      l.expired_date = ?
-    AND rl.licence_ref IS NULL
-  `
-}
-
-function _primaryUserQuery() {
-  return `
-    SELECT
-      ('primary user') AS contact_type,
-      1 AS priority,
-      NULL::jsonb AS contact,
-      md5(LOWER(le."name")) AS contact_hash_id,
-      le."name" AS email,
-      ldh.licence_ref,
-      ('Email') as message_type
-    FROM public.licence_document_headers ldh
-    INNER JOIN public.licence_entity_roles ler
-      ON ler.company_entity_id = ldh.company_entity_id AND ler."role" = 'primary_user'
-    INNER JOIN public.licence_entities le
-      ON le.id = ler.licence_entity_id
-    INNER JOIN public.licences l
-      ON l.licence_ref = ldh.licence_ref
-    WHERE l.expired_date = ?
-    `
-}
-
-function _processForSending() {
-  return `
-  -- PROCESS FOR SENDING NOTICES
-
-  -- Aggregate all licence_refs per contact_hash_id
-  aggregated_contact_data AS (
-    SELECT
-      contact_hash_id,
-      JSONB_AGG(DISTINCT licence_ref ORDER BY licence_ref) AS licence_refs
-    FROM all_contacts
-    GROUP BY contact_hash_id
-  ),
-
-  -- Select the highest priority contact details (Email > Letter)
-  -- and join the aggregated licence list
-  results AS (
-    SELECT DISTINCT ON (ac.contact_hash_id)
-      ac.contact,
-      ac.contact_hash_id,
-      ac.contact_type,
-      ac.email,
-      acd.licence_refs,
-      ac.message_type
-    FROM
-      all_contacts ac
-    INNER JOIN aggregated_contact_data acd
-      ON acd.contact_hash_id = ac.contact_hash_id
-    ORDER BY
-      ac.contact_hash_id,
-      ac.priority ASC
-  )
-  `
 }
 
 module.exports = {
---
+++ test/services/jobs/renewal-invitations/generate-renewal-recipients-query.service.test.js
@@ -0,0 +1,129 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, before, after } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const RecipientScenariosSeeder = require('../../../support/seeders/recipient-scenarios.seeder.js')
+
+// Thing under test
+const GenerateRenewalRecipientsQueryService = require('../../../../app/services/jobs/renewal-invitations/generate-renewal-recipients-query.service.js')
+const { db } = require('../../../../db/db.js')
+
+describe('Jobs - Renewal Invitations - Fetch Renewal recipients service', () => {
+  let clock
+  let expiredDate
+  let scenarios
+  let todayDate
+
+  before(async () => {
+    todayDate = new Date('2026-04-15')
+
+    clock = Sinon.useFakeTimers(todayDate)
+
+    scenarios = []
+
+    let scenario
+
+    // 1) Licence holder only
+    expiredDate = new Date('2027-02-09')
+    scenario = await RecipientScenariosSeeder.licenceHolderOnly([], expiredDate)
+    scenarios.push(scenario)
+
+    // 2) Same licence holder, but is linked to multiple licences with due return logs - only one recipient record
+    // will be returned with multiple licence refs
+    expiredDate = new Date('2027-02-10')
+    scenario = await RecipientScenariosSeeder.licenceHolderWithMultipleLicences([], expiredDate)
+    scenarios.push(scenario)
+
+    // 3) Primary user only. All licences have a licence holder record, but when 'registered' the query will only
+    // return the primary user recipient.
+    expiredDate = new Date('2027-02-11')
+    scenario = await RecipientScenariosSeeder.primaryUserOnly([], expiredDate)
+    scenarios.push(scenario)
+
+    // 7) Same primary user, but is linked to multiple licences - only one recipient record will be returned with
+    // multiple licence refs
+    expiredDate = new Date('2027-02-12')
+    scenario = await RecipientScenariosSeeder.primaryUserWithMultipleLicences([], expiredDate)
+    scenarios.push(scenario)
+  })
+
+  after(async () => {
+    await RecipientScenariosSeeder.clean(scenarios)
+
+    clock.restore()
+    Sinon.restore()
+  })
+
+  describe('when there are renewal invitations to send', () => {
+    it('(Scenario 1) returns the licence holder when only the licence holder is present', async () => {
+      const query = GenerateRenewalRecipientsQueryService.go()
+
+      const { rows } = await db.raw(query, [new Date('2027-02-09'), new Date('2027-02-09')])
+
+      const sendingResults = RecipientScenariosSeeder.transformToSendingResults(scenarios[0])
+
+      _formatScenario(sendingResults[0])
+
+      expect(rows).to.equal(sendingResults)
+    })
+
+    it('(Scenario 2) returns the licence holder when only it is present and the same for multiple licences', async () => {
+      const query = GenerateRenewalRecipientsQueryService.go()
+
+      const { rows } = await db.raw(query, [new Date('2027-02-10'), new Date('2027-02-10')])
+
+      const sendingResults = RecipientScenariosSeeder.transformToSendingResults(scenarios[1])
+
+      _formatScenario(sendingResults[0])
+      _formatScenario(sendingResults[1])
+
+      expect(rows).to.contain(sendingResults[0])
+      // We could test rows contains the second licence holder recipient recorded in the scenario, but they are the
+      // same so it would also return true and not prove anything
+      expect(sendingResults[0]).to.equal(sendingResults[1])
+    })
+
+    it('(Scenario 3) returns only the primary user when the licence', async () => {
+      const query = GenerateRenewalRecipientsQueryService.go()
+
+      const { rows } = await db.raw(query, [new Date('2027-02-11'), new Date('2027-02-11')])
+
+      const sendingResults = RecipientScenariosSeeder.transformToSendingResults(scenarios[2])
+
+      _formatScenario(sendingResults[1])
+
+      expect(rows).to.contain(sendingResults[1])
+
+      // NOTE: When a licence is registered sendingResult[0] will always reference the licence holder
+      expect(rows).not.to.contain(sendingResults[0])
+    })
+
+    it('(Scenario 4) returns only the primary user when it is the same for multiple registered licences', async () => {
+      const query = GenerateRenewalRecipientsQueryService.go()
+
+      const { rows } = await db.raw(query, [new Date('2027-02-12'), new Date('2027-02-12')])
+
+      const sendingResults = RecipientScenariosSeeder.transformToSendingResults(scenarios[3])
+
+      _formatScenario(sendingResults[2])
+
+      expect(rows).to.contain(sendingResults[2])
+
+      // NOTE: When a licence is registered, sendingResult[0] will always reference the licence holder
+      expect(rows).not.to.contain(sendingResults[0])
+    })
+  })
+})
+
+function _formatScenario(sendingResults) {
+  delete sendingResults.due_date_status
+  delete sendingResults.return_log_ids
+  delete sendingResults.latest_due_date
+}
