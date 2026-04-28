05ef75d59ca70e935ee8cd48f82689439d779910
Jason Claxton <jason.claxton@defra.gov.uk>
2026-04-28 11:27:50 +0100

    Add `userEmail` to controller

--- app/controllers/users-setup.controller.js
+++ app/controllers/users-setup.controller.js
@@ -1,18 +1,28 @@
 'use strict'
 
 /**
- * Controller for /users endpoints
+ * Controller for /users/internal/setup endpoints
  * @module UsersController
  */
 
 const InitiateSessionService = require('../services/users/internal/setup/initiate-session.service.js')
+const UserEmailService = require('../services/users/internal/setup/user-email.service.js')
 
-async function setup(request, h) {
+async function userEmail(request, h) {
+  const { sessionId } = request.params
+
+  const pageData = await UserEmailService.go(sessionId)
+
+  return h.view('users/internal/setup/user-email.njk', pageData)
+}
+
+async function setup(_request, h) {
   const { id: sessionId } = await InitiateSessionService.go()
 
-  return h.redirect(`/system/users/internal/setup/${sessionId}/enter-email`)
+  return h.redirect(`/system/users/internal/setup/${sessionId}/user-email`)
 }
 
 module.exports = {
-  setup
+  setup,
+  userEmail
 }
