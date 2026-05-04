8754817ce1892abe8f81ecb73e82c69e2e104728
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-05-04 23:52:56 +0100

    Add the view verifications service

---
+++ app/services/users/external/view-verifications.service.js
@@ -0,0 +1,48 @@
+'use strict'
+
+/**
+ * Orchestrates fetching and presenting external user data for `/users/external/{id}/verifications` page
+ * @module ViewVerificationsService
+ */
+
+const FetchVerificationsService = require('./fetch-verifications.service.js')
+const FetchUserService = require('../../../dal/users/fetch-user.dal.js')
+const PaginatorPresenter = require('../../../presenters/paginator.presenter.js')
+const VerificationsPresenter = require('../../../presenters/users/external/verifications.presenter.js')
+
+/**
+ * Orchestrates fetching and presenting external user data for `/users/external/{id}/verifications` page
+ *
+ * @param {number} id - The user's ID
+ * @param {string} page - The current page for the pagination service
+ * @param {string} back - The 'back' query parameter, used to indicate what back link should be shown on the page
+ *
+ * @returns {Promise<object>} The view data for the external user page
+ */
+async function go(id, page, back = 'users') {
+  const user = await FetchUserService.go(id)
+
+  const { verifications, totalNumber } = await FetchVerificationsService.go(user.licenceEntityId, page)
+
+  const pageData = VerificationsPresenter.go(user, verifications, back)
+
+  const pagination = PaginatorPresenter.go(
+    totalNumber,
+    page,
+    `/system/users/external/${id}/verifications`,
+    verifications.length,
+    'verifications',
+    { back }
+  )
+
+  return {
+    activeNavBar: back === 'users' ? 'users' : 'search',
+    activeSecondaryNav: 'verifications',
+    pagination,
+    ...pageData
+  }
+}
+
+module.exports = {
+  go
+}
---
+++ test/services/users/external/view-verifications.service.test.js
@@ -0,0 +1,88 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+const Sinon = require('sinon')
+
+const { describe, it, beforeEach, afterEach } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const UsersFixture = require('../../../support/fixtures/users.fixture.js')
+
+// Things we want to stub
+const FetchUserDal = require('../../../../app/dal/users/fetch-user.dal.js')
+const FetchVerificationsService = require('../../../../app/services/users/external/fetch-verifications.service.js')
+
+// Thing under test
+const ViewVerificationsService = require('../../../../app/services/users/external/view-verifications.service.js')
+
+describe('Users - External - View Verifications service', () => {
+  const page = '1'
+
+  let back
+  let user
+
+  beforeEach(() => {
+    const { id, username } = UsersFixture.external()
+
+    user = { id, licenceEntityId: 'b2c55396-9bbb-448d-85e7-2be1dbefc02b', username }
+
+    Sinon.stub(FetchUserDal, 'go').resolves(user)
+    Sinon.stub(FetchVerificationsService, 'go').resolves({
+      verifications: [],
+      totalNumber: 0
+    })
+  })
+
+  afterEach(() => {
+    Sinon.restore()
+  })
+
+  describe('when called', () => {
+    it('returns page data for the view', async () => {
+      const result = await ViewVerificationsService.go(user.id, page, back)
+
+      expect(result).to.equal({
+        activeNavBar: 'users',
+        activeSecondaryNav: 'verifications',
+        pagination: {
+          currentPageNumber: 1,
+          numberOfPages: 0,
+          showingMessage: 'Showing all 0 verifications'
+        },
+        backLink: {
+          href: '/system/users',
+          text: 'Go back to users'
+        },
+        backQueryString: null,
+        pageTitle: 'Verifications',
+        pageTitleCaption: user.username,
+        verifications: []
+      })
+    })
+
+    describe('the "activeNavBar" property', () => {
+      describe('when the "back" query parameter is "undefined"', () => {
+        it('defaults to ""users" and returns "users"', async () => {
+          const result = await ViewVerificationsService.go(user.id, page, back)
+
+          expect(result.activeNavBar).to.equal('users')
+        })
+      })
+
+      describe('when the "back" query parameter is not "users" (for example "search")', () => {
+        beforeEach(() => {
+          back = 'search'
+        })
+
+        it('returns "search"', async () => {
+          const result = await ViewVerificationsService.go(user.id, page, back)
+
+          expect(result.activeNavBar).to.equal('search')
+        })
+      })
+    })
+  })
+})
