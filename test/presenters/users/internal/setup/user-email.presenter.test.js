3e7fb8da161b3baa3192ee1e287c0c95449393f0
Jason Claxton <jason.claxton@defra.gov.uk>
2026-05-05 11:08:38 +0100

    Drop the outer `describe('when called')` block as per review comment

--- test/presenters/users/internal/setup/user-email.presenter.test.js
+++ test/presenters/users/internal/setup/user-email.presenter.test.js
@@ -20,40 +20,38 @@ describe('Users - Internal - Setup - User Email Presenter', () => {
     session = { id: generateUUID() }
   })
 
-  describe('when called', () => {
-    it('correctly presents the data', () => {
-      const result = UserEmailPresenter.go(session)
+  it('correctly presents the data', () => {
+    const result = UserEmailPresenter.go(session)
 
-      expect(result).to.equal({
-        backLink: {
-          href: '/system/users',
-          text: 'Back'
-        },
-        email: null,
-        pageTitle: 'Enter an email address for the user',
-        pageTitleCaption: 'Internal'
+    expect(result).to.equal({
+      backLink: {
+        href: '/system/users',
+        text: 'Back'
+      },
+      email: null,
+      pageTitle: 'Enter an email address for the user',
+      pageTitleCaption: 'Internal'
+    })
+  })
+
+  describe('the "email" property', () => {
+    describe('when the email has previously been saved', () => {
+      beforeEach(() => {
+        session.email = 'BOB@test.com'
+      })
+
+      it('returns the email from the session in lowercase', () => {
+        const result = UserEmailPresenter.go(session)
+
+        expect(result.email).to.equal('bob@test.com')
       })
     })
 
-    describe('the "email" property', () => {
-      describe('when the email has previously been saved', () => {
-        beforeEach(() => {
-          session.email = 'BOB@test.com'
-        })
+    describe('when the email has not previously been saved', () => {
+      it('returns an empty string', () => {
+        const result = UserEmailPresenter.go(session)
 
-        it('returns the email from the session in lowercase', () => {
-          const result = UserEmailPresenter.go(session)
-
-          expect(result.email).to.equal('bob@test.com')
-        })
-      })
-
-      describe('when the email has not previously been saved', () => {
-        it('returns an empty string', () => {
-          const result = UserEmailPresenter.go(session)
-
-          expect(result.email).to.be.null()
-        })
+        expect(result.email).to.be.null()
       })
     })
   })
