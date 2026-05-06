daf033b5144defd9dd9a126c122757a82c7b0d6f
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-05-06 23:04:19 +0100

    Remove licences from details presenter

--- test/presenters/users/external/details.presenter.test.js
+++ test/presenters/users/external/details.presenter.test.js
@@ -8,33 +8,24 @@ const { describe, it, beforeEach } = (exports.lab = Lab.script())
 const { expect } = Code
 
 // Test helpers
-const LicenceModel = require('../../../../app/models/licence.model.js')
 const UsersFixture = require('../../../support/fixtures/users.fixture.js')
-const { generateUUID, today } = require('../../../../app/lib/general.lib.js')
-const { tomorrow } = require('../../../support/general.js')
 
 // Thing under test
 const DetailsPresenter = require('../../../../app/presenters/users/external/details.presenter.js')
 
 describe('Users - External - Details Presenter', () => {
   let back
-  let licences
   let user
   let viewingUserScope
 
   beforeEach(() => {
     back = 'users'
-    licences = [
-      _licence('FE/TC/H/US/ER/01', today(), 'user'),
-      _licence('FE/TC/H/US/ER/02', tomorrow(), 'user_returns'),
-      _licence('FE/TC/H/US/ER/03', null, 'primary_user')
-    ]
     user = UsersFixture.external()
     viewingUserScope = ['manage_accounts']
   })
 
   it('correctly presents the data', () => {
-    const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
+    const result = DetailsPresenter.go(user, viewingUserScope, back)
 
     expect(result).to.equal({
       activeNavBar: 'users',
@@ -43,71 +34,19 @@ describe('Users - External - Details Presenter', () => {
         text: 'Go back to users'
       },
       backQueryString: '?back=users',
-      displayLicenceEndedMessage: true,
       lastSignedIn: '6 October 2022 at 10:00:00',
-      licences: [
-        {
-          currentLicenceHolder: licences[0].licenceVersions[0].licenceVersionHolder.derivedName,
-          id: licences[0].id,
-          licenceLink: `/system/licences/${licences[0].id}/summary`,
-          licenceRef: licences[0].licenceRef,
-          permissions: 'Basic access',
-          status: 'expired'
-        },
-        {
-          currentLicenceHolder: licences[1].licenceVersions[0].licenceVersionHolder.derivedName,
-          id: licences[1].id,
-          licenceLink: `/system/licences/${licences[1].id}/summary`,
-          licenceRef: licences[1].licenceRef,
-          permissions: 'Returns user',
-          status: null
-        },
-        {
-          currentLicenceHolder: licences[2].licenceVersions[0].licenceVersionHolder.derivedName,
-          id: licences[2].id,
-          licenceLink: `/system/licences/${licences[2].id}/summary`,
-          licenceRef: licences[2].licenceRef,
-          permissions: 'Primary user',
-          status: null
-        }
-      ],
       pageTitle: 'User details',
       pageTitleCaption: user.username,
       permissions: 'None',
       roles: [],
-      showEditButton: true,
       status: 'enabled'
-    })
-  })
-
-  describe('the "displayLicenceEndedMessage" property', () => {
-    describe('when at least one licence has a status of "expired", "revoked" or "lapsed"', () => {
-      it('returns "true"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
-
-        expect(result.displayLicenceEndedMessage).to.be.true()
-      })
-    })
-
-    describe('when no licences have a status of "expired", "revoked" or "lapsed"', () => {
-      beforeEach(() => {
-        for (const licence of licences) {
-          licence.expiredDate = null
-        }
-      })
-
-      it('returns "false"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
-
-        expect(result.displayLicenceEndedMessage).to.be.false()
-      })
     })
   })
 
   describe('the "lastSignedIn" property', () => {
     describe('when the lastLogin is set', () => {
       it('returns the last signed in date and time', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
+        const result = DetailsPresenter.go(user, viewingUserScope, back)
 
         expect(result.lastSignedIn).to.equal('6 October 2022 at 10:00:00')
       })
@@ -119,7 +58,7 @@ describe('Users - External - Details Presenter', () => {
       })
 
       it('returns "Never signed in"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
+        const result = DetailsPresenter.go(user, viewingUserScope, back)
 
         expect(result.lastSignedIn).to.equal('Never signed in')
       })
@@ -133,7 +72,7 @@ describe('Users - External - Details Presenter', () => {
       })
 
       it('returns an empty array', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
+        const result = DetailsPresenter.go(user, viewingUserScope, back)
 
         expect(result.roles).to.be.empty()
       })
@@ -145,7 +84,7 @@ describe('Users - External - Details Presenter', () => {
       })
 
       it('returns the correct roles for a "Returns user"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
+        const result = DetailsPresenter.go(user, viewingUserScope, back)
 
         expect(result.roles).to.equal([
           {
@@ -162,7 +101,7 @@ describe('Users - External - Details Presenter', () => {
       })
 
       it('returns the correct roles for a "Primary user"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
+        const result = DetailsPresenter.go(user, viewingUserScope, back)
 
         expect(result.roles).to.equal([
           {
@@ -177,69 +116,4 @@ describe('Users - External - Details Presenter', () => {
       })
     })
   })
-
-  describe('the "showEditButton" property', () => {
-    describe('when the viewing user has "manage_accounts" in their scope', () => {
-      it('returns "true"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
-
-        expect(result.showEditButton).to.be.true()
-      })
-    })
-
-    describe('when the viewing user does not have "manage_accounts" in their scope', () => {
-      beforeEach(() => {
-        viewingUserScope = []
-      })
-
-      it('returns "false"', () => {
-        const result = DetailsPresenter.go(user, licences, viewingUserScope, back)
-
-        expect(result.showEditButton).to.be.false()
-      })
-    })
-  })
 })
-
-function _licence(licenceRef, expiredDate, role) {
-  const licenceVersionId = generateUUID()
-  const licenceDocumentHeaderId = generateUUID()
-
-  const licence = LicenceModel.fromJson({
-    expiredDate,
-    id: generateUUID(),
-    lapsedDate: null,
-    licenceRef,
-    revokedDate: null
-  })
-
-  licence.licenceVersions = [
-    {
-      id: licenceVersionId,
-      licenceId: licence.id,
-      licenceVersionHolder: {
-        derivedName: 'Current Holder',
-        id: generateUUID(),
-        licenceVersionId
-      }
-    }
-  ]
-
-  licence.licenceDocumentHeader = {
-    id: licenceDocumentHeaderId,
-    licenceEntityRoles: _licenceEntityRoles(role),
-    licenceRef: licence.licenceRef
-  }
-
-  return licence
-}
-
-function _licenceEntityRoles(role) {
-  const licenceEntityRoles = [{ id: generateUUID(), role }]
-
-  if (role === 'user_returns') {
-    licenceEntityRoles.push({ id: generateUUID(), role })
-  }
-
-  return licenceEntityRoles
-}
