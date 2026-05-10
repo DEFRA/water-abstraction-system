8748e5bf830a0675b7394606a0188acbfe54a6af
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-05-10 11:48:09 +0100

    Add FetchLicences DAL
    Though we already have a FetchLicencesDal, we need a different version for the unlink licences journey.
    
    The first difference is we won't be paging them when we list them out for selection. So, the query needs to lose its paging.
    
    We also won't be displaying the users role for the licence. That is unnecessary at this point. So we can drop linking through to the licence document header and associated licence entity roles.
    
    But we will display the licence holder as an aid to help the when it comes to selecting licences to unlink. So, we keep the licence holder modifier.

---
+++ test/dal/users/external/setup/fetch-licences.dal.test.js
@@ -0,0 +1,169 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, before, after } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const CompanyHelper = require('../../../../support/helpers/company.helper.js')
+const LicenceDocumentHeaderHelper = require('../../../../support/helpers/licence-document-header.helper.js')
+const LicenceEntityHelper = require('../../../../support/helpers/licence-entity.helper.js')
+const LicenceEntityRoleHelper = require('../../../../support/helpers/licence-entity-role.helper.js')
+const LicenceHelper = require('../../../../support/helpers/licence.helper.js')
+const LicenceVersionHelper = require('../../../../support/helpers/licence-version.helper.js')
+const { generateUUID } = require('../../../../../app/lib/general.lib.js')
+
+// Thing under test
+const FetchLicencesDal = require('../../../../../app/dal/users/external/setup/fetch-licences.dal.js')
+
+describe('Users - External - Setup - Fetch Licences DAL', () => {
+  let licenceData1
+  let licenceData2
+  let licenceData3
+  let licenceData4
+  let userEntity
+
+  before(async () => {
+    userEntity = await LicenceEntityHelper.add()
+
+    // Add the licences in non-alphabetical order to ensure the service is correctly ordering them by licenceRef,
+    // not just returning them in the order they were added
+    licenceData3 = await _licenceData(userEntity, 'FE/TC/H/US/ER/03', 'primary_user')
+    licenceData1 = await _licenceData(userEntity, 'FE/TC/H/US/ER/01', 'user')
+    licenceData4 = await _licenceData(userEntity, 'FE/TC/H/US/ER/04', 'user_returns')
+
+    // This will not be returned in the results because no licence entity role will be created, that links the user to
+    // the licence
+    licenceData2 = await _licenceData(userEntity, 'FE/TC/H/US/ER/02')
+  })
+
+  after(async () => {
+    await licenceData4.clean()
+    await licenceData3.clean()
+    await licenceData2.clean()
+    await licenceData1.clean()
+
+    await userEntity.$query().delete()
+  })
+
+  describe('when called', () => {
+    it('returns the matching licences', async () => {
+      const result = await FetchLicencesDal.go(userEntity.id)
+
+      expect(result).to.equal([
+        {
+          id: licenceData1.licence.id,
+          licenceRef: licenceData1.licence.licenceRef,
+          licenceVersions: [
+            {
+              id: licenceData1.licenceVersion.id,
+              issueDate: null,
+              licenceId: licenceData1.licence.id,
+              startDate: licenceData1.licenceVersion.startDate,
+              status: 'current',
+              company: { id: licenceData1.company.id, name: licenceData1.company.name, type: 'organisation' }
+            }
+          ]
+        },
+        {
+          id: licenceData3.licence.id,
+          licenceRef: licenceData3.licence.licenceRef,
+          licenceVersions: [
+            {
+              id: licenceData3.licenceVersion.id,
+              issueDate: null,
+              licenceId: licenceData3.licence.id,
+              startDate: licenceData3.licenceVersion.startDate,
+              status: 'current',
+              company: { id: licenceData3.company.id, name: licenceData3.company.name, type: 'organisation' }
+            }
+          ]
+        },
+        {
+          id: licenceData4.licence.id,
+          licenceRef: licenceData4.licence.licenceRef,
+          licenceVersions: [
+            {
+              id: licenceData4.licenceVersion.id,
+              issueDate: null,
+              licenceId: licenceData4.licence.id,
+              startDate: licenceData4.licenceVersion.startDate,
+              status: 'current',
+              company: { id: licenceData4.company.id, name: licenceData4.company.name, type: 'organisation' }
+            }
+          ]
+        }
+      ])
+    })
+  })
+})
+
+async function _licenceData(userEntity, licenceRef, role) {
+  const companyEntityId = generateUUID()
+  const licence = await LicenceHelper.add({ licenceRef })
+  const licenceDocumentHeader = await LicenceDocumentHeaderHelper.add({
+    companyEntityId,
+    licenceRef: licence.licenceRef
+  })
+
+  const licenceEntityRoles = await _licenceEntityRoles(userEntity.id, companyEntityId, role)
+
+  const supersededCompany = await CompanyHelper.add({ name: 'Superseded Holder' })
+  const licenceVersionSuperseded = await LicenceVersionHelper.add({
+    companyId: supersededCompany.id,
+    licenceId: licence.id,
+    status: 'superseded'
+  })
+
+  const currentCompany = await CompanyHelper.add({ name: 'Current Holder' })
+  const licenceVersionCurrent = await LicenceVersionHelper.add({
+    companyId: currentCompany.id,
+    issue: licenceVersionSuperseded.issue + 1,
+    licenceId: licence.id
+  })
+
+  return {
+    company: currentCompany,
+    licence,
+    licenceEntityRoles,
+    licenceDocumentHeader,
+    licenceVersion: licenceVersionCurrent,
+    clean: async () => {
+      await supersededCompany.$query().delete()
+      await currentCompany.$query().delete()
+      await licence.$query().delete()
+      await licenceDocumentHeader.$query().delete()
+      await licenceVersionSuperseded.$query().delete()
+      await licenceVersionSuperseded.$query().delete()
+      await licenceVersionCurrent.$query().delete()
+
+      for (const licenceEntityRole of licenceEntityRoles) {
+        await licenceEntityRole.$query().delete()
+      }
+    }
+  }
+}
+
+async function _licenceEntityRoles(licenceEntityId, companyEntityId, role) {
+  const licenceEntityRoles = []
+
+  // If the role is `primary_user` or `user`, then we can just add that role.
+  if (role) {
+    const licenceEntityRole = await LicenceEntityRoleHelper.add({ companyEntityId, licenceEntityId, role })
+
+    licenceEntityRoles.push(licenceEntityRole)
+  }
+
+  // If the role is `user_returns`, then we need to add both a `user` role and a `user_returns` role. We'll already have
+  // added the `user_returns` role above, so we just need to add the `user` role here.
+  if (role === 'user_returns') {
+    const licenceEntityRole = await LicenceEntityRoleHelper.add({ companyEntityId, licenceEntityId, role: 'user' })
+
+    licenceEntityRoles.push(licenceEntityRole)
+  }
+
+  return licenceEntityRoles
+}
