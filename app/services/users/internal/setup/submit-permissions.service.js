1330d1bf012c374c38de5917925974af19bc7f25
Jason Claxton <jason.claxton@defra.gov.uk>
2026-05-11 12:40:41 +0100

    Update `SubmitPermissionsService` and tests

--- app/services/users/internal/setup/submit-permissions.service.js
+++ app/services/users/internal/setup/submit-permissions.service.js
@@ -6,28 +6,34 @@
  * @module SubmitPermissionsService
  */
 
+const { formatValidationResult } = require('../../../../presenters/base.presenter.js')
+const { flashNotification } = require('../../../../lib/general.lib.js')
+const FetchSessionDal = require('../../../../dal/fetch-session.dal.js')
 const PermissionsPresenter = require('../../../../presenters/users/internal/setup/permissions.presenter.js')
 const PermissionsValidator = require('../../../../validators/users/internal/setup/permissions.validator.js')
-const FetchSessionDal = require('../../../../dal/fetch-session.dal.js')
-const { formatValidationResult } = require('../../../../presenters/base.presenter.js')
 
 /**
  * Orchestrates validating the data for the '' page
  *
  * @param {string} sessionId - The UUID of the current session
  * @param {object} payload - The submitted form data
+ * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
  *
  * @returns {Promise<object>} The data formatted for the view template
  */
-async function go(sessionId, payload) {
+async function go(sessionId, payload, yar) {
   const session = await FetchSessionDal.go(sessionId)
 
   const validationResult = _validate(payload)
 
   if (!validationResult) {
+    _notification(session, payload, yar)
+
     await _save(session, payload)
 
-    return {}
+    return {
+      redirectUrl: `/system/users/internal/setup/${sessionId}/check`
+    }
   }
 
   const pageData = PermissionsPresenter.go(session)
@@ -38,7 +44,15 @@ async function go(sessionId, payload) {
   }
 }
 
+function _notification(session, payload, yar) {
+  if (session.checkPageVisited && session.permissions !== payload.permissions) {
+    flashNotification(yar, 'Updated', 'Permissions updated')
+  }
+}
+
 async function _save(session, payload) {
+  session.permissions = payload.permissions
+
   return session.$update()
 }
 
--- test/services/users/internal/setup/submit-permissions.service.test.js
+++ test/services/users/internal/setup/submit-permissions.service.test.js
@@ -17,36 +17,99 @@ const FetchSessionDal = require('../../../../../app/dal/fetch-session.dal.js')
 // Thing under test
 const SubmitPermissionsService = require('../../../../../app/services/users/internal/setup/submit-permissions.service.js')
 
-describe('Users - Internal - Setup - Permissions Service', () => {
+describe('Users - Internal - Setup - Submit Permissions Service', () => {
+  let fetchSessionStub
   let payload
   let session
   let sessionData
+  let yarStub
 
   beforeEach(() => {
-    payload = { placeholder: 'change me' }
     sessionData = {}
 
     session = SessionModelStub.build(Sinon, sessionData)
 
-    Sinon.stub(FetchSessionDal, 'go').resolves(session)
+    fetchSessionStub = Sinon.stub(FetchSessionDal, 'go').resolves(session)
+
+    yarStub = { flash: Sinon.stub() }
   })
 
   afterEach(() => {
     Sinon.restore()
   })
 
-  describe('when called', () => {
-    it('saves the submitted value', async () => {
-      await SubmitPermissionsService.go(session.id, payload)
+  describe('when called with a valid payload', () => {
+    beforeEach(() => {
+      payload = { permissions: 'basic' }
+    })
 
-      expect(session).to.equal(session)
+    it('saves the submitted value', async () => {
+      await SubmitPermissionsService.go(session.id, payload, yarStub)
+
+      expect(session).to.equal({
+        ...session,
+        permissions: 'basic'
+      })
       expect(session.$update.called).to.be.true()
     })
 
     it('continues the journey', async () => {
-      const result = await SubmitPermissionsService.go(session.id, payload)
+      const result = await SubmitPermissionsService.go(session.id, payload, yarStub)
 
-      expect(result).to.equal({})
+      expect(result).to.equal({
+        redirectUrl: `/system/users/internal/setup/${session.id}/check`
+      })
+    })
+
+    describe('and the check page has', () => {
+      describe('been visited', () => {
+        beforeEach(() => {
+          session = SessionModelStub.build(Sinon, {
+            ...sessionData,
+            checkPageVisited: true,
+            permissions: 'basic'
+          })
+
+          fetchSessionStub.resolves(session)
+        })
+
+        describe('and the "session" and "payload" value', () => {
+          describe('match', () => {
+            beforeEach(() => {
+              payload = { permissions: 'basic' }
+            })
+
+            it('does not set a notification', async () => {
+              await SubmitPermissionsService.go(session.id, payload, yarStub)
+
+              expect(yarStub.flash.called).to.be.false()
+            })
+          })
+
+          describe('do not match', () => {
+            beforeEach(() => {
+              payload = { permissions: 'super' }
+            })
+
+            it('sets a notification', async () => {
+              await SubmitPermissionsService.go(session.id, payload, yarStub)
+
+              const [flashType, bannerMessage] = yarStub.flash.args[0]
+
+              expect(flashType).to.equal('notification')
+              expect(bannerMessage).to.equal({ titleText: 'Updated', text: 'Permissions updated' })
+            })
+          })
+        })
+      })
+
+      describe('not been visited', () => {
+        it('does not set a notification', async () => {
+          await SubmitPermissionsService.go(session.id, payload, yarStub)
+
+          expect(yarStub.flash.called).to.be.false()
+        })
+      })
     })
   })
 
@@ -56,25 +119,27 @@ describe('Users - Internal - Setup - Permissions Service', () => {
     })
 
     it('returns page data for the view, with errors', async () => {
-      const result = await SubmitPermissionsService.go(session.id, payload)
+      const result = await SubmitPermissionsService.go(session.id, payload, yarStub)
 
       expect(result).to.equal({
         backLink: {
-          href: '',
+          href: `/system/users/internal/setup/${session.id}/user-email`,
           text: 'Back'
         },
+        permissions: undefined,
         error: {
+          permissions: {
+            text: 'Select a permission'
+          },
           errorList: [
             {
-              href: '#placeholder',
-              text: '"placeholder" is required'
+              href: '#permissions',
+              text: 'Select a permission'
             }
-          ],
-          placeholder: {
-            text: '"placeholder" is required'
-          }
+          ]
         },
-        pageTitle: ''
+        pageTitle: 'Select permissions for the user',
+        pageTitleCaption: 'Internal'
       })
     })
   })
