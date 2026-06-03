c2461d4a4a0161cb52c334a44ceb8dd0c2cdf146
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-03 15:50:34 +0100

    Create `SendVerificationEmailService`

---
+++ app/services/users/internal/setup/send-verification-email.service.js
@@ -0,0 +1,65 @@
+'use strict'
+
+/**
+ * Orchestrates sending the verification email to the new user, recording the results, and checking the status
+ * @module SendVerificationEmailService
+ */
+
+const CheckNotificationStatusService = require('../../../notifications/check-notification-status.service.js')
+const CreateEmailRequest = require('../../../../requests/notify/create-email.request.js')
+const NotifyUpdatePresenter = require('../../../../presenters/notices/setup/notify-update.presenter.js')
+
+const { pause } = require('../../../../lib/general.lib.js')
+
+const notifyConfig = require('../../../../../config/notify.config.js')
+
+const { NOTIFY_TEMPLATES } = require('../../../../lib/notify-templates.lib.js')
+
+/**
+ * Orchestrates sending the verification email to the new user, recording the results, and checking the status
+ *
+ * @param {object} notification - The notification linked to the verification email to be sent
+ *
+ * @returns {string} the status of the notification'
+ */
+async function go(notification) {
+  const notificationStatus = await _sendEmail(notification)
+
+  if (notificationStatus !== 'error') {
+    // If we rush to check the status too quickly, Notify will respond with a result for emails that is 'still sending'.
+    // The default wait is 5 seconds, which we have found is more than enough time.
+    await pause(notifyConfig.waitForStatus)
+
+    await CheckNotificationStatusService.go(notification)
+  }
+
+  return notificationStatus
+}
+
+async function _createEmailRequest(notification) {
+  const { personalisation, recipient } = notification
+
+  const templateId = NOTIFY_TEMPLATES.users.verificationEmail
+
+  const notifyResult = await CreateEmailRequest.send(templateId, recipient, { personalisation })
+
+  return NotifyUpdatePresenter.go(notifyResult)
+}
+
+async function _recordResult(notification, sendResult) {
+  const { notifyError, notifyId, notifyStatus, plaintext, status } = sendResult
+
+  await notification.$query().patch({ notifyError, notifyId, notifyStatus, plaintext, status })
+}
+
+async function _sendEmail(notification) {
+  const sendResult = await _createEmailRequest(notification)
+
+  await _recordResult(notification, sendResult)
+
+  return sendResult.status
+}
+
+module.exports = {
+  go
+}
