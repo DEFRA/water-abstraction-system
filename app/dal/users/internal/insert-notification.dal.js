25fc1b8e25d0a9835390796bff8e2141808e1f31
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-04 15:53:07 +0100

    Create `InsertNotificationDal` and tests

---
+++ app/dal/users/internal/insert-notification.dal.js
@@ -0,0 +1,37 @@
+'use strict'
+
+/**
+ * Creates a notification record for the new user
+ * @module InsertNotificationDal
+ */
+
+const NotificationModel = require('../../../models/notification.model.js')
+const { domains } = require('../../../../config/server.config.js')
+
+/**
+ * Creates a notification record for the new user
+ *
+ * @param {string} email - The email address of the new user
+ * @param {string} resetGuid - The reset GUID for the new user
+ * @param {object} trx - The transaction instance
+ *
+ * @returns {Promise<object>} The created notification
+ */
+async function go(email, resetGuid, trx) {
+  const personalisation = {
+    unique_create_password_link: `${domains.internal}/reset_password_change_password?resetGuid=${resetGuid}`
+  }
+
+  const notificationData = {
+    messageRef: 'new_internal_user_email',
+    messageType: 'email',
+    personalisation,
+    recipient: email
+  }
+
+  return NotificationModel.query(trx).insert(notificationData)
+}
+
+module.exports = {
+  go
+}
---
+++ test/dal/users/internal/insert-notification.dal.test.js
@@ -0,0 +1,42 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { beforeEach, describe, it } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const NotificationModel = require('../../../../app/models/notification.model.js')
+const { domains } = require('../../../../config/server.config.js')
+const { generateUserName } = require('../../../support/helpers/user.helper.js')
+const { generateUUID } = require('../../../../app/lib/general.lib.js')
+
+// Thing under test
+const InsertNotificationDal = require('../../../../app/dal/users/internal/insert-notification.dal.js')
+
+describe('Users - Internal - Insert Notification DAL', () => {
+  let email
+  let resetGuid
+
+  describe('when called', () => {
+    beforeEach(() => {
+      email = generateUserName()
+      resetGuid = generateUUID()
+    })
+
+    it('creates a notification', async () => {
+      await InsertNotificationDal.go(email, resetGuid)
+
+      const notification = await NotificationModel.query().where('recipient', email).limit(1).first()
+
+      expect(notification.messageRef).to.equal('new_internal_user_email')
+      expect(notification.messageType).to.equal('email')
+      expect(notification.personalisation).to.equal({
+        unique_create_password_link: `${domains.internal}/reset_password_change_password?resetGuid=${resetGuid}`
+      })
+      expect(notification.recipient).to.equal(email)
+    })
+  })
+})
