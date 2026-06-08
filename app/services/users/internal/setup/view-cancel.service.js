55980fb53c42a4a465fd27fe1ec4b6f84ef38a1c
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-08 14:49:45 +0100

    Create `ViewCancelService` and tests

---
+++ app/services/users/internal/setup/view-cancel.service.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * Orchestrates presenting the data for `/users/internal/setup/{sessionId}/cancel` page
+ * @module ViewCancelService
+ */
+
+const CancelPresenter = require('../../../../presenters/users/internal/setup/cancel.presenter.js')
+const FetchSessionDal = require('../../../../dal/fetch-session.dal.js')
+
+/**
+ * Orchestrates presenting the data for `/users/internal/setup/{sessionId}/cancel` page
+ *
+ * @param {string} sessionId - The UUID of the current session
+ *
+ * @returns {Promise<object>} The view data for the cancel page
+ */
+async function go(sessionId) {
+  const session = await FetchSessionDal.go(sessionId)
+
+  const pageData = CancelPresenter.go(session)
+
+  return {
+    ...pageData
+  }
+}
+
+module.exports = {
+  go
+}
