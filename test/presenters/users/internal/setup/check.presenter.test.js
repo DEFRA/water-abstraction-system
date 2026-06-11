7f2c03449b00b74e9850bc2bb78a138bfa815e7d
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-11 16:23:41 +0100

    Add `showEmailChangeLink` to `CheckPresenter` and update tests

--- test/presenters/users/internal/setup/check.presenter.test.js
+++ test/presenters/users/internal/setup/check.presenter.test.js
@@ -13,7 +13,7 @@ const { generateUUID } = require('../../../../../app/lib/general.lib.js')
 // Thing under test
 const CheckPresenter = require('../../../../../app/presenters/users/internal/setup/check.presenter.js')
 
-describe('Users - Internal - Setup - Check Presenter', () => {
+describe.only('Users - Internal - Setup - Check Presenter', () => {
   let session
 
   beforeEach(() => {
@@ -34,7 +34,42 @@ describe('Users - Internal - Setup - Check Presenter', () => {
         },
         pageTitle: 'Check user',
         pageTitleCaption: 'Internal',
-        permission: 'Billing and Data'
+        permission: 'Billing and Data',
+        showEmailChangeLink: true
+      })
+    })
+  })
+
+  describe('the "showEmailChangeLink" property', () => {
+    describe('when creating a new user', () => {
+      it('returns true', () => {
+        const result = CheckPresenter.go(session)
+
+        expect(result.showEmailChangeLink).to.be.true()
+      })
+    })
+
+    describe('when editing an existing user that is awaiting verification', () => {
+      beforeEach(() => {
+        session.user = { status: 'awaiting' }
+      })
+
+      it('returns true', () => {
+        const result = CheckPresenter.go(session)
+
+        expect(result.showEmailChangeLink).to.be.true()
+      })
+    })
+
+    describe('when editing an existing user that is NOT awaiting verification', () => {
+      beforeEach(() => {
+        session.user = { status: 'enabled' }
+      })
+
+      it('returns false', () => {
+        const result = CheckPresenter.go(session)
+
+        expect(result.showEmailChangeLink).to.be.false()
       })
     })
   })
