7b4098102f6617c6f4cb2d318ab51fdd4aeb3658
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-12 12:16:22 +0100

    Tidy up JSDoc a bit more

--- app/dal/create-session.dal.js
+++ app/dal/create-session.dal.js
@@ -10,13 +10,14 @@ const SessionModel = require('../models/session.model.js')
 /**
  * Creates a new session.
  *
- * @param {object} data - The data for the new session.
+ * NOTE: `data` defaults to `{}` when a new record is created because Objection.js throws a 'The query is empty' error
+ * if we pass nothing into `insert()`.
+ *
+ * @param {object} [data={}] - The data for the new session.
  *
  * @returns {Promise<object>} The ID of the newly created session.
  */
 async function go(data = {}) {
-  // NOTE: data defaults to {} when a new record is created because Objection.js throws a 'The query is empty' if we
-  // pass nothing into `insert()`.
   return SessionModel.query().insert({ data }).returning('id')
 }
 
