08e4b40242763977e290b1c6b10e87ca7cf5ebd8
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-12 12:05:33 +0100

    Create `CreateSessionDal` and tests

---
+++ app/dal/create-session.dal.js
@@ -0,0 +1,23 @@
+'use strict'
+
+/**
+ * Creates a new session.
+ * @module CreateSessionDal
+ */
+
+const SessionModel = require('../models/session.model.js')
+
+/**
+ * Creates a new session.
+ *
+ * @param {Object} data - The data for the new session.
+ */
+async function go(data = {}) {
+  // NOTE: data defaults to {} when a new record is created because Objection.js throws a 'The query is empty' if we
+  // pass nothing into `insert()`.
+  return SessionModel.query().insert({ data }).returning('id')
+}
+
+module.exports = {
+  go
+}
---
+++ test/dal/create-session.dal.test.js
@@ -0,0 +1,55 @@
+'use strict'
+
+// Test framework dependencies
+const Lab = require('@hapi/lab')
+const Code = require('@hapi/code')
+
+const { describe, it, before } = (exports.lab = Lab.script())
+const { expect } = Code
+
+// Test helpers
+const SessionModel = require('../../app/models/session.model.js')
+
+// Thing under test
+const CreateSessionDal = require('../../app/dal/create-session.dal.js')
+
+describe('DAL - Create session dal', () => {
+  describe('when there is no data', () => {
+    it('creates an empty session and returns the session ID', async () => {
+      const result = await CreateSessionDal.go()
+
+      const session = await SessionModel.query().findById(result.id)
+
+      expect(session).to.equal(
+        {
+          data: {},
+          id: result.id
+        },
+        { skip: ['createdAt', 'updatedAt'] }
+      )
+    })
+  })
+
+  describe('when there is data', () => {
+    let data
+
+    before(() => {
+      data = { testData: 'Here is some test data' }
+    })
+
+    it('creates a session with the data and returns the session ID', async () => {
+      const result = await CreateSessionDal.go(data)
+
+      const session = await SessionModel.query().findById(result.id)
+
+      expect(session).to.equal(
+        {
+          data: { testData: 'Here is some test data' },
+          id: result.id,
+          testData: 'Here is some test data'
+        },
+        { skip: ['createdAt', 'updatedAt'] }
+      )
+    })
+  })
+})
