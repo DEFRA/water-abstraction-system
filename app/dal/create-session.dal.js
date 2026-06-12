ff9e5d63c9c03b4bf10740a4f66ff619fbeb0f89
Jason Claxton <30830544+Jozzey@users.noreply.github.com>
2026-06-12 12:37:11 +0100

    Update app/dal/create-session.dal.js
    Co-authored-by: Alan Cruikshanks <alan.cruikshanks@gmail.com>

--- app/dal/create-session.dal.js
+++ app/dal/create-session.dal.js
@@ -8,7 +8,7 @@
 const SessionModel = require('../models/session.model.js')
 
 /**
- * Creates a new session.
+ * Creates a new session
  *
  * NOTE: `data` defaults to `{}` when a new record is created because Objection.js throws a 'The query is empty' error
  * if we pass nothing into `insert()`.
