a9460bdaaf94b3b5e67302b72b4100643261ff59
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2026-06-17 09:15:32 +0100

    Update link from view notification
    Go back to licence -> Go back to communications

--- test/services/notifications/view-notification.service.test.js
+++ test/services/notifications/view-notification.service.test.js
@@ -60,7 +60,7 @@ describe('Notifications - View Notification service', () => {
           activeNavBar: 'search',
           address: [],
           alertDetails: null,
-          backLink: { href: `/system/licences/${licence.id}/communications`, text: 'Go back to licence' },
+          backLink: { href: `/system/licences/${licence.id}/communications`, text: 'Go back to communications' },
           contents: notification.plaintext,
           errorDetails: null,
           messageType: 'email',
