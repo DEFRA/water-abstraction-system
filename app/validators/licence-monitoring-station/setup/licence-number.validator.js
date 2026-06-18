019746feb12ef8985b49a97fd2f68711d847540f
Jonathan Goulding <58443816+jonathangoulding@users.noreply.github.com>
2026-06-18 07:17:17 +0100

    Only tag an active licence for a monitoring station (#3454)
    https://eaflood.atlassian.net/browse/WATER-5684
    
    This change stops the user from tagging a licence that has ended.
    
    We have also lifted out the fetch logic within the service and moved this into the dal, this is part of our ongoing effort to lift database logic out of services into the dal.

--- app/validators/licence-monitoring-station/setup/licence-number.validator.js
+++ app/validators/licence-monitoring-station/setup/licence-number.validator.js
@@ -8,26 +8,31 @@
 
 const Joi = require('joi')
 
+const { isFalse } = require('../../helpers/is-false.validator.js')
+
 const ENTER_A_LICENCE_NUMBER_ERROR = 'Enter a licence number'
 
 /**
  * Validates data submitted for the `/licence-monitoring-station/setup/{sessionId}/licence-number` page
  *
  * @param {object} payload - The payload from the request to be validated
- * @param {boolean} licenceExists - the result of checking if the licence ref is present in the database
+ * @param {object|null} licence - the licence if it exists
  *
  * @returns {object} the result from calling Joi's schema.validate(). It will be an object with a `value:` property. If
  * any errors are found the `error:` property will also exist detailing what the issues were
  */
-function go(payload, licenceExists) {
+function go(payload, licence) {
+  const licenceExists = !!licence
+  const licenceHasEnded = licence?.$ended() ?? false
+
   const schema = Joi.object({
     licenceRef: Joi.string()
       .required()
-      .custom((value, helpers) => {
-        return _licenceExists(value, helpers, licenceExists)
-      }, 'Custom Licence Validation')
+      .custom(isFalse(!licenceExists, 'invalidLicence'))
+      .custom(isFalse(licenceHasEnded, 'licenceEnded'))
       .messages({
         invalidLicence: 'Licence could not be found',
+        licenceEnded: 'The licence has ended',
         'any.required': ENTER_A_LICENCE_NUMBER_ERROR,
         'any.only': ENTER_A_LICENCE_NUMBER_ERROR,
         'string.empty': ENTER_A_LICENCE_NUMBER_ERROR
@@ -35,14 +40,6 @@ function go(payload, licenceExists) {
   })
 
   return schema.validate(payload, { abortEarly: false })
-}
-
-function _licenceExists(value, helpers, licenceExists) {
-  if (licenceExists) {
-    return value
-  }
-
-  return helpers.error('invalidLicence')
 }
 
 module.exports = {
