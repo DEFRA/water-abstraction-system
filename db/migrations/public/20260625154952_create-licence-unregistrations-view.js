bbc17d3944bd73c770a65960707f31078e4b6724
Jason Claxton <jason.claxton@defra.gov.uk>
2026-06-25 17:03:03 +0100

    Fix linting issues

--- db/migrations/public/20260625154952_create-licence-unregistrations-view.js
+++ db/migrations/public/20260625154952_create-licence-unregistrations-view.js
@@ -4,16 +4,7 @@ const viewName = 'licence_unregistrations'
 
 exports.up = function (knex) {
   return knex.schema.createView(viewName, (view) => {
-    view.as(
-      knex(viewName)
-        .withSchema('water')
-        .select([
-          'id',
-          'created_by',
-          'licence_id',
-          'created_at'
-        ])
-    )
+    view.as(knex(viewName).withSchema('water').select(['id', 'created_by', 'licence_id', 'created_at']))
   })
 }
 
