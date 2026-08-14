1c4b8f0ba5464e2ab7e02476057d9a58f01c0c87
Jason Claxton <jason.claxton@defra.gov.uk>
2026-08-14 16:23:32 +0100

    Fix linting issue

--- src/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.js
+++ src/presenters/notices/setup/abstraction-alerts/check-licence-matches.presenter.js
@@ -57,9 +57,8 @@ function _action(sessionId, licenceMonitoringStation) {
 
 function _actionHeaderLink(absPeriodFilter, sessionId, licenceMonitoringStations) {
   if (absPeriodFilter) {
-    const numberOfAlerts = (licenceMonitoringStations.length === 1
-    ? '1 alert'
-    : `${licenceMonitoringStations.length} alerts`)
+    const numberOfAlerts =
+      licenceMonitoringStations.length === 1 ? '1 alert' : `${licenceMonitoringStations.length} alerts`
 
     return {
       link: `/system/notices/setup/${sessionId}/abstraction-alerts/remove-filtered-thresholds/${absPeriodFilter}`,
