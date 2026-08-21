d37780d8e6f63b040b299f9e4ad873e6a53b2419
Jason Claxton <jason.claxton@defra.gov.uk>
2026-08-21 18:08:00 +0100

    Update `SubmitAlertThresholdsService` to clear the filter if the selected thresholds change

--- src/services/notices/setup/abstraction-alerts/submit-alert-thresholds.service.js
+++ src/services/notices/setup/abstraction-alerts/submit-alert-thresholds.service.js
@@ -16,13 +16,16 @@ import AlertThresholdsValidator from '../../../../validators/notices/setup/alert
  *
  * @param {string} sessionId
  * @param {object} payload - The submitted form data
+ * @param {object} yar - The Hapi `request.yar` session manager passed on by the controller
  *
  * @returns {Promise<object>} - The data formatted for the view template
  */
-export default async function submitAlertThresholdsService(sessionId, payload) {
+export default async function submitAlertThresholdsService(sessionId, payload, yar) {
   const session = await FetchSessionDal(sessionId)
 
   handleOneOptionSelected(payload, 'alertThresholds')
+
+  _clearFilterIfRequired(session, payload, yar)
 
   session.alertThresholds = payload.alertThresholds
 
@@ -39,6 +42,23 @@ export default async function submitAlertThresholdsService(sessionId, payload) {
   return {
     error,
     ...pageData
+  }
+}
+
+/**
+ * Clear the licence match filter when the user changes their previously selected alert thresholds
+ *
+ * The filter is only relevant to the thresholds it was applied against, so once they change we drop it to avoid
+ * filtering the licence matches by a threshold that is no longer selected.
+ *
+ * The `session.alertThresholds` array will always have a length of zero upon the first visit to the "Which thresholds
+ * do you need to send an alert for?" page. In this case a filter will not have been set yet.
+ *
+ * @private
+ */
+function _clearFilterIfRequired(session, payload, yar) {
+  if (session.alertThresholds.length > 0 && session.alertThresholds !== payload.alertThresholds) {
+    yar.clear(`checkLicenceMatchesFilter-${session.id}`)
   }
 }
 
